4d2d9abbf6a8f79f74f39f619dcee537cae9bafd
Elton Fernando Goedert <elton@ailos.coop.br>
2017-01-25 12:34:57 +0000

    Desfeito changest
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/PRINCIPAL;C9405

--- ayllosdev/telas/dctror/manter_rotina.php
+++ ayllosdev/telas/dctror/manter_rotina.php
@@ -7,7 +7,6 @@
  * --------------
  * ALTERAÇÕES   : 24/06/2016 - Ajustado para chamar formulario de impressão para Opcao "A" (Lucas Ranghetti #448006 )
  * -------------- 
- *				  12/01/2017 - Incluir flprovis no array da custodia/desconto (Lucas Ranghetti #571653)
  */
 ?> 
 
@@ -358,7 +357,6 @@
 				echo 'aux[\'flgcusto\'] = "'.getByTagName($registro,'flgcusto').'";';
 				echo 'aux[\'flgdesco\'] = "'.getByTagName($registro,'flgdesco').'";';
 				echo 'aux[\'cdhistor\'] = "'.getByTagName($registro,'cdhistor').'";';
-				echo 'aux[\'flprovis\'] = "'.getByTagName($registro,'flprovis').'";';  				
 
 				echo 'arrayCustdesc['.$i.'] = aux;';
 
