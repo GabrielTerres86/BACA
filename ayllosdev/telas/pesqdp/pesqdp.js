f20e19fb1c0be4f58497a62b221db144a873c09d
Andrey Formigari (Mouts) <mouts-andrey.formigari@ailos.coop.br>
2017-02-15 11:56:34 +0000

    Zerar o tabindex após uma consulta de cheque.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/2017_03;C9767

--- ayllosdev/telas/pesqdp/pesqdp.js
+++ ayllosdev/telas/pesqdp/pesqdp.js
@@ -9,7 +9,9 @@
  *				  14/08/2015 - Removidos todos os campos da tela menos os campos
  *							   Data do Deposito e Valor do cheque. Adicionado novos campos
  *							   para filtro, numero de conta e numero de cheque, conforme
- *							   solicitado na melhoria 300189 (Kelvin)	
+ *							   solicitado na melhoria 300189 (Kelvin)
+ *                15/02/2017 - Zerar os tabindex quando busca um cheque e clica no botão
+                               voltar. Andrey Formigari (Mouts) SD 612621.
  * --------------
  */
 
@@ -606,7 +608,8 @@ function Gera_ImpressaoTXT() {
 
 // botoes
 function btnVoltar() {
-	estadoInicial();
+    estadoInicial();
+    zerarTabIndex();
 	return false;
 }
 
@@ -898,4 +901,8 @@ function validaArquivo(){
 	Gera_ImpressaoTXT();
 
 	return false;
+}
+
+function zerarTabIndex() {
+    $('.formulario input').removeAttr('tabindex');
 }
