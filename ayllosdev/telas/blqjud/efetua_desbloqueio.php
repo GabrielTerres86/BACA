03c3b6786bd118436497ca1e5d7368a2dc89e349
Jose Eduardo Gracik <eduardo@ailos.coop.br>
2017-05-05 02:16:11 +0000

    Liberação Emergencial 04/05
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/PROD;C11124

--- ayllosdev/telas/blqjud/efetua_desbloqueio.php
+++ ayllosdev/telas/blqjud/efetua_desbloqueio.php
@@ -3,7 +3,7 @@
 	//***********************************************************************************//
 	//*** Fonte: efetua_desbloqueio.php                                               ***//
 	//*** Autor: Lucas R.                                                             ***//
-	//*** Data : Julho/2013                   Última Alteração:  15/09/2014           ***//
+	//*** Data : Julho/2013                   Última Alteração:  03/05/2017           ***//
 	//***                                                                             ***//
 	//*** Objetivo  : Efetua o Desbloqueio Judicial                                   ***//	
 	//***                                                                             ***//
@@ -16,6 +16,9 @@
 	//***			  03/11/2016 - Realizar a chamada da rotina EFETUA-DESBLOQUEIO-JUD***//
 	//***					       diretamente do oracle via mensageria				  ***//
 	//***						   (Renato Darosci - Supero)                          ***//
+	//***															                  ***//
+	//***			  03/05/2017 - Passagem do parametro NROFIDES para gravacao dos   ***//
+	//***					       dados. (Jaison/Andrino)                            ***//
 	//***															                  ***//
 	//***********************************************************************************//
 	
