99bd7730dc1ecce2f9efc4eb79fa369228e97cda
Lucas Afonso Lombardi Moreira <lucas.moreira@ailos.coop.br>
2017-06-27 16:13:40 +0000

    merge com prod
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/2017_08;C12076

--- ayllosdev/telas/custod/custod.php
+++ ayllosdev/telas/custod/custod.php
@@ -5,7 +5,7 @@
  * DATA CRIAÇÃO : 18/01/2012
  * OBJETIVO     : Mostrar tela custod
  * --------------
- * ALTERAÇÕES   :
+ * ALTERAÇÕES   : 16/12/2016 - Alterações referentes ao projeto 300. (Reinert)
  * --------------
  */
 
@@ -76,7 +76,7 @@ require_once("../../includes/carrega_permissoes.php");
                                                         <table width="100%" border="0" cellpadding="10" cellspacing="0" style="background-color: #F4F3F0;">
                                                             <tr>
                                                                 <td align="center">
-                                                                    <table width="620" border="0" cellpadding="0" cellspacing="0" style="background-color: #F4F3F0;">
+                                                                    <table width="1000" border="0" cellpadding="0" cellspacing="0" style="background-color: #F4F3F0;">
                                                                         <tr>
                                                                             <td>
                                                                                 <!-- INCLUDE DA TELA DE PESQUISA -->
