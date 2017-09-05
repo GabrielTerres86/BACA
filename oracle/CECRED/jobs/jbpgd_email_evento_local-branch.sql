b16606de5ed05c6d84144074c73fc6084c75287a
Carlos Henrique Weinhold <carlos.weinhold@ailos.coop.br>
2017-09-05 13:14:32 +0000

    #551231 Alterado o owner do job para PROGRID e configurado para rodar de segunda a sexta
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/2017_09;C13376

--- oracle/CECRED/jobs/jbpgd_email_evento_local-branch.sql
+++ oracle/CECRED/jobs/jbpgd_email_evento_local-branch.sql
@@ -1,10 +1,11 @@
 -- 14/06/2017 - #551231 Padronização do nome do job e inclusão dos logs de controle de início, erro e fim de execução (Carlos)
-
+-- 05/09/2017 - #551231 Alterado o owner do job para PROGRID (Carlos)
 BEGIN
-  sys.dbms_scheduler.drop_job(job_name => 'CECRED.EVENTOPROGRID$');
 
+  sys.dbms_scheduler.drop_job(job_name => 'CECRED.EVENTOPROGRID$');
+    
   sys.dbms_scheduler.create_job(
-    job_name        => 'CECRED.jbpgd_email_evento_local',
+    job_name        => 'PROGRID.jbpgd_email_evento_local',
     job_type        => 'PLSQL_BLOCK',
     job_action      => 'declare 
       vr_dscritic VARCHAR2(1000); 
@@ -15,8 +16,8 @@ BEGIN
           raise_application_error(-20001, vr_dscritic);
         END IF;
       END;',
-    start_date      => '24/03/2017 01:00:00,000000 AMERICA/SAO_PAULO',
-    repeat_interval => 'Freq=DAILY;ByHour=01;ByMinute=00;BySecond=0',
+    start_date      => '19/09/2017 01:00:00,000000 AMERICA/SAO_PAULO',
+    repeat_interval => 'Freq=DAILY;ByHour=01;ByDay=MON,TUE,WED,THU,FRI',
     end_date        => to_date(null),
     job_class       => 'DEFAULT_JOB_CLASS',
     enabled         => TRUE,
