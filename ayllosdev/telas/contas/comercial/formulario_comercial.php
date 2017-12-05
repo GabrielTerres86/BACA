a27d3f2415c5527f3875c14b97f308c8233c38fc
Mateus Zimmermann (Mouts) <mouts-mateus.zimmermann@ailos.coop.br>
2017-12-05 17:36:08 +0000

    Alteração para buscar o Nome da Empresa a partir do CNPJ digitado e regra de alteração do nome da empresa
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/EMERGENCIAL;C15520

--- ayllosdev/telas/contas/comercial/formulario_comercial.php
+++ ayllosdev/telas/contas/comercial/formulario_comercial.php
@@ -15,7 +15,9 @@
 								caracteres especiais que geravam erro no retorno do Ajax. 
 								SD 535228. (Carlos Rafael Tanholi).
  *                 01/12/2016 - Definir a não obrigatoriedade do PEP (Tiago/Thiago SD532690)  
-				   11/10/2017 - Removendo campo caixa postal (PRJ339 - Kelvin).	
+ *   			   11/10/2017 - Removendo campo caixa postal (PRJ339 - Kelvin).	
+ *                 05/12/2017 - Alteração para buscar o Nome da Empresa a partir do CNPJ digitado e regra de alteração do nome da empresa.
+ *                             (Mateus Z - Mouts) 
  */	
 ?>
 <form name="frmDadosComercial" id="frmDadosComercial" class="formulario">	
@@ -57,11 +59,11 @@
 		<input name="nmresemp" id="nmresemp" type="text" value="<?php echo getByTagName($comercial,'nmresemp') ?>" />
 		<br />
 		
+		<label for="nrcpfemp">C.N.P.J.:</label>
+		<input name="nrcpfemp" id="nrcpfemp" type="text" onfocusout="buscaNomePessoa()" value="<?php echo getByTagName($comercial,'nrcpfemp') ?>" />
+		
 		<label for="nmextemp">Nome Empresa:</label>
 		<input name="nmextemp" id="nmextemp" type="text" value="<?php echo getByTagName($comercial,'nmextemp') ?>" />		
-		
-		<label for="nrcpfemp">C.N.P.J.:</label>
-		<input name="nrcpfemp" id="nrcpfemp" type="text" value="<?php echo getByTagName($comercial,'nrcpfemp') ?>" />
 		<br />
 		 				
 		<label for="dsproftl"><?php echo utf8ToHtml('Cargo:') ?></label>
