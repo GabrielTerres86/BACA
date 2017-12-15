8a9187a67804ea9908e519a9e06ede448af25a31
Paulo Samuel Russi <paulo.russi@ailos.coop.br>
2017-12-15 15:14:47 +0000

    Remoção de Fontes...
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho/TEST;C15764

--- ayllosdev/webservice/esteira/RestControllerAnalise.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require_once('../../includes/config.php');
-  require_once('../../includes/funcoes.php');
-  require_once("class_rest_analise.php");
-
-  $oRestAnalise = new RestAnalise();
-  $oRestAnalise->processaRequisicao();
-?>
