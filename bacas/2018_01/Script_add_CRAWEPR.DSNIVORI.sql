69d33502707821da82d609a133da821fdd77fe40
Daniel Silva (AMcom) <CECRED\t0031672>
2018-01-18 18:35:19 +0000

    Script novos campos, Pck Tela Atenda Prestações.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C16374

---
+++ bacas/2018_01/Script_add_CRAWEPR.DSNIVORI.sql
@@ -0,0 +1,6 @@
+alter table CECRED.CRAWEPR add DSNIVORI varchar2(2)
+/
+comment on column CECRED.CRAWEPR.DSNIVORI is 'Risco Original.'
+/
+comment on column CECRED.CRAWEPR.DSNIVRIS is 'Risco Melhora.'
+/
