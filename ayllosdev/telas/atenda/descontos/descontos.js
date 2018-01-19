fa5cbdacce919bf6c106d9334c708bc342928911
Andrei Vieira (Mouts) <rodrigo.andreatta@mouts.info>
2018-01-19 19:09:10 +0000

    Chamado 743079 - Problema da tela ficar travada quando CMLC7 inválido
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/2018_01;C16389

--- ayllosdev/telas/atenda/descontos/descontos.js
+++ ayllosdev/telas/atenda/descontos/descontos.js
@@ -29,6 +29,8 @@
 
 			 21/07/2017 - Ajuste no cadastro de emitentes. Projeto 300. (Lombardi)
 			 
+			 19/01/2018 - Chamado 743079 - Problema da tela ficar travada quando CMLC7 inválido (Andrei-Mouts)
+			 
 ************************************************************************/
 
 // Carrega biblioteca javascript referente ao RATING
@@ -1103,7 +1105,7 @@ function formataCampoCmc7(exitCampo, nomeForm){
 	}
 	
 	if ( exitCampo && valorAtual.length < 34) {
-		showError('error','Valor do CMC-7 inv&aacute;lido.','Alerta - Ayllos','cDsdocmc7.focus(); blockBackground(parseInt($(\'#divRotina\').css(\'z-index\')));');
+		showError('error','Valor do CMC-7 inv&aacute;lido.','Alerta - Ayllos','hideMsgAguardo(); $(\'#dsdocmc7\', \'#'+nomeForm+'\').focus(); blockBackground(parseInt($(\'#divRotina\').css(\'z-index\')));');
 	}
 	
 	//remover os caracteres de formatação
