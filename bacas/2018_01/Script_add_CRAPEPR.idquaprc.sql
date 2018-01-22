578986fe0ae666aeb2b360a2a632ca725ae9e2f0
Daniel Silva (AMcom) <CECRED\t0031672>
2018-01-22 11:06:53 +0000

    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C16402

--- bacas/2018_01/Script_add_CRAPEPR.idquaprc.sql
+++
@@ -1,6 +0,0 @@
-alter table CECRED.CRAPEPR
-add idquaprc number(5) default 1
-/
-comment on column CECRED.CRAPEPR.idquaprc
- is 'Identificacao da qualificacao da operacao - Controle (proposta).'
-/
--- bacas/2018_01/Script_add_CRAWEPR.DSNIVORI.sql
+++
@@ -1,6 +0,0 @@
-alter table CECRED.CRAWEPR add DSNIVORI varchar2(2)
-/
-comment on column CECRED.CRAWEPR.DSNIVORI is 'Risco Original.'
-/
-comment on column CECRED.CRAWEPR.DSNIVRIS is 'Risco Melhora.'
-/
