47bdc51d50b08e353809a917ff3f4a1c51a1d889
Daniel Silva (AMcom) <CECRED\t0031672>
2018-01-23 11:43:34 +0000

    novo
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C16432

---
+++ bacas/2018_01/06_Script_carga_CRAWEPR.DSNIVORI.sql
@@ -0,0 +1,33 @@
+declare
+  conta  number(10) :=0;
+
+  CURSOR cr_risco IS
+  select t.cdcooper
+       , t.nrdconta
+       , t.nrctremp
+       , t.dsnivris
+    from CRAWEPR t where rownum < 50000;
+  rw_risco cr_risco%ROWTYPE;
+
+begin
+
+  open cr_risco;
+
+  loop
+    fetch cr_risco into rw_risco;
+    exit when cr_risco%notfound;
+
+--    conta := conta + 1;
+
+--    dbms_output.put_line('Update '||conta);
+
+    update CRAWEPR
+       set DSNIVORI = rw_risco.DSNIVRIS
+     where CDCOOPER = rw_risco.CDCOOPER
+       and NRDCONTA = rw_risco.NRDCONTA
+       and NRCTREMP = rw_risco.NRCTREMP;
+
+  end loop;
+
+  close cr_risco;
+end;
