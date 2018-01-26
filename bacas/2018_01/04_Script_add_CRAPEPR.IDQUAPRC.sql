58ace3fab806a01d56d8444a1492a111dff1e72a
Daniel Silva (AMcom) <CECRED\t0031672>
2018-01-26 17:11:23 +0000

    Ajuste script, valor default de 1 para 0
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C16558

--- bacas/2018_01/04_Script_add_CRAPEPR.IDQUAPRC.sql
+++ bacas/2018_01/04_Script_add_CRAPEPR.IDQUAPRC.sql
@@ -1,5 +1,5 @@
 alter table CECRED.CRAPEPR
-add idquaprc number(5) default 1
+add idquaprc number(5) default 0
 /
 comment on column CECRED.CRAPEPR.idquaprc
  is 'Identificacao da qualificacao da operacao - Controle (proposta).'
