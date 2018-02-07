143a85e33f8e3b16c268e759a69d60148fd71a00
Guilherme Boettcher (AMCOM) <CECRED\t0031670>
2018-02-07 16:56:35 +0000

    Script para retirar CRPS641 da cadeia
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C16896

---
+++ scripts/Sprint1/11_Retirar_CRPS641_Cadeia.sql
@@ -0,0 +1,9 @@
+--Script para ser aplicado na cadeia:
+-- retirando uma rotina de calculo do risco de grupo da diária -- Permance na mensal
+-- 
+UPDATE crapprg
+SET crapprg.nrsolici = 999
+   ,crapprg.nrordprg = (SELECT MAX(g.nrordprg) ordem
+                        FROM crapprg g
+                        WHERE g.nrsolici = 999) + 1
+WHERE cdprogra = 'CRPS641';
