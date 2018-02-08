3685ef39685c3823e43b3a3d448347d940ec168b
Kelvin Souza Ott <kelvin.ott@ailos.coop.br>
2018-02-08 16:16:31 +0000

    Ajuste para caracteres especiais não gerarem problemas. (SD 845660 - Kelvin).
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/2018_02;C16938

--- ayllosdev/telas/ctasal/ctasal.js
+++ ayllosdev/telas/ctasal/ctasal.js
@@ -17,6 +17,8 @@
 				  
 				  07/08/2017 - Ajuste realizado para gerar numero de conta automaticamente na
 							   inclusao, conforme solicitado no chamado 689996. (Kelvin)
+							   
+				  08/02/2018 - Ajuste para caracteres especiais não gerarem problemas. (SD 845660 - Kelvin).			   
   
  */
 
@@ -450,7 +452,7 @@ function manterRotina( operacao ) {
 				cddopcao	: cddopcao,
 				cdagenca	: cdagenca,
 				cdempres    : cdempres,
-				nmfuncio    : nmfuncio,
+				nmfuncio    : removeCaracteresInvalidos(nmfuncio),
 				cdagetrf    : cdagetrf,
 				cdbantrf    : cdbantrf,
 				nrdigtrf    : nrdigtrf,
