c7d2d26892fcc6515081676b47351a7d66a4a8bb
Douglas Mario da Silva Ribeiro (GFT) <CECRED\t0031726>
2018-02-14 17:33:07 +0000

    Changes modal
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/403 – Desconto de Título/DEV;C17027

--- ayllosdev/telas/atenda/emprestimos/tabela_tipo_emprestimo.php
+++ ayllosdev/telas/atenda/emprestimos/tabela_tipo_emprestimo.php
@@ -26,7 +26,7 @@
 						<table width="100%" border="0" cellspacing="0" cellpadding="0">
 							<tr>
 								<td width="11"><img src="<?php echo $UrlImagens; ?>background/tit_tela_esquerda.gif" width="11" height="21"></td>
-								<td class="txtBrancoBold ponteiroDrag" background="<?php echo $UrlImagens; ?>background/tit_tela_fundo.gif">Empr&eacute;stimos e Finanaciamentos</td>
+								<td class="txtBrancoBold ponteiroDrag" background="<?php echo $UrlImagens; ?>background/tit_tela_fundo.gif">Empr&eacute;stimos e Financiamentos</td>
 								<td width="12" id="tdTitTela" background="<?php echo $UrlImagens; ?>background/tit_tela_fundo.gif"><a href="#" onClick="fechaRotina($('#divUsoGenerico'),divRotina);"><img src="<?php echo $UrlImagens; ?>geral/excluir.jpg" width="12" height="12" border="0"></a></td>
 								<td width="8"><img src="<?php echo $UrlImagens; ?>background/tit_tela_direita.gif" width="8" height="21"></td>
 							</tr>
@@ -42,8 +42,8 @@
 									<div id="divConteudoOpcao" style="height: 80px;">
 									
 									<div id="divBotoes" style="height:80px;">
-										<a href="#" class="botao gft" id="btEmprestimosFinanciamentos" onClick="fechaRotina($('#divUsoGenerico'),divRotina);controlaOperacao('I');" style="margin: 20px">Empr&eacute;stimos e Financiamentos</a>
-										<a href="#" class="botao gft" id="btEmprestimosConsignado" onClick="alert('Funcionalidade ainda em desenvolvimento!');" style="margin: 20px">Empr&eacute;stimo Consignado</a>
+										<a href="#" class="botao gft" id="btEmprestimosFinanciamentos" onClick="fechaRotina($('#divUsoGenerico'),divRotina);controlaOperacao('I');" style="margin: 20px 5px">Empr&eacute;stimos e Financiamentos</a>
+										<a href="#" class="botao gft" id="btEmprestimosConsignado" onClick="alert('Funcionalidade ainda em desenvolvimento!');" style="margin: 20px 7px">Empr&eacute;stimo Consignado</a>
 									</div>
 								</td>
 							</tr>
