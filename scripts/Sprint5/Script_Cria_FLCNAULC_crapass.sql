3ea9f86c1b1c8be4cb624f0562390aea37908044
Guilherme Boettcher (AMCOM) <CECRED\t0031670>
2018-03-20 13:09:32 +0000

    Faltou COMMMIT / Ajuste Acentuação
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C18295

--- scripts/Sprint5/Script_Cria_FLCNAULC_crapass.sql
+++ scripts/Sprint5/Script_Cria_FLCNAULC_crapass.sql
@@ -1,6 +1,6 @@
 alter table CECRED.crapass
 add column FLCNAULC number(1) not null default 1;
  
-COMMENT ON COLUMN CECRED.CRAPASS.FLCNAULC IS 'Cancelamento Automático do Limite de Crédito (0-nao/1-sim)';
+COMMENT ON COLUMN CECRED.CRAPASS.FLCNAULC IS 'Cancelamento Automatico do Limite de Credito (0-Nao/1-Sim)';
  
  
