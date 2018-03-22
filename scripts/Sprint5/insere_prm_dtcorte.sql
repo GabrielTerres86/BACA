ac7a3706d87ee884e29fe254db2961a50320c82b
Daniel Silva (AMcom) <CECRED\t0031672>
2018-03-22 12:12:51 +0000

    Sprint 5 - PRM data
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C18352

---
+++ scripts/Sprint5/insere_prm_dtcorte.sql
@@ -0,0 +1,7 @@
+INSERT INTO crapprm
+     VALUES('CRED'
+           ,0
+           ,'DT_CORTE_REGCRE'
+           ,'Data de corte do projeto Regulatorios de credito'
+           ,'01/02/2018'
+           ,NULL);
