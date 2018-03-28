90bacc14a2ebaf2c6f3108f23496b90f831b91a7
Andre Luis de Avila Dourado (GFT) <CECRED\t0031807>
2018-03-28 22:10:03 +0000

    403-s004-h0304: Criação Front-end Propostas.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/403 – Desconto de Título/TEST;C18556

---
+++ ayllosdev/telas/atenda/descontos/titulos/titulos_limite_formulario_propostas.php
@@ -0,0 +1,353 @@
+<? 
+/*!
+ * FONTE        : titulos_limite_formulario.php
+ * CRIAÇÃO      : David
+ * DATA CRIAÇÃO : Junho/2007
+ * OBJETIVO     : Carregar formulário de dados para gerenciar limite
+ * --------------
+ * ALTERAÇÕES   : 10/10/2016
+ * --------------
+ * 001: [04/05/2011] Rodolpho Telmo  (DB1) : Adaptação no formulário de avalista genérico
+ * 002: [11/07/2011] Gabriel Capoia  (DB1) : Alterado para layout padrão
+ * 003: [03/02/2012] Tiago        (CECRED) : Retirado a mascara do campo Observações
+ * 004: [20/04/2012] Guilherme    (CECRED) : Digitalizacao.
+ * 005: [24/04/2012] Tiago        (CECRED) : Esconder o botao de visualizar imagem dependendo
+ *				                             do campo flgdigit e tornar link de visualizacao
+ *							                 da imagem dinamico.
+ * 006: [08/05/2012] Tiago        (CECRED) : Inserido codigo da cooperativa no link dinamico.
+ * 007: [09/07/2012] Lucas        (CECRED) : Listagem de Linhas de Desconto de Título disponíveis.
+ * 008: [06/11/2012] Adriano	  (CECRED) : Ajustes referente ao projeto GE.
+ * 009: [02/01/2015] Fabrício     (CECRED) : Ajuste format numero contrato/bordero para consultar imagem 
+                                             do contrato; adequacao ao format pre-definido para nao ocorrer 
+					                         divergencia ao pesquisar no SmartShare.
+                                             (Chamado 181988) - (Fabricio)
+ * 010: [27/06/2016] Jaison/James (CECRED) : Inicializacao da aux_inconfi6.
+ * 010: [10/10/2016] Lucas Ranghetti (CECRED): Remover verificacao de digitalizaco para o botao de consultar imagem(#510032)
+ * 011: [26/06/2017] Jonata (RKAM): Ajuste para rotina ser chamada através da tela ATENDA > Produtos (P364).
+ * 012: [28/03/2018] Andre Avila (GFT):  Alteração da opção retorno dos botões.
+ */
+
+
+?>
+<form action="" name="frmDadosLimiteDscTit" id="frmDadosLimiteDscTit" onSubmit="return false;">
+
+	<div id="divDscTit_Limite">
+	
+		<fieldset>
+		
+			<legend>Dados do Limite</legend>
+			
+			<label for="nrctrlim"><? echo utf8ToHtml('Contrato:') ?></label>
+			<input type="text" name="nrctrlim" id="nrctrlim" value="0" class="campo" disabled>
+			<br />
+			
+			<label></label>
+			<br />
+			
+			<label for="vllimite"><? echo utf8ToHtml('Valor do Limite:') ?></label>
+			<input type="text" name="vllimite" id="vllimite" value="0,00" class="campo">
+			
+			<label for="qtdiavig"><? echo utf8ToHtml('Vigência:') ?></label>
+			<input type="text" name="qtdiavig" id="qtdiavig" value="0" class="campoTelaSemBorda" disabled>
+			<br />
+			
+			<label for="cddlinha"><? echo utf8ToHtml('Linha de descontoss:') ?></label>
+			<input type="text" name="cddlinha" id="cddlinha" value="0" class="campo">
+			<a><img src="<? echo $UrlImagens; ?>geral/ico_lupa.gif"></a>
+			<input type="text" name="cddlinh2" id="cddlinh2" value="" class="campoTelaSemBorda" disabled>
+			<br />
+			
+			<label for="txjurmor"><? echo utf8ToHtml('Juros mora:') ?></label>
+			<input type="text" name="txjurmor" id="txjurmor" value="%" class="campoTelaSemBorda" disabled>
+			<br />
+			
+			<label for="txdmulta"><? echo utf8ToHtml('taxa de multa:') ?></label>
+			<input type="text" name="txdmulta" id="txdmulta" value="0,000000%" class="campoTelaSemBorda" disabled>
+			<br />
+			
+			<label for="dsramati"><? echo utf8ToHtml('Ramo de Atividade:') ?></label>
+			<input type="text" name="dsramati" id="dsramati" value="" class="campo">
+			<br />
+			
+			<label for="vlmedtit"><? echo utf8ToHtml('Valor médio dos títulos:') ?></label>
+			<input type="text" name="vlmedtit" id="vlmedtit" value="0,00" class="campo">
+			<br />
+			
+			<label for="vlfatura"><? echo utf8ToHtml('Faturamento mensal:') ?></label>
+			<input type="text" name="vlfatura" id="vlfatura" value="0,00" class="campo">
+			
+		</fieldset>
+		
+	</div>
+	
+	<div id="divDscTit_Renda">
+	
+		<fieldset>
+		
+			<legend>Rendas</legend>
+								
+				<label for="lbrendas"><? echo utf8ToHtml('Rendas:') ?></label>
+				<label for="vlsalari"><? echo utf8ToHtml('Salário:') ?></label>
+				<input type="text" name="vlsalari" id="vlsalari" value="0,00" class="campo">
+				
+				<label for="vlsalcon"><? echo utf8ToHtml('Cônjuge:') ?></label>
+				<input type="text" name="vlsalcon" id="vlsalcon" value="0,00" class="campo">
+				<br />
+				
+				<label for="vloutras"><? echo utf8ToHtml('Outras:') ?></label>
+				<input type="text" name="vloutras" id="vloutras" value="0,00" class="campo">
+				<br />
+				<label class="rotulo"></label>
+				<br />
+				
+				<label for="dsdbens1"><? echo utf8ToHtml('Bens:') ?></label>
+				<input type="text" name="dsdbens1" id="dsdbens1" value="" class="campo">
+				<br />
+				
+				<label for="dsdbens2"><? echo utf8ToHtml('') ?></label>
+				<input type="text" name="dsdbens2" id="dsdbens2" value="" class="campo">
+								
+		</fieldset>
+		
+	</div>
+	
+	<div id="divDscTit_Observacao">
+	
+		<fieldset>
+		
+			<legend><? echo utf8ToHtml('Observações:') ?></legend>
+			<textarea name="dsobserv" id="dsobserv" style="width: 450px;" rows="5" maxlength="953"></textarea>
+						
+		</fieldset>
+				
+	</div>
+	
+	<div id="divDscTit_Avalistas" class="condensado" >
+	
+		<? 	// ALTERAÇÃO 001: Substituido formulário antigo pelo include				
+			include('../../../../includes/avalistas/form_avalista.php'); 
+		?>
+		
+	</div>
+		
+</form>
+
+<div id="divBotoesLimite">
+		
+	<input type="image" id="btnVoltarLimite" name="btnVoltarLimite" src="<? echo $UrlImagens; ?>botoes/voltar.gif" />
+	
+	
+	<? if ($cddopcao == "C") { ?> 
+				<a href="http://<?php 
+
+				echo $GEDServidor;?>/smartshare/clientes/viewerexterno.aspx?tpdoc=<?php 
+				echo $dados[29]->cdata; ?>&conta=<?php 
+				echo formataContaDVsimples($nrdconta); ?>&contrato=<?php 
+				echo formataNumericos('z.zzz.zz9',$nrctrlim,'.'); ?>&cooperativa=<?php 
+				echo $glbvars["cdcooper"]; ?>" target="_blank"><img src="<? 
+				echo $UrlImagens; ?>botoes/consultar_imagem.gif" /></a>
+	<?	} ?>
+	   
+	<input type="image" id="btnContinuarLimite" name="btnContinuarLimite" src="<? echo $UrlImagens; ?>botoes/continuar.gif" />		
+	
+</div>
+
+<div id="divBotoesRenda">
+		
+	<input type="image" id="btnVoltarRendas" name="btnVoltarRendas" src="<? echo $UrlImagens; ?>botoes/voltar.gif" />
+	<input type="image" id="btnContinuarRendas" name="btnContinuarRendas" src="<? echo $UrlImagens; ?>botoes/continuar.gif" />
+	
+</div>
+
+<div id="divBotoesObs" >
+		
+	<input type="image" id="btnVoltarObservacao" name="btnVoltarObservacao" src="<? echo $UrlImagens; ?>botoes/voltar.gif" />
+	<input type="image" id="btnContinuarObservacao" name="btnContinuarObservacao" src="<? echo $UrlImagens; ?>botoes/continuar.gif" />
+	
+</div>
+
+<div id="divBotoesAval">						
+		
+	<input type="image" id="btnVoltarAvalistas" name="btnVoltarAvalistas" src="<? echo $UrlImagens; ?>botoes/voltar.gif" />
+	<? if ($cddopcao <> "C") { ?>
+		   <input type="image" id="btnCancelarLimite" name="btnCancelarLimite" src="<? echo $UrlImagens; ?>botoes/cancelar.gif" />
+	<? } ?>
+	<input type="image" id="btnConcluirLimite" name="btnConcluirLimite" src="<? echo $UrlImagens; ?>botoes/concluir.gif" />
+			
+</div>	
+
+<? 
+	if ($cddopcao <> "I") {
+		// Alimentar dados do rating - Variáveis utilizadas na include rating_busca_dados.php	
+		$nrgarope = $dados[21]->cdata;
+		$nrinfcad = $dados[22]->cdata;
+		$nrliquid = $dados[23]->cdata;
+		$nrpatlvr = $dados[24]->cdata;
+		$vltotsfn = $dados[25]->cdata;
+		$nrperger = $dados[26]->cdata;
+		$perfatcl = $dados[27]->cdata;
+	}
+	
+	// Variável que indica se é uma operação para cadastrar nova proposta - Utiliza na include rating_busca_dados.php
+	$cdOperacao = $cddopcao;
+	
+	include("../../../../includes/rating/rating_busca_dados.php"); 
+?>
+
+<script type="text/javascript">
+
+	formataLayout('frmDadosLimiteDscTit');
+
+	operacao = '<? echo $cddopcao; ?>';
+	
+	dscShowHideDiv("divOpcoesDaOpcao3;divDscTit_Limite;divBotoesLimite","divBotoesRenda;divBotoesObs;divBotoesAval;divOpcoesDaOpcao2;divDscTit_Renda;divDscTit_Observacao;divDscTit_Avalistas;divDscTit_Confirma");
+
+	$("#divDscTit_Confirma").css("display","<? if ($cddopcao == "I") { echo "block"; } else { echo "none"; } ?>");
+		
+	// Muda o título da tela
+	$("#tdTitRotina").html("DESCONTO DE T&Iacute;TULOS - LIMITE - <? if ($cddopcao == "A") { echo "ALTERAR"; } elseif ($cddopcao == "C") { echo "CONSULTAR"; } else { echo "INCLUIR"; } ?>");
+
+	<? 
+	// Alimentar campos do formulário para gerenciar limite
+	if ($cddopcao == "I") { 
+		echo '$("#qtdiavig","#frmDadosLimiteDscTit").val("'.formataNumericos('zzz.zz9',$dados[6]->cdata,'.').' dias");';
+		echo '$("#txdmulta","#frmDadosLimiteDscTit").val("'.number_format(str_replace(",",".",$dados[12]->cdata),6,",",".").'%");';
+	} else { 
+		echo '$("#nrctrlim","#frmDadosLimiteDscTit").val("'.formataNumericos('z.zzz.zz9',$dados[14]->cdata,'.').'");';
+		echo '$("#vllimite","#frmDadosLimiteDscTit").val("'.number_format(str_replace(",",".",$dados[15]->cdata),2,",",".").'");';
+		echo '$("#qtdiavig","#frmDadosLimiteDscTit").val("'.formataNumericos('zzz.zz9',$dados[16]->cdata,'.').' dias");';
+		echo '$("#cddlinha","#frmDadosLimiteDscTit").val("'.formataNumericos('zz9',$dados[17]->cdata,'.').'");';
+		echo '$("#cddlinh2","#frmDadosLimiteDscTit").val("'.$dados[2]->cdata.'");';
+		echo '$("#txdmulta","#frmDadosLimiteDscTit").val("'.number_format(str_replace(",",".",$dados[0]->cdata),6,",",".").'%");';
+		echo '$("#dsramati","#frmDadosLimiteDscTit").val("'.$dados[4]->cdata.'");';
+		echo '$("#vlmedtit","#frmDadosLimiteDscTit").val("'.number_format(str_replace(",",".",$dados[5]->cdata),2,",",".").'");';
+		echo '$("#vlfatura","#frmDadosLimiteDscTit").val("'.number_format(str_replace(",",".",$dados[6]->cdata),2,",",".").'");';
+		echo '$("#vlsalari","#frmDadosLimiteDscTit").val("'.number_format(str_replace(",",".",$dados[8]->cdata),2,",",".").'");';
+		echo '$("#vlsalcon","#frmDadosLimiteDscTit").val("'.number_format(str_replace(",",".",$dados[9]->cdata),2,",",".").'");';
+		echo '$("#vloutras","#frmDadosLimiteDscTit").val("'.number_format(str_replace(",",".",$dados[7]->cdata),2,",",".").'");';
+		echo '$("#dsdbens1","#frmDadosLimiteDscTit").val("'.trim($dados[10]->cdata).'");';
+		echo '$("#dsdbens2","#frmDadosLimiteDscTit").val("'.trim($dados[11]->cdata).'");';	
+		echo '$("#dsobserv","#frmDadosLimiteDscTit").val("'.str_replace(chr(13),"\\n",str_replace(chr(10),"\\r",$dados[12]->cdata)).'");';
+		echo '$("#antnrctr","#frmDadosLimiteDscTit").val("'.formataNumericos('zzz.zz9',$dados[14]->cdata,'.').'");';
+
+	}
+	?>
+	
+	// Na consulta não permitir manipulação dos campos
+	if (operacao == 'C') {
+	
+		$("input[type='text']",'#'+nomeForm).prop("disabled",true).attr("class","campoTelaSemBorda");
+		$("textarea",'#'+nomeForm).attr("readonly",true);
+		$("select",'#'+nomeForm).prop("disabled",true);
+	
+	// Para demais operações, configurar máscaras para campos do formulário
+	} else {
+	
+		if (operacao == 'A' || operacao == 'I' ) {
+			$("#nrctrlim",'#'+nomeForm).prop("disabled",true).attr("class","campoTelaSemBorda");
+		} 
+	
+		// Dados do Limite	
+		$("#nrctrlim",'#'+nomeForm).setMask("INTEGER","z.zzz.zz9","");
+		$("#vllimite",'#'+nomeForm).setMask("DECIMAL","zzz.zzz.zz9,99","");
+		$("#cddlinha",'#'+nomeForm).setMask("INTEGER","zz9","");
+		$("#vlmedtit",'#'+nomeForm).setMask("DECIMAL","z.zzz.zz9,99","");
+		$("#vlfatura",'#'+nomeForm).setMask("DECIMAL","zzz.zzz.zz9,99","");
+		$("#dsramati",'#'+nomeForm).setMask("STRING","40",charPermitido(),"");
+
+		// Rendas
+		$("#vloutras",'#'+nomeForm).setMask("DECIMAL","zzz.zzz.zz9,99","");
+		$("#vlsalari",'#'+nomeForm).setMask("DECIMAL","zzz.zzz.zz9,99","");
+		$("#vlsalcon",'#'+nomeForm).setMask("DECIMAL","zzz.zzz.zz9,99","");
+		$("#dsdbens1",'#'+nomeForm).setMask("STRING","60",charPermitido(),"");
+		$("#dsdbens2",'#'+nomeForm).setMask("STRING","60",charPermitido(),"");
+
+	}
+
+	// Atribui eventos para botões do formulário
+	$('#btnVoltarLimite','#divBotoesLimite').unbind('click').bind('click',function() {
+		
+		if(executandoProdutos){
+			
+			encerraRotina(true);
+			
+		}else{
+			
+		voltaDiv(3,2,4,'DESCONTO DE T&Iacute;TULOS - LIMITE');
+			
+		}
+		return false;
+	});
+	
+	$('#btnContinuarLimite','#divBotoesLimite').unbind('click').bind('click',function() {
+		if (operacao == 'C') {
+			dscShowHideDiv('divDscTit_Renda;divBotoesRenda','divDscTit_Limite;divBotoesLimite');
+		} else {
+			aux_inconfir = 1; 
+			aux_inconfi2 = 11; 
+			aux_inconfi3 = ""; 
+			aux_inconfi4 = 71; 
+			aux_inconfi5 = 30;
+			aux_inconfi6 = 51;
+			validaLimiteDscTit(operacao,1,11,30);
+		}
+		return false;
+	});
+	
+	$('#btnVoltarRendas','#divBotoesRenda').unbind('click').bind('click',function() {
+		dscShowHideDiv('divDscTit_Limite;divBotoesLimite','divDscTit_Renda;divBotoesRenda');
+		return false;
+	});
+	
+	$('#btnContinuarRendas','#divBotoesRenda').unbind('click').bind('click',function() {
+		if (operacao == 'A') {
+			$('#divBotoesRenda').css('display','none');
+			informarRating('divDscTit_Renda',"dscShowHideDiv('divDscTit_Observacao;divBotoesObs','divDadosRating;divBotoesRenda')","dscShowHideDiv('divDscTit_Renda;divBotoesRenda','divDadosRating');","carregaLimitesTitulosPropostas()");
+		} else if (operacao == 'C') {
+			$('#divBotoesRenda').css('display','none');
+			informarRating('divDscTit_Renda',"dscShowHideDiv('divDscTit_Observacao;divBotoesObs','divDadosRating;divBotoesRenda')","dscShowHideDiv('divDscTit_Renda;divBotoesRenda','divDadosRating');","carregaLimitesTitulos()");
+		} else {
+			$('#divBotoesRenda').css('display','none');
+			informarRating('divDscTit_Renda',"dscShowHideDiv('divDscTit_Observacao;divBotoesObs','divDadosRating;divBotoesRenda')","dscShowHideDiv('divDscTit_Renda;divBotoesRenda','divDadosRating');","mostraImprimirLimite()");
+		}
+		return false;
+	});
+	
+	$('#btnVoltarObservacao','#divBotoesObs').unbind('click').bind('click',function() {
+		dscShowHideDiv('divDadosRating','divDscTit_Observacao;divBotoesObs');
+		return false;
+	});
+	
+	$('#btnContinuarObservacao','#divBotoesObs').unbind('click').bind('click',function() {
+		dscShowHideDiv('divDscTit_Avalistas;divBotoesAval','divDscTit_Observacao;divBotoesObs');
+		return false;
+	});
+	
+	$('#btnVoltarAvalistas','#divBotoesAval').unbind('click').bind('click',function() {
+		dscShowHideDiv('divDscTit_Observacao;divBotoesObs','divDscTit_Avalistas;divBotoesAval');
+		return false;
+	});
+	
+	if (operacao != 'C') {
+		$('#btnCancelarLimite','#divBotoesAval').unbind('click').bind('click',function() {
+			voltaDiv(3,2,4,'DESCONTO DE T&Iacute;TULOS - LIMITE');
+			return false;
+		});
+	}
+	
+	$('#btnConcluirLimite','#divBotoesAval').unbind('click').bind('click',function() {
+		if (operacao == 'C') {
+			voltaDiv(3,2,4,'DESCONTO DE T&Iacute;TULOS - LIMITE');
+		} else {
+			buscaGrupoEconomico();
+		}
+		return false;
+	});
+	
+	$('#nrctrlim','#frmDadosLimiteDscTit').focus();
+				
+	layoutPadrao();
+	hideMsgAguardo();
+	bloqueiaFundo(divRotina);	
+	
+</script>
---
+++ ayllosdev/telas/atenda/descontos/titulos/titulos_propostas.php
@@ -0,0 +1,276 @@
+<?php 
+
+	/************************************************************************
+	 Fonte: titulos_limite.php                                        
+	 Autor: Guilherme                                                 
+	 Data : Novembro/2008                Última Alteração: 26/06/2017
+	                                                                  
+	 Objetivo  : Mostrar opcao Limites de descontos da rotina         
+	             Descontos da tela ATENDA                 		   	  
+	                                                                  	 
+	 Alterações: 09/06/2010 - Mostrar descrição da situação (David).
+
+				 25/06/2010 - Mostar campo de envio a sede (Gabriel).
+				 
+				 12/07/2011 - Alterado para layout padrão (Gabriel Capoia - DB1)
+				 
+				 18/11/2011 - Ajustes para nao mostrar botao quando nao tiver permissao (Jorge)
+				 
+				 21/05/2015 - Alterado para apresentar mensagem ao realizar inclusao
+ 							  de proposta de novo limite de desconto de titulo para
+ 							  menores nao emancipados (Reinert).
+
+				 17/12/2015 - Edição de número do contrato de limite (Lunelli - SD 360072 [M175])
+
+				 26/06/2017 - Ajuste para rotina ser chamada através da tela ATENDA > Produtos (Jonata - RKAM / P364).
+
+				 28/08/2018 - Adaptado arquivo para Porpostas. Andre Avila GFT.
+
+	************************************************************************/
+	
+	session_start();
+
+	// Includes para controle da session, variáveis globais de controle, e biblioteca de funções	
+	require_once("../../../../includes/config.php");
+	require_once("../../../../includes/funcoes.php");
+	require_once("../../../../includes/controla_secao.php");
+
+	// Verifica se tela foi chamada pelo método POST
+	isPostMethod();	
+		
+	// Classe para leitura do xml de retorno
+	require_once("../../../../class/xmlfile.php");
+	
+	setVarSession("nmrotina","DSC TITS - LIMITE");
+
+	// Carrega permissões do operador
+	include("../../../../includes/carrega_permissoes.php");	
+	
+	setVarSession("opcoesTela",$opcoesTela);
+	
+	// Verifica se o número da conta foi informado
+	if (!isset($_POST["nrdconta"])) {
+		exibeErro("Par&acirc;metros incorretos.");
+	}	
+
+	$nrdconta = $_POST["nrdconta"];
+
+	// Verifica se o número da conta é um inteiro válido
+	if (!validaInteiro($nrdconta)) {
+		exibeErro("Conta/dv inv&aacute;lida.");
+	}
+	
+///////////////////// Monta o xml de requisição /////////////////////
+
+	$xmlGetLimites  = "";
+	$xmlGetLimites .= "<Root>";
+	$xmlGetLimites .= "	<Dados>";
+	$xmlGetLimites .= "		<nrdconta>".$nrdconta."</nrdconta>";
+	$xmlGetLimites .= "	</Dados>";
+	$xmlGetLimites .= "</Root>";
+		
+$procedure_acao = 'OBTEM_DADOS_PROPOSTA';
+$pakage = 'TELA_ATENDA_DESCTO';
+$glbvars['rotinasTela'][8] = 'PROPOSTAS';
+
+
+$xmlResult = mensageria($xmlGetLimites, $pakage, $procedure_acao,  $glbvars["cdcooper"], $glbvars["cdagenci"], $glbvars["nrdcaixa"], $glbvars["idorigem"], $glbvars["cdoperad"], "</Root>");
+
+$xmlObjLimites = getObjectXML($xmlResult);
+
+
+	// Se ocorrer um erro, mostra crítica
+	if (strtoupper($xmlObjLimites->roottag->tags[0]->name) == "ERRO") {
+
+		exibeErro($xmlObjLimites->roottag->tags[0]->tags[0]->tags[4]->cdata);
+	} 
+	
+	$limites   = $xmlObjLimites->roottag->tags[0]->tags;
+	$qtLimites = count($limites);
+
+
+	
+	// Fun&ccedil;&atilde;o para exibir erros na tela atrav&eacute;s de javascript
+	function exibeErro($msgErro) { 
+		echo '<script type="text/javascript">';
+		echo 'hideMsgAguardo();';
+		echo 'showError("error","'.$msgErro.'","Alerta - Ayllos","blockBackground(parseInt($(\'#divRotina\').css(\'z-index\')))");';
+		echo '</script>';
+		exit();
+	}
+
+	$insitlim = $limites[$i]->tags[7]->cdata;
+	$dssitest = $limites[$i]->tags[8]->cdata;
+	$insitapr = $limites[$i]->tags[9]->cdata;
+	
+?>
+
+<?  for ($i = 0; $i < $qtLimites; $i++) {
+
+?>
+	<input type="hidden" id="vlLimite" name="vlLimite" value="<? echo $limites[$i]->tags[2]->cdata; ?>" />
+	<input type="hidden" id="insitlim" name="insitlim" value="<? echo $limites[$i]->tags[7]->cdata; ?>" />
+	<input type="hidden" id="dssitest" name="dssitest" value="<? echo $limites[$i]->tags[8]->cdata; ?>" />
+	<input type="hidden" id="insitapr" name="insitapr" value="<? echo $limites[$i]->tags[9]->cdata; ?>" />
+
+<?
+}
+?>
+
+
+<div id="divPropostas">
+	<div class="divRegistros">
+		<table>
+			<thead>
+				<tr>
+					<th>Data Proposta</th>
+					<!--<th>Ini.Vig&ecirc;n.</th>-->
+					<th>Contrato</th>
+					<th>Valor Limite</th>
+					<th>Dias de Vig&acirc;ncia</th>
+					<th>Linha de Desconto</th>
+					<th >Situa&ccedil;&atilde;o da Proposta</th>
+					<th>Situa&ccedil;&atilde;o da An&aacute;lise</th>
+					<th>Decis&atilde;o</th>
+				</tr>			
+			</thead>
+			<tbody>
+				
+				<?  for ($i = 0; $i < $qtLimites; $i++) {
+						
+
+						$mtdClick = "selecionaLimiteTitulos('".($i + 1)."', '".$qtLimites."', '".($limites[$i]->tags[1]->cdata)."', '".($limites[$i]->tags[7]->cdata)."', '".($limites[$i]->tags[8]->cdata)."', '".($limites[$i]->tags[9]->cdata)."', '".($limites[$i]->tags[2]->cdata)."');";
+									
+				?>
+					<tr id="trLimite<? echo $i + 1; ?>" onFocus="<? echo $mtdClick; ?>" onClick="<? echo $mtdClick; ?>">
+
+						<td><? echo getByTagName($limites[$i]->tags,"DTPROPOS"); ?></td>
+						<td><? echo $limites[$i]->tags[1]->cdata; ?></td>
+
+						<td><span>
+						<? $valor_retira_ponto_virgula = str_replace(",","",$limites[$i]->tags[2]->cdata);
+						$valor_retira_ponto_virgula = str_replace(".","",$valor_retira_ponto_virgula);
+						$valor_formatado = formataNumericos('zzz.zz9,99',$valor_retira_ponto_virgula,'.,'); ?>
+						 ?></span>
+						<? echo $valor_formatado ?></td>
+
+						<td><? echo $limites[$i]->tags[3]->cdata; ?></td>
+						
+						<td><? echo $limites[$i]->tags[4]->cdata; ?></td>
+						
+						<td><? echo $limites[$i]->tags[5]->cdata; ?></td>
+						
+						<td><? echo $limites[$i]->tags[6]->cdata; ?></td>
+
+						<td><? echo $limites[$i]->tags[7]->cdata; ?></td>
+					</tr>
+				<?} // Fim do for ?>			
+			</tbody>
+		</table>
+	</div>
+</div>
+
+<?php
+	$dispA = (!in_array("A",$glbvars["opcoesTela"])) ? 'display:none;' : '';
+	$dispX = (!in_array("X",$glbvars["opcoesTela"])) ? 'display:none;' : '';
+	$dispC = (!in_array("C",$glbvars["opcoesTela"])) ? 'display:none;' : '';
+	$dispE = (!in_array("E",$glbvars["opcoesTela"])) ? 'display:none;' : '';
+	$dispM = (!in_array("M",$glbvars["opcoesTela"])) ? 'display:none;' : '';
+?>
+
+<div id="divBotoesTitulosLimite" style="margin-bottom:10px;">
+	
+	
+	<input type="button" class="botao gft" value="Voltar"  onClick="voltaDiv(2,1,4,'DESCONTO DE T&Iacute;TULOS','DSC TITS');carregaTitulos();return false;" />
+	
+	
+	<input type="button" class="botao gft" value="Alterar"  <?php if ($qtLimites == 0) { echo 'style="cursor: default;'.$dispA.'" onClick="return false;"'; } else { echo 'style="'.$dispA.'" onClick="carregaDadosAlteraLimiteDscTitPropostas();return false;"'; } ?> />
+	
+
+	<input type="button" class="botao gft" value="Cancelar"  <?php if ($qtLimites == 0) { echo 'style="cursor: default;'.$dispX.'" onClick="return false;"'; } else { echo 'style="'.$dispX.'" onClick="showConfirmacao(\'Deseja cancelar a Proposta?\',\'Confirma&ccedil;&atilde;o - Ayllos\',\'cancelaLimiteDscTit()\',\'metodoBlock()\',\'sim.gif\',\'nao.gif\');return false;"'; } ?>  />
+	
+
+	<input type="button" class="botao gft" value="Consultar"  <?php if ($qtLimites == 0) { echo 'style="cursor: default;'.$dispC.'" onClick="return false;"'; } else { echo 'style="'.$dispC.'" onClick="carregaDadosConsultaLimiteDscTit();return false;"'; } ?> />
+	
+
+<!--
+	<input type="button" class="botao gft" value="Excluir"  <?php if ($qtLimites == 0) { 
+	echo 'style="cursor: default;'.$dispE.'" onClick="return false;"'; } else { echo 'style="'.$dispE.'" onClick="showConfirmacao(\'Deseja excluir o limite de desconto de t&iacute;tulos?\',\'Confirma&ccedil;&atilde;o - Ayllos\',\'excluirLimiteDscTit()\',\'metodoBlock()\',\'sim.gif\',\'nao.gif\');return false;"'; } ?> />
+-->	
+
+
+	<input type="button" class="botao gft" value="Incluir" id="btnIncluirLimite" name="btnIncluirLimite" <?php if (!in_array("I",$glbvars["opcoesTela"])) { echo 'style="cursor: default;display:none;" onClick="return false;"'; } else { echo 'onClick="carregaDadosInclusaoLimiteDscTit(1);return false;"'; } ?> />
+
+
+
+	<input type="button" class="botao gft" value="Imprimir" <?php if ($qtLimites == 0) { echo 'style="cursor: default;'.$dispM.'" onClick="return false;"'; } else { echo 'style="'.$dispM.'" onClick="mostraImprimirLimite();return false;"'; } ?> />
+	
+
+
+	<input type="button" class="botao gft" value="Analisar"  id="btnAnalisarLimite" name="btnAnalisarLimite" <?php if ($qtLimites == 0) { echo 'style="cursor: default;" onClick="return false;"'; } else { echo 'onClick="confirmaEnvioAnalise();"'; } ?>/>
+	
+
+
+	<input type="button" class="botao gft" value="Detalhes Propostas"  id="btnDetalhesProposta" name="btnDetalhesProposta" <?php if ($qtLimites == 0) { echo 'style="cursor: default;" onClick="return false;"'; } else { echo 'onClick="carregaDadosDetalhesProposta();return false;"'; } ?>/>
+	
+
+	<input type="button" class="botao gft" value="Efetivar Limite"  id="btnEfetivarLimite" name="btnEfetivarLimite" <?php if ($qtLimites == 0) { echo 'style="cursor: default;" onClick="return false;"'; } else { echo 'onClick="efetuarNovoLimite();"'; } ?>/>
+	
+<!--
+	<input type="button" class="botao gft" value="Negar"  id="btnAceitarRejeicao" name="btnAceitarRejeicao" <?php if ($qtLimites == 0) { echo 'style="cursor: default;" onClick="return false;"'; } else { echo 'onClick="aceitarRejeicao();"'; } ?>/>	
+-->
+
+</div>
+
+<script type="text/javascript">
+dscShowHideDiv("divOpcoesDaOpcao2","divOpcoesDaOpcao1;divOpcoesDaOpcao3");
+
+// Muda o título da tela
+$("#tdTitRotina").html("DESCONTO DE T&Iacute;TULOS - LIMITE");
+
+formataLayout('divPropostas');
+
+// Esconde mensagem de aguardo
+hideMsgAguardo();
+
+// Bloqueia conteúdo que está átras do div da rotina
+blockBackground(parseInt($("#divRotina").css("z-index")));
+	
+	//Se esta tela foi chamada através da rotina "Produtos" então acessa a opção conforme definido pelos responsáveis do projeto P364
+	if (executandoProdutos == true) {
+		
+		$("#btnIncluirLimite", "#divBotoesTitulosLimite").click();
+		
+	}
+
+/////////////////////////////colocar este no arquivo de tratamento de layout do módulo descontos.js/////////////////////////////////////
+
+var Inputs1 = $('input[type="button"][value=Cancelar]');
+Inputs1.css({'color':'gray'});
+Inputs1.css({'cursor':'default'});
+Inputs1.css({'pointer-events':'none'});
+
+var Inputs2 = $('input[type="button"][value=Consultar]');
+Inputs2.css({'color':'gray'});
+Inputs2.css({'cursor':'default'});
+Inputs2.css({'pointer-events':'none'});
+
+var Inputs3 = $('input[type="button"][value=Imprimir]');
+Inputs3.css({'color':'gray'});
+Inputs3.css({'cursor':'default'});
+Inputs3.css({'pointer-events':'none'});
+
+var Inputs3 = $('#btnDetalhesProposta');
+Inputs3.css({'color':'gray'});
+Inputs3.css({'cursor':'default'});
+Inputs3.css({'pointer-events':'none'});
+
+var Inputs4 = $('#btnEfetivarLimite');
+Inputs4.css({'color':'gray'});
+Inputs4.css({'cursor':'default'});
+Inputs4.css({'pointer-events':'none'});
+
+
+/////////////////////////////colocar este no arquivo de tratamento de layout do módulo descontos.js/////////////////////////////////////
+
+</script>
