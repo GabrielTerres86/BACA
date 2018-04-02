c2d812b7f11a7c406e43ac6b18e1c39d6e9e2b91
Andre Luis de Avila Dourado (GFT) <CECRED\t0031807>
2018-04-02 22:26:04 +0000

    403-s004-h0304: Formatação Front-end Tabelas.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/403 – Desconto de Título/DEV;C18669

--- ayllosdev/telas/atenda/descontos/descontos.js
+++ ayllosdev/telas/atenda/descontos/descontos.js
@@ -307,24 +307,25 @@ function formataLayout(nomeForm){
 		var ordemInicial = new Array();
 				
 		var arrayLargura = new Array();
-		arrayLargura[0] = '65px';
+		arrayLargura[0] = '60px';
 		arrayLargura[1] = '60px';
 		arrayLargura[2] = '60px';
-		arrayLargura[3] = '55px';
-		arrayLargura[4] = '100px';
-		arrayLargura[5] = '55px';
-		arrayLargura[6] = '100px';
-		arrayLargura[7] = '90px';
+		arrayLargura[3] = '60px';
+		arrayLargura[4] = '80px';
+		arrayLargura[5] = '60px';
+		arrayLargura[6] = '80px';
+		arrayLargura[7] = '120px';
+		
 				
 		var arrayAlinha = new Array();
 		arrayAlinha[0] = 'center';
-		arrayAlinha[1] = 'right';
-		arrayAlinha[2] = 'right';
-		arrayAlinha[3] = 'right';
+		arrayAlinha[1] = 'center';
+		arrayAlinha[2] = 'center';
+		arrayAlinha[3] = 'center';
 		arrayAlinha[4] = 'right';
-		arrayAlinha[5] = 'right';
+		arrayAlinha[5] = 'center';
 		arrayAlinha[6] = 'right';
-		arrayAlinha[7] = 'left';
+		arrayAlinha[7] = 'center';
 		arrayAlinha[8] = 'center';
 						
 		tabela.formataTabela( ordemInicial, arrayLargura, arrayAlinha, '' );
@@ -337,6 +338,50 @@ function formataLayout(nomeForm){
 		
 		ajustarCentralizacao();
 	
+
+	}else if ( nomeForm == 'divBorderosTitulos' ){
+	
+		$('#'+nomeForm).css('width','785px');
+	
+		var divRegistro = $('div.divRegistros','#'+nomeForm);		
+		var tabela      = $('table', divRegistro );
+						
+		divRegistro.css('height','110px');
+		
+		var ordemInicial = new Array();
+				
+		var arrayLargura = new Array();
+		arrayLargura[0] = '60px';
+		arrayLargura[1] = '60px';
+		arrayLargura[2] = '60px';
+		arrayLargura[3] = '60px';
+		arrayLargura[4] = '80px';
+		arrayLargura[5] = '120px';
+		arrayLargura[6] = '80px';
+		
+				
+		var arrayAlinha = new Array();
+		arrayAlinha[0] = 'center';
+		arrayAlinha[1] = 'center';
+		arrayAlinha[2] = 'center';
+		arrayAlinha[3] = 'center';
+		arrayAlinha[4] = 'right';
+		arrayAlinha[5] = 'center';
+		arrayAlinha[6] = 'right';
+		arrayAlinha[7] = 'center';
+						
+		tabela.formataTabela( ordemInicial, arrayLargura, arrayAlinha, '' );
+		
+		$('tbody > tr',tabela).each( function() {
+			if ( $(this).hasClass('corSelecao') ) {
+				$(this).focus();		
+			}
+		});
+		
+		ajustarCentralizacao();
+
+
+
 	}else if ( nomeForm == 'divIncluirBordero' ){
 	
 		$('#'+nomeForm).css('width','940px');
@@ -628,21 +673,19 @@ function formataLayout(nomeForm){
 		arrayLargura[1] = '60px';
 		arrayLargura[2] = '60px';
 		arrayLargura[3] = '80px';
-		arrayLargura[4] = '30px';
-		arrayLargura[5] = '25px';
+		arrayLargura[4] = '60px';
+		arrayLargura[5] = '60px';
 		arrayLargura[6] = '120px';
-		arrayLargura[7] = '120px';
 				
 		var arrayAlinha = new Array();
 		arrayAlinha[0] = 'center';
 		arrayAlinha[1] = 'center';
-		arrayAlinha[2] = 'right';
+		arrayAlinha[2] = 'center';
 		arrayAlinha[3] = 'right';
 		arrayAlinha[4] = 'center';
-		arrayAlinha[5] = 'right';
+		arrayAlinha[5] = 'center';
 		arrayAlinha[6] = 'center';
 		arrayAlinha[7] = 'center';
-		arrayAlinha[8] = 'center';
 						
 		tabela.formataTabela( ordemInicial, arrayLargura, arrayAlinha, '' );
 		
