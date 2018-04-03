ed080ef41e76f2b0965cb4592a987f2c09f1d51a
Daniel Silva (AMcom) <CECRED\t0031672>
2018-04-03 16:38:12 +0000

    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C18708

---
+++ scripts/Sprint6/insere_mtvatvprb_tbgen_motivo.sql
@@ -0,0 +1,14 @@
+﻿-- Insere Motivos de Ativo Problemático
+insert into tbcc_produto values(42, 'ATIVO PROBLEMATICO', 0, 0, 0, 0);
+--
+insert into tbgen_motivo values(57, 'Atraso > 90 dias (Reestruturação)', 42);
+insert into tbgen_motivo values(58, 'Atraso > 90 dias', 42);
+insert into tbgen_motivo values(59, 'Prejuizo', 42);
+insert into tbgen_motivo values(60, 'Socio falecido', 42);
+insert into tbgen_motivo values(61, 'Acao contra', 42);
+insert into tbgen_motivo values(62, 'Cooperado preso', 42);
+insert into tbgen_motivo values(63, 'Falencia PJ', 42);
+insert into tbgen_motivo values(64, 'Recuperacao judicial PJ', 42);
+insert into tbgen_motivo values(65, 'Outros', 42);
+commit;
+/
