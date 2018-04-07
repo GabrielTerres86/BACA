db0268a7bb27db8fa102268fe302b4aead9659d0
Renato Darosci (Supero) <supero-renato.darosci@ailos.coop.br>
2018-04-07 18:37:01 +0000

    P404 - Ajustar regra para bloqueio de campo
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/PRINCIPAL;C18951

--- ayllosdev/telas/garopc/garopc.js
+++ ayllosdev/telas/garopc/garopc.js
@@ -77,8 +77,10 @@ if (cGar_tipaber.val() != 'C') {
     $('#btLupaAssociado', '#frmGAROPC').attr('onClick','pesquisaAssociadoGAROPC(); return false;');
 
     // Se estiver parametrizado para alterar ou anteriormente etava marcado como SIM 
-    if (gar_inresper == 1 || gar_inresaut == 1) {
+    if (gar_inresaut == 1) {
+        if (gar_inresper == 1) {
         cGar_pro_raut.habilitaCampo();
+        }
     }
 
     // Somente permite caso foi informado conta de terceiro
