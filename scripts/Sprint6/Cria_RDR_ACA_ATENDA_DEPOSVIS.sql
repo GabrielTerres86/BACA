54eaf3e2baffb877a08c37e750650a856cd5a467
Reginaldo Rubens da Silva (AMCOM) <CECRED\t0031667>
2018-04-09 18:15:30 +0000

    // Criação RDR e ACA do novo botão de saldos devedores //
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C19008

---
+++ scripts/Sprint6/Cria_RDR_ACA_ATENDA_DEPOSVIS.sql
@@ -0,0 +1,99 @@
+declare 
+
+  CURSOR cr_craprdr(pr_nmprogra IN craprdr.nmprogra%TYPE) IS
+  SELECT * FROM craprdr
+   WHERE craprdr.nmprogra = pr_nmprogra;
+  rw_craprdr cr_craprdr%ROWTYPE;
+
+  CURSOR cr_crapaca(pr_nmdeacao IN crapaca.nmdeacao%TYPE
+                   ,pr_nmpackag IN crapaca.nmpackag%TYPE
+                   ,pr_nmproced IN crapaca.nmproced%TYPE
+                   ,pr_nrseqrdr IN crapaca.nrseqrdr%TYPE) IS
+  SELECT * FROM crapaca
+   WHERE UPPER(crapaca.nmdeacao) = UPPER(pr_nmdeacao)
+     AND UPPER(crapaca.nmpackag) = UPPER(pr_nmpackag)
+     AND UPPER(crapaca.nmproced) = UPPER(pr_nmproced)
+     AND crapaca.nrseqrdr = pr_nrseqrdr;
+  rw_crapaca cr_crapaca%ROWTYPE;
+  
+  vr_exec_erro EXCEPTION;
+  vr_dscritic  VARCHAR2(1000);  
+
+begin
+  
+  dbms_output.put_line('Inicio do programa');
+  
+  -- Mensageria
+  OPEN cr_craprdr(pr_nmprogra => 'TELA_ATENDA_DEPOSVIS');
+  FETCH cr_craprdr INTO rw_craprdr;
+    
+  -- Verifica se existe a tela do ayllos web
+  IF cr_craprdr%NOTFOUND THEN
+   
+    -- Se não encontrar
+    CLOSE cr_craprdr;
+
+    -- Insere ação da tela do aylloas web
+    INSERT INTO craprdr(nmprogra
+                       ,dtsolici) 
+                 values('TELA_ATENDA_DEPOSVIS'
+                       ,SYSDATE);
+    -- Posiciona no registro criado
+    OPEN cr_craprdr(pr_nmprogra => 'TELA_ATENDA_DEPOSVIS');
+    FETCH cr_craprdr INTO rw_craprdr;
+
+    dbms_output.put_line('Insere CRAPRDR -> TELA_ATENDA_DEPOSVIS: ' || rw_craprdr.nrseqrdr);
+
+  END IF;  
+  
+  -- Fecha o cursor
+  CLOSE cr_craprdr;
+
+  -- INICIO MENSAGERIA   
+  
+  -- TELA_ATENDA_OCORRENCIAS - ACAO BUSCAR CONTRATOS LC100
+  OPEN cr_crapaca(pr_nmdeacao => 'BUSCA_SALDOS_DEVEDORES'
+                 ,pr_nmpackag => 'TELA_ATENDA_DEPOSVIS'
+                 ,pr_nmproced => 'pc_busca_saldos_devedores'
+                 ,pr_nrseqrdr => rw_craprdr.nrseqrdr);
+                   
+  FETCH cr_crapaca INTO rw_crapaca;
+    
+  -- Verifica se existe a ação tela do ayllos web
+  IF cr_crapaca%NOTFOUND THEN
+    
+    -- Insere ação da tela do ayllos web
+    INSERT INTO crapaca(nmdeacao, 
+                        nmpackag, 
+                        nmproced, 
+                        lstparam, 
+                        nrseqrdr) 
+                 VALUES('BUSCA_SALDOS_DEVEDORES',
+                        'TELA_ATENDA_DEPOSVIS',
+                        'pc_busca_saldos_devedores',
+                        'pr_nrdconta',
+                        rw_craprdr.nrseqrdr);
+                             
+    dbms_output.put_line('Insere CRAPACA -> BUSCA_SALDOS_DEVEDORES -> TELA_ATENDA_DEPOSVIS.pc_busca_saldos_devedores');
+    
+  END IF;
+  
+  CLOSE cr_crapaca;  
+  
+  dbms_output.put_line('Fim do programa');
+   
+  COMMIT;
+  
+EXCEPTION 
+  WHEN vr_exec_erro THEN
+    
+    dbms_output.put_line('Erro:' || vr_dscritic);
+    ROLLBACK;
+   
+  WHEN OTHERS THEN
+        
+    dbms_output.put_line('Erro a executar o programa:' || SQLERRM);
+
+    ROLLBACK;
+    
+end;
