c0fcf51f7a7ae2f261d7c39b5be664e55b5bde6b
Marcel Vinicius Kohls (AMCOM) <CECRED\t0031792>
2018-04-10 14:20:13 +0000

    atualizacao do script de alteracoa de coluna flcnaulc
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C19038

--- scripts/Sprint5/alterar_params_desope_grava_conta_adiciona_param_flcnaulc.sql
+++ scripts/Sprint5/alterar_params_desope_grava_conta_adiciona_param_flcnaulc.sql
@@ -1,2 +1,2 @@
---select * from crapaca where nmproced='pc_grava_dados_conta'
-update crapaca set LSTPARAM='pr_nrdconta,pr_flgrenli,pr_flmajora,pr_dsmotmaj,pr_flcnaulc' where nmdeacao='DESOPE_GRAVA_CONTA'
+update crapaca set LSTPARAM='pr_nrdconta,pr_flgrenli,pr_flmajora,pr_dsmotmaj,pr_flcnaulc' where nmdeacao='DESOPE_GRAVA_CONTA';
+COMMIT;
