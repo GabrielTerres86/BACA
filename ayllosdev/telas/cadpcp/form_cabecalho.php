455d64c4325e9e02025595b6479b5fd19f463739
Alex Sandro Barrozo (GFT) <CECRED\t0031721>
2018-04-11 20:33:08 +0000

    403-s005-h0000 - Atualização/Merge de fontes de TEST para DEV.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/403 – Desconto de Título/DEV;C19099

--- ayllosdev/telas/cadpcp/form_cabecalho.php
+++ ayllosdev/telas/cadpcp/form_cabecalho.php
@@ -12,6 +12,14 @@ require_once('../../includes/funcoes.php');
 require_once('../../includes/controla_secao.php');
 require_once('../../class/xmlfile.php');
 isPostMethod();
+
+
+	// Carrega permissões do operador
+	include("../../includes/carrega_permissoes.php");	
+	
+	setVarSession("opcoesTela",$opcoesTela);
+
+
 ?>
 
 <form id="frmCab" name="frmCab" class="formulario cabecalho" onSubmit="return false;">
@@ -19,7 +27,10 @@ isPostMethod();
     <label for="cddopcao"><?php echo utf8ToHtml('Op&ccedil;&atilde;o') ?></label>
     <select id="cddopcao" name="cddopcao">
         <option value="C" <?php echo $cddopcao == 'C' ? 'selected' : '' ?>>C - Consultar porcentagem do pagador.</option>
-        <option value="A" <?php echo $cddopcao == 'A' ? 'selected' : '' ?>>A - Alterar porcentagem do pagador.</option>
+		<? if (in_array('A', $glbvars['opcoesTela'])) { ?>
+			<option value="A">A - Alterar porcentagem do pagador</option>
+		<? } ?>
+
     </select>
 
     <a href="#" class="botao" id="btnOk1">Ok</a>
