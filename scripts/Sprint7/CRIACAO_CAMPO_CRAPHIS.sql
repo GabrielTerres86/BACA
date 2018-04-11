eacfde29d73a3fb5ea32e316fb816a73337eba1e
Diego Simas (AMcom) <CECRED\t0031671>
2018-04-11 14:11:41 +0000

    7012 - Alteração tela Histor - Criação de campo na tabela CRAPHIS.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C19073

---
+++ scripts/Sprint7/CRIACAO_CAMPO_CRAPHIS.sql
@@ -0,0 +1,3 @@
+alter table cecred.craphis
+add column inestoura_conta number(1) default 0 not null;
+Comment on column cecred.craphis.inestoura_conta 'Contém o identificador para indicar se o histórico estoura conta';
