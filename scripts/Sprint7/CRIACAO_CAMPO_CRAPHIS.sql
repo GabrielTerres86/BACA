d165d9a6e7abb30b6e9850ecf04136dde50b5a38
Diego Simas (AMcom) <CECRED\t0031671>
2018-04-11 14:45:41 +0000

    Alteração no comentário. (Simas)
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C19075

--- scripts/Sprint7/CRIACAO_CAMPO_CRAPHIS.sql
+++ scripts/Sprint7/CRIACAO_CAMPO_CRAPHIS.sql
@@ -1,3 +1,3 @@
 alter table cecred.craphis
 add column inestoura_conta number(1) default 0 not null;
-Comment on column cecred.craphis.inestoura_conta 'Contém o identificador para indicar se o histórico estoura conta';
+Comment on column cecred.craphis.inestoura_conta 'Contém o identificador para indicar se o histórico estoura conta 0-Não e 1-Sim';
