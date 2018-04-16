9589e4f77954eaa9566f73aed430bebb4636a229
Leonardo de Freitas Oliveira (GFT) <CECRED\t0031725>
2018-04-16 20:11:50 +0000

    403-s006-h0091 - Remoção das linhas bloqueadas na pesquisa
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/403 – Desconto de Título/TEST;C19319

--- ayllosdev/telas/atenda/descontos/titulos/titulos.js
+++ ayllosdev/telas/atenda/descontos/titulos/titulos.js
@@ -2493,9 +2493,6 @@ function mostrarBorderoResumoAlterar() {
             showError("error", "Adicione pelo menos um t&iacute;tulo.", "Alerta - Ayllos", "blockBackground(parseInt($('#divRotina').css('z-index')))");
         }
     }
-    
-    return false;
-    }
     return false;
 }
     
@@ -2913,7 +2910,7 @@ function formataManutencaoDeLimite(){
 
                         if ( campoAnterior == 'cddlinha' ) {
                             
-                        filtrosPesq =   'Código;cddlinha;30px;S;|Descrição;dsdlinha;200px;S;|;flgstlcr;;;;N|Tipo;tpdescto;;;3;N';
+                        filtrosPesq =   'Código;cddlinha;30px;S;|Descrição;dsdlinha;200px;S;|;flgstlcr;;;1;N|Tipo;tpdescto;;;3;N';
                         colunas =       'Código;cddlinha;15%;right|Descrição;dsdlinha;60%;left|Status;flgstlcr;25%;center;';//|Tipo;tpdescto;;;;N
 
                             mostraPesquisa('zoom0001',
