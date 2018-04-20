947712696cb4649e438c3b85427303f544e94f1b
Andre Luis de Avila Dourado (GFT) <CECRED\t0031807>
2018-04-20 23:02:10 +0000

    403-s007-h0554: Permissões Botões do Limite
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/403 – Desconto de Título/DEV;C19589

--- ayllosdev/telas/atenda/descontos/titulos/titulos_limite_alterar_propostas.php
+++ ayllosdev/telas/atenda/descontos/titulos/titulos_limite_alterar_propostas.php
@@ -30,6 +30,10 @@
 	// Classe para leitura do xml de retorno
 	require_once("../../../../class/xmlfile.php");
 	
+		require_once("../../../../includes/carrega_permissoes.php");
+
+	setVarSession("opcoesTela",$opcoesTela);
+	
 	if (($msgError = validaPermissao($glbvars["nmdatela"],$glbvars["nmrotina"],"A")) <> "") {
 		exibeErro($msgError);		
 	}	
