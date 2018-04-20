fe6212e05ad4f963f213a5e65ac09824c1fc894b
Daniel Silva (AMcom) <CECRED\t0031672>
2018-04-20 14:17:37 +0000

    Sprint 6 - Inclusão parametro risco Refin(alteração de escopo)
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C19550

---
+++ scripts/Sprint6/insere_prm_dtriscorefin.sql
@@ -0,0 +1,10 @@
+INSERT INTO crapprm
+     VALUES('CRED'
+           ,0
+           ,'DT_CORTE_RISCO_REFIN'
+           ,'Data de corte para aceleracao do risco na Central'
+           ,to_char(SYSDATE,'DD/MM/YYYY')
+           ,NULL)
+/           
+COMMIT
+/
