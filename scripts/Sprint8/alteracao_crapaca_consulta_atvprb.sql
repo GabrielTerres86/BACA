2e97823dbb561388881e3b09147e46ffbe188f5a
Marcel Vinicius Kohls (AMCOM) <CECRED\t0031792>
2018-04-27 14:17:16 +0000

    correcao de final de linha
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C19892

--- scripts/Sprint8/alteracao_crapaca_consulta_atvprb.sql
+++ scripts/Sprint8/alteracao_crapaca_consulta_atvprb.sql
@@ -1,2 +1,2 @@
-update crapaca set lstparam='pr_idativo,pr_cdcooper,pr_nrdconta,pr_nrctremp,pr_cdmotivo,pr_datainic,pr_datafina, pr_pagina' where nmdeacao='ATVPRB_CONSULTA'
+update crapaca set lstparam='pr_idativo,pr_cdcooper,pr_nrdconta,pr_nrctremp,pr_cdmotivo,pr_datainic,pr_datafina, pr_pagina' where nmdeacao='ATVPRB_CONSULTA';
 COMMIT;
