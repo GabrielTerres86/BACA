85e0db6829f278d622baabd36a1cb92c45d8e473
Reginaldo Rubens da Silva (AMCOM) <CECRED\t0031667>
2018-05-09 18:20:19 +0000

    // Baca para atribuição de permissões de acesso da tela PARDBT //
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Ligeirinho – Debitador Único/DEV;C20231

---
+++ BACA/2018/05/INSERE_CRAPACE_PARDBT.sql
@@ -0,0 +1,37 @@
+/* Libera acesso a tela PARDBT para os operadores do departamento 14 (PRODUTOS),
+   somente da CECRED. */
+declare 
+  cursor cr_crapope is
+	select cdoperad 
+		from crapope
+	 where cdcooper = 3
+		 and cddepart = 14;
+	rw_crapope cr_crapope%ROWTYPE;
+ 
+begin
+  FOR rw_crapope IN cr_crapope LOOP
+		insert into crapace (
+			  nmdatela
+			, cddopcao
+			, cdoperad
+			, nmrotina
+			, cdcooper
+			, nrmodulo
+			, idevento
+			, idambace
+		)
+		values (
+			  'PARDBT'
+			, '@'
+			, rw_crapope.cdoperad
+			, ' '
+			, 3
+			, 12
+			, 0
+			, 2
+		);         	      
+	END LOOP;
+  
+	COMMIT;
+end;
+
