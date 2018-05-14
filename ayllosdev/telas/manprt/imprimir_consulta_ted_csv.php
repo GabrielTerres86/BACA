e04c75ef1452a99a7b7d65bd5d00f0622ad5c821
Fabio Luis Stain (Supero) <CECRED\t0031675>
2018-05-14 18:40:26 +0000

    Merge branch 2018_05. Implementação do estorno e devolução da TED. Implementação da listagem de conciliação. Preparação dos scripts de impressão dos relatórios.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/2018_06;C20378

---
+++ ayllosdev/telas/manprt/imprimir_consulta_ted_csv.php
@@ -0,0 +1,76 @@
+<?php
+
+/***********************************************************************
+  Fonte: imprimir_consulta_ted_csv.php                                              
+  Autor: Hélinton Steffens                                                  
+  Data : Março/2018                       última Alteração: - 		   
+	                                                                   
+  Objetivo  : Gerar o CSV dos titulos.              
+	                                                                 
+  Alterações: 
+  
+***********************************************************************/
+	session_cache_limiter("private");
+	session_start();
+		
+	// Includes para controle da session, variáveis globais de controle, e biblioteca de funções	
+	require_once("../../includes/config.php");
+	require_once("../../includes/funcoes.php");		
+	require_once("../../includes/controla_secao.php");
+	require_once('../../class/xmlfile.php');
+
+	// Verifica se tela foi chamada pelo método POST
+	isPostMethod();
+
+	// Verifica Permissões
+	if (($msgError = validaPermissao($glbvars["nmdatela"],$glbvars["nmrotina"],"@")) <> "") {
+		?><script language="javascript">alert('<?php echo $msgError; ?>');</script><?php
+		exit();	
+	}	
+
+	// Recebe o POST
+	$inidtpro 			= $_POST['inidtpro'] ;
+	$fimdtpro 			= $_POST['fimdtpro'];
+	$inivlpro 			= (isset($_POST['inivlpro'])) ? $_POST['inivlpro'] : 0 ;
+	$fimvlpro 			= (isset($_POST['fimvlpro'])) ? $_POST['fimvlpro'] : 0 ;
+	$dscartor 			= (isset($_POST['dscartor'])) ? $_POST['dscartor'] : null ;
+	$indconci 			= (isset($_POST['indconci'])) ? $_POST['indconci'] : '' ;
+	$nriniseq 			= (isset($_POST['nriniseq'])) ? $_POST['nriniseq'] : 1  ;
+	$nrregist 			= (isset($_POST['nrregist'])) ? $_POST['nrregist'] : 50  ;
+
+	if (($msgError = validaPermissao($glbvars['nmdatela'],$glbvars['nmrotina'],$cddopcao)) <> '') {		
+		exibirErro('error',$msgError,'Alerta - Ayllos','',false);
+	}
+
+	$xml  = "";
+	$xml .= "<Root>";
+	$xml .= " <Dados>";
+	$xml .= "   <cdcooper>".$glbvars["cdcooper"]."</cdcooper>";	
+    $xml .= "   <dtinicial>".$inidtpro."</dtinicial>";
+	$xml .= "   <dtfinal>".$fimdtpro."</dtfinal>";
+	$xml .= "   <vlinicial>".$inivlpro."</vlinicial>";
+	$xml .= "   <vlfinal>".$fimvlpro."</vlfinal>";
+	$xml .= "   <cartorio>".$dscartor."</cartorio>";
+	$xml .= "   <nrregist>".$nrregist."</nrregist>";
+	$xml .= "   <nriniseq>".$nriniseq."</nriniseq>";
+	$xml .= " </Dados>";
+	$xml .= "</Root>";
+
+	$xmlResult = mensageria($xml, "TELA_MANPRT", "EXPORTA_CONSULTA_TED", $glbvars["cdcooper"], $glbvars["cdagenci"], $glbvars["nrdcaixa"], $glbvars["idorigem"], $glbvars["cdoperad"], "</Root>");
+	$xmlObjeto = getObjectXML($xmlResult);
+
+	// Se ocorrer um erro, mostra critica
+	if ($xmlObjeto->roottag->tags[0]->name == "ERRO") {
+		exibeErro($xmlObjeto->roottag->tags[0]->cdata);		 
+	}
+
+	$nmarqcsv = $xmlObjeto->roottag->cdata;
+	visualizaCSV($nmarqcsv);
+
+	// Função para exibir erros na tela através de javascript
+	function exibeErro($msgErro) { 
+	    echo '<script>alert("'.$msgErro.'");</script>';	
+	    exit();
+	}
+ 
+?>
