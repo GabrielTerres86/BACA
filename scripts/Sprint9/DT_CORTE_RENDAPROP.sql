5e99b912bf26514d2a35f8298d803aaa9e965708
Rangel Decker (AMCOM) <CECRED\t0031734>
2018-05-15 19:59:02 +0000

    7570 - Rendas a Apropriar Data de Corte
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C20461

---
+++ scripts/Sprint9/DT_CORTE_RENDAPROP.sql
@@ -0,0 +1,14 @@
+INSERT INTO CRAPPRM
+( nmsistem ,
+  cdcooper ,
+  cdacesso ,
+  dstexprm ,
+  dsvlrprm 
+)
+VALUES
+('CRED',
+  0,
+  'DT_CORTE_RENDAPROP',
+  'Data de corte do projeto Rendas a apropriar',
+  '06/04/2018'
+);
