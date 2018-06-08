912f61ea7fa5083f4827594c7f72919eba42dcb2
Andre Clemer (Supero) <CECRED\t0031381>
2018-06-08 14:02:15 +0000

    Criação da consulta de custas
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/2018_05;C21392

---
+++ ayllosdev/telas/manprt/obtem_consulta_custas.php
@@ -0,0 +1,71 @@
+<?php
+	/***********************************************************************
+	Fonte: obtem_consulta_custas.php                                              
+	Autor: André Clemer                                                  
+	Data : Abril/2018                       Última Alteração: - 		   
+																		
+	Objetivo  : Consulta de custas para front-end.              
+																		
+	Alterações: 
+	
+	***********************************************************************/
+	session_start();
+		
+	// Includes para controle da session, variáveis globais de controle, e biblioteca de funções	
+	require_once("../../includes/config.php");
+	require_once("../../includes/funcoes.php");		
+	require_once("../../includes/controla_secao.php");
+	require_once('../../class/xmlfile.php');
+
+	// Verifica se tela foi chamada pelo método POST
+	isPostMethod();
+
+	// Verifica Permissões
+	if (($msgError = validaPermissao($glbvars["nmdatela"],$glbvars["nmrotina"],"@")) <> "") {
+		?><script language="javascript">alert('<?php echo $msgError; ?>');</script><?php
+		exit();	
+	}	
+
+	// Recebe o POST
+	$inidtpro 			= $_POST['inidtpro'] ;
+	$fimdtpro 			= $_POST['fimdtpro'];
+    $cdcooper 			= (isset($_POST['cdcooper'])) ? $_POST['cdcooper'] : null;
+    $nrdconta 			= (isset($_POST['nrdconta'])) ? $_POST['nrdconta'] : null;
+	$cduflogr 			= (isset($_POST['cduflogr'])) ? $_POST['cduflogr'] : null;
+    $dscartor 			= (isset($_POST['dscartor'])) ? $_POST['dscartor'] : null;
+
+	if (($msgError = validaPermissao($glbvars['nmdatela'],$glbvars['nmrotina'],$cddopcao)) <> '') {		
+		exibirErro('error',$msgError,'Alerta - Ayllos','',false);
+	}
+
+	$xml  = "";
+	$xml .= "<Root>";
+	$xml .= " <Dados>";
+	$xml .= "   <cdcooper>".$cdcooper."</cdcooper>";
+    $xml .= "   <nrdconta>".$nrdconta."</nrdconta>";	
+    $xml .= "   <dtinicial>".$inidtpro."</dtinicial>";
+	$xml .= "   <dtfinal>".$fimdtpro."</dtfinal>";
+	$xml .= "   <cduflogr>".$cduflogr."</cduflogr>";
+	$xml .= "   <cartorio></cartorio>";
+	$xml .= " </Dados>";
+	$xml .= "</Root>";
+
+	$xmlResult = mensageria($xml, "TELA_MANPRT", "CONSULTA_CUSTAS", $glbvars["cdcooper"], $glbvars["cdagenci"], $glbvars["nrdcaixa"], $glbvars["idorigem"], $glbvars["cdoperad"], "</Root>");
+	$xmlObjeto = getObjectXML($xmlResult);
+
+	// Se ocorrer um erro, mostra critica
+	if ( strtoupper($xmlObjeto->roottag->tags[0]->name) == 'ERRO' ) {
+	 	exibirErro('error',$xmlObjeto->roottag->tags[0]->cdata,'Alerta - Ayllos','',false);
+	}
+
+	$registros 	= $xmlObjeto->roottag->tags[0]->tags;
+
+	//include('form_opcao_cabecalho.php');
+	include('form_opcao_r.php');
+	include('form_consulta_custas.php');
+		
+?>
+
+<script>
+	controlaOpcao();
+</script>
