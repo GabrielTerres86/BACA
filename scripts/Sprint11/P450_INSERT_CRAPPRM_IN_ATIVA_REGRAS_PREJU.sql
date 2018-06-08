180612c94027b8890d5957301a6e4dc3d78cc8aa
Reginaldo Rubens da Silva (AMCOM) <CECRED\t0031667>
2018-06-08 13:35:07 +0000

    // Criação de parâmetro para ativação das regras de prejuízo na LANC0001 //
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C21389

---
+++ scripts/Sprint11/P450_INSERT_CRAPPRM_IN_ATIVA_REGRAS_PREJU.sql
@@ -0,0 +1,8 @@
+INSERT INTO crapprm
+     VALUES('CRED'
+           ,0
+           ,'IN_ATIVA_REGRAS_PREJU'
+           ,'Indicador de ativação das regras de prejuízo para uso na LANC0001 (S/N)'
+           ,'N'
+           ,NULL);
+COMMIT;
