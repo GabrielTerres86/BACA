6bbfe571adb30d990ac4d8d447b7f724ef42c2cf
Guilherme Boettcher (AMCOM) <CECRED\t0031670>
2018-06-20 11:21:46 +0000

    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C22016

--- scripts/Sprint12/P450_ADD_CAMPO_TBRISCO_CENTRAL_OCR.sql
+++ scripts/Sprint12/P450_ADD_CAMPO_TBRISCO_CENTRAL_OCR.sql
@@ -1,5 +1,5 @@
 -- Add/modify columns 
-alter table CECRED.TBRISCO_CENTRAL_OCR add qtdias_atraso_refin number(5);
+alter table CECRED.TBRISCO_CENTRAL_OCR add qtdias_atraso_refin number(5) default 0 not null;
 -- Add comments to the columns 
 comment on column CECRED.TBRISCO_CENTRAL_OCR.qtdias_atraso_refin
   is 'Quantidade Dias Atraso utilizado para calculo Risco Refinanciamento';
