169d538d9793a35653e96483e38879f4d05007e7
Reginaldo Rubens da Silva (AMCOM) <CECRED\t0031667>
2018-06-27 10:58:43 +0000

    // Complementação do script //
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C22302

--- scripts/Sprint12/P450_ADD_CAMPO_VLBLQPRJ_SLD_SDA.sql
+++ scripts/Sprint12/P450_ADD_CAMPO_VLBLQPRJ_SLD_SDA.sql
@@ -9,3 +9,6 @@ alter table CECRED.CRAPSDA add vlblqprj number(25,2) default 0 not null;
 -- Add comments to the columns 
 comment on column CECRED.CRAPSDA.vlblqprj
   is 'Valor do saldo da conta transitória (créditos bloqueados por prejuízo em conta)';
+  
+-- Add/modify columns 
+alter table CECRED.CRAPHIS rename column intrasnf_cred_prejuizo to INTRANSF_CRED_PREJUIZO;
