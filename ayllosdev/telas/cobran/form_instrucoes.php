dafe83e0ff663505e7f05a3c975241cd065d7cfd
Andre Clemer (Supero) <CECRED\t0031381>
2018-06-27 17:27:44 +0000

    Alterações referente ao PRJ352
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/2018_05;C22332

--- ayllosdev/telas/cobran/form_instrucoes.php
+++ ayllosdev/telas/cobran/form_instrucoes.php
@@ -22,7 +22,8 @@
 $esta_em_cartorio =  ( in_array($insitcrt, array('1','2','3') )) ? 1 : 0;
 $tem_inst_protest = (( strcasecmp($flgdprot, 'yes') == 0 ) && $qtdiaprt > 0 ) ? 1 : 0;
 $esta_protestado  =  ( $insitcrt == '5' ) ? 1 : 0;
-$esta_negativado  = ((( strcasecmp($flserasa, 'yes') == 0 ) || ( $qtdianeg > 0 )) && ($inserasa > 0)) ? 1 : 0;
+$esta_negativado  = (( strcasecmp($flserasa, 'yes') == 0 || $qtdianeg > 0 ) && $inserasa > 0) ? 1 : 0;
+$tem_inst_serasa  = strcasecmp($flserasa, 'yes') == 0 || $qtdianeg > 0 ? 1 : 0;
 
 $vr_dtvencto = implode('-', array_reverse(explode('/', $dtvencto)));
 $vr_dtmvtolt = implode('-', array_reverse(explode('/', $glbvars['dtmvtolt'])));
@@ -35,6 +36,11 @@ $exec_inst_auto = $vr_dtvencto > $vr_dtmvtolt ? 1 : 0;
 foreach( $registro as $r ) {
     // Por padrão vamos criar a opção da instrução em tela
 	$cria_opcao = 1;
+	
+	if (getByTagName($r->tags,'cdocorre') == 94) { // Instrução Excluir Serasa substituida pela 41 - Cancelar Instrução Automática de Protesto/Serasa
+		$cria_opcao = 0;
+	}
+	
 	// Verificar se já esta protestado
 	if ($esta_protestado == 1){
 		// Não exibir as seguintes regras 
@@ -97,7 +103,7 @@ foreach( $registro as $r ) {
 			// Não criar a opção
 			$cria_opcao = 0;
 		} 
-		if ($esta_negativado == 0 && $esta_em_cartorio == 0){
+		if ($tem_inst_serasa == 0 && $esta_negativado == 0 && $esta_em_cartorio == 0 && $tem_inst_protest == 0){
 			// Não está Negativado no Serasa
 			if( getByTagName($r->tags,'cdocorre') == 41) { // Cancelar Instrução Automática de Protesto/Serasa
 				// Não criar a opção
