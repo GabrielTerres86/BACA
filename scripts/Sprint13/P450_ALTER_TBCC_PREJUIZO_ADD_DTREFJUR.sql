1338615471c0055a49571973a2c4fc0868608371
Reginaldo Rubens da Silva (AMCOM) <CECRED\t0031667>
2018-07-04 13:34:12 +0000

    // Acréscimo de campo para o tipo de transferência  //
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C22695

--- scripts/Sprint13/P450_ALTER_TBCC_PREJUIZO_ADD_DTREFJUR.sql
+++ scripts/Sprint13/P450_ALTER_TBCC_PREJUIZO_ADD_DTREFJUR.sql
@@ -12,3 +12,10 @@ comment on column CECRED.TBCC_PREJUIZO.mesrefju
   is 'Mes de referencia da ultima vez que foi calculado juros para o saldo devedor do prejuizo';
 comment on column CECRED.TBCC_PREJUIZO.anorefju
   is 'Ano  de referencia da ultima vez que foi calculado juros para o saldo devedor do prejuizo';
+
+
+-- Add/modify columns 
+alter table CECRED.TBCC_PREJUIZO add intipo_transf number(1) default 0 not null;
+-- Add comments to the columns 
+comment on column CECRED.TBCC_PREJUIZO.intipo_transf
+  is 'Indicador do tipo de transferencia (0 - Transferencia por atraso/estouro de conta  /  1 - Transferencia por fraude)';
