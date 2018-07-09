5afa869f76c9459640d89bf03c584ee4f7e0e6ba
Guilherme Boettcher (AMCOM) <CECRED\t0031670>
2018-07-09 12:35:49 +0000

    Ajuste do nome do campo
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C22817

--- scripts/Sprint11/P450_MODIFY_CRAPHIS_ADD_INTRANSF_CRED_PREJUIZO.sql
+++ scripts/Sprint11/P450_MODIFY_CRAPHIS_ADD_INTRANSF_CRED_PREJUIZO.sql
@@ -1,7 +1,7 @@
 -- Add/modify columns 
-alter table CECRED.CRAPHIS add intrasnf_cred_prejuizo number(1) default 1 not null;
+alter table CECRED.CRAPHIS add intransf_cred_prejuizo number(1) default 1 not null;
 -- Add comments to the columns 
-comment on column CECRED.CRAPHIS.intrasnf_cred_prejuizo
+comment on column CECRED.CRAPHIS.intransf_cred_prejuizo
   is 'Indica se o historico de credito deve ser transferido para a conta transitoria caso a conta esteja em prejuizo (0 - Nao, 1 - Sim)';
 comment on column CECRED.CRAPHIS.inestoura_conta
   is 'Contem o identificador para indicar se o historico deve ser debitado caso a conta esteja com atraso (ADP) de 60 dias ou mais. (0-Não e 1-Sim)';
