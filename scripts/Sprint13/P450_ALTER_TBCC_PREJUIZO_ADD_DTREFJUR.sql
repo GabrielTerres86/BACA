03c08753af8061afe38a0e1e06df4cdf318fdd37
Reginaldo Rubens da Silva (AMCOM) <CECRED\t0031667>
2018-07-09 13:31:34 +0000

    // Adequação dos nomes de campos ao padrão de novas tabelas //
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C22825

--- scripts/Sprint13/P450_ALTER_TBCC_PREJUIZO_ADD_DTREFJUR.sql
+++ scripts/Sprint13/P450_ALTER_TBCC_PREJUIZO_ADD_DTREFJUR.sql
@@ -1,16 +1,16 @@
 -- Add/modify columns 
 alter table CECRED.TBCC_PREJUIZO add dtrefjur date;
-alter table CECRED.TBCC_PREJUIZO add diarefju number(2);
-alter table CECRED.TBCC_PREJUIZO add mesrefju number(2);
-alter table CECRED.TBCC_PREJUIZO add anorefju number(4);
+alter table CECRED.TBCC_PREJUIZO add nrdiarefju number(2);
+alter table CECRED.TBCC_PREJUIZO add nrmesrefju number(2);
+alter table CECRED.TBCC_PREJUIZO add nranorefju number(4);
 -- Add comments to the columns 
 comment on column CECRED.TBCC_PREJUIZO.dtrefjur
   is 'Data de referencia da ultima vez que foi calculado juros para o saldo devedor do prejuizo';
-comment on column CECRED.TBCC_PREJUIZO.diarefju
+comment on column CECRED.TBCC_PREJUIZO.nrdiarefju
   is 'Dia de referencia da ultima vez que foi calculado juros para o saldo devedor do prejuizo';
-comment on column CECRED.TBCC_PREJUIZO.mesrefju
+comment on column CECRED.TBCC_PREJUIZO.nrmesrefju
   is 'Mes de referencia da ultima vez que foi calculado juros para o saldo devedor do prejuizo';
-comment on column CECRED.TBCC_PREJUIZO.anorefju
+comment on column CECRED.TBCC_PREJUIZO.nranorefju
   is 'Ano  de referencia da ultima vez que foi calculado juros para o saldo devedor do prejuizo';
 
 
