d0942cb354c2529f113f7a040077025c242ea89d
Luis Fernando Sacchi Ribeiro (GFT) <CECRED\t0031723>
2018-07-18 18:58:05 +0000

    403 - Merge do script 10 com o 3
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/403 – Desconto de Título/TEST;C23196

--- sql/Release 3/03 - Piloto por cooperativa - Inicia.sql
+++ sql/Release 3/03 - Piloto por cooperativa - Inicia.sql
@@ -134,6 +134,88 @@ BEGIN
     /*Bloqueia a tela LANBDT do ayllos caracter*/
     UPDATE craptel SET FLGTELBL = 0 WHERE nmdatela = 'LANBDT' AND cdcooper = pr_cdcooper;
 
+    /* INSERE COBTIT */
+    -- Insere a tela
+    INSERT INTO craptel 
+        (nmdatela,
+         nrmodulo,
+         cdopptel,
+         tldatela,
+         tlrestel,
+         flgteldf,
+         flgtelbl,
+         nmrotina,
+         lsopptel,
+         inacesso,
+         cdcooper,
+         idsistem,
+         idevento,
+         nrordrot,
+         nrdnivel,
+         nmrotpai,
+         idambtel)
+        SELECT 'COBTIT', 
+               5, 
+               '@,C,B,G,X,D,Y', 
+               'Cobrança de Títulos', 
+               'Cobrança de Títulos', 
+               0, 
+               1, -- bloqueio da tela 
+               ' ', 
+               'Acesso,Consultar,Baixa,Gerar Boleto,Enviar Email/SMS,Desconto Prejuizo,Boletagem Massiva', 
+               0, 
+               cdcooper, -- cooperativa
+               1, 
+               0, 
+               1, 
+               1, 
+               '', 
+               2 
+          FROM crapcop          
+         WHERE cdcooper = pr_cdcooper; 
+
+    -- Permissões de consulta para os usuários pré-definidos pela CECRED                       
+    INSERT INTO crapace (nmdatela,cddopcao,cdoperad,nmrotina,cdcooper,nrmodulo,idevento,idambace) (SELECT 'COBTIT',cddopcao,cdoperad,nmrotina,cdcooper,nrmodulo,idevento,idambace FROM crapace WHERE nmdatela ='COBEMP' AND cdcooper = pr_cdcooper);
+    -- Insere o registro de cadastro do programa
+    INSERT INTO crapprg
+        (nmsistem,
+         cdprogra,
+         dsprogra##1,
+         dsprogra##2,
+         dsprogra##3,
+         dsprogra##4,
+         nrsolici,
+         nrordprg,
+         inctrprg,
+         cdrelato##1,
+         cdrelato##2,
+         cdrelato##3,
+         cdrelato##4,
+         cdrelato##5,
+         inlibprg,
+         cdcooper) 
+        SELECT 'CRED',
+               'COBTIT',
+               'Cobrança de Títulos',
+               '.',
+               '.',
+               '.',
+               (SELECT max(nrsolici)+1 FROM crapprg),
+               NULL,
+               1,
+               0,
+               0,
+               0,
+               0,
+               0,
+               1,
+               cdcooper
+          FROM crapcop          
+         WHERE cdcooper IN pr_cdcooper;
+    IF (pr_cdcooper=3) THEN
+      UPDATE crapprg c SET cdrelato##1=(SELECT cdrelato##1 FROM crapprg WHERE cdprogra = 'COBEMP' AND cdcooper = 3) WHERE cdprogra='COBTIT' AND cdcooper = 3;
+    END IF;
+
   END LOOP;
   COMMIT;
 END;
---
+++ sql/Release 3/25 - Insercao de carga da tabela de CRITICAS.sql
@@ -0,0 +1,17 @@
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (1,'Qtd Remessa em Cartório acima do permitido',0);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (2,'Qtd de Títulos Protestados acima do permitido',0);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (3,'Qtd de Títulos Não Pagos pelo Pagador acima do permitido',0);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (4,'Perc. Mínimo de Liquidez Cedente x Pagador abaixo do permitido (Qtd. de Títulos).',0);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (5,'Perc. Mínimo de Liquidez Cedente x Pagador abaixo do permitido (Valor dos Títulos)',0);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (6,'Perc. Concentração Máxima Permitida de Títulos excedida',0);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (7,'Emitente é Cônjuge/Sócio do Pagador',0);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (8,'Cooperado possui Títulos Descontados na Conta deste Pagador',0);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (9,'Valor Máximo Permitido por CNAE excedido',2);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (10,'Tit. não possui instr. de protesto e/ou negativação',2);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (11,'Valida Quantidade de Titulos protestados (carteira cooperado)',3);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (12,'Coop com titulos não pagos acima do permitido',3);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (13,'Perc. de Tit. com Mínimo de Liquidez de Títulos Geral do Cedente abaixo do permitido. (Qtd. de Títulos)',3);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (14,'Perc. de Tit. com Mínimo de Liquidez de Títulos Geral do Cedente abaixo do permitido. (Valor dos Títulos)',3);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (15,'Valores utilizados excedidos',1);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (16,'Valor Legal Excedido',1);
+INSERT INTO tbdsct_criticas (cdcritica,dscritica,tpcritica) VALUES (17,'Quantidade de títulos por borderô excedido',1);
