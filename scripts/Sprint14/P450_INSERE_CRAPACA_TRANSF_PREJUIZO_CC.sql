1aede1b31f02dd6853a10cf403349910b27924b9
Reginaldo Rubens da Silva (AMCOM) <CECRED\t0031667>
2018-07-25 18:48:36 +0000

    // Inclusão de ação na CRAPACA para Transf. Prejuízo CC por Fraude //
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C23465

---
+++ scripts/Sprint14/P450_INSERE_CRAPACA_TRANSF_PREJUIZO_CC.sql
@@ -0,0 +1,106 @@
+PL/SQL Developer Test script 3.0
+102
+declare 
+
+  CURSOR cr_craprdr(pr_nmprogra IN craprdr.nmprogra%TYPE) IS
+  SELECT * FROM craprdr
+   WHERE craprdr.nmprogra = pr_nmprogra;
+  rw_craprdr cr_craprdr%ROWTYPE;
+
+  CURSOR cr_crapaca(pr_nmdeacao IN crapaca.nmdeacao%TYPE
+                   ,pr_nmpackag IN crapaca.nmpackag%TYPE
+                   ,pr_nmproced IN crapaca.nmproced%TYPE
+                   ,pr_nrseqrdr IN crapaca.nrseqrdr%TYPE) IS
+  SELECT * FROM crapaca
+   WHERE UPPER(crapaca.nmdeacao) = UPPER(pr_nmdeacao)
+     AND UPPER(crapaca.nmpackag) = UPPER(pr_nmpackag)
+     AND UPPER(crapaca.nmproced) = UPPER(pr_nmproced)
+     AND crapaca.nrseqrdr = pr_nrseqrdr;
+  rw_crapaca cr_crapaca%ROWTYPE;
+  
+  vr_exec_erro EXCEPTION;
+  vr_dscritic  VARCHAR2(1000);  
+
+begin
+  
+  dbms_output.put_line('Inicio do programa');
+  
+  -- Mensageria
+  OPEN cr_craprdr(pr_nmprogra => 'PREJ0003');
+  FETCH cr_craprdr INTO rw_craprdr;
+    
+  -- Verifica se existe a tela do ayllos web
+  IF cr_craprdr%NOTFOUND THEN
+   
+    -- Se não encontrar
+    CLOSE cr_craprdr;
+
+    -- Insere ação da tela do ayllos web
+    INSERT INTO craprdr(nmprogra
+                       ,dtsolici) 
+                 values('PREJ0003'
+                       ,SYSDATE);
+    -- Posiciona no registro criado
+    OPEN cr_craprdr(pr_nmprogra => 'PREJ0003');
+    FETCH cr_craprdr INTO rw_craprdr;
+
+    dbms_output.put_line('Insere CRAPRDR -> PREJ0003: ' || rw_craprdr.nrseqrdr);
+
+  END IF;  
+  
+  -- Fecha o cursor
+  CLOSE cr_craprdr;
+
+  -- INICIO MENSAGERIA   
+  
+  -- TAB089 - ACAO CONSULTAR
+  OPEN cr_crapaca(pr_nmdeacao => 'TRANSF_PREJUIZO_CC'
+                 ,pr_nmpackag => 'PREJ0003'
+                 ,pr_nmproced => 'pc_transf_cc_preju_fraude'
+                 ,pr_nrseqrdr => rw_craprdr.nrseqrdr);
+                   
+  FETCH cr_crapaca INTO rw_crapaca;
+    
+  -- Verifica se existe a ação tela do ayllos web
+  IF cr_crapaca%NOTFOUND THEN
+    
+    -- Insere ação da tela do ayllos web
+    INSERT INTO crapaca(nmdeacao, 
+                        nmpackag, 
+                        nmproced, 
+                        lstparam, 
+                        nrseqrdr) 
+                 VALUES('TRANSF_PREJUIZO_CC',
+                        'PREJ0003',
+                        'pc_transf_cc_preju_fraude',
+                        'pr_cdcooper, pr_nrdconta',
+                        rw_craprdr.nrseqrdr);
+                             
+    dbms_output.put_line('Insere CRAPACA -> TRANSF_PREJUIZO_CC -> PREJ0003.pc_transf_cc_preju_fraude');
+    
+  END IF;
+  
+  CLOSE cr_crapaca; 
+  
+  
+ 
+  
+  dbms_output.put_line('Fim do programa');
+   
+  COMMIT;
+  
+EXCEPTION 
+  WHEN vr_exec_erro THEN
+    
+    dbms_output.put_line('Erro:' || vr_dscritic);
+    ROLLBACK;
+   
+  WHEN OTHERS THEN
+        
+    dbms_output.put_line('Erro a executar o programa:' || SQLERRM);
+
+    ROLLBACK;
+    
+end;
+0
+0
