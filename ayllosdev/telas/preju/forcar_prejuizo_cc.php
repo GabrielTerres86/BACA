5c8511554ecf9e6d08032b8741469c24330affef
Reginaldo Rubens da Silva (AMCOM) <CECRED\t0031667>
2018-07-25 19:11:22 +0000

    // Remoção de comentários desnecessários do código  //
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C23471

--- ayllosdev/telas/preju/forcar_prejuizo_cc.php
+++ ayllosdev/telas/preju/forcar_prejuizo_cc.php
@@ -38,36 +38,6 @@
 		echo "showConfirmacao('Confirma transferência da conta corrente para prejuízo?','Confirma&ccedil;&atilde;o - Ayllos','carregaPrejuizoCC(\'$nrdconta\', 1);','estadoInicial();','sim.gif','nao.gif');";
 		exit();
 	}else{
-		// se confirmar
-		// Monta o xml de requisição
-		// ******************* ADEQUAR PARA CHAMDA DA PROCEDURE DESENVOLVIDA PELO ANDERSON ******************
-		/*$xml  = "";
-		$xml .= "<Root>";
-		$xml .= "	<Cabecalho>";
-		$xml .= "		<Bo>b1wgen0199.p</Bo>";
-		$xml .= "		<Proc>grava_dados</Proc>";
-		$xml .= "	</Cabecalho>";
-		$xml .= "	<Dados>";
-		$xml .= "		<cdcooper>".$glbvars["cdcooper"]."</cdcooper>";
-		$xml .= "		<cdagenci>".$glbvars["cdpactra"]."</cdagenci>";
-		$xml .= "		<nrdcaixa>".$glbvars["nrdcaixa"]."</nrdcaixa>";
-		$xml .= "		<cdoperad>".$glbvars["cdoperad"]."</cdoperad>";
-		$xml .= "		<nmdatela>".$nmdatela."</nmdatela>";
-		$xml .= "		<idorigem>".$glbvars["idorigem"]."</idorigem>";
-		$xml .= "		<nrdconta>".$nrdconta."</nrdconta>";
-		$xml .= "		<idseqttl>".$idseqttl."</idseqttl>";
-		$xml .= "		<dtmvtolt>".$glbvars["dtmvtolt"]."</dtmvtolt>";
-		$xml .= "		<dtmvtopr>".$glbvars["dtmvtopr"]."</dtmvtopr>";
-		$xml .= "		<dtdpagto>".$glbvars["dtmvtopr"]."</dtdpagto>";
-		$xml .= "		<cdfinemp>".$cdfinemp."</cdfinemp>";
-		$xml .= "		<cdlcremp>".$cdlcremp."</cdlcremp>";
-		$xml .= "	</Dados>";
-		$xml .= "</Root>"; 
-		
-		$xmlResult = getDataXML($xml,false);
-		$xmlObj = getObjectXML(retiraAcentos(removeCaracteresInvalidos($xmlResult)));
-		$xmlObj = simplexml_load_string($xmlResult); */
-
 		// Monta o xml de requisição
 		$xml = "<Root>";
 		$xml .= " <Dados>";
