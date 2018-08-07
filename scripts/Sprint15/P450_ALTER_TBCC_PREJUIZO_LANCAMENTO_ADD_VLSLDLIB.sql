4b9df314a287374adf48c289e29464ca70445516
Reginaldo Rubens da Silva (AMCOM) <CECRED\t0031667>
2018-08-07 14:19:02 +0000

    // Remoção de script decartado por erro //
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C23896

--- scripts/Sprint15/P450_ALTER_TBCC_PREJUIZO_LANCAMENTO_ADD_VLSLDLIB.sql
+++
@@ -1,5 +0,0 @@
--- Add/modify columns 
-alter table CECRED.TBCC_PREJUIZO_LANCAMENTO add vlsldlib number(25,2) default 0 not null;
--- Add comments to the columns 
-comment on column CECRED.TBCC_PREJUIZO_LANCAMENTO.vlsldlib
-  is 'Saldo liberado para operacoes na Conta Corrente';
