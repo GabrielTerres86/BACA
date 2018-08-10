5d9760f804e914e0c765c16588a5eccc027d7b1d
Reginaldo Rubens da Silva (AMCOM) <CECRED\t0031667>
2018-08-10 19:37:57 +0000

    // Baca para juste dos históricos do prejuízo //
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C24125

---
+++ scripts/Sprint15/P450_ajusta_historicos_prejuizo.sql
@@ -0,0 +1,28 @@
+-- Marca os históricos para lançamento aberto por PA no CRPS249
+UPDATE craphis his
+   SET his.tpctbcxa = 2
+ WHERE his.cdhistor IN (2408, 2412, 2716, 2717, 2721, 2722)
+;	  
+
+-- Ajusta NMESTRUT (nome da tabela) dos históricos
+UPDATE craphis his
+   SET his.nmestrut = 'TBCC_PREJUIZO_DETALHE'
+ WHERE his.cdhistor IN (2408, 2412, 2716, 2717, 2721, 2733, 2723, 2725, 2727, 2729, 2722, 2732, 2724, 2726, 2728, 2730)
+;	 
+
+-- Ajusta NMESTRUT (nome da tabela) dos históricos
+UPDATE craphis his
+   SET his.nmestrut = 'TBCC_PREJUIZO_LANCAMENTO'
+ WHERE his.cdhistor IN (2738, 2739)
+;
+
+-- Ajusta as flags com efeito nas rotinas de débito/crédito para contas em prejuízo
+UPDATE craphis his
+   SET his.inestoura_conta = 1        -- Debita mesmo com a conta estourada
+	   , his.indebprj = 1               -- Debita mesmo que a conta esteja em prejuízo
+		 , his.intransf_cred_prejuizo = 0 -- Se deve transferir o crédito para a conta transitória
+ WHERE his.cdhistor IN (2408,2412,2716,2717,2718,2719,2720,2738,2739,2721,2733,2723,2725,2727,2729,2722,2732,2724,2726,2728,2730)		 
+;
+
+COMMIT;
+		  
