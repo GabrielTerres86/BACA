396c1d0adc85775dc89d72820a136601b379ecd1
Renato Raul Cordeiro (AMCOM) <CECRED\t0031841>
2018-08-16 12:08:24 +0000

    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C24370

--- scripts/Sprint15/p450_Cria_JOB_efetiv_lanc_pend.sql
+++ scripts/Sprint15/p450_Cria_JOB_efetiv_lanc_pend.sql
@@ -1,17 +1,33 @@
-BEGIN
-  dbms_scheduler.drop_job(job_name => 'JBEPR_MULT_JUR_TR');
-  DBMS_SCHEDULER.CREATE_JOB (job_name => 'JBGEN_EFETIVA_LCTO_PENDENTE', 
-                             job_type => 'PLSQL_BLOCK', 
-                             job_action => 'begin TELA_LAUTOM.pc_efetiva_lcto_pendente_job; end;', 
-                             number_of_arguments => 0, 
-                             start_date => '27/07/16 11:00:00,000000 AMERICA/SAO_PAULO', 
-                             repeat_interval => 'FREQ=DAILY;BYHOUR=11,17', 
-                             end_date => null, 
-                             job_class => 'DEFAULT_JOB_CLASS', 
-                             enabled => FALSE, 
-                             auto_drop => TRUE, 
-                             comments => null, 
-                             credential_name => null, 
-                             destination_name => null);
-END;
-/
+Begin   EXECUTE IMMEDIATE 'ALTER SESSION SET      
+  nls_calendar = ''GREGORIAN''     
+  nls_comp = ''BINARY''     
+  nls_date_format = ''DD-MON-RR''     
+  nls_date_language = ''AMERICAN''     
+  nls_iso_currency = ''AMERICA''     
+  nls_language = ''AMERICAN''     
+  nls_length_semantics = ''BYTE''     
+  nls_nchar_conv_excp = ''FALSE''     
+  nls_numeric_characters = ''.,''     
+  nls_sort = ''BINARY''     
+  nls_territory = ''AMERICA''     
+  nls_time_format = ''HH.MI.SSXFF AM''     
+  nls_time_tz_format = ''HH.MI.SSXFF AM TZR''     
+  nls_timestamp_format = ''DD-MON-RR HH.MI.SSXFF AM''     
+  nls_timestamp_tz_format = ''DD-MON-RR HH.MI.SSXFF AM TZR'''; 
+ 
+  sys.dbms_scheduler.drop_job(job_name => 'CECRED.JBEPR_MULT_JUR_TR');               --Incluir o DROP sempre que houver alteração de configuração de um JOB já existente 
+ 
+  sys.dbms_scheduler.create_job( 
+   job_name            => 'CECRED.JBGEN_EFETIVA_LCTO_PENDENTE', 
+   job_type            => 'PLSQL_BLOCK', 
+   job_action          => 'begin TELA_LAUTOM.pc_efetiva_lcto_pendente_job; end;',
+   start_date          => to_date('27-07-2016 11:00:00', 'dd-mm-yyyy hh24:mi:ss'),
+   repeat_interval => 'FREQ=DAILY;BYHOUR=11,17', 
+   end_date            => null,                    
+   job_class           => 'DEFAULT_JOB_CLASS',                    
+   enabled             => true,                    
+   auto_drop           => true,                    
+   comments            => ''
+   ); 
+end;
+/ 
