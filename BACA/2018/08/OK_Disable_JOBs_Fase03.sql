e1f51ede5023b7472d0cffc6907ec33dde1ed26f
Marcelo Elias Goncalves (AMCOM) <CECRED\t0031760>
2018-08-17 17:41:55 +0000

    Excluido não usado
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Ligeirinho – Debitador Único/TEST;C24455

--- BACA/2018/08/OK_Disable_JOBs_Fase03.sql
+++
@@ -1,7 +0,0 @@
---Desativar JOBs
-BEGIN
-  dbms_scheduler.disable ('CECRED.JBRCEL_DEBAGE');           --rcel0001   
-  dbms_scheduler.disable ('CECRED.JBPAGTO_AGENDAMENTO_DEB'); --CRPS509_CRPS642_CRPS688_DEBBAN
-END;
-/
-commit;
