6c0a1ee7e770f9edcb69a4ac0b8d7093dca676e6
Reginaldo Rubens da Silva (AMCOM) <CECRED\t0031667>
2018-08-21 23:03:11 +0000

    // Inclusão  do histórico 2323 na marcação para debitar mesmo com a conta em prejuízo //
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C24721

--- scripts/Sprint15/P450_ajusta_historicos_prejuizo.sql
+++ scripts/Sprint15/P450_ajusta_historicos_prejuizo.sql
@@ -21,7 +21,7 @@ UPDATE craphis his
    SET his.inestoura_conta = 1        -- Debita mesmo com a conta estourada
 	   , his.indebprj = 1               -- Debita mesmo que a conta esteja em prejuízo
 		 , his.intransf_cred_prejuizo = 0 -- Se deve transferir o crédito para a conta transitória
- WHERE his.cdhistor IN (2408,2412,2716,2717,2718,2719,2720,2738,2739,2721,2733,2723,2725,2727,2729,2722,2732,2724,2726,2728,2730)		 
+ WHERE his.cdhistor IN (2408,2412,2716,2717,2718,2719,2720,2738,2739,2721,2733,2723,2725,2727,2729,2722,2732,2724,2726,2728,2730,2323)		 
 ;
 
 COMMIT;
