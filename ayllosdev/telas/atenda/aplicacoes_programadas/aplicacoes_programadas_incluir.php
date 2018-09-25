83ea73acec90d25a7296e0fe30d40eac32972e11
Anderson Fossa <anderson.fossa@ailos.coop.br>
2018-09-25 14:25:20 +0000

    P411.2 - Comentar a mensagem de solicitação de impressão do termo, paleativamente.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/2018_08;C25977

--- ayllosdev/telas/atenda/aplicacoes_programadas/aplicacoes_programadas_incluir.php
+++ ayllosdev/telas/atenda/aplicacoes_programadas/aplicacoes_programadas_incluir.php
@@ -153,7 +153,11 @@
 	echo "(executandoProdutos == true) ? callafterPoupanca = \"encerraRotina(true)\" : callafterPoupanca = \"".$acessaaba."\";";	
 	
 	// Efetua a impressão do termo de entrega
-	echo 'showConfirmacao("Deseja visualizar a impress&atilde;o?","Confirma&ccedil;&atilde;o - Ayllos","imprimirAutorizacao(\''.$nrdrowid.'\',\'1\');","'.$acessaaba.'","sim.gif","nao.gif");';	
+
+// Comentado temporariamente - pois a impressao por aqui, acaba pegando o registro selecionado no grid
+// echo 'showConfirmacao("Deseja visualizar a impress&atilde;o?","Confirma&ccedil;&atilde;o - Ayllos","imprimirAutorizacao(\''.$nrdrowid.'\',\'1\');","'.$acessaaba.'","sim.gif","nao.gif");';
+ echo 'showError("inform","Para visualizar o termo de ades&atilde;o utilize o bot&atilde;o Imprimir.","Alerta - Ayllos","blockBackground(parseInt($(\'#divRotina\').css(\'z-index\')))");';
+ echo $acessaaba;
 
 	
 	// Função para exibir erros na tela através de javascript
