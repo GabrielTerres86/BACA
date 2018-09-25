d7e144b3c283e5b5ea3a1d03b045a2afb06f42c7
Marcos Martini (Envolti) <envolti-marcos.martini@ailos.coop.br>
2018-09-25 19:33:28 +0000

    P442 - Rollback de versão com problema.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/PRINCIPAL;C25994

--- ayllosdev/telas/gravam/gravam_parametros.js
+++ ayllosdev/telas/gravam/gravam_parametros.js
@@ -9,59 +9,11 @@ $(document).ready(function(){
 		}
 	});
 
-	if($("#hrenvi01","#divFiltroParametros").val() == ''){
-		$("#hrenvi01","#divFiltroParametros").val('--:--');
-	}	else{
-		if($("#hrenvi01","#divFiltroParametros").val().indexOf() == -1){
-		$("#hrenvi01","#divFiltroParametros").val('--:--');	
-		hora = hora.val().substr(0, 2) + ':' + hora.val().substr(2, 2);
-		}
-	}
-	
-	$("#hrenvi01","#divFiltroParametros").unbind('change').bind('change', function (e) {
-		var hora = $(e.currentTarget).val(('0000' + $(e.currentTarget).val()).slice(-4));
-		
-		hora = hora.val().substr(0, 2) + ':' + hora.val().substr(2, 2);
-		$(e.currentTarget).val(hora);
-    });
-	
-	if($("#hrenvi02","#divFiltroParametros").val() == ''){
-		$("#hrenvi02","#divFiltroParametros").val('--:--');
-	} 	else{
-		if($("#hrenvi02","#divFiltroParametros").val().indexOf() == -1){
-		$("#hrenvi02","#divFiltroParametros").val('--:--');	
-		hora = hora.val().substr(0, 2) + ':' + hora.val().substr(2, 2);
-		}
-	}
-	
-	$("#hrenvi02","#divFiltroParametros").unbind('change').bind('change', function (e) {
-		var hora = $(e.currentTarget).val(('0000' + $(e.currentTarget).val()).slice(-4));
-		
-		hora = hora.val().substr(0, 2) + ':' + hora.val().substr(2, 2);
-		$(e.currentTarget).val(hora);
-    });
-	
-	if($("#hrenvi03","#divFiltroParametros").val() == ''){
-		$("#hrenvi03","#divFiltroParametros").val('--:--');
-	}else{
-		if($("#hrenvi03","#divFiltroParametros").val().indexOf() == -1){
-		$("#hrenvi03","#divFiltroParametros").val('--:--');	
-		hora = hora.val().substr(0, 2) + ':' + hora.val().substr(2, 2);
-		}
-	}
-	
-	$("#hrenvi03","#divFiltroParametros").unbind('change').bind('change', function (e) {
-		var hora = $(e.currentTarget).val(('0000' + $(e.currentTarget).val()).slice(-4));
-		
-		hora = hora.val().substr(0, 2) + ':' + hora.val().substr(2, 2);
-		$(e.currentTarget).val(hora);
-    });
-
 	formataFiltroParametros();
 });
 
 function formataFiltroParametros(){
-	// Desabilitar a opção
+	// Desabilitar a opcao
 	$("#cddopcao", "#frmCab").desabilitaCampo();
 	
 	$("#cddopcao", "#frmCab").css("width", "560px");
