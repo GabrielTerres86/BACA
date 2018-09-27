b2ed82448fdf25b2ca8a5637517b558c9071b510
Thaise Medeiros (Envolti) <CECRED\t0031795>
2018-09-27 13:15:14 +0000

    P442 - Teste
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/P442 - Automação de Processos de Crédito/DEV;C26096

---
+++ ayllosdev/telas/gravam/val_permiss.php
@@ -0,0 +1,25 @@
+<?php
+	/*!
+	 * FONTE        : gerar_relatorio_670.php
+	 * CRIAÇÃO      : Andrei - RKAM
+	 * DATA CRIAÇÃO : Maio/2016
+	 * OBJETIVO     : Rotina para gerar relatorio 670 - RELATORIO DE PROCESSAMENTO GRAVAMES
+	 * --------------
+	 * ALTERAÇÕES   : 
+	 * -------------- 
+	 */		
+
+  session_start();
+	require_once('../../includes/config.php');
+	require_once('../../includes/funcoes.php');
+	require_once('../../includes/controla_secao.php');
+	require_once('../../class/xmlfile.php');
+	isPostMethod();			
+	
+	$cddopcao = (isset($_POST["cddopcao"])) ? $_POST["cddopcao"] : '';
+	
+	if (($msgError = validaPermissao($glbvars['nmdatela'],$glbvars['nmrotina'],$cddopcao)) <> '') {
+		exibirErro('error',$msgError,'Alerta - Ayllos','',false);
+	}
+		
+?>
