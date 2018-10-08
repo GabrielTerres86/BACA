85079aaec52c7cc98daab9374c8610429420cde9
Reginaldo Rubens da Silva (AMCOM) <CECRED\t0031667>
2018-10-08 14:38:30 +0000

    // Ajustes nas flagas dos históricos //
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C26426

--- scripts/Sprint12/PJ450 SCRIPT Atualiza TRF CRED PREJ.sql
+++ scripts/Sprint12/PJ450 SCRIPT Atualiza TRF CRED PREJ.sql
@@ -1,3 +1,8 @@
 update craphis set INTRANSF_CRED_PREJUIZO=0
-where cdhistor=2180;
+where cdhistor=2181;
+commit;
+
+-- Histórico de despesas de acordo
+update craphis set INDEBPRJ=1
+where cdhistor=2182;
 commit;
