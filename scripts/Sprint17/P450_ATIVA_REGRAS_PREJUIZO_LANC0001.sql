d96e595c31275c8245ba2793f43190a591dd8e53
Reginaldo Rubens da Silva (AMCOM) <CECRED\t0031667>
2018-10-19 17:41:25 +0000

    // Inclusão de tratamento para o caso do parâmetro não estar cadastrado //
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C26797

--- scripts/Sprint17/P450_ATIVA_REGRAS_PREJUIZO_LANC0001.sql
+++ scripts/Sprint17/P450_ATIVA_REGRAS_PREJUIZO_LANC0001.sql
@@ -1,16 +1,71 @@
-UPDATE crapprm prm
-   set prm.dsvlrprm = 'N'
- WHERE prm.cdcooper = 0
-   AND prm.nmsistem = 'CRED'
-   AND prm.cdacesso = 'IN_ATIVA_REGRAS_PREJU'
-;
+declare 
+  -- Local variables here
+  CURSOR cr_crapprm(pr_cdcooper NUMBER) IS
+	SELECT 1
+	  FROM crapprm prm
+	 WHERE prm.cdcooper = pr_cdcooper
+		 AND prm.nmsistem = 'CRED'
+		 AND prm.cdacesso = 'IN_ATIVA_REGRAS_PREJU'
+	;
+	
+	vr_jacadastrado INTEGER;	
+BEGIN
+	OPEN cr_crapprm(0);
+	FETCH cr_crapprm INTO vr_jacadastrado;
+	
+	IF cr_crapprm%FOUND THEN
+		-- Test statements here
+		UPDATE crapprm prm
+			 SET prm.dsvlrprm = 'N'
+		 WHERE prm.cdcooper = 0
+			 AND prm.nmsistem = 'CRED'
+			 AND prm.cdacesso = 'IN_ATIVA_REGRAS_PREJU'
+		;
+	ELSE
+		INSERT INTO crapprm (
+		    cdcooper
+			, nmsistem
+			, cdacesso
+			, dsvlrprm
+		)
+		VALUES (
+		    0
+			, 'CRED'
+			, 'IN_ATIVA_REGRAS_PREJU'
+			, 'N'
+		);
+	END IF;
+	
+	CLOSE cr_crapprm;
 
-UPDATE crapprm prm
-   set prm.dsvlrprm = 'S'
- WHERE prm.cdcooper = 11 
-   AND prm.nmsistem = 'CRED'
-   AND prm.cdacesso = 'IN_ATIVA_REGRAS_PREJU'
-;
+	OPEN cr_crapprm(11);
+	FETCH cr_crapprm INTO vr_jacadastrado;
+	
+	IF cr_crapprm%FOUND THEN
+		-- Test statements here
+		UPDATE crapprm prm
+			 SET prm.dsvlrprm = 'S'
+		 WHERE prm.cdcooper = 0
+			 AND prm.nmsistem = 'CRED'
+			 AND prm.cdacesso = 'IN_ATIVA_REGRAS_PREJU'
+		;
+	ELSE
+		INSERT INTO crapprm (
+		    cdcooper
+			, nmsistem
+			, cdacesso
+			, dsvlrprm
+		)
+		VALUES (
+		    0
+			, 'CRED'
+			, 'IN_ATIVA_REGRAS_PREJU'
+			, 'S'
+		);
+	END IF;
+	
+	CLOSE cr_crapprm;
 
-COMMIT;
+	COMMIT;
+end;
 
