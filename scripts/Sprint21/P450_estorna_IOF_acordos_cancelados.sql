4da05be7f1069eb304af68fc60a3e87422f716b7
Reginaldo Rubens da Silva (AMCOM) <CECRED\t0031667>
2018-10-29 19:08:11 +0000

    // Baca de estorno do IOF de acordos cancelados //
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Projeto Ligeirinho - Contratação de Crédito/DESEN;C27164

---
+++ scripts/Sprint21/P450_estorna_IOF_acordos_cancelados.sql
@@ -0,0 +1,35 @@
+rem PL/SQL Developer Test Script
+
+set feedback off
+set autoprint off
+
+rem Execute PL/SQL Block
+declare 
+  CURSOR cr_acordos IS
+	SELECT aco.cdcooper
+	     , aco.nrdconta
+			 , aco.nracordo
+			 , ctr.vliofdev - ctr.vliofpag iof_pendente
+	  FROM tbrecup_acordo aco
+		   , tbrecup_acordo_contrato ctr
+	 WHERE aco.cdsituacao = 3
+	   AND aco.dtcancela >= to_date('26/10/2018', 'DD/MM/YYYY')
+		 AND ctr.nracordo = aco.nracordo
+		 AND ctr.cdorigem = 1
+		 AND nvl(ctr.vliofdev, 0) - nvl(ctr.vliofpag, 0) > 0;
+	rw_acordos cr_acordos%ROWTYPE;
+begin
+  FOR rw_acordos IN cr_acordos LOOP
+		UPDATE crapsld
+		   SET vliofmes = nvl(vliofmes, 0) + rw_acordos.iof_pendente
+		 WHERE cdcooper = rw_acordos.cdcooper
+		   AND nrdconta = rw_acordos.nrdconta;
+			 
+		UPDATE tbgen_iof_lancamento
+		   SET nracordo = NULL
+		 WHERE nracordo = rw_acordos.nracordo;
+	END LOOP;  
+	
+	COMMIT;
+end;
+/
