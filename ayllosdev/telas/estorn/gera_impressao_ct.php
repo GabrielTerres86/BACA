60f018c8c6bc701c860c900d323139d5d075f0df
Odirlei Busana (AMCOM) <amcom-odirlei.busana@ailos.coop.br>
2018-11-07 13:55:10 +0000

    PRJ450 - Tela Estorn
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/PRINCIPAL;C27486

---
+++ ayllosdev/telas/estorn/gera_impressao_ct.php
@@ -0,0 +1,56 @@
+<?php 
+	/**************************************************************************
+	  Fonte: gera_impressao_ct.php
+	  Autor: Diego Simas (AMcom)
+	  Data : Setembro/2018                   Última Alteração:
+
+	  Objetivo  : Gera o relatorio de estorno de pagamento de prejuizo de conta corrente.
+
+	  Alterações:  
+	**************************************************************************/
+	session_cache_limiter("private");
+	session_start();
+	// Includes para controle da session, variáveis globais de controle, e biblioteca de funções	
+	require_once('../../includes/config.php');
+	require_once('../../includes/funcoes.php');
+	require_once('../../includes/controla_secao.php');
+	require_once('../../class/xmlfile.php');
+	// Verifica se tela foi chamada pelo método POST
+	isPostMethod();
+
+	if (($msgError = validaPermissao($glbvars["nmdatela"], $glbvars["nmrotina"], "RCT")) <> "") {		
+		exibeErro($msgError);
+	}
+
+	$nrdconta = str_ireplace(array('-','.'),'',$_POST["nrdconta"]);
+	$dtiniest = $_POST['dtiniest'];
+	$dtfinest = $_POST['dtfinest'];
+
+	$xml  = '<Root>';
+	$xml .= '	<Dados>';
+	$xml .= '		<nrdconta>'.$nrdconta.'</nrdconta>';
+	$xml .= '		<dtmvtolt>'.$glbvars['dtmvtolt'].'</dtmvtolt>';
+	$xml .= '		<dtiniest>'.$dtiniest.'</dtiniest>';
+	$xml .= '		<dtfinest>'.$dtfinest.'</dtfinest>';
+	$xml .= '	</Dados>';
+	$xml .= '</Root>';
+	
+	// Executa script para envio do XML
+	$xmlResult = mensageria($xml, "PREJ0003", "ESTORNO_RELATORIO_PREJUIZO", $glbvars["cdcooper"], $glbvars["cdagenci"], $glbvars["nrdcaixa"], $glbvars["idorigem"], $glbvars["cdoperad"], "</Root>");
+	// Cria objeto para classe de tratamento de XML
+	$xmlObj = simplexml_load_string($xmlResult);
+	// Se ocorrer um erro, mostra crítica
+	if ($xmlObj->Erro->Registro->dscritic != ''){
+		exibeErro($xmlObj->Erro->Registro->dscritic);
+	}
+	// Obtém nome do arquivo PDF 
+	$nmarqpdf = $xmlObj;
+	// Chama função para mostrar PDF do impresso gerado no browser
+	visualizaPDF($nmarqpdf);	
+	// Função para exibir erros na tela através de javascript
+    function exibeErro($msgErro) { 
+	  echo '<script>alert("'.$msgErro.'");</script>';	
+	  exit();
+    }
+
+?>
