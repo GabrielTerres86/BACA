f5cc2a1a8348511a0b678d53518ce083dfe41c4d
Augusto Henrique da Conceicao (Supero) <augusto.conceicao@supero.com.br>
2018-11-12 17:51:01 +0000

    Valor padrão para "Informações cadastrais" e "Patrimônio pessoal livre em relação ao endividamento" para casos de conta salário.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/P485 - Portabilidade de Salário/DEV;C27664

--- ayllosdev/telas/contas/inf_adicionais/principal.php
+++ ayllosdev/telas/contas/inf_adicionais/principal.php
@@ -1,16 +1,16 @@
 <?php
 /*!
  * FONTE        : principal.php
- * CRIAÇÃO      : Rodolpho Telmo (DB1)
- * DATA CRIAÇÃO : Abril/2010 
+ * CRIAï¿½ï¿½O      : Rodolpho Telmo (DB1)
+ * DATA CRIAï¿½ï¿½O : Abril/2010 
  * OBJETIVO     : Mostrar opcao Principal da rotina de Inf. Adicionais da tela de CONTAS
  *
  * ALTERACOES   : 20/12/2010 - Adicionado chamada validaPermissao (Gabriel - DB1). 
  *                06/08/2015 - Reformulacao Cadastral (Gabriel-RKAM)
  *                14/07/2016 - Correcao no tratamento da msg de retorno XML. SD 479874. Carlos R.
  *
- *                01/12/2016 - P341-Automatização BACENJUD - Removido passagem do departamento como parametros
- *                             pois a BO não utiliza o mesmo (Renato Darosci)
+ *                01/12/2016 - P341-Automatizaï¿½ï¿½o BACENJUD - Removido passagem do departamento como parametros
+ *                             pois a BO nï¿½o utiliza o mesmo (Renato Darosci)
  */	
 	session_start();
 	require_once('../../../includes/config.php');
@@ -31,34 +31,34 @@
 		default  : $op = "@"; break;
 	}
 	
-	// Verifica permissões de acessa a tela
+	// Verifica permissï¿½es de acessa a tela
 	if (($msgError = validaPermissao($glbvars['nmdatela'],$glbvars['nmrotina'],$op,false)) <> '') {
 		$metodo =  ($flgcadas == 'M') ? 'proximaRotina();' : 'encerraRotina(false);';
 		exibirErro('error',$msgError,'Alerta - Ayllos',$metodo,false);
 	}
 	
-	// Verifica se o número da conta foi informado
+	// Verifica se o nï¿½mero da conta foi informado
 	if (!isset($_POST['nrdconta']) || !isset($_POST['idseqttl'])) exibirErro('error','Par&acirc;metros incorretos.','Alerta - Ayllos','bloqueiaFundo(divRotina)',false);
 
-	// Carrega permissões do operador
+	// Carrega permissï¿½es do operador
 	include("../../../includes/carrega_permissoes.php");	
 	
 	setVarSession("opcoesTela",$opcoesTela);
 
 	$qtOpcoesTela = count($opcoesTela);
 
-	// Carregas as opções da Rotina de Bens
+	// Carregas as opï¿½ï¿½es da Rotina de Bens
 	$flgAlterar  = (in_array("A", $glbvars["opcoesTela"]));
 	
 	$nrdconta = $_POST['nrdconta'] == '' ? 0 : $_POST['nrdconta'];
 	$idseqttl = $_POST['idseqttl'] == '' ? 0 : $_POST['idseqttl'];
 	
-	// Verifica se o número da conta e o titular são inteiros válidos
+	// Verifica se o nï¿½mero da conta e o titular sï¿½o inteiros vï¿½lidos
 	if (!validaInteiro($nrdconta)) exibirErro('error','Conta/dv inv&aacute;lida.','Alerta - Ayllos','bloqueiaFundo(divRotina)',false);
 	if (!validaInteiro($idseqttl)) exibirErro('error','Seq. Titular n&atilde;o foi informada.','Alerta - Ayllos','bloqueiaFundo(divRotina)',false);	
 	if ($idseqttl==0) exibirErro('error','Seq. Titular n&atilde;o foi informada.','Alerta - Ayllos','bloqueiaFundo(divRotina)',false);
 	
-	// Monta o xml de requisição
+	// Monta o xml de requisiï¿½ï¿½o
 	$xml  = '';
 	$xml .= '<Root>';
 	$xml .= '	<Cabecalho>';
@@ -83,14 +83,47 @@
 	// Cria objeto para classe de tratamento de XML
 	$xmlObjFiliacao = getObjectXML($xmlResult);
 	
-	// Se ocorrer um erro, mostra crítica
+	// Se ocorrer um erro, mostra crï¿½tica
 	if (strtoupper($xmlObjFiliacao->roottag->tags[0]->name) == 'ERRO') exibirErro('error',$xmlObjeto->roottag->tags[0]->tags[0]->tags[4]->cdata,'Alerta - Ayllos','bloqueiaFundo(divRotina);',false);
 	
 	$infAdicionais = $xmlObjFiliacao->roottag->tags[0]->tags[0]->tags;
+
+
+	//------------
+	$xml = "<Root>";
+	$xml .= " <Dados>";
+	$xml .= "   <cdcooper>".$glbvars['cdcooper']."</cdcooper>";
+	$xml .= "   <nrdconta>".$nrdconta."</nrdconta>";
+	$xml .= " </Dados>";
+	$xml .= "</Root>";
+
+	$xmlResultModalidade = mensageria($xml, "CADA0006", "BUSCA_MODALIDADE", $glbvars["cdcooper"], $glbvars["cdagenci"], $glbvars["nrdcaixa"], $glbvars["idorigem"], $glbvars["cdoperad"], "</Root>");
+	$xmlObjModalidade = getObjectXML($xmlResultModalidade);
+
+	if (strtoupper($xmlObjModalidade->roottag->tags[0]->name) == "ERRO") {
+		$msgErro = $xmlObjModalidade->roottag->tags[0]->tags[0]->tags[4]->cdata;
+		if ($msgErro == "") {
+			$msgErro = $xmlObjModalidade->roottag->tags[0]->cdata;
+		}
+		exibeErro($msgErro);
+	}
+	$modalidade = $xmlObjModalidade->roottag->tags[0]->cdata;
+	//------------
+	$nrinfcad = 0;
+	$nrpatlvr = 0;
+	if (!empty($infAdicionais)) {
+		$nrinfcad = getByTagName($infAdicionais,'nrinfcad');
+		$nrpatlvr = getByTagName($infAdicionais,'nrpatlvr');
+	}
+	// Se for conta salÃ¡rio e for inclusÃ£o
+	if ($flgcadas == 'M' && $modalidade == 2) {
+		$nrinfcad = 1;
+		$nrpatlvr = 1;
+	}
 	
 	$textareaTags = $xmlObjFiliacao->roottag->tags[0]->tags[0]->tags[3]->tags;
 	
-	// Se não retornou erro, então pegar a mensagem de alerta do Progress na variável msgAlert, para ser utilizada posteriormente
+	// Se nï¿½o retornou erro, entï¿½o pegar a mensagem de alerta do Progress na variï¿½vel msgAlert, para ser utilizada posteriormente
 	$msgAlert = ( isset($xmlObjeto->roottag->tags[0]->attributes['MSGALERT']) ) ? trim($xmlObjeto->roottag->tags[0]->attributes['MSGALERT']) : '';	
 	
 	include('formulario_inf_adicionais.php');
