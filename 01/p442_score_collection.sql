eeb004e5e4d286b7a94ea28487b063450d55fc82
Marcos Martini (Envolti) <envolti-marcos.martini@ailos.coop.br>
2018-12-11 12:03:41 +0000

    P442 - Behaviour - Ajustes para liberação de Janeiro.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C28664

--- 01/p442_score_collection.sql
+++ 01/p442_score_collection.sql
@@ -5,18 +5,21 @@ BEGIN
    WHERE prm.cdacesso = 'DBLINK_SAS_DESEN';
   UPDATE crapprm prm 
      SET prm.cdacesso = 'DBLINK_SAS_PROD_RO' 
+        ,prm.dsvlrprm = 'SASRO'
    WHERE prm.cdacesso = 'DBLINK_SAS_PROD';
   
   insert into crapprm (nmsistem,cdcooper,cdacesso,dstexprm,dsvlrprm)
                VALUES ('CRED',0,'DBLINK_SAS_DESEN_RW','DBLink para acesso escrita ao SAS em ambientes não produção','sasd1');  
   insert into crapprm (nmsistem,cdcooper,cdacesso,dstexprm,dsvlrprm)
-               VALUES ('CRED',0,'DBLINK_SAS_PROD_RW','DBLink para acesso escrita ao SAS em ambiente produção','sasp');  
+               VALUES ('CRED',0,'DBLINK_SAS_PROD_RW','DBLink para acesso escrita ao SAS em ambiente produção','SASRW');  
 
   insert into crapprm (nmsistem,cdcooper,cdacesso,dstexprm,dsvlrprm)
                VALUES ('CRED',0,'OWNER_SAS_DESEN','Owner para acesso ao SAS em ambientes não produção','');  
   insert into crapprm (nmsistem,cdcooper,cdacesso,dstexprm,dsvlrprm)
                VALUES ('CRED',0,'OWNER_SAS_PROD','Owner para acesso ao SAS em ambiente produção','integradados.');  
 
-  commit;
+  insert into crapprm (nmsistem,cdcooper,cdacesso,dstexprm,dsvlrprm)
+               VALUES ('CRED',0,'SKPROC_SCORE_BEHAVIOUR','SK de carga do Score Behaviour','856');  
+  commit;  
   
 end;
