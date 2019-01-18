0bfed0a49af5d2963b6d5fe3dd620bee13426a4e
Anderson Fossa <anderson.fossa@ailos.coop.br>
2019-01-18 17:46:39 +0000

    Desvincula aplicação com problema no registro da B3.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C29650

--- 01/P411-registra-aplicacao-b3.sql
+++ 01/P411-registra-aplicacao-b3.sql
@@ -1,9 +1,6 @@
 
 
--- Carrega o dscodigo_b3 = 'RDB0181FG1M' e atualiza as cotas, já que já foi resgatado manualmente na B3.
-update tbcapt_custodia_aplicacao set dscodigo_b3 = 'RDB0181FG1M', qtcotas = 1046 where idaplicacao = 626050;
-
--- Originalmente dscodigo_b3 = 'RDB0181J17R' - Resgatado total
-update tbcapt_custodia_aplicacao set dscodigo_b3 = null where idaplicacao = 666940;
+-- antes era RDB0180XXNC
+update tbcapt_custodia_aplicacao set dscodigo_b3 = null where idaplicacao = 391124;
 
 commit;
