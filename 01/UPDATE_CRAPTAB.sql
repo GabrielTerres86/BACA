02d33ff154a0a7322d33817b50cc157579f8bf6b
Lucas Reinert <lucas.reinert@ailos.coop.br>
2019-01-24 11:32:18 +0000

    P470 - Ajustes
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C29811

--- 01/UPDATE_CRAPTAB.sql
+++ 01/UPDATE_CRAPTAB.sql
@@ -1,18 +1,18 @@
 BEGIN
   UPDATE craptab
      SET dstextab = '10000,00;10000,00'
-   WHERE nmsistem = 'CRED'
-     AND tptabela = 'GENERI'
+   WHERE upper(nmsistem) = 'CRED'
+     AND upper(tptabela) = 'GENERI'
      AND cdempres = 0
-     AND cdacesso = 'VLMICFEMPR'
+     AND upper(cdacesso) = 'VLMICFEMPR'
      AND tpregist = 0;
   --
   UPDATE craptab
      SET dstextab = '0,00'
-   WHERE nmsistem = 'CRED'
-     AND tptabela = 'GENERI'
+   WHERE upper(nmsistem) = 'CRED'
+     AND upper(tptabela) = 'GENERI'
      AND cdempres = 0
-     AND cdacesso = 'VLMICFLIMI'
+     AND upper(cdacesso) = 'VLMICFLIMI'
      AND tpregist = 0;
   COMMIT;
 END;
