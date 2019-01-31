c979c8d6791a68d9cfcf3b4a07ede98a6451e371
Luis Fernando Sacchi Ribeiro (GFT) <CECRED\t0031723>
2019-01-31 21:06:03 +0000

    403 - Merge com PROD
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/EMERGENCIAL;C30187

---
+++ ayllosdev/telas/atenda/descontos/titulos/rotinas_analise.php
@@ -0,0 +1,116 @@
+<?php 
+
+	/************************************************************************
+	 Fonte: rotinas_analise.php                                        
+	 Autor: Luis Fernando (GFT)
+	 Data : 24/01/2019                Última Alteração: 
+	                                                                  
+	 Objetivo  : Agrupa as rotinas de analise 
+
+	************************************************************************/
+	
+	session_start();
+	
+	// Includes para controle da session, variáveis globais de controle, e biblioteca de funções	
+	require_once("../../../../includes/config.php");
+	require_once("../../../../includes/funcoes.php");
+	require_once("../../../../includes/controla_secao.php");
+
+
+	// Verifica se tela foi chamada pelo método POST
+	isPostMethod();	
+		
+	// Classe para leitura do xml de retorno
+	require_once("../../../../class/xmlfile.php");
+	
+	setVarSession("nmrotina","DSC TITS - BORDERO");
+
+	
+	function erroJson($mensagem){
+		$json = array();
+		$json["status"] = 'erro';
+		$json["mensagem"] = $mensagem;
+		echo json_encode($json);
+		exit();
+	}
+
+	// Verifica se o número da conta foi informado
+	if (!isset($_POST["nrdconta"])) {
+		erroJson("Par&acirc;metros incorretos.");
+	}	
+
+	$nrdconta = $_POST["nrdconta"];
+	$nrctrlim = $_POST["nrctrlim"];
+	$rotina   = $_POST["rotina"];
+	// Verifica se o número da conta é um inteiro válido
+	if (!validaInteiro($nrdconta)) {
+		erroJson("Conta/dv inv&aacute;lida.");
+	}
+	// Verifica se o número da conta é um inteiro válido
+	if (!validaInteiro($nrctrlim)) {
+		erroJson("Contrato inv&aacute;lido.");
+	}
+	
+	$selecionados = isset($_POST["selecionados"]) ? $_POST["selecionados"] : array();
+	if(count($selecionados)==0){
+		erroJson("Selecione ao menos um t&iacute;tulo");
+	}
+	$selecionados = implode($selecionados,",");
+
+	switch($rotina){
+		case "biro":
+			$xml = "<Root>";
+		    $xml .= " <Dados>";
+		    $xml .= "   <nrdconta>".$nrdconta."</nrdconta>";
+		    $xml .= "   <nrctrlim>".$nrctrlim."</nrctrlim>";
+		    $xml .= "   <chave>".$selecionados."</chave>";
+		    $xml .= " </Dados>";
+		    $xml .= "</Root>";
+		    
+		 	// CONSULTA DA IBRATAN	
+		    $xmlResult = mensageria($xml,"TELA_ATENDA_DESCTO","SOLICITA_BIRO_BORDERO", $glbvars["cdcooper"], $glbvars["cdagenci"], $glbvars["nrdcaixa"], $glbvars["idorigem"], $glbvars["cdoperad"], "</Root>");
+		    $xmlObj = getClassXML($xmlResult);
+		    $root = $xmlObj->roottag;
+			if ($root->erro){
+				// erroJson(htmlentities($root->erro->registro->dscritic));
+			}
+		break;
+		case "analise_pagadores":
+			$xml = "<Root>";
+		    $xml .= " <Dados>";
+		    $xml .= "   <nrdconta>".$nrdconta."</nrdconta>";
+		    $xml .= "   <nrctrlim>".$nrctrlim."</nrctrlim>";
+		    $xml .= "   <chave>".$selecionados."</chave>";
+		    $xml .= " </Dados>";
+		    $xml .= "</Root>";
+		    
+		 	// CONSULTA DA IBRATAN	
+		    $xmlResult = mensageria($xml,"TELA_ATENDA_DESCTO","SOLICITA_ANALISE_PAGADOR", $glbvars["cdcooper"], $glbvars["cdagenci"], $glbvars["nrdcaixa"], $glbvars["idorigem"], $glbvars["cdoperad"], "</Root>");
+		    $xmlObj = getClassXML($xmlResult);
+		    $root = $xmlObj->roottag;
+			if ($root->erro){
+				erroJson(htmlentities($root->erro->registro->dscritic));
+			}
+		break;
+		case "valida_alteracao":
+			// FAZ VALIDAÇÃO DOS TITULOS SE BATEM COM AS REGRAS DA TAB052 DE VALOR E PRAZO
+			$xml = "<Root>";
+		    $xml .= " <Dados>";
+		    $xml .= "   <nrdconta>".$nrdconta."</nrdconta>";
+		    $xml .= "   <chave>".$selecionados."</chave>";
+			$xml .= "	<dtmvtolt>".$glbvars["dtmvtolt"]."</dtmvtolt>";
+		    $xml .= " </Dados>";
+		    $xml .= "</Root>";
+
+		    $xmlResult = mensageria($xml,"TELA_ATENDA_DESCTO","VALIDAR_TITULOS_ALTERACAO", $glbvars["cdcooper"], $glbvars["cdagenci"], $glbvars["nrdcaixa"], $glbvars["idorigem"], $glbvars["cdoperad"], "</Root>");
+		    $xmlObj = getClassXML($xmlResult);
+		    $root = $xmlObj->roottag;
+			if ($root->erro){
+				erroJson(htmlentities($root->erro->registro->dscritic));
+			}
+		break;
+	}
+
+	$json = array();
+	$json['status'] = 'sucesso';
+	echo json_encode($json);
---
+++ ayllosdev/telas/atenda/descontos/titulos/titulos_renova_limite.php
@@ -0,0 +1,82 @@
+<?php 
+/***************************************************************************************
+ * FONTE        : titulos_renova_limite.php				Última alteração: --/--/----
+ * CRIAÇÃO      : Leonardo
+ * DATA CRIAÇÃO : 13/03/2018
+ * OBJETIVO     : Efetuar renovação Manual de Limites.
+ *
+ * Alterações   : 
+ *
+ * 001: [16/03/2018] Leonardo Oliveira (GFT): Considerar o parâmetro 'cddlinha' na operação.
+ * 002: [25/04/2018] Leonardo Oliveira (GFT): Correção da mensagem de erro.
+ *
+ **************************************************************************************/
+
+	session_start();
+		
+	require_once('../../../../includes/config.php');
+	require_once('../../../../includes/funcoes.php');
+	require_once('../../../../class/xmlfile.php');
+	
+
+	setVarSession("nmrotina","DSC TITS - LIMITE");
+	
+	require_once("../../../../includes/carrega_permissoes.php");
+	
+
+	if (($msgError = validaPermissao($glbvars["nmdatela"],$glbvars["nmrotina"],"V")) <> "") {
+		exibirErro('error',htmlentities($msgError),'Alerta - Aimaro',"blockBackground(parseInt($('#divUsoGenerico').css('z-index')))",false);
+	}
+	
+	isPostMethod();		
+	  
+	$nrdconta = (isset($_POST['nrdconta'])) ? $_POST['nrdconta'] : 0;
+	$vllimite = (isset($_POST['vllimite'])) ? $_POST['vllimite'] : 0;
+	$nrctrlim = (isset($_POST['nrctrlim'])) ? $_POST['nrctrlim'] : 0;
+	$cddlinha = (isset($_POST['cddlinha'])) ? $_POST['cddlinha'] : 0;
+
+    $xmlRenovaLimite  = "";
+	$xmlRenovaLimite .= "<Root>";
+	$xmlRenovaLimite .= "   <Dados>";
+	$xmlRenovaLimite .= "	   <nrdconta>".$nrdconta."</nrdconta>";
+	$xmlRenovaLimite .= "	   <idseqttl>1</idseqttl>";
+	$xmlRenovaLimite .= "	   <vllimite>".$vllimite."</vllimite>";
+	$xmlRenovaLimite .= "	   <nrctrlim>".$nrctrlim."</nrctrlim>";
+	$xmlRenovaLimite .= "	   <cddlinha>".$cddlinha."</cddlinha>";
+	$xmlRenovaLimite .= "   </Dados>";
+	$xmlRenovaLimite .= "</Root>";
+  
+	// Executa script para envio do XML	
+	$xmlResult = mensageria(
+		$xmlRenovaLimite, 
+		"TELA_ATENDA_DESCTO", 
+		"RENOVA_LIMITE_TIT", 
+		$glbvars["cdcooper"], 
+		$glbvars["cdagenci"], 
+		$glbvars["nrdcaixa"], 
+		$glbvars["idorigem"], 
+		$glbvars["cdoperad"], 
+		"</Root>");
+	
+	$xmlObjRenovaLimite = getObjectXML($xmlResult);
+	
+	// Se ocorrer um erro, mostra crítica
+	if (strtoupper($xmlObjRenovaLimite->roottag->tags[0]->name) == "ERRO") {
+		$msgErro = $xmlObjRenovaLimite->roottag->tags[0]->tags[0]->tags[4]->cdata;
+		if ($msgErro == "") {
+			$msgErro = $xmlObjRenovaLimite->roottag->tags[0]->cdata;
+		}
+		exibirErro(
+			"error",
+			htmlentities($msgErro),
+			"Alerta - Aimaro",
+			"fecharRotinaGenerico('TITULOS');",
+			false);
+	}
+	
+	echo "showError(
+			\"inform\",
+			\"Opera&ccedil;&atilde;o efetuada com sucesso!\",
+			\"Alerta - Aimaro\",
+			\"fecharRotinaGenerico('TITULOS');\");";	
+?>
