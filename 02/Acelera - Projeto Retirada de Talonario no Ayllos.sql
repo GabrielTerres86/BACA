755da8bccbc6e786933dca9423ba96f998d951b3
Lucas Afonso Lombardi Moreira <lucas.moreira@ailos.coop.br>
2019-02-01 16:57:35 +0000

    Inserido commit.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C30234

--- 02/Acelera - Projeto Retirada de Talonario no Ayllos.sql
+++ 02/Acelera - Projeto Retirada de Talonario no Ayllos.sql
@@ -5,3 +5,5 @@ UPDATE crapaca
 UPDATE crapaca
    SET lstparam = 'pr_nrdconta,pr_tprequis,pr_nrinichq,pr_nrfinchq,pr_terceiro,pr_cpfterce,pr_nmtercei,pr_nrtaloes,pr_qtreqtal,pr_verifica'
  WHERE nmdeacao = 'CHQ_ENTREGA_TALONARIO';
+
+COMMIT;
