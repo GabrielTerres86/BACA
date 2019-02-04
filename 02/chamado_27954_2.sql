60e7d23963c7b3dddbf90301f2668962bf5fe2d5
Douglas Alessandro Pagel (AMCOM) <douglas.pagel@amcom.com.br>
2019-02-04 16:11:18 +0000

    INC0027954 - Excluir registros do arquivo BB - cheques da conta ITG
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C30295

---
+++ 02/chamado_27954_2.sql
@@ -0,0 +1,13 @@
+--- chamado INC0027954 - Arq C00406 - Excluir registros do arquivo BB - cheques da conta ITG
+--- Ação: Alterar Flag para = 2- Processado, de registros de cheques já compensados e que ao serem recusados pelo banco, 
+----      sempre são reenviados pelos sistema.
+
+
+update crapcch 
+set    crapcch.flgctitg = 2
+WHERE crapcch.cdcooper = 1      
+  and crapcch.nrdconta = 1885839  
+  and crapcch.rowid = 'AAAS+DAKvAABF1CAAU';
+
+Commit;
+
