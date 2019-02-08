18b4f0fde9830550f8973ef69121476134b74f5c
Marcelo Coelho (Mouts) <mouts-marcelo.coelho@ailos.coop.br>
2019-02-08 12:03:53 +0000

    projeto 470
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C30435

--- 01/INSERT_RDR_ACA_CNTR0001.sql
+++ 01/INSERT_RDR_ACA_CNTR0001.sql
@@ -2,7 +2,7 @@ INSERT INTO craprdr (nmprogra, dtsolici)
               VALUES('CNTR0001',SYSDATE);
 
 INSERT INTO crapaca (nmdeacao,nmpackag,nmproced,lstparam,nrseqrdr)
-              VALUES('GERA_PROTOCOLO_CTD_PF','CNTR0001','PC_GERA_PROTOCOLO_CTD_PF','pr_nrdconta,pr_cdtippro,pr_nrcontrato,pr_vlcontrato',
+              VALUES('GERA_PROTOCOLO_CTD_PF','CNTR0001','PC_GERA_PROTOCOLO_CTD_PF','pr_nrdconta,pr_cdtippro,pr_nrcontrato,pr_vlcontrato,pr_dscomplemento',
                      (SELECT r.nrseqrdr FROM craprdr r WHERE r.nmprogra = 'CNTR0001'));
 
 INSERT INTO crapaca (nmdeacao,nmpackag,nmproced,lstparam,nrseqrdr)
@@ -10,7 +10,7 @@ INSERT INTO crapaca (nmdeacao,nmpackag,nmproced,lstparam,nrseqrdr)
                      (SELECT r.nrseqrdr FROM craprdr r WHERE r.nmprogra = 'CNTR0001'));
 
 INSERT INTO crapaca (nmdeacao,nmpackag,nmproced,lstparam,nrseqrdr)
-              VALUES('GERA_PROTOCOLO_CTD_PJ','CNTR0001','PC_GERA_PROTOCOLO_CTD_PJ','pr_nrdconta,pr_cdtippro,pr_dhcontrato,pr_nrcontrato,pr_vlcontrato,pr_cdtransacao_pendente,pr_contas_digitadas,pc_cdcooper_tel
+              VALUES('GERA_PROTOCOLO_CTD_PJ','CNTR0001','PC_GERA_PROTOCOLO_CTD_PJ','pr_nrdconta,pr_cdtippro,pr_dhcontrato,pr_nrcontrato,pr_vlcontrato,pr_cdtransacao_pendente,pr_contas_digitadas,pc_cdcooper_tel,pr_dscomplemento',
                      (SELECT r.nrseqrdr FROM craprdr r WHERE r.nmprogra = 'CNTR0001'));
 COMMIT;
 
