3f24596ecae0c034e10ea6403991ee56374ecf3f
Heitor Schmitt (Mouts) <mouts-heitor.schmitt@ailos.coop.br>
2019-02-12 11:33:51 +0000

    Correcao no update
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C30556

--- 02/INC0031113_ajuste_crapcob.sql
+++ 02/INC0031113_ajuste_crapcob.sql
@@ -1,6 +1,6 @@
 BEGIN
   UPDATE crapcob c
-     SET c.dsdoccop = REPLACE(c.dsdoccop,'á','A')
+     SET c.dsdoccop = DECODE(c.rowid,'AAAS/0ABcAAA2unAAe','GAS AP 203/0001','GAS AP 202/0001')
    WHERE c.rowid IN ('AAAS/0ABcAAA2unAAe','AAAS/0ABcAABlhDAAl');
   
   COMMIT;
