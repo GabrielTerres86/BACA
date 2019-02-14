660ce6bf3dcef45e7a728faa57a3d8e343a09dd8
Kelvin Souza Ott <kelvin.ott@ailos.coop.br>
2019-02-14 17:21:47 +0000

    Script para criação de um PRM para o projeto XSLPROCESSOR.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C30712

---
+++ 02/Script_CRAPPRM.sql
@@ -0,0 +1,5 @@
+begin
+  insert into crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  values ('CRED',0,'XSLPROCESSOR','Diretorio temporario no servidor Linux','DIR_TMP_FILE');
+  commit;
+end;
