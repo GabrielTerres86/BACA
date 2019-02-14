8657bc14dcfd5888d0900e4760c3cdcc5a7dfc38
Jaison Fernando Ferreira <jaison.ferreira@ailos.coop.br>
2019-02-14 14:20:56 +0000

    P403 - Alteração do limite ativo
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C30699

--- 02/P403_Ajustar_Contrato_Bordero_Cancelado.sql
+++ 02/P403_Ajustar_Contrato_Bordero_Cancelado.sql
@@ -1,8 +1,15 @@
 UPDATE crapbdt bdt
-   SET bdt.nrctrlim = 230
- WHERE bdt.cdcooper = 5
-   AND bdt.nrdconta = 121525
-   AND bdt.nrborder = 13718
-   AND bdt.nrctrlim = 64;
+   SET bdt.nrctrlim = 8544
+ WHERE bdt.cdcooper = 1
+   AND bdt.nrdconta = 10179283
+   AND bdt.nrborder = 551203
+   AND bdt.nrctrlim = 7938;
+
+UPDATE crapbdt bdt
+   SET bdt.nrctrlim = 8545
+ WHERE bdt.cdcooper = 1
+   AND bdt.nrdconta = 7414200
+   AND bdt.nrborder = 551474
+   AND bdt.nrctrlim = 3295;
 
 COMMIT;
