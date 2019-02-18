ef4e283e0ac078db9dfc522d673cbc0e7e1eccce
Evandro (MOUTS) <mouts-evandro.garanha@ailos.coop.br>
2019-02-18 22:37:57 +0000

    Corrigido código da finalidade 400 para 157 - INC0032530.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C30823

---
+++ 02/INC0032530.sql
@@ -0,0 +1,47 @@
+/*
+
+	Se for necessário efetuar o rollback, usar os scripts abaixo:
+		
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862498;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862483;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862484;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862499;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862485;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862486;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862487;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862488;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862496;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862493;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862494;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862495;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862497;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862489;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862490;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862491;
+    update craptab a    set a.tpregist = 400  where a.progress_recid = 3862492;
+
+
+*/
+
+--Atualiza com o código da finalidade correto 
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862498;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862483;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862484;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862499;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862485;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862486;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862487;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862488;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862496;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862493;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862494;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862495;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862497;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862489;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862490;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862491;
+update craptab a    set a.tpregist = 157  where a.progress_recid = 3862492;
+
+COMMIT;
+
+
