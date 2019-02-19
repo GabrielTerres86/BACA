a7756c9f8291643bae09b9bda210fe58c225ffc1
Otavio Theiss <otavio.theiss@ailos.coop.br>
2019-02-19 17:14:48 +0000

    Acript de atualização de cartões provisórios no sistema,
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C30848

---
+++ 02/P432_ajuste_cartao_provisorio_legado.sql
@@ -0,0 +1,92 @@
+BEGIN
+	 
+
+-----------#################################################
+
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 80355706 and a.nrcrcard = 5127077300604258 and a.cdcooper = 1;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 488526 and a.nrcrcard = 5127077300880361 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 486493 and a.nrcrcard = 5127077300876153 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 448320 and a.nrcrcard = 5127077300880254 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 477753 and a.nrcrcard = 5127077300880171 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 487406 and a.nrcrcard = 5127077300880304 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 459356 and a.nrcrcard = 5127077300880213 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 308919 and a.nrcrcard = 5127077300880288 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 477095 and a.nrcrcard = 5127077300880205 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 491420 and a.nrcrcard = 5127077300880551 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 486612 and a.nrcrcard = 5127077300880338 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 442526 and a.nrcrcard = 5127077300880163 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 7145055 and a.nrcrcard = 5127077300604324 and a.cdcooper = 1;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 7579993 and a.nrcrcard = 5127077300604274 and a.cdcooper = 1;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 9544259 and a.nrcrcard = 5127077300604266 and a.cdcooper = 1;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 859370 and a.nrcrcard = 6042037300033494 and a.cdcooper = 1;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 489816 and a.nrcrcard = 5127077300880445 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 478199 and a.nrcrcard = 5127077300880353 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 487414 and a.nrcrcard = 5127077300880296 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 488160 and a.nrcrcard = 5127077300880494 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 488739 and a.nrcrcard = 5127077300880346 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 395714 and a.nrcrcard = 5127077300880262 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 859370 and a.nrcrcard = 6042037300033502 and a.cdcooper = 1;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 6175694 and a.nrcrcard = 5127077300604340 and a.cdcooper = 1;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 482927 and a.nrcrcard = 5127077300880502 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 475270 and a.nrcrcard = 5127077300880247 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 8974292 and a.nrcrcard = 5127077300604282 and a.cdcooper = 1;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 479993 and a.nrcrcard = 5127077300880189 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 480134 and a.nrcrcard = 5127077300880437 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 486299 and a.nrcrcard = 5127077300880320 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 478270 and a.nrcrcard = 5127077300880197 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 8974292 and a.nrcrcard = 5127077300604308 and a.cdcooper = 1;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 1926403 and a.nrcrcard = 5127077300604290 and a.cdcooper = 1;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 479160 and a.nrcrcard = 5127077300880270 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 48631 and a.nrcrcard = 5127077300880221 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 442453 and a.nrcrcard = 5127077300880452 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 483125 and a.nrcrcard = 5127077300880411 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 482099 and a.nrcrcard = 5127077300880379 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 489883 and a.nrcrcard = 5127077300880429 and a.cdcooper = 11;
+update crapcrd a  set a.flgprovi = 1 where 1=1 and a.nrdconta = 369020 and a.nrcrcard = 5127077300880239 and a.cdcooper = 11;
+   
+-----------#################################################
+
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 80355706 and a.nrcrcard = 5127077300604258 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 488526 and a.nrcrcard = 5127077300880361 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 486493 and a.nrcrcard = 5127077300876153 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 448320 and a.nrcrcard = 5127077300880254 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 477753 and a.nrcrcard = 5127077300880171 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 487406 and a.nrcrcard = 5127077300880304 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 459356 and a.nrcrcard = 5127077300880213 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 308919 and a.nrcrcard = 5127077300880288 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 3 where 1=1 and a.nrdconta = 477095 and a.nrcrcard = 5127077300880205 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 491420 and a.nrcrcard = 5127077300880551 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 486612 and a.nrcrcard = 5127077300880338 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 442526 and a.nrcrcard = 5127077300880163 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 7145055 and a.nrcrcard = 5127077300604324 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 7579993 and a.nrcrcard = 5127077300604274 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 9544259 and a.nrcrcard = 5127077300604266 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 3 where 1=1 and a.nrdconta = 859370 and a.nrcrcard = 6042037300033494 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 489816 and a.nrcrcard = 5127077300880445 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 478199 and a.nrcrcard = 5127077300880353 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 487414 and a.nrcrcard = 5127077300880296 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 3 where 1=1 and a.nrdconta = 488160 and a.nrcrcard = 5127077300880494 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 488739 and a.nrcrcard = 5127077300880346 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 395714 and a.nrcrcard = 5127077300880262 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 859370 and a.nrcrcard = 6042037300033502 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 6175694 and a.nrcrcard = 5127077300604340 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 482927 and a.nrcrcard = 5127077300880502 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 475270 and a.nrcrcard = 5127077300880247 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 8974292 and a.nrcrcard = 5127077300604282 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 479993 and a.nrcrcard = 5127077300880189 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 3 where 1=1 and a.nrdconta = 480134 and a.nrcrcard = 5127077300880437 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 486299 and a.nrcrcard = 5127077300880320 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 478270 and a.nrcrcard = 5127077300880197 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 8974292 and a.nrcrcard = 5127077300604308 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 1926403 and a.nrcrcard = 5127077300604290 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 479160 and a.nrcrcard = 5127077300880270 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 48631 and a.nrcrcard = 5127077300880221 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 442453 and a.nrcrcard = 5127077300880452 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 483125 and a.nrcrcard = 5127077300880411 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 3 where 1=1 and a.nrdconta = 482099 and a.nrcrcard = 5127077300880379 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 4 where 1=1 and a.nrdconta = 489883 and a.nrcrcard = 5127077300880429 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 5 where 1=1 and a.nrdconta = 369020 and a.nrcrcard = 5127077300880239 and a.cdcooper = 11;
+
+
+commit;
+END;
