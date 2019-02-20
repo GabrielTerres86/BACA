ed643a3597fce9df2b28bb21f3259a02a7651c66
Otavio Theiss <otavio.theiss@ailos.coop.br>
2019-02-20 17:13:56 +0000

    PJ432 - Ajuste de status de cartões provisórios incidente INC0033417
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C30886

---
+++ 02/P432_ajuste_cartao_provisorio_legado_2.sql
@@ -0,0 +1,26 @@
+BEGIN
+	 
+-----------#################################################
+
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 80355706 and a.nrcrcard = 5127077300604258 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 488526 and a.nrcrcard = 5127077300880361 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 477753 and a.nrcrcard = 5127077300880171 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 308919 and a.nrcrcard = 5127077300880288 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 486612 and a.nrcrcard = 5127077300880338 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 442526 and a.nrcrcard = 5127077300880163 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 7145055 and a.nrcrcard = 5127077300604324 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 7579993 and a.nrcrcard = 5127077300604274 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 9544259 and a.nrcrcard = 5127077300604266 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 395714 and a.nrcrcard = 5127077300880262 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 859370 and a.nrcrcard = 6042037300033502 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 6175694 and a.nrcrcard = 5127077300604340 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 478270 and a.nrcrcard = 5127077300880197 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 8974292 and a.nrcrcard = 5127077300604308 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 1926403 and a.nrcrcard = 5127077300604290 and a.cdcooper = 1;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 479160 and a.nrcrcard = 5127077300880270 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 48631 and a.nrcrcard = 5127077300880221 and a.cdcooper = 11;
+update crawcrd a  set a.insitcrd = 6 where 1=1 and a.nrdconta = 369020 and a.nrcrcard = 5127077300880239 and a.cdcooper = 11;
+
+
+commit;
+END;
