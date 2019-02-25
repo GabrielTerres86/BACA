be89df336c8f056a9be40eec3b93e82f2982e18d
Adriano Marchi <adriano@ailos.coop.br>
2019-02-25 22:16:35 +0000

    Ajustes para novas validações no código de barras para faturas DARF/DAS - SCTASK0046009.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C30978

---
+++ 02/sctask0046009_cria_parametro_controle.sql
@@ -0,0 +1,17 @@
+
+INSERT INTO crapprm
+  (nmsistem
+  ,cdcooper
+  ,cdacesso
+  ,dstexprm
+  ,dsvlrprm)
+VALUES
+  ('CRED'
+  ,0
+  ,'VALIDACAO_DARF_DAS'
+  ,'Ligar/desligar validacao do codigo de barras para faturas DARF(385)/DAS.'
+  ,1);
+
+COMMIT;
+
+
