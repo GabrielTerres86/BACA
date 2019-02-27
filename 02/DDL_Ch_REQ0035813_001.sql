7ab311a2ae32cc5a4bdd8f3e4f6ef1531a9bd1ae
Cesar Belli <cesar.belli@envoltti.com.br>
2019-02-27 18:37:51 +0000

    Chd REQ0035813 - Inclusão de registro de critica - Belli
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31055

---
+++ 02/DDL_Ch_REQ0035813_001.sql
@@ -0,0 +1,7 @@
+--DDL_Ch_REQ0035813_001.sql
+--COBR0007
+
+insert into crapcri (CDCRITIC, DSCRITIC, TPCRITIC, FLGCHAMA)
+values (1397, '1397 - Registro de remessa nao encontrado.', 4, 0);
+                     
+commit;
