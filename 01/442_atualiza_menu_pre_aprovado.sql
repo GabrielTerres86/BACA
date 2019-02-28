87e4129f608178050b69c8114ce17134f39213c5
Petter Rafael Silva <petter.silva@envolti.com.br>
2019-02-28 16:43:38 +0000

    0442 - Ajustar BACA para criar menu ATENDA.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31094

--- 01/442_atualiza_menu_pre_aprovado.sql
+++ 01/442_atualiza_menu_pre_aprovado.sql
@@ -1,4 +1,9 @@
+DECLARE
+  idx PLS_INTEGER := 1;
+  
 BEGIN
+  dbms_output.put_line('Inicializando...');
+
   UPDATE craptel
   SET tldatela = 'Pre Aprovado'
      ,tlrestel = 'Pre Aprovado'
@@ -6,6 +11,22 @@ BEGIN
   WHERE UPPER(nmdatela) LIKE UPPER('atenda')
      AND nrmodulo = 1
      AND UPPER(tlrestel) LIKE UPPER('Desabilitar Operacoes');
+  
+  dbms_output.put_line('Contador => ' || SQL%ROWCOUNT);
+  
+  IF SQL%ROWCOUNT = 0 THEN
+    WHILE idx <= 17 LOOP
+      
+      dbms_output.put_line('INSERT => ' || idx);
+    
+      INSERT INTO craptel(nmdatela, nrmodulo, cdopptel, tldatela, tlrestel, flgteldf, flgtelbl, nmrotina,lsopptel, inacesso, cdcooper, idsistem, idevento, nrordrot, nrdnivel, idambtel)
+         VALUES('ATENDA', 1, '@,A', 'Pre Aprovado', 'Pre Aprovado', 0, 1, 'PRE APROVADO', 'ACESSO,ALTERACAO', 2, idx, 1, 0, 33, 1, 2);
+    
+      idx := idx + 1;
+    END LOOP;
+  END IF;
+  
+  dbms_output.put_line('... finalizando!');
      
   COMMIT;
 END;
