d9b7673674de5cdb4a35eec92db3e6cb3a93eed8
Fernanda K. de Oliveira (AMCOM) <fernanda.oliveira@amcom.com.br>
2019-03-05 14:42:46 +0000

    P437.Consignado
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/P437 - Consignado2/DEV;C31191

--- BACA/10.CONSIGNADO - insert_crapaca_TELA_CONSIG.sql
+++ BACA/10.CONSIGNADO - insert_crapaca_TELA_CONSIG.sql
@@ -18,7 +18,7 @@ BEGIN
     ('INC_ALT_VENC_PARCELA'
     ,'TELA_CONSIG'
     ,'pc_alt_empr_consig_param_web'
-    ,'pr_idemprconsigparam,pr_idemprconsig,pr_dtinclpropostade,pr_dtinclpropostaate,pr_dtenvioarquivo,pr_dtvencimento'
+    ,'pr_idemprconsigparam,pr_cdempres,pr_dtinclpropostade,pr_dtinclpropostaate,pr_dtenvioarquivo,pr_dtvencimento'
     ,v_nrseqrdr);
     
   --Incluir a ação EXCLUIR_VENC_PARCELA 
@@ -32,7 +32,7 @@ BEGIN
     ('EXCLUIR_VENC_PARCELA'
     ,'TELA_CONSIG'
     ,'pc_excluir_empr_consig_param_web'
-    ,'pr_idemprconsigparam,pr_idemprconsig'
+    ,'pr_idemprconsigparam,pr_cdempres'
     ,v_nrseqrdr);
     
   --Incluir a ação REPLICAR_VENC_PARCELA 
@@ -46,7 +46,7 @@ BEGIN
     ('REPLICAR_VENC_PARCELA'
     ,'TELA_CONSIG'
     ,'pc_replicar_empr_consig_param_web'
-    ,'pr_idemprconsigparam,pr_idemprconsig,pr_dtinclpropostade,pr_dtinclpropostaate,pr_dtenvioarquivo,pr_dtvencimento'
+    ,'pr_idemprconsigparam,pr_cdempres,pr_dtinclpropostade,pr_dtinclpropostaate,pr_dtenvioarquivo,pr_dtvencimento'
     ,v_nrseqrdr); 
     
   --Incluir a ação BUSCAR_VENC_PARCELA 
@@ -60,7 +60,7 @@ BEGIN
     ('BUSCAR_VENC_PARCELA'
     ,'TELA_CONSIG'
     ,'pc_buscar_empr_consig_param_web'
-    ,'pr_idemprconsigparam,pr_idemprconsig'
+    ,'pr_idemprconsigparam,pr_cdempres'
     ,v_nrseqrdr);    
     
   COMMIT;             
