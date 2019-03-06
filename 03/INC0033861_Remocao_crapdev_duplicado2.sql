4c9b8ecc86395985ca4be8b1e411dee3ffd9c6d6
Andre Bohn (Mouts) <mouts-andre.bohn@ailos.coop.br>
2019-03-06 18:53:12 +0000

    INC0033861 - Cheque devolvidos duplicados das 16h
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31234

--- 03/INC0033861_Remocao_crapdev_duplicado2.sql
+++ 03/INC0033861_Remocao_crapdev_duplicado2.sql
@@ -1,9 +1,6 @@
 begin
-	delete crapdev where crapdev.cdcooper = 13 and progress_recid = 2275252;
-	delete crapdev where crapdev.cdcooper = 13 and progress_recid = 2275253;
-
-	DELETE FROM crapsol WHERE crapsol.cdcooper = 13  AND
-                               crapsol.dtrefere = '06/03/2019' AND
-                               crapsol.nrsolici = 78;
+	delete crapdev where crapdev.cdcooper = 1 and progress_recid = 2275314;
+	delete crapdev where crapdev.cdcooper = 1 and progress_recid = 2275315;
+	
 	commit;
 end;
