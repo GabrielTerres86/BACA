bf1fbe755c8704cb20f6e3fbb3e3e00f05f0172c
Andre Bohn (Mouts) <mouts-andre.bohn@ailos.coop.br>
2019-03-06 16:52:13 +0000

    Ajuste exclusão solicitação
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31225

--- 03/INC0033861_Remocao_crapdev_duplicado2.sql
+++ 03/INC0033861_Remocao_crapdev_duplicado2.sql
@@ -2,5 +2,8 @@ begin
 	delete crapdev where crapdev.cdcooper = 13 and progress_recid = 2275252;
 	delete crapdev where crapdev.cdcooper = 13 and progress_recid = 2275253;
 
+	DELETE FROM crapsol WHERE crapsol.cdcooper = 13  AND
+                               crapsol.dtrefere = '06/03/2019' AND
+                               crapsol.nrsolici = 78;
 	commit;
 end;
