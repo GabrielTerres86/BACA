7ee508a581ed12537d36264fe53bcf26eb3a994e
Josiane Stiehler (AMCOM) <josiane.stiehler@amcom.com.br>
2019-03-07 19:41:04 +0000

    projeto 437 - consignado - incluido a atualização da crapaca para gravação da simulação
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/P437 - Consignado2/DEV;C31287

--- BACA/11.CONSIGNADO - insere_crapaca_tela_simulacao.sql
+++ BACA/11.CONSIGNADO - insere_crapaca_tela_simulacao.sql
@@ -1,8 +1,10 @@
 INSERT INTO CRAPACA VALUES (SEQACA_NRSEQACA.NEXTVAL,'SIMULA_VALIDA_CONSIGNADO',
 'TELA_ATENDA_SIMULACAO','pc_valida_simul_consig','pr_nrdconta,pr_cdlcremp',1893)
 /
-INSERT INTO CRAPACA VALUES (SEQACA_NRSEQACA.NEXTVAL,'SIMULA_BUSCA_VENCIMENTOS',
-'TELA_ATENDA_SIMULACAO','pc_retorna_vencto_simul_consig','pr_nrdconta',1893)
+UPDATE  crapaca  a
+set a.lstparam = a.lstparam||',pr_vlparepr,pr_vliofepr'
+where a.nmpackag = 'TELA_ATENDA_SIMULACAO'
+AND A.NMDEACAO = 'SIMULA_GRAVA_SIMULACAO'
 /
 commit
 /
