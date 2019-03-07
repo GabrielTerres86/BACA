b78eba025ddc2f1fe4004d2aea7c18936c999271
Andre Bohn (Mouts) <mouts-andre.bohn@ailos.coop.br>
2019-03-07 15:56:11 +0000

    INC0033861 - Remoção crapdev duplicado
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31273

--- 03/INC0033861_Remocao_crapdev_duplicado2.sql
+++ 03/INC0033861_Remocao_crapdev_duplicado2.sql
@@ -1,6 +1,9 @@
 begin
-	delete crapdev where crapdev.cdcooper = 1 and progress_recid = 2275314;
-	delete crapdev where crapdev.cdcooper = 1 and progress_recid = 2275315;
-	
+	delete crapdev where crapdev.cdcooper = 1 and progress_recid = 2281134;
+	delete crapdev where crapdev.cdcooper = 1 and progress_recid = 2281135;
+    delete FROM crapsol WHERE crapsol.cdcooper = 1  AND
+                               crapsol.dtrefere = '07/03/2019' AND
+                               crapsol.nrsolici = 78;
+							   
 	commit;
 end;
