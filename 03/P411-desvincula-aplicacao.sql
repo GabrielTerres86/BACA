8e8430baae441a830819935fa967a590135f7329
Anderson Fossa <anderson.fossa@ailos.coop.br>
2019-03-08 18:44:53 +0000

    P411 - Desativa aplicação ativa indevidamente.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31330

---
+++ 03/P411-desvincula-aplicacao.sql
@@ -0,0 +1,7 @@
+
+
+update craprac set idsaqtot = 1 where cdcooper = 1 and nrdconta = 8702691 and nraplica = 4;
+
+update craprac set idsaqtot = 1 where cdcooper = 16 and nrdconta = 85243 and nraplica = 8;
+
+commit;
