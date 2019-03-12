ad1585643e57706293a27e8337d9673023d0c573
Wagner da Silva <wagner.silva@ailos.coop.br>
2019-03-12 17:54:16 +0000

    Exclusão de lançamentos futuros - INC0034509 - Wagner - Sustentação. Inclusão de mais 2.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31391

--- 03/INC0034509_Cancelar_3_Lanc_Futuros.sql
+++ 03/INC0034509_Cancelar_3_Lanc_Futuros.sql
@@ -3,6 +3,6 @@ UPDATE craplau a
    SET a.insitlau = 4
      , a.dtdebito = TO_DATE('12/03/2019', 'DD/MM/RRRR')
      , a.dscritic = 'Exclusão de lançamentos futuros - INC0034509'
- WHERE a.progress_recid in (26096503,26096592,26096667);
+ where a.progress_recid in (26096503,26096592,26096667,26331967,26332001);
    
 COMMIT;
