37ff93f6ca7ebe91fd52f7402b44873c814e97b3
Renato Darosci (Supero) <supero-renato.darosci@ailos.coop.br>
2019-03-13 14:07:45 +0000

    Inclusão dos scripts para a Fase 2 do Portabilidade de Salário
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31429

---
+++ 04/P485.027_Incluir_dominio_impedimento.sql
@@ -0,0 +1,59 @@
+BEGIN
+  
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 1, 'Limite de Credito');
+	
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 2, 'Convenio');
+	
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 3, 'PAMCARD');
+	
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 4, 'Conta ITG');
+
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 5, 'CDC');
+	
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 6, 'Folhas de cheque');
+	
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 7, 'Bordero');
+	
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 8, 'Cartao de crédito');
+
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 9, 'Bloqueto de cobrança');
+	
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 10, 'Resgate de poupança programada');
+	
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 11, 'Aplicação');
+	
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 12, 'Lançamento futuro');
+	
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 13, 'Cartão magnético');
+
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 14, 'Plano de cotas');
+	
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 15, 'Poupança programada');
+	
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 16, 'Empréstimo e financiamento');
+	
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 17, 'Limite de desconto de cheques');
+	
+	INSERT INTO tbcc_dominio_campo (nmdominio, cddominio, dscodigo) VALUES 
+	('IMPED_DESLIG_CTA_SAL', 18, 'Limite de desconto de títulos');
+
+	COMMIT;
+  
+END;
