99f775a72340bce788352d328211d533a2f1193a
Jaison Fernando Ferreira <jaison.ferreira@ailos.coop.br>
2019-03-13 19:33:07 +0000

    P450 - Remoção de update
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31458

--- 02/P450_Ajustes_Central_3040.sql
+++ 02/P450_Ajustes_Central_3040.sql
@@ -1,19 +1,11 @@
 DELETE crapris ris
  WHERE ris.cdcooper = 1
    AND ris.inddocto = 2
-   AND ris.dtrefere = '28/02/2019'
-;
+   AND ris.dtrefere = '28/02/2019';
+
 UPDATE crapris ris
    SET ris.flgindiv = 0
  WHERE ris.cdcooper = 1
-   AND ris.dtrefere = '28/02/2019'
-
-COMMIT;
-
-UPDATE crapprm prm
-   SET prm.dsvlrprm = prm.dsvlrprm || ';1402;2162;2425;2648'
- WHERE nmsistem = 'CRED' 
-   AND cdcooper = 0 
-   AND cdacesso = 'HISTOR_PREJ_N_SALDO';
+   AND ris.dtrefere = '28/02/2019';
 
 COMMIT;
