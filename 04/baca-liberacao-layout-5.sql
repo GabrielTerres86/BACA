96257b892636bbc642ba0f8783ae93f7a2212904
Gabriel Fronza Marcos (Mouts) <mouts-gabriel.marcos@ailos.coop.br>
2019-03-14 07:24:07 +0000

    Correção de baca.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31486

--- 04/baca-liberacao-layout-5.sql
+++ 04/baca-liberacao-layout-5.sql
@@ -2,6 +2,9 @@ begin
   update gnconve c
      set c.flgvlcpf = 1
    where c.nrlayout = 5;
+  update gnconve c
+     set c.flgvlcpf = 0
+   where c.nrlayout = 4;
   commit;
 exception
   when others then
