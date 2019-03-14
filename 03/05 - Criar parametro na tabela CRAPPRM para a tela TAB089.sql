a413d90467f15e94292dfcb004b6a043cfcbce83
Cassia de Oliveira (GFT) <Cassia.Oliveira@gft.com>
2019-03-14 16:25:05 +0000

    403: Merge de scripts
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31517

---
+++ 03/05 - Criar parametro na tabela CRAPPRM para a tela TAB089.sql
@@ -0,0 +1,32 @@
+ /*---------------------------------------------------------------------------------------------------------------------
+    Programa    : Tela TAB089 - Script de carga
+    Projeto     : 403 - Desconto de Títulos - Release 4
+    Autor       : Cassia de Oliveira (GFT)
+    Data        : Setembro/2018
+    Objetivo    : Criar parametro na tabela CRAPPRM para a tela TAB089
+  ---------------------------------------------------------------------------------------------------------------------*/
+
+begin
+
+-- Insere o valor maximo por cooperativa de estorno permitido sem autorização para desconto de titulo
+INSERT INTO crapprm
+	(nmsistem, 
+	 cdcooper, 
+	 cdacesso, 
+	 dstexprm, 
+	 dsvlrprm)
+	SELECT 'CRED', 
+           cdcooper, 
+           'VL_MAX_ESTORN_DST', 
+           'Vl. máx. de estorno perm. sem autorização da coordenação/gerência para desconto de titulo', 
+           SUBSTR(dstextab,40,12)
+	  FROM craptab
+	 WHERE nmsistem = 'CRED' 
+       AND tptabela = 'USUARI'
+       AND cdempres = 11
+       AND cdacesso = 'PAREMPREST'
+       AND tpregist = 01;
+
+commit;
+end;
+
