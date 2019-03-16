507547dd150e1191efd98b582de19e4ee4b7bf13
Paulo Manoel R Penteado (GFT) <CECRED\t0031735>
2019-03-16 13:53:51 +0000

    403: Adicionado prm FLG_CRPS538_CONCLUIDA
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31597

--- 03/01 - Parâmetros e Mensageria.sql
+++ 03/01 - Parâmetros e Mensageria.sql
@@ -27,6 +27,8 @@ DELETE FROM craprdr WHERE nmprogra = 'TITCTO';
 */
 
 -- PARÂMETROS
+INSERT INTO crapprm (SELECT 'CRED',cdcooper,'FLG_CRPS538_CONCLUIDA','Define se a crps538 foi concluida no processo noturno','1',NULL FROM crapcop where flgativo = 1);
+
 
 -- Parâmetros Tela COBTIT
 
