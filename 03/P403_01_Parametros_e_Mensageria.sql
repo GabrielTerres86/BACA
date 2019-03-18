80a9624a3ebd6d6138ec014c0d9ca41adfe81cb7
Paulo Manoel R Penteado (GFT) <CECRED\t0031735>
2019-03-18 20:01:29 +0000

    403: Correção de script
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31658

--- 03/P403_01_Parametros_e_Mensageria.sql
+++ 03/P403_01_Parametros_e_Mensageria.sql
@@ -33,8 +33,8 @@ DELETE FROM craprdr WHERE nmprogra = 'TITCTO';
 -- MENSAGERIA
 
 -- ATENDA->DESCONTOS->TITULOS
-INSERT INTO crapaca (nrseqaca, nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
-     VALUES (SEQACA_NRSEQACA.NEXTVAL, 'LISTAR_HIST_ALT_LIMITE', 'TELA_ATENDA_DSCTO_TIT', 'pc_busca_hist_alt_limite_web', 'pr_nrdconta,pr_tpctrlim,pr_nrctrlim', (SELECT nrseqrdr FROM craprdr WHERE nmprogra = 'TELA_ATENDA_DESCTO'));
+--INSERT INTO crapaca (nrseqaca, nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
+--     VALUES (SEQACA_NRSEQACA.NEXTVAL, 'LISTAR_HIST_ALT_LIMITE', 'TELA_ATENDA_DSCTO_TIT', 'pc_busca_hist_alt_limite_web', 'pr_nrdconta,pr_tpctrlim,pr_nrctrlim', (SELECT nrseqrdr FROM craprdr WHERE nmprogra = 'TELA_ATENDA_DESCTO'));
 
 -- Consultar Prejuizo
 INSERT INTO CRAPACA(NRSEQACA, NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
@@ -52,10 +52,10 @@ INSERT INTO CRAPACA(NRSEQACA, NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
 VALUES (SEQACA_NRSEQACA.NEXTVAL,'BUSCAR_BORDEROS_LIBERADOS','DSCT0003','pc_buscar_borderos_liberados','pr_nrdconta,pr_nrctremp,pr_nrborder',(SELECT nrseqrdr FROM craprdr WHERE nmprogra ='DSCT0003'));
 UPDATE crapaca SET lstparam = lstparam||',pr_cdtpprod' WHERE nmdeacao = 'ESTORN_CONSULTAR_EST';
 UPDATE crapaca SET lstparam = lstparam||',pr_cdtpprod' WHERE nmdeacao = 'ESTORN_CONSULTAR_DET_EST' ;
-UPDATE crapaca SET nmpackag = 'TELA_ESTORN', lstparam = lstparam || ',pr_cdtpprod', nmproced = 'pc_tela_busca_lancto' WHERE nmdeacao = 'ESTORN_CON_LANCTO_EST' 
-UPDATE crapaca SET nmpackag = 'TELA_ESTORN', lstparam = lstparam || ',pr_cdtpprod', nmproced = 'pc_tela_valida_lancto' WHERE nmdeacao = 'ESTORN_VALIDA_INC'
-UPDATE crapaca SET nmpackag = 'TELA_ESTORN', nmproced = 'pc_tela_estornar_web', lstparam = 'pr_nrdconta,pr_nrctremp,pr_dsjustificativa' WHERE nmdeacao = 'ESTORN_INC_LANCTO_EST'
-UPDATE crapaca SET nmpackag = 'TELA_ESTORN', nmproced = 'pc_tela_imprimir_relatorio_web', lstparam = lstparam || ', pr_cdtpprod' WHERE nmdeacao = 'ESTORN_IMPRESSAO'
+UPDATE crapaca SET nmpackag = 'TELA_ESTORN', lstparam = lstparam || ',pr_cdtpprod', nmproced = 'pc_tela_busca_lancto' WHERE nmdeacao = 'ESTORN_CON_LANCTO_EST' ;
+UPDATE crapaca SET nmpackag = 'TELA_ESTORN', lstparam = lstparam || ',pr_cdtpprod', nmproced = 'pc_tela_valida_lancto' WHERE nmdeacao = 'ESTORN_VALIDA_INC';
+UPDATE crapaca SET nmpackag = 'TELA_ESTORN', nmproced = 'pc_tela_estornar_web', lstparam = 'pr_nrdconta,pr_nrctremp,pr_dsjustificativa' WHERE nmdeacao = 'ESTORN_INC_LANCTO_EST';
+UPDATE crapaca SET nmpackag = 'TELA_ESTORN', nmproced = 'pc_tela_imprimir_relatorio_web', lstparam = lstparam || ', pr_cdtpprod' WHERE nmdeacao = 'ESTORN_IMPRESSAO';
 
 -- TAB089 - Insere o parametro pr_vlmaxdst para a chamada da procedure pc_alterar da tela TAB089	   
 UPDATE crapaca aca SET aca.lstparam = aca.lstparam || ' ,pr_vlmaxdst' WHERE aca.nmdeacao = 'TAB089_ALTERAR';
@@ -73,6 +73,7 @@ INSERT INTO crapaca (nrseqaca, nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
   VALUES ((SELECT MAX(NRSEQACA)+1 FROM CRAPACA), 'ESTORN_INC_LANCTO_EST_PRJ', 'TELA_ESTORN', 'pc_efetua_estorno_prj_web', 'pr_nrdconta,pr_nrborder,pr_justificativa', (SELECT nrseqrdr FROM craprdr WHERE nmprogra = 'ESTORN'));
 
 -- Consulta antes de cancelar contrato
-INSERT INTO crapaca(nmdeacao,nmpackag,nmproced,lstparam,nrseqrdr) VALUES ('VERIFICA_BORDERO_CONTRATO', 'DSCT0003','pc_verifica_contrato_bodero', 'pr_nrdconta, pr_nrctrlim',724)
-commit;
+INSERT INTO crapaca(nmdeacao,nmpackag,nmproced,lstparam,nrseqrdr) VALUES ('VERIFICA_BORDERO_CONTRATO', 'DSCT0003','pc_verifica_contrato_bodero', 'pr_nrdconta, pr_nrctrlim',724);
+
+COMMIT;
 end;
