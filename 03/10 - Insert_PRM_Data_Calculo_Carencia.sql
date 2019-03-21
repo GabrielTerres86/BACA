ea51effc3d7a7edefef49eb60e0f9217c7653bf2
Lucas Lazari da Silva (GFT) <CECRED\t0031648>
2019-03-21 21:51:51 +0000

    P403 - Ajuste na data de corte da carência
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31823

--- 03/10 - Insert_PRM_Data_Calculo_Carencia.sql
+++ 03/10 - Insert_PRM_Data_Calculo_Carencia.sql
@@ -1,4 +1,4 @@
 BEGIN
-	INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 0, 'DT_CALC_CARENCIA', 'Data que indica a partir de quando se deve aplicar a nova regra de calculo de dias de carencia.', '22/03/2019');
+	INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 0, 'DT_CALC_CARENCIA', 'Data que indica a partir de quando se deve aplicar a nova regra de calculo de dias de carencia.', '23/03/2019');
 	COMMIT;
 END;	
