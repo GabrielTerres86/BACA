a05906f7af5af132586c395502c03ed8e960ac26
Paulo Manoel R Penteado (GFT) <CECRED\t0031735>
2019-03-23 13:42:30 +0000

    403: Update PRM com valor nulo da TAB089
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C31888

--- 03/P403_12_Update_PRM_valor_nulo_TAB089.sql
+++ 03/P403_12_Update_PRM_valor_nulo_TAB089.sql
@@ -3,6 +3,6 @@ BEGIN
      SET dsvlrprm = '000000000,00'
    WHERE nmsistem = 'CRED'
      AND cdacesso = 'VL_MAX_ESTORN_DST'
-     AND TRIM(dsvlrprm) IS NULL;
+     AND TRIM(dsvlrprm)  IS NULL;
   COMMIT;
 END;
