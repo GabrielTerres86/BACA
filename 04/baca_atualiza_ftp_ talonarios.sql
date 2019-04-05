b404614ecd610f2efebb0d08f902a10bf20c497a
Elton Fernando Goedert <elton@ailos.coop.br>
2019-04-05 21:06:42 +0000

    Atualização script FTP
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C32378

--- 04/baca_atualiza_ftp_ talonarios.sql
+++ 04/baca_atualiza_ftp_ talonarios.sql
@@ -8,4 +8,9 @@ UPDATE crapprm
    SET dsvlrprm = 'TrA4Uka!=Sp@!I#rAfrl'
  WHERE cdacesso = 'RRD_PASS_FTP';
 
+UPDATE crapprm
+   SET dsvlrprm = '/usr/local/cecred/bin/ftp_envia_recebe.pl'
+ WHERE cdacesso = 'RRD_SCRIPT';
+
+
 COMMIT;
