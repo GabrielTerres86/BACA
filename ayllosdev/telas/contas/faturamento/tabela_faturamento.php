cce69e48052ef11c2d40193b6bd973c4cfee6aab
Cassia de Oliveira (GFT) <Cassia.Oliveira@gft.com>
2019-04-09 18:01:09 +0000

    D407 - Inclusão de média de faturamento
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/2019_02;C32458

--- ayllosdev/telas/contas/faturamento/tabela_faturamento.php
+++ ayllosdev/telas/contas/faturamento/tabela_faturamento.php
@@ -6,7 +6,50 @@
  * OBJETIVO     : Tabela que apresenda os bens do titular selecionado
  *
  * ALTERACOES   : 05/08/2015 - Reformulacao cadastral (Gabriel-RKAM)
+ *                04/04/2019 - Adicionado campos de média de faturamente (Cássia de Oliveira - GFT)
  */
+?>
+<?
+	function retorna_mes ($mes){
+	    switch ($mes) {
+	   		case 1:
+	        	return "Jan";
+	        	break;
+	    	case 2:
+	        	return "Fev";
+	        	break;
+	    	case 3:
+	        	return "Mar";
+	        	break;
+	        case 4:
+	        	return "Abr";
+	        	break;
+	    	case 5:
+	        	return "Mai";
+	        	break;
+	    	case 6:
+	        	return "Jun";
+	        	break;
+	        case 7:
+	        	return "Jul";
+	        	break;
+	    	case 8:
+	        	return "Ago";
+	        	break;
+	    	case 9:
+	        	return "Set";
+	        	break;
+	        case 10:
+	        	return "Out";
+	        	break;
+	    	case 11:
+	        	return "Nov";
+	        	break;
+	    	case 12:
+	        	return "Des";
+	        	break;
+		}
+	}
 ?>
 
 <div class="divRegistros">
@@ -17,10 +60,34 @@
 				<th>Faturamento</th></tr>			
 		</thead>
 		<tbody>
+			<? $media = 0; 
+			   $primeiromes = 0;
+			   $primeiroano = 0;
+			   $ultimomes = 0;
+			   $ultimoano = 0;?>
 			<? foreach( $registros as $faturamento ) {
 					if ( getByTagName($faturamento->tags,'mesftbru') != 0 && getByTagName($faturamento->tags,'anoftbru') != 0) {
 			?>
-				
+				<?  $media = $media + (float)str_replace(',','.',getByTagName($faturamento->tags,'vlrftbru')); 
+					if($primeiromes == 0){
+						$primeiromes = (int)getByTagName($faturamento->tags,'mesftbru');
+						$primeiroano = (int)getByTagName($faturamento->tags,'anoftbru');
+					} elseif((int)getByTagName($faturamento->tags,'anoftbru') <= $primeiroano){
+						if((int)getByTagName($faturamento->tags,'mesftbru') < $primeiromes){
+							$primeiromes = (int)getByTagName($faturamento->tags,'mesftbru');
+							$primeiroano = (int)getByTagName($faturamento->tags,'anoftbru');
+						}
+					}
+					if($ultimomes == 0){
+						$ultimomes = (int)getByTagName($faturamento->tags,'mesftbru');
+						$ultimoano = (int)getByTagName($faturamento->tags,'anoftbru');
+					} elseif((int)getByTagName($faturamento->tags,'anoftbru') >= $ultimoano){
+						if((int)getByTagName($faturamento->tags,'mesftbru') > $ultimomes){
+							$ultimomes = (int)getByTagName($faturamento->tags,'mesftbru');
+							$ultimoano = (int)getByTagName($faturamento->tags,'anoftbru');
+						}
+					}
+				?>
 				<tr><td><span><? echo getByTagName($faturamento->tags,'mesftbru') ?></span>
 						<? echo getByTagName($faturamento->tags,'mesftbru') ?>						
 						<input type="hidden" id="nrposext" name="nrposext" value="<? echo getByTagName($faturamento->tags,'nrposext') ?>" /></td>
@@ -34,13 +101,22 @@
 </div>
 
 <div class="divFinanc">
-	<? $faturamento = $registros[0]->tags; ?>
+	<? $faturamento = $registros[0]->tags; 
+	   $ultimo = end($registros); 
+	   $media = $media / count($registros);
+	   $primeiromes = retorna_mes($primeiromes);
+	   $ultimomes = retorna_mes($ultimomes);
+	?>
 	<label for="dtaltjfn">Alterado:</label>	
 	<input name="dtaltjfn" id="dtaltjfn" type="text" value="<? echo getByTagName($faturamento,'dtaltjfn') ?>" />
 	
 	<label for="cdoperad">Operador:</label>
 	<input name="cdoperad" id="cdoperad" type="text" value="<? echo getByTagName($faturamento,'cdoperad') ?>" />
 	<input name="nmoperad" id="nmoperad" type="text" value="<? echo getByTagName($faturamento,'nmoperad') ?>" />
+
+	<label for="vlmediaf">M&eacutedia Faturamento:</label>
+	<input name="vlmediaf" id="vlmediaf" type="text" value="<? echo number_format(str_replace(',','.',$media),2,',','.') ?>" />
+	<input name="nmperiod" id="nmperiod" type="text" value="<? echo $primeiromes, ' - ', $ultimomes ?>" />
 </div>
 
 <div id="divBotoes">
