2215731fab561503fa6cad43b653574a6f5b3f0f
Andre Bohn (Mouts) <mouts-andre.bohn@ailos.coop.br>
2019-04-09 19:07:50 +0000

    Projeto revitalizaçãom Ajuste conforme script DBA
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C32469

--- 04/reestruturacao_indices.sql
+++ 04/reestruturacao_indices.sql
@@ -1,14 +1,20 @@
--- Drop indexes 
-drop index CRAPSDA##CRAPSDA2;
+-- Drop indexes
+drop index cecred.CRAPSDA##CRAPSDA2;
+drop index cecred.CRAPSDA##CRAPSDA1;
 
--- Create/Recreate indexes 
-drop index CRAPSDA##CRAPSDA1;
-create unique index CRAPSDA##CRAPSDA1 on CRAPSDA (CDCOOPER, DTMVTOLT, NRDCONTA, PROGRESS_RECID)
-  nologging  local;
+-- Create/Recreate indexes
+
+create unique index cecred.CRAPSDA##CRAPSDA1 on cecred.CRAPSDA (CDCOOPER, DTMVTOLT, NRDCONTA, PROGRESS_RECID) nologging local parallel 16;
+ALTER INDEX cecred.CRAPSDA##CRAPSDA1 noparallel;
 
 -- Statistics
 begin
-  dbms_stats.gather_table_stats(ownname          => 'CECRED',
-                                tabname          => 'CRAPSDA',
-                                estimate_percent => 33);
+  execute DBMS_STATS.GATHER_TABLE_STATS (ownname => 'CECRED',
+                                         TABNAME =>'CRAPSDA',
+										 estimate_percent => 33,
+										 method_opt => 'FOR ALL COLUMNS SIZE AUTO',
+										 degree => 16 ,
+										 granularity => 'ALL', 
+										 cascade => TRUE, 
+										 no_invalidate => FALSE);
 end;
