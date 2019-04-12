424afc28dceea345fd4b11d8e67320048d3e7849
Anderson Alan Sauberlich (Supero) <anderson.sauberlich@supero.com.br>
2019-04-12 17:57:21 +0000

    Ajustado data de corte
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C32602

--- 04/P429.005_Insere_CRAPREL_Termo_Adesao_Cartao.sql
+++ 04/P429.005_Insere_CRAPREL_Termo_Adesao_Cartao.sql
@@ -15,10 +15,10 @@ BEGIN
     BEGIN
       
       INSERT INTO TBGEN_VERSAO_TERMO (CDCOOPER, DSCHAVE_VERSAO, DTINICIO_VIGENCIA, DTFIM_VIGENCIA, DSNOME_JASPER, DTCADASTRO, DSDESCRICAO)
-      VALUES (rw_crapcop.cdcooper, 'TERMO ADESAO CDC PF V1', to_date('28-02-2019', 'dd-mm-yyyy'), null, 'termo_adesao_pf_novo.jasper', to_date('28-02-2019', 'dd-mm-yyyy'), 'Termo de adesão de cartão de Crédito AILOS para pessoa física em novo layout.');
+      VALUES (rw_crapcop.cdcooper, 'TERMO ADESAO CDC PF V1', to_date('16-04-2019', 'dd-mm-yyyy'), null, 'termo_adesao_pf_novo.jasper', to_date('16-04-2019', 'dd-mm-yyyy'), 'Termo de adesão de cartão de Crédito AILOS para pessoa física em novo layout.');
 
       INSERT INTO TBGEN_VERSAO_TERMO (CDCOOPER, DSCHAVE_VERSAO, DTINICIO_VIGENCIA, DTFIM_VIGENCIA, DSNOME_JASPER, DTCADASTRO, DSDESCRICAO)
-	    VALUES (rw_crapcop.cdcooper, 'TERMO ADESAO CDC PJ V1', to_date('28-02-2019', 'dd-mm-yyyy'), null, 'termo_adesao_pj_novo.jasper', to_date('28-02-2019', 'dd-mm-yyyy'), 'Termo de adesão de cartão de Crédito AILOS para pessoa jurídica em novo layout.');
+	    VALUES (rw_crapcop.cdcooper, 'TERMO ADESAO CDC PJ V1', to_date('16-04-2019', 'dd-mm-yyyy'), null, 'termo_adesao_pj_novo.jasper', to_date('16-04-2019', 'dd-mm-yyyy'), 'Termo de adesão de cartão de Crédito AILOS para pessoa jurídica em novo layout.');
 
       
     EXCEPTION
