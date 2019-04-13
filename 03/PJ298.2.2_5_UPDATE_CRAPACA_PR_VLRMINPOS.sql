d1d41832075f14d770123a1e3fe1470d421cdb3c
Rafael Faria (Supero) <supero-rafael.faria@ailos.coop.br>
2019-04-13 23:47:19 +0000

    PJ298.2.2 - Ajuste de aca
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C32634

--- 03/PJ298.2.2_5_UPDATE_CRAPACA_PR_VLRMINPOS.sql
+++ 03/PJ298.2.2_5_UPDATE_CRAPACA_PR_VLRMINPOS.sql
@@ -1,6 +1,6 @@
 -- incluir parametro para identificar o tipo de Emprestimo
 UPDATE CRAPACA 
-SET LSTPARAM = LSTPARAM || ',pr_pr_vlrminpos'
+SET LSTPARAM = 'pr_cdcooper,pr_nrconven,pr_nrdconta,pr_prazomax,pr_prazobxa,pr_vlrminpp,pr_vlrmintr,pr_vlrminpos,pr_dslinha1,pr_dslinha2,pr_dslinha3,pr_dslinha4,pr_dstxtsms,pr_dstxtema,pr_blqemiss,pr_qtdmaxbl,pr_flgblqvl,pr_descprej,pr_tpproduto'
 WHERE NMDEACAO = 'TAB096_GRAVAR'
   AND NMPACKAG = 'TELA_TAB096';
 
