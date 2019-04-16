135ee93505bf3ccbefe268e0fc61c0c6093968ed
Adriano Marchi <adriano@ailos.coop.br>
2019-04-16 16:31:02 +0000

    Script para cancelar agendamentos duplicados pelo sistema.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C32723

--- 04/inc0012970_cancelar_agendamentos.sql
+++ 04/inc0012970_cancelar_agendamentos.sql
@@ -4,14 +4,14 @@
 */
 
 --Atualiza
-update craplau set insitlau = 3, dtdebito = '16/04/19' where progress_recid = '26331836';
-update craplau set insitlau = 3, dtdebito = '16/04/19' where progress_recid = '26331861';
-update craplau set insitlau = 3, dtdebito = '16/04/19' where progress_recid = '26331885';
-update craplau set insitlau = 3, dtdebito = '16/04/19' where progress_recid = '26331906';
-update craplau set insitlau = 3, dtdebito = '16/04/19' where progress_recid = '26331928';
-update craplau set insitlau = 3, dtdebito = '16/04/19' where progress_recid = '26331954';
-update craplau set insitlau = 3, dtdebito = '16/04/19' where progress_recid = '26331975';
-update craplau set insitlau = 3, dtdebito = '16/04/19' where progress_recid = '26331997';
+update craplau set insitlau = 4, dtdebito = '16/04/19' where progress_recid = '26331836';
+update craplau set insitlau = 4, dtdebito = '16/04/19' where progress_recid = '26331861';
+update craplau set insitlau = 4, dtdebito = '16/04/19' where progress_recid = '26331885';
+update craplau set insitlau = 4, dtdebito = '16/04/19' where progress_recid = '26331906';
+update craplau set insitlau = 4, dtdebito = '16/04/19' where progress_recid = '26331928';
+update craplau set insitlau = 4, dtdebito = '16/04/19' where progress_recid = '26331954';
+update craplau set insitlau = 4, dtdebito = '16/04/19' where progress_recid = '26331975';
+update craplau set insitlau = 4, dtdebito = '16/04/19' where progress_recid = '26331997';
 
 
 /*
