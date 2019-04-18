6f3bdfa0ead0ca40097f9f0805e4efb544673d44
Adriano Nagasava (Supero) <CECRED\t0031546>
2019-04-18 12:46:09 +0000

    PRJ298.2.2 - Alterações referente as Sprints 01 e 02
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C32831

---
+++ 04/PJ298.2.2_1_alteracao_mascara_contrato_crapprm.sql
@@ -0,0 +1,7 @@
+UPDATE crapprm
+   SET dsvlrprm = 'z.zzz.zzz.zz9'
+ WHERE cdcooper = 0
+   AND nmsistem = 'CRED'
+   AND cdacesso = 'MASK_CONTRATO';
+   
+COMMIT;
---
+++ 04/PJ298.2.2_2_INSERT_DOMINIO.sql
@@ -0,0 +1,10 @@
+insert into tbepr_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
+values ('TPEMPRST', '0', 'Price TR');
+
+insert into tbepr_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
+values ('TPEMPRST', '1', 'Price Pre-Fixado');
+
+insert into tbepr_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
+values ('TPEMPRST', '2', 'Pos-Fixado');
+
+commit;
