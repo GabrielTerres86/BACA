fcdf074aa8e08f471f8a1b6cf23e8c189fa659e0
Petter Rafael Silva <petter.silva@envolti.com.br>
2019-04-23 18:28:52 +0000

    p442 - Ajustar processo de autenticação tela iMPPRE
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C32988

--- 03/P442_3_LIBERA_IMPPRE.sql
+++ 03/P442_3_LIBERA_IMPPRE.sql
@@ -21,7 +21,7 @@ BEGIN
                          ,idambtel)
          VALUES('IMPPRE'
                 ,6
-                ,'I,M,E,D'
+                ,'I,L,E,D'
                 ,'CRIACAO E IMPORTACAO DE CARGAS MANUAIS'
                 ,'CRI/IMP DE CARGAS MANU.'
                 ,0
@@ -43,23 +43,48 @@ BEGIN
                        ,ope.cdoperad 
                    FROM crapope ope 
                  WHERE ope.cdsitope = 1) LOOP
+BEGIN
+    DELETE crapace
+    WHERE cdcooper = rw_ope.cdcooper
+      AND cddopcao = 'M'
+      AND UPPER(nmdatela) = UPPER('IMPPRE');
+  EXCEPTION
+      WHEN OTHERS THEN
+        NULL;
+  END;
+  
     BEGIN
       IF rw_ope.cdcooper = 3 THEN
         INSERT INTO crapace (NMDATELA, CDDOPCAO, CDOPERAD, CDCOOPER, NRMODULO, IDEVENTO, IDAMBACE)
         VALUES ('IMPPRE', 'L', rw_ope.cdoperad, rw_ope.cdcooper, 1, 0, 2);
       END IF;
-
-        INSERT INTO crapace (NMDATELA, CDDOPCAO, CDOPERAD, CDCOOPER, NRMODULO, IDEVENTO, IDAMBACE)
-        VALUES ('IMPPRE', 'I', rw_ope.cdoperad, rw_ope.cdcooper, 1, 0, 2);
-
-        INSERT INTO crapace (NMDATELA, CDDOPCAO, CDOPERAD, CDCOOPER, NRMODULO, IDEVENTO, IDAMBACE)
-        VALUES ('IMPPRE', 'D', rw_ope.cdoperad, rw_ope.cdcooper, 1, 0, 2);
-        
-        INSERT INTO crapace (NMDATELA, CDDOPCAO, CDOPERAD, CDCOOPER, NRMODULO, IDEVENTO, IDAMBACE)
-        VALUES ('IMPPRE', 'E', rw_ope.cdoperad, rw_ope.cdcooper, 1, 0, 2);
     EXCEPTION
       WHEN OTHERS THEN
-        CONTINUE;
+        NULL;
+    END;
+
+    BEGIN
+      INSERT INTO crapace (NMDATELA, CDDOPCAO, CDOPERAD, CDCOOPER, NRMODULO, IDEVENTO, IDAMBACE)
+      VALUES ('IMPPRE', 'I', rw_ope.cdoperad, rw_ope.cdcooper, 1, 0, 2);
+    EXCEPTION
+      WHEN OTHERS THEN
+        NULL;
+    END;
+  
+    BEGIN
+      INSERT INTO crapace (NMDATELA, CDDOPCAO, CDOPERAD, CDCOOPER, NRMODULO, IDEVENTO, IDAMBACE)
+      VALUES ('IMPPRE', 'D', rw_ope.cdoperad, rw_ope.cdcooper, 1, 0, 2);
+    EXCEPTION
+      WHEN OTHERS THEN
+        NULL;
+    END;
+  
+    BEGIN
+      INSERT INTO crapace (NMDATELA, CDDOPCAO, CDOPERAD, CDCOOPER, NRMODULO, IDEVENTO, IDAMBACE)
+      VALUES ('IMPPRE', 'E', rw_ope.cdoperad, rw_ope.cdcooper, 1, 0, 2);
+    EXCEPTION
+      WHEN OTHERS THEN
+        NULL;
     END;
   END LOOP;
   
