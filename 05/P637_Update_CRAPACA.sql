18e0412b299f15dd0eee694c13a1e2b671db0de5
Rafael Faria (Supero) <supero-rafael.faria@ailos.coop.br>
2019-04-24 17:37:05 +0000

    637 - Ajuste de baca
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C33035

--- 05/P637_Update_CRAPACA.sql
+++ 05/P637_Update_CRAPACA.sql
@@ -1,12 +1,12 @@
 begin
 
-update crapaca set lstparam = 'pr_cdorigem,pr_dsprotoc,pr_cdcooper,pr_nrdconta,pr_nrctremp,pr_nrtransa,pr_dsresana,pr_indrisco,pr_nrnotrat,pr_nrinfcad,pr_nrliquid,pr_nrgarope,pr_nrparlvr,pr_nrperger,pr_desscore,pr_datscore,pr_flgpreap,pr_dsrequis,pr_namehost,pr_idfluata'
-where nmdeacao = 'PROCESSA_ANALISE'
-and nmpackag = 'EMPR0012';
+update crapaca set lstparam = lstparam || ',pr_idfluata'
+where upper(nmdeacao) = 'PROCESSA_ANALISE'
+and upper(nmpackag) = 'EMPR0012';
 
-update crapaca set lstparam = 'pr_cdorigem,pr_dsprotoc,pr_nrtransa,pr_dsresana,pr_indrisco,pr_nrnotrat,pr_nrinfcad,pr_nrliquid,pr_nrgarope,pr_inopeatr,pr_nrparlvr,pr_nrperger,pr_desscore,pr_datscore,pr_dsrequis,pr_namehost,pr_idfluata'
-where nmdeacao = 'WEBS0001_ANALISE_MOTOR'
-and nmpackag = 'WEBS0001'; 
+update crapaca set lstparam = lstparam || ',pr_idfluata'
+where upper(nmdeacao) = 'WEBS0001_ANALISE_MOTOR'
+and upper(nmpackag) = 'WEBS0001'; 
 
 commit;
 
