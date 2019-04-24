d5141a40f37d15ca2d16dfedf0c57ec3abdcabd1
Marcelo Coelho (Mouts) <mouts-marcelo.coelho@ailos.coop.br>
2019-04-24 12:49:26 +0000

    Acerto Prejuízo - 24/04
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C33011

--- 04/Baca Acerto Prejuízo 23-04.sql
+++ 04/Baca Acerto Prejuízo 23-04.sql
@@ -1,5 +1,5 @@
 BEGIN
--- =================================================================================================
+-- ================================================================================================= 1
 DECLARE
 /* o script foi montado com base na PC_CRPS002 - PCVERIFICA_CONTA_PREJUIZO */
   pr_cdcooper   craplcm.cdcooper%TYPE := 1;
@@ -89,7 +89,7 @@ DECLARE
     pr_dscritic := 'Erro ao atualizar CRAPSLD = '||SQLERRM;
     RAISE_APPLICATION_ERROR(-20510,pr_dscritic);
   end;
--- =================================================================================================
+-- ================================================================================================= 2
 begin
 
   -- Conta 8811539 / 1-Viacredi
@@ -104,7 +104,7 @@ begin
 
   COMMIT;
 end;
--- =================================================================================================
+-- ================================================================================================= 3
 begin
 
   -- Conta 2699249 / 1-Viacredi
@@ -119,7 +119,7 @@ begin
 
   COMMIT;
 end;
--- =================================================================================================
+-- ================================================================================================= 4
 begin
 
   -- Conta 7769946 / 1-Viacredi
@@ -134,7 +134,7 @@ begin
 
   COMMIT;
 end;
--- =================================================================================================
+-- ================================================================================================= 5
 DECLARE
   pr_cdcooper   craplcm.cdcooper%TYPE := 1;
   pr_nrdconta   craplcm.nrdconta%TYPE := 80287549;
@@ -186,7 +186,7 @@ WHEN OTHERS THEN
   rollback;
   RAISE_APPLICATION_ERROR(-20510,pr_dscritic);
 end;
--- =================================================================================================
+-- ================================================================================================= 6
 DECLARE
   pr_cdcooper   craplcm.cdcooper%TYPE := 1;
   pr_nrdconta   craplcm.nrdconta%TYPE := 2699249;
@@ -249,5 +249,103 @@ WHEN OTHERS THEN
   rollback;
   RAISE_APPLICATION_ERROR(-20510,pr_dscritic);
 end;
--- =================================================================================================
+-- ================================================================================================= 7
+DECLARE
+  pr_cdcooper   craplcm.cdcooper%TYPE := 1;
+  pr_nrdconta   craplcm.nrdconta%TYPE := 8179034;
+  pr_vllanmto   craplcm.vllanmto%TYPE := .9;
+  pr_dtmvtolt   craplcm.dtmvtolt%TYPE;
+--  pr_cdhistor IN  craplcm.cdhistor%TYPE
+  pr_nrdocmto   craplcm.nrdocmto%type := 1;
+  pr_cdagenci   craplcm.cdagenci%type := 1;
+  pr_cdbccxlt   craplcm.cdbccxlt%type := 100;
+--  pr_nrdctabb IN  craplcm.nrdctabb%TYPE
+  pr_cdcritic   NUMBER;
+  pr_dscritic   VARCHAR2(10000);
+  --
+  vr_dthrtran   DATE := SYSDATE;
+  vr_idprejuizo tbcc_prejuizo.idprejuizo%TYPE;
+  --
+  vr_exc_saida  EXCEPTION;
+BEGIN
+  BEGIN
+    SELECT dtmvtolt
+      INTO pr_dtmvtolt
+      FROM crapdat
+     WHERE cdcooper = pr_cdcooper;
+  EXCEPTION
+  WHEN OTHERS THEN
+    pr_dscritic := 'Erro ao buscar DTMVTOLT = '||SQLERRM;
+    RAISE vr_exc_saida;
+  END;
+  --
+  UPDATE tbcc_prejuizo a
+     SET vlsdprej = nvl(vlsdprej, 0) + pr_vllanmto
+   WHERE cdcooper = pr_cdcooper
+     AND nrdconta = pr_nrdconta
+  RETURNING a.idprejuizo INTO vr_idprejuizo;
+  --
+  -- Lança débito referente ao pagamento do prejuízo
+  PREJ0003.pc_gera_lcto_extrato_prj(pr_cdcooper => pr_cdcooper
+                          , pr_nrdconta => pr_nrdconta
+                          , pr_dtmvtolt => pr_dtmvtolt
+                          , pr_cdhistor => 2722
+                          , pr_idprejuizo => vr_idprejuizo
+                          , pr_vllanmto => pr_vllanmto
+                          , pr_dthrtran => vr_dthrtran
+                          , pr_cdcritic => pr_cdcritic
+                          , pr_dscritic => pr_dscritic);
+  IF NVL(pr_cdcritic,0) > 0
+  OR pr_dscritic IS NOT NULL
+  THEN
+    RAISE vr_exc_saida;
+  END IF;
+  -- Lança crédito referente ao pagamento do prejuízo
+  PREJ0003.pc_gera_lcto_extrato_prj(pr_cdcooper => pr_cdcooper
+                          , pr_nrdconta => pr_nrdconta
+                          , pr_dtmvtolt => pr_dtmvtolt
+                          , pr_cdhistor => 2732
+                          , pr_idprejuizo => vr_idprejuizo
+                          , pr_vllanmto => pr_vllanmto
+                          , pr_dthrtran => vr_dthrtran
+                          , pr_cdcritic => pr_cdcritic
+                          , pr_dscritic => pr_dscritic);
+  IF NVL(pr_cdcritic,0) > 0
+  OR pr_dscritic IS NOT NULL
+  THEN
+    RAISE vr_exc_saida;
+  END IF;
+  -- Valor pago do saldo devedor principal do prejuízo
+  PREJ0003.pc_gera_lcto_extrato_prj(pr_cdcooper => pr_cdcooper
+                         , pr_nrdconta => pr_nrdconta
+                         , pr_dtmvtolt => pr_dtmvtolt
+                         , pr_cdhistor => 2726
+                         , pr_idprejuizo => vr_idprejuizo
+                         , pr_vllanmto => pr_vllanmto
+                         , pr_dthrtran => vr_dthrtran
+                         , pr_cdcritic => pr_cdcritic
+                         , pr_dscritic => pr_dscritic);
+  IF NVL(pr_cdcritic,0) > 0
+  OR pr_dscritic IS NOT NULL
+  THEN
+    RAISE vr_exc_saida;
+  END IF;
+  --
+  COMMIT;
+EXCEPTION
+WHEN vr_exc_saida THEN
+  if pr_cdcritic is not null and pr_dscritic is null then
+    pr_dscritic := gene0001.fn_busca_critica(pr_cdcritic);
+  end if;
+  rollback;
+  RAISE_APPLICATION_ERROR(-20500,pr_dscritic);
+WHEN OTHERS THEN
+  pr_dscritic := 'Erro ao atualizar CRAPSLD = '||SQLERRM;
+  RAISE_APPLICATION_ERROR(-20510,pr_dscritic);
+end;
+-- ================================================================================================= 8
+-- ================================================================================================= 9
+-- ================================================================================================= 10
+-- ================================================================================================= 11
+-- ================================================================================================= 12
 END;
