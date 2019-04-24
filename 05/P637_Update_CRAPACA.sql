508de9bd3201cb976327531bc54386f1138c56a3
Rafael Faria (Supero) <supero-rafael.faria@ailos.coop.br>
2019-04-24 01:01:39 +0000

    PJ637 - Ajuste de ACA
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C33007

--- 05/P637_Update_CRAPACA.sql
+++ 05/P637_Update_CRAPACA.sql
@@ -1,12 +1,14 @@
+begin
+
 update crapaca set lstparam = 'pr_cdorigem,pr_dsprotoc,pr_cdcooper,pr_nrdconta,pr_nrctremp,pr_nrtransa,pr_dsresana,pr_indrisco,pr_nrnotrat,pr_nrinfcad,pr_nrliquid,pr_nrgarope,pr_nrparlvr,pr_nrperger,pr_desscore,pr_datscore,pr_flgpreap,pr_dsrequis,pr_namehost,pr_idfluata'
-where nrseqaca = 2636
-and nmdeacao = 'PROCESSA_ANALISE'
+where nmdeacao = 'PROCESSA_ANALISE'
 and nmpackag = 'EMPR0012';
 
 update crapaca set lstparam = 'pr_cdorigem,pr_dsprotoc,pr_nrtransa,pr_dsresana,pr_indrisco,pr_nrnotrat,pr_nrinfcad,pr_nrliquid,pr_nrgarope,pr_inopeatr,pr_nrparlvr,pr_nrperger,pr_desscore,pr_datscore,pr_dsrequis,pr_namehost,pr_idfluata'
-where nrseqaca = 1630
-and nmdeacao = 'WEBS0001_ANALISE_MOTOR'
+where nmdeacao = 'WEBS0001_ANALISE_MOTOR'
 and nmpackag = 'WEBS0001'; 
 
+commit;
 
-  
+end;
+
