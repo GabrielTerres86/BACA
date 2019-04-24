5d48dc534e4236f884a140a9d1206b3fb16c5693
Dioni Francisco (Supero) <CECRED\t0032450>
2019-04-24 14:28:19 +0000

    Adiciona begin end e commit no script
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C33023

--- 05/P559_CRAPACA_atualiza_nome_procedimento_busca_de_telefone.sql
+++ 05/P559_CRAPACA_atualiza_nome_procedimento_busca_de_telefone.sql
@@ -1,1 +1,4 @@
-update crapaca set nmproced = 'pc_buscar_telefone_web' where nmdeacao = 'BUSCAR_TELEFONE'
+begin
+update crapaca set nmproced = 'pc_buscar_telefone_web' where nmdeacao = 'BUSCAR_TELEFONE';
+commit;
+end;
