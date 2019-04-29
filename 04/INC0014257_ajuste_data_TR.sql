fa64fa1f174e9bef6cddb37a6b7ebcda755963b7
Fabricio Weitgenant <fabricio@ailos.coop.br>
2019-04-29 17:21:18 +0000

    29/04/2019 - Ajuste do campo data de pagto de contratos TR - INC0014257. (Fabrício)
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C33159

---
+++ 04/INC0014257_ajuste_data_TR.sql
@@ -0,0 +1,13 @@
+update crapepr e set e.dtdpagto = to_date('02/01/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta =1708368 and  nrctremp = 598880;
+update crapepr e set e.dtdpagto = to_date('25/04/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta =1900528 and  nrctremp = 777054;
+update crapepr e set e.dtdpagto = to_date('15/05/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta =2490447 and  nrctremp = 150139;
+update crapepr e set e.dtdpagto = to_date('10/05/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta =6637914 and  nrctremp = 992383;
+update crapepr e set e.dtdpagto = to_date('10/05/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta =6816029 and  nrctremp = 982404;
+update crapepr e set e.dtdpagto = to_date('15/05/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta =6800173 and  nrctremp = 840099;
+update crapepr e set e.dtdpagto = to_date('10/05/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta =915092 and  nrctremp = 725071;
+update crapepr e set e.dtdpagto = to_date('10/05/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta =915092 and  nrctremp = 725079;
+update crapepr e set e.dtdpagto = to_date('10/05/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta =6571077 and  nrctremp = 766702;
+update crapepr e set e.dtdpagto = to_date('20/05/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta =7665318 and  nrctremp = 621280;
+update crapepr e set e.dtdpagto = to_date('20/01/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta =7504101 and  nrctremp = 594635;
+
+COMMIT;
