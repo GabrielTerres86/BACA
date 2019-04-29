d318142ad418605e48f3b35c79ecdfefd3e77ccb
Jaison Fernando Ferreira <jaison.ferreira@ailos.coop.br>
2019-04-29 19:39:35 +0000

    baca de ajuste
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C33183

---
+++ 04/Baca_Acerto_Prejuizo_2604.sql
@@ -0,0 +1,121 @@
+BEGIN
+-- ================================================================================================= 1
+BEGIN
+  UPDATE crapsld a
+     SET a.dtrisclq = '16/07/2018'
+   WHERE cdcooper = 1
+     AND nrdconta = 6429904;
+  COMMIT;
+END;
+-- ================================================================================================= 2
+  BEGIN
+    UPDATE tbcc_prejuizo
+       SET VLSDPREJ = 0
+     WHERE cdcooper = 1
+       AND nrdconta = 8844240;
+  EXCEPTION
+  WHEN OTHERS THEN
+    RAISE_APPLICATION_ERROR(20501,'Erro no acerto da conta = 8844240 '||sqlerrm);
+  END;
+-- ================================================================================================= 3
+DECLARE
+/* o script foi montado com base na PC_CRPS002 - PCVERIFICA_CONTA_PREJUIZO */
+  pr_cdcooper   craplcm.cdcooper%TYPE := 1;
+  pr_nrdconta   craplcm.nrdconta%TYPE := 2699249;
+  pr_dtmvtolt   craplcm.dtmvtolt%TYPE;
+  pr_vllanmto   craplcm.vllanmto%TYPE := 3388.47;
+--  pr_cdhistor IN  craplcm.cdhistor%TYPE
+  pr_nrdocmto   craplcm.nrdocmto%type := 1;
+  pr_cdagenci   craplcm.cdagenci%type := 1;
+  pr_cdbccxlt   craplcm.cdbccxlt%type := 100;
+  pr_cdoperad   craplcm.cdoperad%TYPE := 1;
+--  pr_nrdctabb IN  craplcm.nrdctabb%TYPE
+  pr_cdcritic   NUMBER;
+  pr_dscritic   VARCHAR2(10000);
+
+  vr_nrseqdig       craplot.nrseqdig%TYPE;   -- Aramazena o valor do campo "nrseqdig" da CRAPLOT para referência na CRAPLCM
+  vr_tab_retorno    lanc0001.typ_reg_retorno;
+  vr_incrineg       INTEGER;
+  vr_exc_saida      EXCEPTION;
+
+  begin
+    BEGIN
+      SELECT dtmvtolt
+        INTO pr_dtmvtolt
+        FROM crapdat
+       WHERE cdcooper = pr_cdcooper;
+    EXCEPTION
+    WHEN OTHERS THEN
+      pr_dscritic := 'Erro ao buscar DTMVTOLT = '||SQLERRM;
+    END;
+    --
+--    if prej0003.fn_verifica_preju_conta(pr_cdcooper => pr_cdcooper, pr_nrdconta => pr_nrdconta) then
+
+        vr_nrseqdig := FN_SEQUENCE(pr_nmtabela => 'CRAPLOT'
+                                  ,pr_nmdcampo => 'NRSEQDIG'
+                                  ,pr_dsdchave => to_char(pr_cdcooper)||';'||
+                                  to_char(pr_dtmvtolt, 'DD/MM/RRRR')||';'||
+                                  '1;100;650009');
+
+        -- Efetua débito do valor que será transferido para a Conta Transitória (créditos bloqueados por prejuízo em conta)
+        lanc0001.pc_gerar_lancamento_conta(
+           pr_dtmvtolt => pr_dtmvtolt,
+           pr_cdagenci => pr_cdagenci,
+           pr_cdbccxlt => pr_cdbccxlt,
+           pr_nrdolote => 650009,
+           pr_nrdconta => pr_nrdconta,
+           pr_nrdocmto => pr_nrdocmto,
+           pr_cdhistor => 2719,
+           pr_nrseqdig => vr_nrseqdig,
+           pr_vllanmto => pr_vllanmto,
+           pr_nrdctabb => pr_nrdconta,
+           pr_cdpesqbb => 'ESTORNO DE CREDITO RECEBIDO EM C/C EM PREJUIZO',
+           pr_dtrefere => pr_dtmvtolt,
+           pr_hrtransa => gene0002.fn_busca_time,
+           pr_cdoperad => 1,
+           pr_cdcooper => pr_cdcooper,
+           pr_cdorigem => 5,
+           pr_tab_retorno => vr_tab_retorno,
+           pr_incrineg => vr_incrineg,
+           pr_cdcritic => pr_cdcritic,
+           pr_dscritic => pr_dscritic
+        );
+
+        if (nvl(pr_cdcritic,0) <> 0 or trim(pr_dscritic) is not null) then
+           RAISE vr_exc_saida;
+        end if;
+
+        PREJ0003.pc_gera_cred_cta_prj(pr_cdcooper => pr_cdcooper
+                                    , pr_nrdconta => pr_nrdconta
+                                    , pr_cdoperad => pr_cdoperad
+                                    , pr_vlrlanc =>  pr_vllanmto
+                                    , pr_dtmvtolt => pr_dtmvtolt
+                                    , pr_cdcritic => pr_cdcritic
+                                    , pr_dscritic => pr_dscritic);
+        if (nvl(pr_cdcritic,0) <> 0 or trim(pr_dscritic) is not null) then
+          RAISE vr_exc_saida;
+        end if;
+--    end if;
+    --
+    COMMIT;
+  EXCEPTION
+  WHEN vr_exc_saida THEN
+    if pr_cdcritic is not null and pr_dscritic is null then
+      pr_dscritic := gene0001.fn_busca_critica(pr_cdcritic);
+    end if;
+    rollback;
+    RAISE_APPLICATION_ERROR(-20500,pr_dscritic);
+  WHEN OTHERS THEN
+    pr_dscritic := 'Erro ao atualizar CRAPSLD = '||SQLERRM;
+    RAISE_APPLICATION_ERROR(-20510,pr_dscritic);
+  end;
+-- ================================================================================================= 4
+-- ================================================================================================= 5
+-- ================================================================================================= 6
+-- ================================================================================================= 7
+-- ================================================================================================= 8
+-- ================================================================================================= 9
+-- ================================================================================================= 10
+-- ================================================================================================= 11
+-- ================================================================================================= 12
+END;
