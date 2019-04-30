c854efd5aa9c4f32761170e541cd0045cc7373bb
Fernanda K. de Oliveira (AMCOM) <fernanda.oliveira@amcom.com.br>
2019-04-30 17:40:11 +0000

    P437.Consignado - Criação da Sequence e trigger da tabela TBEPR_CONSIGNADO_DEBCONVEN
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/P437 - Consignado2/DEV;C33237

--- BACA/18 CONSIGNADO - create table tbepr_consignado_deb_conveniad.sql
+++ BACA/18 CONSIGNADO - create table tbepr_consignado_deb_conveniad.sql
@@ -1,6 +1,6 @@
 -- Create table
-create table TBEPR_CONSIGNADO_DEB_CONVENIAD
-( IDEPR_CONSIGNADO_DEB_CONVENIAD NUMBER NOT NULL,
+create table TBEPR_CONSIGNADO_DEBCONVEN
+( IDSEQUENCIA  NUMBER NOT NULL,
   CDCOOPER     NUMBER(10)   NOT NULL,
   CDEMPRES     NUMBER(10)   NOT NULL,
   IDPAGTO      NUMBER       NOT NULL,
@@ -11,19 +11,47 @@ create table TBEPR_CONSIGNADO_DEB_CONVENIAD
   DTUPDREG     DATE);
 
 -- Add comments to the table 
-comment on table TBEPR_CONSIGNADO_DEB_CONVENIAD is 'Tabela que armazena as informações do consignado enviados pela FIS Brasil para débito da conveniada.';
+comment on table TBEPR_CONSIGNADO_DEBCONVEN is 'Tabela de controle de debito da conveiada.';
 -- Add comments to the columns 
-comment on column TBEPR_CONSIGNADO_DEB_CONVENIAD.IDEPR_CONSIGNADO_DEB_CONVENIAD  is 'Número sequencial da tabela';
-comment on column TBEPR_CONSIGNADO_DEB_CONVENIAD.CDCOOPER is 'Codigo da Cooperativa';
-comment on column TBEPR_CONSIGNADO_DEB_CONVENIAD.CDEMPRES is 'Codigo da empresa';
-comment on column TBEPR_CONSIGNADO_DEB_CONVENIAD.IDPAGTO  is 'Identificador de pagamento da FIS';
-comment on column TBEPR_CONSIGNADO_DEB_CONVENIAD.VRDEBITO is 'Valor do débito da conveniada';
-comment on column TBEPR_CONSIGNADO_DEB_CONVENIAD.INSTATUS is 'Indicador de processamento';
-comment on column TBEPR_CONSIGNADO_DEB_CONVENIAD.CDOPERAD is 'Codigo do Operador';
-comment on column TBEPR_CONSIGNADO_DEB_CONVENIAD.DTINCREG is 'Data de inclusão do registro';
-comment on column TBEPR_CONSIGNADO_DEB_CONVENIAD.DTUPDREG is 'Data de alteração do registro';
+comment on column TBEPR_CONSIGNADO_DEBCONVEN.IDSEQUENCIA  is 'Numero sequencial da tabela';
+comment on column TBEPR_CONSIGNADO_DEBCONVEN.CDCOOPER is 'Codigo da Cooperativa';
+comment on column TBEPR_CONSIGNADO_DEBCONVEN.CDEMPRES is 'Codigo da empresa';
+comment on column TBEPR_CONSIGNADO_DEBCONVEN.IDPAGTO  is 'Identificador de pagamento da FIS';
+comment on column TBEPR_CONSIGNADO_DEBCONVEN.VRDEBITO is 'Valor do debito da conveniada';
+comment on column TBEPR_CONSIGNADO_DEBCONVEN.INSTATUS is 'Indicador de processamento';
+comment on column TBEPR_CONSIGNADO_DEBCONVEN.CDOPERAD is 'Codigo do Operador';
+comment on column TBEPR_CONSIGNADO_DEBCONVEN.DTINCREG is 'Data de inclusao do registro';
+comment on column TBEPR_CONSIGNADO_DEBCONVEN.DTUPDREG is 'Data de alteracao do registro';
 
 -- Create/Recreate primary, unique and foreign key constraints 
-alter table TBEPR_CONSIGNADO_DEB_CONVENIAD
-  add constraint TBEPR_CONSIGNADO_DEB_CONVENIAD_PK primary key (IDEPR_CONSIGNADO_DEB_CONVENIAD);
+alter table TBEPR_CONSIGNADO_DEBCONVEN
+  add constraint TBEPR_CONSIGNADO_DEBCONVEN_PK primary key (IDSEQUENCIA);
+  
+-- Create sequence 
+create sequence CECRED.TBEPR_CONSIG_DEBCONV_SEQ
+minvalue 1
+maxvalue 9999999999999999999999999999
+start with 1
+increment by 1
+nocache
+order;
+
+-- Create Trigger
+CREATE OR REPLACE TRIGGER CECRED.TBEPR_CONSIGNADO_DEBCONVEN
+BEFORE INSERT ON TBEPR_CONSIGNADO_DEBCONVEN
+
+FOR EACH ROW
+
+BEGIN
+  
+  IF :NEW.IDSEQUENCIA IS NULL THEN
+    :NEW.IDSEQUENCIA := TBEPR_CONSIG_DEBCONV_SEQ.NEXTVAL;
+  END IF;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    RAISE_APPLICATION_ERROR(-20100,'ERRO GERACAO SEQUENCE IDSEQUENCIA - TABELA TBEPR_CONSIGNADO_DEBCONVEN!');
+END;  
+  
+  
  
