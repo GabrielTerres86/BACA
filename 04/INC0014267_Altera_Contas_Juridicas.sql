00c1bb0b69f592d02c14773bb5746fe6e2ba8ab3
Jackson Rosa <jackson.rosa@ailos.coop.br>
2019-04-30 15:34:05 +0000

    Setar campo INPESSOA para tipo 2-Administrativo. Jackson - INC0014267
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C33224

---
+++ 04/INC0014267_Altera_Contas_Juridicas.sql
@@ -0,0 +1,66 @@
+/* 
+Solicitação: INC0014267
+Objetivo   : Setar campo INPESSOA para tipo 2-Administrativo.
+Autor      : Jackson
+*/
+
+update CRAPASS set INPESSOA = 2 where cdcooper = 1 and nrdconta = 8579997;
+update CRAPASS set INPESSOA = 2 where cdcooper = 1 and nrdconta = 10385983;
+update CRAPASS set INPESSOA = 2 where cdcooper = 1 and nrdconta = 10389342;
+update CRAPASS set INPESSOA = 2 where cdcooper = 1 and nrdconta = 10389393;
+update CRAPASS set INPESSOA = 2 where cdcooper = 2 and nrdconta = 850012;
+update CRAPASS set INPESSOA = 2 where cdcooper = 2 and nrdconta = 850020;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 19;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 27;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 43;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 51;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 60;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 78;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 86;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 94;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 108;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 132;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 159;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 264;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 272;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 280;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 299;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 302;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 310;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 329;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 337;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 345;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 353;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 361;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 370;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 388;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 434;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 10014;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 10022;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 10049;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 10057;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 10065;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 10073;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 10081;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 10090;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 10103;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 10111;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 10120;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 10138;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 10154;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 100013;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 1010000;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 1020005;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 1040006;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 1050001;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 1060007;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 1070002;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 1080008;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 1090003;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 1100009;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 1110004;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 1120000;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 1130005;
+update CRAPASS set INPESSOA = 2 where cdcooper = 3 and nrdconta = 1150006;
+  
+COMMIT;
