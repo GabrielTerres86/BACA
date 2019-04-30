21169477f70523cc917945e4e93a2d38f6b03e61
Jackson Rosa <jackson.rosa@ailos.coop.br>
2019-04-30 17:59:31 +0000

    Efetua a alteração na tabela CRAPRPP (Cadastro de poupanca programada) do produtos que atualmente estão como suspensos. INC0011570 - Jackson
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C33245

--- 05/INC0011570_altera_situacao_poup_progr.sql
+++ 05/INC0011570_altera_situacao_poup_progr.sql
@@ -5,16 +5,17 @@ Objetivo   : Efetua a alteração na tabela CRAPRPP (Cadastro de poupanca programa
              Esta alteração de dará para:
                 Cooperativa 1 e Conta 8813850 (progress_recid = 475413)
                 Cooperativa 2 e Conta 473162 (progress_recid = 502289)
+                Cooperativa 16 e Conta 264938 (progress_recid = 480610)
 Autor      : Jackson
 */
 UPDATE craprpp
    SET cdsitrpp = 3 /*3=Cancelado*/
- WHERE progress_recid in (475413, 502289);
+ WHERE progress_recid in (475413, 502289, 480610);
 COMMIT;
 /*
 --Efetua o Rollback, voltando a Situação para o tipo 2-Suspenso
 UPDATE craprpp
    SET cdsitrpp = 2 
- WHERE progress_recid in (475413, 502289);
+ WHERE progress_recid in (475413, 502289, 480610);
 COMMIT;
 */
