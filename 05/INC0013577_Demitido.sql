fe85d06d24bc82835245fcd974b2c143dcd2f1b8
Jackson Rosa <jackson.rosa@ailos.coop.br>
2019-04-30 19:17:56 +0000

    Setar campo CRAPASS.CDSITDCT para tipo 4-encerrada por demissao na empresa. O cooperado já possui da de demissão mas continua com situação 7-Processo de Demissão INC0013577 - Jackson
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C33265

---
+++ 05/INC0013577_Demitido.sql
@@ -0,0 +1,21 @@
+/* 
+Solicitação: INC0013577
+Objetivo   : Setar campo CRAPASS.CDSITDCT para tipo 4-encerrada por demissao na empresa.
+             O cooperado já possui da de demissão mas continua com 
+             situação 7-Processo de Demissão
+Autor      : Jackson
+*/
+
+UPDATE CRAPASS SET CDSITDCT = 4 WHERE PROGRESS_RECID = 862364;
+COMMIT;
+
+/* 
+SCRIPT DE ROLLBACK
+Objetivo   : Volta o registro de cooperado para a situação: 7-Processo de Demissão
+             Descomentar os códigos abaixo, caso necessite de rollback
+*/
+/*
+UPDATE CRAPASS SET CDSITDCT = 7 WHERE PROGRESS_RECID = 862364;
+COMMIT;
+*/
+
