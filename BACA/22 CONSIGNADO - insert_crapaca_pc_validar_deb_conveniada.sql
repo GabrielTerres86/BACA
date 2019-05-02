1eabd681d9031a74a2f5c4bd89f82b45b3923311
Fernanda K. de Oliveira (AMCOM) <fernanda.oliveira@amcom.com.br>
2019-05-02 19:59:05 +0000

    P437.Consignado - Insert CRAPACA - Ação VALIDA_DTPGTO_ANTECIP, Tela ATENDA Rotina EMPR0020
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/P437 - Consignado2/DEV;C33329

--- BACA/22 CONSIGNADO - insert_crapaca_pc_validar_deb_conveniada.sql
+++ BACA/22 CONSIGNADO - insert_crapaca_pc_validar_deb_conveniada.sql
@@ -1,21 +1,16 @@
-DECLARE
-  
-  aux_nrseqrdr number := null;
-  aux_nrseqaca number := null;
-  
-BEGIN
-  
+DECLARE  
+  aux_nrseqaca number := null;  
+BEGIN  
    begin 
      select c.nrseqaca
        into aux_nrseqaca
        from crapaca c
       where c.nrseqrdr = (SELECT nrseqrdr FROM craprdr WHERE nmprogra = 'ATENDA')
         and c.nmdeacao = 'VALIDA_DTPGTO_ANTECIP'
-        and c.nmpackag = 'EMPR0019'
+        and c.nmpackag = 'EMPR0020'
         and c.nmproced =  'pc_validar_dtpgto_antecipada';
    exception
-     when no_data_found then
-      if aux_nrseqrdr is not null then    
+     when no_data_found then       
         insert into cecred.crapaca
           (nrseqaca, 
            nmdeacao, 
@@ -26,10 +21,10 @@ BEGIN
         values
           (SEQACA_NRSEQACA.NEXTVAL, 
            'VALIDA_DTPGTO_ANTECIP', 
-           'EMPR0019', 
+           'EMPR0020', 
            'pc_validar_dtpgto_antecipada', 
            '', 
           (SELECT nrseqrdr FROM craprdr WHERE nmprogra = 'ATENDA'));
-     end if;   
+        commit;     
    end;  
 END; 
