6ec3b6c8aa094ddcd5d066a08cf9927b1e1822d9
Cassia de Oliveira (GFT) <Cassia.Oliveira@gft.com>
2019-05-08 18:12:04 +0000

    508 - correção script inserção
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C33524

--- 05/508 - adiciona campos de confirmacao.sql
+++ 05/508 - adiciona campos de confirmacao.sql
@@ -2,16 +2,22 @@ BEGIN
   /************/
   /* ENDEREÇO */
   /************/
-  ALTER TABLE CECRED.TBCADAST_PESSOA_ENDERECO ADD statusrevisão NUMBER(1) DEFAULT(0);
-  COMMENT ON COLUMN CECRED.TBCADAST_PESSOA_ENDERECO.statusrevisão IS 'Status Revisão: 1 - Confirmado, 2 - Não Confirmado';
+  ALTER TABLE CECRED.TBCADAST_PESSOA_ENDERECO ADD statusrevisao NUMBER(1) DEFAULT(0);
+  COMMENT ON COLUMN CECRED.TBCADAST_PESSOA_ENDERECO.statusrevisao IS 'Status Revisão (Dominio:TBCADAST_DOMINIO_CAMPO)';
   
   /**********************/
-	/*  RENDA E EMPRESA   */
-	/**********************/
-	ALTER TABLE CECRED.TBCADAST_PESSOA_RENDA ADD statusrevisao_renda NUMBER(1) DEFAULT(0);
+  /*  RENDA E EMPRESA   */
+  /**********************/
+  ALTER TABLE CECRED.TBCADAST_PESSOA_RENDA ADD statusrevisao_renda NUMBER(1) DEFAULT(0);
   ALTER TABLE CECRED.TBCADAST_PESSOA_RENDA ADD statusrevisao_empresa NUMBER(1) DEFAULT(0);
-  COMMENT ON COLUMN CECRED.TBCADAST_PESSOA_ENDERECO.statusrevisao_renda IS 'Status Revisão Renda: 1 - Confirmado, 2 - Não Confirmado';
-  COMMENT ON COLUMN CECRED.TBCADAST_PESSOA_ENDERECO.statusrevisao_empresa IS 'Status Revisão Empresa: 1 - Confirmado, 2 - Não Confirmado';
+  COMMENT ON COLUMN CECRED.TBCADAST_PESSOA_ENDERECO.statusrevisao_renda IS 'Status Revisão Renda (Dominio:TBCADAST_DOMINIO_CAMPO)';
+  COMMENT ON COLUMN CECRED.TBCADAST_PESSOA_ENDERECO.statusrevisao_empresa IS 'Status Revisão Empresa (Dominio:TBCADAST_DOMINIO_CAMPO)';
+  
+  /************************************/
+  /*  INSERT DOMINIO STATUS REVISÃO   */
+  /************************************/
+  INSERT INTO TBCADAST_DOMINIO_CAMPO (NMDOMINIO, CDDOMINIO, DSCODIGO) VALUES ('TBCADAST_STATUSREVISAO', 1, 'Confirmado');
+  INSERT INTO TBCADAST_DOMINIO_CAMPO (NMDOMINIO, CDDOMINIO, DSCODIGO) VALUES ('TBCADAST_STATUSREVISAO', 2, 'Não confirmado'); 
   
   COMMIT;
 END;
