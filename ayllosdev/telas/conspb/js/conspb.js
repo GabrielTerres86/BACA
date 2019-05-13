d164bbbc13c6088eecc3e6ff49392eda91b626b3
Bruno Katzjarowski (Mouts) <CECRED\t0032121>
2019-05-13 16:20:29 +0000

    PRJ 470 - Adicionar Funções para Internet Explorer - Compatibilidade
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/P475 - Melhorias SPB Contingência/Sprint D2 e E1;C33733

--- ayllosdev/telas/conspb/js/conspb.js
+++ ayllosdev/telas/conspb/js/conspb.js
@@ -13,6 +13,8 @@ var _modalEmailAberto = false;
 
  $(function(){
 
+    addFuncoesIE();
+
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -126,7 +128,7 @@ var _modalEmailAberto = false;
  */
  var resetarOpcaoB = function(){
     $('#periodoDe,#periodoAte').val(_dataAtual); //Atualizar campos de data
-    $('#dsendere').val(_dataAtual); //Atualizar campos de data
+    $('#dsendere').val(""); //Atualizar campos de data
  }
 
  var atribuirEventosCampos = function(){
@@ -334,3 +336,64 @@ var _modalEmailAberto = false;
   * Bloquear fundo atrás da mensagem de erro
   */
  var bloqueiaFundoMsgErro = function(){blockBackground(91);}
+
+ function addFuncoesIE(){
+     if(detectIE()){
+
+        if (!String.prototype.repeat) {
+            String.prototype.repeat = function(count) {
+                'use strict';
+                if (this == null) {
+                    throw new TypeError('can\'t convert ' + this + ' to object');
+                }
+                var str = '' + this;
+                count = +count;
+                if (count != count) {
+                    count = 0;
+                }
+                if (count < 0) {
+                    throw new RangeError('repeat count must be non-negative');
+                }
+                if (count == Infinity) {
+                    throw new RangeError('repeat count must be less than infinity');
+                }
+                count = Math.floor(count);
+                if (str.length == 0 || count == 0) {
+                    return '';
+                }
+                // Ensuring count is a 31-bit integer allows us to heavily optimize the
+                // main part. But anyway, most current (August 2014) browsers can't handle
+                // strings 1 << 28 chars or longer, so:
+                if (str.length * count >= 1 << 28) {
+                    throw new RangeError('repeat count must not overflow maximum string size');
+                }
+                var maxCount = str.length * count;
+                count = Math.floor(Math.log(count) / Math.log(2));
+                while (count) {
+                    str += str;
+                    count--;
+                }
+                str += str.substring(0, maxCount - str.length);
+                return str;
+            }
+        }
+
+        if (!String.prototype.padStart) {
+            String.prototype.padStart = function padStart(targetLength,padString) {
+                targetLength = targetLength>>0; //truncate if number or convert non-number to 0;
+                padString = String((typeof padString !== 'undefined' ? padString : ' '));
+                if (this.length > targetLength) {
+                    return String(this);
+                }
+                else {
+                    targetLength = targetLength-this.length;
+                    if (targetLength > padString.length) {
+                        padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+                    }
+                    return padString.slice(0,targetLength) + String(this);
+                }
+            };
+        }
+
+    } //Fim detectIE();
+ }
