b143b8b9fe60838c9ce27755cb2f3b7c5a905dee
Jackson D. Barcellos (AMCOM) <jackson.barcellos@amcom.com.br>
2019-05-13 17:41:58 +0000

    Product Backlog Item 20914:Integrar Calculo de Simulação - SOA
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/P437 - Consignado2/DEV;C33746

---
+++ BACA/28 CONSIGNADO insert crapaca mensageria para calculo simulacao e log.sql
@@ -0,0 +1,6 @@
+insert into crapaca (NRSEQACA, NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
+values (null, 'SIMULA_BUSCA_DADOS_CALC_FIS', 'TELA_ATENDA_SIMULACAO', 'pc_busca_dados_soa_fis_calcula', '', 1684);
+
+insert into crapaca (NRSEQACA, NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
+values (null, 'LOG_ERRO_SOA_FIS_CALCULA', 'TELA_ATENDA_SIMULACAO', 'prc_log_erro_soa_fis_calcula', '', 1684);
+COMMIT;
