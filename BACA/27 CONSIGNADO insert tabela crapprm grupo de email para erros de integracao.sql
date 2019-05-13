212259ff45b351d1fd8b770b53645b6b0e6176e2
Jackson D. Barcellos (AMCOM) <jackson.barcellos@amcom.com.br>
2019-05-13 17:28:26 +0000

    Product Backlog Item 20902:Procedure Enviar e-mail (erro evento SOA)
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/P437 - Consignado2/DEV;C33742

---
+++ BACA/27 CONSIGNADO insert tabela crapprm grupo de email para erros de integracao.sql
@@ -0,0 +1,3 @@
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 0, 'EMAIL_PAG_ERR_FIS', 'Email para retorno de erros de integração no pagamento com a FIS (CONSIGNDO)', 'consignado.ti@ailos.coop.br');
+commit;
