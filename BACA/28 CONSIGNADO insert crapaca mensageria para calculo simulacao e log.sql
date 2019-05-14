a415385565d6991036905e498b4b054b9f125237
Jackson D. Barcellos (AMCOM) <jackson.barcellos@amcom.com.br>
2019-05-14 19:17:31 +0000

    Ajuste pkg
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/P437 - Consignado2/DEV;C33844

--- BACA/28 CONSIGNADO insert crapaca mensageria para calculo simulacao e log.sql
+++ BACA/28 CONSIGNADO insert crapaca mensageria para calculo simulacao e log.sql
@@ -1,6 +1,14 @@
-insert into crapaca (NRSEQACA, NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
-values (null, 'SIMULA_BUSCA_DADOS_CALC_FIS', 'TELA_ATENDA_SIMULACAO', 'pc_busca_dados_soa_fis_calcula', '', 1684);
 
 insert into crapaca (NRSEQACA, NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
-values (null, 'LOG_ERRO_SOA_FIS_CALCULA', 'TELA_ATENDA_SIMULACAO', 'prc_log_erro_soa_fis_calcula', '', 1684);
+values (null, 'SIM_BUSCA_DADOS_CALC_FIS', 'EMPR0020', 'pc_busca_dados_soa_fis_calcula', '', (select NRSEQRDR from crapRDR where nmprogra = 'TELA_ATENDA_SIMULACAO'));
+
+insert into crapaca (NRSEQACA, NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
+values (null, 'SIM_LOG_ERRO_SOA_FIS_CALCULA', 'EMPR0020', 'prc_log_erro_soa_fis_calcula', '', (select NRSEQRDR from crapRDR where nmprogra = 'TELA_ATENDA_SIMULACAO'));
+
+insert into crapaca (NRSEQACA, NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
+values (null, 'PRO_BUSCA_DADOS_CALC_FIS', 'EMPR0020', 'pc_busca_dados_soa_fis_calcula', '', (select NRSEQRDR from crapRDR where nmprogra = 'TELA_ATENDA_EMPRESTIMO'));
+
+insert into crapaca (NRSEQACA, NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
+values (null, 'PRO_LOG_ERRO_SOA_FIS_CALCULA', 'EMPR0020', 'prc_log_erro_soa_fis_calcula', '', (select NRSEQRDR from crapRDR where nmprogra = 'TELA_ATENDA_EMPRESTIMO'));
+
 COMMIT;
