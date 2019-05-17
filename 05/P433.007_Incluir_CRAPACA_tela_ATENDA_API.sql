6858f2263bf04309448355295577d026a1050447
Rafael Cechet <rafael.cechet@ailos.coop.br>
2019-05-17 19:20:01 +0000

    [P433 - API de Cobranca] Removido inserts duplicados
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C34066

--- 05/P433.007_Incluir_CRAPACA_tela_ATENDA_API.sql
+++ 05/P433.007_Incluir_CRAPACA_tela_ATENDA_API.sql
@@ -1,40 +1,4 @@
 BEGIN
-  INSERT INTO crapaca
-      (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
-  VALUES
-      ('CONSULTA_SERVICOS_COOP'
-      ,'TELA_ATENDA_API'
-      ,'pc_consulta_servicos_coop'
-      ,'pr_nrdconta'
-      ,(SELECT nrseqrdr FROM craprdr WHERE craprdr.nmprogra = 'TELA_ATENDA_API'));
-      
-  INSERT INTO crapaca
-      (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
-  VALUES
-      ('CONSULTA_DESENV_COOP'
-      ,'TELA_ATENDA_API'
-      ,'pc_consulta_desenvolvedor_coop'
-      ,'pr_nrdconta,pr_idservico_api'
-      ,(SELECT nrseqrdr FROM craprdr WHERE craprdr.nmprogra = 'TELA_ATENDA_API'));
-      
-  INSERT INTO crapaca
-      (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
-  VALUES
-      ('CONSULTA_FINALIDADE_COOP'
-      ,'TELA_ATENDA_API'
-      ,'pc_consulta_finalidade_coop'
-      ,'pr_nrdconta,pr_idservico_api'
-      ,(SELECT nrseqrdr FROM craprdr WHERE craprdr.nmprogra = 'TELA_ATENDA_API'));
-
-  INSERT INTO crapaca
-      (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
-  VALUES
-      ('GRAVA_SERVICOS_COOP'
-      ,'TELA_ATENDA_API'
-      ,'pc_grava_servicos_coop'
-      ,'pr_nrdconta,pr_idservico_api,pr_dtadesao,pr_idsituacao_adesao,pr_tp_autorizacao,pr_ls_finalidades,pr_ls_desenvolvedores,pr_cddopcao'
-      ,(SELECT nrseqrdr FROM craprdr WHERE craprdr.nmprogra = 'TELA_ATENDA_API'));
-	  
    INSERT INTO crapaca
       (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
    VALUES
