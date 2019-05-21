20d16922d99d1a59a577fe74bc9de3ef59a11faa
Daniel Zimmermann <daniel.zimmermann@ailos.coop.br>
2019-05-21 13:03:33 +0000

    Ajuste script permissão
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C34150

--- 04/BACA_Permissao_pagamento_prejuizo_manual.sql
+++ 04/BACA_Permissao_pagamento_prejuizo_manual.sql
@@ -1,33 +1,37 @@
- /*---------------------------------------------------------------------------------------------------------------------
-    Programa    : Tela ATENDA->DESCONTOS->TITULOS->BORDEROS - Script de carga
-    Projeto     : 403 - Desconto de Títulos - Release 4
-    Autor       : Lucas Lazari da Silva (GFT)
-    Data        : Abril/2019
-    Objetivo    : Realiza o cadastro das novas funcionalidades da tela ATENDA->DESCONTOS->TÍTULOS->BORDERÔS->PREJUIZO
-  ---------------------------------------------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------------------------------------
+  Programa    : Tela ATENDA->DESCONTOS->TITULOS->BORDEROS - Script de carga
+  Projeto     : 403 - Desconto de Títulos - Release 4
+  Autor       : Lucas Lazari da Silva (GFT)
+  Data        : Abril/2019
+  Objetivo    : Realiza o cadastro das novas funcionalidades da tela ATENDA->DESCONTOS->TÍTULOS->BORDERÔS->PREJUIZO
+---------------------------------------------------------------------------------------------------------------------*/
 
-begin
+BEGIN
 
--- Atualiza as permissões das telas de borderô
-UPDATE craptel 
-   SET cdopptel = concat(cdopptel, ',D,B'), 
-       lsopptel = concat (lsopptel, ',PREJUIZO,ABONO PREJUIZO')
- WHERE nmdatela = 'ATENDA' 
-   AND nmrotina = 'DSC TITS - BORDERO'
-   AND cdcooper NOT IN (7,3)
-   AND cdcooper IN (SELECT cdcooper from crapcop cop where cop.flgativo = 1);
-   
--- Fornece as permissões de acesso dos botões novos para os usuários que já possuem permissão na tela de borderôs
-INSERT INTO crapace
+  -- Atualiza as permissões das telas de borderô
+  /*
+  UPDATE craptel
+     SET cdopptel = concat(cdopptel, ',D,B'),
+         lsopptel = concat(lsopptel, ',PREJUIZO,ABONO PREJUIZO')
+   WHERE nmdatela = 'ATENDA'
+     AND nmrotina = 'DSC TITS - BORDERO'
+     AND cdcooper NOT IN (7, 3)
+     AND cdcooper IN (SELECT cdcooper
+                        FROM crapcop cop
+                       WHERE cop.flgativo = 1);
+                       */
+
+  -- Fornece as permissões de acesso dos botões novos para os usuários que já possuem permissão na tela de borderôs
+  INSERT INTO crapace
     (nmdatela,
      cddopcao,
-     cdoperad,   
-     nmrotina,   
-     cdcooper,   
-     nrmodulo,   
-     idevento,   
+     cdoperad,
+     nmrotina,
+     cdcooper,
+     nrmodulo,
+     idevento,
      idambace)
-    SELECT acn.nmdatela, 
+    SELECT acn.nmdatela,
            'D', -- Prejuizo
            ope.cdoperad,
            acn.nmrotina,
@@ -38,30 +42,43 @@ INSERT INTO crapace
       FROM crapcop cop,
            crapope ope,
            crapace acn
-     WHERE cop.cdcooper NOT IN (7,3) /*14);*/ 
+     WHERE cop.cdcooper NOT IN (7, 3)
        AND cop.flgativo = 1
-       AND ope.cdsitope = 1 
+       AND ope.cdsitope = 1
        AND cop.cdcooper = ope.cdcooper
        AND acn.cdcooper = ope.cdcooper
-       AND trim(upper(acn.cdoperad)) = trim(upper(ope.cdoperad))
+       AND TRIM(upper(acn.cdoperad)) = TRIM(upper(ope.cdoperad))
        AND upper(acn.cddopcao) = 'C' -- copia permissões do botão "consultar"
        AND upper(acn.nmrotina) = 'DSC TITS - BORDERO'
        AND upper(acn.nmdatela) = 'ATENDA'
-       AND acn.idambace = 2;
+       AND acn.idambace = 2
+       AND NOT EXISTS (SELECT 1
+          FROM crapace a
+         WHERE upper(a.nmdatela) = upper(acn.nmdatela)
+           AND upper(a.cddopcao) = 'D' -- Prejuizo
+           AND upper(a.cdoperad) = upper(ope.cdoperad)
+           AND upper(a.nmrotina) = upper(acn.nmrotina)
+           AND a.cdcooper = acn.cdcooper
+         --  AND a.nrmodulo = acn.nrmodulo
+         --  AND a.idevento = acn.idevento
+           AND a.idambace = acn.idambace);
+           
+           
+     --      CDCOOPER, UPPER(NMDATELA), UPPER(NMROTINA), UPPER(CDDOPCAO), UPPER(CDOPERAD), IDAMBACE
 
-commit;
+--  COMMIT;
 
--- Fornece as permissões de acesso dos botões novos para os usuários que já possuem permissão na tela de borderôs
-INSERT INTO crapace
+  -- Fornece as permissões de acesso dos botões novos para os usuários que já possuem permissão na tela de borderôs
+  INSERT INTO crapace
     (nmdatela,
      cddopcao,
-     cdoperad,   
-     nmrotina,   
-     cdcooper,   
-     nrmodulo,   
-     idevento,   
+     cdoperad,
+     nmrotina,
+     cdcooper,
+     nrmodulo,
+     idevento,
      idambace)
-    SELECT acn.nmdatela, 
+    SELECT acn.nmdatela,
            'B', -- Abono Prejuizo
            ope.cdoperad,
            acn.nmrotina,
@@ -72,16 +89,28 @@ INSERT INTO crapace
       FROM crapcop cop,
            crapope ope,
            crapace acn
-     WHERE cop.cdcooper NOT IN (7,3) /*14);*/ 
+     WHERE cop.cdcooper NOT IN (7, 3)
        AND cop.flgativo = 1
-       AND ope.cdsitope = 1 
+       AND ope.cdsitope = 1
        AND cop.cdcooper = ope.cdcooper
        AND acn.cdcooper = ope.cdcooper
-       AND trim(upper(acn.cdoperad)) = trim(upper(ope.cdoperad))
+       AND TRIM(upper(acn.cdoperad)) = TRIM(upper(ope.cdoperad))
        AND upper(acn.cddopcao) = 'A' -- copia permissões de concessão de abono de empréstimos
        AND upper(acn.nmrotina) = 'PRESTACOES'
        AND upper(acn.nmdatela) = 'ATENDA'
-       AND acn.idambace = 2;
+       AND acn.idambace = 2
+       
+       AND NOT EXISTS (SELECT 1
+          FROM crapace a
+         WHERE upper(a.nmdatela) = acn.nmdatela
+           AND upper(a.cddopcao) = 'B' -- Abono Prejuizo
+           AND upper(a.cdoperad) = upper(ope.cdoperad)
+           AND upper(a.nmrotina) = acn.nmrotina
+           AND a.cdcooper = acn.cdcooper
+         --  AND a.nrmodulo = acn.nrmodulo
+         --  AND a.idevento = acn.idevento
+           AND a.idambace = acn.idambace);
+       
 
-commit;
-end;
+  COMMIT;
+END;
