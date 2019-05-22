e78cb631188a3da2eb878ca2b6cba851bddf49ed
Rafael Cechet <rafael.cechet@ailos.coop.br>
2019-05-22 11:36:14 +0000

    [P433 - API de Cobrança] Atualizado local do manual da API
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C34198

--- 05/P433.011_Incluir_Parametro_Manual.sql
+++ 05/P433.011_Incluir_Parametro_Manual.sql
@@ -1,7 +1,7 @@
 BEGIN
 
   insert into CRAPPRM (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
-       values ('CRED', 0, 'MANUAL_UTILIZACAO_API', 'Manual de utilização dos serviços de API Ailos', '/usr/coopd6/cecred/manualAPI/Manual - API de Cobranca v1.0.pdf');
+       values ('CRED', 0, 'MANUAL_UTILIZACAO_API', 'Manual de utilização dos serviços de API Ailos', '/usr/coop/cecred/manualAPI/Manual - API de Cobranca v1.0.pdf');
 
   commit;
 
