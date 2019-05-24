2965ec6c8d83261d9b5f177b339ef9dac21ec2b8
Alcemir Belinski Junior <alcemir.junior@ailos.coop.br>
2019-05-24 14:32:50 +0000

    inserir acao na crapaca
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C34331

---
+++ 05/insert_crapaca_tela_mancrd.sql
@@ -0,0 +1,9 @@
+BEGIN
+
+insert into CRAPACA (NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
+values ('PC_VALIDA_ATUALIZA_CARTOES', 'TELA_MANCRD', 'pc_valida_atualiza_cartoes', 'pr_nrdconta, pr_nrcrcard, pr_nrcctitg, pr_nrcpftit, pr_cdadmcrd, pr_nrctrcrd, pr_flgprcrd', 803);
+
+COMMIT;
+
+END;
+
