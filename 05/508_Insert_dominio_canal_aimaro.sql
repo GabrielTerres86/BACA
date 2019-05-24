6d890152c8e920658b13aae08d4901b60609c6c7
Luis Alberto Feltrin <luis.feltrin@ailos.coop.br>
2019-05-24 19:00:04 +0000

    SM 508.1 - Atualização cadastral no Aimaro.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C34369

--- 05/508_Insert_dominio_canal_aimaro.sql
+++ 05/508_Insert_dominio_canal_aimaro.sql
@@ -2,7 +2,7 @@ BEGIN
 
 	/***************************/
 	/*  INSERT DOMINIO CANAL   */
-	/***************************/
+	/***************************/ 
 	INSERT INTO TBCADAST_DOMINIO_CAMPO (NMDOMINIO, CDDOMINIO, DSCODIGO) VALUES ('TBCADAST_CANAL', 3, 'Aimaro');
 	
 	COMMIT;
