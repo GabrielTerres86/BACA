fa3bc559f0113f2332105c0babba875842849c8b
Gabriel Fronza Marcos (Mouts) <mouts-gabriel.marcos@ailos.coop.br>
2019-05-28 19:40:37 +0000

    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/PJ438-Agilidade nas Contratações de Crédito/DEV;C34473

--- ayllosdev/telas/atenda/emprestimos/emprestimos.js
+++ ayllosdev/telas/atenda/emprestimos/emprestimos.js
@@ -1931,8 +1931,6 @@ function manterRotina(operacao) {
 
     var vlpreant = (typeof vleprori == 'undefined') ? '' : vleprori;
 
-    var dsdopcao = "SVP";
-
     var nrctrant = (typeof nrctremp == 'undefined') ? '' : nrctremp;
 
     var nrctreax = (operacao == 'F_NUMERO') ? new_nrctremp : nrctremp;
