76a3f1b63608fbf56e4b769eb99acd6d55a66b92
Marcelo Coelho (Mouts) <mouts-marcelo.coelho@ailos.coop.br>
2019-05-30 18:28:10 +0000

    Acertos diversos Prejuízo
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C34581

--- 05/Baca_Acerto_Prejuizo_24_05.sql
+++ 05/Baca_Acerto_Prejuizo_24_05.sql
@@ -608,6 +608,88 @@ BEGIN
        AND nrdconta = 80616;
   END;
 -- ================================================================================================= 14
+  DECLARE
+    pr_cdcooper   craplcm.cdcooper%TYPE := 7;
+    pr_nrdconta   craplcm.nrdconta%TYPE := 141909;
+    pr_dtmvtolt   craplcm.dtmvtolt%TYPE;
+  --  pr_cdhistor IN  craplcm.cdhistor%TYPE
+    pr_nrdocmto   craplcm.nrdocmto%type := 1;
+    pr_cdagenci   craplcm.cdagenci%type := 1;
+    pr_cdbccxlt   craplcm.cdbccxlt%type := 100;
+    pr_vllanmto   craplcm.vllanmto%TYPE := 2040.37;
+    pr_nrdctabb   craplcm.nrdctabb%TYPE := 7662050;
+    pr_cdcritic   NUMBER;
+    pr_dscritic   VARCHAR2(10000);
+    vr_nrseqdig       craplot.nrseqdig%TYPE;   -- Aramazena o valor do campo "nrseqdig" da CRAPLOT para referência na CRAPLCM
+    vr_tab_retorno    lanc0001.typ_reg_retorno;
+    vr_incrineg       INTEGER;
+
+    vr_exc_saida      EXCEPTION;
+
+  BEGIN
+    BEGIN
+      SELECT dtmvtolt
+        INTO pr_dtmvtolt
+        FROM crapdat
+       WHERE cdcooper = pr_cdcooper;
+    EXCEPTION
+    WHEN OTHERS THEN
+      pr_dscritic := 'Erro ao buscar DTMVTOLT = '||SQLERRM;
+    END;
+    --
+    prej0003.pc_gera_transf_cta_prj(pr_cdcooper => pr_cdcooper
+                                   ,pr_nrdconta => pr_nrdconta
+                                   ,pr_cdoperad => 1
+                                   ,pr_vllanmto => pr_vllanmto
+                                   ,pr_dtmvtolt => pr_dtmvtolt
+                                   ,pr_versaldo => 0 -- pr_versaldo
+                                   ,pr_atsldlib => 0 -- pr_atsldlib
+                                   ,pr_cdcritic => pr_cdcritic
+                                   ,pr_dscritic => pr_dscritic);
+    if (nvl(pr_cdcritic,0) <> 0 or trim(pr_dscritic) is not null) then
+      RAISE vr_exc_saida;
+    end if;
+    --
+    vr_nrseqdig := FN_SEQUENCE(pr_nmtabela => 'CRAPLOT'
+                              ,pr_nmdcampo => 'NRSEQDIG'
+                              ,pr_dsdchave => to_char(pr_cdcooper)||';'||
+                              to_char(pr_dtmvtolt, 'DD/MM/RRRR')||';'||
+                              '1;100;650010');
+    -- Efetua lancamento de crédito na CRAPLCM(LANC0001)
+    LANC0001.pc_gerar_lancamento_conta(pr_cdcooper => pr_cdcooper
+                                      ,pr_dtmvtolt => pr_dtmvtolt
+                                      ,pr_cdagenci => 1
+                                      ,pr_cdbccxlt => 100
+                                      ,pr_nrdolote => 650010
+                                      ,pr_nrdconta => pr_nrdconta
+                                      ,pr_nrdocmto => pr_nrdctabb
+                                      ,pr_cdoperad => 1
+                                      ,pr_nrdctabb => pr_nrdconta
+                                      ,pr_nrseqdig => vr_nrseqdig
+                                      ,pr_dtrefere => pr_dtmvtolt
+                                      ,pr_vllanmto => pr_vllanmto
+                                      ,pr_cdhistor => 535
+                                      -- OUTPUT --
+                                      ,pr_tab_retorno => vr_tab_retorno
+                                      ,pr_incrineg => vr_incrineg
+                                      ,pr_cdcritic => pr_cdcritic
+                                      ,pr_dscritic => pr_dscritic);
+
+    IF nvl(pr_cdcritic, 0) > 0 OR pr_dscritic IS NOT NULL THEN
+      RAISE vr_exc_saida;
+    END IF;
+  EXCEPTION
+  WHEN vr_exc_saida THEN
+    if pr_cdcritic is not null and pr_dscritic is null then
+      pr_dscritic := gene0001.fn_busca_critica(pr_cdcritic);
+    end if;
+    rollback;
+    RAISE_APPLICATION_ERROR(-20500,pr_dscritic);
+  WHEN OTHERS THEN
+    pr_dscritic := 'Erro ao atualizar CRAPSLD = '||SQLERRM;
+    rollback;
+    RAISE_APPLICATION_ERROR(-20510,pr_dscritic);
+  end;
 -- ================================================================================================= 15
   COMMIT;
 END;
