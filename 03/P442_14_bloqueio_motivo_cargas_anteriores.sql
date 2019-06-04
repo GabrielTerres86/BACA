a5e2f1a004c354d14347b03e6c484b9d64e3da9b
Petter Rafael Silva <petter.silva@envolti.com.br>
2019-06-04 19:44:30 +0000

    p442 - Ajustar carga de motivos
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C34718

--- 03/P442_14_bloqueio_motivo_cargas_anteriores.sql
+++ 03/P442_14_bloqueio_motivo_cargas_anteriores.sql
@@ -1,6 +1,34 @@
 -- Script para ajustar cargas anteriores a liberação bloqueando e exibindo o motivo de bloqueio
 BEGIN
-  -- Criar motivos de exclusão
+  -- Incluir registros de bloqueio manual
+  INSERT INTO tbcc_hist_param_pessoa_prod(cdcooper
+                                       ,nrdconta
+                                       ,tppessoa
+                                       ,nrcpfcnpj_base
+                                       ,dtoperac
+                                       ,cdproduto
+                                       ,cdoperac_produto
+                                       ,flglibera
+                                       ,idmotivo
+                                       ,cdoperad)
+		  SELECT apr.cdcooper
+				,apr.nrdconta
+				,ass.inpessoa
+				,ass.nrcpfcnpj_base
+				,apr.dtatualiza_pre_aprv
+				,25
+				,1
+				,0
+				,apr.idmotivo
+				,1
+		  FROM tbepr_param_conta apr
+			  ,crapass ass
+		  WHERE apr.idmotivo IS NOT NULL
+			AND apr.flglibera_pre_aprv = 0
+			AND ass.cdcooper = apr.cdcooper
+			AND ass.nrdconta = apr.nrdconta;
+  
+  -- Criar motivos de exclusão de cargas de pre-aprovado legado por garantia
   INSERT INTO tbepr_motivo_nao_aprv(cdcooper
                                    ,nrdconta
                                    ,idcarga
