88dd178b79ddfb4aa5e1827a0470210dfec0f495
Anderson Fossa <anderson.fossa@ailos.coop.br>
2019-06-04 16:59:26 +0000

    P411 - Retorno da correção de qtd. de cotas equivocada
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C34708

---
+++ 06/P411-corrige-qtd-cotas-equivocada-v2-retorno.sql
@@ -0,0 +1,6 @@
+
+
+update tbcapt_custodia_lanctos set qtcotas = 6200 where idaplicacao = 2955211 and idlancamento = 5974062;
+update tbcapt_custodia_lanctos set qtcotas = -8   where idaplicacao = 2955211 and idlancamento = 5974061;
+
+commit;
