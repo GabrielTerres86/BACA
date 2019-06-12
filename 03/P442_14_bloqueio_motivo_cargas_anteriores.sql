2d29e85a7c25234b695b83d66781c5924cd44e2d
Petter Rafael Silva <petter.silva@envolti.com.br>
2019-06-12 14:42:03 +0000

    p442 - Ajustar a manutenção de cargas antigas conforme identificado no merge do libera.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C35006

--- 03/P442_14_bloqueio_motivo_cargas_anteriores.sql
+++ 03/P442_14_bloqueio_motivo_cargas_anteriores.sql
@@ -27,37 +27,6 @@ BEGIN
 			AND apr.flglibera_pre_aprv = 0
 			AND ass.cdcooper = apr.cdcooper
 			AND ass.nrdconta = apr.nrdconta;
-  
-  -- Criar motivos de exclusão de cargas de pre-aprovado legado por garantia
-  INSERT INTO tbepr_motivo_nao_aprv(cdcooper
-                                   ,nrdconta
-                                   ,idcarga
-                                   ,idmotivo
-                                   ,tppessoa
-                                   ,nrcpfcnpj_base
-                                   ,dtbloqueio)
-        SELECT cpa.cdcooper
-              ,0
-              ,cpa.iddcarga
-              ,79
-              ,cpa.tppessoa
-              ,cpa.nrcpfcnpj_base
-              ,SYSDATE
-        FROM crapcpa cpa
-        WHERE cpa.nrdconta > 0
-          AND cpa.cdsituacao IS NULL
-          AND EXISTS (SELECT 1
-                      FROM tbepr_carga_pre_aprv aprv
-                      WHERE aprv.idcarga = cpa.iddcarga
-                        AND aprv.cdcooper = cpa.cdcooper);
-  
-  -- Atualizar CPA das cargas antigas
-  UPDATE crapcpa cpa
-    SET cpa.dtbloqueio = SYSDATE
-       ,cpa.cdoperad_bloque = 1
-       ,cpa.cdsituacao = 'B'
-  WHERE cpa.nrdconta > 0
-    AND cpa.cdsituacao IS NULL;
-    
+
   COMMIT;
 END;
