ee852ffa31c2c98aefe58306ec0112323cee819e
Rafael Antunes Ferreira (Mouts) <mouts-rafael.ferreira@ailos.coop.br>
2019-06-17 14:36:41 +0000

    Deletando Scripts Antigos para colocar novos
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C35201

--- 06/1_CECRED.tbgen_analise_credito_acessos_SEQ.sql
+++
@@ -1,9 +0,0 @@
--- Create sequence 
-create sequence CECRED.tbgen_analise_credito_acessos_SEQ
-minvalue 1
-maxvalue 99999999999999999999999
-start with 0
-increment by 1
-nocache
-order;
-commit;
--- 06/3_CECRED.TRG_tbgen_analise_credito_acessos_ID.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TRIGGER CECRED.TRG_tbgen_analise_credito_acessos_ID BEFORE
-    INSERT ON tbgen_analise_credito_acessos
-    FOR EACH ROW
-    WHEN ( new.idanalise_contrato_acesso IS NULL )
-BEGIN
-    :new.idanalise_contrato_acesso := tbgen_analise_credito_acessos_SEQ.nextval;
-END;
-commit;
