d055ce73a8601fb1c4b639c703883a52ba689dc0
Adriano Marchi <adriano@ailos.coop.br>
2019-06-18 17:04:34 +0000

    Script para liberação de valor bloqueado - Adriano INC0018204
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C35253

--- 06/atualiza_deposito_custodia_bloqueado.sql
+++ 06/atualiza_deposito_custodia_bloqueado.sql
@@ -5,6 +5,14 @@ BEGIN
      and nrdconta = 9679 
      and dtmvtolt = '16/01/2019' 
      and nrdocmto = 197239;
+
+  update crapdpb 
+     set dtliblan = to_char(SYSDATE + 1,'DD/MM/RRRR') 
+   where cdcooper = 6 
+     and nrdconta = 124346 
+     and dtmvtolt = '17/01/2019' 
+     and nrdocmto = 174759;
+
   COMMIT;
   EXCEPTION
     WHEN others THEN
