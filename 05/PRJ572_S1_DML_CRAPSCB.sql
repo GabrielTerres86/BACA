3099143a140d44ede7b9eded18e49829fb8e6d67
Adriano Nagasava (Supero) <CECRED\t0031546>
2019-06-18 21:30:01 +0000

    PRJ572 - Ajustes durante homologação
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C35270

--- 05/PRJ572_S1_DML_CRAPSCB.sql
+++ 05/PRJ572_S1_DML_CRAPSCB.sql
@@ -1,5 +1,13 @@
-UPDATE crapscb scb
-   SET scb.dsdirarq = '/usr/connect/bancoob'
- WHERE scb.tparquiv = 6;
---
-COMMIT;
+BEGIN
+	--
+	UPDATE crapscb scb
+		 SET scb.dsdirarq = '/usr/connect/bancoob'
+	 WHERE scb.tparquiv = 6;
+	--
+	COMMIT;
+	--
+EXCEPTION
+	WHEN OTHERS THEN
+	  ROLLBACK;
+		dbms_output.put_line('Erro: ' || SQLERRM);
+END;  
