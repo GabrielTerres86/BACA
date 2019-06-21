5e900364ffde9032630c9191d97ed4d7a176f8a4
Petter Rafael Silva <petter.silva@envolti.com.br>
2019-06-21 19:07:29 +0000

    p442 - Ajuste no cadastros de motivos.
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C35387

--- 06/P442_0_DDL_PRE_APRV.sql
+++ 06/P442_0_DDL_PRE_APRV.sql
@@ -640,38 +640,7 @@ comment on column CRAPASS.nrcpfcnpj_base
 
 /* Carga inicial de motivos padrão do sistema */
 begin
-    -- Motivo para decsontos de títulos
-  INSERT INTO tbgen_motivo(idmotivo
-						  ,dsmotivo
-						  ,cdproduto
-						  ,flgreserva_sistema
-						  ,flgativo
-						  ,flgtipo
-						  ,flgexibe)
-			  VALUES(78
-					,'Desconto de titulo em atraso'
-					,25
-					,0
-					,1
-					,0
-					,1);
-  
-  -- Motivo para BACA de integração de cargas antigas
-  INSERT INTO tbgen_motivo(idmotivo
-						  ,dsmotivo
-						  ,cdproduto
-						  ,flgreserva_sistema
-						  ,flgativo
-						  ,flgtipo
-						  ,flgexibe)
-			  VALUES(79
-					,'Bloqueio manual'
-					,25
-					,1
-					,1
-					,0
-					,1);
-					
+  -- Motivo para BACA de integração de cargas antigas				
 	INSERT INTO tbgen_motivo(idmotivo
 							,dsmotivo
 							,cdproduto
