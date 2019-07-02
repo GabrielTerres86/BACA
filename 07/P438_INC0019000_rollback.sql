3f7e953022a6942e73d657f93d300c656fd1e187
Odirlei Busana <odirlei.busana@ailos.coop.br>
2019-07-02 19:52:59 +0000

    Ajustes nova liberação
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C35717

--- 07/P438_INC0019000_rollback.sql
+++ 07/P438_INC0019000_rollback.sql
@@ -1,35 +1,13 @@
 /* INC0019000  -- rollback */
-update crawlim x
-   set x.insitlim = 9, x.dtanulac = to_date('25/06/2019', 'DD/MM/RRRR')
- where x.cdcooper = 1
-   and x.nrdconta = 4035216
-   and x.tpctrlim = 3
-   and x.nrctrlim = 7482;
-
-update crawlim x
-   set x.insitapr = 8
- where x.cdcooper = 1
-   and x.nrdconta = 4035216
-   and x.tpctrlim = 3
-   and x.nrctrlim = 9782
-
- insert into tbmotivo_anulacao(CDCOOPER,
-                         NRDCONTA,
-                         NRCTRATO,
-                         TPCTRLIM,
-                         DTCADASTRO,
-                         CDMOTIVO,
-                         DSMOTIVO,
-                         DSOBSERVACAO,
-                         CDOPERAD)
- values(1,
-              4035216,
-              7482,
-              3,
-              to_date('25-06-2019', 'dd-mm-yyyy'),
-              8,
-              'Outros',
-              'aumento de limite',
-              'f0011259');
-
+update crawlim lim
+      set    insitlim = 1 
+            ,insitest = 3 
+            ,dtrejeit = null
+            ,hrrejeit = 0
+            ,cdoperej = ' '
+      where  lim.cdcooper = 1
+      and    lim.nrdconta = 4035216
+      and    lim.nrctrlim = 9782
+      and    lim.tpctrlim = 3;
+      
 commit;
