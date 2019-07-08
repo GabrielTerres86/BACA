faaf5443c5b86c78ebb765fd1fac32d91c446616
Josiane Stiehler (AMCOM) <josiane.stiehler@amcom.com.br>
2019-07-08 19:41:59 +0000

    P437 - Consignado - inclusão da coluna inconciliado
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/P437 - Consignado2/DEV;C35946

---
+++ BACA/36 CONSIGNADO - tbepr_consignado_movimento_tmp_conc.sql
@@ -0,0 +1,4 @@
+-- Add/modify columns 
+alter table CECRED.TBEPR_CONSIG_MOVIMENTO_TMP add inconciliado number(1);
+-- Add comments to the columns 
+comment on column CECRED.TBEPR_CONSIG_MOVIMENTO_TMP.inconcliado  is 'Indicador de movimento conciliado (0 - Não conciliado, 1 - Conciliado)';
