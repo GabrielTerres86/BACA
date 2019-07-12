6723ca6df494fa10c26f5d90de3151b0650a938f
Diego Vicentini <diego@ailos.coop.br>
2019-07-12 20:39:07 +0000

    Tratamentos para geração de número de documento único no envio de TED
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C36113

---
+++ 07/atualiza_sequence_ted.sql
@@ -0,0 +1,8 @@
+
+
+-- Alter sequence
+alter sequence CECRED.SEQ_TEDENVIO maxvalue 99999999;
+-- Modify the last number 
+alter sequence CECRED.SEQ_TEDENVIO increment by 88000000 nocache;
+select CECRED.SEQ_TEDENVIO.nextval from dual;
+alter sequence CECRED.SEQ_TEDENVIO increment by 1 cache 20;
