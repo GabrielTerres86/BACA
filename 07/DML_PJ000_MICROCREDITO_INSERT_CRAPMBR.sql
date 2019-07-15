4671092903b1af11427be226757524791028193a
Adriano Nagasava (Supero) <CECRED\t0031546>
2019-07-15 14:41:22 +0000

    PRJ000 - Microcrédito
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C36143

---
+++ 07/DML_PJ000_MICROCREDITO_INSERT_CRAPMBR.sql
@@ -0,0 +1,11 @@
+BEGIN
+	--
+	insert into crapmbr (CDBIRCON, CDMODBIR, DSMODBIR, INPESSOA, NMTAGMOD, NRORDIMP)
+	values (1, 4, '482', 2, 'PEFINCOMP', 2);
+  --
+	insert into crapmbr (CDBIRCON, CDMODBIR, DSMODBIR, INPESSOA, NMTAGMOD, NRORDIMP)
+	values (1, 5, '309', 2, 'PLUSMASTER', 1);
+  --
+	COMMIT;
+	--
+END;
