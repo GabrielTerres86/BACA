caf91537bedd34b2232fe735b02b41ce55d8b21b
Werinton Ferrari <werinton.ferrari@ailos.coop.br>
2019-07-15 17:44:40 +0000

    Adicionado contrato
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C36157

--- 07/P439_Ajustar_Contrato_CDC_1592543.sql
+++ 07/P439_Ajustar_Contrato_CDC_1592543.sql
@@ -4,4 +4,11 @@ where e.cdcooper = 1
 and e.nrdconta = 7970188 
 and e.nrctremp = 1592543;
 
+ 
+update crawepr e 
+set e.flgdocdg = 1
+where e.cdcooper = 1 
+and e.nrdconta = 9659480 
+and e.nrctremp = 1592650;
+
 commit;
