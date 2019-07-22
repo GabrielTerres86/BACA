587b21d4d02b0128fd452fc97cccd54d2f7fdf76
Anderson Fossa <anderson.fossa@ailos.coop.br>
2019-07-22 14:21:03 +0000

    P411.3 - Ajuste para remoção dos warnings
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/FONTES/PRINCIPAL;C36426

--- ayllosdev/telas/cusapl/busca_log_arquivos.php
+++ ayllosdev/telas/cusapl/busca_log_arquivos.php
@@ -11,9 +11,6 @@
 //*********************************************************************************************//
 
 ini_set('memory_limit','256M'); // set memory to prevent fatal errors
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 	session_start();
 	require_once('../../includes/config.php');
--- ayllosdev/telas/cusapl/busca_log_operacoes.php
+++ ayllosdev/telas/cusapl/busca_log_operacoes.php
@@ -8,10 +8,6 @@
 //***                                                                  						          ***//
 //*** Alterações: 																			                                    ***//
 //*********************************************************************************************//
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 
  session_start();
  require_once('../../includes/config.php');
