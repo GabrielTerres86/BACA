43e1e5cf25a870cd9f011ee041d399fa7af36146
Fernanda K. de Oliveira (AMCOM) <fernanda.oliveira@amcom.com.br>
2019-07-25 19:59:00 +0000

    P437.Consignado - Regra da politica de credito Consignado a ser executada no Motor de Credito IBRATAN
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C36570

---
+++ 07/37_CONSIGNADO_Insert_crapprm.sql
@@ -0,0 +1,9 @@
+insert into cecred.crapprm
+  (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+values
+  ('CRED',
+   0,
+   'REGRA_MOTOR_IBRA_CONSIG',
+   'Nome da politica de credito para Consignado a ser executada no Motor de Credito IBRATAN',
+   'PoliticaConsignado');   
+commit;
