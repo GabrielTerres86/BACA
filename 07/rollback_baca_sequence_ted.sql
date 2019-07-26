a315a505a90d414b1579d79a751212f90767638a
Diego Vicentini <diego@ailos.coop.br>
2019-07-26 17:56:35 +0000

    Baca de rollback da sequence TED
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C36600

---
+++ 07/rollback_baca_sequence_ted.sql
@@ -0,0 +1,7 @@
+-- Alter sequence
+alter sequence CECRED.SEQ_TEDENVIO 
+maxvalue 999;
+-- Modify the last number 
+alter sequence CECRED.SEQ_TEDENVIO increment by -65000000 nocache;
+select CECRED.SEQ_TEDENVIO.nextval from dual;
+alter sequence CECRED.SEQ_TEDENVIO increment by 1 cache 20;
