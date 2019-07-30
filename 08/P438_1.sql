b41e7e8a5f0643d1bcaefb059087855efecc2b84
Odirlei Busana <odirlei.busana@ailos.coop.br>
2019-07-30 14:41:34 +0000

    PRJ438 - Ajustado codigo de critica por ja estar em uso
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C36695

--- 08/P438_1.sql
+++ 08/P438_1.sql
@@ -2,6 +2,6 @@ BEGIN
   INSERT INTO crapcri
     (cdcritic, dscritic)
   VALUES
-    (1468, '1468 - CPF/CNPJ de cooperado. Consulte a tela contas.');
+    (1499, '1499 - CPF/CNPJ de cooperado. Consulte a tela contas.');
   COMMIT;
 END;
