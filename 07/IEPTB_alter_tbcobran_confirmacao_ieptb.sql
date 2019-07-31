d38aaf07835c471f11408d309cdc6341a0536a7c
Darlei Fernando Zillmer (Supero) <CECRED\t0032717>
2019-07-31 16:49:02 +0000

    Adição de campos de controle de arquivos de recebimento para o IEPTB
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C36741

---
+++ 07/IEPTB_alter_tbcobran_confirmacao_ieptb.sql
@@ -0,0 +1,4 @@
+alter table cecred.tbcobran_confirmacao_ieptb add nrnmarqu number(5);
+
+comment on table tbcobran_confirmacao_ieptb.nrnmarqu
+  is 'Sequencial do nome do arquivo de confirmacao';
---
+++ 07/IEPTB_alter_tbcobran_retorno_ieptb.sql
@@ -0,0 +1,6 @@
+alter table cecred.tbcobran_retorno_ieptb add nrnmarqu number(5);
+
+comment on table tbcobran_retorno_ieptb.nrnmarqu
+  is 'Sequencial do nome do arquivo de retorno';
+
+  
