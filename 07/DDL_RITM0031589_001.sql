6ce018bc1d84913dce1a57b6c0e13f97984ec3c9
Ana Lucia Engel Volles <ana.volles@ailos.coop.br>
2019-07-31 13:02:53 +0000

    inclusão email cfe ritm0031589
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C36729

--- 07/DDL_RITM0031589_001.sql
+++ 07/DDL_RITM0031589_001.sql
@@ -5,14 +5,22 @@
 
 SELECT * FROM CRAPPRM T
 WHERE T.NMSISTEM = 'CRED' AND T.CDACESSO = 'PC_LISTA_TBGEN_EMAIL_1';
+
+SELECT * FROM CRAPPRM T
+WHERE T.NMSISTEM = 'CRED' AND T.CDACESSO = 'PC_LISTA_TBGEN_EMAIL_2';
 */
 
 
 BEGIN
 
   UPDATE CRAPPRM T SET 
-  T.DSVLRPRM = 'rodrigo.siewerdt@ailos.coop.br, helio.mariano@ailos.coop.br, marcos@ailos.coop.br, frank.ribeiro@ailos.coop.br, renan.censi@ailos.coop.br, plantonistas@ailos.coop.br, jhonatan.moraes@ailos.coop.br'
+  T.DSVLRPRM = 'rodrigo.siewerdt@ailos.coop.br, helio.mariano@ailos.coop.br, marcos@ailos.coop.br, frank.ribeiro@ailos.coop.br, renan.censi@ailos.coop.br, jhonatan.moraes@ailos.coop.br'
   WHERE T.NMSISTEM = 'CRED' AND T.CDACESSO = 'PC_LISTA_TBGEN_EMAIL_1';
+
+  UPDATE CRAPPRM T SET 
+  T.DSVLRPRM = 'ana.volles@ailos.coop.br, plantonistas@ailos.coop.br'
+  WHERE T.NMSISTEM = 'CRED' AND T.CDACESSO = 'PC_LISTA_TBGEN_EMAIL_2';
+
 
   COMMIT;
 EXCEPTION
