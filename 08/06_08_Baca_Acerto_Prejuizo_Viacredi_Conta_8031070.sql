2aa9c00653cd7642fbe63654dba858be6c502a98
Marcio Jose de Carvalho (MOUTS) <mouts-marcio.carvalho@ailos.coop.br>
2019-08-06 19:04:15 +0000

    Script de ajuste de contas de prejuízo devido lançamentos indevidos nas contas
    git-tfs-id: [http://alm.cecred.coop.br:8080/tfs/DefaultCollection]$/Baca/2019;C36920

---
+++ 08/06_08_Baca_Acerto_Prejuizo_Viacredi_Conta_8031070.sql
@@ -0,0 +1,10 @@
+﻿ BEGIN
+    DELETE FROM tbcc_prejuizo_detalhe
+     WHERE idlancto IN (34101);
+
+    UPDATE tbcc_prejuizo
+       SET vlsdprej = vlsdprej - 1453.00
+     WHERE cdcooper = 1
+       AND nrdconta = 8031070;
+       commit;
+  END;
