34adbddb36956836c0c2d5e605a3b4145e6a5a5f
Rafael Ferreira <rafaelferreira761@gmail.com>
2019-09-03 15:09:16 -0300

    Cria novo parametro de data de corte
    Cria parametro de data de corte da nova reciprocidade para as cooperativas VIACREDI e VIACREDI AV

---
+++ 2019/09/cria_CRAPPRM_data_reciprocidade.sql
@@ -0,0 +1,6 @@
+insert into cecred.CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 1, 'DT_VIG_RECECIPR_V2', 'Data de vigencia do novo modelo de reciprocidade.', '01/08/2019');
+
+insert into cecred.CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 16, 'DT_VIG_RECECIPR_V2', 'Data de vigencia do novo modelo de reciprocidade.', '27/11/2018');
+commit;
