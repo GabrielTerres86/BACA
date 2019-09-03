fd59a07b73cf447fde3f02f19df12073d9797a46
Anderson Fossa <anderson.fossa@ailos.coop.br>
2019-09-03 12:14:30 -0300

    Liberação de ajuste do INC0024175 Visa adequar PU calculada de uma aplicação que está impedindo o andamento do processo de registro da aplicação na B3.

---
+++ 2019/09/INC0024175-ajusta-pu-negativo.sql
@@ -0,0 +1,5 @@
+
+-- Valor original: vlpreco_unitario = -0,57823529
+update tbcapt_custodia_lanctos set vlpreco_unitario = 0.01000000 where idlancamento = 8171810;
+
+commit;
---
+++ 2019/09/RDM0032381_rollback.sql
@@ -0,0 +1,5 @@
+
+-- Rollback
+update tbcapt_custodia_lanctos set vlpreco_unitario = -0.57823529 where idlancamento = 8171810;
+
+commit;
