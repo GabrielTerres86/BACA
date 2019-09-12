9533a3bfcf355e9267190c3374789fcbaafd6eff
Douglas Alessandro Pagel (AMCOM) <t0031898@ailos.coop.br>
2019-09-12 08:22:28 -0300

    Ajuste código de critica de 4516 para 1503.

--- 2019/09/P438_crapcri_1503.sql
+++ 2019/09/P438_crapcri_1503.sql
@@ -1,5 +1,5 @@
 insert into crapcri (cdcritic,dscritic,tpcritic,flgchama) values
-                    (4516,'4516 - Ja existe um reenvio para analise em andamento. Aguarde.',4,0);
+                    (1503,'1503 - Ja existe um reenvio para analise em andamento. Aguarde.',4,0);
 
 COMMIT;					
                     
