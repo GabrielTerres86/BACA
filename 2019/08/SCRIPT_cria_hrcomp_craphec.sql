902b8c83854853318a14b63618a47d1a15a550c7
Glauco Becaro <glauco.becaro@ailos.coop.br>
2019-09-18 14:28:33 -0300

    P565 Scripts Fase 1 arquivos FAC

---
+++ 2019/08/SCRIPT_cria_hrcomp_craphec.sql
@@ -0,0 +1,2 @@
+insert into craphec (cdcooper, cdprogra, dsprogra, hriniexe, hrfimexe, hrultexc, dtultexc, cdoperad, flgativo, nrseqexe)
+values (3,'crps264.p','DEVOLUCAO DIURNA',47100,48600,47133,trunc(sysdate),'1',0,4);
