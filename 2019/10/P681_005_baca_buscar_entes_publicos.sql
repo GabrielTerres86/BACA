e25d0018de463697513e9f43aa0706503143e09b
Moises Marques da Silva <moises.silva@supero.com.br>
2019-09-19 16:55:32 -0300

    bacas GRPMUN

---
+++ 2019/10/P681_005_baca_buscar_entes_publicos.sql
@@ -0,0 +1,49 @@
+
+DECLARE
+  
+  -- Buscar registro da RDR
+  CURSOR cr_craprdr IS
+    SELECT t.nrseqrdr
+      FROM craprdr t
+     WHERE t.NMPROGRA = 'TELA_GRPMUN';
+  
+  -- Variaveis
+  vr_nrseqrdr craprdr.nrseqrdr%TYPE;
+  
+BEGIN
+  
+  -- Buscar RDR
+  OPEN  cr_craprdr;
+  FETCH cr_craprdr INTO vr_nrseqrdr;
+  
+  -- Se nao encontrar
+  IF cr_craprdr%NOTFOUND THEN
+  
+  INSERT INTO craprdr(nmprogra,dtsolici)
+       VALUES('TELA_GRPMUN', SYSDATE)
+       RETURNING craprdr.nrseqrdr INTO vr_nrseqrdr;
+  
+  END IF;
+  
+  -- Fechar o cursor
+  CLOSE cr_craprdr;
+  
+  INSERT INTO crapaca (
+         nmdeacao
+         , nmpackag
+         , nmproced
+         , lstparam
+         , nrseqrdr
+  ) VALUES (
+        'BUSCAR_ENTES_PUBLICOS'
+        , 'TELA_GRPMUN'
+        , 'pc_buscar_entes_publicos'
+		, 'pr_pagina, pr_tamanho_pagina, pr_idgrupo_municipal'
+        , vr_nrseqrdr);
+  
+  COMMIT;
+  
+  -- Apresenta uma mensagem de ok
+  dbms_output.put_line('Referencia a TELA_GRPMUN criado com sucesso!');
+  
+END;
