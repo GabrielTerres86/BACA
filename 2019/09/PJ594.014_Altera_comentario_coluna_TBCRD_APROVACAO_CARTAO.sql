385609a60064af7603ca818f8d8028c4e0fdae94
Guilherme Cervo (Supero) <t0032613@cecred.coop.br>
2019-09-23 16:34:45 -0300

    BACAS do projeto

---
+++ 2019/09/PJ594.014_Altera_comentario_coluna_TBCRD_APROVACAO_CARTAO.sql
@@ -0,0 +1,3 @@
+-- Add comments to the columns 
+comment on column CECRED.TBCRD_APROVACAO_CARTAO.indtipo_senha
+  is '1 - Cartao Magnetico TA, 2 - Cartao Cecred TA, 3 - Conta Online, 4 - Supervisor, 5 - Operador, 6 - Canais';
