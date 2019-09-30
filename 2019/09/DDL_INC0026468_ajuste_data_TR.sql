c20c59dcf8ae6069cebe8f7a93da2437b98eda12
Ana Lucia Engel Volles <ana.volles@ailos.coop.br>
2019-09-30 16:11:56 -0300

    update em mais 2 contas cfe inc0026468
    update em mais 2 contas cfe inc0026468

--- 2019/09/DDL_INC0026468_ajuste_data_TR.sql
+++ 2019/09/DDL_INC0026468_ajuste_data_TR.sql
@@ -10,6 +10,9 @@ BEGIN
   update crapepr e set e.dtdpagto = to_date('20/07/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta = 1200046  and  nrctremp = 553548;
   update crapepr e set e.dtdpagto = to_date('22/07/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta = 90054970 and  nrctremp = 284907;
 
+  update crapepr e set e.dtdpagto = to_date('05/07/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta = 3869237 and  nrctremp = 997429;
+  update crapepr e set e.dtdpagto = to_date('25/07/2019','DD/MM/YYYY') where cdcooper = 1 and nrdconta = 1900528 and  nrctremp = 777054;
+
   COMMIT;
 EXCEPTION
   WHEN OTHERS THEN
