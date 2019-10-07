62fc478998dd42c2894bcab921c774230fffb572
Miguel Fernando Coelho Rodrigues <miguel.rodrigues@supero.com.br>
2019-10-07 18:00:45 -0300

    Baca para correcao do motor esteira

--- 2019/10/Baca_P637_correcao-esteira.sql
+++ 2019/10/Baca_P637_correcao-esteira.sql
@@ -3,6 +3,6 @@ declare
 begin
 update crawepr
   set insitest=0
- where cdcooper=1 and insitapr=0 and insitest=1 and p.dtenvmot<trunc(sysdate);
+ where cdcooper=1 and insitapr=0 and insitest=1 and dtenvmot<trunc(sysdate);
  commit;
 end; 
