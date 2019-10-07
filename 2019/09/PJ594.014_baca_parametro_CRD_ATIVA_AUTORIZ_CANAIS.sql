a672f44544ce7ec5d996fec6fe9334c13a546891
Otavio Theiss <otavio.theiss@ailos.coop.br>
2019-10-07 17:31:53 -0300

    criando parametro na PRM - CRD_ATIVA_AUTORIZ_CANAIS - Parametro para informar se a funcionalidade de autorizacao de cartao de credito via canais esta ativa ou nao para a cooperativa.

---
+++ 2019/09/PJ594.014_baca_parametro_CRD_ATIVA_AUTORIZ_CANAIS.sql
@@ -0,0 +1,40 @@
+--Baca para adicionar novo parametro no Ayllos e Web 
+DECLARE
+  
+  -- Cursor para as cooperativas
+  CURSOR cr_crapcop IS
+    SELECT  *
+      FROM crapcop cop
+     WHERE cop.flgativo = 1;
+     
+BEGIN
+  
+  -- Percorrer as cooperativas do cursor
+  FOR rw_crapcop IN cr_crapcop LOOP
+
+    BEGIN
+
+      INSERT INTO crapprm
+	  (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+	   values
+	  ('CRED',
+	   rw_crapcop.cdcooper,
+	   'CRD_ATIVA_AUTORIZ_CANAIS',
+	   'Parametro para informar se a funcionalidade de autorizacao de cartao de credito via canais esta ativa ou nao para a cooperativa',
+	   '0');
+      
+    EXCEPTION
+      WHEN dup_val_on_index THEN
+        RAISE_APPLICATION_ERROR(-20001,'Erro ao criar parametrização: '||SQLERRM);
+      WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20002,'Erro ao criar parametrização: '||SQLERRM);
+    END;
+    
+  END LOOP;
+  
+  COMMIT;
+  
+  -- Apresenta uma mensagem de ok
+  dbms_output.put_line('Parametrização criada com sucesso');
+  
+END;
