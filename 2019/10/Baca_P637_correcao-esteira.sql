af55a2ae758f714cce79cf5a72ffe5b80e14e64a
Miguel Fernando Coelho Rodrigues <miguel.rodrigues@supero.com.br>
2019-10-09 11:12:28 -0300

    Adicionado restricoes AND dtenvest IS NULL AND dtenvmot IS NOT NULL a pedido do analista Jaison

--- 2019/10/Baca_P637_correcao-esteira.sql
+++ 2019/10/Baca_P637_correcao-esteira.sql
@@ -3,6 +3,6 @@ declare
 begin
 update crawepr
   set insitest=0
- where insitapr=0 and insitest=1 and dtenvmot<trunc(sysdate);
+ where insitapr=0 and insitest=1 and dtenvmot<trunc(sysdate) AND dtenvest IS NULL AND dtenvmot IS NOT NULL;
  commit;
 end; 
