682421b4edb19f405bc9625ea7a44fd83bb2e9c5
Miguel Fernando Coelho Rodrigues <miguel.rodrigues@supero.com.br>
2019-10-09 10:43:58 -0300

    removido a restricao de coop

--- 2019/10/Baca_P637_correcao-esteira.sql
+++ 2019/10/Baca_P637_correcao-esteira.sql
@@ -3,6 +3,6 @@ declare
 begin
 update crawepr
   set insitest=0
- where cdcooper=1 and insitapr=0 and insitest=1 and dtenvmot<trunc(sysdate);
+ where insitapr=0 and insitest=1 and dtenvmot<trunc(sysdate);
  commit;
 end; 
