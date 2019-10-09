805092172b3652c6fc34e32c918b74553951daa4
Miguel Fernando Coelho Rodrigues <miguel.rodrigues@supero.com.br>
2019-10-09 14:14:50 -0300

    Removido as restricoes conforme solicitacao do analista Jaison

--- 2019/10/Baca_P637_correcao-esteira.sql
+++ 2019/10/Baca_P637_correcao-esteira.sql
@@ -3,6 +3,6 @@ declare
 begin
 update crawepr
   set insitest=0
- where insitapr=0 and insitest=1 and dtenvmot<trunc(sysdate) AND dtenvest IS NULL AND dtenvmot IS NOT NULL;
+ where insitapr=0 and insitest=1 and dtenvmot<trunc(sysdate);
  commit;
 end; 
