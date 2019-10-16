89101c55fce438eb4cf0c6f3836ce720da4e238b
Rafael Rocha Santos <rafael.rocha@amcom.com.br>
2019-10-16 09:43:08 -0300

    Subindo alter coment na crapscn.dsoparre

---
+++ 2019/10/alter_comment_crapscn_dsoparre.sql
@@ -0,0 +1,4 @@
+-- Add comments to the columns 
+comment on column CECRED.crapscn.dsoparre
+  is 'Identifica os canais do convenio. A-Caixa/B-ATM/C-Agente credenciado/D-IB/E-Debito automatico/F-CNAB'
+/
