859028f22313ae0992d37fd1a653f7eb5453a580
Luis Felipe Fagundes <luis.fagundes@amcom.com.br>
2019-10-21 11:10:47 -0300

    PJ 565.2 RF16 Tratamento de Novos campos, ajuste nos scrpts de preenchimento dos campos

--- 2019/10/pj565_2_scripts_atualizacao_base.sql
+++ 2019/10/pj565_2_scripts_atualizacao_base.sql
@@ -15,17 +15,41 @@
    
  --Scrip para atualização campo 'flgdebfacil' SAMAE ILHOTA DA - AGUAS GUARAMIRIM DA - AGUAS SCHROEDER DA
    update cecred.gnconve
-set gnconve.flgdebfacil = 1
-where gnconve.cdconven in (108,87,126)
-  and gnconve.intpconvenio in(2,3) 
-  and gnconve.inorigem_inclusao in (1,3) 
+	  set gnconve.flgdebfacil = 1
+	where gnconve.cdconven in (108,87,126)
+	  and gnconve.intpconvenio in(2,3) 
+	  and gnconve.inorigem_inclusao in (1,3) 
 
  --Scrip para atualização campo 'flgenv_dt_repasse' PREVISC -    
 	update cecred.gnconve 
 	  set  gnconve.flgenv_dt_repasse = 1
-	where gnconve.cdconven in (128,127,085);  	  
+	where gnconve.cdconven in (128,127,085,68);  	  
 
   --Scrip para atualização campo 'qttamanho_optante'  	
+   update cecred.gnconve
+	set gnconve.qttamanho_optante = 19
+	where gnconve.cdconven in (8, 16, 19, 20, 25, 26, 11, 49); 
+	
+	update cecred.gnconve
+	set gnconve.qttamanho_optante = 17
+	where gnconve.cdconven in (4, 24, 31, 33, 53, 54, 108, 96); 
+	
+	update cecred.gnconve
+	set gnconve.qttamanho_optante = 16
+	where gnconve.cdconven in (2, 10, 5, 30, 14, 45); 
+	
+    update cecred.gnconve
+	set gnconve.qttamanho_optante = 5
+	where gnconve.cdconven in (3, 48); 
+	
+	update cecred.gnconve
+	set gnconve.qttamanho_optante = 14
+	where gnconve.cdconven = 15; 
+	
+	update cecred.gnconve
+	set gnconve.qttamanho_optante = 6
+	where gnconve.cdconven = 22; 
+  
 	update cecred.gnconve
      set gnconve.qttamanho_optante = 8
    where gnconve.cdconven = 101;
