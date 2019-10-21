45ddb8574ef2cd96bee9742f8d3e38fa0e82e172
Renato Cordeiro <renato.cordeiro@amcom.com.br>
2019-10-21 10:00:59 -0300

    BUG30 - correção para uso adequado da CRAPPRM

--- 2019/10/P565_2_DML0007_RF24_NUMLOTE.sql
+++ 2019/10/P565_2_DML0007_RF24_NUMLOTE.sql
@@ -1,7 +1,8 @@
 begin
   for creg in (select cdcooper from crapcop) loop
-     insert into craptab (nmsistem, tptabela, cdempres, cdacesso, tpregist, dstextab, cdcooper)
-     values ('CRED','GENERI',0,'NUMLOTEXTCONV',0,1,creg.cdcooper);
+     insert into crapprm (nmsistem, cdcooper, cdacesso,dstexprm,dsvlrprm)
+     values ('CRED',   creg.cdcooper,'NUMLOTEXTCONV','Nro lote para geracao historico 2996', 1);
   end loop;
   commit;
 end;
+/
