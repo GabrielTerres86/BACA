0a00f30eff59e22190b7b2358bc0851c567a4397
Rafael Rocha Santos <rafael.rocha@amcom.com.br>
2019-10-25 10:20:25 -0300

    Movendo scripts para a fase 3

--- 2019/10/alter_comment_crapscn_cddmoden.sql
+++
@@ -1,4 +0,0 @@
--- Add comments to the columns 
-comment on column CECRED.crapscn.cddmoden
-  is 'Origem da inclusão da fatura em débito automático'
-/
--- 2019/10/alter_comment_crapscn_dsoparre.sql
+++
@@ -1,4 +0,0 @@
--- Add comments to the columns 
-comment on column CECRED.crapscn.dsoparre
-  is 'Identifica os canais do convenio. A-Caixa/B-ATM/C-Agente credenciado/D-IB/E-Debito automatico/F-CNAB'
-/
