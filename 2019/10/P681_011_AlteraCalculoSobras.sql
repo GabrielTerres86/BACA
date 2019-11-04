0266fe0bf25a1c928d7d90a674e7a44eb3605df4
Renato Darosci <supero-renato.darosci@ailos.coop.br>
2019-11-04 14:10:09 -0300

    P681 - Adicionar script para alteração do parametros de messageria da tela SOL030

---
+++ 2019/10/P681_011_AlteraCalculoSobras.sql
@@ -0,0 +1,11 @@
+BEGIN 
+       UPDATE crapaca
+          SET lstparam = 'pr_ininccmi,pr_increret,pr_txretorn,pr_txjurcap,pr_txjurapl,pr_txjursdm,pr_txjurtar,pr_txreauat,pr_inpredef,pr_indeschq,pr_indemiti,pr_indestit,pr_unsobdep,pr_dssopfco,pr_dssopjco,pr_inentpub'
+        WHERE nmdeacao = 'ALTERA_CALC_SOBRAS'
+          AND nmpackag = 'TELA_SOL030'
+          AND nmproced = 'pc_altera_calculo_sobras';
+       
+       COMMIT;
+				
+       
+END;
