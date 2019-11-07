c9eb11fcd969bbf0908860bb7365fea25024284b
Evandro Augusto Guaranha Da Silva (AMCOM) <evandro.silva@amcom.com.br>
2019-11-07 18:09:00 +0300

    Scripts 565 entrega_1

---
+++ 2019/11/P565_3_alter_comment_crapscn.sql
@@ -0,0 +1,8 @@
+-- Add comments to the columns 
+comment on column CECRED.crapscn.dsoparre
+  is 'Identifica os canais do convenio. A-Caixa/B-ATM/C-Agente credenciado/D-IB/E-Debito automatico/F-CNAB'
+/
+-- Add comments to the columns 
+comment on column CECRED.crapscn.cddmoden
+  is 'Origem da inclusão da fatura em débito automático'
+/
