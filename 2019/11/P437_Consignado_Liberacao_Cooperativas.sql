fc3e155be8a6d70ff5b3d17a8684deea41afb467
Jackson Barcellos <jackson.barcellos@ailos.coop.br>
2019-11-07 10:12:24 -0300

    P437 - Liberação das cooperativas no consignado Pacote3

---
+++ 2019/11/P437_Consignado_Liberacao_Cooperativas.sql
@@ -0,0 +1,21 @@
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 1, 'COOPER_CONSIGNADO', 'Cooperativas que podem utilizar o consignado e suas validações', 'S');
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 6, 'COOPER_CONSIGNADO', 'Cooperativas que podem utilizar o consignado e suas validações', 'S');
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 7, 'COOPER_CONSIGNADO', 'Cooperativas que podem utilizar o consignado e suas validações', 'S');
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 8, 'COOPER_CONSIGNADO', 'Cooperativas que podem utilizar o consignado e suas validações', 'S');
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 9, 'COOPER_CONSIGNADO', 'Cooperativas que podem utilizar o consignado e suas validações', 'S');
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 10, 'COOPER_CONSIGNADO', 'Cooperativas que podem utilizar o consignado e suas validações', 'S');
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 11, 'COOPER_CONSIGNADO', 'Cooperativas que podem utilizar o consignado e suas validações', 'S');
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 12, 'COOPER_CONSIGNADO', 'Cooperativas que podem utilizar o consignado e suas validações', 'S');
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 14, 'COOPER_CONSIGNADO', 'Cooperativas que podem utilizar o consignado e suas validações', 'S');
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 16, 'COOPER_CONSIGNADO', 'Cooperativas que podem utilizar o consignado e suas validações', 'S');
+commit;
