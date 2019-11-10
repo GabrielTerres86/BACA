773a223198d118d1189717d276efe7eab303d700
Otavio Theiss <otavio.theiss@ailos.coop.br>
2019-11-10 11:30:40 -0300

    PJ594 - removendo scripts de baca para criação de script consolidado

--- 2019/09/PJ594.014_Altera_comentario_coluna_TBCRD_APROVACAO_CARTAO.sql
+++
@@ -1,3 +0,0 @@
--- Add comments to the columns 
-comment on column CECRED.TBCRD_APROVACAO_CARTAO.indtipo_senha
-  is '1 - Cartao Magnetico TA, 2 - Cartao Cecred TA, 3 - Conta Online, 4 - Supervisor, 5 - Operador, 6 - Canais';
