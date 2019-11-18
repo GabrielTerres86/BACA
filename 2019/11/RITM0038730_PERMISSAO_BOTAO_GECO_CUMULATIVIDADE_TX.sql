6190db0e14f421ace6d18931ee541665ea4cb343
Guilherme Cervo (Supero) <t0032613@cecred.coop.br>
2019-11-18 17:15:34 -0300

    Alteracao de scripts e remocao de arquivo ddl

--- 2019/11/RITM0038730_PERMISSAO_BOTAO_GECO_CUMULATIVIDADE_TX.sql
+++ 2019/11/RITM0038730_PERMISSAO_BOTAO_GECO_CUMULATIVIDADE_TX.sql
@@ -1,38 +1,10 @@
-DECLARE
+/* Concede permissao a nova rotina 'T' para quem ja tem permissao na rotina 'I' */
 
-  CURSOR cr_crapcop IS
-    SELECT cop.cdcooper
-      FROM crapcop cop
-     WHERE cop.flgativo = 1;
-		   
-BEGIN
-  
-  FOR rw_crapcop IN cr_crapcop LOOP
-      -- Op  o 'T' Taxa Cumulatividade Grupo Economico
-      INSERT INTO crapace (
-			            NMDATELA
-									, CDDOPCAO
-									, CDOPERAD
-									, NMROTINA
-									, CDCOOPER
-									, NRMODULO
-									, IDEVENTO
-									, IDAMBACE) values (
-									'CONTAS'
-									, 'T'
-									, 'f0030519'
-									, 'GRUPO ECONOMICO'
-									, rw_crapcop.cdcooper
-									, 1
-									, 1
-									, 2);
-  END LOOP;
- 
-  
-  COMMIT;
-END;
+insert into crapace (nmdatela, cddopcao, cdoperad, nmrotina, cdcooper, nrmodulo, idevento, idambace)
+(select nmdatela, 'T' cddopcao, cdoperad, nmrotina, cdcooper, nrmodulo, idevento, idambace
+  from crapace
+ where UPPER(NMDATELA) = 'CONTAS'
+   and UPPER(NMROTINA) = 'GRUPO ECONOMICO'
+   and UPPER(CDDOPCAO) = 'I');
 
-
-
-
-
+commit; 
