e1a13da76a0f85596f2eaf9f5fe8cf15fc8aafe3
Douglas Alessandro Pagel (AMCOM) <t0031898@ailos.coop.br>
2019-11-20 08:47:29 -0300

    Correção de script reportado pelo Odirlei

--- 2019/11/P438_Insere_tbgen_sms_param.sql
+++ 2019/11/P438_Insere_tbgen_sms_param.sql
@@ -1,8 +1,10 @@
-FOR rw_crapcop IN (SELECT cdcooper FROM crapcop c WHERE c.flgativo = 1) LOOP 
+BEGIN
+	FOR rw_crapcop IN (SELECT cdcooper FROM crapcop c WHERE c.flgativo = 1) LOOP 
 
-  insert into tbgen_sms_param (CDCOOPER, FLGENVIA_SMS, FLGCOBRA_TARIFA, HRENVIO_SMS, CDOPERAD, DTULTIMA_ATU, CDPRODUTO, CDTARIFA_PF, CDTARIFA_PJ)
-  values (rw_crapcop.cdcooper, 1, 0, 0, '1', null, 31, null, null);
+	  insert into tbgen_sms_param (CDCOOPER, FLGENVIA_SMS, FLGCOBRA_TARIFA, HRENVIO_SMS, CDOPERAD, DTULTIMA_ATU, CDPRODUTO, CDTARIFA_PF, CDTARIFA_PJ)
+	  values (rw_crapcop.cdcooper, 1, 0, 0, '1', null, 31, null, null);
 
-END LOOP;
+	END LOOP;
 
-COMMIT;
+	COMMIT;
+END;
