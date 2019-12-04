02ed90460f0c6370643a6f8bf64b99fb49851713
Lucas Eduardo ranghetti <lucas.ranghetti@ailos.coop.br>
2019-12-04 18:01:36 -0300

    Ajuste

--- 2019/12/INC0032216_atualiza_agencia_dev_protesto.sql
+++ 2019/12/INC0032216_atualiza_agencia_dev_protesto.sql
@@ -9,8 +9,14 @@ BEGIN
       AND tr.dsconta_debitada = 76740
       AND tr.cdagenci_creditada = 100
       AND tr.vllanmto IN( 2366.94));
---  COMMIT;
+      
+   UPDATE crapsab b
+     SET b.nmbaisac = REGEXP_REPLACE(b.nmbaisac,'[^a-z_A-Z ]')
+   WHERE b.cdcooper > 0
+     AND b.nmbaisac LIKE '%<%>%';
+           
+  COMMIT;
 EXCEPTION 
   WHEN OTHERS THEN
-    dbms_output.put_line('Erro ao atualziar tbfin_recursos_movimento: '||sqlerrm);
+    dbms_output.put_line('Erro ao atualziar registros: '||sqlerrm);
 END;
