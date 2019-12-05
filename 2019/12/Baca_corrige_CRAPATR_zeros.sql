97a2693a14190c596d0f73a6e9caa3bf607ebe7b
Glauco Becaro <glauco.becaro@ailos.coop.br>
2019-12-05 17:17:32 -0300

    Tratatia 25 caracteres

--- 2019/12/Baca_corrige_CRAPATR_zeros.sql
+++ 2019/12/Baca_corrige_CRAPATR_zeros.sql
@@ -2,3 +2,6 @@
 
 update crapatr set cdseqtel = LPAD(cdrefere,8,'0') where cdhistor = 961 and cdseqtel like '000000000000000000000000000000%';
 commit;
+
+update crapatr set cdseqtel = LPAD(cdrefere,8,'0') where cdhistor = 48 and (cdseqtel like '9807  %' or cdseqtel like '9806  %') ;
+commit;
