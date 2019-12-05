ef5e7aee148d5c83a51c01d6a36e317e968b8bcd
Marcus Vinícius Fonseca de Abreu <marcus.abreu@supero.com.br>
2019-12-05 14:20:59 -0300

    Incluido no script a inclusao do valor de 250 mil, na coluna VLFGCOOP - Este é utilizado no calculo das Contas de Compensacao dentro da rotina PC_CRPS249.

--- 2019/10/P681_012_AdicionarFGCOOPTelaCADCOP.sql
+++ 2019/10/P681_012_AdicionarFGCOOPTelaCADCOP.sql
@@ -5,6 +5,12 @@ begin
   where NMDEACAO = 'ALTCOOP'
    and NMPACKAG = 'TELA_CADCOP';  
 
+  -- Atualiza a tabela da Cooperativa com o valor 250000,
+  -- definido ate este momento para o calculo das Contas de Compensacao
+  -- (valor excedente da soma dos depositos a vista + a prazo)
+  update CRAPCOP
+     set VLFGCOOP = 250000;	 
+	 
   COMMIT;
       
 end;  
