5df553b4263e41a3c6cb7a0eee740d901ba8eb76
Douglas Kloppel (MoutS) <douglas.kloppel@mouts.info>
2019-12-16 15:43:25 -0300

    Exclusão de lançamentos automáticos.

--- 2019/12/INC0033219_delete_craplau.sql
+++ 2019/12/INC0033219_delete_craplau.sql
@@ -1,20 +1,9 @@
 --Exclusão de registro da tabela craplau - INC0033219
 
-delete from craplau
-where cdcooper = 6
-  and cdagenci = 1
-  and dtmvtolt = '16/07/2019'
-  and ((nrdconta = 76066  and nrdocmto = 129) or
-       (nrdconta = 87262  and nrdocmto = 130) or
-       (nrdconta = 107468 and nrdocmto = 131) or
-       (nrdconta = 107859 and nrdocmto = 132) or
-       (nrdconta = 113964 and nrdocmto = 133) or
-       (nrdconta = 125776 and nrdocmto = 134) or
-       (nrdconta = 129763 and nrdocmto = 135) or
-       (nrdconta = 136930 and nrdocmto = 136) or
-       (nrdconta = 137863 and nrdocmto = 137) or
-       (nrdconta = 157384 and nrdocmto = 138) or
-       (nrdconta = 157414 and nrdocmto = 139) or
-       (nrdconta = 504734 and nrdocmto = 140));
+delete from craplau l
+where l.progress_recid in (29377869,29377870,29377871,
+						   29377872,29377873,29377874,
+                           29377875,29377876,29377877,
+						   29377878,29377879,29377880); 
 	   
 	COMMIT;	   
