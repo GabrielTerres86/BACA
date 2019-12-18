1776717e42219be162b26cf8e998c4b553925e6d
gbfronza <gabriel.fronza@mouts.info>
2019-12-18 16:12:17 -0300

    Commit

--- 2019/12/484_script_hotfix1812.sql
+++ 2019/12/484_script_hotfix1812.sql
@@ -2,7 +2,13 @@ begin
 
   delete from tbevento_grupos where cdcooper in (13,14);
   delete from tbevento_pessoa_grupos where cdcooper in (13,14);
-  
+  delete from tbgen_notif_automatica_prm where cdorigem_mensagem = 11 and cdoriegm_mensagem = 3;
+  update tbgen_notif_automatica_prm
+     set nrdias_semana = '1,2,3,4,5,6,7'
+       , nrsemanas_repeticao = '1,2,3,4,5'
+   where cdorigem_mensagem = 11
+     and cdmotivo_mensagem = 3;
+
   commit;
 
 end;  
