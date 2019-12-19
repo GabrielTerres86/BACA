b4f32af3f5ff5543d9b3ad4dca9f13f86332dc5e
Josiane <josiane.stiehler@amcom.com.br>
2019-12-19 10:58:48 -0300

    P426 - Ajuste no script

--- 2019/12/P426_SCRIPTS_CARGA_DADOS.sql
+++ 2019/12/P426_SCRIPTS_CARGA_DADOS.sql
@@ -1,14 +1,16 @@
 --------------------------------------------------------------------------------
 -- INCLUSÃO DE PARAMETRO DO VALOR DO LIMITE/DIA DO DEPOSITO DE CHEQUE NA TB045
 --------------------------------------------------------------------------------
+-- **** Deverá ser feito o script para todas as cooperativas, verifica os valores para pessoa fisica e juridica
+-- **** Deverá ter o valor para o Operacional e para Ailos
 update craptab 
 set dstextab = dstextab||'000000002000,00;000000300000,00'
 where  craptab.nmsistem = 'CRED' and
                             craptab.tptabela = 'GENERI'     AND
                             craptab.cdempres = 0            AND
                             craptab.cdacesso = 'LIMINTERNT' and
-                            craptab.cdcooper = 1  and 
-                            craptab.tpregist in (1,2);
+                            craptab.cdcooper = 1  and    -- coperativa
+                            craptab.tpregist in (1,2); --  fisica e Juridica
 COMMIT;
 
 --------------------------------------- 
@@ -16,7 +18,7 @@ COMMIT;
 ---------------------------------------
 -- select * from crapprm where upper(cdacesso) like '%EMAIL_CHEQUE_MOBILE%'
 -- Script para cadastrar email utilizado no processo do termo de aceito do deposito de cheque mobile
--- deverá ser cadastrado para todas as cooperativas
+-- ******* Deverá ser cadastrado para todas as cooperativas  ***********
 insert into crapprm 
 values ('CRED',1,'EMAIL_CHEQUE_MOBILE', 'Email utilizado processo de deposito de cheque Mobile','josiane.stiehler@amcom.com.br',null);
 
