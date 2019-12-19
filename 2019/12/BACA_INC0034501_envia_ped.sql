e3fd1d36c539953d57d4185beb6285df2d7c1807
Renato Cordeiro <renato.cordeiro@amcom.com.br>
2019-12-19 12:31:09 -0300

    Correção referente INC0034501

---
+++ 2019/12/BACA_INC0034501_envia_ped.sql
@@ -0,0 +1,13 @@
+begin
+  for creg (select cdcooper, nrpedido 
+            from crapfdc a 
+            where  a.dtconchq in ('04/12/2019' , '05/12/2019') and 
+                   dtemschq is  null 
+            group by cdcooper, nrpedido) loop
+     update tbchq_seguranca_cheque a
+     set a.idstatus_atualizacao_hsm = 2
+     where a.cdcooper = creg.cdcooper
+       and a.nrpedido = creg.nrpedido;
+  end loop;
+  commit;
+end;
