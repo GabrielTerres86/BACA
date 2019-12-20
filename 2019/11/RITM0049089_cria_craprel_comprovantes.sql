c237c696d456f2afdf00c175d3b72337aedaabc1
Lucas Reinert <lucas.reinert@ailos.coop.br>
2019-12-20 16:45:21 -0300

    Ajustado codigo do relatorio

--- 2019/11/RITM0049089_cria_craprel_comprovantes.sql
+++ 2019/11/RITM0049089_cria_craprel_comprovantes.sql
@@ -1,6 +1,6 @@
 -- Criar registro de relatório
 INSERT INTO craprel (cdrelato, nmrelato, nmformul, cdcooper, periodic)
-(SELECT 789, 'COMPROVANTE DE PAGAMENTO DARF', '132col', cdcooper, 'Online' FROM crapcop);
+(SELECT 794, 'COMPROVANTE DE PAGAMENTO DARF', '132col', cdcooper, 'Online' FROM crapcop);
 
 INSERT INTO craprel (cdrelato, nmrelato, nmformul, cdcooper, periodic)
 (SELECT 790, 'COMPROVANTE DE AGENDAMENTO DARF', '132col', cdcooper, 'Online' FROM crapcop);
