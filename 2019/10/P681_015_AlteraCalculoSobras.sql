0f6791f3ea143b4617fa7d3c8a1896821deb4e3c
Renato Darosci <supero-renato.darosci@ailos.coop.br>
2019-12-23 08:42:23 -0300

    P681 - Ajustes

--- 2019/10/P681_015_AlteraCalculoSobras.sql
+++ 2019/10/P681_015_AlteraCalculoSobras.sql
@@ -1,6 +1,6 @@
 BEGIN 
        UPDATE crapaca
-          SET lstparam = 'pr_ininccmi,pr_increret,pr_txretorn,pr_txjurcap,pr_txjurapl,pr_txjursdm,pr_txjurtar,pr_txreauat,pr_inpredef,pr_indeschq,pr_indemiti,pr_indestit,pr_unsobdep,pr_dssopfco,pr_dssopjco,pr_inentpub'
+          SET lstparam = 'pr_ininccmi,pr_increret,pr_txretorn,pr_txjurcap,pr_txjurapl,pr_txjursdm,pr_txjurtar,pr_txreauat,pr_inpredef,pr_indeschq,pr_indemiti,pr_indestit,pr_unsobdep,pr_dssopfco,pr_dssopjco,pr_inentpub,pr_txretcrd'
         WHERE nmdeacao = 'ALTERA_CALC_SOBRAS'
           AND nmpackag = 'TELA_SOL030'
           AND nmproced = 'pc_altera_calculo_sobras';
