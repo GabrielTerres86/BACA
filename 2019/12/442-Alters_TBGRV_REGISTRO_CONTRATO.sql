062f797e540c0dcd40d8957f9e13ce053f559864
Jean Domingos - Supero <jean.domingos@supero.com.br>
2019-12-23 16:58:20 -0300

    P442 - Adição do Baca com os alters table na tabela TBGRV_REGISTRO_CONTRATO

---
+++ 2019/12/442-Alters_TBGRV_REGISTRO_CONTRATO.sql
@@ -0,0 +1,11 @@
+alter table CECRED.TBGRV_REGISTRO_CONTRATO
+add dsc_identificador varchar(100);
+
+alter table CECRED.TBGRV_REGISTRO_CONTRATO
+add dsc_identificador_registro varchar(100);
+
+comment on column CECRED.TBGRV_REGISTRO_CONTRATO.dsc_identificador
+  is 'Identificador que retorna do serviço Gravames';
+  
+comment on column CECRED.TBGRV_REGISTRO_CONTRATO.dsc_identificador_registro
+  is 'Identificador Hash que retorna do serviço Gravames';
