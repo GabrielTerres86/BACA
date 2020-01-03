4a670693c9b75d8c8b938406a1caed38da113c21
Weverton Otoni <weverton.otoni@amcom.com.br>
2020-01-03 07:19:34 -0300

    Estória 27427 - Inclusão da lista de usuários no script de liberação

--- 2019/11/P450_01_DACAO_DE_BENS_ACESSOS.sql
+++ 2019/11/P450_01_DACAO_DE_BENS_ACESSOS.sql
@@ -9,7 +9,7 @@ DECLARE
     SELECT cdcooper
       FROM crapcop
      WHERE flgativo = 1
-       AND cdcooper <> 3
+       --AND cdcooper <> 3
      ORDER BY cdcooper;
   rw_crapcop cr_crapcop%ROWTYPE;
   
@@ -17,7 +17,45 @@ DECLARE
     SELECT cdoperad
       FROM crapope t
      WHERE cdcooper = pr_cdcooper
-       AND upper(t.cdoperad) IN ('F0020295'); -- Pendente incluir lista de usuários.
+       AND upper(t.cdoperad) IN ('F0032951'
+                                ,'F0030688'
+                                ,'F0130101'
+                                ,'F0130140'
+                                ,'F0130170'
+                                ,'F0130241'
+                                ,'F0130259'
+                                ,'F0090197'
+                                ,'F0090256'
+                                ,'F0090295'
+                                ,'F0090330'
+                                ,'F0090407'
+                                ,'F0140108'
+                                ,'F0140113'
+                                ,'F0140132'
+                                ,'F0140178'
+                                ,'F0120029'
+                                ,'F0120010'
+                                ,'F0050121'
+                                ,'F0050084'
+                                ,'F0050184'
+                                ,'F0050200'
+                                ,'F0020174'
+                                ,'F0020295'
+                                ,'F0160248'
+                                ,'F0160055'
+                                ,'F0160039'
+                                ,'F0010104'
+                                ,'F0012926'
+                                ,'F0010727'
+                                ,'F0013502'
+                                ,'F0013301'
+                                ,'F0060168'
+                                ,'F0060078'
+                                ,'F0060125'
+                                ,'F0060067'
+                                ,'F0100108'
+                                ,'F0100160'
+                                ,'F0080004');
   rw_crapope cr_crapope%ROWTYPE;
   sigla_tela  VARCHAR2(400);
   rotina_tela VARCHAR2(400);
