e0e9b6e073532fbbd725a7dbe4af628af87ed513
Ramon Martins <ramon.silva@ailos.coop.br>
2020-01-07 14:20:58 -0300

    RDM0033680-INC0034599

--- 2019/12/RDM0033680.sql
+++ 2019/12/RDM0033680.sql
@@ -2,6 +2,13 @@
 DELETE
   from craplim
  where cdcooper = 1
-   and nrdconta = 3605329 
+   and nrdconta = 3605329 ;
+   
+UPDATE crapepr   
+   SET inliquid = 1
+ WHERE cdcooper = 1
+   AND nrdconta = 3559238
+   AND nrctremp = 804907;
+   
 
 commit;
