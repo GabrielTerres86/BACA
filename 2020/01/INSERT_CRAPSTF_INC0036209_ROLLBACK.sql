af9f17ac605e83fb473cd6fd6a7a7bda8c41d967
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2020-01-13 09:49:14 -0300

    INC0036209 - Gerar saldo do caixa eletrônico
    Gerar saldo do caixa eletrônico

---
+++ 2020/01/INSERT_CRAPSTF_INC0036209_ROLLBACK.sql
@@ -0,0 +1,20 @@
+DELETE crapstf
+ WHERE dtmvtolt = '13/01/2020'
+   AND nrterfin = 1
+   AND cdcooper = 10;
+
+COMMIT;
+
+DELETE crapstf
+ WHERE dtmvtolt = '13/01/2020'
+   AND nrterfin = 1
+   AND cdcooper = 12;
+
+COMMIT;
+
+DELETE crapstf
+ WHERE dtmvtolt = '13/01/2020'
+   AND nrterfin = 10
+   AND cdcooper = 16;
+
+COMMIT;
