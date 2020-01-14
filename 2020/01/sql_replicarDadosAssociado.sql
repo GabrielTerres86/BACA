5df614c00a84ff51e7dde1e7be4863afc4fae8b2
Thiago Coppi <thiago.silva@supero.com.br>
2020-01-14 16:23:14 -0300

    Adicionado sql revitalização parametros e update na crapaca

---
+++ 2020/01/sql_replicarDadosAssociado.sql
@@ -0,0 +1,3 @@
+﻿--Update da replicar associado
+UPDATE CRAPACA SET NMPACKAG = NULL, NMPROCED = 'replicarDadosAssociado' WHERE NMPROCED = 'pc_duplica_conta' AND NMPACKAG = 'CADA0003' AND NRSEQRDR = 211;
+
