b5f8b85b6cd8829b85fee4f75f420919ba25c81b
Jaison Fernando Ferreira <jaison.ferreira@ailos.coop.br>
2020-01-28 14:41:03 -0300

    Correção do INC0034158

---
+++ 2020/01/INC0034158.sql
@@ -0,0 +1,98 @@
+DECLARE
+  CURSOR cr_tbcc_prejuizo_detalhe(pr_cdcooper IN tbcc_prejuizo_detalhe.cdcooper%TYPE
+                                 ,pr_nrdconta IN tbcc_prejuizo_detalhe.nrdconta%TYPE
+                                 ,pr_cdhistor IN tbcc_prejuizo_detalhe.cdhistor%TYPE
+                                 ,pr_dtmvtolt IN tbcc_prejuizo_detalhe.dtmvtolt%TYPE
+                                 ,pr_vllanmto IN tbcc_prejuizo_detalhe.vllanmto%TYPE) IS
+    SELECT cdcooper, nrdconta, idlancto, vllanmto
+      FROM tbcc_prejuizo_detalhe
+     WHERE cdcooper = pr_cdcooper
+       AND nrdconta = pr_nrdconta
+       AND cdhistor = pr_cdhistor
+       AND dtmvtolt = pr_dtmvtolt
+       AND vllanmto = pr_vllanmto;
+  rw_tbcc_prejuizo_detalhe cr_tbcc_prejuizo_detalhe%ROWTYPE;
+
+  vr_cdcritic   NUMBER;
+  vr_dscritic   VARCHAR2(10000);
+  vr_exc_saida  EXCEPTION;
+  vr_idlancto   tbcc_prejuizo_detalhe.idlancto%TYPE;
+  vr_vllanmto   tbcc_prejuizo_detalhe.vllanmto%TYPE;
+  vr_cdcooper   tbcc_prejuizo_detalhe.cdcooper%TYPE;
+  vr_nrdconta   tbcc_prejuizo_detalhe.nrdconta%TYPE;
+  vr_dtmvtolt   craplcm.dtmvtolt%TYPE;
+  vr_cdhistor   tbcc_prejuizo_detalhe.cdhistor%TYPE;
+  vr_idprejuizo tbcc_prejuizo.idprejuizo%TYPE;
+
+BEGIN
+
+  dbms_output.put_line('Inicio');
+
+  -- Credifoz: 436976
+  vr_cdcooper := 11;
+  vr_nrdconta := 436976;
+  vr_vllanmto := 24.87;
+  vr_cdhistor := 2721;
+  
+  -- Busca Data Atual da Cooperativa
+  BEGIN
+    SELECT dtmvtolt
+    INTO   vr_dtmvtolt
+    FROM   crapdat
+    WHERE  cdcooper = vr_cdcooper;
+  EXCEPTION
+    WHEN OTHERS THEN
+      vr_dscritic := 'Erro ao Buscar Data da Cooperatriva. Erro: '||SubStr(SQLERRM,1,255);
+      RAISE vr_exc_saida; 
+  END;
+
+  BEGIN
+    UPDATE tbcc_prejuizo
+       SET vlsdprej = vlsdprej - vr_vllanmto
+     WHERE cdcooper = vr_cdcooper
+       AND nrdconta = vr_nrdconta
+    RETURNING idprejuizo 
+      INTO vr_idprejuizo;
+
+    INSERT INTO tbcc_prejuizo_detalhe (
+        dtmvtolt
+       ,nrdconta
+       ,cdhistor
+       ,vllanmto
+       ,dthrtran
+       ,cdoperad
+       ,cdcooper
+       ,idprejuizo
+       ,nrctremp
+      )
+      VALUES (
+        vr_dtmvtolt
+       ,vr_nrdconta
+       ,vr_cdhistor
+       ,vr_vllanmto
+       ,SYSDATE
+       ,1
+       ,vr_cdcooper
+       ,vr_idprejuizo
+       ,0
+      );
+
+  END;
+
+  COMMIT;
+
+  dbms_output.put_line('Término');
+
+EXCEPTION
+
+  WHEN vr_exc_saida THEN
+    ROLLBACK;
+    dbms_output.put_line('Mensagem: ' || vr_dscritic);
+    RAISE_APPLICATION_ERROR(-20500,vr_dscritic);
+  WHEN OTHERS THEN
+    ROLLBACK;
+    vr_dscritic := 'Erro genérico: ' || SQLERRM;
+    dbms_output.put_line('Mensagem: ' || vr_dscritic);
+    RAISE_APPLICATION_ERROR(-20510,vr_dscritic);
+
+END;
