f5b92250bf944cc81d3e83cabaaeb1ecf708ddd6
Jackson Barcellos <jackson.barcellos@ailos.coop.br>
2020-02-03 14:02:26 -0300

    Ajuste saldo crapris e crapvri nrdconta = 8376573 and nrctremp = 1864288

---
+++ 2020/02/INC0038232.sql
@@ -0,0 +1,12 @@
+update crapris 
+set vldivida = 6954.17
+    ,vlvec180 = 6954.17
+    ,qtdiaatr = 0    
+where cdcooper =1 and nrdconta = 8376573 and nrctremp = 1864288 and progress_recid = 1304399284;
+
+update crapvri 
+set vldivida = 6954.17
+,vlempres = 6954.17
+where cdcooper =1 and nrdconta = 8376573 and nrctremp = 1864288 and progress_recid = 4217861213;
+
+commit;
