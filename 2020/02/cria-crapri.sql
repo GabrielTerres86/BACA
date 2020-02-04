5d9035cef4fb392f646f3579979b133dbf2b7ff6
Anderson Fossa <anderson.fossa@ailos.coop.br>
2020-02-04 12:23:33 -0300

    Adicao de critica na crapcri.

--- 2020/02/cria-crapri.sql
+++ 2020/02/cria-crapri.sql
@@ -1,2 +1,2 @@
-insert into crapcri (cdcritic, dscritic, tpcritic, flgchama) values (1511, '1511 - Não foi possível atualizar a qtd. cotas do dia anterior!', 4, 0);
+insert into crapcri (cdcritic, dscritic, tpcritic, flgchama) values (1511, '1511 - Nao foi possivel atualizar a qtd. cotas do dia anterior!', 4, 0);
 commit;
