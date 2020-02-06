f2dba44a0ea8e5d47783ff9c2b25afff59bb65d7
Darlei Fernando Zillmer <darlei.zillmer@supero.com.br>
2020-02-06 16:12:08 -0300

    correção baca para prg

--- 2019/10/P442_inserts_programa_aca.sql
+++ 2019/10/P442_inserts_programa_aca.sql
@@ -1,14 +1,61 @@
 BEGIN
-	--RDR
+  DECLARE
+    CURSOR cr_cop IS
+      SELECT c.cdcooper
+        FROM crapcop c
+       WHERE c.flgativo = 1;
+    rw_cop cr_cop%ROWTYPE;
+    
+    vr_nrsolici INTEGER := 10001;
+  BEGIN
+    --RDR
 	insert into craprdr (NMPROGRA, DTSOLICI)
 	values ('GRVM0002', SYSDATE);
 	
-	--PRG
-	insert into CRAPPRG (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-	values ('CRED', 'GRVM0002', 'Definições de contratos/imagens', ' ', ' ', ' ', 50, 10001, 1, 0, 0, 0, 0, 0, 1, 3, null);
+    FOR rw_cop IN cr_cop LOOP
+      INSERT INTO CRAPPRG
+        (NMSISTEM,
+         CDPROGRA,
+         DSPROGRA##1,
+         DSPROGRA##2,
+         DSPROGRA##3,
+         DSPROGRA##4,
+         NRSOLICI,
+         NRORDPRG,
+         INCTRPRG,
+         CDRELATO##1,
+         CDRELATO##2,
+         CDRELATO##3,
+         CDRELATO##4,
+         CDRELATO##5,
+         INLIBPRG,
+         CDCOOPER,
+         QTMINMED)
+      VALUES
+        ('CRED',
+         'GRVM0002',
+         'Definições de contratos/imagens',
+         ' ',
+         ' ',
+         ' ',
+         50,
+         vr_nrsolici,
+         1,
+         0,
+         0,
+         0,
+         0,
+         0,
+         1,
+         rw_cop.cdcooper,
+         NULL);
+         vr_nrsolici := vr_nrsolici + 1;
+    END LOOP;
 
-	insert into crapaca (NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
+    insert into crapaca (NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
 	values ('VALIDA_INCLUSAO_CONTRATO', 'GRVM0002', 'pc_valida_inclusao_contrato', 'pr_cdcooper,pr_nrdconta,pr_nrctrpro,pr_tpctrpro,pr_idseqbem,pr_cddopcao,pr_tpinclus', (SELECT NRSEQRDR FROM craprdr WHERE NMPROGRA = 'GRVM0002'));
-	
+
 	COMMIT;
+  END;
 END;
+
