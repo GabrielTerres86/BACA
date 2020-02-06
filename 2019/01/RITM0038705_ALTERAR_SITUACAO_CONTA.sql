0847c0379471aa8c430f9441ee80e26a2af46cf9
Guilherme Cervo (Supero) <t0032613@cecred.coop.br>
2020-02-06 16:34:52 -0300

    Adicionado cursor de cooperativas

--- 2019/01/RITM0038705_ALTERAR_SITUACAO_CONTA.sql
+++ 2019/01/RITM0038705_ALTERAR_SITUACAO_CONTA.sql
@@ -1,5 +1,24 @@
-UPDATE crapass a 
-   SET a.cdsitdct = 1 
- WHERE a.cdsitdct = 0;
- 
- COMMIT;
+-- Created on 15/01/2020 by T0032613 
+DECLARE 
+
+    ------> Cursor de cooperativas
+    CURSOR cr_crapcop IS
+		SELECT cdcooper
+			   ,flgativo
+	      FROM crapcop
+	     WHERE cdcooper <> 3
+	     ORDER BY cdcooper DESC;
+
+BEGIN
+  
+	FOR coop IN cr_crapcop LOOP
+
+		UPDATE crapass a 
+		   SET a.cdsitdct = 1 
+		 WHERE a.cdcooper = coop.cdcooper 
+		   AND a.cdsitdct = 0;
+  
+	END LOOP;	 
+	COMMIT;
+
+END;
