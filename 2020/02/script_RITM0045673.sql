66d74076034cc85904b5926b423449b47cdf08e0
Yuri Krueger (Mouts) <yuri.krueger@mouts.info>
2020-02-13 17:28:07 -0300

    Script para inclusão de parametros referentes RITM0045673

---
+++ 2020/02/script_RITM0045673.sql
@@ -0,0 +1,5 @@
+begin
+	insert into crapprm (nmsistem,cdcooper,cdacesso,dstexprm,dsvlrprm) values
+	('CRED',0,'CRD_MONITORAMENTO','Email da area de negocio produto cartoes',' cartoes@ailos.coop.br,gionei.correia@ailos.coop.br,fabricio@ailos.coop.br');
+    commit;
+end;
