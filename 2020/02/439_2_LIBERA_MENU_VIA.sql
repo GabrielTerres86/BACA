716736778805a40a51f997de8ade5a46b7a870d7
Werinton Ferrari <werinton.ferrari@ailos.coop.br>
2020-02-20 17:43:48 -0300

    Removido baca que esta sendo enviado via hotfix. Novo PR 2032

--- 2020/02/439_2_LIBERA_MENU_VIA.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO CECRED.CRAPPRM (NMSISTEM,CDCOOPER,CDACESSO,DSTEXPRM,DSVLRPRM) VALUES ('CRED','1', 'LIBERA_COOP_CDC_IB' , 'Flag para determinar se o menu do CDC ira aparecer para a cooperativa' , '1' );
-
-commit;
