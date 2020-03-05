9837985b5d0d34485e7e211ea2fa31c0f0d079dc
Werinton Ferrari <werinton.ferrari@cecred.coop.br>
2020-03-05 14:56:52 -0300

    baca para alterar vinculo de cdc

---
+++ 2020/03/INC0040712.sql
@@ -0,0 +1,3 @@
+update tbepr_cdc_vendedor vendedor set vendedor.idcooperado_cdc = 2400 where vendedor.idvendedor = 6985;
+update tbepr_cdc_vendedor vendedor set vendedor.idcooperado_cdc = 572 where vendedor.idvendedor = 7072;
+commit;
