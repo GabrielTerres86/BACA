5b4e2f03d45de7e5f6405d4c01453d36e16424e5
Josiane <josiane.stiehler@amcom.com.br>
2020-03-20 15:21:26 -0300

    Criação de parametro

--- 2020/03/INC0040265_insert_crapprm.sql
+++ 2020/03/INC0040265_insert_crapprm.sql
@@ -1,3 +1,5 @@
 insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
 values ('CRED', 0, 'HISTOR_SALDO_FIM_SEMANA', 'Históricos que contabilizam no saldo do fim de semana', '2308,2719', null);
+
 commit;
+
