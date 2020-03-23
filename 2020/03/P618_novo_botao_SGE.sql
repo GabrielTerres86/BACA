95e3876062b56b0acd0c406c5aae5e62acd525c6
Glauco Becaro <glauco.becaro@ailos.coop.br>
2020-03-23 13:35:01 -0300

    comentário commit

--- 2020/03/P618_novo_botao_SGE.sql
+++ 2020/03/P618_novo_botao_SGE.sql
@@ -1,7 +1,7 @@
 begin
   insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
   values ('CRED', 0, 'URL_NOVO_SGE', 'URL do portal Dynamics do novo Sistema de Gerenciamento de Eventos', 'https://ailosevents.powerappsportals.com/home');
---  commit;
+  commit;
 exception
   when others then
     cecred.pc_internal_exception;
