8ab315d481bd69e7efb9530fa50f11822cd9537e
Glauco Becaro <glauco.becaro@ailos.coop.br>
2020-03-23 13:32:45 -0300

    P681 parâmetro de botão para novo SGE

---
+++ 2020/03/P618_novo_botao_SGE.sql
@@ -0,0 +1,8 @@
+begin
+  insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+  values ('CRED', 0, 'URL_NOVO_SGE', 'URL do portal Dynamics do novo Sistema de Gerenciamento de Eventos', 'https://ailosevents.powerappsportals.com/home');
+--  commit;
+exception
+  when others then
+    cecred.pc_internal_exception;
+end;
