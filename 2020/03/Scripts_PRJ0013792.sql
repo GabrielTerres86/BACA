b66fa1d4810d3665fb87f00a345f3a16ebca2879
Elton Giusti <elton.giusti@amcom.com.br>
2020-03-25 15:21:07 -0300

    P13792 - incluir motivos anulação e Carga Parest

--- 2020/03/Scripts_PRJ0013792.sql
+++ 2020/03/Scripts_PRJ0013792.sql
@@ -117,3 +117,120 @@ BEGIN
 END;
 ---------------------
 
+DECLARE
+/*
+  Autor   : 
+  Data    : Março/2020
+
+  Frequencia: Especifico
+
+  Objetivo  : Rotina para popular nova tabela de parametros TBCRED_PARAMETRO_ANALISE, com tipo de produto 3 (Limite Crédito),
+*/
+
+  -- Variáveis
+  vr_qtcooper NUMBER;
+  vr_dscritic VARCHAR2(10000);
+
+  CURSOR cr_crapcop IS
+    SELECT   0 incontigen, --Não
+
+           0 incomite, -- Não
+
+           1  inanlautom, -- Sim
+           'PoliticaLimiteCreditoPF' nmregmpf,
+           'PoliticaLimiteCreditoPJ' nmregmpj,
+           GENE0001.fn_param_sistema(pr_nmsistem => 'CRED',
+                                     pr_cdcooper => cop.cdcooper,
+                                     pr_cdacesso => 'TIME_RESP_MOTOR_DESC') qtsstime,                                     
+           GENE0001.fn_param_sistema(pr_nmsistem => 'CRED',
+                                     pr_cdcooper => cop.cdcooper,
+                                     pr_cdacesso => 'QTD_MES_HIST_DEVCHQ_DESC') qtmeschq,
+           GENE0001.fn_param_sistema(pr_nmsistem => 'CRED',
+                                     pr_cdcooper => cop.cdcooper,
+                                     pr_cdacesso => 'QTD_MES_HIST_DC_A11_DESC') qtmeschqal11,
+           GENE0001.fn_param_sistema(pr_nmsistem => 'CRED',
+                                     pr_cdcooper => cop.cdcooper,
+                                     pr_cdacesso => 'QTD_MES_HIST_DC_A12_DESC') qtmeschqal12,  
+           GENE0001.fn_param_sistema(pr_nmsistem => 'CRED',
+                                     pr_cdcooper => cop.cdcooper,
+                                     pr_cdacesso => 'QTD_MES_HIST_EST_DESC') qtmesest,
+           GENE0001.fn_param_sistema(pr_nmsistem => 'CRED',
+                                     pr_cdcooper => cop.cdcooper,
+                                     pr_cdacesso => 'QTD_MES_HIST_EMPRES_DESC') qtmesemp,
+           --
+           cdcooper,
+           Initcap(nmrescop) nmrescop
+
+      FROM crapcop cop
+     WHERE cop.flgativo = 1
+    -- and cop.cdcooper =1
+     ORDER BY cop.cdcooper;
+  rw_crapcop cr_crapcop%ROWTYPE;
+
+begin
+  FOR rw_crapcop  in cr_crapcop  LOOP
+      -- Grava linha para as cooperativas
+      BEGIN
+        INSERT INTO CECRED.TBCRED_PARAMETRO_ANALISE (
+              cdcooper,
+              tpproduto,
+              incomite,
+              incontigen,
+              inanlautom,
+              nmregmpf,
+              nmregmpj,
+              qtsstime,
+              qtmeschq,
+              qtmeschqal11,
+              qtmeschqal12,
+              qtmesest,
+              qtmesemp)        
+        VALUES(
+             rw_crapcop.cdcooper,  -- cdcooper,
+             3,  -- tpproduto, --Limite Crédito
+             rw_crapcop.incomite, -- incomite,
+             rw_crapcop.incontigen, -- incontigen,
+             rw_crapcop.inanlautom,  -- inanlautom,
+             rw_crapcop.nmregmpf,  -- nmregmpf,
+             rw_crapcop.nmregmpj, -- nmregmpj,
+             rw_crapcop.qtsstime,  -- qtsstime,
+             rw_crapcop.qtmeschq,  -- qtmeschq,
+             NVL(rw_crapcop.qtmeschqal11,0), -- qtmeschqal11,
+             NVL(rw_crapcop.qtmeschqal12,0), -- qtmeschqal12,
+             rw_crapcop.qtmesest,  -- qtmesest,
+             rw_crapcop.qtmesemp  -- qtmesemp
+             )  ;     
+      EXCEPTION
+        WHEN OTHERS THEN
+          vr_dscritic := 'Não inseriu linha na tab TBCRED_PARAMETRO_ANALISE - ' || SQLERRM;
+          DBMS_OUTPUT.PUT_LINE (vr_dscritic);
+          EXIT;
+      END;
+  END LOOP;
+  Commit;
+END;
+/
+
+-- criar motivos anulação
+insert into tbcadast_motivo_anulacao
+( cdcooper
+,cdmotivo
+,dsmotivo
+,tpproduto
+,dtcadastro
+,inobservacao
+,idativo
+,incanaldigital
+)
+SELECT cdcooper
+,cdmotivo
+,dsmotivo
+,4 --tpproduto Limite Crédito
+,dtcadastro
+,inobservacao
+,idativo
+,incanaldigital
+  from tbcadast_motivo_anulacao t
+ WHERE tpproduto =3;
+ 
+ commit;
