a5b4846750cf7d792abd8d41b787efb71a694707
Diógenes Lazzarini <diogenes.lazzarini@ailos.coop.br>
2020-03-27 14:06:24 -0300

    Create baca_risco_melhora_2703.sql

---
+++ 2020/03/baca_risco_melhora_2703.sql
@@ -0,0 +1,20 @@
+
+update tbrisco_operacoes set inrisco_melhora = 3 where cdcooper = 1 and nrdconta = 3129292 and nrctremp = 1020619;
+update tbrisco_operacoes set inrisco_melhora = 5 where cdcooper = 1	and nrdconta = 9901558 and nrctremp = 1740274;
+update tbrisco_operacoes set inrisco_melhora = 3 where cdcooper = 1 and nrdconta = 8852901 and nrctremp = 877630;
+update tbrisco_operacoes set inrisco_melhora = 3 where cdcooper = 1 and nrdconta = 8852901 and nrctremp = 1278684;
+update tbrisco_operacoes set inrisco_melhora = 4 where cdcooper = 1 and nrdconta = 3864758 and nrctremp = 783651;
+update tbrisco_operacoes set inrisco_melhora = 3 where cdcooper = 1 and nrdconta = 8852901 and nrctremp = 877618;
+update tbrisco_operacoes set inrisco_melhora = 7 where cdcooper = 1 and nrdconta = 10318089 and nrctremp = 1457318;
+update tbrisco_operacoes set inrisco_melhora = 5 where cdcooper = 1 and nrdconta = 2518056 and nrctremp = 970904;
+update tbrisco_operacoes set inrisco_melhora = 7 where cdcooper = 13 and nrdconta = 252328 and nrctremp = 29660;
+update tbrisco_operacoes set inrisco_melhora = 7 where cdcooper = 13 and nrdconta = 252328 and nrctremp = 48643;
+update tbrisco_operacoes set inrisco_melhora = 5 where cdcooper = 13 and nrdconta = 603554 and nrctremp = 13679;
+update tbrisco_operacoes set inrisco_melhora = 3 where cdcooper = 13 and nrdconta = 354317 and nrctremp = 47016;
+update tbrisco_operacoes set inrisco_melhora = 7 where cdcooper = 16 and nrdconta = 141712 and nrctremp = 102134;
+update tbrisco_operacoes set inrisco_melhora = 4 where cdcooper = 16 and nrdconta = 234338 and nrctremp = 35718;
+update tbrisco_operacoes set inrisco_melhora = 4 where cdcooper = 16 and nrdconta = 234338 and nrctremp = 132954;
+update tbrisco_operacoes set inrisco_melhora = 4 where cdcooper = 16 and nrdconta = 234338 and nrctremp = 61078;
+update tbrisco_operacoes set inrisco_melhora = 5 where cdcooper = 9 and nrdconta = 173053  and nrctremp = 6906;
+
+commit;
