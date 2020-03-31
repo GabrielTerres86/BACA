f54983e40b0a63f0efbec67daf55343dae3edafd
Paulo Martins <paulo.martins@mouts.info>
2020-03-31 18:29:06 -0300

    Incluído Update crawepr

--- 2020/03/INC0039863_script.sql
+++ 2020/03/INC0039863_script.sql
@@ -6,4 +6,12 @@ update tbgar_cobertura_operacao o
    and insituacao = 1 
    and nrconta_terceiro = 219428 
    and nrcontrato = 255585;
+update crawepr e
+   set e.idcobope = 0,
+       e.idcobefe = 0
+ where e.nrdconta = 689211 
+   and e.nrctremp = 255585
+   and e.cdcooper = 2;
    commit;
+   
+   
