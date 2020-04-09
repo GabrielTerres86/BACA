793e2bef1e1211c8682c91fafc1cd47cb1bbb13b
Guilherme Cervo (Supero) <t0032613@cecred.coop.br>
2020-04-09 09:24:53 -0300

    Parametro de intervalo de dias

---
+++ 2020/04/P593_594_relatorios_insert_prm_intervalo.sql
@@ -0,0 +1,3 @@
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 0, 'JOB_INTERV_CARTOES', 'Intervalo de dias de execução do job para geração dos relatórios 593 e 594', '15');
+commit;
