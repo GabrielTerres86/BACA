8b750c4778ab345e2632106fc53b51250e1a4796
gabrielfronza <gfronzaeng@gmail.com>
2020-04-16 13:20:35 -0300

    Update

--- 2020/04/RITM0046001_script_email_prm.sql
+++ 2020/04/RITM0046001_script_email_prm.sql
@@ -1,5 +1,13 @@
 begin
 
+  insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+  values ('CRED'
+         ,0
+         ,'CHQ_UNICRED_LST_EMAIL'
+         ,'Email das pessoas responsáveis por receber a informação de '||
+          'sinistros em cheques UNICRED'
+         ,'prev.fraudes03@ailos.coop.br');
+
   for rw_crapcop in (select cdcooper from crapcop order by cdcooper) loop
     insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
     values ('CRED'
