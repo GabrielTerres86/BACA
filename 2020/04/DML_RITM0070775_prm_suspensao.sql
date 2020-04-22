5b2c130dbac949512613a82c9e09eb4e7b91bd2c
Odirlei Busana <odirlei.busana@ailos.coop.br>
2020-04-22 09:34:31 -0300

    RITM0070775 - Inclusao de periodo de suspensão de cobrança de juros de mora e multa

---
+++ 2020/04/DML_RITM0070775_prm_suspensao.sql
@@ -0,0 +1,4 @@
+
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 0, 'PER_SUSPENSAO_JUR_EPR', 'Periodo de suspensao de cobrança de juros de mora e multa', '01/04/2020;30/06/2020');
+COMMIT;
