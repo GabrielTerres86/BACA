733093555de6ce8fb805fba4e26abe07e0470268
Odirlei Busana <odirlei.busana@ailos.coop.br>
2020-04-22 19:36:39 -0300

    Ajuste script

--- 2020/04/DML_RITM0070775_prm_suspensao.sql
+++ 2020/04/DML_RITM0070775_prm_suspensao.sql
@@ -1,4 +1,5 @@
 
 insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
-values ('CRED', 0, 'PER_SUSPENSAO_JUR_EPR', 'Periodo de suspensao de cobrança de juros de mora e multa', '01/04/2020;30/06/2020');
+SELECT  'CRED', c.cdcooper, 'PER_SUSPENSAO_JUR_EPR', 'Periodo de suspensao de cobrança de juros de mora e multa', '01/04/2020;30/06/2020'
+ FROM crapcop c;
 COMMIT;
