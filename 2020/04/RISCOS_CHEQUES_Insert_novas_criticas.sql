0e68a66216a73174bad21c77d727135e4bf91694
Guilherme Boettcher <guilherme.boettcher@amcom.com.br>
2020-04-24 08:54:03 -0300

    CHEQUE - Criacao 2 novas criticas

---
+++ 2020/04/RISCOS_CHEQUES_Insert_novas_criticas.sql
@@ -0,0 +1,7 @@
+insert into cecred.crapcri (CDCRITIC, DSCRITIC,  TPCRITIC, FLGCHAMA)
+values (1099, '1099 - Contra-Ordem Conta Prejuizo', 4, 0);
+
+insert into cecred.crapcri (CDCRITIC, DSCRITIC,  TPCRITIC, FLGCHAMA)
+values (1129, '1129 - Operacao nao permitida em Conta Prejuizo', 4, 0);
+
+commit;
