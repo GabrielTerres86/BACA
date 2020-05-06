92cbbb9524365f7a3ce26899f4f3df400a1be2f0
Guilherme Cervo (Supero) <t0032613@cecred.coop.br>
2020-05-06 16:21:34 -0300

    Parametros para controle de log com bancoob

---
+++ 2020/05/RITM0073462_BACA_INSERE_PARAMETRO_EMAIL_NOTIFICACAO_ERRO_PRM.sql
@@ -0,0 +1,22 @@
+BEGIN
+DELETE FROM crapprm p WHERE p.cdacesso = 'EMAIL_ERR_CON_BANCOOB';
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 1, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 2, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 3, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 4, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 5, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 6, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 7, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 8, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 9, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 10, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 11, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 12, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 13, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 14, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 15, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 16, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+INSERT INTO CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES ('CRED', 17, 'EMAIL_ERR_CON_BANCOOB', 'Email para receber informacao de quando ocorrer erro na comunicacao com o bancoob', 'otavio.theiss@ailos.coop.br');
+COMMIT;
+END;
+
