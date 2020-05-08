fb510a4b5cc8947c7e3b1127c8e275339e9e8605
Alexandre Diego Girardi <t0033227@cecred.coop.br>
2020-05-08 16:21:06 -0300

    P513 - Fase 2 - Depósito Varejista

---
+++ 2020/05/P513_INSERT_CRAPCRI.sql
@@ -0,0 +1,15 @@
+INSERT INTO crapcri(cdcritic,
+                    dscritic,
+                    progress_recid,
+                    tpcritic,
+                    flgchama)
+              VALUES( 5017
+                   , '28 - Conta varejista não reconhecida'
+                   , null
+                   , 1
+                   , 0);
+
+COMMIT;                 
+
+
+
