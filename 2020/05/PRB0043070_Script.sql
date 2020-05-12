a5e4e24f8bd5dbe26ae63368becea29a05dadce7
Paulo Martins <paulo.martins@mouts.info>
2020-05-12 16:49:58 -0300

    Tratamento Especial para 2 casos

--- 2020/05/PRB0043070_Script.sql
+++ 2020/05/PRB0043070_Script.sql
@@ -10,7 +10,7 @@ declare
          tbgar_cobertura_operacao c
    where e.idcobope = c.idcobertura   
      and e.nrctremp <> c.nrcontrato     
-     and e.idcobope in (801,992,7870,21619,21726,23831,26680)
+     and e.idcobope in (801,992,21619,23831,26680)
    order by e.idcobope; 
                       
 begin
@@ -22,6 +22,29 @@ begin
        and p.nrdconta = r1.nrdconta
        and p.nrctremp = r1.nrctremp;
   end loop;
+  -- 
+  /*7870*/
+  update tbgar_cobertura_operacao tb
+     set tb.nrcontrato = 221887
+   where tb.idcobertura = 7870 ;
+  --
+  update crawepr p
+     set p.idcobope = 0,
+         p.idcobefe = 0
+   where p.cdcooper = 6
+     and p.nrdconta = 30791
+     and p.nrctremp = 221888;   
+  /*21726*/ 
+  update tbgar_cobertura_operacao tb
+     set tb.nrcontrato = 31900
+   where tb.idcobertura = 21726 ;
+  -- 
+  update crawepr p
+     set p.idcobope = 0,
+         p.idcobefe = 0
+   where p.cdcooper = 7
+     and p.nrdconta = 38768
+     and p.nrctremp = 31803;   
   Commit;
 exception
   when others then
