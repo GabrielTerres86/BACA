afafdf150c48635ff1da30b4f544cf8710fba1db
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2020-05-14 16:10:59 -0300

    P513.2 - Depósito Varejista - Scripts
    Scripts:
     - Criar opção da tela ATENDA
     - Gerar acesso a nova opção da tela
     - Criar crapaca para realizar as operações na tela nova
     - Inserir parametro com o código da transação da Saque & Pague
     - Gerar novos códigos de erro

---
+++ 2020/05/P513_inserir_parametro_sistema.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO crapprm
+  (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+VALUES
+  ('CRED',
+   0,
+   'CDPROC_DEP_VAR_SEP',
+   'Codigo de processamento para deposito varejista',
+   069035);
+
+COMMIT;
