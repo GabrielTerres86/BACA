3c2476e846421a6fcc096f85c6efdeba0a056b75
Jose Dill (Mouts) <jose.junior@mouts.info>
2020-05-26 17:22:31 -0300

    INC0050035 - Inclusao do arquivo baca

---
+++ 2020/05/INC00500035 _baixa_titulo_cip.sql
@@ -0,0 +1,99 @@
+/*
+
+Dessa forma os títulos com instrução "01-Remessa de Titulos" ficam bloqueados no Ayllos e 
+não aceitam baixa pela tela, tendo que ser baixados via script, conforme solicitação da área de cobrança.
+
+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+         Cooperativa |      Conta |      Boletos    |
+          1-VIACREDI |    3920798 |        3195     |
+          1-VIACREDI |    9414568 |         169     |
+          1-VIACREDI |   10690840 |          13     |                    
+          1-VIACREDI |       1961 |        2574     |
+
+(Jose Dill (Mouts) - INC0050035)
+
+*/
+
+declare 
+  -- Local variables here
+  i integer;
+  vr_dserro VARCHAR2(100);
+  vr_dscritic VARCHAR2(4000);
+  
+  CURSOR cr_crapcob IS
+    SELECT cob.rowid
+          ,cob.incobran
+          ,cob.cdcooper
+          ,cob.nrdconta
+          ,cob.nrcnvcob
+          ,cob.nrdocmto
+          ,cob.dsdoccop
+          ,cob.nrnosnum
+      FROM crapcob cob
+     WHERE (cob.cdcooper,cob.cdbandoc,cob.nrdctabb,cob.nrcnvcob,cob.nrdconta,cob.nrdocmto)
+      IN (
+           (           1,          85,      101070,      101070,     3920798,       3195),  --1	85	101070	101070	3920798	3195
+           (           1,          85,      101002,      101002,     9414568,       169),   --1	85	101002	101002	9414568	169
+           (           1,          85,      101002,      101002,     10690840,       13),   --1	85	101002	101002	10690840	13
+           (          14,          85,      113002,      113002,     1961,       2574) --14	85	113002	113002	1961	2574
+         )
+     ORDER BY COB.CDCOOPER, COB.NRDCONTA, COB.DSDOCCOP, COB.NRDOCMTO;
+
+BEGIN
+
+  dbms_output.put_line('Situacao (0=A, 3=B, 5=L) - Cooperativa - Conta - Convenio - Boleto - Documento');
+
+  -- Test statements here
+  FOR rw IN cr_crapcob LOOP
+  
+    dbms_output.put_line(rw.incobran || ' - ' ||
+                         rw.cdcooper || ' - ' ||
+                         rw.nrdconta || ' - ' ||
+                         rw.nrcnvcob || ' - ' ||
+                         rw.nrdocmto || ' - ' ||
+                         rw.dsdoccop || ' - ' ||
+                         rw.nrnosnum );
+                         
+
+       UPDATE crapcob SET incobran = 3, 
+                          dtdbaixa = TRUNC(SYSDATE)
+        WHERE cdcooper = rw.cdcooper
+          AND nrdconta = rw.nrdconta
+          AND nrcnvcob = rw.nrcnvcob
+          AND nrdocmto = rw.nrdocmto;
+          
+      paga0001.pc_cria_log_cobranca(pr_idtabcob => rw.rowid
+                                  , pr_cdoperad => '1'
+                                  , pr_dtmvtolt => trunc(SYSDATE)
+                                  , pr_dsmensag => 'Titulo baixado manualmente'
+                                  , pr_des_erro => vr_dserro
+                                  , pr_dscritic => vr_dscritic );
+          
+    
+    COMMIT;
+    
+  END LOOP;
+  
+  dbms_output.put_line(' ');
+  dbms_output.put_line('Apos atualizacao');  
+  
+  FOR rw IN cr_crapcob LOOP
+  
+    dbms_output.put_line(rw.incobran || ' - ' ||
+                         rw.cdcooper || ' - ' ||
+                         rw.nrdconta || ' - ' ||
+                         rw.nrcnvcob || ' - ' ||
+                         rw.nrdocmto || ' - ' ||
+                         rw.dsdoccop || ' - ' ||
+                         rw.nrnosnum);                                                           
+    
+  END LOOP;
+
+  COMMIT;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    RAISE;
+END;
