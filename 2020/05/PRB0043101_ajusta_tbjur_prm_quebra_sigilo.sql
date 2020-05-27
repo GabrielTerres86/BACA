0c88e6f17ebeed569f3b50a0e459289f5977633d
Lucas Eduardo ranghetti <lucas.ranghetti@ailos.coop.br>
2020-05-27 11:32:22 -0300

    Delete PRB0043101_ajusta_tbjur_prm_quebra_sigilo.sql

--- 2020/05/PRB0043101_ajusta_tbjur_prm_quebra_sigilo.sql
+++
@@ -1,9 +0,0 @@
-BEGIN 
-  UPDATE Tbjur_prm_quebra_sigilo o 
-     SET o.nmestsig = 'CONTA ITG'
-   WHERE o.cdestsig = 25;
-   COMMIT;
-EXCEPTION
-  WHEN OTHERS THEN
-    DBMS_OUTPUT.put_line('Erro: '||sqlerrm);
-END;
