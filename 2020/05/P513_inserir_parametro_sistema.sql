3014a3a96f05e843dadc8e634163943ddf033cbc
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2020-05-29 11:54:05 -0300

    P513.2 - Depósito Varejista
    - Ajustar o nome dos parametros para os códigos de processamento da Saque & Pague, para consulta da conta e depósito

--- 2020/05/P513_inserir_parametro_sistema.sql
+++ 2020/05/P513_inserir_parametro_sistema.sql
@@ -3,8 +3,17 @@
 VALUES
   ('CRED',
    0,
-   'CDPROC_DEP_VAR_SEP',
+   'SEP_CDPROC_DEPVAREJISTA',
    'Codigo de processamento para deposito varejista',
-   068004);
+   069035);
 
+INSERT INTO crapprm
+  (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+VALUES
+  ('CRED',
+   0,
+   'SEP_CDPROC_CONVAREJISTA',
+   'Codigo de processamento para consultar a conta de varejista',
+   068004);
+   
 COMMIT;
