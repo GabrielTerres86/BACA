444addec93e6118be12144ebf9f5e348f715f669
Gustavo Felipe Alves <t0033089@cecred.coop.br>
2020-06-02 11:51:58 -0300

    D1086 - BACA - Inserir parametro para job rendas automaticas

---
+++ 2020/06/D1086 - Inserir parametro para rodar job rendas automaticas.sql
@@ -0,0 +1,35 @@
+
+DECLARE
+  --Buscar na craprdr
+  CURSOR cr_crapcop IS 
+   SELECT cop.cdcooper
+   FROM crapcop cop;     
+   
+  --Vars
+  vr_cdcooper crapcop.cdcooper%TYPE;
+  vr_nmparam crappat.nmpartar%TYPE;
+  vr_cdpartar crappat.cdpartar%TYPE; 
+BEGIN
+  vr_nmparam := 'Rendas Automáticas na Atualização Cadastral';
+  select max(crappat.cdpartar) + 1 into vr_cdpartar from crappat;
+  
+  insert into crappat(cdpartar, nmpartar, tpdedado, cdprodut)
+  values (vr_cdpartar, vr_nmparam, 1, 0);
+  
+  --select crappat.cdpartar into vr_cdpartar from crappat where nmpartar = 'Rendas Automáticas na Atualização Cadastral';
+  
+  OPEN cr_crapcop;
+  LOOP
+       FETCH cr_crapcop INTO vr_cdcooper;
+       EXIT WHEN cr_crapcop%NOTFOUND;
+       insert into crappco(cdpartar, cdcooper, dsconteu)
+       values (vr_cdpartar, vr_cdcooper, 0);
+  END LOOP;
+  CLOSE cr_crapcop;
+  
+  COMMIT;
+END; 
+
+
+
+
