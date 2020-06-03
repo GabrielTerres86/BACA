7cdf8202d89ca589f67b259a6e31e8820dd2f40b
Otavio Theiss <otavio.theiss@ailos.coop.br>
2020-06-03 16:04:37 -0300

    Ajuste de script para gerar DBMS_OUTPUT

--- 2020/05/RITM0072927_BACA_Alterar_PRM_limite_operacaional.sql
+++ 2020/05/RITM0072927_BACA_Alterar_PRM_limite_operacaional.sql
@@ -1,30 +1,37 @@
-
-BEGIN
-  DECLARE
-    CURSOR cr_crapcop IS 
-       SELECT cdcooper
-         FROM crapcop c;
-  BEGIN
-    DELETE FROM crapprm p WHERE p.cdacesso = 'CRD_LIMITE_OPERAC';
+begin
+  begin
+    declare
+      -- nome da rotina
+      wk_rotina varchar2(200) := 'Script de BACA da RITM0072927';
     
-    FOR rw_cracop IN cr_crapcop LOOP
+      CURSOR cr_crapcop IS
+        SELECT cdcooper FROM crapcop c;
+    
+    BEGIN
+    
+      DELETE FROM crapprm p WHERE p.cdacesso = 'CRD_LIMITE_OPERAC';
+    
+      FOR rw_cracop IN cr_crapcop LOOP
         BEGIN
-          INSERT INTO crapprm (
-                 cdacesso, 
-                 cdcooper, 
-                 dstexprm, 
-                 dsvlrprm, 
-                 nmsistem
-          ) VALUES (
-                 'CRD_LIMITE_OPERAC', 
-                 rw_cracop.cdcooper, 
-                 'Verifica limite operacional da cooperativa (1 = Verifica / 0 = Nao verifica)',
-                 0,
-                 'CRED'                
-          );
+          INSERT INTO crapprm
+            (cdacesso, cdcooper, dstexprm, dsvlrprm, nmsistem)
+          VALUES
+            ('CRD_LIMITE_OPERAC',
+             rw_cracop.cdcooper,
+             'Verifica limite operacional da cooperativa (1 = Verifica / 0 = Nao verifica)',
+             0,
+             'CRED');
         END;
-    END LOOP;
+      END LOOP;
     
-    COMMIT;
-  END; 
+      COMMIT;
+      dbms_output.put_line('Sucesso ao executar: ' || wk_rotina);
+    exception
+      when others then
+        dbms_output.put_line('Erro ao executar: ' || wk_rotina ||
+                             ' --- detalhes do erro: ' || SQLCODE || ': ' ||
+                             SQLERRM);
+        ROLLBACK;
+    end;
+  END;
 END;
