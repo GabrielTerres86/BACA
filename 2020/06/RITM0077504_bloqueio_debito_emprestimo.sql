86f07ce3016706b77c4493496dd3f8c3883032cf
Miguel Rodrigues <miguel.rodrigues@ailos.coop.br>
2020-06-03 09:28:54 -0300

    Bloqueio de debitos na coop 11, conta 3891, contrato 42021

---
+++ 2020/06/RITM0077504_bloqueio_debito_emprestimo.sql
@@ -0,0 +1,8 @@
+BEGIN
+/* coop 11, conta 3891, contrato 42021*/  
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 11, 'CTA_CTR_ACAO_JUDICIAL', 'Lista de contas e contratos específicos que não podem debitar na conta corrente devido a ação judicial |formato=(cta,ctr)', '(3891,42021)');
+
+COMMIT;
+
+END;
