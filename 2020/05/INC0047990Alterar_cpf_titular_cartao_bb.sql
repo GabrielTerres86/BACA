45336b7457ece1e86939362c3f6ee258fae94946
Otavio Theiss <otavio.theiss@ailos.coop.br>
2020-06-03 16:27:51 -0300

    Script para alterar cpf do titular do cartao BB

--- 2020/05/INC0047990Alterar_cpf_titular_cartao_bb.sql
+++ 2020/05/INC0047990Alterar_cpf_titular_cartao_bb.sql
@@ -1,9 +1,21 @@
-BEGIN  
- UPDATE crawcrd w 
-    SET w.nrcpftit = 67459706949    
- WHERE w.nrcctitg = 47318046
-   AND w.nrdconta = 3093
-   AND w.cdcooper = 5;
-   
- COMMIT;   
-END;
+begin  
+  begin
+    declare 
+    -- nome da rotina
+    wk_rotina varchar2(200) := 'Script de BACA do INC0047990';
+    begin
+		 UPDATE crawcrd w 
+			SET w.nrcpftit = 67459706949    
+		 WHERE w.nrcctitg = 47318046
+		   AND w.nrdconta = 3093
+		   AND w.cdcooper = 5;
+		   
+      commit;
+      dbms_output.put_line('Sucesso ao executar: ' || wk_rotina);
+    exception
+      when others then
+      dbms_output.put_line('Erro ao executar: ' || wk_rotina ||' --- detalhes do erro: '|| SQLCODE || ': ' || SQLERRM);
+      ROLLBACK;
+    end;
+  end;
+end;
