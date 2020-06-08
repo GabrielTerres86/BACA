e22ecc6c11e3471cb2f65fc191782661113dc5f7
James Prust Júnior <james.junior@ailos.coop.br>
2020-06-08 16:24:45 -0300

    Baca para corrigir os valores das parcelas

---
+++ 2020/06/PRB0043156.sql
@@ -0,0 +1,27 @@
+UPDATE crawepr SET
+       vlpreemp = 5533.96
+ WHERE cdcooper = 5
+   and nrdconta = 245690
+   and nrctremp = 21086;       
+
+UPDATE crapepr SET
+       vlpreemp = 5533.96
+ WHERE cdcooper = 5
+   and nrdconta = 245690
+   and nrctremp = 21086;       
+  
+UPDATE crappep SET
+       vlparepr = 5533.96,
+       vlsdvpar = 5533.96
+ WHERE cdcooper = 5
+   and nrdconta = 245690
+   and nrctremp = 21086;
+   
+UPDATE craplem SET
+       vlpreemp = 5533.96
+ WHERE cdcooper = 5
+   and nrdconta = 245690
+   and nrctremp = 21086
+   and dtmvtolt = '29/05/2020';
+
+commit;
