0a895040dbbe67396f35f0feb0601ae1d7e432f5
Evandro Guaranha(Amcom) <evandro.silva@amcom.com.br>
2020-06-09 10:59:10 -0300

    Ajuste script crapprm

--- 2020/06/RITM0033189_script_crapprm.sql
+++ 2020/06/RITM0033189_script_crapprm.sql
@@ -1,17 +1,12 @@
 DECLARE
 BEGIN
-  FOR rw_crapcop IN (SELECT c.cdcooper FROM crapcop c WHERE c.flgativo = 1) LOOP
-
-    INSERT INTO CRAPPRM
-      (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
-    VALUES
-      ('CRED',
-        rw_crapcop.cdcooper,
-       'DATA_TARI_DOCMTO_BORDERO',
-       'Data de início de tarifacao titulo liberado no bordero',
-       SYSDATE);
-
-  END LOOP;
-
+  INSERT INTO CRAPPRM
+    (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+  VALUES
+    ('CRED',
+      0,
+     'DATA_TARI_DOCMTO_BORDERO',
+     'Data de início de tarifacao titulo liberado no bordero',
+     SYSDATE);
   COMMIT;
 END;
