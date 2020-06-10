a48f2219291ea9d1eead9fe03ed9bcef81d7c3ef
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2020-06-10 15:58:53 -0300

    Update P513_inserir_parametro_sistema.sql

--- 2020/05/P513_inserir_parametro_sistema.sql
+++ 2020/05/P513_inserir_parametro_sistema.sql
@@ -6,14 +6,5 @@ VALUES
    'SEP_CDPROC_DEPVAREJISTA',
    'Codigo de processamento para deposito varejista',
    069035);
-
-INSERT INTO crapprm
-  (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
-VALUES
-  ('CRED',
-   0,
-   'SEP_CDPROC_CONVAREJISTA',
-   'Codigo de processamento para consultar a conta de varejista',
-   068004);
-   
+  
 COMMIT;
