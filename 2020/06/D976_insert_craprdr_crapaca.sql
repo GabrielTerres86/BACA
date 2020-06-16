3acc96697f95acbb5712b942f62b4a5fe0df4c26
Gustavo Felipe Alves <t0033089@cecred.coop.br>
2020-06-16 14:54:18 -0300

    D976 - BACA - Insert na craprdr e crapaca

---
+++ 2020/06/D976_insert_craprdr_crapaca.sql
@@ -0,0 +1,22 @@
+
+DECLARE
+  --Buscar na craprdr
+  CURSOR cr_craprdr(pr_nmprogra IN craprdr.nmprogra%TYPE) IS 
+         SELECT t.nrseqrdr
+         FROM craprdr t
+         WHERE t.nmprogra = pr_nmprogra;
+  --Vars
+  vr_nrseqrdr craprdr.nrseqrdr%TYPE;         
+  vr_nmprogra craprdr.nmprogra%TYPE;
+BEGIN
+  insert into craprdr(nmprogra, dtsolici) values ('ATENDA_LANCAMENTOS_FUTUROS', SYSDATE);
+  vr_nmprogra := 'ATENDA_LANCAMENTOS_FUTUROS';
+  
+  OPEN cr_craprdr(pr_nmprogra => vr_nmprogra);
+  FETCH cr_craprdr INTO vr_nrseqrdr;
+  CLOSE cr_craprdr;
+  
+  insert into crapaca (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
+  values ('LAUTOM_DEBITA_CARTAO','','debitarFaturaIndividual','pr_cdcooper,pr_cdoperad,pr_fatrowid,pr_valor_pag_fatura,pr_autorizador', vr_nrseqrdr);  
+  COMMIT;
+END; 
