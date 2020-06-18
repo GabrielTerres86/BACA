c63b7c23e0c7a3ad9872549f09a63d0e292283b1
Renato Darosci <supero-renato.darosci@ailos.coop.br>
2020-06-18 09:55:49 -0300

    Scripts para adicionar registros da CRAPACA do projeto

---
+++ 2020/06/PRJ0014522_adicionar_crapaca_matric.sql
@@ -0,0 +1,55 @@
+DECLARE
+  
+  vr_nrseqrdr   craprdr.nrseqrdr%TYPE;
+  
+BEGIN
+  BEGIN
+    SELECT t.nrseqrdr
+      INTO vr_nrseqrdr
+      FROM craprdr t
+     WHERE t.nmprogra = 'MATRIC';
+  EXCEPTION
+    WHEN no_data_found THEN
+      BEGIN
+        INSERT INTO craprdr(nrseqrdr
+                           ,nmprogra
+                           ,dtsolici)
+                     VALUES(craprdr_nrseqrdr.nextval
+                           ,'MATRIC'
+                           ,SYSDATE)
+                 RETURNING nrseqrdr 
+                      INTO vr_nrseqrdr;
+      EXCEPTION
+        WHEN OTHERS THEN
+          raise_application_error(-20005, 'Erro ao inserir CRAPRDR: '||SQLERRM);
+      END;
+    WHEN OTHERS THEN
+      raise_application_error(-20004, 'Erro ao buscar CRAPRDR: '||SQLERRM);
+  END;
+  
+  
+  BEGIN
+    INSERT INTO crapaca(nrseqaca
+                       ,nmdeacao
+                       ,nmpackag
+                       ,nmproced
+                       ,lstparam
+                       ,nrseqrdr)
+                VALUES ((SELECT NVL(MAX(a.nrseqaca),0)+1 FROM crapaca a)
+                       ,'GRAVAR_PRE_CADASTRO'
+                       ,NULL
+                       ,'CADASTRO.GravarPreCadastro'
+                       ,'pr_cdcooper,pr_cdagenci,pr_cdoperad,pr_dsxmltel'
+                       ,vr_nrseqrdr);
+                
+  EXCEPTION
+    WHEN dup_val_on_index THEN
+      dbms_output.put_line('Registro LIST_ORGAO_EMISSOR, já cadastrado.');
+    WHEN OTHERS THEN
+      raise_application_error(-20010, 'Erro ao inserir CRAPACA: '||SQLERRM);
+  END;
+  
+  
+  COMMIT;
+ 
+END;
