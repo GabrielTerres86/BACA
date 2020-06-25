ccbbef518ed3e37e0a4eede36214eb3d1d247f8a
laercioschluter <laercio.schluter@mouts.info>
2020-06-25 08:54:27 -0300

    Baca para remover Apostrofo, para que não ocorra erros no relatório 549

--- 2020/06/PRB0042963_Remove_Apostrofo.sql
+++ 2020/06/PRB0042963_Remove_Apostrofo.sql
@@ -5,3 +5,9 @@ UPDATE crapass a
  UPDATE crapass a
    SET a.nmprimtl = replace(a.nmprimtl,chr(39),' ')
  WHERE a.nmprimtl like  ('%' || chr(39)) || '%' AND a.cdcooper = 6 AND a.nrdconta = 12475;
+ 
+  UPDATE crapass a
+   SET a.nmprimtl = replace(a.nmprimtl,chr(39),' ')
+ WHERE a.nmprimtl like  ('%' || chr(39)) || '%' AND a.cdcooper = 4 AND a.nrdconta = 10243;
+ 
+ COMMIT;
