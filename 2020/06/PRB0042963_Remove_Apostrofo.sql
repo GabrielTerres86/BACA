e9af5098e2d9adfd2e9068795151653dc4249066
laercioschluter <laercio.schluter@mouts.info>
2020-06-25 08:38:24 -0300

    Baca para remover apostrofo de contas , para que não ocorra mais erro no relatorio 549

---
+++ 2020/06/PRB0042963_Remove_Apostrofo.sql
@@ -0,0 +1,7 @@
+UPDATE crapass a
+   SET a.nmprimtl = replace(a.nmprimtl,chr(39),' ')
+ WHERE a.nmprimtl like  ('%' || chr(39)) || '%' AND a.cdcooper = 1 AND a.nrdconta = 9110240;
+ 
+ UPDATE crapass a
+   SET a.nmprimtl = replace(a.nmprimtl,chr(39),' ')
+ WHERE a.nmprimtl like  ('%' || chr(39)) || '%' AND a.cdcooper = 6 AND a.nrdconta = 12475;
