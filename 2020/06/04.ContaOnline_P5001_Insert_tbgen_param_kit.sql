444d31eb514299c9a0f3e364556589247f42399f
george.kruger <george.kruger@ailos.coop.br>
2020-06-26 09:20:43 -0300

    Ajustes

--- 2020/06/04.ContaOnline_P5001_Insert_tbgen_param_kit.sql
+++ 2020/06/04.ContaOnline_P5001_Insert_tbgen_param_kit.sql
@@ -1,5 +1,8 @@
 begin
 
+  insert into tbgen_param_kit (DSURLSERVER, NMUSUARIO, CDDSENHA)
+  values ('https://conteudo.cecred.coop.br/kit_materiais/', 'conteudo', 'c0n10.t3u1d70');
+
   insert into crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
   values('CRED',0,'PRM_VALIDACAO_500', 'Parametro para validar se o piloto está ativo', 1);
   
