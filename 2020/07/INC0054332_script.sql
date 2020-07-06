8f367f8cdd7318d60954d72a443e7c298654fa06
Otavio Theiss <otavio.theiss@ailos.coop.br>
2020-07-06 11:08:48 -0300

    INC0054332 - script de atualização sequencial DAUR

---
+++ 2020/07/INC0054332_script.sql
@@ -0,0 +1,21 @@
+begin  
+  begin
+    declare 
+    -- nome da rotina
+    wk_rotina varchar2(200) := 'Script de atualização do sequencial do arquivo DAUR';
+    begin
+
+    UPDATE crapscb
+       SET nrseqarq = 832
+         , dtultint = SYSDATE
+     WHERE crapscb.tparquiv = 8; -- Arquivo DAUR - Debito em cota das faturas
+           
+      commit;
+      dbms_output.put_line('Sucesso ao executar: ' || wk_rotina);
+    exception
+      when others then
+      dbms_output.put_line('Erro ao executar: ' || wk_rotina ||' --- detalhes do erro: '|| SQLCODE || ': ' || SQLERRM);
+      ROLLBACK;
+    end;
+  end;
+end;
