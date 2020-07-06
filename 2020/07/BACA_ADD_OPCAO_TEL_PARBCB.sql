5a1ce804f22f9f6abdef45c7b6d238330bcb8902
Otavio Theiss <otavio.theiss@ailos.coop.br>
2020-07-06 16:38:02 -0300

    INC0054359 - script

--- 2020/07/BACA_ADD_OPCAO_TEL_PARBCB.sql
+++ 2020/07/BACA_ADD_OPCAO_TEL_PARBCB.sql
@@ -1,21 +1,34 @@
-BEGIN
-  -- Adicionar opcao C na tela PARBCB devido ao erro de consulta, opcao nao cadastrada.
-  UPDATE craptel t
-     SET t.cdopptel = t.cdopptel || ',C', t.lsopptel = t.lsopptel || ',Consulta'
-   WHERE t.nmdatela = 'PARBCB';
+begin  
+  begin
+    declare 
+    -- nome da rotina
+    wk_rotina varchar2(200) := 'Script de atualização de permissão tela PARBCB';
+    begin
 
-  INSERT INTO crapace (NMDATELA, CDDOPCAO, CDOPERAD, NMROTINA, CDCOOPER, NRMODULO, IDEVENTO, IDAMBACE)
-  SELECT nmdatela,
-         'C',
-         cdoperad,
-         nmrotina,
-         3,
-         nrmodulo,
-         idevento,
-         idambace
-  FROM crapace
-  WHERE nmdatela = 'PARBCB'
-    AND cdcooper = 3;
-  
-  COMMIT;
-END;
+    -- Adicionar opcao C na tela PARBCB devido ao erro de consulta, opcao nao cadastrada.
+	  UPDATE craptel t
+		 SET t.cdopptel = t.cdopptel || ',C', t.lsopptel = t.lsopptel || ',Consulta'
+	   WHERE t.nmdatela = 'PARBCB';
+
+	  INSERT INTO crapace (NMDATELA, CDDOPCAO, CDOPERAD, NMROTINA, CDCOOPER, NRMODULO, IDEVENTO, IDAMBACE)
+	  SELECT nmdatela,
+			 'C',
+			 cdoperad,
+			 nmrotina,
+			 3,
+			 nrmodulo,
+			 idevento,
+			 idambace
+	  FROM crapace
+	  WHERE nmdatela = 'PARBCB'
+		AND cdcooper = 3;
+           
+      commit;
+      dbms_output.put_line('Sucesso ao executar: ' || wk_rotina);
+    exception
+      when others then
+      dbms_output.put_line('Erro ao executar: ' || wk_rotina ||' --- detalhes do erro: '|| SQLCODE || ': ' || SQLERRM);
+      ROLLBACK;
+    end;
+  end;
+end;
