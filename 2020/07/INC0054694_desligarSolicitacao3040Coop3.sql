74d9d786f310022e609c3228f4c30be08a2b90d8
Luiz Otávio Olinger Momm <luiz.momm@amcom.com.br>
2020-07-09 14:32:05 -0300

    Ajuste script

--- 2020/07/INC0054694_desligarSolicitacao3040Coop3.sql
+++ 2020/07/INC0054694_desligarSolicitacao3040Coop3.sql
@@ -9,10 +9,10 @@ ativa a solicitação do 3040 rodando todos os dias na noturna.
 */
 UPDATE craptab tab
    SET tab.dstextab = '2' || SUBSTR(tab.dstextab,2)
- WHERE tab.nmsistem = 'CRED'      
-   AND tab.tptabela = 'USUARI'    
-   AND tab.cdempres = 11          
+ WHERE tab.nmsistem = 'CRED'
+   AND tab.tptabela = 'USUARI'
+   AND tab.cdempres = 11
    AND tab.cdacesso = 'RISCOBACEN'
-   AND tab.tpregist = 000;
+   AND tab.tpregist = 000
    AND tab.cdcooper = 3;
 COMMIT;
