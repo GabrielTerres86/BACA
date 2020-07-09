4deb2cc0600aaaefd439efd30f94edfb8fa5c748
Daniel Zimmermann <daniel.zimmermann@ailos.coop.br>
2020-07-09 14:51:06 -0300

    Inclusão data liquidação prejuizo

--- 2020/07/Script_PRB0043327_ajustar_prejuizo.sql
+++ 2020/07/Script_PRB0043327_ajustar_prejuizo.sql
@@ -2,6 +2,7 @@
 
 UPDATE crapbdt bdt
    SET bdt.inprejuz = 0
+      ,bdt.dtliqprj = to_date('20/05/2020','DD/MM/RRRR')
  WHERE bdt.nrborder = 547576
    AND bdt.cdcooper = 1
    AND bdt.nrdconta = 6766404;
