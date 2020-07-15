a284e521a95b7c7d40ff134441eff603077b35cf
Douglas Kloppel <douglas.kloppel@ailos.coop.br>
2020-07-15 16:19:46 -0300

    INC0054056_update_craprac
    Alterando registro da aplicação programada

---
+++ 2020/07/INC0054056_update_craprac.sql
@@ -0,0 +1,79 @@
+ UPDATE craprac rac
+   SET rac.idsaqtot = 1
+WHERE  rac.cdcooper = 10 AND
+       rac.nrdconta = 100439 AND
+       rac.nraplica = 20 ;
+       
+ UPDATE craprac rac
+   SET rac.idsaqtot = 1
+WHERE  rac.cdcooper = 1 AND
+       rac.nrdconta = 2405342 AND
+       rac.nraplica = 37 ;       
+       
+ UPDATE craprac rac
+   SET rac.idsaqtot = 1
+WHERE  rac.cdcooper = 1 AND
+       rac.nrdconta = 3694623 AND
+       rac.nraplica = 33 ;
+       
+ UPDATE craprac rac
+   SET rac.idsaqtot = 1
+WHERE  rac.cdcooper = 1 AND
+       rac.nrdconta = 3866602 AND
+       rac.nraplica = 21 ;   
+       
+ UPDATE craprac rac
+   SET rac.idsaqtot = 1
+WHERE  rac.cdcooper = 1 AND
+       rac.nrdconta = 6989950 AND
+       rac.nraplica = 4 ; 
+       
+ UPDATE craprac rac
+   SET rac.idsaqtot = 1
+WHERE  rac.cdcooper = 1 AND
+       rac.nrdconta = 7408706 AND
+       rac.nraplica = 11 ;  
+       
+ UPDATE craprac rac
+   SET rac.idsaqtot = 1select * from craprac rac
+WHERE  rac.cdcooper = 1 AND
+       rac.nrdconta = 7800070 AND
+       rac.nraplica = 27;  
+
+ UPDATE craprac rac
+   SET rac.idsaqtot = 1
+WHERE  rac.cdcooper = 1 AND
+       rac.nrdconta = 7974833 AND
+       rac.nraplica = 11;    
+       
+ UPDATE craprac rac
+   SET rac.idsaqtot = 1
+WHERE  rac.cdcooper = 1 AND
+       rac.nrdconta = 8823693 AND
+       rac.nraplica = 5;    
+                           
+ UPDATE craprac rac
+   SET rac.idsaqtot = 1
+WHERE  rac.cdcooper = 1 AND
+       rac.nrdconta = 10987002 AND
+       rac.nraplica = 10;                        
+       
+ UPDATE craprac rac
+   SET rac.idsaqtot = 1
+WHERE  rac.cdcooper = 1 AND
+       rac.nrdconta = 11291036 AND
+       rac.nraplica = 2;   
+       
+ UPDATE craprac rac
+   SET rac.idsaqtot = 1
+WHERE  rac.cdcooper = 13 AND
+       rac.nrdconta = 327336 AND
+       rac.nraplica = 13;     
+       
+ UPDATE craprac rac
+   SET rac.idsaqtot = 1
+WHERE  rac.cdcooper = 16 AND
+       rac.nrdconta = 161519 AND
+       rac.nraplica = 8;                     
+COMMIT;       
+
