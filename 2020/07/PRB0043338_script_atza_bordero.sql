586b46b9aa4568b97b2a6631a30da58558cc7ce7
Elton Giusti <elton.giusti@amcom.com.br>
2020-07-20 11:19:31 -0300

    PRB0043338 - Ajuste no script valor saldo e no lancamento do bordero.

--- 2020/07/PRB0043338_script_atza_bordero.sql
+++ 2020/07/PRB0043338_script_atza_bordero.sql
@@ -11,6 +11,8 @@ and a.nrborder = 548675;
 -- alterar insitit de 4-liberado para 2-processado 
 update  craptdb a
 set a.insittit = 2--processado  
+   ,a.vlsldtit = 0  --689,85
+   ,a.vlprejuz = 0  --723,47
    ,a.vljraprj = 54.31 -- 0 (era zero)   
    ,a.vlmratit = 96.6  --128,64
    ,a.vliofcpl = 3.85  --5,23  
@@ -19,9 +21,24 @@ and a.nrdconta = 8791740
 and a.nrborder = 548675
 and a.nrdocmto = 705;
 
+declare
+  vr_nrseqdig tbdsct_lancamento_bordero.nrseqdig%TYPE;
+
+begin
+
+  SELECT nvl(MAX(nrseqdig),0)+1
+  INTO   vr_nrseqdig
+  FROM   tbdsct_lancamento_bordero
+  WHERE  nrtitulo = 6
+  AND    nrborder = 548675
+  AND    nrdconta = 8791740
+  AND    cdcooper = 1;
+
 -- Valor inserido para zerar o saldo do extrato.
 insert into TBDSCT_LANCAMENTO_BORDERO (CDCOOPER, NRDCONTA, NRBORDER, CDBANDOC, NRDCTABB, NRCNVCOB, NRDOCMTO, NRSEQDIG, NRTITULO, DTMVTOLT, CDORIGEM, CDHISTOR, VLLANMTO, DTHRTRAN, DTREFATU, PROGRESS_RECID, DTESTORN)
-values (1, 8791740, 548675, 0, 0, 0, 0, 98, 0, to_date('30-06-2020', 'dd-mm-yyyy'), 7, 2798, 7.55, to_date('30-06-2020 08:43:50', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2020 08:43:50', 'dd-mm-yyyy hh24:mi:ss'), null, null);
+values (1, 8791740, 548675, 0, 0, 0, 0, vr_nrseqdig, 6, to_date('30-06-2020', 'dd-mm-yyyy'), 7, 2798, 7.55, to_date('30-06-2020 08:43:50', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2020 08:43:50', 'dd-mm-yyyy hh24:mi:ss'), null, null);
 
+end;
+/
 
 commit;
