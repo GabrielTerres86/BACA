5b2c6d026c06a94d5bc6d305ba5ec5d18df1f380
Miguel Rodrigues <miguel.rodrigues@ailos.coop.br>
2020-07-20 10:36:11 -0300

    Viacredi - Bloqueio de debitos por ordem judicial

--- 2020/07/RITM0084629_bloqueio_debito_emprestimo.sql
+++ 2020/07/RITM0084629_bloqueio_debito_emprestimo.sql
@@ -1,7 +1,8 @@
-/* conta 652.555-5 (Viacredi), contrato  1.485.346  */
+/* conta 652.555-5 (Viacredi), contrato  1.485.346 
+   conta 771.732-6 (Viacredi), contrato 1.746.320 */
 BEGIN
   UPDATE crapprm prm
-     SET prm.dsvlrprm = prm.dsvlrprm || ',(6525555,1485346)'
+     SET prm.dsvlrprm = prm.dsvlrprm || ',(6525555,1485346),(7717326,1746320)'
    WHERE prm.cdcooper = 1
      AND prm.cdacesso = 'CTA_CTR_ACAO_JUDICIAL';
      
