5f330efdf8332c0b9b1e376c1413e6e949dad71c
Evandro Guaranha(Amcom) <evandro.silva@amcom.com.br>
2020-07-21 17:02:16 -0300

    script crapcyb - ajuste dtdbaixa

--- 2020/07/baca_crapcyb_INC0054607.sql
+++ 2020/07/baca_crapcyb_INC0054607.sql
@@ -1,10 +1,18 @@
-UPDATE crapcyb cyb
-   SET cyb.dtdbaixa = NULL
- WHERE EXISTS (SELECT 1
-          FROM crapepr epr
-         WHERE cyb.cdcooper = epr.cdcooper
-           AND cyb.nrdconta = epr.nrdconta
-           AND cyb.nrctremp = epr.nrctremp
-           AND cyb.vlsdprej > 0
-           AND cyb.dtdbaixa IS NOT NULL);
-COMMIT;
+BEGIN
+  BEGIN
+    UPDATE crapcyb cyb
+       SET cyb.dtdbaixa = NULL
+     WHERE EXISTS (SELECT 1
+              FROM crapepr epr
+             WHERE cyb.cdcooper = epr.cdcooper
+               AND cyb.nrdconta = epr.nrdconta
+               AND cyb.nrctremp = epr.nrctremp
+               AND cyb.vlsdprej > 0
+               AND cyb.dtdbaixa IS NOT NULL);
+  EXCEPTION
+    WHEN OTHERS THEN
+      dbms_output.put_line('Falha update crapcyb ' || SQLERRM);
+  END;
+
+  COMMIT;
+END;
