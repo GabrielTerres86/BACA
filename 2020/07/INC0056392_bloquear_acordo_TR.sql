ffac117547b3a67b82beb02336bfa70f1fb45c7e
Odirlei Busana <odirlei.busana@ailos.coop.br>
2020-07-28 13:53:08 -0300

    Ajuste Baca

--- 2020/07/INC0056392_bloquear_acordo_TR.sql
+++ 2020/07/INC0056392_bloquear_acordo_TR.sql
@@ -7,6 +7,6 @@ UPDATE tbrecup_acordo ac
 --> Reativar acordo 
 UPDATE tbrecup_acordo ac
    SET ac.cdsituacao = 1
- WHERE ac.nracordo IN (201325,275847,179160);
+ WHERE ac.nracordo IN (201325,275847,179160,220822,278268,202798,274828);
 
 COMMIT; 
--- 2020/07/NC0056392_desbloquear_acordo_TR.sql
+++ 2020/07/NC0056392_desbloquear_acordo_TR.sql
@@ -2,7 +2,7 @@
 --> cancelar acordo
 UPDATE tbrecup_acordo ac
    SET ac.cdsituacao = 3
- WHERE ac.nracordo IN (201325,275847,179160);
+ WHERE ac.nracordo IN (201325,275847,179160,220822,278268,202798,274828);
 /*
 --> Reativar acordo 
 UPDATE tbrecup_acordo ac
