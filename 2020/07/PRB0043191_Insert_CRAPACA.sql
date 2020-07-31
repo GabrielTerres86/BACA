84f357cfecc3affcdc2cbfc4115d10ff57a1e543
Danilo Korczagin <t0033084@ailos.coop.br>
2020-07-31 14:21:32 -0300

    PRB0043191 - Correção de script de BACA - insert na crapaca

--- 2020/07/PRB0043191_Insert_CRAPACA.sql
+++ 2020/07/PRB0043191_Insert_CRAPACA.sql
@@ -1,7 +1,7 @@
 BEGIN
   
   INSERT INTO crapaca (nrseqaca, nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
-  VALUES (NULL, 'ALTERA_TITULAR', 'TELA_ATENDA_CARTAOCREDITO', 'pc_cartao_titular', 'pr_cdcooper,pr_cdadmcrd,pr_nrdconta', 1228);
+  VALUES (NULL, 'EXCLUI_CARTAO', 'TELA_ATENDA_CARTAOCREDITO', 'pc_excluir_proposta', 'pr_cdcooper,pr_nrdconta,pr_nrctrcrd', 1228);
   
   COMMIT;
   
