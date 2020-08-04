2f2283b95e7208e81ee832737ebd0cadfec0484e
Darlei Fernando Zillmer <darlei.zillmer@supero.com.br>
2020-08-04 15:35:32 -0300

    Adicionados emails para envio de aviso de erro na renegociacao

---
+++ 2020/08/INC0056461.sql
@@ -0,0 +1,3 @@
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 0, 'ERRO_TRT_RENEG_EMAIL', 'Email para envio de avisos de erro ao efetivar renegociacao', 'telvio@ailos.coop.br;diogenes@ailos.coop.br');
+commit;
