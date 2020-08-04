cdf3c66082096f0b4bb0ca9b461c2c20dddeb467
André Ricardo Würges <andre.wurges@mouts.info>
2020-08-04 11:49:09 -0300

    Ajuste de script

--- 2020/08/INC0055179 _update_dtadmiss_2.sql
+++ 2020/08/INC0055179 _update_dtadmiss_2.sql
@@ -2,30 +2,35 @@ begin
   --Informar a data de admissão para CREDCREA
   update crapass c 
      set c.dtadmiss = to_date('07/05/2004', 'dd/mm/yyyy')
+       , c.dtmvtolt = to_date('07/05/2004', 'dd/mm/yyyy')
    where c.nrdconta = 7239530 
      and c.cdcooper = 7;
 
   --Informar a data de admissão para ACREDICOOP
   update crapass c 
      set c.dtadmiss = to_date('31/03/2000', 'dd/mm/yyyy')
+       , c.dtmvtolt = to_date('31/03/2000', 'dd/mm/yyyy')
    where c.nrdconta = 7239530 
      and c.cdcooper = 2;
 
   --Informar a data de admissão para UNILOS
   update crapass c 
      set c.dtadmiss = to_date('31/03/2004', 'dd/mm/yyyy')
+       , c.dtmvtolt = to_date('31/03/2004', 'dd/mm/yyyy')
    where c.nrdconta = 7239530 
      and c.cdcooper = 6;
 
   --Informar a data de admissão para ACENTRA
   update crapass c 
      set c.dtadmiss = to_date('28/11/2003', 'dd/mm/yyyy')
+       , c.dtmvtolt = to_date('28/11/2003', 'dd/mm/yyyy')
    where c.nrdconta = 7239530 
      and c.cdcooper = 5;
 
   --Informar a data de admissão para CREDICOMIN
   update crapass c 
      set c.dtadmiss = to_date('18/06/2008', 'dd/mm/yyyy')
+       , c.dtmvtolt = to_date('18/06/2008', 'dd/mm/yyyy')
    where c.nrdconta = 7239530 
      and c.cdcooper = 10;
 
