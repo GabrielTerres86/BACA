81e043ab036a32a25f112e3e4c4c334150ad8b24
Gustavo Felipe Alves <t0033089@cecred.coop.br>
2020-08-06 16:42:07 -0300

    D1177 - Estoria 1 - Correcao rollback da crapaca

--- 2020/07/D1177_update_crapaca_rollback.sql
+++ 2020/07/D1177_update_crapaca_rollback.sql
@@ -1,4 +1,8 @@
 UPDATE crapaca 
+SET lstparam = 'pr_nrdconta, pr_dtmvtolt, pr_vllimite_saque, pr_flgemissao_recibo_saque'
+WHERE nmdeacao = 'LIMITE_TAA_ALTERAR';
+
+UPDATE crapaca 
 SET nmdeacao = 'LIMITE_SAQUE_TAA_ALTERAR',nmproced = 'pc_tela_lim_saque_alterar'
 WHERE nmdeacao = 'LIMITE_TAA_ALTERAR';
 
@@ -6,8 +10,5 @@ UPDATE crapaca
 SET nmdeacao = 'LIMITE_SAQUE_TAA_CONSULTAR',nmproced = 'pc_tela_lim_saque_consultar'
 WHERE nmdeacao = 'LIMITE_TAA_CONSULTAR';
 
-UPDATE crapaca 
-SET lstparam = 'pr_nrdconta, pr_dtmvtolt, pr_vllimite_saque, pr_flgemissao_recibo_saque'
-WHERE nmdeacao = 'LIMITE_TAA_ALTERAR';
 
 COMMIT;
