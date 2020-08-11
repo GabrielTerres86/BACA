ee878531648793800e6288a8fa29e2a96b7d55ff
Ramon Martins <ramon.silva@ailos.coop.br>
2020-08-11 13:59:58 -0300

    RITM0067280-RDM0035985

---
+++ 2020/08/RDM0035985.sql
@@ -0,0 +1,5 @@
+
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 0, 'PARDBT_MONITORAMENTO', 'Monitoramento da tela PARDBT', 'contacorrente@ailos.coop.br, creditosuporte@cecred.coop.br');
+
+commit;
