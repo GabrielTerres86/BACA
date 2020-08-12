2d1f1dda1708ed0dcbb6e1022ef367b9618db985
David Giovanni Kistner <david.kistner@ailos.coop.br>
2020-08-12 08:44:16 -0300

    Scripts DMND0001414
    - Ajustar nome do script
    - Incluir INSERT de novo parâmetro de controle do PAGFOR na crapprm

--- 2020/09/baca_parametros_crapprm_pagfor.sql
+++ 2020/09/baca_parametros_crapprm_pagfor.sql
@@ -4,4 +4,8 @@ UPDATE crapprm p
  WHERE p.nmsistem = 'CRED'
    AND p.cdcooper = 0
    AND p.cdacesso = 'PLN_CTRL_PAGFOR';
+   
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 0, 'DATA_CTRL_PAGFOR', 'Data que indica quando próxima mudança no plano de controle do Pagfor deve entrar em vigor', '16/09/2020');   
+   
 COMMIT;
