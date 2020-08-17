d900642f99f27d5ad7f67926f694a23755538d70
David Giovanni Kistner <david.kistner@ailos.coop.br>
2020-08-17 09:51:39 -0300

    Criação de nova crítica
    Criar crítica 3000

---
+++ 2020/09/baca_insert_crapcri.sql
@@ -0,0 +1,9 @@
+INSERT INTO crapcri (cdcritic,
+                     dscritic,
+                     tpcritic,
+                     flgchama)
+             VALUES (3000,
+                     'Convênio bloqueado para pagamento neste terminal.',
+                     3,
+                     0);
+COMMIT;                     
