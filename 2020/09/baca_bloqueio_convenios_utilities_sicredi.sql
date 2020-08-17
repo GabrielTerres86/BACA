a3701440d056c8f75576bcef9c027c391bee8e57
David Giovanni Kistner <david.kistner@ailos.coop.br>
2020-08-17 16:19:37 -0300

    Ajustes DMND0001414
    - Cancelar script de atualização da crapscn
    - Atualizar diretório de postagem do arquivo csv para consumo no SOA

--- 2020/09/baca_bloqueio_convenios_utilities_sicredi.sql
+++
@@ -1,2 +0,0 @@
-UPDATE crapscn SET crapscn.dtencemp = to_date('16/09/2020','dd/mm/rrrr') WHERE crapscn.cdempres NOT IN ('147','149','609','K0','A0','D0','C06') AND crapscn.dtencemp IS NULL AND crapscn.dsoparre <> 'E';
-COMMIT;
