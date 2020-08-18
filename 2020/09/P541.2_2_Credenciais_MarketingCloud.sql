e4732eda116e9c43602e8744d53ccbfa3b83c933
Augusto Henrique da Conceição <augusto.conceicao@supero.com.br>
2020-08-17 21:58:50 -0300

    [P541.2] Criação dos BACAs do projeto
    Criação dos BACAs do projeto

---
+++ 2020/09/P541.2_2_Credenciais_MarketingCloud.sql
@@ -0,0 +1,4 @@
+begin
+ insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+ values ('CRED', 0, 'ACESSO_MARKETING_CLOUD', 'Dados de acesso do MarketingCloud.', '<AutenticarInputElement><grant_type>client_credentials</grant_type><client_id>fxehmvq7i1g9w8p8wuht596m</client_id><client_secret>KnaNyr3SJddpP0L3jiAdjJr8</client_secret><account_id>110006533</account_id></AutenticarInputElement>');
+end;
