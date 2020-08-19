794bc0cdc1c83c756d3dded298a3344474c818b3
Diógenes Lazzarini <diogenes.lazzarini@ailos.coop.br>
2020-08-19 17:34:44 -0300

    Create INC0056461.sql

---
+++ 2020/08/INC0056461.sql
@@ -0,0 +1,3 @@
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 0, 'ERRO_TRT_RENEG_EMAIL', 'Email para envio de avisos de erro ao efetivar renegociacao', 'telvio.cunha@ailos.coop.br;diogenes.lazzarini@ailos.coop.br');
+commit;
