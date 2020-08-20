21871e71d4ed046de098a6063fda501574bbcd80
Odirlei Busana <odirlei.busana@ailos.coop.br>
2020-08-20 21:11:55 +0300

    Removido comando de rollback

--- 2020/08/PRB0043385.sql
+++ 2020/08/PRB0043385.sql
@@ -15,21 +15,3 @@ UPDATE crapcyb SET idcessao = 65706 WHERE cdcooper = 16 AND nrdconta = 178756  A
 UPDATE crapcyb SET idcessao = 65687 WHERE cdcooper = 16 AND nrdconta = 309621  AND idcessao = 65300;
 
 COMMIT;
-/*
-    -- ROLLBACK
-    UPDATE crapcyb SET idcessao = 65454 WHERE cdcooper = 1 AND nrdconta = 2583380  AND idcessao = 65786;
-    UPDATE crapcyb SET idcessao = 65464 WHERE cdcooper = 1 AND nrdconta = 3766489  AND idcessao = 65792;
-    UPDATE crapcyb SET idcessao = 65477 WHERE cdcooper = 1 AND nrdconta = 7674147  AND idcessao = 65805;
-    UPDATE crapcyb SET idcessao = 65433 WHERE cdcooper = 1 AND nrdconta = 7767889  AND idcessao = 65760;
-    UPDATE crapcyb SET idcessao = 65664 WHERE cdcooper = 1 AND nrdconta = 10997520 AND idcessao = 65864;
-    UPDATE crapcyb SET idcessao = 65287 WHERE cdcooper = 5 AND nrdconta = 183970   AND idcessao = 65704;
-    UPDATE crapcyb SET idcessao = 65358 WHERE cdcooper = 7 AND nrdconta = 284246   AND idcessao = 65711;
-    UPDATE crapcyb SET idcessao = 65377 WHERE cdcooper = 11 AND nrdconta = 42528   AND idcessao = 65738;
-    UPDATE crapcyb SET idcessao = 65380 WHERE cdcooper = 11 AND nrdconta = 284807  AND idcessao = 65740;
-    UPDATE crapcyb SET idcessao = 65404 WHERE cdcooper = 11 AND nrdconta = 335312  AND idcessao = 65746;
-    UPDATE crapcyb SET idcessao = 65394 WHERE cdcooper = 11 AND nrdconta = 418650  AND idcessao = 65749;
-    UPDATE crapcyb SET idcessao = 65421 WHERE cdcooper = 11 AND nrdconta = 506478  AND idcessao = 65752;
-    UPDATE crapcyb SET idcessao = 65326 WHERE cdcooper = 13 AND nrdconta = 380830  AND idcessao = 65721;
-    UPDATE crapcyb SET idcessao = 65297 WHERE cdcooper = 16 AND nrdconta = 178756  AND idcessao = 65706;
-    UPDATE crapcyb SET idcessao = 65300 WHERE cdcooper = 16 AND nrdconta = 309621  AND idcessao = 65687;
-*/
