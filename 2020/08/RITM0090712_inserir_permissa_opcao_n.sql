20e5c750bccadd764500ca9fee3541a4b80bcad4
André Ricardo Würges <andre.wurges@mouts.info>
2020-08-25 10:57:04 -0300

    Ajustes na consulta dos operadores

--- 2020/08/RITM0090712_inserir_permissa_opcao_n.sql
+++ 2020/08/RITM0090712_inserir_permissa_opcao_n.sql
@@ -1,10 +1,17 @@
 declare
   cursor cr_crapope is
+    --Somente operadores que possuem permissão na opção I da MATRIC
     select co.cdoperad, co.cdcooper
       from crapope co
      where co.cdcooper = 11
        and co.cdsitope = 1
-       and co.cdoperad <> '1';
+       and co.cdoperad <> '1'
+       and co.cdoperad in (select distinct ca.cdoperad
+                             from crapace ca
+                            where ca.cdcooper = 11
+                              and ca.nmdatela = 'MATRIC'
+                              and ca.cddopcao = 'I'
+                              and ca.idambace = 2);
   rw_crapope cr_crapope%rowtype;
 begin
   for rw_crapope in cr_crapope loop
