c6cc79f3001079b11027a9c029bf48fff52c0ebd
Diógenes Lazzarini <diogenes.lazzarini@ailos.coop.br>
2020-08-25 10:15:04 -0300

    Create INC0059196_ususenha.sql

---
+++ 2020/08/INC0059196_ususenha.sql
@@ -0,0 +1,128 @@
+select * from crapprm p
+where p.cdacesso = 'USUARIO_AUTH_REG_CTR' or p.cdacesso = 'SENHA_AUTH_REG_CTR' and cdcooper = 1
+
+
+UPDATE crapprm p
+SET p.dsvlrprm = '49540953014'
+WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' AND p.cdcooper = 1;
+
+UPDATE crapprm p
+SET p.dsvlrprm = '18677819053'
+WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' AND p.cdcooper = 2;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 
+WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' AND p.cdcooper = 3;
+
+UPDATE crapprm p
+SET p.dsvlrprm = '30358386071'
+WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' AND p.cdcooper = 5;
+
+UPDATE crapprm p
+SET p.dsvlrprm = '76698111023'
+WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' AND p.cdcooper = 6;
+
+UPDATE crapprm p
+SET p.dsvlrprm = '85439293000'
+WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' AND p.cdcooper = 7;
+
+UPDATE crapprm p
+SET p.dsvlrprm = '26842037093'
+WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' AND p.cdcooper = 8;
+
+UPDATE crapprm p
+SET p.dsvlrprm = '20860636011' 
+WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' AND p.cdcooper = 9;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 
+WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' AND p.cdcooper = 10;
+
+UPDATE crapprm p
+SET p.dsvlrprm = '08244865016'
+WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' AND p.cdcooper = 11;
+
+UPDATE crapprm p
+SET p.dsvlrprm = '46442568032'
+WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' AND p.cdcooper = 12;
+
+UPDATE crapprm p
+SET p.dsvlrprm = '57343225049'
+WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' AND p.cdcooper = 13;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 
+WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' AND p.cdcooper = 14;
+
+UPDATE crapprm p
+SET p.dsvlrprm = '01055854088'
+WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' AND p.cdcooper = 16;
+
+-- SENHA_AUTH_REG_CTR
+
+UPDATE crapprm p
+SET p.dsvlrprm = 'Ailos@2020'
+WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' AND p.cdcooper = 1;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 'Ailos@2020'
+WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' AND p.cdcooper = 2;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 
+WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' AND p.cdcooper = 3;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 'Ailos@2020'
+WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' AND p.cdcooper = 5;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 'Ailos@2020'
+WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' AND p.cdcooper = 6;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 'Ailos@2020'
+WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' AND p.cdcooper = 7;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 'Ailos@2020'
+WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' AND p.cdcooper = 8;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 'Ailos@2020' 
+WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' AND p.cdcooper = 9;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 
+WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' AND p.cdcooper = 10;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 'Ailos@2020'
+WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' AND p.cdcooper = 11;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 'Ailos@2020'
+WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' AND p.cdcooper = 12;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 'Ailos@2020'
+WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' AND p.cdcooper = 13;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 
+WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' AND p.cdcooper = 14;
+
+UPDATE crapprm p
+SET p.dsvlrprm = 'Ailos@2020'
+WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' AND p.cdcooper = 16;
+
+
+NMRESCOP	CDCOOPER
+AILOS	3
+UNILOS	6
+EVOLUA	14
+CREDIMILSUL	15
+CREDICOMIN	10
+TRANSULCRED	17
+CONCREDI	4
+
