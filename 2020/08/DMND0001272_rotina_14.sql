94bb40d4bdfdeec8e417112302bd68f35909dc15
gabrielfronza <gfronzaeng@gmail.com>
2020-08-25 18:21:56 -0300

    Update

--- 2020/08/DMND0001272_rotina_14.sql
+++ 2020/08/DMND0001272_rotina_14.sql
@@ -1,7 +1,7 @@
 begin
 
   insert into tbgen_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
-  values ('MANPLD_PRODUTOS', '17', 'Rotina 14 - Pagamentos');
+  values ('MANPLD_PRODUTOS', '15', 'Rotina 14 - Pagamentos');
   
   insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
   values ('CRED', 0, 'MANPLD_VLCORTE_14', 'Valor de corte que obriga preenchimento de campos.', 2000);
