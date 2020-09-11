a60496ecd02b0525ad0b89104640c4bb50fa4bd9
Daniel Zimmermann <daniel.zimmermann@ailos.coop.br>
2020-09-11 17:10:49 -0300

    Ajuste para tirar vinculo crapdcc e crapcst do bordero excluido

--- 2020/09/INC0060820_Excluir_Bordero_Cheque.sql
+++ 2020/09/INC0060820_Excluir_Bordero_Cheque.sql
@@ -1,5 +1,4 @@
 -- Script exclusão bordero de cheque
-
 DELETE FROM crapbdc bdc
  WHERE bdc.cdcooper = 1
    AND bdc.nrdconta = 8383316
@@ -9,5 +8,17 @@ DELETE FROM crapbdc bdc
          WHERE cdb.cdcooper = bdc.cdcooper
            AND cdb.nrdconta = bdc.nrdconta
            AND cdb.nrborder = bdc.nrborder);
+
+UPDATE crapdcc
+   SET nrborder = 0
+ WHERE cdcooper = 1
+   AND nrdconta = 8383316
+   AND nrborder = 2016803;
+
+UPDATE crapcst
+   SET nrborder = 0
+ WHERE cdcooper = 1
+   AND nrdconta = 8383316
+   AND nrborder = 2016803;  
 
 COMMIT;
