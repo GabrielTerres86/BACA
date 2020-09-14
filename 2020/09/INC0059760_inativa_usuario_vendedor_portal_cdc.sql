1f4efbabca38ea60eb50ab3639550471b7b99037
t0033065 <adriano.luis@amcom.com.br>
2020-09-14 17:02:22 -0300

    INC0059760 - Crédito - Dúvida - CDC - 200827-008045

---
+++ 2020/09/INC0059760_inativa_usuario_vendedor_portal_cdc.sql
@@ -0,0 +1,14 @@
+update   tbepr_cdc_usuario
+set FLGATIVO=0
+where DSLOGIN='15687976000151';
+
+update   tbepr_cdc_usuario u
+set FLGATIVO=0
+where DSLOGIN='APP_COOPERADO'
+and   idusuario = (select  i.idusuario
+                   from    tbepr_cdc_vendedor v
+                          ,tbepr_cdc_usuario_vinculo i
+                   where v.nrcpf         = 15687976000151
+                   AND   v.idvendedor    = i.idvendedor
+                   AND i.idusuario     = u.idusuario
+                  );
