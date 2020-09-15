a0e961c594f33e991e7922ee47d5f0f673618f92
Douglas Pagel <douglas.pagel@ailos.coop.br>
2020-09-15 11:39:19 -0300

    prj0015279 validação da segunda entrega

---
+++ 2020/09/prj0015279_valida_prod.sql
@@ -0,0 +1,37 @@
+declare
+vr_dtmvtolt crapdat.dtmvtolt%TYPE;
+vr_cdcooper crawepr.cdcooper%TYPE;
+vr_nrdconta crawepr.nrdconta%TYPE;
+vr_nrctremp crawepr.nrctremp%TYPE;
+vr_dscritic crapcri.dscritic%TYPE;
+vr_cdcritic crapcri.cdcritic%TYPE;
+vr_dsmensag varchar2(2000);
+
+begin
+  --Parametros
+  vr_cdcooper := 1;
+  vr_nrdconta := 000;
+  vr_nrctremp := 000;
+  
+  -- Busca a data atual
+  select dtmvtolt into vr_dtmvtolt from crapdat where cdcooper = vr_cdcooper;
+
+  -- Call the procedure
+  cecred.este0001.pc_incluir_proposta_est(pr_cdcooper => vr_cdcooper,
+                                          pr_cdagenci => 90,
+                                          pr_cdoperad => 996,
+                                          pr_cdorigem => 3,
+                                          pr_nrdconta => vr_nrdconta,
+                                          pr_nrctremp => vr_nrctremp,
+                                          pr_dtmvtolt => vr_dtmvtolt,
+                                          pr_nmarquiv => '',
+                                          pr_dsmensag => vr_dsmensag,
+                                          pr_cdcritic => vr_cdcritic,
+                                          pr_dscritic => vr_dscritic);
+                                          
+  dbms_output.put_line('cdcritic: ' || vr_cdcritic);
+  dbms_output.put_line('dscritic: ' || vr_dscritic);
+  dbms_output.put_line('dsmensag: ' || vr_dsmensag);
+                                            
+end;
+
