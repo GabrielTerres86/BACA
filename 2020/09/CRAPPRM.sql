28f2ae3cd45a1bc84c3e5d734009378f5582338c
Andrey <t0032436@ailos.coop.br>
2020-09-15 11:56:10 -0300

    Scripts projeto

--- 2020/09/CRAPPRM.sql
+++ 2020/09/CRAPPRM.sql
@@ -16,6 +16,17 @@ BEGIN
      'CONSOR_SEQ_RETORNO',
      'Sequancial de retorno do arquivo que vamos enviar para Administradora',
      '3405');
+	 
+  INSERT INTO crapprm
+    (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+  VALUES 
+    ('CRED', 0, 'DIR_658_ENVIA', 'Diretorio que enviar os arquivos de convenios', '/usr/connect/sicredi/envia');
+
+  INSERT INTO crapprm 
+    (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+  VALUES
+    ('CRED', 0, 'DIR_658_ENVIADOS', 'Diretorio que possui os arquivos enviados e processados dos convenios', '/usr/connect/sicredi/enviados');
+	
      COMMIT;
 EXCEPTION
   WHEN OTHERS THEN
