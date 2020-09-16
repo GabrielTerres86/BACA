ebe1d16c088472986da05cc67d621663a4aff771
Paulo Felipe Kowalsky <paulo.kowalsky@mouts.info>
2020-09-16 11:23:56 -0300

    Update na tabela CrapTab/INC0058669

--- 2020/09/INC0058669 - Update_Craptab.sql
+++ 2020/09/INC0058669 - Update_Craptab.sql
@@ -2,8 +2,8 @@
 update craptab c
   set DSTEXTAB = ('7,91,128,130,260,274,368')
 where c.cdcooper = 0
-  and UPPER(c.nmsistem) = '%CRED%'
-  and UPPER(c.tptabela) = '%GENERI%'
+  and UPPER(c.nmsistem) = 'CRED'
+  and UPPER(c.tptabela) = 'GENERI'
   and c.cdempres = 0
   and UPPER(c.cdacesso) = 'AGE_ATIVAS_CAF'
   and c.tpregist = 0 ;
