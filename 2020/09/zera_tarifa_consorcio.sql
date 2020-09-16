e154314ced55f13ac4b881c93641b823e21dd1b6
Lucas Eduardo ranghetti <lucas.ranghetti@ailos.coop.br>
2020-09-16 16:35:29 -0300

    Remover script de zerar tarifa

--- 2020/09/zera_tarifa_consorcio.sql
+++
@@ -1,9 +0,0 @@
-BEGIN
-  UPDATE crapthi i SET i.vltarifa = 0
-   WHERE i.cdhistor IN (1230, 1231, 1232, 1233, 1234, 2027)
-     AND i.dsorigem = 'AIMARO';
-     COMMIT;
-EXCEPTION
-  WHEN OTHERS THEN
-    dbms_output.put_line('Erro: '||sqlerrm);
-END;
