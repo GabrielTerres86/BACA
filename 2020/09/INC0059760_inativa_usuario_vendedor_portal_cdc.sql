bafaba40839a0f2099984e484fbb981874188fa9
t0033065 <adriano.luis@amcom.com.br>
2020-09-17 10:28:44 -0300

    INC0059760 - Crédito - Dúvida - CDC - 200827-008045

--- 2020/09/INC0059760_inativa_usuario_vendedor_portal_cdc.sql
+++ 2020/09/INC0059760_inativa_usuario_vendedor_portal_cdc.sql
@@ -1,16 +1,23 @@
-update   tbepr_cdc_usuario
-set FLGATIVO=0
-where DSLOGIN='15687976000151';
+BEGIN
+  update   tbepr_cdc_usuario
+  set FLGATIVO=0
+  where DSLOGIN='15687976000151';
+--
+  update   tbepr_cdc_usuario u
+  set FLGATIVO=0
+  where DSLOGIN='APP_COOPERADO'
+  and   idusuario = (select  i.idusuario
+                     from    tbepr_cdc_vendedor v
+                            ,tbepr_cdc_usuario_vinculo i
+                     where v.nrcpf         = 15687976000151
+                     AND   v.idvendedor    = i.idvendedor
+                     AND i.idusuario     = u.idusuario
+                    );
 
-update   tbepr_cdc_usuario u
-set FLGATIVO=0
-where DSLOGIN='APP_COOPERADO'
-and   idusuario = (select  i.idusuario
-                   from    tbepr_cdc_vendedor v
-                          ,tbepr_cdc_usuario_vinculo i
-                   where v.nrcpf         = 15687976000151
-                   AND   v.idvendedor    = i.idvendedor
-                   AND i.idusuario     = u.idusuario
-                  );
-
-commit;
+--
+  commit;
+EXCEPTION
+  WHEN others THEN
+    rollback;
+    raise_application_error(-20501, 'Erro : ' || sqlerrm);
+END;
