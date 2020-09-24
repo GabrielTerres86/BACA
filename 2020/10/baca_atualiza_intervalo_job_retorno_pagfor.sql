dfca4daa08cfa53297e127ae89add7fafc9c1a45
David Giovanni Kistner <david.kistner@ailos.coop.br>
2020-09-24 10:18:55 -0300

    Remover script de update do JOB do Pagfor.

--- 2020/10/baca_atualiza_intervalo_job_retorno_pagfor.sql
+++
@@ -1,7 +0,0 @@
-BEGIN
-  DBMS_SCHEDULER.SET_ATTRIBUTE (
-   name         =>  'CECRED.JBCONV_PROC_RETORNO_PAGFOR',
-   attribute    =>  'repeat_interval',
-   value        =>  'Freq=MINUTELY;Interval=5;ByDay=MON,TUE,WED,THU,FRI;ByHour=8,9,10,11,12,13,14,15,16,17,18,19,20;BySecond=0');
-END;
-/
