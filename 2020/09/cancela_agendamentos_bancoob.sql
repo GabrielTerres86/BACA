c7193be30cc7b70b2e9645f8020f3c5ab111dec0
Lucas Reinert <lucas.reinert@ailos.coop.br>
2020-09-25 09:53:29 -0300

    Script para cancelar agendamentos bancoob sem saldo

---
+++ 2020/09/cancela_agendamentos_bancoob.sql
@@ -0,0 +1,14 @@
+UPDATE craplau SET insitlau = 3 WHERE idlancto = 39212866;
+UPDATE craplau SET insitlau = 3 WHERE idlancto = 39212868;
+UPDATE craplau SET insitlau = 3 WHERE idlancto = 39212869;
+UPDATE craplau SET insitlau = 3 WHERE idlancto = 39289235;
+UPDATE craplau SET insitlau = 3 WHERE idlancto = 39290488;
+UPDATE craplau SET insitlau = 3 WHERE idlancto = 39291387;
+UPDATE craplau SET insitlau = 3 WHERE idlancto = 39291581;
+UPDATE craplau SET insitlau = 3 WHERE idlancto = 39289529;
+UPDATE craplau SET insitlau = 3 WHERE idlancto = 39162363;
+UPDATE craplau SET insitlau = 3 WHERE idlancto = 39291119;
+UPDATE craplau SET insitlau = 3 WHERE idlancto = 39289757;
+UPDATE craplau SET insitlau = 3 WHERE idlancto = 39153560;
+
+commit;
