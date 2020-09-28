7276874f5a2cfd22b8b3e6e74f5f35b8b8d73597
Lucas Eduardo ranghetti <lucas.ranghetti@ailos.coop.br>
2020-09-28 17:03:37 -0300

    Ajuste

--- 2020/09/INC0062544_Integra_Consorcio.sql
+++ 2020/09/INC0062544_Integra_Consorcio.sql
@@ -1,23 +1,23 @@
-begin
-UPDATE CRAPPRM PRM
-       SET PRM.DSVLRPRM = 6796
-     WHERE PRM.CDCOOPER = 0
-       AND PRM.NMSISTEM = 'CRED'
-       AND PRM.CDACESSO = 'CONSOR_SEQ_REMESSA' ;
-  commit;       
-end;
-/
-
 declare 
-pr_cdcooper number := 3;
-  pr_cdcritic number;
-  pr_dscritic varchar2(2000);  
-begin
-  cecred.PC_CNS_RECEBE_ARQ(pr_cdcooper => pr_cdcooper,
-                           pr_cdcritic => pr_cdcritic,
-                           pr_dscritic => pr_dscritic);
-  if nvl(pr_dscritic,' ') <> ' ' or nvl(pr_cdcritic ,0) <> 0 then
-     DBMS_OUTPUT.PUT(pr_dscritic);     
+  vr_cdcritic number;
+  vr_dscritic varchar2(2000);  
+BEGIN
+  -- Atualizar para sequancia correta e commitar o ajusta da sequencia
+  begin
+    UPDATE CRAPPRM PRM
+           SET PRM.DSVLRPRM = 6796
+         WHERE PRM.CDCOOPER = 0
+           AND PRM.NMSISTEM = 'CRED'
+           AND PRM.CDACESSO = 'CONSOR_SEQ_REMESSA' ;
+    commit;       
+  end;
+  
+  -- Processar arquivos pendentes
+  cecred.PC_CNS_RECEBE_ARQ(pr_cdcooper => 3,
+                           pr_cdcritic => vr_cdcritic,
+                           pr_dscritic => vr_dscritic);
+
+  if TRIM(vr_dscritic) IS NOT NULL then
+     DBMS_OUTPUT.PUT(vr_dscritic);     
   end if; 
 end;
-/
