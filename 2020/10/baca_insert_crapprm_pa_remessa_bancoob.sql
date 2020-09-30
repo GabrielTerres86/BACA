1e3d3e49c2467db9f6d81bc2a111dc05a4cf9a1d
David Giovanni Kistner <david.kistner@ailos.coop.br>
2020-09-30 13:10:18 -0300

    Tratamento INC0062341 Atualização das lista de PAs de contingência

--- 2020/10/baca_insert_crapprm_pa_remessa_bancoob.sql
+++ 2020/10/baca_insert_crapprm_pa_remessa_bancoob.sql
@@ -3,13 +3,13 @@ values ('CRED', 1, 'CONTINGENCIA_PA_REM_BCB', 'PA que será utilizado como contig
 insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
 values ('CRED', 2, 'CONTINGENCIA_PA_REM_BCB', 'PA que será utilizado como contigência no arquivo de remessa quando PA do cooperado não estiver cadastrado no CAF e Bancoob', '14');
 insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
-values ('CRED', 5, 'CONTINGENCIA_PA_REM_BCB', 'PA que será utilizado como contigência no arquivo de remessa quando PA do cooperado não estiver cadastrado no CAF e Bancoob', '1');
+values ('CRED', 5, 'CONTINGENCIA_PA_REM_BCB', 'PA que será utilizado como contigência no arquivo de remessa quando PA do cooperado não estiver cadastrado no CAF e Bancoob', '9');
 insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
 values ('CRED', 6, 'CONTINGENCIA_PA_REM_BCB', 'PA que será utilizado como contigência no arquivo de remessa quando PA do cooperado não estiver cadastrado no CAF e Bancoob', '1');
 insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
-values ('CRED', 7, 'CONTINGENCIA_PA_REM_BCB', 'PA que será utilizado como contigência no arquivo de remessa quando PA do cooperado não estiver cadastrado no CAF e Bancoob', '1');
+values ('CRED', 7, 'CONTINGENCIA_PA_REM_BCB', 'PA que será utilizado como contigência no arquivo de remessa quando PA do cooperado não estiver cadastrado no CAF e Bancoob', '9');
 insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
-values ('CRED', 8, 'CONTINGENCIA_PA_REM_BCB', 'PA que será utilizado como contigência no arquivo de remessa quando PA do cooperado não estiver cadastrado no CAF e Bancoob', '1');
+values ('CRED', 8, 'CONTINGENCIA_PA_REM_BCB', 'PA que será utilizado como contigência no arquivo de remessa quando PA do cooperado não estiver cadastrado no CAF e Bancoob', '4');
 insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
 values ('CRED', 9, 'CONTINGENCIA_PA_REM_BCB', 'PA que será utilizado como contigência no arquivo de remessa quando PA do cooperado não estiver cadastrado no CAF e Bancoob', '1');
 insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
