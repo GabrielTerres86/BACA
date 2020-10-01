407190bee6c3c8af7beb330a1f6a236651a995f6
David Giovanni Kistner <david.kistner@ailos.coop.br>
2020-10-01 08:20:54 -0300

    Projeto Migração Convênios Sicredi Incluir novo parâmetro para controle de status da integração com a TIVIT

--- 2020/10/baca_insert_parametros_crapprm_tivit.sql
+++ 2020/10/baca_insert_parametros_crapprm_tivit.sql
@@ -4,4 +4,6 @@ insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
 values ('CRED', 0, 'DIR_TIVIT_CONNECT_REC', 'Diretório onde arquivos de retorno serão postados pela TIVIT via Connect Direct', '/usr/connect/tivit/recebe/');
 insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
 values ('CRED', 0, 'DIR_TIVIT_CONNECT_RECBS', 'Diretório onde arquivos de retorno postados pela TIVIT via Connect Direct serão salvos', '/usr/connect/tivit/recebidos/');
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 0, 'SITUACAO_TIVIT', 'Indicador da situação da integração com TIVIT (0-Inativo / 1-Ativo)', '1');
 commit;
