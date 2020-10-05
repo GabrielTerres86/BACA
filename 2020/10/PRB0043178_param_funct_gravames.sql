8441ff4c2e48b9cd5496e6412280dd0be6a4d943
Rafael Innig <rafael.innig@amcom.com.br>
2020-10-05 17:23:54 -0300

    Gravames - Baca que cria parâmetros de sequência de processamento no SOA e cria parâmetros de lote de processamento de gravames.

---
+++ 2020/10/PRB0043178_param_funct_gravames.sql
@@ -0,0 +1,46 @@
+ -- Cria parâmetros necessários para funcionamento da função.
+  declare
+    CURSOR cr_crapcop IS
+      SELECT c.cdcooper
+        FROM crapcop      c
+       WHERE c.flgativo = 1;
+    rw_crapcop cr_crapcop%ROWTYPE; 
+  begin
+    FOR rw_crapcop IN cr_crapcop LOOP   
+      insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+      values ('CRED'
+             ,rw_crapcop.cdcooper
+             ,'SEQ_PROC_B3_NRO'
+             ,'Sequencial de processamento do arquivo na B3.'
+             ,'0');
+      --      
+      insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+      values ('CRED'
+             ,rw_crapcop.cdcooper
+             ,'SEQ_PROC_B3_DATA'
+             ,'Data do sequencial de processamento do arquivo na B3. Incrementa se processo é na mesma data ou reinicia e atualiza data se data é diferente.'
+             ,to_char(sysdate,'dd/mm/rrrr'));
+      --
+    END LOOP;
+    COMMIT;
+  end;
+  --
+  -- Cria parâmetros necessários para funcionamento da função.
+  declare
+    CURSOR cr_crapcop IS
+      SELECT c.cdcooper
+        FROM crapcop      c
+       WHERE c.flgativo = 1;
+    rw_crapcop cr_crapcop%ROWTYPE; 
+  begin
+    FOR rw_crapcop IN cr_crapcop LOOP   
+      insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+      values ('CRED'
+             ,rw_crapcop.cdcooper
+             ,'SEQ_PROC_LOTE_NRO'
+             ,'Lote de processamento do arquivo por cooperativa.'
+             ,'0');
+    END LOOP;
+    COMMIT;
+  end;
+  
