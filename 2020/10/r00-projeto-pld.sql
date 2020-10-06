a70dbbf184271feef7bd1fe9ef3719bfcfbffc72
gabrielfronza <gfronzaeng@gmail.com>
2020-10-06 16:20:10 -0300

    Update

--- 2020/10/r00-projeto-pld.sql
+++ 2020/10/r00-projeto-pld.sql
@@ -28,27 +28,6 @@ BEGIN
        1025);
   END;
   --
-  BEGIN
-    UPDATE tbcc_monitoramento_parametro a
-       SET a.vlmonitoracao_pagamento = 2000;
-  EXCEPTION
-    WHEN OTHERS THEN
-      raise_application_error(-20000, dbms_utility.format_error_backtrace);
-  END;
-  --
-  BEGIN
-    UPDATE craptab a
-       SET a.dstextab = '2000,01'
-     WHERE a.cdcooper > -1
-       AND a.nmsistem = 'CRED'
-       AND a.tptabela = 'GENERI'
-       AND a.cdempres = 0
-       AND a.cdacesso = 'VLCTRMVESP';
-  EXCEPTION
-    WHEN OTHERS THEN
-      raise_application_error(-20000, dbms_utility.format_error_backtrace);
-  END;
-  --
   COMMIT;
 END;
 /
