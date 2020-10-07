439270c31ffafdfe3e7866a0c2519a045eab63f1
Anderson <anderson.fossa@ailos.coop.br>
2020-10-07 11:14:51 -0300

    Adicao dos indices de ipca numerico ja existentes.

--- 2020/10/PRJ0018631-IPCA-config.sql
+++ 2020/10/PRJ0018631-IPCA-config.sql
@@ -2,6 +2,33 @@
 insert into crapind (CDDINDEX, NMDINDEX, IDPERIOD, IDCADAST, IDEXPRES, PROGRESS_RECID)
 values (5, 'IPCA NUMERICO', 2, 1, 1, null);
 
+insert into craptxi (CDDINDEX, DTINIPER, DTFIMPER, VLRDTAXA, DTCADAST, PROGRESS_RECID)
+values (5, to_date('01-12-2019', 'dd-mm-yyyy'), to_date('01-12-2019', 'dd-mm-yyyy'), 5320.25000000, to_date('29-09-2020', 'dd-mm-yyyy'), null);
+
+insert into craptxi (CDDINDEX, DTINIPER, DTFIMPER, VLRDTAXA, DTCADAST, PROGRESS_RECID)
+values (5, to_date('01-01-2020', 'dd-mm-yyyy'), to_date('01-01-2020', 'dd-mm-yyyy'), 5331.42000000, to_date('14-09-2020', 'dd-mm-yyyy'), null);
+
+insert into craptxi (CDDINDEX, DTINIPER, DTFIMPER, VLRDTAXA, DTCADAST, PROGRESS_RECID)
+values (5, to_date('01-02-2020', 'dd-mm-yyyy'), to_date('01-02-2020', 'dd-mm-yyyy'), 5344.75000000, to_date('14-09-2020', 'dd-mm-yyyy'), null);
+
+insert into craptxi (CDDINDEX, DTINIPER, DTFIMPER, VLRDTAXA, DTCADAST, PROGRESS_RECID)
+values (5, to_date('01-03-2020', 'dd-mm-yyyy'), to_date('01-03-2020', 'dd-mm-yyyy'), 5348.49000000, to_date('14-09-2020', 'dd-mm-yyyy'), null);
+
+insert into craptxi (CDDINDEX, DTINIPER, DTFIMPER, VLRDTAXA, DTCADAST, PROGRESS_RECID)
+values (5, to_date('01-04-2020', 'dd-mm-yyyy'), to_date('01-04-2020', 'dd-mm-yyyy'), 5331.91000000, to_date('14-09-2020', 'dd-mm-yyyy'), null);
+
+insert into craptxi (CDDINDEX, DTINIPER, DTFIMPER, VLRDTAXA, DTCADAST, PROGRESS_RECID)
+values (5, to_date('01-05-2020', 'dd-mm-yyyy'), to_date('01-05-2020', 'dd-mm-yyyy'), 5311.65000000, to_date('15-09-2020', 'dd-mm-yyyy'), null);
+
+insert into craptxi (CDDINDEX, DTINIPER, DTFIMPER, VLRDTAXA, DTCADAST, PROGRESS_RECID)
+values (5, to_date('01-06-2020', 'dd-mm-yyyy'), to_date('01-06-2020', 'dd-mm-yyyy'), 5325.46000000, to_date('15-06-2020', 'dd-mm-yyyy'), null);
+
+insert into craptxi (CDDINDEX, DTINIPER, DTFIMPER, VLRDTAXA, DTCADAST, PROGRESS_RECID)
+values (5, to_date('01-07-2020', 'dd-mm-yyyy'), to_date('01-07-2020', 'dd-mm-yyyy'), 5344.63000000, to_date('15-07-2020', 'dd-mm-yyyy'), null);
+
+insert into craptxi (CDDINDEX, DTINIPER, DTFIMPER, VLRDTAXA, DTCADAST, PROGRESS_RECID)
+values (5, to_date('01-08-2020', 'dd-mm-yyyy'), to_date('01-08-2020', 'dd-mm-yyyy'), 5357.46000000, to_date('15-08-2020', 'dd-mm-yyyy'), null);
+
 
 insert into crapprg ( 
 select 'CRED' nmsistem
