1e28b82450c5fdeb52aa716848b726499382f763
Jackson Rosá <jackson.rosa@ailos.coop.br>
2020-10-07 13:19:24 -0300

    Cancelamento de cheques da VIACREDI nao retirados antes de 31/12/2014

--- 2020/09/RITM0088836_alteracao.sql
+++ 2020/09/RITM0088836_alteracao.sql
@@ -1,11 +1,17 @@
 BEGIN
    /* Cancelamento de cheques da VIACREDI não retirados antes de 31/12/2014 */
-   UPDATE crapfdc fdc
-      SET dtretchq = trunc(SYSDATE), dtliqchq = trunc(SYSDATE), incheque = 8
-    WHERE fdc.cdcooper = 1
-      AND fdc.dtretchq IS NULL
-      AND fdc.incheque = 0
-      AND fdc.dtemschq <= '31/12/2014';
+	UPDATE crapfdc fdc
+	   SET dtretchq = trunc(SYSDATE), 
+		   dtliqchq = trunc(SYSDATE), 
+		   incheque = 8
+	 WHERE fdc.cdcooper = 1
+	   AND fdc.dtretchq IS NULL
+	   AND fdc.incheque = 0
+	   AND progress_recid >= 6685544
+	   AND progress_recid <= 37879967
+	   AND fdc.cdbanchq IN (1, 85, 756)
+	   AND fdc.cdagechq IN (101, 103, 3239, 3420, 4415)
+	   AND fdc.dtemschq <= '31/12/2014';
 
    COMMIT;
 
