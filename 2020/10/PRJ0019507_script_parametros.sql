5aa2603b1aaf4ab4023dd9fc529c31a234feb59b
Elton Giusti <t0031664@ailos.coop.br>
2020-10-21 15:49:46 -0300

    P19507 - Crapaca tela reccrd

--- 2020/10/PRJ0019507_script_parametros.sql
+++ 2020/10/PRJ0019507_script_parametros.sql
@@ -8,4 +8,9 @@ SET a.lstparam = a.lstparam||',pr_flgrepro'
 where a.nmdeacao = 'ALTLINHA'
 AND a.nmpackag = 'TELA_LCREDI';
 
+INSERT INTO CRAPACA (NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
+VALUES ('BUSCA_FAIXA_RECIPRO_TAXA', 'TELA_RECCRD', 'pc_busca_faixa_recipro_web', 'pr_tpproduto,pr_cdcatego,pr_inpessoa ',
+                      NVL((SELECT a.nrseqrdr FROM CRAPRDR a WHERE a.nmprogra = 'TELA_RECCRD' AND ROWNUM = 1),1));                                                                 
+            
+			
 commit;
