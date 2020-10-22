3e6675e49b5e291ba5f7758fbba3cf8d2153b868
Elton Giusti <t0031664@ailos.coop.br>
2020-10-22 10:31:04 -0300

    P19507 - Crapaca tela TELA_RECCRD

--- 2020/10/PRJ0019507_script_parametros.sql
+++ 2020/10/PRJ0019507_script_parametros.sql
@@ -13,7 +13,12 @@ INSERT INTO CRAPRDR (NMPROGRA, DTSOLICI) values ('TELA_RECCRD', sysdate);
 
 INSERT INTO CRAPACA (NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
 VALUES ('BUSCA_FAIXA_RECIPRO_TAXA', 'TELA_RECCRD', 'pc_busca_faixa_recipro_web', 'pr_tpproduto,pr_cdcatego,pr_inpessoa ',
-                      NVL((SELECT a.nrseqrdr FROM CRAPRDR a WHERE a.nmprogra = 'TELA_RECCRD' AND ROWNUM = 1),1));                                                                 
+                      (SELECT a.nrseqrdr FROM CRAPRDR a WHERE a.nmprogra = 'TELA_RECCRD' AND ROWNUM = 1));                                                                 
             
-			
+
+INSERT INTO CRAPACA (NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
+VALUES ('EXEC_ACAO_FAIXA_RECIPRO_TAXA', 'TELA_RECCRD', 'pc_executa_acao_web', 'pr_tpproduto,pr_cdcatego,pr_inpessoa,pr_cdfaixa_recipro,pr_tpdeacao
+            ,pr_peinicial,pr_pefinal,pr_dtinicio_vigencia,pr_dtfim_vigencia,pr_pedesconto',
+               (SELECT a.nrseqrdr FROM CRAPRDR a WHERE a.nmprogra = 'TELA_RECCRD' AND ROWNUM = 1));
+			   
 commit;
