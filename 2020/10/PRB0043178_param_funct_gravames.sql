19e05ebc87ea290adac2d0c5b12b14cbf8259df4
Rafael Innig <t0033292@ailos.coop.br>
2020-10-23 17:46:55 +0000

    Updated PRB0043178_param_funct_gravames.sql

--- 2020/10/PRB0043178_param_funct_gravames.sql
+++ 2020/10/PRB0043178_param_funct_gravames.sql
@@ -1,31 +1,4 @@
- -- Cria par‚metros necess·rios para funcionamento da funÁ„o.
-  declare
-    CURSOR cr_crapcop IS
-      SELECT c.cdcooper
-        FROM crapcop      c
-       WHERE c.flgativo = 1;
-    rw_crapcop cr_crapcop%ROWTYPE; 
-  begin
-    FOR rw_crapcop IN cr_crapcop LOOP   
-      insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
-      values ('CRED'
-             ,rw_crapcop.cdcooper
-             ,'SEQ_PROC_B3_NRO'
-             ,'Sequencial de processamento do arquivo na B3.'
-             ,'0');
-      --      
-      insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
-      values ('CRED'
-             ,rw_crapcop.cdcooper
-             ,'SEQ_PROC_B3_DATA'
-             ,'Data do sequencial de processamento do arquivo na B3. Incrementa se processo È na mesma data ou reinicia e atualiza data se data È diferente.'
-             ,to_char(sysdate,'dd/mm/rrrr'));
-      --
-    END LOOP;
-    COMMIT;
-  end;
-  --
-  -- Cria par‚metros necess·rios para funcionamento da funÁ„o.
+ -- Cria par√¢metros necess√°rios para funcionamento da fun√ß√£o.
   declare
     CURSOR cr_crapcop IS
       SELECT c.cdcooper
