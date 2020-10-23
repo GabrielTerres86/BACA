9a08d076d499b1137e48552f01c080fb9beaf3ed
Rafael Torres Estima <t0033404@cecred.coop.br>
2020-10-23 16:16:45 -0300

    Insert na crapaca da funcionalidade de gerar arquivo CSV na tela qbrsig

---
+++ 2020/10/RITM0093121.sql
@@ -0,0 +1,5 @@
+begin
+insert into crapaca (NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
+values ('QBRSIG_SALVAR_CSV', 'TELA_QBRSIG', 'pc_salva_quebra_csv', 'pr_nrseq_quebra_sigilo', 1744);
+commit;
+end;
