30515d3d82ecf3401bb85fabfe1715f93f49cdf1
Paulo Roberto Machado Rech <paulo.rech@ailos.coop.br>
2020-10-27 09:37:14 -0300

    PRJ0019429 - entrega 2

--- 2020/10/PRJ0019429 - entrega 2.sql
+++ 2020/10/PRJ0019429 - entrega 2.sql
@@ -1,4 +1,17 @@
 BEGIN
+  FOR coop IN (SELECT cdcooper FROM crapcop c WHERE c.flgativo = 1 AND c.cdcooper <> 3) LOOP
+    
+      INSERT INTO TBGEN_VERSAO_TERMO 
+        VALUES (
+          coop.cdcooper,
+          'TERMO ADESAO CDC PF V2',
+          to_date('01/01/2022', 'DD/MM/RRRR'),
+          NULL,
+          'termo_adesao_pf_now.jasper',
+          TRUNC(sysdate),
+          'Termo de adesão de cartão de Crédito AILOS para pessoa física da modalidade NOW'
+        );
+  END LOOP;
 
   --deletar configurações de limite
   DELETE FROM tbcrd_config_categoria a WHERE a.cdadmcrd = 19;
@@ -15,6 +28,7 @@ BEGIN
    UPDATE crapacb a 
       SET a.cdadmcrd = 18
     WHERE a.cdadmcrd = 19;
-   
-   COMMIT;    
+
+  
+  COMMIT;
 END;
