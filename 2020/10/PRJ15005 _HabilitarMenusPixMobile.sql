6a6340535926bf27e2358e472ad46d315e57e10b
Vinicius LIma <vinicius.lima@ailos.coop.br>
2020-10-29 10:32:17 -0300

    Update PRJ15005 _HabilitarMenusPixMobile.sql

--- 2020/10/PRJ15005 _HabilitarMenusPixMobile.sql
+++ 2020/10/PRJ15005 _HabilitarMenusPixMobile.sql
@@ -6,6 +6,6 @@ VALUES (1017,1009,'Pix Copia e Cola',1,0,1,'2.12.1.0',NULL);
 
 UPDATE menumobile SET habilitado = 1 WHERE menumobileid = 1011;
 UPDATE menumobile SET habilitado = 1 WHERE menumobileid = 1012;
-UPDATE menumobile SET habilitado = 1 WHERE menumobileid = 1014;
+UPDATE menumobile SET habilitado = 0 WHERE menumobileid = 1014;
     
 COMMIT;
