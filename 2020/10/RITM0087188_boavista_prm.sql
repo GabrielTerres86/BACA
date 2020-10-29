a028e0f614caec4e241170acc221bce6cee9b90e
Odirlei Busana <odirlei.busana@ailos.coop.br>
2020-10-29 17:06:10 -0300

    Ajuste script

--- 2020/10/RITM0087188_boavista_prm.sql
+++ 2020/10/RITM0087188_boavista_prm.sql
@@ -9,7 +9,7 @@ values ('CRED', 0, 'SFTP_PORTA_BOAVISTA', 'Porta do ftp do BOA VISTA', '9022');
 COMMIT;
 
 insert into crappbc (IDTPREME, FLGATIVO, DSFNCHRM, FLREMSEQ, IDTPENVI, DSDIRENV, DSFNBURM, DSSITFTP, DSUSRFTP, DSPWDFTP, DSDREFTP, DSDRENCD, DSDREVCD, DSFNRNEN, IDTPRETO, IDOPRETO, QTHORRET, DSDRRFTP, DSDRRECD, DSDRRTCD, DSDIRRET, DSFNBURT, DSFNRNDV, DSFNCHRT, QTINTERR, IDTPSOLI, IDENVSEG)
-values ('BOAVISTA', 1, 'cybe0002.fn_chvrem_serasa', 1, 'F', '/micros/cecred/cyber/boavista/envia', 'cybe0002.fn_chbusca_ppware', 'ts.bvsnet.com.br', 'ailos', 'Central2020.', 'negativacao/remessa', null, null, null, null, 'M', 1, 'negativacao/retorno', null, null, '/micros/cecred/cyber/boavista/recebe', 'cybe0002.fn_nmarquiv_ret', null, null, null, 'A', 'S');
+values ('BOAVISTA', 0, 'cybe0002.fn_chvrem_serasa', 1, 'F', '/micros/cecred/cyber/boavista/envia', 'cybe0002.fn_chbusca_ppware', 'ts.bvsnet.com.br', 'ailos', 'Central2020.', 'negativacao/remessa', null, null, null, null, 'M', 1, 'negativacao/retorno', null, null, '/micros/cecred/cyber/boavista/recebe', 'cybe0002.fn_nmarquiv_ret', null, null, null, 'A', 'S');
 
 COMMIT;
 
