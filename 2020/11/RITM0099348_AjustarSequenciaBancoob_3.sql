52c769829ce281c1b950dd14c3bc84b8ed197c4b
Daniel Dallagnese - Mout'S <daniel.dallagnese@mouts.info>
2020-10-30 11:02:10 -0300

    RITM0099348
    Ajustar a sequência para que possamos integrar os arquivos recebidos.

---
+++ 2020/11/RITM0099348_AjustarSequenciaBancoob_3.sql
@@ -0,0 +1,15 @@
+﻿-- Net
+update crapcon c
+   set c.nrseqint = decode(c.cdcooper,
+                           2, 280,
+                           5, 30,
+                           6, 172,
+                           9, 35,
+                           12, 41,
+                           13, 11)
+ where c.cdempcon = 296
+   and c.cdsegmto = 4
+   and c.cdcooper in (2, 5, 6, 9, 12, 13)
+/
+commit
+/
