0542881ada79d0224238480c0f6e437b723a9fbf
Miguel Rodrigues <miguel.rodrigues@ailos.coop.br>
2020-11-04 17:42:19 -0300

    Ajuste na data de lancamento conta corrente

--- 2020/11/ajuste_lcm_pix.sql
+++ 2020/11/ajuste_lcm_pix.sql
@@ -1,9 +1,3 @@
-rem PL/SQL Developer Test Script
-
-set feedback off
-set autoprint off
-
-rem Execute PL/SQL Block
 -- Created on 04/11/2020 by F0032990 
 declare 
   CURSOR cr_craplcm IS
@@ -15,22 +9,24 @@ declare
                 from craplem t, craphis h
                 where t.cdcooper=h.cdcooper and t.cdhistor=h.cdhistor
                 and t.cdcooper IN (1, 16, 3) and t.cdhistor IN (1077, 1047,1044,1039)
-                and t.dtmvtolt='03/11/2020' AND t.dthrtran > to_date('04/11/2020 06:00','DD/MM/RRRR Hh24:MI'))
+                and t.dtmvtolt=to_date('03/11/2020','DD/MM/YYYY') AND t.dthrtran > to_date('04/11/2020 06:00','DD/MM/YYYY Hh24:MI'))
       AND t.cdpesqbb LIKE '%.%'
-      AND t.dtmvtolt = '03/11/2020' AND t.dttrans > to_date('04/11/2020 06:00','DD/MM/RRRR Hh24:MI')
+      AND t.dtmvtolt = '03/11/2020' AND t.dttrans > to_date('04/11/2020 06:00','DD/MM/YYYY Hh24:MI')
       AND h.indebcre = 'D';
       
 begin
 
   FOR rw_craplcm IN cr_craplcm LOOP
     UPDATE craplcm l
-       SET l.dtmvtolt = '04/11/2020'
+       SET l.dtmvtolt = to_date('04/11/2020','DD/MM/YYYY')
      WHERE l.progress_recid = rw_craplcm.progress_recid;
+    
+
   END LOOP;
 
   COMMIT;
 EXCEPTION 
   WHEN OTHERS THEN
+
     ROLLBACK;
 END;
-/
