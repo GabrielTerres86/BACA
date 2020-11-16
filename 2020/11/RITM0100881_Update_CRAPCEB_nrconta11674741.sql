7c9d2458e2ef98bdd5cf4d063d1bb8818fc51b3d
Alexandre Pietrobelli <alexandre.pietrobelli@supero.com.br>
2020-11-16 14:58:12 -0300

    RITM0100881  Liberação FTP - Convênio Internet
    RITM0100881  Liberação FTP - Convênio Internet
    
    Liberação da modalidade FTP para o cooperado  1167.474-1 (Icatu), convênio 101002. Para emissão de boletos para cobrança de inadimplentes.

--- 2020/11/RITM0100881_Update_CRAPCEB_nrconta11674741.sql
+++ 2020/11/RITM0100881_Update_CRAPCEB_nrconta11674741.sql
@@ -1,4 +1,5 @@
 update crapceb  set inenvcob  = 2 where cdcooper = 1 and nrdconta = 11674741 and nrconven = 101002;
-
 commit;
 
+insert into crapprm (cdcooper, NMSISTEM, CDACESSO, DSTEXPRM, DSVLRPRM) values ('1', 'CRED', 	'FTP_CONVENIO_11674741'	,'Contas que podem usar FTP pelo convenio internet ( 1-Ativo / 0-Inativo )'	,'1');
+COMMIT ;
--- 2020/11/RITM0100881_param_FTP_11674741.sql
+++
@@ -1,2 +0,0 @@
-insert into crapprm (cdcooper, NMSISTEM, CDACESSO, DSTEXPRM, DSVLRPRM) values ('1', 'CRED', 	'FTP_CONVENIO_11674741'	,'Contas que podem usar FTP pelo convenio internet ( 1-Ativo / 0-Inativo )'	,'1');
-COMMIT ;
