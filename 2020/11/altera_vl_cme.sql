de2bd3453e5cc0c605d17c7a0d089fa7eb0d1869
Jefferson Luiz da Silva - Mouts <t0033489@ailos.coop.br>
2020-11-18 15:33:51 -0300

    Alterado para todas as cooperativas, antes estava apenas para 11

--- 2020/11/altera_vl_cme.sql
+++ 2020/11/altera_vl_cme.sql
@@ -1,7 +1,6 @@
 UPDATE cecred.craptab
    SET dstextab = '2000,01'
- WHERE craptab.cdcooper = 11
-   AND craptab.nmsistem = 'CRED'
+ WHERE craptab.nmsistem = 'CRED'
    AND craptab.tptabela = 'GENERI'
    AND craptab.cdempres = 0
    AND craptab.cdacesso = 'VLCTRMVESP'
