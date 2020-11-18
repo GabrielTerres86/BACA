6e5791e84b8bf64d616d67757aace58e1b4230f4
Danilo Korczagin <danilo.korczagin@supero.com.br>
2020-11-18 16:53:51 -0300

    RITM0098655 - script de insert na crapprm

---
+++ 2020/11/RITM0098655-Insert_crapprm.sql
@@ -0,0 +1,20 @@
+BEGIN
+  BEGIN
+    insert into CRAPPRM
+      (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+    values
+      ('CRED',
+       3,
+       'SLC_GRAVA_LOG_TRANSACAO',
+       'Habilitar a gravação de logs das transacoes de cartoes, recebidos pelo SILOC(0 = Não Gravar / 1 = Gravar)',
+       '0',
+       NULL);
+    COMMIT;
+  EXCEPTION
+    WHEN OTHERS THEN
+      DBMS_OUTPUT.PUT_LINE('Erro = '||SQLERRM);
+  END;
+END;
+
+-- Log de liquidacao das transacoes de cartoes, recebidos pelo SILOC
+-- Status das transacoes de cartoes pendentes de processamento no domicilio bancario
