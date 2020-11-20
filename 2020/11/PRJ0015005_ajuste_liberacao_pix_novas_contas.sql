0e5e17ba6538a2a5d61d48cd3bb9a6d376ce14a2
Augusto <augusto_conceicao@hotmail.com>
2020-11-20 14:25:19 -0300

    Script para liberar as novas contas no PIX

--- 2020/11/PRJ0015005_ajuste_liberacao_pix_novas_contas.sql
+++ 2020/11/PRJ0015005_ajuste_liberacao_pix_novas_contas.sql
@@ -1,4 +1,4 @@
 BEGIN
-	UPDATE tbpix_crapass SET flliberacao_restrita = 1 WHERE dtdemiss IS NULL AND flliberacao_restrita = 0;
+	UPDATE tbpix_crapass SET flliberacao_restrita = 1 WHERE dtdemiss IS NULL AND flliberacao_restrita IS NULL;
 	COMMIT;
 END;
