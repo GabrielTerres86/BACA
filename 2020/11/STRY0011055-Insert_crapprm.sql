c6be772834092fdf4ece8d82d827f695ead8918c
Danilo Korczagin <danilo.korczagin@supero.com.br>
2020-11-20 16:32:56 -0300

    STRY0011055 - script de insert de parâmetro na tabela crapprm

---
+++ 2020/11/STRY0011055-Insert_crapprm.sql
@@ -0,0 +1,17 @@
+BEGIN
+  BEGIN
+    insert into CRAPPRM
+      (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+    values
+      ('CRED',
+       3,
+       'ENV_AILOS_NOW_CANAIS',
+       'Habilitar o envio de solicitação de autorização por meio dos canais para a Modalidade Ailos Now Personalizado (0 = Não Envia / 1 = Envia)',
+       '0',
+       NULL);
+    COMMIT;
+  EXCEPTION
+    WHEN OTHERS THEN
+      DBMS_OUTPUT.PUT_LINE('Erro = '||SQLERRM);
+  END;
+END;
