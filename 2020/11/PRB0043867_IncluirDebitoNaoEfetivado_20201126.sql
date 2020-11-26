326cd79ef2f112252c252bd9983fc8cbece54cbb
Daniel Dallagnese - Mout'S <daniel.dallagnese@mouts.info>
2020-11-26 11:26:55 -0300

    PRB0043867
    Ajustar o sequencial dos convênios que tiveram arquivos gerados manualmente.

--- 2020/11/PRB0043867_IncluirDebitoNaoEfetivado_20201126.sql
+++ 2020/11/PRB0043867_IncluirDebitoNaoEfetivado_20201126.sql
@@ -1,4 +1,12 @@
 -- PRB0043867
+-- Ajustar o sequencial dos convênios que tiveram arquivos gerados manualmente.
+insert into gncontr (cdcooper, tpdcontr, cdconven, dtmvtolt, nrsequen, cdsitret, nmarquiv, qtdoctos, vldoctos)
+values (1, 7, 8460082, '26/11/2020', 27, 2, '3239-EF00002700000000846008220201126.CNV', 1, 94.99)
+/
+update crapcon c
+   set c.nrseqatu = c.nrseqatu + 1
+ where (c.cdempcon, c.cdsegmto, c.cdcooper) in ((82, 4, 1))
+/
 -- Documentos com autorizações canceladas, pendente envio do registro F ao Bancoob.
 insert into tbconv_deb_nao_efetiv values ( 1,  8269238,  82, 4, 0, 'F8999909916233            3239000000082692382020112500000000000994430667903180                                                                       0', trunc(sysdate), 8460082)
 /
