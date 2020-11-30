a440752e078e53a3a3475bbc8828cdbef92221b0
Marcos Ernani Martini <marcos.martini@mouts.info>
2020-11-30 14:27:28 -0300

    Inclusao ajustes idseqttl PJ

--- 2020/11/PRJ0015005_BACA_Pix_Pos_Libera.sql
+++ 2020/11/PRJ0015005_BACA_Pix_Pos_Libera.sql
@@ -1,6 +1,7 @@
 begin
-
-       update pix.tbpix_tipo_fase
+     
+	 /* Ajuste labels fases */
+     update pix.tbpix_tipo_fase
 	   set nmfase = 'Validação dos créditos do cooperado'
 	 where idfase = 19;
 
@@ -8,10 +9,21 @@ begin
 	   set nmfase = 'Retorno Análise OFSAA (Automático)'
 	 where idfase = 12;
 
+	/* Nova fase */
        insert into pix.tbpix_tipo_fase (idfase,nmfase,idsituacao) values (20, 'Cancelamento Análise Manual', 'A');
 
        insert into pix.tbpix_tipo_fase_transacao(idsequen,idtipo_transacao,idfase,idsequen_anterior,flfase_auditada,qtsegundo_maximo) 
                                          values (49,'P',20,35,'S',null); 
+
+    /* Ajustes chaves PJ */
+	update tbpix_chave_enderecamento ce
+	   set ce.idseqttl = 1
+	 where ce.idseqttl > 1
+	   and exists(select 1
+					from tbpix_crapass a
+				   where a.cdcooper = ce.cdcooper
+					 and a.nrdconta = ce.nrdconta
+					 and a.INPESSOA > 1)
 
 
       commit;
