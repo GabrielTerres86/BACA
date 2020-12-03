192d3bb6fa864e829fac40902dd8120fd2180472
Jefferson Luiz da Silva - Mouts <t0033489@ailos.coop.br>
2020-12-03 09:37:29 -0300

    Update na craptab alterando valor para chamada da CME

---
+++ 2020/12/INC0069967_VLCTRMVESP.sql
@@ -0,0 +1,8 @@
+UPDATE cecred.craptab
+   SET dstextab = '10000,01'
+ WHERE craptab.nmsistem = 'CRED'
+   AND craptab.tptabela = 'GENERI'
+   AND craptab.cdempres = 0
+   AND craptab.cdacesso = 'VLCTRMVESP'
+   AND craptab.tpregist = 0;
+COMMIT;
