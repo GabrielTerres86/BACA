1e38eeb3abf12b4aa38fa6a401603e1506d1e4ac
Daniel Dallagnese - Mout'S <daniel.dallagnese@mouts.info>
2020-12-04 16:22:09 -0300

    PRB0043867
    Substituir '03/12/2020' por to_date, a pedido do comitê, pra evitar problema de formato de data.

--- 2020/12/PRB0043867_AjustarSequencial_20201204.sql
+++ 2020/12/PRB0043867_AjustarSequencial_20201204.sql
@@ -1,23 +1,23 @@
 -- PRB0043867
 -- Ajustar o sequencial dos convênios que tiveram arquivos B gerados manualmente.
 insert into gncontr (cdcooper, tpdcontr, cdconven, dtmvtolt, nrsequen, cdsitret, nmarquiv, qtdoctos, vldoctos)
-values (6, 7, 8460004, '03/12/2020', 1, 2, '3232-EF00000100000000846000420201203.CNV', 1, 0);
+values (6, 7, 8460004, to_date('03122020','ddmmyyyy'), 1, 2, '3232-EF00000100000000846000420201203.CNV', 1, 0);
 insert into gncontr (cdcooper, tpdcontr, cdconven, dtmvtolt, nrsequen, cdsitret, nmarquiv, qtdoctos, vldoctos)
-values (1, 7, 8460004, '03/12/2020', 9, 2, '3239-EF00000900000000846000420201203.CNV', 37, 0);
+values (1, 7, 8460004, to_date('03122020','ddmmyyyy'), 9, 2, '3239-EF00000900000000846000420201203.CNV', 37, 0);
 insert into gncontr (cdcooper, tpdcontr, cdconven, dtmvtolt, nrsequen, cdsitret, nmarquiv, qtdoctos, vldoctos)
-values (1, 7, 8460464, '03/12/2020', 25, 2, '3239-EF00002500000000846046420201203.CNV', 22, 0);
+values (1, 7, 8460464, to_date('03122020','ddmmyyyy'), 25, 2, '3239-EF00002500000000846046420201203.CNV', 22, 0);
 insert into gncontr (cdcooper, tpdcontr, cdconven, dtmvtolt, nrsequen, cdsitret, nmarquiv, qtdoctos, vldoctos)
-values (2, 7, 8460004, '03/12/2020', 2, 2, '3265-EF00000200000000846000420201203.CNV', 1, 0);
+values (2, 7, 8460004, to_date('03122020','ddmmyyyy'), 2, 2, '3265-EF00000200000000846000420201203.CNV', 1, 0);
 insert into gncontr (cdcooper, tpdcontr, cdconven, dtmvtolt, nrsequen, cdsitret, nmarquiv, qtdoctos, vldoctos)
-values (5, 7, 8460464, '03/12/2020', 2, 2, '3318-EF00000200000000846046420201203.CNV', 1, 0);
+values (5, 7, 8460464, to_date('03122020','ddmmyyyy'), 2, 2, '3318-EF00000200000000846046420201203.CNV', 1, 0);
 insert into gncontr (cdcooper, tpdcontr, cdconven, dtmvtolt, nrsequen, cdsitret, nmarquiv, qtdoctos, vldoctos)
-values (7, 7, 8460004, '03/12/2020', 2, 2, '4416-EF00000200000000846000420201203.CNV', 1, 0);
+values (7, 7, 8460004, to_date('03122020','ddmmyyyy'), 2, 2, '4416-EF00000200000000846000420201203.CNV', 1, 0);
 insert into gncontr (cdcooper, tpdcontr, cdconven, dtmvtolt, nrsequen, cdsitret, nmarquiv, qtdoctos, vldoctos)
-values (11, 7, 8460004, '03/12/2020', 3, 2, '4438-EF00000300000000846000420201203.CNV', 3, 0);
+values (11, 7, 8460004, to_date('03122020','ddmmyyyy'), 3, 2, '4438-EF00000300000000846000420201203.CNV', 3, 0);
 insert into gncontr (cdcooper, tpdcontr, cdconven, dtmvtolt, nrsequen, cdsitret, nmarquiv, qtdoctos, vldoctos)
-values (11, 7, 8460464, '03/12/2020', 5, 2, '4438-EF00000500000000846046420201203.CNV', 3, 0);
+values (11, 7, 8460464, to_date('03122020','ddmmyyyy'), 5, 2, '4438-EF00000500000000846046420201203.CNV', 3, 0);
 insert into gncontr (cdcooper, tpdcontr, cdconven, dtmvtolt, nrsequen, cdsitret, nmarquiv, qtdoctos, vldoctos)
-values (13, 7, 8460004, '03/12/2020', 2, 2, '4457-EF00000200000000846000420201203.CNV', 1, 0);
+values (13, 7, 8460004, to_date('03122020','ddmmyyyy'), 2, 2, '4457-EF00000200000000846000420201203.CNV', 1, 0);
 update crapcon c
    set c.nrseqatu = c.nrseqatu + 1
  where (c.cdempcon, c.cdsegmto, c.cdcooper) in ((4, 4, 6),
@@ -31,16 +31,16 @@ update crapcon c
                                                 (4, 4, 13));
 -- Ajustar o sequencial dos convênios que tiveram arquivos F gerados manualmente.
 insert into gncontr (cdcooper, tpdcontr, cdconven, dtmvtolt, nrsequen, cdsitret, nmarquiv, qtdoctos, vldoctos)
-values (1, 7, 8460004, '03/12/2020', 10, 2, '3239-EF00001000000000846000420201203.CNV', 8, 0);
+values (1, 7, 8460004, to_date('03122020','ddmmyyyy'), 10, 2, '3239-EF00001000000000846000420201203.CNV', 8, 0);
 insert into gncontr (cdcooper, tpdcontr, cdconven, dtmvtolt, nrsequen, cdsitret, nmarquiv, qtdoctos, vldoctos)
-values (1, 7, 8460464, '03/12/2020', 26, 2, '3239-EF00002600000000846046420201203.CNV', 3, 0);
+values (1, 7, 8460464, to_date('03122020','ddmmyyyy'), 26, 2, '3239-EF00002600000000846046420201203.CNV', 3, 0);
 update crapcon c
    set c.nrseqatu = c.nrseqatu + 1
  where (c.cdempcon, c.cdsegmto, c.cdcooper) in ((4, 4, 1),
                                                 (464, 4, 1));
 -- Encerrar as autorizações
 update crapatr a
-   set a.dtfimatr = '03/12/2020'
+   set a.dtfimatr = to_date('03122020','ddmmyyyy')
  where a.cdhistor = 3292
    and a.dtfimatr is null
    and a.cdempres in (8460004, 8460464);
