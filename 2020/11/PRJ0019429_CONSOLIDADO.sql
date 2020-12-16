cc028a134e040801bb542647293a8a8414954123
Guilherme Cervo <guic91@gmail.com>
2020-12-16 16:16:57 -0300

    Renomeacao de arquivo e inclusao de alteracao de data do termo adesao cartao now

--- 2020/11/PRJ0019429_CONSOLIDADO.sql
+++ 2020/11/PRJ0019429_CONSOLIDADO.sql
@@ -14,4 +14,12 @@ BEGIN
     WHEN OTHERS THEN
       DBMS_OUTPUT.PUT_LINE('Erro = '||SQLERRM);
   END;
+  
+  BEGIN
+    UPDATE tbgen_versao_termo t
+       SET t.dtinicio_vigencia = to_date('26/01/2021', 'DD/MM/RRRR')
+     WHERE t.dschave_versao = 'TERMO ADESAO CDC PF V2';
+     
+     COMMIT;
+  END;  
 END;
