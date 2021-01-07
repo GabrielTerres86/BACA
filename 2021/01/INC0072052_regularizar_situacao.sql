c66d3ec63000d1a6dcb2886f9c55362bee4940b3
Adriano Marchi <adriano@ailos.coop.br>
2021-01-06 21:16:10 -0300

    inc0072052: regularizar situação de pagamento pix.

---
+++ 2021/01/INC0072052_regularizar_situacao.sql
@@ -0,0 +1,11 @@
+--executar no banco JDSPIAPIP - SPI-API
+insert into "TbJdPiSpiApi_OpDebitoSt"
+values
+  ('D694E1DB4F99F4488C4D1E18B9CE5844',
+   '302E26F3C080A24EB3CA7CE95E368B3F',
+   '3',
+   '16/12/20 01:35:28,2706770',
+   'AB03',
+   'Pagamento expirado por timeout');
+
+commit;
