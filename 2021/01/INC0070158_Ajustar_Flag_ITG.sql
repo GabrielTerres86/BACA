9c1a2a0b8a9668f67fec166dcdfcb4c8a9088e95
Renato Darosci <renato.darosci@supero.com.br>
2021-01-13 14:17:37 -0300

    Alteramos o script para inativar as solicitações de ITG, pois devido ao tempo que elas estão paradas, as solicitações devem ser reavaliadas.

--- 2021/01/INC0070158_Ajustar_Flag_ITG.sql
+++ 2021/01/INC0070158_Ajustar_Flag_ITG.sql
@@ -2,7 +2,7 @@ BEGIN
   
   -- Remover o flag de enviada de todas as contas que não tem código de ITG gerado
   UPDATE crapass t
-     SET t.flgctitg = 0 
+     SET t.flgctitg = 3 -- Inativar 
        , t.nrdctitg = ' ' 
    WHERE t.flgctitg = 1
      AND TRIM(t.nrdctitg) IS NULL ;
