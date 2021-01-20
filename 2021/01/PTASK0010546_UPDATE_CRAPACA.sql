39d280c07d6558129731369881326cfec7a849a8
Andrey <t0032436@ailos.coop.br>
2021-01-20 16:24:42 -0300

    Novo Parametro

--- 2021/01/PTASK0010546_UPDATE_CRAPACA.sql
+++ 2021/01/PTASK0010546_UPDATE_CRAPACA.sql
@@ -1,6 +1,6 @@
 BEGIN
 	UPDATE crapaca c
-	   SET c.lstparam = 'pr_dsdlista,pr_cdcooper,pr_nrdconta,pr_nraplica,pr_flgcritic,pr_datade,pr_datate,pr_nmarquiv,pr_dscodib3'
+	   SET c.lstparam = 'pr_dsdlista,pr_cdcooper,pr_nrdconta,pr_nraplica,pr_flgcritic,pr_datade,pr_datate,pr_nmarquiv,pr_dscodib3,pr_qtdemtel'
 	 WHERE c.nmdeacao = 'CUSAPL_SOLIC_REENVIO'
 	   AND c.nmpackag = 'TELA_CUSAPL';
 	COMMIT;
