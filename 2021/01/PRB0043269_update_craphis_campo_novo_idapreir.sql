23a2ad9c453f7a54e97f3746fc67dc9cd38ea04c
Felipe Renato Mendes <felipe.mendes@mout.info>
2021-01-27 13:55:12 -0300

    att

---
+++ 2021/01/PRB0043269_update_craphis_campo_novo_idapreir.sql
@@ -0,0 +1,9 @@
+BEGIN
+  UPDATE CECRED.CRAPHIS SET IDAPREIR = 1 WHERE CDHISTOR IN (108, 2332, 2333, 275, 92, 384, 1706, 317, 1539, 2336, 
+                                                            2337, 394, 1715, 2370, 2372, 2374, 2376, 2386, 2387);
+  COMMIT;
+  
+  EXCEPTION 
+    WHEN OTHERS THEN
+      ROLLBACK;                                               
+END;
