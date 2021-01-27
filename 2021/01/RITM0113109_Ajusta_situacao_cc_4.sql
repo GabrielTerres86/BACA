51bc6575df39803b6dd1fa10345f4d73c7fd7cdf
Alexandre Santos <alexandre.santos@supero.com.br>
2021-01-27 14:06:36 -0300

    * Atualização do script de encerramento de contas conforme solicitação de mudança.

--- 2021/01/RITM0113109_Ajusta_situacao_cc_4.sql
+++ 2021/01/RITM0113109_Ajusta_situacao_cc_4.sql
@@ -24,9 +24,22 @@ DECLARE
   --
   rg_crapass cr_crapass%rowtype;
   --
+  cursor cr_crapcot (p_nrdconta   NUMBER
+                     , p_cdcooper number) is
+    select vldcotas
+    from crapcot
+    where cdcooper = p_cdcooper
+      and nrdconta = p_nrdconta;
+  --
   vr_dttransa   DATE;
   vr_hrtransa   VARCHAR2(5);
-  vr_log_script VARCHAR2(1000);
+  vr_log_script VARCHAR2(8000);
+  vr_dstransa   varchar2(1000);
+  vr_vldcotas   NUMBER(25,2);
+  vr_nrdocmto   INTEGER;
+  vr_nrseqdig   INTEGER;
+  vr_dtmvtolt   DATE;
+  vr_capdev_ant NUMBER(15,2);
   --
 BEGIN
   --
@@ -41,10 +54,80 @@ BEGIN
                      || rg_crapass.nrdconta || ') da situação (' || rg_crapass.cdsitdct
                      || ') para (4)';
     --
+    vr_dstransa := 'Alterada situacao de conta por script. RITM0113109.';
+    --
+    -- Verificar o valor de cotas a devolver ao cooperado.
+    vr_vldcotas := 0;
+    --
+    open cr_crapcot(rg_crapass.nrdconta
+                    , rg_crapass.cdcooper);
+    fetch cr_crapcot into vr_vldcotas;
+    close cr_crapcot;
+    --
+    SELECT dtmvtolt
+      INTO vr_dtmvtolt
+    FROM crapdat 
+    WHERE cdcooper = rg_crapass.cdcooper;
+    --
+    if nvl(vr_vldcotas, 0) > 0 then 
+      --
+      select VLCAPITAL
+        into vr_capdev_ant
+      from TBCOTAS_DEVOLUCAO
+      where TPDEVOLUCAO = 3
+        and cdcooper = rg_crapass.cdcooper
+        and nrdconta = rg_crapass.nrdconta;
+      --
+      -- Adiciona valor de cota capital para devolução.
+      UPDATE TBCOTAS_DEVOLUCAO
+        SET VLCAPITAL = VLCAPITAL + vr_vldcotas
+      WHERE CDCOOPER = rg_crapass.cdcooper
+        AND NRDCONTA = rg_crapass.nrdconta
+        AND TPDEVOLUCAO = 3;
+      --
+      -- Remove valor de cota capital
+      UPDATE crapcot
+        SET vldcotas = vldcotas - vr_vldcotas
+      WHERE cdcooper = rg_crapass.cdcooper
+        AND nrdconta = rg_crapass.nrdconta; 
+      --
+      vr_nrdocmto := fn_sequence('CRAPLAU','NRDOCMTO', rg_crapass.cdcooper || ';' || TRIM(to_char( vr_dtmvtolt,'DD/MM/YYYY')) ||';16;100;600040');
+      vr_nrseqdig := fn_sequence('CRAPLOT','NRSEQDIG', rg_crapass.cdcooper || ';' ||to_char(vr_dtmvtolt,'DD/MM/YYYY') || ';16;100;600040');
+      --
+      INSERT INTO craplct(cdcooper
+        ,cdagenci
+        ,cdbccxlt
+        ,nrdolote
+        ,dtmvtolt
+        ,cdhistor
+        ,nrctrpla
+        ,nrdconta
+        ,nrdocmto
+        ,nrseqdig
+        ,vllanmto)
+      VALUES (rg_crapass.cdcooper
+        ,16
+        ,100
+        ,600040
+        ,vr_dtmvtolt
+        ,2080
+        ,0
+        ,rg_crapass.nrdconta 
+        ,vr_nrdocmto
+        ,vr_nrseqdig
+        ,vr_vldcotas); 
+      --
+      vr_dstransa := vr_dstransa || ' Movimentado capital de ' || to_char(vr_vldcotas, '9G999D99') || ' para Valores a Devolver.';
+      vr_log_script := vr_log_script || ' | ' || vr_dstransa;
+      --
+    end if;
+    
+    --
     -- Atualiza crapass
-    UPDATE CRAPASS
+    UPDATE cecred.CRAPASS
       -- Em processo de demissão BACEN
       SET cdsitdct = 4
+        , dtdemiss = vr_dtmvtolt
     WHERE nrdconta = rg_crapass.nrdconta
       AND cdcooper = rg_crapass.cdcooper;
     --
@@ -52,7 +135,7 @@ BEGIN
     vr_dttransa := trunc(sysdate);
     vr_hrtransa := to_char(sysdate,'SSSSS');
 
-    INSERT INTO craplgm(cdcooper
+    INSERT INTO cecred.craplgm(cdcooper
       ,nrdconta
       ,idseqttl
       ,nrsequen
@@ -72,7 +155,7 @@ BEGIN
       ,1
       ,vr_dttransa
       ,vr_hrtransa
-      ,'Alterada situacao de conta por script. RITM0113109'
+      ,vr_dstransa
       ,'AIMARO'
       ,''
       ,1
@@ -80,8 +163,8 @@ BEGIN
       ,1
       ,' ');
     --
-    -- Insere log com valores de antes x depois.
-    INSERT INTO craplgi(cdcooper
+    -- Insere log com valores de antes x depois da crapass.
+    INSERT INTO cecred.craplgi(cdcooper
       ,nrdconta
       ,idseqttl
       ,nrsequen
@@ -99,9 +182,81 @@ BEGIN
       ,vr_dttransa
       ,vr_hrtransa
       ,1
-      ,'cdsitdct'
+      ,'crapass.cdsitdct'
       ,rg_crapass.cdsitdct
       ,'4');
+    --
+    -- Insere log com valores de antes x depois da crapass.
+    INSERT INTO cecred.craplgi(cdcooper
+      ,nrdconta
+      ,idseqttl
+      ,nrsequen
+      ,dttransa
+      ,hrtransa
+      ,nrseqcmp
+      ,nmdcampo
+      ,dsdadant
+      ,dsdadatu)
+    VALUES
+      (rg_crapass.cdcooper
+      ,rg_crapass.nrdconta
+      ,1
+      ,1
+      ,vr_dttransa
+      ,vr_hrtransa
+      ,2
+      ,'crapass.dtdemiss'
+      ,null
+      ,to_char(vr_dtmvtolt, 'dd/mm/rrrr') );
+    --
+    if nvl(vr_vldcotas, 0) > 0 then 
+      --
+      -- Insere log com valores de antes x depois da TBCOTAS_DEVOLUCAO.
+      INSERT INTO cecred.craplgi(cdcooper
+        ,nrdconta
+        ,idseqttl
+        ,nrsequen
+        ,dttransa
+        ,hrtransa
+        ,nrseqcmp
+        ,nmdcampo
+        ,dsdadant
+        ,dsdadatu)
+      VALUES
+        (rg_crapass.cdcooper
+        ,rg_crapass.nrdconta
+        ,1
+        ,1
+        ,vr_dttransa
+        ,vr_hrtransa
+        ,3
+        ,'tbcotas_devolucao.VLCAPITAL'
+        ,vr_capdev_ant
+        ,(vr_capdev_ant + vr_vldcotas) );
+      --
+      -- Insere log com valores de antes x depois da CRAPCOT.
+      INSERT INTO cecred.craplgi(cdcooper
+        ,nrdconta
+        ,idseqttl
+        ,nrsequen
+        ,dttransa
+        ,hrtransa
+        ,nrseqcmp
+        ,nmdcampo
+        ,dsdadant
+        ,dsdadatu)
+      VALUES
+        (rg_crapass.cdcooper
+        ,rg_crapass.nrdconta
+        ,1
+        ,1
+        ,vr_dttransa
+        ,vr_hrtransa
+        ,4
+        ,'crapcot.vldcotas'
+        ,vr_vldcotas
+        ,0 );
+    end if;
     --
   END LOOP;
   CLOSE cr_crapass;
@@ -120,9 +275,3 @@ EXCEPTION
     RAISE_APPLICATION_ERROR(-20000, 'Erro ao executar script: ' || SQLERRM);
     --
 END;
-
-
-
-
-
-
