f81d6748648b93158069400fffdb1b726075360f
Felipe Renato Mendes <felipe.mendes@mout.info>
2021-01-28 11:16:11 -0300

    Att

--- 2021/01/INC0063110_INC0066566_ajuste_vlsdeved.sql
+++ 2021/01/INC0063110_INC0066566_ajuste_vlsdeved.sql
@@ -1,13 +1,13 @@
 BEGIN
 
   UPDATE crapepr epr
-     SET epr.vlsdeved = 17548.48
+     SET epr.vlsdeved = 16761.43
    WHERE epr.cdcooper = 12
      AND epr.nrdconta = 40703
      AND epr.nrctremp = 21704;
 
   UPDATE crapepr epr
-     SET epr.vlsdeved = 277975.26
+     SET epr.vlsdeved = 277331.98
    WHERE epr.cdcooper = 1
      AND epr.nrdconta = 8668957
      AND epr.nrctremp = 1916169;
