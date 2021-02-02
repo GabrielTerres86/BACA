bf53cdd34f3bd4a0602ab08c3040bc051c2c3da8
Renan Fiedler de Oliveira <t0032898@ailos.coop.br>
2021-02-02 09:49:01 -0300

    [CALRIS] Insert do monitoramento da CALRIS

---
+++ 2021/01/CALRIS_insert_monitoramento.sql
@@ -0,0 +1,23 @@
+BEGIN
+  INSERT INTO crapprm
+    (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES
+    ('CRED',0,'MONITORA_CALRIS','Flag ativo/inativo para enviar email de monitoramentos do calculo de risco.','0');
+    
+  FOR cr_crapcop IN (SELECT cdcooper
+                           ,flgativo
+                       FROM crapcop) LOOP
+    INSERT INTO crapprm
+      (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+    VALUES
+      ('CRED',cr_crapcop.cdcooper,'MONITORA_CALRIS','Flag ativo/inativo para enviar email de monitoramentos do calculo de risco.',cr_crapcop.flgativo);
+  END LOOP;
+  
+  INSERT INTO crapprm
+    (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES
+    ('CRED',0,'MONITORA_EMAIL_CALRIS','Email de destino dos monitoramentos.','renan.oliveira@supero.com.br');
+    
+  COMMIT;
+END;
+/
