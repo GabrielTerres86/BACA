836ea1b26e25ac85730f8a9be7f90b468213d22c
Adriano Luis da Silva <t0033065@cecred.coop.br>
2021-02-04 09:59:17 -0300

    STRY0010926 - Analisar proposta - Parâmetro com webservice da FIS

---
+++ 2021/02/STRY0010926_test_insert_parametros_webservice_soafis.sql
@@ -0,0 +1,38 @@
+delete crapprm
+where CDACESSO = 'SOAFIS_ENDERECO_CALCULO'
+/
+delete crapprm
+where CDACESSO = 'AUTHORIZATION_SOAFIS'
+/
+
+INSERT INTO crapprm ( NMSISTEM
+                     ,CDCOOPER
+                     ,CDACESSO
+                     ,DSTEXPRM
+                     ,DSVLRPRM
+                     ,PROGRESS_RECID
+                    )
+             values ( 'CRED'
+                     ,0
+                     ,'SOAFIS_ENDERECO_CALCULO'
+                     ,'Endereço de serviço do webservice do FIS via SOA'
+                     ,'https://servicosinternostest.ailos.coop.br/osb-soa/TransacaoCreditoRestService/v1/CalcularCreditoConsignado'
+                     ,NULL
+                    );
+
+INSERT INTO crapprm ( NMSISTEM
+                     ,CDCOOPER
+                     ,CDACESSO
+                     ,DSTEXPRM
+                     ,DSVLRPRM
+                     ,PROGRESS_RECID
+                    )
+             values ( 'CRED'
+                     ,0
+                     ,'AUTHORIZATION_SOAFIS'
+                     ,'Authorization para cálculo do empréstimo no FIS via SOA'
+                     ,'Basic aWJzdnJjb3JlOjMrRlJFRStzdGF5'
+                     ,NULL
+                    );
+
+commit;
