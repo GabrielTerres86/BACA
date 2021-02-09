c9e5a9ae73de0eeaffaf946aa6efee544705c0c3
Jefferson Luiz da Silva - Mouts <t0033489@ailos.coop.br>
2021-02-09 09:21:31 -0300

    Update alterando o valor de chamanda das telas de controle de movimentação em espécie

---
+++ 2021/02/prb0043934.sql
@@ -0,0 +1,8 @@
+UPDATE cecred.craptab
+   SET dstextab = '2000,01'
+ WHERE craptab.nmsistem = 'CRED'
+   AND craptab.tptabela = 'GENERI'
+   AND craptab.cdempres = 0
+   AND craptab.cdacesso = 'VLCTRMVESP'
+   AND craptab.tpregist = 0;
+COMMIT;
