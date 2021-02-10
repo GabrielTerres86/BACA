17ceb39a8810c70de6a7e42d94a69bfff0cef9b1
Alexandre Diego Girardi <t0033227@cecred.coop.br>
2021-02-10 09:18:25 -0300

    Realizar o ajuste na tabela crapaut

---
+++ 2021/02/INC0073662_ajusta_crapaut.sql
@@ -0,0 +1,29 @@
+INSERT INTO crapaut(cdagenci, nrdcaixa, dtmvtolt, nrsequen, cdopecxa, hrautent, vldocmto, nrdocmto, tpoperac, cdstatus, estorno, nrseqaut 
+                   , cdhistor, dslitera, blidenti, bltotpag, bltotrec, blsldini, blvalrec, inusodbl, nrdconta, nrdctadp, dsleitur, dsobserv
+                   , cdcooper, dsprotoc) 
+     VALUES (1, 4, to_date('07-01-2021', 'dd-mm-yyyy'), 5, 'f0010292', 31339, 1234.08, 31338.00, 1, '1', 0, 0, 135, 'VIACREDI - COOPERATIVA DE CREDITO DO VALE DO ITA                                                07/01/21 08:42:19 PA  001  CAIXA:  04/f0010292                                                        ** COMPROVANTE   31.338 **                                                                CONTA...: 58.2   PA:  1                                MOACIR KRAMBECK                                 MARY KRAMBECK                                                                               TIPO DE DEBITO                   VALOR EM R$ ------------------------------------------------    DB.AUTORIZADO                      1.234,08                                                 FGTS E social                                                                                   _______________________________________________ ASSINATURA                                                                                      085010100104 00004 070121             *1.234,08P                                                                                                                                                                                                                                                                                                                                                                                                ', '58.2 - MOACIR KRAMBECK 08:33:56', 1234.08
+             , 1234.08, 0.00, 0.00, 0, 0, 0, ' ', ' ', 1, ' ');
+commit;
+INSERT INTO crapaut(cdagenci, nrdcaixa, dtmvtolt, nrsequen, cdopecxa, hrautent, vldocmto, nrdocmto, tpoperac, cdstatus, estorno, nrseqaut 
+                   , cdhistor, dslitera, blidenti, bltotpag, bltotrec, blsldini, blvalrec, inusodbl, nrdconta, nrdctadp, dsleitur, dsobserv
+                   , cdcooper, dsprotoc) 
+     VALUES (1, 4, to_date('07-01-2021', 'dd-mm-yyyy'), 6, 'f0010292', 31339, 1234.08, 31338.00, 1, '1', 0, 0, 135, 'VIACREDI - COOPERATIVA DE CREDITO DO VALE DO ITA                                                07/01/21 08:42:19 PA  001  CAIXA:  04/f0010292                                                        ** COMPROVANTE   31.338 **                                                                CONTA...: 58.2   PA:  1                                MOACIR KRAMBECK                                 MARY KRAMBECK                                                                               TIPO DE DEBITO                   VALOR EM R$ ------------------------------------------------    DB.AUTORIZADO                      1.234,08                                                 FGTS E social                                                                                   _______________________________________________ ASSINATURA                                                                                      085010100104 00004 070121             *1.234,08P                                                                                                                                                                                                                                                                                                                                                                                                ', '58.2 - MOACIR KRAMBECK 08:33:56', 1234.08
+             , 1234.08, 0.00, 0.00, 0, 0, 0, ' ', ' ', 1, ' ');
+commit;
+
+update crapaut
+   set nrseqaut = 5
+ WHERE cdcooper = 1
+   AND cdagenci = 1
+   AND nrdcaixa = 4
+   AND dtmvtolt = to_date('07/01/2021', 'dd/mm/yyyy')
+   AND nrsequen = 43;
+commit;
+ update crapaut
+      set nrseqaut = 6
+ WHERE cdcooper = 1
+   AND cdagenci = 1
+   AND nrdcaixa = 4
+   AND dtmvtolt = to_date('07/01/2021', 'dd/mm/yyyy')
+   AND nrsequen = 44;
+commit;
