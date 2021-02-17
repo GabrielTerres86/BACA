e9b9783c786c01d925d07e7f09a3235d802c7a6c
Tiago Marcel Knipers <tiago.knipers@cecred.coop.br>
2021-02-17 09:50:03 -0300

    Script para adicionar novos campos do pix cobrança na tabela de fluxo financeiro crapffm e novos código de remessa para o pix cobrança na tabela tbfin_remessa_fluxo_caixa

---
+++ 2021/02/PRJ0022700-campos-fluxo-financeiro-e-codigos-remessa-pix-cobranca.sql
@@ -0,0 +1,13 @@
+ALTER TABLE crapffm ADD vltotpixcob NUMERIC(25,2) DEFAULT 0 NOT NULL;
+COMMENT ON COLUMN crapffm.vltotpixcob IS 'Valor total das movimentacoes do PIX Cobrança';
+
+ALTER TABLE crapffm ADD vldevpixcob NUMERIC(25,2) DEFAULT 0 NOT NULL;
+COMMENT ON COLUMN crapffm.vldevpixcob IS 'Valor total das movimentacoes de devolucoes do PIX Cobrança';
+
+INSERT INTO tbfin_remessa_fluxo_caixa (cdremessa, nmremessa, cdoperador, dtalteracao, tpfluxo_entrada, tpfluxo_saida, flremessa_dinamica)
+VALUES(20, 'PIX COB', 1, SYSDATE, 1, 2, 1);
+
+INSERT INTO tbfin_remessa_fluxo_caixa (cdremessa, nmremessa, cdoperador, dtalteracao, tpfluxo_entrada, tpfluxo_saida, flremessa_dinamica)
+VALUES(21, 'DEV PIX COB', 1, SYSDATE, 1, 2, 1);
+
+COMMIT;
