a43778e7ce19414e7ed0efefd4f545c23b69b13f
Ramon Martins <ramon.silva@ailos.coop.br>
2021-02-18 09:58:30 -0300

    hotfix/INC0076807/master

--- 2021/02/INC0076807.sql
+++ 2021/02/INC0076807.sql
@@ -8,6 +8,10 @@ BEGIN
 	where cdcooper = 11
 	and nrdconta = 722324
 	AND INSIT_ATUALIZA = 3;
+	
+	update tbgen_inconsist_email_grp
+	set dsendereco_email = 'luis.feltrin@ailos.coop.br; ramon.silva@ailos.coop.br'
+	where idinconsist_grp = 3	
   
   COMMIT;
 END;  
