b94d30809cb44bc51e79bc9f9382f2b18469fb59
Alcemir Junior <alcemir.junior@mouts.info>
2021-02-18 14:10:31 -0300

    script para descontinuar crps524
    script para descontinuar crps524

---
+++ 2021/02/RITM0118237_descontinuar_crps524.sql
@@ -0,0 +1,8 @@
+﻿begin
+  update crapprg  p set p.inlibprg = 2 /* 2 - bloqueado */
+  /* Programas a serem descontinuados*/                                         
+  where upper(p.cdprogra) = 'CRPS524';
+                             
+  commit;
+  
+end;
