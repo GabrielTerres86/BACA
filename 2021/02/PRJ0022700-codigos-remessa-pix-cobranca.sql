79dc9985bbbe4e43b1838ddfe4d26514b1351b36
Tiago Marcel Knipers <tiago.knipers@cecred.coop.br>
2021-02-18 17:42:42 -0300

    removido do script a criação dos novos campos

--- 2021/02/PRJ0022700-codigos-remessa-pix-cobranca.sql
+++ 2021/02/PRJ0022700-codigos-remessa-pix-cobranca.sql
@@ -1,8 +1,4 @@
-ALTER TABLE crapffm ADD vltotpixcob NUMERIC(25,2) DEFAULT 0 NOT NULL;
-COMMENT ON COLUMN crapffm.vltotpixcob IS 'Valor total das movimentacoes do PIX Cobrança';
-
-ALTER TABLE crapffm ADD vldevpixcob NUMERIC(25,2) DEFAULT 0 NOT NULL;
-COMMENT ON COLUMN crapffm.vldevpixcob IS 'Valor total das movimentacoes de devolucoes do PIX Cobrança';
+BEGIN
 
 INSERT INTO tbfin_remessa_fluxo_caixa (cdremessa, nmremessa, cdoperador, dtalteracao, tpfluxo_entrada, tpfluxo_saida, flremessa_dinamica)
 VALUES(25, 'PIX COB', 1, SYSDATE, 1, 2, 1);
@@ -11,3 +7,9 @@ INSERT INTO tbfin_remessa_fluxo_caixa (cdremessa, nmremessa, cdoperador, dtalter
 VALUES(26, 'DEV PIX COB', 1, SYSDATE, 1, 2, 1);
 
 COMMIT;
+
+EXCEPTION 
+  WHEN OTHERS THEN
+    sistema.excecaointerna(pr_compleme => 'PRJ0022700: Não foi possível inserir códigos de remessa pix cobrança');
+
+END;
