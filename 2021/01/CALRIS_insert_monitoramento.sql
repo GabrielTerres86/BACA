051ef36eefde625110eb53b00ca54fb895959bb3
Renan Fiedler de Oliveira <t0032898@ailos.coop.br>
2021-02-18 10:24:44 -0300

    [CALRIS] Incluindo e-mail de monitoramento

--- 2021/01/CALRIS_insert_monitoramento.sql
+++ 2021/01/CALRIS_insert_monitoramento.sql
@@ -16,7 +16,7 @@ BEGIN
   INSERT INTO crapprm
     (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
   VALUES
-    ('CRED',0,'MONITORA_EMAIL_CALRIS','Email de destino dos monitoramentos.','renan.oliveira@supero.com.br');
+    ('CRED',0,'MONITORA_EMAIL_CALRIS','Email de destino dos monitoramentos.','joana.aguida@ailos.coop.br;thiago.rodrigues@ailos.coop.br;renan.oliveira@supero.com.br');
     
   COMMIT;
 END;
