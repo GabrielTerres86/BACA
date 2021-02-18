8c1d8ca396f165a32080fc784ce1837d62f1dedf
Adriano Erik Weiguert Nagasava <t0031546@cecred.coop.br>
2021-02-18 08:38:33 -0300

    Script para inserir os dominios

---
+++ 2021/02/baca_insere_tbgen_dominio_campo.sql
@@ -0,0 +1,204 @@
+BEGIN
+	-- tpProdutoEmprestimo       CONSTANT NUMBER := 0;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPPRODUTO_RECIPROCIDADE'
+																 ,'0'
+																 ,'Empréstimo'
+																 );
+	-- tpCategCotas              CONSTANT NUMBER := 0;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'0'
+																 ,'Cotas'
+																 );
+
+	-- tpCategInvestimentos      CONSTANT NUMBER := 1;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'1'
+																 ,'Investimentos'
+																 );
+
+	-- tpCategLimiteCredito      CONSTANT NUMBER := 2;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'2'
+																 ,'Limite Crédito'
+																 );
+
+	-- tpCategLimiteEmpresarial  CONSTANT NUMBER := 3;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'3'
+																 ,'Limite Empresarial'
+																 );
+
+	-- tpCategDomicilioBancario  CONSTANT NUMBER := 4;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'4'
+																 ,'Domicílio Bancário'
+																 );
+
+	-- tpCategConvenioFolhaPag   CONSTANT NUMBER := 5;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'5'
+																 ,'Convênio Folha Pagamento'
+																 );
+
+	-- tpCategConvenioConsigado  CONSTANT NUMBER := 6;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'6'
+																 ,'Convênio Consignado'
+																 );
+
+	-- tpCategConvenioCDC        CONSTANT NUMBER := 7;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'7'
+																 ,'Convênio CDC'
+																 );
+
+	-- tpCategConsorcio          CONSTANT NUMBER := 8;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'8'
+																 ,'Consórcio'
+																 );
+
+	-- tpCategSeguroEmpresarial  CONSTANT NUMBER := 9;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'9'
+																 ,'Seguro Empresarial'
+																 );
+
+	-- tpCategSeguroAuto         CONSTANT NUMBER := 10;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'10'
+																 ,'Seguro Auto'
+																 );
+
+	-- tpCategSeguroVida         CONSTANT NUMBER := 11;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'11'
+																 ,'Seguro Vida'
+																 );
+
+	-- tpCategSeguroResidencia   CONSTANT NUMBER := 12;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'12'
+																 ,'Seguro Residência'
+																 );
+
+	-- tpCategCartaoCred         CONSTANT NUMBER := 13;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'13'
+																 ,'Cartão Crédito'
+																 );
+
+	-- tpCategPix                CONSTANT NUMBER := 14;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'14'
+																 ,'Pix'
+																 );
+
+	-- tpCategServico            CONSTANT NUMBER := 15;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'15'
+																 ,'Serviço'
+																 );
+
+	-- tpCategAutoatendimento    CONSTANT NUMBER := 16;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'16'
+																 ,'Autoatendimento'
+																 );
+
+	-- tpCategPrevidencia        CONSTANT NUMBER := 17;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'17'
+																 ,'Previdência'
+																 );
+
+	-- tpCategRecebSalario       CONSTANT NUMBER := 18;
+	INSERT INTO tbgen_dominio_campo(nmdominio
+																 ,cddominio
+																 ,dscodigo
+																 )
+													 VALUES('TPCATEG_RECIPROCIDADE'
+																 ,'18'
+																 ,'Recebimento Salário'
+																 );
+  COMMIT;
+EXCEPTION
+	WHEN OTHERS THEN
+		ROLLBACK;
+END;  
