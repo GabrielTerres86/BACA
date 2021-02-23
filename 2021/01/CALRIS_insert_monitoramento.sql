3aba85ea6bda7aff2759dda4bfee72b5afee1d74
Renan Fiedler de Oliveira <t0032898@ailos.coop.br>
2021-02-23 15:35:59 -0300

    [CALRIS] Removendo flag de monitoramento de e-mail

--- 2021/01/CALRIS_insert_monitoramento.sql
+++ 2021/01/CALRIS_insert_monitoramento.sql
@@ -1,18 +1,4 @@
-BEGIN
-  INSERT INTO crapprm
-    (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
-  VALUES
-    ('CRED',0,'MONITORA_CALRIS','Flag ativo/inativo para enviar email de monitoramentos do calculo de risco.','0');
-    
-  FOR cr_crapcop IN (SELECT cdcooper
-                           ,flgativo
-                       FROM crapcop) LOOP
-    INSERT INTO crapprm
-      (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
-    VALUES
-      ('CRED',cr_crapcop.cdcooper,'MONITORA_CALRIS','Flag ativo/inativo para enviar email de monitoramentos do calculo de risco.',cr_crapcop.flgativo);
-  END LOOP;
-  
+BEGIN  
   INSERT INTO crapprm
     (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
   VALUES
