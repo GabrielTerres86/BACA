413313347559bbfe0c19f185e31f87e1a7bf59ba
Alexandre Pietrobelli <alexandre.pietrobelli@supero.com.br>
2021-02-25 08:03:53 -0300

    Projeto repique Consórcio
    Projeto repique consórcio PRJ0022855

---
+++ 2021/02/insert_CRAPPRM.sql
@@ -0,0 +1,52 @@
+
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',1,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+  /
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',10,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',11,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',12,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',13,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',14,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',15,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',16,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',17,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',2,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',3,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',4,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',5,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',6,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',7,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',8,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  VALUES('CRED',9,'QTD_DIAS_REPIQUE_CNS', 'Quantidade de dias de repique por cooperativa', '10');  
+/
