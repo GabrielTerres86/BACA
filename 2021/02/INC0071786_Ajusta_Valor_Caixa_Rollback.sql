36346a00216206ce0499522702c0947d15007e85
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2021-03-02 10:06:03 -0300

    Remover o script de Rollback
    Remover o script de Rollback
    Ele é anexado na RDM

--- 2021/02/INC0071786_Ajusta_Valor_Caixa_Rollback.sql
+++
@@ -1,7 +0,0 @@
-update crapbcx a
-   set a.vldsdfin =  -12117.01
- where a.cdcooper = 13
-   and a.cdagenci = 1
-   and a.nrdcaixa = 6
-   and a.dtmvtolt = to_date('08/12/2020', 'dd/mm/rrrr');
-commit;
