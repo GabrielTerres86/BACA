e044bb65a107330cd9b914151527140989329986
Alexandre Pietrobelli <alexandre.pietrobelli@supero.com.br>
2021-03-03 15:27:08 -0300

    PRJ0022855 - Cancelar agendamento na Atenda Deb.Autom
    PRJ0022855 - Parametros para EXTR0002.pck  Cancelar Debito Automático

---
+++ 2021/03/EXTR0002_parametros_Cancelamento_repique.sql
@@ -0,0 +1,44 @@
+
+update crappco set dsconteu = dsconteu|| ',1230,1231,1232,1233,1234,2027'      where cdpartar = 31;
+
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 1,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170'); 
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',10,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',11,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',12,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',13,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',14,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',15,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',16,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',17,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 2,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 3,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 4,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 5,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 6,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 7,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 8,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 9,'HORALIMITE_CNS_CANCEL', 'Horário limite Cancelamento Repique', '1170');  
+
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 1,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',10,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',11,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',12,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',13,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',14,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',15,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',16,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED',17,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 2,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 3,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 4,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 5,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 6,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 7,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 8,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+ INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)  VALUES('CRED', 9,'HIST_CNS_CANCEL', 'Históricos de Cancelamento de Consórcio', ',1230,1231,1232,1233,1234,2027,');  
+
+commit;
+
+
+
+
