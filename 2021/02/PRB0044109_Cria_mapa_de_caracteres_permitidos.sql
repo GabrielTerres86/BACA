b68918f27b477311e1225919f2efde1a0bc44ec2
Alexandre Santos <alexandre.santos@supero.com.br>
2021-03-10 09:09:27 -0300

    * Remoção de alguns caracteres especiais da lista dos bloqueados conforme solicitado.

--- 2021/02/PRB0044109_Cria_mapa_de_caracteres_permitidos.sql
+++ 2021/02/PRB0044109_Cria_mapa_de_caracteres_permitidos.sql
@@ -23,7 +23,7 @@ BEGIN
   end loop;
   --
   -- Lista de caracteres que geram erro no xml
-  vr_bloqueados := 'ְֱֲֳִֵאבגדהוׂ׃װױײ״עףפץצרָֹÊֻטיךכַחּֽ־ֿלםמןÙÚÛÜשתûüÿׁס!#$%*()-=+[]{}/\°÷×^~¨´`';
+  vr_bloqueados := 'ְֱֲֳִֵאבגדהוׂ׃װױײ״עףפץצרָֹÊֻטיךכַחּֽ־ֿלםמןÙÚÛÜשתûüÿׁס!#*-=+[]{}\°÷×^~¨´`';
   --
   FOR i in 1..length(vr_bloqueados)
   LOOP
