2bd5e9f2e3597dada68dd11c133aa70fc2f98479
Paulo Martins (Mouts) <t0031812@ailos.coop.br>
2021-03-15 15:17:05 +0000

    Baca para alteração dos status do Bem.

---
+++ INC0081603_ROLLBACK.sql
@@ -0,0 +1,12 @@
+begin
+  UPDATE crapbpr        bpr
+  SET    bpr.flgbaixa = 1
+        ,bpr.cdsitgrv = 1
+        ,bpr.flginclu = 0
+  WHERE  bpr.cdcooper = 14
+  AND    bpr.nrdconta = 4766
+  AND    bpr.tpctrpro = 90
+  AND    bpr.nrctrpro = 188
+  AND    bpr.flgalien = 1;
+  commit;
+end;
