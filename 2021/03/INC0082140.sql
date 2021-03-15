61d7d447b250a40e839d221fda87b8e5f8bfc0a7
Ramon Martins <ramon.silva@ailos.coop.br>
2021-03-15 11:51:56 -0300

    hotfix/INC0082140/master

--- 2021/03/INC0082140.sql
+++ 2021/03/INC0082140.sql
@@ -6,6 +6,13 @@ WHERE CDCOOPER = 1
 AND NRDCONTA = 7268203
 AND CDDEMAIL = 1;
 
+
+
+update tbcadast_pessoa_email
+set dsemail = 'aliatan.matheusg@gmail.com'
+where idpessoa = 2276290
+and nrseq_email = 1;
+
  
  COMMIT;
  
