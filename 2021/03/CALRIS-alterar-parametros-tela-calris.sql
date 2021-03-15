cb6aafab81fded54897f8454a8223c4d0368484c
Jefferson Luiz da Silva - Mouts <t0033489@ailos.coop.br>
2021-03-15 14:17:24 -0300

    Alteração nos parametros de chamada da consulta de tanque

---
+++ 2021/03/CALRIS-alterar-parametros-tela-calris.sql
@@ -0,0 +1,14 @@
+DECLARE
+
+BEGIN
+
+UPDATE cecred.crapaca
+   SET lstparam = 'pr_dtinicio,pr_dtfim,pr_status,pr_tppessoa,pr_nriniseq,pr_nrregist,pr_cdclasrisco'
+ WHERE nmdeacao = 'BUSCA_TANQUE'
+   AND nmpackag = 'TELA_CALRIS'
+   AND nmproced = 'pc_busca_tanque';
+COMMIT;
+WHEN OTHERS THEN
+	NULL;
+END;
+/
