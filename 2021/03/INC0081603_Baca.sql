e785279fce2902b3f777f026ffee28b4e7bb608d
Paulo Martins (Mouts) <t0031812@ailos.coop.br>
2021-03-15 17:14:17 +0000

    Baca para baixar bem.

---
+++ 2021/03/INC0081603_Baca.sql
@@ -0,0 +1,16 @@
+begin
+  UPDATE crapbpr        bpr
+  SET    bpr.flgbaixa = 0
+        ,bpr.cdsitgrv = 4
+        ,bpr.flginclu = 0
+  WHERE  bpr.cdcooper = 14
+  AND    bpr.nrdconta = 4766
+  AND    bpr.tpctrpro = 90
+  AND    bpr.nrctrpro = 188
+  AND    bpr.flgalien = 1;
+  commit;
+exception
+  when others then
+    null;  
+end;
+
