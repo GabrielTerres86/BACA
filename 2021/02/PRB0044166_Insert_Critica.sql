e39ce1880d6a96290c2570633be16db084af2f0e
Jefferson Gubetti <jefferson.gubetti@ailos.coop.br>
2021-03-24 14:43:22 -0300

    Removida a criacao de nova critica

--- 2021/02/PRB0044166_Insert_Critica.sql
+++
@@ -1,8 +0,0 @@
-begin
-  
-  insert into crapcri (CDCRITIC, DSCRITIC, PROGRESS_RECID, TPCRITIC, FLGCHAMA)
-  values (5029, '5029 - Alteracao de vencimento retroativa nao permitida.', null, 4, 0);
- 
-  commit;
-end;
-
