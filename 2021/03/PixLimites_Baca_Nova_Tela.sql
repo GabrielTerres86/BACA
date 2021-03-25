65fac4c07a3370e4a035ae710523be263a7bcbd1
Marcos Martini <t0033182@cecred.coop.br>
2021-03-25 13:33:28 -0300

    Nova ação e ajuste na exclusão

--- 2021/03/PixLimites_Baca_Nova_Tela.sql
+++ 2021/03/PixLimites_Baca_Nova_Tela.sql
@@ -6,7 +6,7 @@ BEGIN
   FOR rw_crapcop IN (SELECT cdcooper FROM crapcop where cdcooper = 3) LOOP 
     -- LIMITE
     insert into craptel (NMDATELA, NRMODULO, CDOPPTEL, TLDATELA, TLRESTEL, FLGTELDF, FLGTELBL, NMROTINA, LSOPPTEL, INACESSO, CDCOOPER, IDSISTEM, IDEVENTO, NRORDROT, NRDNIVEL, NMROTPAI, IDAMBTEL)
-    values ('LIMITE', 5, '@,C,I,A,E', 'Consulta e Gerenciamento de Limites', 'Consulta e Gerenciamento de Limites', 0, 1, ' ', 'ACESSO,CONSULTAR,INSERIR,ALTERAR,EXCLUIR', 2, rw_crapcop.cdcooper, 1, 0, 0, 0, ' ', 0);
+    values ('LIMITE', 7, '@,C,I,A,E', 'Consulta e Gerenciamento de Limites', 'Consulta e Gerenciamento de Limites', 0, 1, ' ', 'ACESSO,CONSULTAR,INSERIR,ALTERAR,EXCLUIR', 2, rw_crapcop.cdcooper, 1, 0, 0, 0, ' ', 0);
     insert into CRAPPRG (nmsistem,cdprogra,dsprogra##1,dsprogra##2,dsprogra##3,dsprogra##4,nrsolici,nrordprg,inctrprg,cdrelato##1,cdrelato##2,cdrelato##3,cdrelato##4,cdrelato##5,inlibprg,cdcooper)
     values ('CRED','LIMITE', 'Consulta e Gerenciamento de Limites',NULL,NULL,NULL,989,999,1,0,0,0,0,0,0,RW_CRAPCOP.CDCOOPER);
   END LOOP;  
@@ -34,12 +34,17 @@ BEGIN
   INSERT INTO crapaca
     (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
   VALUES
-    ('EXCLUIR_FAIXA','TELA_LIMITE','pc_excluir_faixa','pr_idfailim',vr_nrseqrdr);
+    ('EXCLUIR_FAIXA','TELA_LIMITE','pc_excluir_faixa','pr_idfailim,pr_tlcooper,pr_cdprodut',vr_nrseqrdr);
     
   INSERT INTO crapaca
     (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
   VALUES
     ('MANTER_FAIXA','TELA_LIMITE','pc_mantem_faixa','pr_idfailim,pr_tlcooper,pr_cdprodut,pr_vlfaixa_ate,pr_vlaumento_permitido',vr_nrseqrdr);
+	
+  INSERT INTO crapaca
+    (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
+  VALUES
+    ('LISTA_COOP_PIX','TELA_LIMITE','pc_lista_cooperativas','pr_cdcopsel,pr_flgativo',vr_nrseqrdr);	
 
     
   COMMIT;
