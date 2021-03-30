9eafecb433977c5a8f44591f7bd98caa6fd82b99
Daniel Zimmermann <daniel.zimmermann@ailos.coop.br>
2021-03-30 12:17:13 -0300

    Ajuste mensagem

--- 2021/03/INC0084575_Liberar_Prejuizo.sql
+++ 2021/03/INC0084575_Liberar_Prejuizo.sql
@@ -1,6 +1,6 @@
 UPDATE tbdscc_ocorrencias oco
    SET oco.flgbloqueio = 0
-      ,oco.dsocorre    = 'Cooperado possui ou teve prejuízo'
+      ,oco.dsocorre    = 'Cooperado possui ou causou prejuízo'
  WHERE oco.cdocorre = 13;
  
 COMMIT;
