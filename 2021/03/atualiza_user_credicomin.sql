5eebfb30fa01787ce077f73cc400d36d60c04125
Darlei Fernando Zillmer <darlei.zillmer@supero.com.br>
2021-03-30 15:35:24 -0300

    Retirado update desnecessario

--- 2021/03/atualiza_user_credicomin.sql
+++ 2021/03/atualiza_user_credicomin.sql
@@ -5,10 +5,5 @@ BEGIN
    WHERE p.cdacesso = 'USUARIO_AUTH_REG_CTR' 
      AND p.cdcooper = 10;
      
-   UPDATE crapprm p
-     SET p.dsvlrprm = 'Ailos@2020'
-   WHERE p.cdacesso = 'SENHA_AUTH_REG_CTR' 
-     AND p.cdcooper = 10;
-     
   COMMIT;
 END;
