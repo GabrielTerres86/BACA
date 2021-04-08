9188078d38ead6308674c529c2971d79f2b7bb8c
Ricardo de Souza <ricardo.souza@supero.com.br>
2021-04-08 11:42:32 -0300

    [PRB0044260] - Medida de contorno para resgate de aplicação programada

--- 2021/04/PRB0044260-UPDATE-CRAPRAC.sql
+++ 2021/04/PRB0044260-UPDATE-CRAPRAC.sql
@@ -1,6 +1,257 @@
-update craprac 
-   set idsaqtot = 1
- where cdcooper = 7 
-   and nrdconta = 214299
-   and nraplica = 31;
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 6943730
+  AND nraplica = 6 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 9371842
+  AND nraplica = 3 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 7980108
+  AND nraplica = 9 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 3709469
+  AND nraplica = 57 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 7059140
+  AND nraplica = 18 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 1965921
+  AND nraplica = 33 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 8418462
+  AND nraplica = 15 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 3781054
+  AND nraplica = 29 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 10910140
+  AND nraplica = 19 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 6880533
+  AND nraplica = 20 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 8285691
+  AND nraplica = 67 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 8662150
+  AND nraplica = 28 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 10930370
+  AND nraplica = 2 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 11280271
+  AND nraplica = 4 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 11019913
+  AND nraplica = 10 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 2405342
+  AND nraplica = 49 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 6425151
+  AND nraplica = 18 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 10773070
+  AND nraplica = 9 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 7748574
+  AND nraplica = 21 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 9958304
+  AND nraplica = 11 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 6800181
+  AND nraplica = 67 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 6021689
+  AND nraplica = 51 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 2205793
+  AND nraplica = 23 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 6315011
+  AND nraplica = 109 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 7850727
+  AND nraplica = 27 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 11965517
+  AND nraplica = 2 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 1
+  AND nrdconta = 11913754
+  AND nraplica = 1 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 2
+  AND nrdconta = 487902
+  AND nraplica = 14 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 5
+  AND nrdconta = 201804
+  AND nraplica = 9 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 7
+  AND nrdconta = 214299
+  AND nraplica = 31 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 9
+  AND nrdconta = 376213
+  AND nraplica = 7 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 11
+  AND nrdconta = 379557
+  AND nraplica = 2 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 11
+  AND nrdconta = 666467
+  AND nraplica = 4 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 11
+  AND nrdconta = 426091
+  AND nraplica = 33 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 16
+  AND nrdconta = 285943
+  AND nraplica = 3 ;
+
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 16
+  AND nrdconta = 232645
+  AND nraplica = 9 ;
+
+UPDATE craprac
+SET idsaqtot = 1
+WHERE cdcooper = 16
+  AND nrdconta = 162396
+  AND nraplica = 24 ;
 commit;
