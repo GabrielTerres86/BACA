dfe3a80b8c3a44d50222e1d14ca4f328a6562707
Adriano Luis da Silva <t0033065@cecred.coop.br>
2021-04-13 14:15:29 -0300

    INC0085645 - Produtos - Erros - Crédito - CDC

---
+++ 2021/04/INC0085645_acerta_usuario_ativo_convenio_inativo.sql
@@ -0,0 +1,18 @@
+UPDATE tbepr_cdc_usuario
+SET flgbloque=1, flgativo=0
+WHERE idusuario in
+(
+SELECT  u.IDUSUARIO
+FROM    tbepr_cdc_usuario_vinculo   uv
+       ,tbepr_cdc_usuario           u
+       ,tbsite_cooperado_cdc        tcc
+       ,crapcdr                     cdr
+WHERE tcc.idcooperado_cdc = uv.idcooperado_cdc
+AND   u.idusuario = uv.idusuario
+AND   u.dslogin = 'APP_COOPERADO'
+AND   cdr.cdcooper = tcc.cdcooper
+AND   cdr.nrdconta = tcc.nrdconta
+AND   u.flgativo = 1
+AND   cdr.flgconve = 0
+)
+/
