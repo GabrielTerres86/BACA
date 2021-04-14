c21008cbba7a95e6f4c942d847c393e5cb2d9111
Gustavo Rossi Muller <t0033426@ailos.coop.br>
2021-04-14 19:17:18 +0000

    Adicionados 2 novos casos

--- 2021/04/INC0085026.sql
+++ 2021/04/INC0085026.sql
@@ -1,5 +1,8 @@
 begin
-	-- Caso B
+  -- Caso A
+  update tbcc_lancamentos_pendentes set idsituacao = 'M', dscritica = '[INC0085026] - ' || dscritica where IDSEQ_LANCAMENTO in (7336286, 7278499);
+  
+  -- Caso B
   update tbcc_lancamentos_pendentes set idsituacao = 'M', dscritica = '[INC0085026] - ' || dscritica where IDSEQ_LANCAMENTO in (5319838, 5769506, 5319872, 5319841);
   
   -- Caso C
