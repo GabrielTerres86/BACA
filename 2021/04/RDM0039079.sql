17159f19f4f8948141ecdebf422276317b4dc3e2
Gustavo Rossi Muller <t0033426@ailos.coop.br>
2021-04-16 17:45:53 +0000

    Adicionado script para corrigir os lançamentos com erro no tanque

---
+++ 2021/04/RDM0039079.sql
@@ -0,0 +1,9 @@
+begin
+  update tbcc_lancamentos_pendentes tbcc
+     set tbcc.idsituacao = 'M',
+         tbcc.dscritica  = 'INC0086649 Sala de crise 15/04, pix afetado em decorrencia da RDM0038933'
+    where tbcc.idtransacao in (7626536,7626561,7626572,7626573,7626575,7626578,7626590,7626618,7626625,7626635,7626748,7626750,7626751,7626753,7626754,7626755,7626772,7626775,7626776,7626777,7626779,7626783)
+       and tbcc.dtmvtolt >= '15/04/2021'
+       and tbcc.idsituacao = 'E';
+   commit;
+end;
