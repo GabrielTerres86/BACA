9d1768ee926bf9b9323de117f96dbaa4eda7cdb9
Anderson-Supero <t0033412@ailos.coop.br>
2021-04-22 17:30:00 -0300

    Scripts para teste do PRB0043715

---
+++ 2021/04/PRB0043715 - Scripts.sql
@@ -0,0 +1,7 @@
+insert into crapaca(nmdeacao, nmpackag, nmProced, lstParam, nrseqrdr)
+values('JOB_CUSAPL_LISTA_CONT_ARQ', 'TELA_CUSAPL', 'pc_executa_carga_job',
+       'pr_cdcooper,pr_nrdconta,pr_nraplica,pr_flgcritic,pr_datade,pr_datate,pr_nmarquiv,pr_dscodib3,pr_inacao,pr_nrregist,pr_nriniseq, pr_idarquivo', 1249);
+Commit;
+
+update crapprm set dsvlrprm = 'douglas.kloppel@ailos.coop.br' where cdacesso in ('MONITORAMENTO_TRANSF', 'EMAIL_TESTE');
+Commit;
