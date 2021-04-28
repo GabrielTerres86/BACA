965719dfb2bc0356446b00386241123952e712dd
Alexandre Diego Girardi <t0033227@cecred.coop.br>
2021-04-28 13:22:22 -0300

    Retirado os segundos dos horários

--- 2021/04/RITM0129641.sql
+++ 2021/04/RITM0129641.sql
@@ -1,8 +1,8 @@
-INSERT INTO CRAPPRM(NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES('CRED', 0 , 'HORA_SAC_FDS_INI', 'Hora inicial de funcionamento do SAC nos finais de semanas e feriados.','08:00:00' );
+INSERT INTO CRAPPRM(NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES('CRED', 0 , 'HORA_SAC_FDS_INI', 'Hora inicial de funcionamento do SAC nos finais de semanas e feriados.','08:00' );
 /
 COMMIT;
 /
-INSERT INTO CRAPPRM(NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES('CRED', 0 , 'HORA_SAC_FDS_FIM', 'Hora final de funcionamento do SAC nos finais de semanas e feriados.','20:00:00' );
+INSERT INTO CRAPPRM(NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) VALUES('CRED', 0 , 'HORA_SAC_FDS_FIM', 'Hora final de funcionamento do SAC nos finais de semanas e feriados.','20:00' );
 /
 COMMIT;
 /
