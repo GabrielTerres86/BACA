d032f35975cb479da65c1175bf7c31bb9f684de2
Rafael Torres Estima <t0033404@cecred.coop.br>
2021-05-03 15:34:25 -0300

    Meses de historico para cooperados e procurados com relacionamento encerrado

---
+++ 2021/05/PRJ0023028_historico.sql
@@ -0,0 +1,5 @@
+begin
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 0, 'CRPS691_HIST_ENCERRADO', 'Meses de historico para cooperados e procurados com relacionamento encerrado', '60');
+commit;
+end;
