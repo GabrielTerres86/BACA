502d5cee1ba9bbbe8aecb1784eecdd315a0d4176
Daniel <daniel.lombardi@mouts.info>
2021-05-05 17:28:57 -0300

    PRJ0023326 Exportação CSV PESQTI - Script de parâmetros.

---
+++ 2021/05/PRJ0023326_insert_crapaca_params_expcsv.sql
@@ -0,0 +1,13 @@
+/*
+
+  Insert na tabela de ações relacionado ao projeto de exportação CSV da tela PESQTI
+  
+  Daniel Lombardi - Mout'S
+
+*/
+INSERT INTO crapaca VALUES (4550, 'EXPORTACAO_CSV_PESQTI', 'CONV0002', 'pc_gera_csv_pesqti', 'pr_cdcooper,pr_dtipagto,pr_dtfpagto,pr_cdagenci,pr_cdempcon,pr_cdsegmto,pr_nrdconta,pr_nrautdoc,pr_cdagearr', 505); 
+COMMIT;
+
+
+
+
