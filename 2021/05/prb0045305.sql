59b20dd3f83e9b678f9140ecde9ceeefeef8afbf
Renier Figueiredo Padilha <renier.padilha@ailos.coop.br>
2021-05-12 10:43:07 -0300

    Alteração da quantidade de execuções antes do commit do envio em lote de boletos agendados

--- 2021/05/prb0045305.sql
+++ 2021/05/prb0045305.sql
@@ -4,7 +4,7 @@ BEGIN
   values ('CRED',0,'QT_ENVIOCNSJD','Quantidade de execução da job de envio de consultas JD',31);
 
   insert into crapprm (nmsistem,cdcooper,cdacesso,dstexprm,dsvlrprm) 
-  values ('CRED',0,'QT_COMMITLOTEJD','Quantidade de execuções entre o commit na consulta em lote com a JD',100);
+  values ('CRED',0,'QT_COMMITLOTEJD','Quantidade de execuções entre o commit na consulta em lote com a JD',1000);
 
   COMMIT;
 EXCEPTION
