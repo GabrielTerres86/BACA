de9b9433b042a4185fc35fe4f9dd61dd73af4350
Rafael Innig <t0033292@ailos.coop.br>
2021-05-13 14:14:59 -0300

    Atualização manutenções na TAB089.

---
+++ 2021/05/Parametros_renegociacao_canais_r5.sql
@@ -0,0 +1,26 @@
+-- Created on 12/05/2021 by t0033292 
+BEGIN
+  DECLARE
+    -- nome da rotina
+    wk_rotina varchar2(200) := 'Criação de novos parâmetros da Renegociação Facilitada para Canais (tabela crapprm)';
+    CURSOR cr_crapcop IS
+    SELECT *
+      FROM crapcop cop
+     WHERE cop.flgativo = 1;
+  BEGIN
+    -- Percorrer as cooperativas do cursor
+    FOR rw_crapcop IN cr_crapcop LOOP
+      INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) values ('CRED', rw_crapcop.cdcooper, 'QT_VALIDADE_PRO_RENCAN', 'Quantidade de dias para validade da proposta.', '1');
+    END LOOP;
+    
+    UPDATE crapaca SET lstparam = 'pr_prtlmult,pr_prestorn,pr_prpropos,pr_vlempres,pr_pzmaxepr,pr_vlmaxest,pr_pcaltpar,pr_vltolemp,pr_qtdpaimo,pr_qtdpaaut,pr_qtdpaava,pr_qtdpaapl,pr_qtdpasem,pr_qtdpameq,pr_qtdibaut,pr_qtdibapl,pr_qtdibsem,pr_qtditava,pr_qtditapl,pr_qtditsem,pr_pctaxpre,pr_qtdictcc,pr_avtperda,pr_vlperavt,pr_vlmaxdst,pr_inpreapv,pr_vlmincnt,pr_nrmxrene,pr_nrmxcore,pr_pcalttax,pr_nrperccc,pr_qtdiamin,pr_qtdiamax,pr_cdlcrcpp,pr_cdlcrpos,pr_cdfincan,pr_idcarenc,pr_tptrrene,pr_flgfinta,pr_flglimcr,pr_flgcores,pr_nrmxreca,pr_nrmxcoca,pr_qtvalsim,pr_flatmobi,pr_flatconl,pr_vlmxremo,pr_vlmxreon,pr_vlmxreca,pr_vlmxcohi,pr_qtvalpro,pr_preapvhib,pr_vlmaxhibfis,pr_vlmaxhibjur'
+    WHERE  nmdeacao             = 'TAB089_ALTERAR';
+	
+    COMMIT;
+    dbms_output.put_line('Sucesso ao executar: ' || wk_rotina);
+  EXCEPTION
+    WHEN OTHERS THEN
+      dbms_output.put_line('Erro ao executar: ' || wk_rotina ||' --- detalhes do erro: '|| SQLCODE || ': ' || SQLERRM);
+    ROLLBACK;
+  END;
+END;
