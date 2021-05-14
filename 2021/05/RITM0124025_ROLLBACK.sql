827734551c1fcc3bf09a2c1cb415104acad341c3
fronza1003 <fronza1003@gmail.com>
2021-05-14 15:12:24 -0300

    hotfix/RITM0124025/master

---
+++ 2021/05/RITM0124025_ROLLBACK.sql
@@ -0,0 +1,61 @@
+update crapnac c
+set c.cdpais = null
+   ,c.nmpais = null
+where c.cdnacion = 12;
+
+update crapnac c
+set c.cdpais = null 
+   ,c.nmpais= null
+where c.cdnacion = 13;
+
+update crapnac c
+set c.cdpais = null
+   ,c.nmpais= null
+where c.cdnacion = 24;
+
+update crapnac c
+set c.cdpais = null
+   ,c.nmpais= null
+where c.cdnacion = 30;
+
+update crapnac c
+set c.cdpais = null
+   ,c.nmpais= null
+where c.cdnacion = 43;
+
+update crapnac c
+set c.cdpais = null
+   ,c.nmpais= null
+where c.cdnacion = 44;
+
+update crapnac c
+set c.cdpais = null
+   ,c.nmpais= null
+where c.cdnacion = 50;
+
+update crapnac c
+set c.cdpais = null
+   ,c.nmpais= null
+where c.cdnacion = 54;
+
+update crapnac c
+set c.cdpais = null
+   ,c.nmpais= null
+where c.cdnacion = 57;
+
+update crapnac c
+set c.cdpais = null
+   ,c.nmpais= null
+where c.cdnacion = 59;
+
+update crapnac c
+set c.cdpais = null
+   ,c.nmpais = null
+where c.cdnacion = 61;
+
+update crapnac c
+set c.cdpais = null
+   ,c.nmpais= null
+where c.cdnacion = 67;
+
+commit;
