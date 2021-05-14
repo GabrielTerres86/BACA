dc4ea930925a80673222bafee40067d51b112022
Odirlei Busana <odirlei.busana@ailos.coop.br>
2021-05-14 17:06:29 +0000

    Updated PRJ0016715_insert_crapprm_ini_modelo_2.sql

--- 2020/10/PRJ0016715_insert_crapprm_ini_modelo_2.sql
+++ 2020/10/PRJ0016715_insert_crapprm_ini_modelo_2.sql
@@ -11,7 +11,9 @@ BEGIN
     ,'INIC_GERA_ACOR_3040_MOD2'
     ,'Data inicio da geração acordo modelo modelo 2'
     ,trunc(SYSDATE));
+
   COMMIT;
+  
 EXCEPTION
   WHEN OTHERS THEN
     raise_application_error(-20100,
