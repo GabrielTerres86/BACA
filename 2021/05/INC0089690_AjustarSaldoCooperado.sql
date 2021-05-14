805d013867f3b0015baab6421058d3eb213f84cd
Renato Darosci <renato.darosci@supero.com.br>
2021-05-14 09:44:53 -0300

    Script para atualização de saldo do cooperado

--- 2021/05/INC0089690_AjustarSaldoCooperado.sql
+++ 2021/05/INC0089690_AjustarSaldoCooperado.sql
@@ -5,14 +5,14 @@ BEGIN
      SET t.vlsddisp = t.vlsddisp + 407
    WHERE t.cdcooper = 1
      AND t.nrdconta = 80276180
-     AND t.dtmvtolt BETWEEN '06/04/2021' AND TRUNC(SYSDATE);
+     AND t.dtmvtolt BETWEEN to_date('06/04/2021','dd/mm/yyyy') AND TRUNC(SYSDATE);
 
   -- Atualizar os registros da CRAPSDA com o valor não contabilizado em 07/05/2021
   UPDATE crapsda t
      SET t.vlsddisp = t.vlsddisp + 407
    WHERE t.cdcooper = 1
      AND t.nrdconta = 80276180
-     AND t.dtmvtolt BETWEEN '07/05/2021' AND TRUNC(SYSDATE);
+     AND t.dtmvtolt BETWEEN to_date('07/05/2021','dd/mm/yyyy') AND TRUNC(SYSDATE);
   
   COMMIT;
   
