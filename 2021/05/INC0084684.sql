28109d7a82d0cbdaed46a0690549d794c002fef5
fronza1003 <fronza1003@gmail.com>
2021-05-19 09:32:02 -0300

    hotfix/INC0084684/master

--- 2021/05/INC0084684.sql
+++ 2021/05/INC0084684.sql
@@ -11,15 +11,15 @@ begin
               left join tbcadast_pessoa_historico his
                 on his.idpessoa = pes.idpessoa
              where ass.dtnasctl is null
-               and ass.dtdemiss is not null
                and his.idcampo = 41
              order by dhalteracao) loop
     if i.dsvalor_anterior is not null then
+	
       update crapass c
          set c.dtnasctl = to_date(i.dsvalor_anterior, 'dd/mm/yyyy')
        where c.nrdconta = i.nrdconta
          and c.cdcooper = i.cdcooper;
-    
+		 
       update crapttl t
          set t.dtnasttl = to_date(i.dsvalor_anterior, 'dd/mm/yyyy')
        where t.nrdconta = i.nrdconta
