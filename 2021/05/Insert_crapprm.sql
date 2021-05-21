e9e10bd10cc513255fcc0c57da43b39ad1697c50
Danilo Korczagin <danilo.korczagin@supero.com.br>
2021-05-21 15:57:33 -0300

    PRB0045431 - inclusão de commit no script

--- 2021/05/Insert_crapprm.sql
+++ 2021/05/Insert_crapprm.sql
@@ -3,4 +3,6 @@ begin
   insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
   values ('CRED', 3, 'HABILITA_LOG_CONTA_CRD', 'Habilita LOG Conta Cartão via trigger - TBCRD_LOG_CONTA_CARTAO (1 - Ativado / 0 - Desativado)', '1', null);
 
+  commit;
+  
 end;
