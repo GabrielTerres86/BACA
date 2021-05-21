65f450bd211aecdfcbb8e452216e20064953d2bb
Danilo Korczagin <danilo.korczagin@supero.com.br>
2021-05-21 15:56:11 -0300

    PRB0045431 - Script de insert na tabela crapprm

---
+++ 2021/05/Insert_crapprm.sql
@@ -0,0 +1,6 @@
+begin
+
+  insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+  values ('CRED', 3, 'HABILITA_LOG_CONTA_CRD', 'Habilita LOG Conta Cartão via trigger - TBCRD_LOG_CONTA_CARTAO (1 - Ativado / 0 - Desativado)', '1', null);
+
+end;
