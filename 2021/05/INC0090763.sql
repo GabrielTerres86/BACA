959fa3d09ab5c26f27d3fb7a1200ea96b2792a16
fronza1003 <fronza1003@gmail.com>
2021-05-26 16:33:58 -0300

    hotfix/INC0090763/master

--- 2021/05/INC0090763.sql
+++ 2021/05/INC0090763.sql
@@ -1,4 +1,18 @@
-update crapass ass set ass.cdsitdct = 4 where ass.cdcooper = 7 and ass.nrdconta = 72893;
-
-commit;
-
+begin
+  for i in (select ass.cdcooper,
+                   ass.nrdconta,
+                   ass.dtdemiss,
+                   ass.cdsitdct
+              from crapass ass
+             where ass.dtadmiss is not null               
+               and ass.cdsitdct = 7) loop
+             
+      update crapass c
+         set c.cdsitdct = 4
+       where c.nrdconta = i.nrdconta
+         and c.cdcooper = i.cdcooper
+         and c.dtadmiss < '01/01/2019';
+    
+  end loop;
+  commit;
+end;
