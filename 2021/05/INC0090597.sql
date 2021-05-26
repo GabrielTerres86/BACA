e9544f1b81c9643101627947b017497c343c9d76
Felipe Fronza Marcos (Mouts) <mouts-felipe.marcos@ailos.coop.br>
2021-05-26 19:37:43 +0000

    Deleted INC0090597.sql

--- 2021/05/INC0090597.sql
+++
@@ -1,4 +0,0 @@
-update crapass ass set ass.cdsitdct = 4 where ass.cdcooper = 7 and ass.nrdconta = 809;
-update crapass ass set ass.cdsitdct = 4 where ass.cdcooper = 7 and ass.nrdconta = 70858;
-
-commit;
