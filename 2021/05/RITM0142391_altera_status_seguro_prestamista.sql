ac765d97b2034f048185d468f8ddf3fca51c84c5
philipe <philipe.leal@ailos.coop.br>
2021-05-27 09:27:12 -0300

    alterado tambem o status

--- 2021/05/RITM0142391_altera_status_seguro_prestamista.sql
+++ 2021/05/RITM0142391_altera_status_seguro_prestamista.sql
@@ -1,1 +1,5 @@
-update crapseg set CDSITSEG = 1 where PROGRESS_RECID = 934783;
+begin
+update crapseg set CDSITSEG = 1, dtcancel = '', cdopeexc = 0, cdageexc = 0, dtinsexc = '',cdopecnl = '' where PROGRESS_RECID = 934783;
+update tbseg_prestamista set tpregist = 3 where idseqtra = 213341;
+commit;
+end;
