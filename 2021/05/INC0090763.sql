933ec58850435da945906600d934865b39427597
Felipe Fronza Marcos (Mouts) <mouts-felipe.marcos@ailos.coop.br>
2021-05-27 12:40:22 +0000

    Updated INC0090763.sql

--- 2021/05/INC0090763.sql
+++ 2021/05/INC0090763.sql
@@ -1,18 +1,7 @@
-begin
-  for i in (select ass.cdcooper,
-                   ass.nrdconta,
-                   ass.dtdemiss,
-                   ass.cdsitdct
-              from crapass ass
-             where ass.dtdemiss is not null               
-               and ass.cdsitdct = 7) loop
-             
-      update crapass c
-         set c.cdsitdct = 4
-       where c.nrdconta = i.nrdconta
-         and c.cdcooper = i.cdcooper
-         and c.dtdemiss < to_date('01/01/2019','DD/MM/YYYY');
-    
-  end loop;
-  commit;
-end;
+update crapass c
+set c.cdsitdct = 4
+where c.dtdemiss is not null
+and c.cdsitdct = 7
+and c.dtdemiss < to_date('01/01/2019','DD/MM/YYYY');
+
+commit;
