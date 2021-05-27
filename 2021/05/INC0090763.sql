cb67fc15927283a2cc7fae693f02bf479d909523
Felipe Fronza Marcos (Mouts) <mouts-felipe.marcos@ailos.coop.br>
2021-05-27 12:28:05 +0000

    Updated INC0090763.sql

--- 2021/05/INC0090763.sql
+++ 2021/05/INC0090763.sql
@@ -4,14 +4,14 @@ begin
                    ass.dtdemiss,
                    ass.cdsitdct
               from crapass ass
-             where ass.dtadmiss is not null               
+             where ass.dtdemiss is not null               
                and ass.cdsitdct = 7) loop
              
       update crapass c
          set c.cdsitdct = 4
        where c.nrdconta = i.nrdconta
          and c.cdcooper = i.cdcooper
-         and c.dtadmiss < '01/01/2019';
+         and c.dtdemiss < to_date('01/01/2019','DD/MM/YYYY');
     
   end loop;
   commit;
