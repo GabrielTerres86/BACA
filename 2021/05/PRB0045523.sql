b02a7543dea92f107ac11c65e74a764c6e382e33
Paulo Cesar Schmidt <paulo.schmidt@cecred.coop.br>
2021-05-28 14:26:17 -0300

    Script para correcao dos comprovantes Tivit

--- 2021/05/PRB0045523.sql
+++ 2021/05/PRB0045523.sql
@@ -3,9 +3,9 @@ update
 set 
 	pro.dsinform##3 = replace(pro.dsinform##3, '#Agente Arrecadador: #Agência:', '#Agente Arrecadador: 93 - P0LOCRED SOCIEDADE DE CREDITO AO MICROEMPREENDEDOR #Agência: 9999 - P0LOCRED SOCIEDADE DE CREDITO AO MICROEMPREENDEDOR')
 where 
-	p.cdcooper >= 1 
-	and p.cdtippro = 16 
-	and p.dtmvtolt between '06/05/2021' and '27/05/2021' 
-	and p.dsinform##3 like '%#Agente Arrecadador: #Agência: %';
+	pro.cdcooper >= 1 
+	and pro.cdtippro = 16 
+	and pro.dtmvtolt between '06/05/2021' and '27/05/2021' 
+	and pro.dsinform##3 like '%#Agente Arrecadador: #Agência: %';
 
 commit;
