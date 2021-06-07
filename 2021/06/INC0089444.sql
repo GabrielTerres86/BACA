7d891048f621616e961dcca7ba77290bf40c28f9
Darlei Fernando Zillmer <darlei.zillmer@supero.com.br>
2021-06-07 14:33:22 -0300

    Correcao contabil Financ Abril Reneg

--- 2021/06/INC0089444.sql
+++ 2021/06/INC0089444.sql
@@ -4,9 +4,11 @@ BEGIN
   DELETE FROM tbepr_renegociacao_craplem l WHERE l.cdcooper = 1 AND l.nrdconta = 10469150 AND l.nrctremp = 1523295 AND l.nrversao = 3 AND l.cdhistor = 2305;
   
   UPDATE crapris SET cdmodali = 499 WHERE cdcooper = 2 AND nrdconta = 766909 AND nrctremp = 291866 AND cdmodali = 299;
+  UPDATE crapvri SET cdmodali = 499 WHERE cdcooper = 2 AND nrdconta = 766909 AND nrctremp = 291866 AND cdmodali = 299;
   UPDATE crapepr SET cdlcremp = 955 WHERE cdcooper = 2 AND nrdconta = 766909 AND nrctremp = 291866;
   
   UPDATE crapris SET cdmodali = 499 WHERE cdcooper = 13 AND nrdconta = 141739 AND nrctremp = 49713 AND cdmodali = 299;
+  UPDATE crapvri SET cdmodali = 499 WHERE cdcooper = 13 AND nrdconta = 141739 AND nrctremp = 49713 AND cdmodali = 299;
   UPDATE crapepr SET cdlcremp = 171 WHERE cdcooper = 13 AND nrdconta = 141739 AND nrctremp = 49713;  
   COMMIT;
 END;
