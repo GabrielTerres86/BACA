326d8d5c9a95c5f71fa31c6895d461ef920b0a4e
Guilherme Boettcher <guilherme.boettcher@ailos.coop.br>
2021-06-08 14:04:52 -0300

    MOTOR - Ajuste Patrimonio Referencia

---
+++ 2021/06/MOTOR_Atualizar_Patrimonio_Referencia.sql
@@ -0,0 +1,53 @@
+update tbcadast_cooperativa 
+   set VLPATRIMONIO_REFERENCIAL =  1822545853.48 
+where cdcooper = 1;
+
+update tbcadast_cooperativa 
+   set VLPATRIMONIO_REFERENCIAL =  123056131.87 
+where cdcooper = 16;
+
+update tbcadast_cooperativa 
+   set VLPATRIMONIO_REFERENCIAL = 96819165.60 
+where cdcooper = 9;
+
+update tbcadast_cooperativa 
+   set VLPATRIMONIO_REFERENCIAL = 47672176.15  
+where cdcooper = 13;
+
+update tbcadast_cooperativa 
+   set VLPATRIMONIO_REFERENCIAL = 62596086.17 
+where cdcooper = 14;
+
+update tbcadast_cooperativa 
+   set VLPATRIMONIO_REFERENCIAL = 25957203.66
+where cdcooper = 12;
+
+update tbcadast_cooperativa 
+   set VLPATRIMONIO_REFERENCIAL = 80961800.74
+where cdcooper = 11;
+
+update tbcadast_cooperativa 
+   set VLPATRIMONIO_REFERENCIAL = 29087507.14 
+where cdcooper = 6;
+
+update tbcadast_cooperativa 
+   set VLPATRIMONIO_REFERENCIAL = 22610381.51 
+where cdcooper = 10;
+
+update tbcadast_cooperativa 
+   set VLPATRIMONIO_REFERENCIAL = 6894862.68 
+where cdcooper = 8;
+
+update tbcadast_cooperativa 
+   set VLPATRIMONIO_REFERENCIAL = 64276055.25
+where cdcooper = 7;
+
+update tbcadast_cooperativa 
+   set VLPATRIMONIO_REFERENCIAL = 22102249.79 
+where cdcooper = 5;
+
+update tbcadast_cooperativa 
+   set VLPATRIMONIO_REFERENCIAL = 43713075.38 
+where cdcooper = 2;
+
+commit;
