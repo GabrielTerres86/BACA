be113951715ff0ebfbec26219f130834e5acd201
Darlei Fernando Zillmer <darlei.zillmer@supero.com.br>
2021-06-16 08:37:20 -0300

    Correcao de linhas de credito

---
+++ 2021/06/correcao_linhas_reneg_jan.sql
@@ -0,0 +1,5 @@
+BEGIN
+  UPDATE crapepr SET cdlcremp = 15503 WHERE cdcooper = 1   AND nrdconta = 10257659   AND nrctremp = 1958447;
+  UPDATE crapepr SET cdlcremp = 15503 WHERE cdcooper = 1   AND nrdconta = 10257659   AND nrctremp = 2859425;
+  COMMIT;
+END;
