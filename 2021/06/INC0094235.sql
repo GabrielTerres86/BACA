a70911c4f36d082a58308b1c59157170819fb6d3
Ramon Martins <ramon.silva@ailos.coop.br>
2021-06-22 11:34:06 -0300

    hotfix/INC0094235/master

--- 2021/06/INC0094235.sql
+++ 2021/06/INC0094235.sql
@@ -3,5 +3,11 @@ UPDATE CRAPENC
  where cdcooper = 1
    and nrdconta = 12765287;
    
+
+UPDATE CRAPTTL
+SET NRDOCTTL = '8083140916'
+where cdcooper = 9
+and nrdconta = 145971;   
+   
    
    commit;
