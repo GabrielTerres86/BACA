6bfcaf5d313efa6ed85f14815d2c7d5f0a0617d9
Joao <amcom-joao.brasil@ailos.coop.br>
2021-06-23 11:42:03 -0300

    remover 2 registros da craplem de R60,48, lançados no dia: 02/06/2021, c/c 350052, Viacredi Alto Vale

---
+++ 2021/06/INC0093063.sql
@@ -0,0 +1,9 @@
+-- INC0093063 - remover 2 registros de R$160,48 lançados no dia: 02/06/2021 
+DELETE FROM craplem lem
+ WHERE lem.cdcooper = 16
+   AND lem.nrdconta = 350052
+   AND lem.nrctremp = 132801
+   AND lem.dtmvtolt = '02/06/2021'
+   AND lem.vllanmto = 160.48;
+
+COMMIT;
