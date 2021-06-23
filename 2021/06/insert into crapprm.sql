8e20c14156741a523006c87933e1b4a1f43157af
Petrônio Sifuentes Gomes Júnior <mouts-petronio.junior@ailos.coop.br>
2021-06-23 13:38:55 -0300

    alteração no insert

--- 2021/06/insert into crapprm.sql
+++ 2021/06/insert into crapprm.sql
@@ -2,7 +2,7 @@ declare
 begin 
 	
 insert into crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) 
-values ('CRED', 0, 'MONIT_EMAIL_CHAVES', 'Email de destino dos monitoramentos.', 'coaf@ailos.coop.br');
+values ('CRED', 0, 'MONIT_EMAIL_CHAVES', 'Email de destino dos monitoramentos.', 'monitoracaodefraudes@ailos.coop.br');
 
 commit;
 end;
