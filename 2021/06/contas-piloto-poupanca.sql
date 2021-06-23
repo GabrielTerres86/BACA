b2e6bc6f8b6f24b53baaac01d5859fcb92d02cfc
Douglas <douglas.kloppel@ailos.coop.br>
2021-06-23 17:50:46 -0300

    testes em contas piloto para o produto poupanca

---
+++ 2021/06/contas-piloto-poupanca.sql
@@ -0,0 +1,7 @@
+begin
+   update crapprm 
+      set dsvlrprm = dsvlrprm || '8000166,8721386,12591645,12609099,10561,12591645,9515984,7144091,11206705,12236071,2236192,11830336,6767214,12880833,12880841,9465855,9467343,'
+    where cdcooper = 1 
+      and cdacesso = 'CONTA_PILOTO_POUPANCA_PF';
+ commit;
+end;
