84572826205b3873727026085c49969a92f2aa87
Darlei Fernando Zillmer <darlei.zillmer@supero.com.br>
2021-06-23 10:31:55 -0300

    Insert de param de atraso

---
+++ 2021/06/param_atraso.sql
@@ -0,0 +1,87 @@
+begin 
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 1, 'QT_ATRASO_MAXIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(max).', '15');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 1, 'QT_ATRASO_MINIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(min).', '02');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 2, 'QT_ATRASO_MAXIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(max).', '30');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 2, 'QT_ATRASO_MINIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(min).', '01');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 3, 'QT_ATRASO_MAXIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(max).', '30');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 3, 'QT_ATRASO_MINIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(min).', '1');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 5, 'QT_ATRASO_MAXIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(max).', '30');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 5, 'QT_ATRASO_MINIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(min).', '1');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 6, 'QT_ATRASO_MAXIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(max).', '30');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 6, 'QT_ATRASO_MINIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(min).', '1');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 7, 'QT_ATRASO_MAXIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(max).', '30');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 7, 'QT_ATRASO_MINIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(min).', '1');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 8, 'QT_ATRASO_MAXIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(max).', '30');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 8, 'QT_ATRASO_MINIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(min).', '1');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 9, 'QT_ATRASO_MAXIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(max).', '30');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 9, 'QT_ATRASO_MINIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(min).', '1');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 10, 'QT_ATRASO_MAXIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(max).', '30');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 10, 'QT_ATRASO_MINIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(min).', '1');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 11, 'QT_ATRASO_MAXIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(max).', '30');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 11, 'QT_ATRASO_MINIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(min).', '1');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 12, 'QT_ATRASO_MAXIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(max).', '30');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 12, 'QT_ATRASO_MINIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(min).', '1');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 13, 'QT_ATRASO_MAXIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(max).', '30');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 13, 'QT_ATRASO_MINIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(min).', '1');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 14, 'QT_ATRASO_MAXIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(max).', '30');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 14, 'QT_ATRASO_MINIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(min).', '1');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 16, 'QT_ATRASO_MAXIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(max).', '30');
+
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 16, 'QT_ATRASO_MINIMO_RENCAN', 'Dias em atraso do contrato disponivel para renegociar(min).', '1');
+
+    commit;
+end;
