c63e0f3558152acaa51d72863d6aa6389b0b91b3
Kleber Reis Dos Santos <supero-kleber.santos@cecred.coop.br>
2021-06-23 10:48:16 -0300

    Ajustes do script para colocar nos padrões definidos

--- 2021/06/INSERT_INC172747_PARAMETRO-LMT_CREDITO_COTA_CAPITAL.sql
+++ 2021/06/INSERT_INC172747_PARAMETRO-LMT_CREDITO_COTA_CAPITAL.sql
@@ -1,6 +1,7 @@
 /*Insere parametro LMT_CREDITO_COTA_CAPITAL na tabela crapprm */
-
-insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
-values ('CRED', 0, 'LMT_CREDITO_COTA_CAPITAL', 'Cooperativa para desconsiderar o limite de crédito em débito de cotas',
- ',6,7,9,13,16,', 599496);
-/
+begin
+	insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+	values ('CRED', 0, 'LMT_CREDITO_COTA_CAPITAL', 'Cooperativa para desconsiderar o limite de crédito em débito de cotas',
+	 ',6,7,9,13,16,', null);
+end;	
+/	
