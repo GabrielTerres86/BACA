96179b3dab1042739cf313d920c65614838e06d0
mouts-mateus.zimmermann@ailos.coop.br <zim.mateus@gmail.com>
2021-06-24 18:33:14 -0300

    Ajuste no script

--- 2021/06/INC0094974_ajustar_flag_envio_lote_sefaz.sql
+++ 2021/06/INC0094974_ajustar_flag_envio_lote_sefaz.sql
@@ -1,13 +1,14 @@
 -- Atualizacao da flag e data de envio do registros para que nao seja enviado no arquivo consolidado por erro de pgto
 
 UPDATE craplft SET 
-  craplft.insitfat = 2, 
+  craplft.insitfat = 1, 
   craplft.dtdenvio = trunc(sysdate)
 WHERE 
   craplft.cdcooper = 14 
-  AND craplft.insitfat = 1
+  AND craplft.insitfat = 2
   AND craplft.cdhistor = 1063
   AND craplft.vllanmto = 62.31
-  AND craplft.dtmvtolt = trunc(sysdate);
+  AND craplft.dtmvtolt = trunc(sysdate)
+  AND craplft.cdbarras = 85250000000623100247200022778587000210021353;
 
 COMMIT;
