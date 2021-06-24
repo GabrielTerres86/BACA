16921f5dbd27377a57701696d9487c7dde6981c1
Odirlei Busana <odirlei.busana@ailos.coop.br>
2021-06-24 20:54:24 -0300

    Ajuste lancamentos realizados no dia errado

---
+++ 2021/02/DML_Ajuste_lanc.sql
@@ -0,0 +1,23 @@
+BEGIN
+  
+  UPDATE craplcm x
+     SET x.dtmvtolt = to_date('25/06/2021','DD/MM/RRRR')
+  WHERE x.cdcooper = 12
+  AND x.dtmvtolt = to_date('24/06/2021','DD/MM/RRRR')
+  AND TRUNC(x.dttrans) >= to_date('24/06/2021','DD/MM/RRRR');
+
+  UPDATE craplem x
+     SET x.dtmvtolt = to_date('25/06/2021','DD/MM/RRRR')
+  WHERE x.cdcooper = 12
+  AND x.dtmvtolt = to_date('24/06/2021','DD/MM/RRRR')
+  AND x.dthrtran >= to_date('24/06/2021','DD/MM/RRRR');
+
+  UPDATE tbdsct_lancamento_bordero x
+     SET x.dtmvtolt = to_date('25/06/2021','DD/MM/RRRR')
+  WHERE x.cdcooper = 12
+  AND x.dtmvtolt = to_date('24/06/2021','DD/MM/RRRR')
+  AND x.dtrefatu >= to_date('24/06/2021','DD/MM/RRRR');
+
+  COMMIT;
+
+END;
