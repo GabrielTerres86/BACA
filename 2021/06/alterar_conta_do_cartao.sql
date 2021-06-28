23813e3864d6434465967dbfc2a87e7a13507c9d
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2021-06-28 14:12:15 -0300

    Ajustar cartão do Gislon

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -1,50 +1,46 @@
-DECLARE
+declare
 
   vr_cooperativa INTEGER;
-  vr_conta       INTEGER;
-  vr_cartao      NUMBER(25);
+  vr_conta INTEGER;
+  vr_cartao NUMBER(25);
 
   vr_cpf_titular NUMBER(20);
 
-BEGIN
+begin
 
   -- Cooperativa de destino do cartão
-  vr_cooperativa := 1;
+  vr_cooperativa := 8;
   -- Conta de destino do cartão
-  vr_conta := 8075620;
+  vr_conta := 19;
 
   -- Numero do cartão que precisamos ajustar
---  vr_cartao := 5127070162667067; -- Jeff
---  vr_cartao := 5127070162667935; -- XV
---  vr_cartao := 5127070161674411; -- Luis
---  vr_cartao := 5158940000000188; -- Matheus
-  vr_cartao := 5156010019676523; -- SeP - PF
+  vr_cartao := 5127070161674411; -- Luis
 
   -- Verificar se a conta possui algum outro cartão para buscar o CPF do Titular
-  FOR cartao IN (SELECT DISTINCT a.nrcpftit
-                   FROM crawcrd a
-                  WHERE a.cdcooper = vr_cooperativa
-                    AND a.nrdconta = vr_conta
-                    AND a.insitcrd = 4 -- Apenas cartão ativo
+  FOR cartao IN (select distinct a.nrcpftit
+                  from crawcrd a
+                 where a.cdcooper = vr_cooperativa
+                   and a.nrdconta = vr_conta
+                   and a.insitcrd = 4 -- Apenas cartão ativo
                  ) LOOP
     -- carregar o CPF da conta
     vr_cpf_titular := cartao.nrcpftit;
-  
+
   END LOOP;
 
   IF vr_cpf_titular IS NULL THEN
     -- Se não tem CPF tem que buscar o CPF do titular da conta
-    FOR titular IN (SELECT a.nrcpfcgc
-                      FROM crapttl a
-                     WHERE a.cdcooper = vr_cooperativa
-                       AND a.nrdconta = vr_conta
-                       AND a.idseqttl = 1 -- CPF do primeiro titular do cartão
-                    ) LOOP
+    FOR titular IN (select a.nrcpfcgc
+                      from crapttl a
+                     where a.cdcooper = vr_cooperativa
+                       and a.nrdconta = vr_conta
+                       and a.idseqttl = 1 -- CPF do primeiro titular do cartão
+                   ) LOOP
       -- carregar o CPF da conta
       vr_cpf_titular := titular.nrcpfcgc;
-    
+
     END LOOP;
-  
+
   END IF;
 
   IF vr_cpf_titular IS NULL THEN
@@ -52,23 +48,21 @@ BEGIN
     RETURN;
   END IF;
 
-  -- Atualizar os dados do cartão
-  UPDATE crapcrd card
-     SET card.cdcooper = vr_cooperativa
-        ,card.nrdconta = vr_conta
-        ,card.nrcpftit = vr_cpf_titular
-        ,card.cdadmcrd = 15
-        ,card.qtsenerr = 0
-        ,card.inacetaa = 1
-   WHERE card.nrcrcard = vr_cartao;
 
-  UPDATE crawcrd card
-     SET card.cdcooper = vr_cooperativa
-        ,card.nrdconta = vr_conta
-        ,card.nrcpftit = vr_cpf_titular
-        ,card.cdadmcrd = 15
-   WHERE card.nrcrcard = vr_cartao;
+
+  -- Atualizar os dados do cartão
+  UPDATE crapcrd
+     SET crapcrd.cdcooper = vr_cooperativa
+        ,crapcrd.nrdconta = vr_conta
+        ,crapcrd.nrcpftit = vr_cpf_titular
+   WHERE crapcrd.nrcrcard = vr_cartao;
+
+  UPDATE crawcrd
+     SET crawcrd.cdcooper = vr_cooperativa
+        ,crawcrd.nrdconta = vr_conta
+        ,crawcrd.nrcpftit = vr_cpf_titular
+   WHERE crawcrd.nrcrcard = vr_cartao;
 
   COMMIT;
 
-END;
+end;
