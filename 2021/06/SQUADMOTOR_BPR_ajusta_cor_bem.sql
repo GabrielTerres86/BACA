0ad3db6f3fd92ed930a3e97380349346fdef14c4
Guilherme Boettcher <guilherme.boettcher@ailos.coop.br>
2021-06-30 16:28:41 -0300

    SQUADMOTOR - Ajustar Cor Bens

--- 2021/06/SQUADMOTOR_BPR_ajusta_cor_bem.sql
+++ 2021/06/SQUADMOTOR_BPR_ajusta_cor_bem.sql
@@ -1,22 +1,25 @@
-update crapbpr set dscorbem = 'PRATA METÁLICA'
-where cdcooper = 1
-and nrdconta = 8009082
-and tpctrpro = 90
-and nrctrpro = 1465171
-and idseqbem = 1;
+begin
+  update crapbpr set dscorbem = 'PRATA METÁLICA'
+  where cdcooper = 1
+  and nrdconta = 8009082
+  and tpctrpro = 90
+  and nrctrpro = 1465171
+  and idseqbem = 1;
 
-update crapbpr set dscorbem = 'PRATA METÁLICA'
-where cdcooper = 1
-and nrdconta = 9353607
-and tpctrpro = 90
-and nrctrpro = 1468263
-and idseqbem = 1;
+  update crapbpr set dscorbem = 'PRATA METÁLICA'
+  where cdcooper = 1
+  and nrdconta = 9353607
+  and tpctrpro = 90
+  and nrctrpro = 1468263
+  and idseqbem = 1;
 
-update crapbpr set dscorbem = 'PRATA METÁLICA'
-where cdcooper = 1
-and nrdconta = 8563101
-and tpctrpro = 90
-and nrctrpro = 1476207
-and idseqbem = 1;
+  update crapbpr set dscorbem = 'PRATA METÁLICA'
+  where cdcooper = 1
+  and nrdconta = 8563101
+  and tpctrpro = 90
+  and nrctrpro = 1476207
+  and idseqbem = 1;
 
-commit;
+  commit;
+end;
+/
