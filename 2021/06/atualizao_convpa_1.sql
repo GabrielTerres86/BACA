e758dfaf2f3d0865c70e9a1ace7010ff3ed7ecda
Anderson Felix <mouts-anderson.felix@ailos.coop.br>
2021-06-30 16:28:15 -0300

    correcao

--- 2021/06/atualizao_convpa_1.sql
+++ 2021/06/atualizao_convpa_1.sql
@@ -1,4 +1,4 @@
-update crapaca set  LSTPARAM = 'pr_cddopcao,pr_cdcooper,pr_cdsegmto,pr_flagbarr,pr_vllimite,pr_hrlimite,pr_vhlimite,pr_hrlimitefim' 
+update CECRED.creacrapaca set  LSTPARAM = 'pr_cddopcao,pr_cdcooper,pr_cdsegmto,pr_flagbarr,pr_vllimite,pr_hrlimite,pr_vhlimite,pr_hrlimitefim' 
 WHERE nmpackag = 'TELA_CONVPA' 
 and nmdeacao = 'GRAVAR_SEGMTO_LIMITE';
 commit; 
