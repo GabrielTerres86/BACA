b0d672a0ab872b79fd959d6e1134b6ca1bae7922
Alexandre Pietrobelli <alexandre.pietrobelli@supero.com.br>
2021-07-02 15:25:28 -0300

    PRB0045656 Test
     Update Para validar idade limite
     e cancelamento do prestamista.

--- 2021/07/Update_PRB0045656_.sql
+++ 2021/07/Update_PRB0045656_.sql
@@ -1,20 +1,7 @@
 Begin 
-update tbseg_prestamista set   tpregist = 3 where cdcooper = 1 and
- nrctrseg  in(711648,561133,313526,417386,417390,553543,312905,312893,417377 
-              ,585429,714972,714971,174228,174228  
-              ,707628,707628,707628,711288,760635,405061,409986,698570,644791,653291,664288);  
-              
- update crapseg set cdsitseg = 1  where cdcooper = 1 and
- nrctrseg  in(711648,561133,313526,417386,417390,553543,312905,312893,417377 
-              ,585429,714972,714971,174228,174228
-              ,707628,707628,707628,711288,760635,405061,409986,698570,644791,653291,664288);
-
-update crapass set dtnasctl = to_date('01/06/1950','DD/MM/YYYY') where  cdcooper = 1 and nrdconta = 2552787;
-update tbseg_prestamista set dtnasctl = to_date('01/06/1950','DD/MM/YYYY') where  cdcooper = 1 and nrdconta = 2552787;
-
-update crapass set dtnasctl = to_date('01/07/1950','DD/MM/YYYY') where  cdcooper = 1 and nrdconta = 8380279;
-update tbseg_prestamista set dtnasctl = to_date('01/07/1950','DD/MM/YYYY') where  cdcooper = 1 and nrdconta = 8380279;
+update crapass set dtnasctl = to_date('01/07/1950','DD/MM/YYYY') where  cdcooper = 1 and nrdconta in (2200368 , 2780844, 907472);
+update tbseg_prestamista set dtnasctl = to_date('01/07/1950','DD/MM/YYYY') where  cdcooper = 1 and nrdconta in (2200368 , 2780844, 907472);
 
 commit;
 end;
-/
+/     
