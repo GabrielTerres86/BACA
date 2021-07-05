c79c410117cfb944a4d1405cc0b3b99dfafa6719
Adriano Luis da Silva <t0033065@cecred.coop.br>
2021-07-05 14:13:00 -0300

    208586 - INC0095460 - Erro Job CECRED.JBEPR_CDC_EFETIVA_PROPOSTA

--- 2021/07/altera_uf_gravame.sql
+++ 2021/07/altera_uf_gravame.sql
@@ -1,6 +1,11 @@
+begin
+
 update crapbpr
 set  UFLICENC= 'RN'
 where cdcooper = 1
 and   nrdconta = 8733503
-and   nrctrpro in (4078065, 4078064)
+and   nrctrpro in (4078065, 4078064);
+
+commit;
+end;
 /
