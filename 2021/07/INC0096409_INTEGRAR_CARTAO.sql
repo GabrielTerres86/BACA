7cd76f1fbdf17370773f88e8fc6f1d4bc763cecc
Guilherme Cervo (Supero) <t0032613@ailos.coop.br>
2021-07-07 15:46:55 -0300

    Integracao de cartao de representante nao cadastrado

---
+++ 2021/07/INC0096409_INTEGRAR_CARTAO.sql
@@ -0,0 +1,106 @@
+BEGIN
+  
+  DECLARE
+    vr_nrctrcrd crawcrd.nrctrcrd%TYPE := fn_sequence('CRAPMAT','NRCTRCRD', 1);
+    vr_nrseqcrd crawcrd.nrseqcrd%TYPE := CCRD0003.fn_sequence_nrseqcrd(pr_cdcooper => 1);
+  BEGIN
+     
+      INSERT INTO crawcrd
+         (nrdconta,
+          nrcrcard,
+          nrcctitg,
+          nrcpftit,
+          vllimcrd,
+          flgctitg,
+          dtmvtolt,
+          nmextttl,
+          flgprcrd,
+          tpdpagto,
+          flgdebcc,
+          tpenvcrd,
+          vlsalari,
+          dddebito,
+          cdlimcrd,
+          tpcartao,
+          dtnasccr,
+          nrdoccrd,
+          nmtitcrd,
+          nrctrcrd,
+          cdadmcrd,
+          cdcooper,
+          nrseqcrd,
+          dtpropos,
+          dtsolici,
+          flgdebit,
+          cdgraupr,
+          insitcrd,
+      flgimpnp)
+      VALUES
+         (12300, 
+          5474080215076652, 
+          7564457000721, 
+          50675702968,
+          55200,   
+          3,  
+          to_date('16/06/2021', 'DD/MM/RRRR'),
+          'GUILHERME WEIHERMANN',
+          1,
+          1,
+          1,
+          0,
+          0,
+          11,
+          0,
+          2,
+          to_date('26/09/1964', 'DD/MM/RRRR'),
+          978946,
+          'GUILHERME WEIHERMANN',
+          vr_nrctrcrd,
+          15, 
+          13,
+          vr_nrseqcrd,
+          to_date('15/06/2021', 'DD/MM/RRRR'),
+          to_date('15/06/2021', 'DD/MM/RRRR'),
+          1, 
+          5,
+          3,
+          0); 
+
+      INSERT INTO crapcrd
+         (cdcooper,
+          nrdconta,
+          nrcrcard,
+          nrcpftit,
+          nmtitcrd,
+          dddebito,
+          cdlimcrd,
+          dtvalida,
+          nrctrcrd,
+          cdmotivo,
+          nrprotoc,
+          cdadmcrd,
+          tpcartao,
+          dtcancel,
+          flgdebit,
+          flgprovi)
+      SELECT w.cdcooper, 
+			       w.nrdconta,
+						 w.nrcrcard,
+						 w.nrcpftit,
+						 w.nmtitcrd,
+						 w.dddebito,
+						 w.cdlimcrd,
+						 w.dtvalida,
+						 w.nrctrcrd,
+						 w.cdmotivo,
+						 w.nrprotoc,
+						 w.cdadmcrd,
+						 w.tpcartao,
+						 w.dtcancel,
+						 w.flgdebit,
+						 0
+				FROM crawcrd w WHERE w.cdcooper = 13 AND w.nrdconta = 12300 AND w.nrctrcrd = vr_nrctrcrd; 
+  END;  
+
+  COMMIT;
+END;  
