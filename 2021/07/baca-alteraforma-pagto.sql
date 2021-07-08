11f4aad08b731360012353cb383f2f4d9eecb9e1
Vinicius Bedin Knapp <mouts-vinicius.knapp@ailos.coop.br>
2021-07-08 16:55:39 -0300

    TEST ACAO

--- 2021/07/baca-alteraforma-pagto.sql
+++ 2021/07/baca-alteraforma-pagto.sql
@@ -5,11 +5,14 @@ BEGIN
 										 ,lstparam
 										 ,nrseqrdr
 										 ) 
-							 VALUES('ALTERA_FORMA_PAGTO'
+							 VALUES('ALTERA_FORMA_PAGTOTESTV'
 										 ,NULL
 										 ,'CREDITO.alterarFormaPagtoWeb	'
 										 ,'pr_nrdconta,pr_nrcontrato'
-										 ,1045);
-										 
+										 ,1045
+										 );
   COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
 END;
