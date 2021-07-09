ad8e5e97349641ddfd08e60e5fc02258ec9e63c2
Jefferson Luiz da Silva <jefferson.luiz@cecred.coop.br>
2021-07-09 14:06:23 -0300

    Alterado para update com valores fixos

--- 2021/07/calris_reenvio_kn1.sql
+++ 2021/07/calris_reenvio_kn1.sql
@@ -1,37 +1,26 @@
 DECLARE
 
-  CURSOR c01 IS
-    SELECT b.idcalris_tanque, a.idcalris_pessoa
-      FROM tbcalris_pessoa a, tbcalris_tanque b
-     WHERE a.cdclasrisco_final > 2
-       AND trunc(a.dhcalculofinal) > '04/06/2021'
-       AND a.cdstatus = 1
-       AND a.cdclasrisco_kn1 is not null
-       AND b.nrcpfcgc = a.nrcpfcgc
-       AND b.tpcalculadora = a.tpcalculadora
-       AND NOT EXISTS
-     (SELECT 1
-              FROM tbgen_req_webservice x
-             WHERE x.dhrequis between a.dhcalculofinal - (1 / 24 / 60) AND
-                   a.dhcalculofinal + (1 / 24 / 60)
-               AND upper(x.dsservico) =
-                   'HTTP://EGUARDIAN.CECRED.COOP.BR/WSINTEGRACAORISC/WSINTEGRACAORISC.ASMX?OP=WSREQUISICAOINICIALKN1'
-               AND x.dsconteudo_requis like '%' || a.nrcpfcgc || '%');
-
 BEGIN
 
-  for r01 in c01 loop
-  
-    UPDATE tbcalris_tanque
-       SET cdstatus = 7
-     WHERE idcalris_tanque = r01.idcalris_tanque;
-  
-    UPDATE tbcalris_pessoa
-       SET dhkn1 = null, cdclasrisco_kn1 = null
-     WHERE idcalris_pessoa = r01.idcalris_pessoa;
-  end loop;
+UPDATE tbcalris_tanque
+   SET cdstatus = 7
+ WHERE idcalris_tanque in (
+	72605,72583,75556,69606,79380,77991,77925,68702,68998,70017,70275,69712,74984
+   ,75442,75459,76269,77988,78713,78033,79215,79381,79382,79383,78670,79412,79413
+   ,79414,79423,78855,77872,79384,81191,80921,81862,81746);
+COMMIT;
 
-  commit;
+UPDATE tbcalris_pessoa
+   SET dhkn1 = null
+     , cdclasrisco_kn1 = null
+ WHERE idcalris_pessoa IN (
+	91706,91684,95638,88593,98581,97192,97126,
+	87902,88198,89004,89262,88699,94084,94542,
+	94559,95348,97189,97914,97234,98416,98582,
+	98583,98584,97871,98613,98614,98615,98624,
+	98056,97073,98585,100605,100335,100104,99988
+ );
+ COMMIT;
 
 END;
 /
