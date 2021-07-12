d625790e2f21402b08205d0daa3aa27a3f39df1b
Maicon Milke <milke.maicon@gmail.com>
2021-07-12 14:22:02 -0300

    Adição da mensagem

---
+++ 2021/07/PRB0045708_criar_mensagem_estorno_ofsaa_cooperado.sql
@@ -0,0 +1,57 @@
+declare 
+  -- Local variables here
+  V_CODIGO TBGEN_NOTIF_MSG_CADASTRO.cdmensagem%TYPE;
+  
+begin
+
+SELECT MAX(CDMENSAGEM)
+    INTO V_CODIGO
+    FROM TBGEN_NOTIF_MSG_CADASTRO;
+
+  INSERT INTO TBGEN_NOTIF_MSG_CADASTRO
+    (CDMENSAGEM
+    ,CDORIGEM_MENSAGEM
+    ,DSTITULO_MENSAGEM
+    ,DSTEXTO_MENSAGEM
+    ,DSHTML_MENSAGEM
+    ,CDICONE
+    ,INEXIBIR_BANNER
+    ,INEXIBE_BOTAO_ACAO_MOBILE  
+    ,INENVIAR_PUSH )
+  VALUES
+    ((V_CODIGO + 1)
+    ,13
+    ,'PIX - Pagamento Pix: OFSAA (cancelado cooperado)'
+    ,'O valor foi estornado para a sua conta. Saiba Mais.'
+    ,'<b>A sua transação foi cancelada.</b></br></br>Valor: #valorpix </br>Beneficiário: #beneficiario </br>Instituição: #instituicao </br></br><b>O valor foi estornado e está disponível em sua conta corrente.</b></br></br>Em caso de dúvidas, entre em contato com a Cooperativa.</br></br>'
+    ,16
+    ,0
+    ,0
+    ,1);
+
+  INSERT INTO TBGEN_NOTIF_AUTOMATICA_PRM
+    (CDORIGEM_MENSAGEM
+    ,CDMOTIVO_MENSAGEM
+    ,DSMOTIVO_MENSAGEM
+    ,CDMENSAGEM
+    ,DSVARIAVEIS_MENSAGEM
+    ,INMENSAGEM_ATIVA
+    ,INTIPO_REPETICAO)
+  VALUES
+    (13
+    ,19
+    ,'PIX - Pagamento Pix: OFSAA (cancelamento cooperado)'
+    ,(V_CODIGO + 1)
+    ,'</br>#valorpix - Valor do Pix (Ex.: 45,00)</br>#instituicao - PSP do beneficiário</br>#beneficiario - Nome do beneficiário.'
+    ,1
+    ,0);
+  
+               
+COMMIT;
+
+  
+EXCEPTION
+  WHEN OTHERS THEN 
+       dbms_output.put_line('Erro: ' || SQLERRM);
+       
+end;
