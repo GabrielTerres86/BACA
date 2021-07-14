8bb0a2ef91e586ec42ce00cad9f6e3c9292d0173
caroline castro <premiersoft-caroline.castro@ailos.coop.br>
2021-07-14 16:04:27 -0300

    INC0097262 - Valores diferentes de parcela do mesmo contrato sendo exibido nas telas do Aimaro e extrato.

--- 2021/07/INC0097262_updatevalorparcelas.sql
+++ 2021/07/INC0097262_updatevalorparcelas.sql
@@ -4,10 +4,13 @@ BEGIN
    WHERE c.cdcooper = 1
      AND c.nrdconta = 9948333
      AND c.nrctremp = 4195837;
+     
   UPDATE crappep c
      SET vlparepr = 218.16,
          vlsdvpar = 218.16,
-         vlsdvsji = 218.16
+         vlsdvsji = 218.16,
+         vlsdvatu = 218.16,
+         vljura60 = 218.16
    WHERE c.cdcooper = 1
      AND c.nrdconta = 9948333
      AND c.nrctremp = 4195837;
