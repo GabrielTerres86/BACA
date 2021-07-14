b2e599d166fc350e18781699bfeb3b0386716758
Guilherme Pinheiro <guilherme.pinheiro@mouts.com.br>
2021-07-14 14:59:10 -0300

    script envio de boleto regulatorio

---
+++ 2021/07/baca-envio-email-boleto-test.sql
@@ -0,0 +1,19 @@
+DECLARE
+  dserro VARCHAR2(1000);
+BEGIN
+  FOR i IN (SELECT *
+              FROM crapsle e
+             WHERE e.cdcooper = 8
+               AND e.dsendere = 'fabricio.michelato@ailos.coop.br'
+               AND e.flenviad = 'N') LOOP
+  
+    gene0003.pc_process_email_penden(i.nrseqsol, dserro);
+  
+    UPDATE crapsle
+       SET flenviad = 'S'
+     WHERE nrseqsol = i.nrseqsol;
+  
+    COMMIT;
+  
+  END LOOP;
+END;
