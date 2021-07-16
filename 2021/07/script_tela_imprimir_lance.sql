4b2673c126720fa8201a8b4b2bcc8e774e4875c9
Bruno Cesar Dias <mouts-bruno.dias@cecred.coop.br>
2021-07-16 09:27:51 -0300

    script para gravar e imprimir simulacao de lance da tela simcon

---
+++ 2021/07/script_tela_imprimir_lance.sql
@@ -0,0 +1,44 @@
+begin
+----realiza o insert na tabela TBCONSOR_SIMULANCE dos dados da simulacao de aquisicao
+insert into crapaca
+  (NRSEQACA, NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
+values
+  ((select max(NRSEQACA) + 1 from crapaca),
+   'GRAVA_SIMU_LANCE',
+   'CNSO0002',
+   'pc_cadastra_simu_lance',
+   'pr_nrdconta
+   ,pr_idsegmento
+,pr_prtgmseguro
+,pr_vlcnso
+,pr_prazo
+,pr_txadm
+,pr_fundoreserva
+,pr_utilizacarta
+,pr_slddevedorlance
+,pr_rcrsproprio
+,pr_embutidovlcarta
+,pr_prcntlanceofertado
+,pr_coopusarcarta
+,pr_nvsaldo
+,pr_antescontemp
+,pr_aposcontemp
+,pr_reduzirprazo
+,pr_comissaolance'
+   , 
+   (select nrseqrdr from CRAPRDR WHERE NMprogra = 'SIMCON'));    
+
+-----imprime simulacao de Lance
+insert into crapaca
+  (NRSEQACA, NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
+values
+  ((select max(NRSEQACA) + 1 from crapaca),
+   'IMP_SIMU_LANCE',
+   'CNSO0002',
+   'pc_imp_simulador_lance_web',
+   'pr_id', 
+   (select nrseqrdr from CRAPRDR WHERE NMprogra = 'SIMCON'));          
+        
+   
+COMMIT;
+end;
