8f029a86cd65d904cb7da88d8326d51f6a106568
Guilherme Henrique Boettcher <guilherme.boettcher@cecred.coop.br>
2021-07-19 21:11:22 -0300

    RECUPERACAO - Reversão liberação

---
+++ 2021/07/RECUPERACAO_Atualiza_CYBER_19_07_2021.sql
@@ -0,0 +1,101 @@
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 201.55 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 82.77 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 363.13 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 415.32 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 1437.56 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 3.67 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 28.9 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 4.31 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 218.79 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 359.19 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 198.92 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 63.37 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 28.9 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 387.8 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 79.06 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 85.91 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 117.32 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 50.46 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 166.65 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 258.33 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 0 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 0 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 323.19 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 13.1 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 193.49 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 196.75 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 553.07 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 28.9 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 63.15 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 198.64 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 605.75 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 0 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 0 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 0 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 172.37 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 339.34 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 186.13 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 79.6 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 232.88 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 86.7 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 3806.17 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 143.39 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 370.17 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 47.56 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 9.18 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 81.16 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 820.38 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 600.99 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 210.65 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 0.36 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 321.02 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 48.87 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 70.17 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 330.76 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 28.9 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 684.55 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 37.59 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 432.56 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 20.07 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 1000.21 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 1023.04 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 35.32 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 28.9 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 35.58 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 352.32 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 28.9 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 1118.52 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 100.04 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 555.41 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 306.34 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 8.66 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 233.66 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 232.94 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 135.98 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 609.18 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 28.9 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 410.63 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 28.9 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 1085.7 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 285.07 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 1855.1 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 859.87 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 996.8 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 0 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+UPDATE crapcyb c SET c.dtdbaixa = NULL,c.vlpreapg = c.vlprapga,c.vlprapga = 19.59 ,c.vlsdeved = c.vlsdevan,c.vlsdevan = 0,c.vlsdprej = c.vlsdprea,c.vlsdprea = 0 WHERE c.progress_recid = 2501759;
+
+
+
+-- Limpar controle Jobs
+DELETE FROM tbgen_batch_controle c
+ WHERE c.cdcooper = 3
+   AND c.cdprogra = 'CRPS652'
+   AND c.dtmvtolt = '16/07/2021'
+   AND c.nrexecucao = 1
+   AND c.tpagrupador = 3;
+   
+   
+DELETE FROM tbgen_batch_relatorio_wrk x WHERE x.cdprograma LIKE '%652%' AND x.dtmvtolt = '16/07/2021';
+
+
+COMMIT;
