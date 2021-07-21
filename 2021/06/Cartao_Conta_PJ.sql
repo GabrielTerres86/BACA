2c05e1e06d8a7b9e70856a7feb696571590b4d47
Djonata Wehmuth <djonata.wehmuth@ailos.coop.br>
2021-07-21 16:34:19 -0300

    Update Cartao_Conta_PJ.sql

--- 2021/06/Cartao_Conta_PJ.sql
+++ 2021/06/Cartao_Conta_PJ.sql
@@ -9,7 +9,7 @@ BEGIN
 	 
 	UPDATE crawcrd
 	SET crawcrd.cdcooper = 16
-	,crawcrd.nrdconta = 20.500-1
+	,crawcrd.nrdconta = 205001
 	,crawcrd.nrcpftit = 00904280993
 	,cdadmcrd = 15
 	WHERE crawcrd.nrcrcard = 5161620000587872;
