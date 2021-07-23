25bedaf9fc601311e884bced86593fcf64e61584
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2021-07-23 14:23:04 -0300

    Ajuste cartão

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -1,68 +1,39 @@
-declare
+DECLARE
 
   vr_cooperativa INTEGER;
-  vr_conta INTEGER;
-  vr_cartao NUMBER(25);
+  vr_conta       INTEGER;
+  vr_cartao      NUMBER(25);
+
   vr_cpf_titular NUMBER(20);
 
-begin
+BEGIN
 
   -- Cooperativa de destino do cartão
-  vr_cooperativa := 16;
+  vr_cooperativa := 6;
   -- Conta de destino do cartão
-  vr_conta := 329;
+  vr_conta := 199028;
 
   -- Numero do cartão que precisamos ajustar
-  vr_cartao := 5127070320149719; 
-  --vr_cartao := 5127070161674411; 
+  vr_cartao := 5158940000000188; -- Matheus
 
-  -- Verificar se a conta possui algum outro cartão para buscar o CPF do Titular
-  FOR cartao IN (select distinct a.nrcpftit
-                  from crawcrd a
-                 where a.cdcooper = vr_cooperativa
-                   and a.nrdconta = vr_conta
-                   and a.insitcrd = 4 -- Apenas cartão ativo
-                 ) LOOP
-    -- carregar o CPF da conta
-    vr_cpf_titular := cartao.nrcpftit;
-
-  END LOOP;
-
-  IF vr_cpf_titular IS NULL THEN
-    -- Se não tem CPF tem que buscar o CPF do titular da conta
-    FOR titular IN (select a.nrcpfcgc
-                      from crapttl a
-                     where a.cdcooper = vr_cooperativa
-                       and a.nrdconta = vr_conta
-                       and a.idseqttl = 1 -- CPF do primeiro titular do cartão
-                   ) LOOP
-      -- carregar o CPF da conta
-      vr_cpf_titular := titular.nrcpfcgc;
-
-    END LOOP;
-
-  END IF;
-
-  IF vr_cpf_titular IS NULL THEN
-    -- Se não tem CPF para o processo
-    RETURN;
-  END IF;
-
-
+  -- carregar o CPF da conta
+  vr_cpf_titular := 1105508030;
 
   -- Atualizar os dados do cartão
-  UPDATE crapcrd
-     SET crapcrd.cdcooper = vr_cooperativa
-        ,crapcrd.nrdconta = vr_conta
-        ,crapcrd.nrcpftit = vr_cpf_titular
-   WHERE crapcrd.nrcrcard = vr_cartao;
+  UPDATE crapcrd card
+     SET card.cdcooper = vr_cooperativa
+        ,card.nrdconta = vr_conta
+        ,card.nrcpftit = vr_cpf_titular
+        ,card.qtsenerr = 0
+        ,card.inacetaa = 1
+   WHERE card.nrcrcard = vr_cartao;
 
-  UPDATE crawcrd
-     SET crawcrd.cdcooper = vr_cooperativa
-        ,crawcrd.nrdconta = vr_conta
-        ,crawcrd.nrcpftit = vr_cpf_titular
-   WHERE crawcrd.nrcrcard = vr_cartao;
+  UPDATE crawcrd card
+     SET card.cdcooper = vr_cooperativa
+        ,card.nrdconta = vr_conta
+        ,card.nrcpftit = vr_cpf_titular
+   WHERE card.nrcrcard = vr_cartao;
 
   COMMIT;
 
-end;
+END;
