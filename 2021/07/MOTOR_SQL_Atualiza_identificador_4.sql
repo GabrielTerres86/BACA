74c386962e686ae55de21651d63a340a2d95345d
Guilherme Henrique Boettcher <guilherme.boettcher@cecred.coop.br>
2021-07-23 16:33:52 -0300

    MOTOR - Ajuste Valor

---
+++ 2021/07/MOTOR_SQL_Atualiza_identificador_4.sql
@@ -0,0 +1,154 @@
+INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (8
+  ,47708
+  ,8562
+  ,19
+  ,1
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+
+
+
+INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (8
+  ,46035
+  ,8372
+  ,19
+  ,1
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+
+
+
+INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (8
+  ,45411
+  ,8386
+  ,19
+  ,1
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+
+
+INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (8
+  ,20435
+  ,8384
+  ,19
+  ,1
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+  
+INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (8
+  ,2976
+  ,8661
+  ,19
+  ,1
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+  
+INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (8
+  ,2976
+  ,3363
+  ,19
+  ,1
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('23-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
