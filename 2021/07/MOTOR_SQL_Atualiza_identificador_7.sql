d6a852fea9384ab133f1eb27f32fc3744a3da66d
Guilherme Henrique Boettcher <guilherme.boettcher@cecred.coop.br>
2021-07-26 11:17:47 -0300

    MOTOR - Ajuste Gravames

---
+++ 2021/07/MOTOR_SQL_Atualiza_identificador_7.sql
@@ -0,0 +1,358 @@
+BEGIN
+
+  INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (6
+  ,48674
+  ,222187
+  ,1
+  ,1
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+
+  INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (6
+  ,151211
+  ,241525
+  ,1
+  ,1
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+  
+  INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (6
+  ,162310
+  ,238402
+  ,1
+  ,1
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+
+  INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (6
+  ,149616
+  ,240197
+  ,1
+  ,1
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+
+
+  INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (6
+  ,507547
+  ,239336
+  ,1
+  ,1
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+
+  INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (6
+  ,172863
+  ,239661
+  ,1
+  ,1
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+
+  INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (6
+  ,202240
+  ,237702
+  ,1
+  ,1
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+
+  INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (6
+  ,114871
+  ,240703
+  ,3
+  ,1
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+  
+  INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (6
+  ,124257
+  ,240703
+  ,1
+  ,1
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+  
+ 
+  INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (6
+  ,198480
+  ,241186
+  ,1
+  ,1
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+  
+ 
+  INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (6
+  ,167770
+  ,239471
+  ,1
+  ,1
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+  
+  
+  INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (6
+  ,73830
+  ,239421
+  ,2
+  ,1
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+  
+    
+  
+  INSERT INTO tbgrv_registro_imagem
+  (cdcooper
+  ,nrdconta
+  ,nrctrpro
+  ,idseqbem
+  ,tpregistro_imagem
+  ,dtregistro_imagem
+  ,cdsituacao_imagem
+  ,dsretorno_imagem
+  ,cdopereg
+  ,dtinsori
+  ,dtrefatu)
+VALUES
+  (6
+  ,27294
+  ,217783
+  ,1
+  ,1
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,0
+  ,NULL
+  ,'1'
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss')
+  ,to_date('26-07-2021 17:17:17', 'dd-mm-yyyy hh24:mi:ss'));
+  
+  commit;
+
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300060 WHERE  x.cdcooper = 6 AND x.nrdconta = 27294  AND x.NRCTRPRO = 217783 AND x.idseqbem = 1 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300061 WHERE  x.cdcooper = 6 AND x.nrdconta = 27294  AND x.NRCTRPRO = 245304 AND x.idseqbem = 1 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300062 WHERE  x.cdcooper = 6 AND x.nrdconta = 48674  AND x.NRCTRPRO = 222187 AND x.idseqbem = 1 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300063 WHERE  x.cdcooper = 6 AND x.nrdconta = 48674  AND x.NRCTRPRO = 245301 AND x.idseqbem = 2 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300064 WHERE  x.cdcooper = 6 AND x.nrdconta = 73830  AND x.NRCTRPRO = 239421 AND x.idseqbem = 2 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300065 WHERE  x.cdcooper = 6 AND x.nrdconta = 110302 AND x.NRCTRPRO = 247954 AND x.idseqbem = 3 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300066 WHERE  x.cdcooper = 6 AND x.nrdconta = 110302 AND x.NRCTRPRO = 247954 AND x.idseqbem = 4 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300067 WHERE  x.cdcooper = 6 AND x.nrdconta = 114871 AND x.NRCTRPRO = 220060 AND x.idseqbem = 3 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300068 WHERE  x.cdcooper = 6 AND x.nrdconta = 124257 AND x.NRCTRPRO = 240703 AND x.idseqbem = 1 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300069 WHERE  x.cdcooper = 6 AND x.nrdconta = 149616 AND x.NRCTRPRO = 240197 AND x.idseqbem = 1 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300070 WHERE  x.cdcooper = 6 AND x.nrdconta = 151211 AND x.NRCTRPRO = 241525 AND x.idseqbem = 1 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300071 WHERE  x.cdcooper = 6 AND x.nrdconta = 162310 AND x.NRCTRPRO = 238402 AND x.idseqbem = 1 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300072 WHERE  x.cdcooper = 6 AND x.nrdconta = 167770 AND x.NRCTRPRO = 239471 AND x.idseqbem = 1 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300073 WHERE  x.cdcooper = 6 AND x.nrdconta = 172430 AND x.NRCTRPRO = 248102 AND x.idseqbem = 2 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300074 WHERE  x.cdcooper = 6 AND x.nrdconta = 172863 AND x.NRCTRPRO = 239661 AND x.idseqbem = 1 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300075 WHERE  x.cdcooper = 6 AND x.nrdconta = 198480 AND x.NRCTRPRO = 241186 AND x.idseqbem = 1 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300076 WHERE  x.cdcooper = 6 AND x.nrdconta = 202240 AND x.NRCTRPRO = 237702 AND x.idseqbem = 1 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300077 WHERE  x.cdcooper = 6 AND x.nrdconta = 237272 AND x.NRCTRPRO = 248133 AND x.idseqbem = 1 ;
+	UPDATE tbgrv_registro_contrato x SET dsc_identificador = 20210010300078 WHERE  x.cdcooper = 6 AND x.nrdconta = 507547 AND x.NRCTRPRO = 239336 AND x.idseqbem = 1 ;
+
+	commit;
+
+END;
