9e5785aac1138b0723cfb52c89f041734b1288f3
Ancelmo Luiz <ancelmo.luiz@hotmail.com>
2021-07-27 10:02:09 -0400

    Alteração do valor do campo crítica para INC0098669

--- 2021/07/INC0098669-UpdateLancamentosPendentes.sql
+++ 2021/07/INC0098669-UpdateLancamentosPendentes.sql
@@ -1,4 +1,4 @@
 BEGIN
-	UPDATE tbcc_lancamentos_pendentes set idsituacao = 'M', dscritica = 'RDM0040501' WHERE nrdcmto = 20021158;
+	UPDATE tbcc_lancamentos_pendentes set idsituacao = 'M', dscritica = 'INC0098669' WHERE nrdcmto = 20021158;
   COMMIT;
 END;
