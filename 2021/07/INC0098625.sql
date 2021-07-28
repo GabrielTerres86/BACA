9f2834e1b2fe9e5ab4673cd807f352c32dd1e424
Leandro da Silva <leandro.s@cecred.coop.br>
2021-07-28 17:08:30 -0300

    Ajuste do status das parcelas que tiveram erro na fis

---
+++ 2021/07/INC0098625.sql
@@ -0,0 +1,14 @@
+BEGIN
+  UPDATE
+    tbepr_consignado_pagamento p
+  SET
+    p.instatus = NULL  
+  WHERE 
+    ( p.cdcooper = 1  AND p.nrdconta = 12720828  AND p.nrctremp = 3995814 and p.nrparepr = 1 and p.instatus = 3)
+    OR ( p.cdcooper = 10  AND p.nrdconta = 141674  AND p.nrctremp = 18679 and p.nrparepr = 5 and p.instatus = 3)
+    
+  COMMIT;
+EXCEPTION
+        WHEN OTHERS THEN
+        ROLLBACK;
+END;
