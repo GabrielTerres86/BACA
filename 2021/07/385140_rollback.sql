63ecf0b1208a55d5345503addd89686141c9c11f
Joao <amcom-joao.brasil@ailos.coop.br>
2021-07-30 15:38:38 -0300

    remover lançamento duplicado

--- 2021/07/385140_rollback.sql
+++ 2021/07/385140_rollback.sql
@@ -13,6 +13,36 @@ BEGIN
 	and cdhistor = 2739
 	and vllanmto = 10
 	and dtmvtolt = to_date('28/07/2021', 'dd/mm/rrrr');
+
+	/*
+	 Cooperativa....: 11
+     Conta..........: 433578
+     Data...........: 23/07/2021
+     Valor..........: 15,58
+	 */
+	 
+	delete from tbcc_prejuizo_lancamento
+	where cdcooper = 11
+	and nrdconta = 433578
+	and cdhistor = 2739
+	and vllanmto = 15.58
+	and dtmvtolt = to_date('28/07/2021', 'dd/mm/rrrr');
+	
+	
+	
+	/*
+	 Cooperativa....: 11
+     Conta..........: 203262
+     Data...........: 23/07/2021
+     Valor..........: 12,52
+	 */
+	
+	delete from tbcc_prejuizo_lancamento
+	where cdcooper = 11
+	and nrdconta = 203262
+	and cdhistor = 2739
+	and vllanmto = 12.52
+	and dtmvtolt = to_date('28/07/2021', 'dd/mm/rrrr');
 	
 	COMMIT;
 	
