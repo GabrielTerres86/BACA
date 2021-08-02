318665a5eddaed265470a9225608bb0e1e7dda21
Guilherme Cervo (Supero) <t0032613@ailos.coop.br>
2021-08-02 08:29:43 -0300

    Adicionado novo codigo de produto para cartao visa

--- 2021/07/PRJ0023037_DE_PARA_HISTORICOS_UNICRED.sql
+++ 2021/07/PRJ0023037_DE_PARA_HISTORICOS_UNICRED.sql
@@ -1,16 +1,13 @@
 BEGIN
-	-- Compra debito
-	INSERT INTO tbcrd_his_vinculo_unicred VALUES (3469, 3577);
-	-- Estorno
-	INSERT INTO tbcrd_his_vinculo_unicred VALUES (5812, 3578);
-	-- Saque ATM   
-	INSERT INTO tbcrd_his_vinculo_unicred VALUES (3465, 3579);
-	-- Estorno Saque ATM   
-	INSERT INTO tbcrd_his_vinculo_unicred VALUES (5810, 3580);
-	-- Tarifa Saque ATM
-	INSERT INTO tbcrd_his_vinculo_unicred VALUES (3467, 3581);
-	-- Estorno Tarifa Saque ATM        
-	INSERT INTO tbcrd_his_vinculo_unicred VALUES (5811, 3582);
+
+	INSERT INTO crapprd (cdprodut, dsprodut, cdarnego) VALUES (29, 'CARTAO VISA UNICRED', 4);
+	
+	INSERT INTO cartao.tbcrd_his_vinculo_unicred VALUES (3469, 3577);
+	INSERT INTO cartao.tbcrd_his_vinculo_unicred VALUES (5812, 3578);
+	INSERT INTO cartao.tbcrd_his_vinculo_unicred VALUES (3465, 3579);
+	INSERT INTO cartao.tbcrd_his_vinculo_unicred VALUES (5810, 3580);
+	INSERT INTO cartao.tbcrd_his_vinculo_unicred VALUES (3467, 3581);
+	INSERT INTO cartao.tbcrd_his_vinculo_unicred VALUES (5811, 3582);
 	
 	COMMIT;
 	
