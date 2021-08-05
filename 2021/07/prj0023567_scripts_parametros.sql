1d8131af28b7fdd9a984186a63248c3db567ca27
Evandro Guaranha(Amcom) <evandro.silva@amcom.com.br>
2021-08-05 16:18:25 -0300

    ajuste padrao script

--- 2021/07/prj0023567_scripts_parametros.sql
+++ 2021/07/prj0023567_scripts_parametros.sql
@@ -1,40 +1,42 @@
 -- project/prj0023567 / parametros
+BEGIN
+  INSERT INTO craprdr
+    (NMPROGRA
+    ,DTSOLICI)
+  VALUES
+    ('LPRV0001'
+    ,TRUNC(SYSDATE));
+  
+  COMMIT;  
 
-insert into craprdr ( NMPROGRA, DTSOLICI)
-values ( 'LPRV0001', TRUNC(SYSDATE));
-/
-commit;
-/
+  UPDATE crapaca a
+     SET a.lstparam = a.lstparam || ',pr_tpctrlim'
+   WHERE a.nmdeacao = 'EXEC_CARGA_MANUAL';
 
-UPDATE crapaca a
-   SET a.lstparam = a.lstparam || ',pr_tpctrlim'
- WHERE a.nmdeacao = 'EXEC_CARGA_MANUAL';
-/
- 
-COMMIT;
-/
-insert into crapaca 
-(nmdeacao, 
-nmpackag, 
-nmproced, 
-lstparam, 
-nrseqrdr)
-values 
-('LISTA_DETALHE_CARGAS_PRECDC', 
-'LPRV0001', 
-'pc_lista_hist_cargas_precdc', 
-'pr_cdcooper,pr_idcarga,pr_tppreapr,pr_tpcarga,pr_indsitua,pr_dtlibera,pr_dtliberafim,pr_dtvigencia,pr_dtvigenciafim,pr_skcarga,pr_dscarga,pr_tpretorn,pr_nrregist,pr_nriniseq',
-(SELECT a.nrseqrdr
-     FROM craprdr a
-    WHERE a.nmprogra = 'LPRV0001'
-      AND ROWNUM = 1)
-) ;
-/
-commit;
-/
+  INSERT INTO crapaca
+    (nmdeacao
+    ,nmpackag
+    ,nmproced
+    ,lstparam
+    ,nrseqrdr)
+  VALUES
+    ('LISTA_DETALHE_CARGAS_PRECDC'
+    ,'LPRV0001'
+    ,'pc_lista_hist_cargas_precdc'
+    ,'pr_cdcooper,pr_idcarga,pr_tppreapr,pr_tpcarga,pr_indsitua,pr_dtlibera,pr_dtliberafim,pr_dtvigencia,pr_dtvigenciafim,pr_skcarga,pr_dscarga,pr_tpretorn,pr_nrregist,pr_nriniseq'
+    ,(SELECT a.nrseqrdr
+       FROM craprdr a
+      WHERE a.nmprogra = 'LPRV0001'
+        AND ROWNUM = 1));
 
-insert into tbgen_dominio_campo (NMDOMINIO,CDDOMINIO,DSCODIGO)
-values ('TP_PRE_APROVADO'	,1,	'Pre-Aprovado CDC');
-/
-commit;
-/
+  INSERT INTO tbgen_dominio_campo
+    (NMDOMINIO
+    ,CDDOMINIO
+    ,DSCODIGO)
+  VALUES
+    ('TP_PRE_APROVADO'
+    ,1
+    ,'Pre-Aprovado CDC');
+
+  COMMIT;
+END;
