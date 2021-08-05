ae6a208062bf794f2a8aaa9974a0e1f96d57c885
Joao <amcom-joao.brasil@ailos.coop.br>
2021-08-05 15:16:29 -0300

    ajustar conta 127744 - acentra

--- 2021/08/INC0099061.sql
+++ 2021/08/INC0099061.sql
@@ -6,7 +6,7 @@ BEGIN
    WHERE lem.cdcooper = 5
      AND lem.nrdconta = 127744
      AND lem.nrctremp = 14792
-     AND lem.dtmvtolt = to_date('16/07/2021')
+     AND lem.dtmvtolt = to_date('16/07/2021','dd/mm/rrrr')
      AND lem.vllanmto = 42.83; 
 	 
    COMMIT;
