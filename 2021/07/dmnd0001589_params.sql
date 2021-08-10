e092c434625844f52331396e67efa28ccec3d25f
Andrey Leonardo Bauler <t0033422@ailos.coop.br>
2021-08-10 16:42:48 -0300

    Criação de parametros para definir a visibilidade do menu de assembleias digitais.

---
+++ 2021/07/dmnd0001589_params.sql
@@ -0,0 +1,20 @@
+--Parametro de ativação de assembleias digitais v1.1
+
+INSERT INTO crappat (
+CDPARTAR,
+NMPARTAR,
+TPDEDADO
+) VALUES (
+117,
+'ASSEMBLEIA COM MODELO DE DELEGADOS (S/N)',
+2
+);
+
+
+INSERT INTO crappco (
+cdpartar,
+cdcooper,
+dsconteu
+) select 117,cdcooper,'N' from crapcop;
+
+commit;
