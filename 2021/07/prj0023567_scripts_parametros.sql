b8b1c7fa116fa52703bbf5a147cb44ebee7e992a
Evandro Guaranha(Amcom) <evandro.silva@amcom.com.br>
2021-08-10 15:42:17 -0300

    add novas chamadas crapaca tela cadpre

--- 2021/07/prj0023567_scripts_parametros.sql
+++ 2021/07/prj0023567_scripts_parametros.sql
@@ -28,6 +28,38 @@ BEGIN
        FROM craprdr a
       WHERE a.nmprogra = 'LPRV0001'
         AND ROWNUM = 1));
+        
+  INSERT INTO crapaca
+    (nmdeacao
+    ,nmpackag
+    ,nmproced
+    ,lstparam
+    ,nrseqrdr)
+  VALUES
+    ('CONSULTA_PREAPV_CADPRE'
+    ,'LPRV0001'
+    ,'pc_consulta_preapv_cadpre'
+    ,'pr_inpessoa,pr_tpprodut'
+    ,(SELECT a.nrseqrdr
+       FROM craprdr a
+      WHERE a.nmprogra = 'LPRV0001'
+        AND ROWNUM = 1));  
+        
+  INSERT INTO crapaca
+    (nmdeacao
+    ,nmpackag
+    ,nmproced
+    ,lstparam
+    ,nrseqrdr)
+  VALUES
+    ('ALTERA_PREAPV_CADPRE'
+    ,'LPRV0001'
+    ,'pc_altera_preapv_cadpre'
+    ,'pr_inpessoa,pr_tpprodut,pr_loadpas,pr_loadseg'
+    ,(SELECT a.nrseqrdr
+       FROM craprdr a
+      WHERE a.nmprogra = 'LPRV0001'
+        AND ROWNUM = 1));              
 
   INSERT INTO tbgen_dominio_campo
     (NMDOMINIO
