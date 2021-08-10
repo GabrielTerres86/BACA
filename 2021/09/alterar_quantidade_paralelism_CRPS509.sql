72914d45bb44b253a816321932eeaf2a5e5af4d1
Tiago Cucco <mouts-tiago.cucco@ailos.coop.br>
2021-08-10 14:46:18 -0300

    prb0045612: ajuste update paralelismo

--- 2021/09/alterar_quantidade_paralelism_CRPS509.sql
+++ 2021/09/alterar_quantidade_paralelism_CRPS509.sql
@@ -1,7 +1,7 @@
 -- Altera a quantidade de paralelismo da CRPS509
 
 UPDATE tbgen_batch_param
-   SET QTPARALELO = 10
+   SET QTPARALELO = 07
  WHERE CDPROGRAMA = 'CRPS509'
    AND CDCOOPER = 1;
    
