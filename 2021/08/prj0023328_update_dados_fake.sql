d136b476de91ab690394298be6d86e4d1d5b5fa7
CECRED\f0033505 <filipe.cardozo@ailos.coop.br>
2021-08-10 14:43:35 -0300

    Script para atualizar dado fictício para teste no Sicredi

---
+++ 2021/08/prj0023328_update_dados_fake.sql
@@ -0,0 +1,32 @@
+﻿DECLARE
+  conta NUMBER(11) := 2879107;
+  benefAntigo NUMBER(11) := 1096907019;
+  benefNovo  NUMBER(11) := 202881776;
+
+BEGIN
+
+UPDATE crapcbi c 
+   SET c.nrrecben = benefNovo
+ WHERE c.nrdconta = conta
+   AND c.nrrecben = benefAntigo;
+   
+UPDATE crapdbi d
+   SET d.nrrecben = benefNovo
+ WHERE d.nrrecben = benefAntigo;
+
+UPDATE craplcm l
+   SET l.cdpesqbb = REPLACE(l.cdpesqbb,to_char(benefAntigo),to_char(benefNovo))
+ WHERE l.nrdconta = conta
+   AND TRUNC(l.dtmvtolt) >= TRUNC(SYSDATE - 90);
+   
+UPDATE tbinss_dcb d
+   SET d.nrrecben = benefNovo
+ WHERE d.nrrecben = benefAntigo
+   AND d.nrdconta = conta;
+   
+DELETE FROM tbinss_notif_benef_sicredi ntf
+ WHERE ntf.nrdconta = conta
+   AND ntf.nrrecben = benefAntigo;
+ 
+COMMIT;
+END;
