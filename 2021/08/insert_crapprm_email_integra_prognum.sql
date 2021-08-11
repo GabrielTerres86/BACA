ba5d8f96e567f6cfd9fecbb28bd4e5eaf315189c
Filipe Ribeiro <mouts-filipe.rego@ailos.coop.br>
2021-08-11 12:10:16 -0300

    Script - Update crapaca da tela linha de crédito (Incluir/Alterar)

--- 2021/08/insert_crapprm_email_integra_prognum.sql
+++
@@ -1,8 +0,0 @@
-BEGIN
-  INSERT INTO crapprm(nmsistem,cdcooper,cdacesso,dstexprm,dsvlrprm)
-  VALUES('CRED',0,'EMAIL_INTEGRA_PROGNUM','Email para envio de avisos de erro ao importar arquivo crédtio imobiliário PROGNUM','marjelli.prada@ailos.coop.br');
-  COMMIT;
-EXCEPTION WHEN OTHERS THEN
-  ROLLBACK;
-END;
-/
