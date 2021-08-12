4d02b8bccc0be9b81e671d56f63f907944f1230e
Paulo Monteiro <supero-paulo.monteiro@ailos.coop.br>
2021-08-12 17:57:11 -0300

    Script INC0101377.sql

---
+++ 2021/08/INC0101377.sql
@@ -0,0 +1,82 @@
+begin
+  
+  update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 10251294
+   and nraplica = 21;
+   commit;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 7725531
+   and nraplica = 12;
+   commit;
+
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 7725531
+   and nraplica = 2;
+   commit;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 11557044
+   and nraplica = 8;
+   commit;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 2269384
+   and nraplica = 86;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 10340831
+   and nraplica = 22;
+   commit;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 7759444
+   and nraplica = 28;
+   commit;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 6780539
+   and nraplica = 8;
+   commit;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 1844873
+   and nraplica = 29;
+   commit;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 8894370
+   and nraplica = 54;
+   commit;
+   
+end
