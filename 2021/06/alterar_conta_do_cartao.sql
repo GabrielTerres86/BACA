300d5164c3c70306bd093734c57c0a66c737fbdc
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2021-08-14 09:09:03 -0300

    Ajuste Cartão Dudu

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -11,25 +11,25 @@ BEGIN
   -- Cooperativa de destino do cartão
   vr_cooperativa := 6;
   -- Conta de destino do cartão
-  vr_conta := 248;
+  vr_conta := 101630  ;
 
   -- Numero do cartão que precisamos ajustar
 --  vr_cartao := 5127070162667067; -- Jeff
 --  vr_cartao := 5127070162667935; -- XV
-  vr_cartao := 5127070161674411; -- Luis
+-- vr_cartao := 5127070161674411; -- Luis
 --  vr_cartao := 5158940000000188; -- Matheus(saque & pague)
 --  vr_cartao := 5156010019676523; -- SeP - PF
 --  vr_cartao := 5127070340534221; -- Paty
 --  vr_cartao := 5588190184171591; -- Topaz
+  vr_cartao := 6393500069948041; -- Dudu
   
-  vr_cpf_titular := 00625521986;
+  vr_cpf_titular := 94426406072;
 
   -- Atualizar os dados do cartão
   UPDATE crapcrd card
      SET card.cdcooper = vr_cooperativa
         ,card.nrdconta = vr_conta
         ,card.nrcpftit = vr_cpf_titular
-		,card.cdadmcrd = 15
         ,card.qtsenerr = 0
         ,card.inacetaa = 1
    WHERE card.nrcrcard = vr_cartao;
@@ -38,7 +38,6 @@ BEGIN
      SET card.cdcooper = vr_cooperativa
         ,card.nrdconta = vr_conta
         ,card.nrcpftit = vr_cpf_titular
-		,card.cdadmcrd = 15
    WHERE card.nrcrcard = vr_cartao;
 
   COMMIT;
