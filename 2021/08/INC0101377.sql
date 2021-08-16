3464776cb9b1edf919fe061451f86bc18b1a32d4
Paulo Monteiro <supero-paulo.monteiro@ailos.coop.br>
2021-08-16 10:14:03 -0300

    Script INC0101377.sql

---
+++ 2021/08/INC0101377.sql
@@ -0,0 +1,103 @@
+begin
+  
+  update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 7725531
+   and nraplica = 12;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 2985829
+   and nraplica = 2;
+
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 2659417
+   and nraplica = 73;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 11557044
+   and nraplica = 8;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 10251294
+   and nraplica = 21;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 2269384
+   and nraplica = 86;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 10340831
+   and nraplica = 22;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 7759444
+   and nraplica = 28;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 6780539
+   and nraplica = 8;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 1844873
+   and nraplica = 29;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 6582478
+   and nraplica = 38;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 1
+   and cdprodut = 1007
+   and nrdconta = 8894370	
+   and nraplica = 54;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 11
+   and cdprodut = 1007
+   and nrdconta = 391468	
+   and nraplica = 40;
+   
+update craprac
+   set idsaqtot = 1
+ where cdcooper = 16
+   and cdprodut = 1007
+   and nrdconta = 412520	
+   and nraplica = 19;
+   
+   commit;
+   
+end;
