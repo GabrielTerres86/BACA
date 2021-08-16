481f2539346b835372210ec945632baeda97fe2a
Filipe Ribeiro <mouts-filipe.rego@ailos.coop.br>
2021-08-16 16:40:16 -0300

    Script Integração Arquivo PROGNUM

--- 2021/08/insert_crapprm_email_integra_prognum.sql
+++ 2021/08/insert_crapprm_email_integra_prognum.sql
@@ -1,6 +1,6 @@
 BEGIN
   INSERT INTO crapprm(nmsistem,cdcooper,cdacesso,dstexprm,dsvlrprm)
-  VALUES('CRED',0,'EMAIL_INTEGRA_PROGNUM','Email para envio de avisos de erro ao importar arquivo crédtio imobiliário PROGNUM','marjelli.prada@ailos.coop.br');
+  VALUES('CRED',0,'EMAIL_INTEGRA_PROGNUM','Email para envio de avisos de erro ao importar arquivo crédtio imobiliário PROGNUM','verificar@ailos.coop.br');
   COMMIT;
 EXCEPTION WHEN OTHERS THEN
   ROLLBACK;
