4bd2d870f7f8f4fe5c894023eb437d7fc619403e
Filipe Ribeiro <mouts-filipe.rego@ailos.coop.br>
2021-08-16 17:24:36 -0300

    SCRIPT - Gravar histórico do relatório proposta prestamista

--- 2021/08/prj0023311_insert_caminho_diretorio.sql
+++
@@ -1,8 +0,0 @@
-BEGIN
-  INSERT INTO crapprm(nmsistem,cdcooper,cdacesso,dstexprm,dsvlrprm) VALUES ('CRED',0,'DIR_SCCI_SEGUROS_RECEBE','Diretorio que recebe os arquivos da Prognum','/usr/sistemas/SCCI/Seguros');
-  INSERT INTO crapprm(nmsistem,cdcooper,cdacesso,dstexprm,dsvlrprm) VALUES ('CRED',0,'DIR_SCCI_SEGUROS_RECEBID','Diretorio que possui os arquivos recebidos e processados da Prognum','/usr/sistemas/SCCI');
- COMMIT;
-EXCEPTION WHEN OTHERS THEN
-  ROLLBACK;
-END;
-/
