13e5f046162302f808d91ef6df2e4750633f2d65
Reginaldo Rubens da Silva <t0033608@ailos.coop.br>
2021-08-17 09:05:39 -0300

    Projeto 686 - Correção nos scripts (inclusão do 'commit' faltante)

--- 2021/08/PRJ686-insert-inicial-tbjur_config_termo_adesao.sql
+++ 2021/08/PRJ686-insert-inicial-tbjur_config_termo_adesao.sql
@@ -1,8 +1,6 @@
-prompt Importing table juridico.tbjur_config_termo_adesao...
-set feedback off
-set define off
+BEGIN
+	insert into juridico.tbjur_config_termo_adesao (IDCONFIG_TERMO_ADESAO, TPTERMO_ADESAO, NRVERSAO, NMVIEW, NMDOMINIO)
+	values (1, 1, 1, 'VWIB_TERMO_MULT_CONTAS', 'CECRED');
 
-insert into juridico.tbjur_config_termo_adesao (IDCONFIG_TERMO_ADESAO, TPTERMO_ADESAO, NRVERSAO, NMVIEW, NMDOMINIO)
-values (1, 1, 1, 'VWIB_TERMO_MULT_CONTAS', 'CECRED');
-
-prompt Done.
+	commit;
+END;
--- 2021/08/PRJ686-insert-inicial-tbjur_termos_adesao.sql
+++ 2021/08/PRJ686-insert-inicial-tbjur_termos_adesao.sql
@@ -1,8 +1,6 @@
-prompt Importing table juridico.tbjur_termos_adesao...
-set feedback off
-set define off
+BEGIN
+	insert into juridico.tbjur_termos_adesao (IDTERMO_ADESAO, TPTERMO_ADESAO, NRVERSAO, DSCOMPLETA, DSRESUMIDA)
+	values (3, 1, 1, 'Descricao completa do termo de adesao', 'Descricao resumida do termo de adesao');
 
-insert into juridico.tbjur_termos_adesao (IDTERMO_ADESAO, TPTERMO_ADESAO, NRVERSAO, DSCOMPLETA, DSRESUMIDA)
-values (3, 1, 1, 'Descricao completa do termo de adesao', 'Descricao resumida do termo de adesao');
-
-prompt Done.
+	commit;
+END;
