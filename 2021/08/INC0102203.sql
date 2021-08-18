2f94e4e29e51e9f13919edf71ac9d7df9c71f5c6
Paulo Monteiro <supero-paulo.monteiro@ailos.coop.br>
2021-08-18 10:03:30 -0300

    Atualização de Script INC0102203.sql

--- 2021/08/INC0102203.sql
+++ 2021/08/INC0102203.sql
@@ -13,10 +13,16 @@ begin
      and nrdconta = 9324712;
 
   update craplac
-     set nrdocmto = 1474
+     set nrdocmto = 1474, nraplica = 27
    where cdcooper = 1
      and nrdconta = 9324712
      and nrseqdig = 1474;
+
+  update crapsqu
+     set nrseqatu = 28
+   where nmdcampo = 'NRAPLICA'
+     and nmtabela = 'CRAPRAC'
+     and dsdchave = '1;9324712';
 
   commit;
 
