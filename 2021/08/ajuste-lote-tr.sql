ce3e7cd87c075aa80547a8733bcbb3e6e436802e
Miguel Rodrigues <miguel.rodrigues@ailos.coop.br>
2021-08-19 15:01:53 -0300

    Ajuste no lote de TR

--- 2021/08/ajuste-lote-tr.sql
+++ 2021/08/ajuste-lote-tr.sql
@@ -1,7 +1,9 @@
 BEGIN
   UPDATE craplot l 
-     SET l.vlcompdb = l.vlinfodb
-        ,l.qtcompln = l.qtinfoln
+     SET l.vlcompcr = 0
+        ,l.vlinfocr = 0
+        ,l.qtcompln = 6
+        ,l.qtinfoln = 6 
    WHERE l.cdcooper = 3 
      AND l.dtmvtolt = to_date('19/08/2021','dd/mm/yyyy') 
      AND l.nrdolote = 4110;
