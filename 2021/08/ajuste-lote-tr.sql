adbbbbda134ffe61abc04701f786a475ecdebbbb
Miguel Rodrigues <miguel.rodrigues@ailos.coop.br>
2021-08-19 14:19:41 -0300

    Ajuste lote para execucao no baca

---
+++ 2021/08/ajuste-lote-tr.sql
@@ -0,0 +1,11 @@
+BEGIN
+  UPDATE craplot l 
+     SET l.vlcompdb = l.vlinfodb
+   WHERE l.cdcooper = 3 
+     AND l.dtmvtolt = to_date('19/08/2021','dd/mm/yyyy') 
+     AND l.nrdolote = 4110;
+  COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+END;
