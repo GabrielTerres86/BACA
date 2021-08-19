77f5dababeaca4180c9f0a4d61bf347749af1021
Miguel Rodrigues <miguel.rodrigues@ailos.coop.br>
2021-08-19 14:35:43 -0300

    ajuste nos lancamentos para fechamento do lote

--- 2021/08/ajuste-lote-tr.sql
+++ 2021/08/ajuste-lote-tr.sql
@@ -1,6 +1,7 @@
 BEGIN
   UPDATE craplot l 
      SET l.vlcompdb = l.vlinfodb
+        ,l.qtcompln = l.qtinfoln
    WHERE l.cdcooper = 3 
      AND l.dtmvtolt = to_date('19/08/2021','dd/mm/yyyy') 
      AND l.nrdolote = 4110;
