1908b5ba559a9ffa09c2e70da797e7929af29852
Paulo Felipe Kowalsky <t0032603@cecred.coop.br>
2021-08-19 09:12:35 -0300

    scritp de cancelamento de folhas ed cheque

--- 2021/08/Update_crapfdc_RITM0160894.sql
+++ 2021/08/Update_crapfdc_RITM0160894.sql
@@ -1,7 +1,10 @@
 begin
   update crapfdc fdc
-    set fdc.nrctatic = 0, fdc.cdbantic = 0, fdc.cdagetic = 0, 
-        fdc.incheque = 8, fdc.dtretchq = trunc(sysdate), fdc.dtliqchq = trunc(sysdate)
+    set fdc.nrctatic = 0, 
+        fdc.cdbantic = 0, 
+        fdc.cdagetic = 0, 
+        fdc.incheque = 8,
+        fdc.dtliqchq = trunc(sysdate)
   where  fdc.cdcooper = 16
     and  fdc.nrdconta = 818798
     and  fdc.nrcheque in (5,7,17,18);
