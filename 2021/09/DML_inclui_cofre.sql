7c53b7f6e5702913d9ba64da1b149c0d573b9b85
Odirlei Busana <odirlei.busana@ailos.coop.br>
2021-08-20 10:42:06 -0300

    Incluir cofre no ambiente de teste

---
+++ 2021/09/DML_inclui_cofre.sql
@@ -0,0 +1,34 @@
+BEGIN
+  INSERT INTO tbged_assina_cofre
+    (CDUUID
+    ,NMCOFRE
+    ,DSWHOOK
+    ,CDCOFRE
+    ,CDCOOPER)
+  VALUES
+    ('1b03fd03-a103-483d-bb92-b31d1d4fc1a7'
+    ,'Termo de acordo'
+    ,NULL
+    ,4
+    ,1);
+
+  INSERT INTO tbged_assina_conf
+    (CDTOKEN
+    ,CDKEY
+    ,CDCOOPER
+    ,CDCONFIG
+    ,CDHMAC
+    ,CDUUIDCOF
+    ,CDTPDOC)
+  VALUES
+    ('live_2e6c764f1e9ad1feb0b9d07169f41237980d20f971dec2733cfb160f8c4cb3bd'
+    ,'live_crypt_qNJETLawMkjGIuUZu44BXEJYsy5csA4Z'
+    ,1
+    ,2
+    ,'76092e80629647b1fb54a8432ffdbf28974849e02008149450a79a7bc21b3381'
+    ,'1b03fd03-a103-483d-bb92-b31d1d4fc1a7'
+    ,283);
+
+  COMMIT;
+
+END;
