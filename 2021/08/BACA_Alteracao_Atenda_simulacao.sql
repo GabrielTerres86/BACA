f3c9377c92b52da2a30c27143c2ad9d64b24b693
Anderson Felix <mouts-anderson.felix@ailos.coop.br>
2021-08-25 17:24:40 -0300

    EXCEPTION ROLLBACK

--- 2021/08/BACA_Alteracao_Atenda_simulacao.sql
+++ 2021/08/BACA_Alteracao_Atenda_simulacao.sql
@@ -1,7 +1,9 @@
 BEGIN
-  
     insert into crapaca (nrseqaca, nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
     values ((select max(nrseqaca)+1 from crapaca), 'CARREGA_DATAS_PAGTO', 'TELA_ATENDA_SIMULACAO', 'pc_retorna_dtvenc', 'pr_nrdconta,pr_cdcooper', (SELECT   nrseqrdr  from craprdr  WHERE nmprogra= 'TELA_ATENDA_SIMULACAO'));
 
     COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+    ROLLBACK; 
 END;
