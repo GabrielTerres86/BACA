14b14e04c541f3c9c372bf4bdea9837e03619625
Anderson Felix <mouts-anderson.felix@ailos.coop.br>
2021-08-25 17:20:47 -0300

    baca de atenda_simulacao

---
+++ 2021/08/BACA_Alteracao_Atenda_simulacao.sql
@@ -0,0 +1,7 @@
+BEGIN
+  
+    insert into crapaca (nrseqaca, nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
+    values ((select max(nrseqaca)+1 from crapaca), 'CARREGA_DATAS_PAGTO', 'TELA_ATENDA_SIMULACAO', 'pc_retorna_dtvenc', 'pr_nrdconta,pr_cdcooper', (SELECT   nrseqrdr  from craprdr  WHERE nmprogra= 'TELA_ATENDA_SIMULACAO'));
+
+    COMMIT;
+END;
