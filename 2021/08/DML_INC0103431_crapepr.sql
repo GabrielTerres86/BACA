11365ce9fb926dcd339dbe9bdd5d1d1b2c01854c
Odirlei Busana <odirlei.busana@ailos.coop.br>
2021-08-26 12:02:24 -0300

    INC0103431 - Ajuste no emprestimo na data da referencia de calculo de juros - Credcorreios.

---
+++ 2021/08/DML_INC0103431_crapepr.sql
@@ -0,0 +1,18 @@
+
+BEGIN
+  UPDATE crapepr epr
+     SET epr.diarefju = '28',
+         epr.mesrefju = '07',
+         epr.anorefju = '2021',
+         epr.dtrefjur = to_date('28/07/2021','DD/MM/RRRR')
+   WHERE EXISTS (SELECT 1
+                   FROM crapass a
+                  WHERE a.cdagenci = 28
+                   AND a.cdcooper = epr.cdcooper
+                   AND a.nrdconta = epr.nrdconta 
+                   AND a.cdcooper = 9)
+     AND ((epr.inprejuz = 1 AND epr.vlsdprej > 0 ) OR
+           epr.inliquid = 0);             
+    
+  COMMIT;           
+END;         
