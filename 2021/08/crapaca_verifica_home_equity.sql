f568d316503b9c0e70b961714ea1e3c1e07c670a
Guilherme Pinheiro <guilherme.pinheiro@mouts.com.br>
2021-08-27 18:15:32 -0300

    CRAPACA - mensagem no EXCEPTION

--- 2021/08/crapaca_verifica_home_equity.sql
+++ 2021/08/crapaca_verifica_home_equity.sql
@@ -5,14 +5,15 @@ BEGIN
                      ,lstparam
                      ,nrseqrdr
                      ) 
-               VALUES('VERIFICA_HOME_EQUITY'
+               VALUES('VALIDA_HOME_EQUITY'
                      ,NULL
-                     ,'credito.verificarHomeEquity'
+                     ,'CREDITO.validarHomeEquity'
                      ,'pr_cdlcremp,pr_flgtpimovel,pr_dsclassi,pr_dscatbem'
                      ,1045
                      );
   COMMIT;
 EXCEPTION
   WHEN OTHERS THEN
+    RAISE_application_error(-20500,SQLERRM);
     ROLLBACK;
 END;
