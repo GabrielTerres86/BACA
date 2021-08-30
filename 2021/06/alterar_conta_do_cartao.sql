14ee8fa73cec4f48ac8c69058a098ba0ca3c8129
Djonata Wehmuth <djonata.wehmuth@ailos.coop.br>
2021-08-30 15:19:59 -0300

    QA - Conta Unilos

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -8,14 +8,14 @@ DECLARE
 BEGIN
 
   -- Cooperativa de destino do cartão
-  vr_cooperativa := 11;
+  vr_cooperativa := 6;
   -- Conta de destino do cartão
-  vr_conta := 181528;
+  vr_conta := 5320;
 
   -- Numero do cartão que precisamos ajustar
 --  vr_cartao := 5127070162667067; -- Jeff
 --  vr_cartao := 5127070162667935; -- XV
---  vr_cartao := 5127070161674411; -- Luis
+  vr_cartao := 5127070161674411; -- Luis
 --  vr_cartao := 5127070320149719; -- Luis 2
 --  vr_cartao := 5158940000000188; -- Matheus(saque & pague)
 --  vr_cartao := 5156010019676523; -- SeP - PF
@@ -23,7 +23,7 @@ BEGIN
 --  vr_cartao := 6393500069948041; -- Dudu
 --  vr_cartao := 5161620000587872; -- Djonata
 --  vr_cartao := 5127070162666663; -- Matheus barros
-vr_cartao := 5588190184171591; -- Topaz
+--  vr_cartao := 5588190184171591; -- Topaz
   
   -- Verificar se a conta possui algum outro cartão para buscar o CPF do Titular
   FOR cartao IN (select distinct a.nrcpftit
