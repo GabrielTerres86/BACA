a97e782ac77a9c65b5508ca54565a71868256d67
CECRED\f0033505 <filipe.cardozo@ailos.coop.br>
2021-08-31 15:06:31 -0300

    Alterando conta para teste INSS

--- 2021/08/taa_cartao_apontamento.sql
+++ 2021/08/taa_cartao_apontamento.sql
@@ -11,7 +11,7 @@ begin
   -- Cooperativa de destino do cartão
   vr_cooperativa := 1;
   -- Conta de destino do cartão
-  vr_conta := 396;
+  vr_conta := 90264878;
 
   -- Numero do cartão que precisamos ajustar
   vr_cartao := 5161620000264183; -- Luis
