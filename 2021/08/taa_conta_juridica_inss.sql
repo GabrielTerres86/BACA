908fec9f9682044da56f686829e3d385f3aee7ce
CECRED\f0033505 <filipe.cardozo@ailos.coop.br>
2021-08-31 16:15:04 -0300

    Alterando conta para jurídica teste INSS

--- 2021/08/taa_conta_juridica_inss.sql
+++ 2021/08/taa_conta_juridica_inss.sql
@@ -1,15 +1,15 @@
 BEGIN
  UPDATE crapcrd
     SET crapcrd.cdcooper = 1
-    ,crapcrd.nrdconta = 191
-    ,crapcrd.nrcpftit = 00407195963
+    ,crapcrd.nrdconta = 80107052
+    ,crapcrd.nrcpftit = 00620541989
     ,cdadmcrd = 15
     WHERE crapcrd.nrcrcard = 5161620000264183;
 
     UPDATE crawcrd
     SET crawcrd.cdcooper = 1
-    ,crawcrd.nrdconta = 191
-    ,crawcrd.nrcpftit = 00407195963
+    ,crawcrd.nrdconta = 80107052
+    ,crawcrd.nrcpftit = 00620541989
     ,cdadmcrd = 15
     WHERE crawcrd.nrcrcard = 5161620000264183;
 
