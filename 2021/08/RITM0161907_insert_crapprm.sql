dbef99200f82490b7e2f044c4ebec354e350ed3a
Evandro Augusto Guaranha da Silva (Amcom) <t0032597@ailos.coop.br>
2021-09-01 15:16:37 +0000

    Ajuste Owner Updated RITM0161907_insert_crapprm.sql

--- 2021/08/RITM0161907_insert_crapprm.sql
+++ 2021/08/RITM0161907_insert_crapprm.sql
@@ -1,5 +1,5 @@
 BEGIN
-  INSERT INTO crapprm
+  INSERT INTO CECRED.crapprm
     (NMSISTEM
     ,CDCOOPER
     ,CDACESSO
@@ -9,7 +9,7 @@ BEGIN
     ('CRED'
     ,0
     ,'TAXA_MAX_LIM_CHQ_ESP'
-    ,'Taxa m·xima para linhas de limite de crÈdito (cheque especial) PF e PJ'
+    ,'Taxa m√°xima para linhas de limite de cr√©dito (cheque especial) PF e PJ'
     ,8);
 
   COMMIT;
