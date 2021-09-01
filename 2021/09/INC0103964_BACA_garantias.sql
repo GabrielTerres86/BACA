5437dab05d3fc9fc7654a4959d999632f7b0a6f4
Luiz Otávio Olinger Momm <luiz.momm@ailos.coop.br>
2021-09-01 16:38:17 -0300

    Ajustado garantias para todos empréstimos da Credcorreios

---
+++ 2021/09/INC0103964_BACA_garantias.sql
@@ -0,0 +1,17 @@
+update crapprp p
+   set nrgarope = 10
+ where p.cdcooper = 9
+   and p.tpctrato = 90
+   and NVL(p.nrgarope,0) = 0
+   and (p.cdcooper, p.nrdconta, p.nrctrato)
+     in (select e.cdcooper
+              , e.nrdconta
+              , e.nrctremp
+           from crapepr e
+              , crapass a
+          where e.cdcooper = a.cdcooper
+            and e.nrdconta = a.nrdconta
+            and a.cdcooper = 9
+            and a.cdagenci = 28);
+
+COMMIT;
