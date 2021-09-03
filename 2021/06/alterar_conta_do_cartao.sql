9a1df18a0ab715bf740b72cd8a6adf6bd0ce11cf
Djonata Wehmuth <djonata.wehmuth@ailos.coop.br>
2021-09-03 16:05:11 -0300

    Update alterar_conta_do_cartao.sql

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -25,39 +25,6 @@ BEGIN
 --  vr_cartao := 5588190184171591; -- Topaz
 --  vr_cartao := 5161620000264183; -- Poliane 
 
-  
-  -- Verificar se a conta possui algum outro cartão para buscar o CPF do Titular
-  FOR cartao IN (select distinct a.nrcpftit
-                  from crawcrd a
-                 where a.cdcooper = vr_cooperativa
-                   and a.nrdconta = vr_conta
-                   and a.insitcrd = 4 -- Apenas cartão ativo
-                 ) LOOP
-    -- carregar o CPF da conta
-    vr_cpf_titular := cartao.nrcpftit;
-
-  END LOOP;
-  
-  IF vr_cpf_titular IS NULL THEN
-  -- Se não tem CPF tem que buscar o CPF do titular da conta
-    FOR titular IN (select a.nrcpfcgc
-                      from crapttl a
-                     where a.cdcooper = vr_cooperativa
-                       and a.nrdconta = vr_conta
-                       and a.idseqttl = 1 -- CPF do primeiro titular do cartão
-                   ) LOOP
-      -- carregar o CPF da conta
-      vr_cpf_titular := titular.nrcpfcgc;
-
-    END LOOP;
-
-  END IF;
-
-  IF vr_cpf_titular IS NULL THEN
-    -- Se não tem CPF para o processo
-    RETURN;
-  END IF;
-
   -- Atualizar os dados do cartão
   UPDATE crapcrd card
      SET card.cdcooper = vr_cooperativa
@@ -65,12 +32,14 @@ BEGIN
         ,card.nrcpftit = vr_cpf_titular
         ,card.qtsenerr = 0
         ,card.inacetaa = 1
+        ,card.cdadmcrd = 12
    WHERE card.nrcrcard = vr_cartao;
 
   UPDATE crawcrd card
      SET card.cdcooper = vr_cooperativa
         ,card.nrdconta = vr_conta
         ,card.nrcpftit = vr_cpf_titular
+        ,card.cdadmcrd = 12
    WHERE card.nrcrcard = vr_cartao;
 
 
