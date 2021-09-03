4ee0619cf4a50b2deb62b6a99fc4b820f30ecc42
Djonata Wehmuth <djonata.wehmuth@ailos.coop.br>
2021-09-03 09:55:27 -0300

    Update alterar_conta_do_cartao.sql

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -16,7 +16,7 @@ BEGIN
 --  vr_cartao := 5127070162667067; -- Jeff
 --  vr_cartao := 5127070162667935; -- XV
 --  vr_cartao := 5127070161674411; -- Luis
---  vr_cartao := 5127070320149719; -- Luis 2
+  vr_cartao := 5127070320149719; -- Luis 2
 --  vr_cartao := 5158940000000188; -- Matheus(saque & pague)
 --  vr_cartao := 5156010019676523; -- SeP - PF
 --  vr_cartao := 5127070340534221; -- Paty
