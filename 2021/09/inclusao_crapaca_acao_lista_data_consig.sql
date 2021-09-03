7f396ed84c5316a370787f37e110ceb5dffab375
Leandro da Silva <leandro.s@cecred.coop.br>
2021-09-03 09:48:57 -0300

    Ajuste do script de criacao da acao de busca de datas de pagamentos do consig.

--- 2021/09/inclusao_crapaca_acao_lista_data_consig.sql
+++ 2021/09/inclusao_crapaca_acao_lista_data_consig.sql
@@ -1,6 +1,6 @@
 BEGIN
-    insert into crapaca (nrseqaca, nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
-    values ((select max(nrseqaca)+1 from crapaca), 'CARREGA_DATAS_PAGTO_CONSIG', 'TELA_ATENDA_SIMULACAO', 'pc_retorna_dtvenc_consig', 'pr_nrdconta,pr_cdcooper,pr_diascare', (SELECT   nrseqrdr  from craprdr  WHERE nmprogra= 'TELA_ATENDA_SIMULACAO'));
+    insert into crapaca (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
+    values ('CARREGA_DATAS_PAGTO_CONSIG', 'TELA_ATENDA_SIMULACAO', 'pc_retorna_dtvenc_consig', 'pr_nrdconta,pr_cdcooper,pr_diascare', (SELECT   nrseqrdr  from craprdr  WHERE nmprogra= 'TELA_ATENDA_SIMULACAO'));
     COMMIT;
 EXCEPTION
     WHEN OTHERS THEN
