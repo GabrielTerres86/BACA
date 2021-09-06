ae364694aa222e69c50040d67cc5f0e516ca2152
Paulo Felipe Kowalsky <t0032603@cecred.coop.br>
2021-09-06 14:57:20 -0300

    ajuste no script de saldos

--- 2021/09/delete-crapsdaCompesacao_06_09.sql
+++ 2021/09/delete-crapsdaCompesacao_06_09.sql
@@ -1,6 +1,6 @@
 begin
   delete crapsda
   where  cdcooper = 8
-    and  dtmvtolt between '08/08/2021' and '31/08/2021';
+    and  dtmvtolt between to_date('08/08/2021','DD/MM/YYYY') and to_date('31/08/2021','DD/MM/YYYY');
 commit;
 end;
