ebb9da3656dfd67c4a56c58a926bb90ac7a726cd
Daniel <daniel.lombardi@mouts.info>
2021-09-08 13:13:21 -0300

    Script parametros PARHPG erro 500.

--- 2021/09/update_crapaca_altera_horario_parhpg.sql
+++ 2021/09/update_crapaca_altera_horario_parhpg.sql
@@ -1,5 +1,5 @@
-UPDATE crapaca aca
-SET aca.lstparam = 'pr_cddopcao,pr_cdcooper,pr_hrsicatu,pr_hrsicini,pr_hrsicfim,pr_hrbanatu,pr_hrbanini,pr_hrbanfim,pr_hrtitatu,pr_hrtitini,pr_hrtitfim,pr_hrnetatu,pr_hrnetini,pr_hrnetfim,pr_hrtaaatu,pr_hrtaaini,pr_hrtaafim,pr_hrgpsatu,pr_hrgpsini,pr_hrgpsfim,pr_hrsiccau,pr_hrsiccan,pr_hrbancau,pr_hrbancan,pr_hrtitcau,pr_hrtitcan,pr_hrnetcau,pr_hrnetcan,pr_hrtaacau,pr_hrtaacan,pr_hrdiuatu,pr_hrdiuini,pr_hrdiufim,pr_hrnotatu,pr_hrnotini,pr_hrnotfim,pr_hrlimreprocess,pr_hrlimite'
-WHERE aca.nmdeacao = 'ALTERA_HORARIO_PARHPG';
+UPDATE crapaca paca
+SET paca.lstparam = 'pr_cddopcao,pr_cdcooper,pr_hrsicatu,pr_hrsicini,pr_hrsicfim,pr_hrbanatu,pr_hrbanini,pr_hrbanfim,pr_hrtitatu,pr_hrtitini,pr_hrtitfim,pr_hrnetatu,pr_hrnetini,pr_hrnetfim,pr_hrtaaatu,pr_hrtaaini,pr_hrtaafim,pr_hrgpsatu,pr_hrgpsini,pr_hrgpsfim,pr_hrsiccau,pr_hrsiccan,pr_hrbancau,pr_hrbancan,pr_hrtitcau,pr_hrtitcan,pr_hrnetcau,pr_hrnetcan,pr_hrtaacau,pr_hrtaacan,pr_hrdiuatu,pr_hrdiuini,pr_hrdiufim,pr_hrnotatu,pr_hrnotini,pr_hrnotfim,pr_hrlimreprocess,pr_hrlimite'
+WHERE paca.nmdeacao = 'ALTERA_HORARIO_PARHPG';
 
 COMMIT;
