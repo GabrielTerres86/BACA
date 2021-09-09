130e14328d42e3a168b21b2e03b5e191d1fbb87b
Rafael Innig <t0033292@ailos.coop.br>
2021-09-09 16:41:31 -0300

    Bloqueio Renegociação facilitada Pronampe - Cadastro CADPAR linhas de crédito bloqueadas.

---
+++ 2021/09/RITM0161758_cadpar.sql
@@ -0,0 +1,68 @@
+DECLARE
+  CURSOR cr_crapbat(pr_cdbattar IN crapbat.cdbattar%TYPE) IS
+    SELECT cdcadast
+      FROM crapbat
+     WHERE cdbattar = pr_cdbattar;
+  rw_crapbat cr_crapbat%ROWTYPE;
+  aux_cdpartar_add  crappat.cdpartar%TYPE;
+  aux_cdpartar_del  crappat.cdpartar%TYPE;
+BEGIN
+  --
+  OPEN cr_crapbat('LCR_BLQ_RENEG');
+  FETCH cr_crapbat INTO rw_crapbat;
+  CLOSE cr_crapbat;
+  --
+  IF rw_crapbat.cdcadast IS NOT NULL THEN
+    aux_cdpartar_del := rw_crapbat.cdcadast;
+    DELETE FROM crapbat WHERE cdcadast = aux_cdpartar_del;
+    DELETE FROM crappat WHERE cdpartar = aux_cdpartar_del;
+    DELETE FROM crappco WHERE cdpartar = aux_cdpartar_del;
+  END IF;
+  --
+  COMMIT;
+  --
+  SELECT MAX(cdpartar) INTO aux_cdpartar_add FROM crappat;
+  --
+  aux_cdpartar_add :=  aux_cdpartar_add + 1;
+  --
+  insert into crappat (CDPARTAR, NMPARTAR, TPDEDADO, CDPRODUT)
+  values (aux_cdpartar_add, 'Renegociação Facilitada - Bloqueio das operações de PRONAMPE para as Linhas de crédito do parâmetro(Separadas por vírgula).', 2, 12);
+  insert into crapbat (CDBATTAR, NMIDENTI, CDPROGRA, TPCADAST, CDCADAST)
+  values ('LCR_BLQ_RENEG', 'Renegociação Facilitada - Bloqueio das operações de PRONAMPE para as Linhas de crédito do parâmetro(Separadas por vírgula).', ' ', 2, aux_cdpartar_add);
+  --        
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 17, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 1,  '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 15, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 14, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 13, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 11, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 10, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 9, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 8, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 7, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 6, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 5, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 4, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 3, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 2, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 12, '2600,4600,5600');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 16, '2600,4600,5600');
+  COMMIT;
+END;
