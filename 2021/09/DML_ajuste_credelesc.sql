48f2fe3dac037def8328c9bbfe95e487ad352d02
Odirlei Busana <odirlei.busana@ailos.coop.br>
2021-09-13 09:06:44 -0300

    Ajustes credelesc

--- 2021/09/DML_ajuste_credelesc.sql
+++ 2021/09/DML_ajuste_credelesc.sql
@@ -1,22 +1,8 @@
 BEGIN
-  UPDATE craplem s
-     SET s.dtmvtolt = to_date('10/08/2021', 'DD/MM/RRRR')
-   WHERE s.cdcooper = 8
-     AND s.dtmvtolt = to_date('25/08/2021', 'DD/MM/RRRR');
-
-  UPDATE craplcm s
-     SET s.dtmvtolt = to_date('10/08/2021', 'DD/MM/RRRR') ,
-         s.nrseqdig = nrseqdig + 10
-   WHERE s.cdcooper = 8
-     AND s.dtmvtolt = to_date('25/08/2021', 'DD/MM/RRRR');
-
-  DELETE crapris s
+  
+  DELETE crapvri s
    WHERE s.cdcooper = 8
      AND s.dtrefere > to_date('09/08/2021', 'DD/MM/RRRR');
-
-  DELETE crapsda s
-   WHERE s.cdcooper = 8
-     AND s.dtmvtolt > to_date('09/08/2021', 'DD/MM/RRRR');   
   
   COMMIT;
      
