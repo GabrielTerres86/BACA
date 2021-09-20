941d7e8ad02e5f1dcd54cae36c4f2a307d9f3b86
Evandro Guaranha(Amcom) <evandro.silva@amcom.com.br>
2021-09-20 15:35:09 -0300

    removendo comments

--- 2021/09/INC0102207_script_ajuste_parcelas.sql
+++ 2021/09/INC0102207_script_ajuste_parcelas.sql
@@ -1,7 +1,6 @@
--- INC0102207 - Consignado
 BEGIN
   UPDATE tbepr_consignado_pagamento
-     SET instatus = 2 --processado
+     SET instatus = 2
    WHERE cdcooper = 13
      AND nrdconta = 252514
      AND nrctremp = 72630
@@ -9,18 +8,17 @@ BEGIN
      AND idintegracao IS NULL;
 
   UPDATE tbepr_consignado_pagamento
-     SET instatus = 2 --processado
+     SET instatus = 2
    WHERE cdcooper = 13
      AND nrdconta = 66877
      AND nrctremp = 91959
      AND nrparepr IN (9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36)
      AND idintegracao IS NULL;
-     
-     
+          
   UPDATE crappep
      SET dtultpag = '13/07/2021'
-        ,vlsdvatu = 0 -- saldo devedor atualizado da parcela
-        ,inliquid = 1 -- liquidado                            
+        ,vlsdvatu = 0
+        ,inliquid = 1                      
    WHERE cdcooper = 13
      AND nrdconta = 252514
      AND nrctremp = 72630
@@ -28,15 +26,13 @@ BEGIN
 
   UPDATE crappep
      SET dtultpag = '16/09/2021'
-        ,vlsdvatu = 0 -- saldo devedor atualizado da parcela
-        ,inliquid = 1 -- liquidado                            
+        ,vlsdvatu = 0
+        ,inliquid = 1                        
    WHERE cdcooper = 13
      AND nrdconta = 66877
      AND nrctremp = 91959
      AND inliquid = 0;
 
-
-  -- Cadastro de emprestimos
   UPDATE crapepr 
      SET inliquid = 1
         ,vlsdeved = 0
