3a6b5e56dd80dce5d151dc92d2e1ee50928955b2
JONATHAN CANI KUNTZ <jonathan.kuntz@ailos.coop.br>
2021-09-20 12:20:05 -0300

    Retirada do contrato da fila do programa crps784

---
+++ 2021/09/INC0106295.sql
@@ -0,0 +1,15 @@
+begin
+  update crapepr
+  set cdcooper = 99
+  where nrctremp = 21200155 
+  and nrdconta = 502464
+  and cdcooper = 9;
+        
+  update crawepr 
+  set cdcooper = 99
+  where nrctremp = 21200155
+  and nrdconta = 502464
+  and cdcooper = 9;
+  
+  commit;
+end;
