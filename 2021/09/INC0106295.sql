ffe1ddeecac048ef4e85918cb73c256fba40f224
JONATHAN CANI KUNTZ <jonathan.kuntz@ailos.coop.br>
2021-09-20 12:30:57 -0300

    Ajuste contrato 21200155

--- 2021/09/INC0106295.sql
+++ 2021/09/INC0106295.sql
@@ -1,15 +1,9 @@
 begin
-  update crapepr
-  set cdcooper = 99
-  where nrctremp = 21200155 
-  and nrdconta = 502464
-  and cdcooper = 9;
-        
-  update crawepr 
-  set cdcooper = 99
-  where nrctremp = 21200155
-  and nrdconta = 502464
-  and cdcooper = 9;
-  
-  commit;
+    update crapepr
+    set CDORIGEM = 4
+    where nrctremp = 21200155
+    and nrdconta = 502464
+    and cdcooper = 9;
+
+    commit;
 end;
