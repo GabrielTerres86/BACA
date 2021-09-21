bf320e6d410337a86bccbe0a1aa2c9ebd68dc28b
Douglas <douglas.kloppel@ailos.coop.br>
2021-09-21 15:23:10 -0300

    correção no script, retirando o progress_recid

--- 2021/09/inc0105973-insert-crapttx.sql
+++ 2021/09/inc0105973-insert-crapttx.sql
@@ -1,6 +1,6 @@
 BEGIN
 
-  insert into crapttx (CDCOOPER, TPTAXRDC, CDPERAPL, QTDIAINI, QTDIAFIM, QTDIACAR, PROGRESS_RECID)
-               values (2, 7, 1, 90, 91, 0, null);
+  insert into crapttx (CDCOOPER, TPTAXRDC, CDPERAPL, QTDIAINI, QTDIAFIM, QTDIACAR)
+               values (2, 7, 1, 90, 91, 0);
   commit;
 END;
