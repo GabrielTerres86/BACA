7ef9ac537dca7631cd6892964e58f90edb5548d0
Darlei Fernando Zillmer <darlei.zillmer@supero.com.br>
2021-09-22 16:42:15 -0300

    Adicao de to_date

--- 2021/07/INC0093328.sql
+++ 2021/07/INC0093328.sql
@@ -1,10 +1,10 @@
 BEGIN
-  UPDATE tbepr_renegociacao_craplem SET nrversao = 1 WHERE cdcooper = 1 AND nrdconta = 10819061 AND cdhistor = 2304 AND dtmvtolt = '03/05/2021' AND nrversao = 2;
-  UPDATE tbepr_renegociacao_craplem SET nrversao = 1 WHERE cdcooper = 1 AND nrdconta = 6862462  AND cdhistor = 2304 AND dtmvtolt = '05/05/2021' AND nrversao = 2;
-  UPDATE tbepr_renegociacao_craplem SET nrversao = 1 WHERE cdcooper = 1 AND nrdconta = 10484485 AND cdhistor = 2304 AND dtmvtolt = '07/05/2021' AND nrversao = 2;
-  UPDATE tbepr_renegociacao_craplem SET nrversao = 1 WHERE cdcooper = 1 AND nrdconta = 7327668  AND cdhistor = 2304 AND dtmvtolt = '13/05/2021' AND nrversao = 2;
-  UPDATE tbepr_renegociacao_craplem SET nrversao = 1 WHERE cdcooper = 1 AND nrdconta = 7835612  AND cdhistor = 2304 AND dtmvtolt = '20/05/2021' AND nrversao = 2;
-  DELETE FROM tbepr_renegociacao_craplem WHERE cdcooper = 1 AND cdhistor = 1036 AND dtmvtolt = '12/05/2021' AND nrdconta = 11164140;
+  UPDATE tbepr_renegociacao_craplem SET nrversao = 1 WHERE cdcooper = 1 AND nrdconta = 10819061 AND cdhistor = 2304 AND dtmvtolt = TO_DATE('03/05/2021', 'DD/MM/RRRR') AND nrversao = 2;
+  UPDATE tbepr_renegociacao_craplem SET nrversao = 1 WHERE cdcooper = 1 AND nrdconta = 6862462  AND cdhistor = 2304 AND dtmvtolt = TO_DATE('05/05/2021', 'DD/MM/RRRR') AND nrversao = 2;
+  UPDATE tbepr_renegociacao_craplem SET nrversao = 1 WHERE cdcooper = 1 AND nrdconta = 10484485 AND cdhistor = 2304 AND dtmvtolt = TO_DATE('07/05/2021', 'DD/MM/RRRR') AND nrversao = 2;
+  UPDATE tbepr_renegociacao_craplem SET nrversao = 1 WHERE cdcooper = 1 AND nrdconta = 7327668  AND cdhistor = 2304 AND dtmvtolt = TO_DATE('13/05/2021', 'DD/MM/RRRR') AND nrversao = 2;
+  UPDATE tbepr_renegociacao_craplem SET nrversao = 1 WHERE cdcooper = 1 AND nrdconta = 7835612  AND cdhistor = 2304 AND dtmvtolt = TO_DATE('20/05/2021', 'DD/MM/RRRR') AND nrversao = 2;
+  DELETE FROM tbepr_renegociacao_craplem WHERE cdcooper = 1 AND cdhistor = 1036 AND dtmvtolt = TO_DATE('12/05/2021', 'DD/MM/RRRR') AND nrdconta = 11164140;
 
   COMMIT;
 END;
