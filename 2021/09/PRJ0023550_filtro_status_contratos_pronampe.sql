6281a0b8de1c0d8f7f0669d05883b25272046941
Thompson Dantas <supero-thompson.dantas@ailos.coop.br>
2021-09-23 10:55:47 -0300

    PRJ0023550 Filtro Status Honra Pronampe

---
+++ 2021/09/PRJ0023550_filtro_status_contratos_pronampe.sql
@@ -0,0 +1,13 @@
+--238065 Opcoes na coluna Status da Honra - filtro
+BEGIN
+
+  UPDATE crapaca
+     SET lstparam = 'pr_cdcooper,pr_nrdconta,pr_nrcontrato,pr_nriniseq,pr_nrregist,pr_datrini,pr_datrfim,pr_sthonra'
+   WHERE UPPER(nmdeacao) = 'CONSULTA_CONTRATOS';
+
+  COMMIT;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+END;
