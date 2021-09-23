b2a8de9dce4ad3b0a60e45a66e2b18decc5fd2fa
JONATHAN CANI KUNTZ <jonathan.kuntz@ailos.coop.br>
2021-09-23 11:09:47 -0300

    Atualizacao script, contrato 91959 ja liquidado

--- 2021/09/INC0102207_script_ajuste_parcelas.sql
+++ 2021/09/INC0102207_script_ajuste_parcelas.sql
@@ -1,42 +1,23 @@
--- INC0102207 - Consignado
 BEGIN
   UPDATE tbepr_consignado_pagamento
-     SET instatus = 2 --processado
+     SET instatus = 2 
    WHERE cdcooper = 13
      AND nrdconta = 252514
      AND nrctremp = 72630
      AND nrparepr IN (12, 58, 59)
      AND idintegracao IS NULL;
-
-  UPDATE tbepr_consignado_pagamento
-     SET instatus = 2 --processado
-   WHERE cdcooper = 13
-     AND nrdconta = 66877
-     AND nrctremp = 91959
-     AND nrparepr IN (9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36)
-     AND idintegracao IS NULL;
      
      
   UPDATE crappep
      SET dtultpag = '13/07/2021'
-        ,vlsdvatu = 0 -- saldo devedor atualizado da parcela
-        ,inliquid = 1 -- liquidado                            
+        ,vlsdvatu = 0 
+        ,inliquid = 1                           
    WHERE cdcooper = 13
      AND nrdconta = 252514
      AND nrctremp = 72630
      AND inliquid = 0;
 
-  UPDATE crappep
-     SET dtultpag = '16/09/2021'
-        ,vlsdvatu = 0 -- saldo devedor atualizado da parcela
-        ,inliquid = 1 -- liquidado                            
-   WHERE cdcooper = 13
-     AND nrdconta = 66877
-     AND nrctremp = 91959
-     AND inliquid = 0;
 
-
-  -- Cadastro de emprestimos
   UPDATE crapepr 
      SET inliquid = 1
         ,vlsdeved = 0
@@ -44,10 +25,11 @@ BEGIN
         ,vlsdevat = 0
         ,vlpapgat = 0
    WHERE cdcooper = 13
-     AND nrdconta IN (252514, 66877)
-     AND nrctremp IN (72630, 91959);
+     AND nrdconta = 252514
+     AND nrctremp = 72630;
 
    COMMIT;
+
 EXCEPTION
   WHEN OTHERS THEN
     ROLLBACK;
