c0766350399ae8205f39a0f83c5a7a7a67482497
CECRED\f0033505 <filipe.cardozo@ailos.coop.br>
2021-09-24 13:59:57 -0300

    Atualizando pagamentos para testar segunda via Bancoob

--- 2021/09/prj0023640_update_pagamentos_bancoob.sql
+++ 2021/09/prj0023640_update_pagamentos_bancoob.sql
@@ -1,7 +1,19 @@
 ﻿BEGIN
 
+DELETE FROM crappro a                  
+ WHERE EXISTS (SELECT 0
+                 FROM craplft l
+                WHERE a.cdcooper = l.cdcooper
+                  AND a.nrdconta = l.nrdconta
+                  AND a.dtmvtolt = l.dtmvtolt
+                  AND a.cdtippro <> 13
+                  AND a.nrseqaut = l.nrautdoc
+                  AND l.idsicred IN (3553099,3553100,3553103,3553101,3553102));
+                  
 UPDATE tbconv_registro_remessa_pagfor a
-   SET a.cdstatus_processamento = 2
+   SET a.cdstatus_processamento = 4,
+       a.dsprocessamento = 'ID Transação já utilizada',
+       a.cdstatushttp = 400
  WHERE a.idsicredi IN (3553099,3553100,3553103,3553101,3553102);
  
  COMMIT;
