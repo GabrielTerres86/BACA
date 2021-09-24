d8e1297cf0ff015221e5ad4a1416080f5185dbff
Djonata Wehmuth <djonata.wehmuth@ailos.coop.br>
2021-09-24 09:33:03 -0300

    Revert "Merge branch 'project/649/ajuste_apontamento_cartao' of https://dev.azure.com/Ailos/Ailos%20Fontes/_git/BACA into project/649/ajuste_apontamento_cartao"
    This reverts commit 23d43643fc6471932e5d31509030a25d2df35b60, reversing
    changes made to 158de463749026d0e2c77e951945c600d1245c6a.

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -10,10 +10,9 @@ BEGIN
   -- Cooperativa de destino do cartão
   vr_cooperativa := 1;
   -- Conta de destino do cartão
-  vr_conta := 7399359;
+  vr_conta := 90264878;
 
--- Numero do cartão que precisamos ajustar
-  vr_cartao := 5588190184171591; -- Topaz
+  -- Numero do cartão que precisamos ajustar
 --  vr_cartao := 5127070162667067; -- Jeff
 --  vr_cartao := 5127070162667935; -- XV
 --  vr_cartao := 5127070161674411; -- Luis
@@ -22,7 +21,8 @@ BEGIN
 --  vr_cartao := 5127070340534221; -- Paty
 --  vr_cartao := 5161620000587872; -- Djonata
 --  vr_cartao := 5127070162666663; -- Matheus barros
---  vr_cartao := 5158940000000188; -- Matheus barros 2
+  vr_cartao := 5158940000000188; -- Matheus barros 2
+--  vr_cartao := 5588190184171591; -- Topaz
 --  vr_cartao := 5161620000264183; -- Poliane 
 
   
