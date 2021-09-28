7b00ae15fb671b68cfd583d4a7d75c3a26a97c61
Giovane Diego Gibbert <giovane.gibbert@ailos.coop.br>
2021-09-28 11:45:08 -0300

    Ajustado baca - comentário antes do begin

--- 2021/09/PRJ0023550_filtro_status_contratos_pronampe.sql
+++ 2021/09/PRJ0023550_filtro_status_contratos_pronampe.sql
@@ -1,12 +1,9 @@
---238065 Opcoes na coluna Status da Honra - filtro
 BEGIN
-
+  --238065 Opcoes na coluna Status da Honra - filtro
   UPDATE crapaca
      SET lstparam = 'pr_cdcooper,pr_nrdconta,pr_nrcontrato,pr_nriniseq,pr_nrregist,pr_datrini,pr_datrfim,pr_sthonra'
    WHERE UPPER(nmdeacao) = 'CONSULTA_CONTRATOS';
-
   COMMIT;
-
 EXCEPTION
   WHEN OTHERS THEN
     ROLLBACK;
