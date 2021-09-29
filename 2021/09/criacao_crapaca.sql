296c2bcda2be4ffd45ae14141b10bc823c073070
Tiago Cucco <mouts-tiago.cucco@ailos.coop.br>
2021-09-29 09:51:58 -0300

    PRJTASK0100060 - Ajuste Script

--- 2021/09/criacao_crapaca.sql
+++ 2021/09/criacao_crapaca.sql
@@ -2,34 +2,28 @@ declare
   vr_nrseqrdr craprdr.nrseqrdr%TYPE := 0;
 BEGIN
   INSERT INTO craprdr(nrseqrdr,nmprogra,dtsolici) VALUES (((select max(nrseqrdr) from craprdr) + 1),'TCARGA',SYSDATE) returning craprdr.nrseqrdr into vr_nrseqrdr;
-  INSERT INTO crapaca(nrseqaca 
-             ,nmdeacao 
+  INSERT INTO crapaca(nmdeacao 
              ,nmpackag 
              ,nmproced             
              ,nrseqrdr)
-         VALUES(((select max(nrseqaca) from crapaca) + 1)  -- nrseqaca
-             ,'VERIFICAAMBIENTE' -- nmdeacao
+         VALUES('VERIFICAAMBIENTE' -- nmdeacao
              ,'tela_tcarga'-- nmpackag
              ,'pc_verifica_ambiente'-- nmproced                      
              ,vr_nrseqrdr); -- nrseqrdr
-  INSERT INTO crapaca(nrseqaca 
-             ,nmdeacao 
+  INSERT INTO crapaca(nmdeacao 
              ,nmpackag 
              ,nmproced             
              ,nrseqrdr)
-         VALUES(((select max(nrseqaca) from crapaca) + 1)  -- nrseqaca
-             ,'VERIFICAEXECUCAO' -- nmdeacao
+         VALUES('VERIFICAEXECUCAO' -- nmdeacao
              ,'tela_tcarga'-- nmpackag
              ,'pc_verifica_execucao'-- nmproced                      
              ,vr_nrseqrdr); -- nrseqrdr
-  INSERT INTO crapaca(nrseqaca 
-             ,nmdeacao 
+  INSERT INTO crapaca(nmdeacao 
              ,nmpackag 
              ,nmproced             
              ,nrseqrdr
              ,lstparam)
-         VALUES(((select max(nrseqaca) from crapaca) + 1)  -- nrseqaca
-             ,'SOLICITACARGA' -- nmdeacao
+         VALUES('SOLICITACARGA' -- nmdeacao
              ,'tela_tcarga'-- nmpackag
              ,'pc_solicita_teste_carga'-- nmproced                      
              ,vr_nrseqrdr
