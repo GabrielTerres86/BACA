ed5e68996506f36839683cd8293294f7ccd85f75
Paulo Martins (Mouts) <t0031812@ailos.coop.br>
2021-09-29 17:40:17 +0000

    Inclusão Exception

--- 2021/09/PARDBT_Parametrizacao.sql
+++ 2021/09/PARDBT_Parametrizacao.sql
@@ -2,13 +2,12 @@ DECLARE
  
   -- Variaveis
   vr_cdprocesso     CONSTANT tbgen_debitador_param.cdprocesso%TYPE := 'EMPR0025.PC_DEBITAR_IMOBILIARIO';
-  vr_nrprioridade   tbgen_debitador_param.nrprioridade%TYPE;
   
 BEGIN 
   
   UPDATE tbgen_debitador_param P
      SET NRPRIORIDADE = NRPRIORIDADE + 1
-   WHERE P.NRPRIORIDADE > 6;
+   WHERE P.NRPRIORIDADE >= 6;
 
   -- Incluir dados 
   INSERT INTO tbgen_debitador_param(cdprocesso
@@ -29,15 +28,15 @@ BEGIN
                                    ,0);
 
   FOR ind IN 1..5 LOOP                            
-    -- Inserir os hor·rios de execuÁ„o
+    -- Inserir os hor√°rios de execu√ß√£o
     INSERT INTO tbgen_debitador_horario_proc
                       (cdprocesso
                       ,idhora_processamento)
                VALUES (vr_cdprocesso
                       ,ind);
   END LOOP;
-
-
   COMMIT;
-
+EXCEPTION
+  WHEN OTHERS THEN
+    NULL;
 END;
