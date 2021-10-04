de83c43d6f09ae7ebc34f7aa762ee7045f60fae8
Bruno Pascoal dos Reis <supero-bruno.reis@ailos.coop.br>
2021-10-04 11:50:20 -0300

    SQUADMOTOR - Scripts CADPAR

---
+++ 2021/10/SQUADMOTOR_RITM0170468_4.sql
@@ -0,0 +1,60 @@
+DECLARE
+  CURSOR cr_crapbat(pr_cdbattar IN crapbat.cdbattar%TYPE) IS
+    SELECT cdcadast
+      FROM crapbat
+     WHERE cdbattar = pr_cdbattar;
+  rw_crapbat cr_crapbat%ROWTYPE;
+  aux_cdpartar_add  crappat.cdpartar%TYPE;
+  aux_cdpartar_del  crappat.cdpartar%TYPE;
+BEGIN
+  OPEN cr_crapbat('PERCVARIRENDA');
+  FETCH cr_crapbat INTO rw_crapbat;
+  CLOSE cr_crapbat;
+  IF rw_crapbat.cdcadast IS NOT NULL THEN
+    aux_cdpartar_del := rw_crapbat.cdcadast;
+    DELETE FROM crapbat WHERE cdcadast = aux_cdpartar_del;
+    DELETE FROM crappat WHERE cdpartar = aux_cdpartar_del;
+    DELETE FROM crappco WHERE cdpartar = aux_cdpartar_del;
+  END IF;
+  COMMIT;
+  SELECT MAX(cdpartar) INTO aux_cdpartar_add FROM crappat;
+ 
+  aux_cdpartar_add :=  aux_cdpartar_add + 1;
+ 
+  insert into crappat (CDPARTAR, NMPARTAR, TPDEDADO, CDPRODUT)
+  values (aux_cdpartar_add, 'Meses para análise motor de percentual variação renda', 2, 13);
+
+  insert into crapbat (CDBATTAR, NMIDENTI, CDPROGRA, TPCADAST, CDCADAST)
+  values ('PERCVARIRENDA', 'Meses para análise motor de percentual variação renda', ' ', 2, aux_cdpartar_add);
+           
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 1,  '6');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 2,  '6');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 5,  '6');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 6,  '6');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 7,  '6');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 8,  '6');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 9,  '6');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 10, '6');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 11, '6');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 12, '6');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 13, '6');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 14, '6');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 16, '6');
+  
+  COMMIT;
+ 
+END;
+/
