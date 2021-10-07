1fbe783fce7fa337f4cb0ac259352669080a8cb5
Elton Giusti <t0031664@ailos.coop.br>
2021-10-07 14:52:38 -0300

    Ajuste

--- 2021/10/prj0023311_update_crapris.sql
+++ 2021/10/prj0023311_update_crapris.sql
@@ -3,7 +3,7 @@ UPDATE crapris a
 SET a.vljura60 = 0
 WHERE a.cdcooper = 1
 AND   a.nrdconta IN (2515938,2515938,8481997,9356703)
-AND a.dtrefere = '27/09/2021'
+AND a.dtrefere = to_date('27/09/2021','dd/mm/yyyy')
 AND a.nrctremp IN (3930543,3969605,2103837,2956154)
 AND a.vljura60 IS NULL;
 
