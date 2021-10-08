9b6887175fa500ee3a63ef148fa7de53ac7c1e0e
Elton Giusti <t0031664@ailos.coop.br>
2021-10-08 19:02:08 -0300

    ajuste

--- 2021/10/prj0023311_update_venc_rating.sql
+++ 2021/10/prj0023311_update_venc_rating.sql
@@ -4,6 +4,7 @@ BEGIN
   WHERE o.cdcooper IN(1, 16)
   AND o.insituacao_rating = 4 --efetivado
   AND o.inrisco_rating_autom IS NOT NULL
+  AND o.nrctremp in (4237504, 4237772,4238081,4238009, 307974,4238066)
   AND o.tpctrato = 93  --imobiliario
   AND o.flencerrado = 0 -- Contratos ativo
   AND o.flintegrar_sas = 0;
