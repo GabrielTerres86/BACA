0b40a72d10599c2e3cfa8416751f073e5ce53227
Djonata Wehmuth <djonata.wehmuth@ailos.coop.br>
2021-10-13 16:22:00 -0300

    Update alterar_conta_do_cartao.sql

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -10,7 +10,7 @@ BEGIN
   -- Cooperativa de destino do cartão
   vr_cooperativa := 1;
   -- Conta de destino do cartão
-  vr_conta := 329;
+  vr_conta := 12770183;
 
 -- Numero do cartão que precisamos ajustar
 
