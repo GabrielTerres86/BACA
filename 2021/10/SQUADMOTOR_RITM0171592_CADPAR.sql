ce638e3e792a506154b53de39a64dc665fbed621
Bruno Pascoal dos Reis <supero-bruno.reis@ailos.coop.br>
2021-10-15 15:40:34 -0300

    SQUADMOTOR - Script CADPAR

---
+++ 2021/10/SQUADMOTOR_RITM0171592_CADPAR.sql
@@ -0,0 +1,222 @@
+/* e-mail para receber os alertas */
+DECLARE
+  CURSOR cr_crapbat(pr_cdbattar IN crapbat.cdbattar%TYPE) IS
+    SELECT cdcadast
+      FROM crapbat
+     WHERE cdbattar = pr_cdbattar;
+  rw_crapbat cr_crapbat%ROWTYPE;
+  aux_cdpartar_add  crappat.cdpartar%TYPE;
+  aux_cdpartar_del  crappat.cdpartar%TYPE;
+BEGIN
+  OPEN cr_crapbat('ERRMOTOREMAIL');
+  FETCH cr_crapbat INTO rw_crapbat;
+  CLOSE cr_crapbat;
+  IF rw_crapbat.cdcadast IS NOT NULL THEN
+    aux_cdpartar_del := rw_crapbat.cdcadast;
+    DELETE FROM crapbat WHERE cdcadast = aux_cdpartar_del;
+    DELETE FROM crappat WHERE cdpartar = aux_cdpartar_del;
+    DELETE FROM crappco WHERE cdpartar = aux_cdpartar_del;
+  END IF;
+  COMMIT;
+  SELECT MAX(cdpartar) INTO aux_cdpartar_add FROM crappat;
+ 
+  aux_cdpartar_add :=  aux_cdpartar_add + 1;
+ 
+  insert into crappat (CDPARTAR, NMPARTAR, TPDEDADO, CDPRODUT)
+  values (aux_cdpartar_add, 'Job erros do motor - E-mail para envio', 2, 13);
+
+  insert into crapbat (CDBATTAR, NMIDENTI, CDPROGRA, TPCADAST, CDCADAST)
+  values ('ERRMOTOREMAIL', 'Job erros do motor - E-mail para envio', ' ', 2, aux_cdpartar_add);
+           
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 3,  'cmo@ailos.coop.br');
+  
+  COMMIT;
+ 
+END;
+/
+/* quantidade de erros mínima para começar a enviar os alertas */
+DECLARE
+  CURSOR cr_crapbat(pr_cdbattar IN crapbat.cdbattar%TYPE) IS
+    SELECT cdcadast
+      FROM crapbat
+     WHERE cdbattar = pr_cdbattar;
+  rw_crapbat cr_crapbat%ROWTYPE;
+  aux_cdpartar_add  crappat.cdpartar%TYPE;
+  aux_cdpartar_del  crappat.cdpartar%TYPE;
+BEGIN
+  OPEN cr_crapbat('ERRMOTORQTD');
+  FETCH cr_crapbat INTO rw_crapbat;
+  CLOSE cr_crapbat;
+  IF rw_crapbat.cdcadast IS NOT NULL THEN
+    aux_cdpartar_del := rw_crapbat.cdcadast;
+    DELETE FROM crapbat WHERE cdcadast = aux_cdpartar_del;
+    DELETE FROM crappat WHERE cdpartar = aux_cdpartar_del;
+    DELETE FROM crappco WHERE cdpartar = aux_cdpartar_del;
+  END IF;
+  COMMIT;
+  SELECT MAX(cdpartar) INTO aux_cdpartar_add FROM crappat;
+ 
+  aux_cdpartar_add :=  aux_cdpartar_add + 1;
+ 
+  insert into crappat (CDPARTAR, NMPARTAR, TPDEDADO, CDPRODUT)
+  values (aux_cdpartar_add, 'Job erros do motor - Quantidade mínima de erros para envio', 2, 13);
+
+  insert into crapbat (CDBATTAR, NMIDENTI, CDPROGRA, TPCADAST, CDCADAST)
+  values ('ERRMOTORQTD', 'Job erros do motor - Quantidade mínima de erros para envio', ' ', 2, aux_cdpartar_add);
+           
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 3,  '15');
+  
+  COMMIT;
+ 
+END;
+/
+/* período de tempo para verificar os erros durante o dia */
+DECLARE
+  CURSOR cr_crapbat(pr_cdbattar IN crapbat.cdbattar%TYPE) IS
+    SELECT cdcadast
+      FROM crapbat
+     WHERE cdbattar = pr_cdbattar;
+  rw_crapbat cr_crapbat%ROWTYPE;
+  aux_cdpartar_add  crappat.cdpartar%TYPE;
+  aux_cdpartar_del  crappat.cdpartar%TYPE;
+BEGIN
+  OPEN cr_crapbat('ERRMOTORDIA');
+  FETCH cr_crapbat INTO rw_crapbat;
+  CLOSE cr_crapbat;
+  IF rw_crapbat.cdcadast IS NOT NULL THEN
+    aux_cdpartar_del := rw_crapbat.cdcadast;
+    DELETE FROM crapbat WHERE cdcadast = aux_cdpartar_del;
+    DELETE FROM crappat WHERE cdpartar = aux_cdpartar_del;
+    DELETE FROM crappco WHERE cdpartar = aux_cdpartar_del;
+  END IF;
+  COMMIT;
+  SELECT MAX(cdpartar) INTO aux_cdpartar_add FROM crappat;
+ 
+  aux_cdpartar_add :=  aux_cdpartar_add + 1;
+ 
+  insert into crappat (CDPARTAR, NMPARTAR, TPDEDADO, CDPRODUT)
+  values (aux_cdpartar_add, 'Job erros do motor - Tempo para verificação de erros (entre 06h e 18h)', 2, 13);
+
+  insert into crapbat (CDBATTAR, NMIDENTI, CDPROGRA, TPCADAST, CDCADAST)
+  values ('ERRMOTORDIA', 'Job erros do motor - Tempo para verificação de erros (entre 06h e 18h)', ' ', 2, aux_cdpartar_add);
+           
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 3,  '15');
+  
+  COMMIT;
+ 
+END;
+/
+/* período de tempo para verificar os erros durante a noite */
+DECLARE
+  CURSOR cr_crapbat(pr_cdbattar IN crapbat.cdbattar%TYPE) IS
+    SELECT cdcadast
+      FROM crapbat
+     WHERE cdbattar = pr_cdbattar;
+  rw_crapbat cr_crapbat%ROWTYPE;
+  aux_cdpartar_add  crappat.cdpartar%TYPE;
+  aux_cdpartar_del  crappat.cdpartar%TYPE;
+BEGIN
+  OPEN cr_crapbat('ERRMOTORNOITE');
+  FETCH cr_crapbat INTO rw_crapbat;
+  CLOSE cr_crapbat;
+  IF rw_crapbat.cdcadast IS NOT NULL THEN
+    aux_cdpartar_del := rw_crapbat.cdcadast;
+    DELETE FROM crapbat WHERE cdcadast = aux_cdpartar_del;
+    DELETE FROM crappat WHERE cdpartar = aux_cdpartar_del;
+    DELETE FROM crappco WHERE cdpartar = aux_cdpartar_del;
+  END IF;
+  COMMIT;
+  SELECT MAX(cdpartar) INTO aux_cdpartar_add FROM crappat;
+ 
+  aux_cdpartar_add :=  aux_cdpartar_add + 1;
+ 
+  insert into crappat (CDPARTAR, NMPARTAR, TPDEDADO, CDPRODUT)
+  values (aux_cdpartar_add, 'Job erros do motor - Tempo para verificação de erros (entre 18h e 06h)', 2, 13);
+
+  insert into crapbat (CDBATTAR, NMIDENTI, CDPROGRA, TPCADAST, CDCADAST)
+  values ('ERRMOTORNOITE', 'Job erros do motor - Tempo para verificação de erros (entre 18h e 06h)', ' ', 2, aux_cdpartar_add);
+           
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 3,  '45');
+  
+  COMMIT;
+ 
+END;
+/
+/* período de tempo entre e-mails de alerta */
+DECLARE
+  CURSOR cr_crapbat(pr_cdbattar IN crapbat.cdbattar%TYPE) IS
+    SELECT cdcadast
+      FROM crapbat
+     WHERE cdbattar = pr_cdbattar;
+  rw_crapbat cr_crapbat%ROWTYPE;
+  aux_cdpartar_add  crappat.cdpartar%TYPE;
+  aux_cdpartar_del  crappat.cdpartar%TYPE;
+BEGIN
+  OPEN cr_crapbat('ERRMOTORPERMAIL');
+  FETCH cr_crapbat INTO rw_crapbat;
+  CLOSE cr_crapbat;
+  IF rw_crapbat.cdcadast IS NOT NULL THEN
+    aux_cdpartar_del := rw_crapbat.cdcadast;
+    DELETE FROM crapbat WHERE cdcadast = aux_cdpartar_del;
+    DELETE FROM crappat WHERE cdpartar = aux_cdpartar_del;
+    DELETE FROM crappco WHERE cdpartar = aux_cdpartar_del;
+  END IF;
+  COMMIT;
+  SELECT MAX(cdpartar) INTO aux_cdpartar_add FROM crappat;
+ 
+  aux_cdpartar_add :=  aux_cdpartar_add + 1;
+ 
+  insert into crappat (CDPARTAR, NMPARTAR, TPDEDADO, CDPRODUT)
+  values (aux_cdpartar_add, 'Job erros do motor - Período de tempo entre e-mails de alerta', 2, 13);
+
+  insert into crapbat (CDBATTAR, NMIDENTI, CDPROGRA, TPCADAST, CDCADAST)
+  values ('ERRMOTORPERMAIL', 'Job erros do motor - Período de tempo entre e-mails de alerta', ' ', 2, aux_cdpartar_add);
+           
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 3,  '30');
+  
+  COMMIT;
+ 
+END;
+/
+/* parametro para ativar o job de envio de e-mails - inicia como desativado */
+DECLARE
+  CURSOR cr_crapbat(pr_cdbattar IN crapbat.cdbattar%TYPE) IS
+    SELECT cdcadast
+      FROM crapbat
+     WHERE cdbattar = pr_cdbattar;
+  rw_crapbat cr_crapbat%ROWTYPE;
+  aux_cdpartar_add  crappat.cdpartar%TYPE;
+  aux_cdpartar_del  crappat.cdpartar%TYPE;
+BEGIN
+  OPEN cr_crapbat('ERRMOTORATIVADO');
+  FETCH cr_crapbat INTO rw_crapbat;
+  CLOSE cr_crapbat;
+  IF rw_crapbat.cdcadast IS NOT NULL THEN
+    aux_cdpartar_del := rw_crapbat.cdcadast;
+    DELETE FROM crapbat WHERE cdcadast = aux_cdpartar_del;
+    DELETE FROM crappat WHERE cdpartar = aux_cdpartar_del;
+    DELETE FROM crappco WHERE cdpartar = aux_cdpartar_del;
+  END IF;
+  COMMIT;
+  SELECT MAX(cdpartar) INTO aux_cdpartar_add FROM crappat;
+ 
+  aux_cdpartar_add :=  aux_cdpartar_add + 1;
+ 
+  insert into crappat (CDPARTAR, NMPARTAR, TPDEDADO, CDPRODUT)
+  values (aux_cdpartar_add, 'Job erros do motor - Configuração de execução do job (S=Ativado N=Desativado)', 2, 13);
+
+  insert into crapbat (CDBATTAR, NMIDENTI, CDPROGRA, TPCADAST, CDCADAST)
+  values ('ERRMOTORATIVADO', 'Job erros do motor - Configuração de execução do job (S=Ativado N=Desativado)', ' ', 2, aux_cdpartar_add);
+           
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 3,  'N');
+  
+  COMMIT;
+ 
+END;
+/
