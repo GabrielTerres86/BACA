84403fdb108a6fb8c8a02221f7087d5b98c42f19
Paulo Felipe Kowalsky <t0032603@cecred.coop.br>
2021-10-19 16:07:03 -0300

    insert na crapprm abbc

---
+++ 2021/10/insert_crapprm_cobrança.sql
@@ -0,0 +1,8 @@
+begin
+
+insert into crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) 
+       values ('CRED', 0, 'DIR_CONNECT_ABBC_ENVIA', 'Diretorio de envio dos arquivos para ABBC', '/usr/connect/abbc/envia');
+  
+  
+commit;
+end;  
