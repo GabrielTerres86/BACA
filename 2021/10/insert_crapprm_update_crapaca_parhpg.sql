9dc95ee707c147c685e6069c0b826d86d4ffeaf0
Daniel <daniel.lombardi@mouts.info>
2021-10-21 09:23:51 -0300

    PRJ0023768 - Ajustes parametros CRAPACA e CRAPPRM.

---
+++ 2021/10/insert_crapprm_update_crapaca_parhpg.sql
@@ -0,0 +1,13 @@
+BEGIN
+  INSERT INTO crapprm (crapprm.nmsistem, crapprm.cdcooper, crapprm.cdacesso, crapprm.dstexprm, crapprm.dsvlrprm)
+               VALUES ('CRED', 0, 'FLG_DA_ULTDIANO_AILOS', 'Ailos - Flag para habilitar agendamento de débito automático para última dia útil do ano (fora da compensação).', 0);
+                   
+  INSERT INTO crapprm (crapprm.nmsistem, crapprm.cdcooper, crapprm.cdacesso, crapprm.dstexprm, crapprm.dsvlrprm)
+               VALUES ('CRED', 0, 'FLG_DA_ULTDIANO_BANCOOB', 'Bancoob - Flag para habilitar agendamento de débito automático para última dia útil do ano (fora da compensação).', 1);  
+               
+  UPDATE crapaca
+     SET crapaca.lstparam = 'pr_vrdupgto,pr_fgdupgto,pr_daultutilailos,pr_daultutilbancoob'
+  WHERE crapaca.nmdeacao = 'ALTERA_PARAMETRO_PARHPG'; 
+             
+  COMMIT;  
+END;            
