b36f281770526ea5fa8e8b166bfab07895e3eec3
Daniel <daniel.lombardi@mouts.info>
2021-10-21 09:34:48 -0300

    PRJ0023768 - Ajuste váriavel.

--- 2021/10/insert_crapprm_update_crapaca_parhpg.sql
+++ 2021/10/insert_crapprm_update_crapaca_parhpg.sql
@@ -3,7 +3,7 @@ BEGIN
                VALUES ('CRED', 0, 'FLG_DA_ULTDIANO_AILOS', 'Ailos - Flag para habilitar agendamento de débito automático para última dia útil do ano (fora da compensação).', 0);
                    
   INSERT INTO crapprm (crapprm.nmsistem, crapprm.cdcooper, crapprm.cdacesso, crapprm.dstexprm, crapprm.dsvlrprm)
-               VALUES ('CRED', 0, 'FLG_DA_ULTDIANO_BANCOOB', 'Bancoob - Flag para habilitar agendamento de débito automático para última dia útil do ano (fora da compensação).', 1);  
+               VALUES ('CRED', 0, 'FLG_DA_ULTDIANO_BANCOOB', 'Bancoob - Flag para habilitar agendamento de débito automático para última dia útil do ano (fora da compensação).', 0);  
                
   UPDATE crapaca
      SET crapaca.lstparam = 'pr_vrdupgto,pr_fgdupgto,pr_daultutilailos,pr_daultutilbancoob'
