ad3ddf1949c6806148b5dcb165103914998af783
Luiz Otávio Olinger Momm <luiz.momm@ailos.coop.br>
2021-10-25 19:07:38 -0300

    Solicitar atualização RATING SAS

--- 2021/10/INC0110558_AtualizarRating.sql
+++ 2021/10/INC0110558_AtualizarRating.sql
@@ -1,8 +1,8 @@
 -- Atualizar 2 RATINGS
 -- Menos de 1 Segundo
 BEGIN
-  UPDATE tbrisco_operacoes o SET o.flintegrar_sas = 1, o.dtvencto_rating = TO_DATE('25/10/2021', 'dd/mm/yyyy') WHERE o.cdcooper = 10 AND o.nrdconta = 82384  AND o.nrctremp = 143685 AND o.tpctrato = 1;
-  UPDATE tbrisco_operacoes o SET o.flintegrar_sas = 1, o.dtvencto_rating = TO_DATE('25/10/2021', 'dd/mm/yyyy') WHERE o.cdcooper = 10 AND o.nrdconta = 101083 AND o.nrctremp = 101181 AND o.tpctrato = 1;
+  UPDATE tbrisco_operacoes o SET o.flintegrar_sas = 1, o.dtvencto_rating = TO_DATE('25/10/2021', 'dd/mm/yyyy') WHERE o.cdcooper = 10 AND o.nrdconta = 82384  AND o.nrctremp = 101083 AND o.tpctrato = 1;
+  UPDATE tbrisco_operacoes o SET o.flintegrar_sas = 1, o.dtvencto_rating = TO_DATE('25/10/2021', 'dd/mm/yyyy') WHERE o.cdcooper = 10 AND o.nrdconta = 143685 AND o.nrctremp = 101181 AND o.tpctrato = 1;
   COMMIT;
 EXCEPTION
   WHEN others THEN
