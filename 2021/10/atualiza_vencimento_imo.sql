20188bec6f175893ccc6229a6070e6bc825c584e
Paulo Martins (Mouts) <t0031812@ailos.coop.br>
2021-10-25 19:43:41 +0000

    Atualizar data de vencimento.

---
+++ 2021/10/atualiza_vencimento_imo.sql
@@ -0,0 +1,7 @@
+update CREDITO.TBEPR_PARCELAS_CRED_IMOB parc 
+   set parc.dtvencto = to_date('15/10/2021','dd/mm/rrrr')
+where nrparcel = 0 and cdcooper =1; 
+update CREDITO.TBEPR_PARCELAS_CRED_IMOB parc 
+   set parc.dtvencto = to_date('18/10/2021','dd/mm/rrrr')
+where nrparcel = 0 and cdcooper =16; 
+commit;
