50b6397e3563d653308e02848014ef3d34d3ed1f
mouts-mateus.zimmermann@ailos.coop.br <zim.mateus@gmail.com>
2021-10-27 08:30:05 -0300

    PRJ0022517 - Update CRAPACA Receita Federal

---
+++ 2021/10/prj0022517_update_crapaca_receita_federal.sql
@@ -0,0 +1,13 @@
+BEGIN
+
+  UPDATE cecred.crapaca aca
+     SET aca.lstparam = 'pr_dtiniper,pr_dtfimper,pr_cdempres,pr_status1perna,pr_status2perna,pr_enviadoreproces,pr_tlcooper,pr_nrdconta,pr_tlagenci,pr_nmarquivo,pr_textopesquisa,pr_orderby,pr_nriniseq,pr_nrregist'
+   WHERE aca.nmdeacao = 'TAB057_REGISTROS_REMESSAS_RFB';
+
+  UPDATE cecred.crapaca aca 
+     SET aca.lstparam = 'pr_idremessa,pr_dtiniper,pr_dtfimper,pr_cdempres,pr_status1perna,pr_status2perna,pr_enviadoreproces,pr_tlcooper,pr_nrdconta,pr_tlagenci,pr_nmarquivo,pr_textopesquisa,pr_orderby,pr_nriniseq,pr_nrregist'
+   WHERE aca.nmdeacao = 'TAB057_REMESSAS_RECEITA_FEDERAL';
+   
+   COMMIT;
+
+END;   
