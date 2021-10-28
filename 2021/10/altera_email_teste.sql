a2357d4c3e51d3a6cca7a7e58394d286ce4d5e11
Reginaldo Rubens da Silva <t0033608@ailos.coop.br>
2021-10-28 17:56:58 -0300

    Projeto 686 - Correção no script de alteração do e-mail de teste

--- 2021/10/altera_email_teste.sql
+++ 2021/10/altera_email_teste.sql
@@ -1,4 +1,7 @@
-UPDATE crapprm p
-set p.dsvlrprm = 'vitor.schlindwein@ailos.coop.br'
-where p.cdacesso = 'EMAIL_TESTE';
-commit;
+BEGIN
+
+	UPDATE crapprm p
+	set p.dsvlrprm = 'vitor.schlindwein@ailos.coop.br'
+	where p.cdacesso = 'EMAIL_TESTE';
+	commit;
+END;
