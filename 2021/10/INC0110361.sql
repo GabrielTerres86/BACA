ce38c48e3e2a0a617906fbe55a97c1b6f888a790
Alessandro Hoffelder <alessandro.hoffelder@cecred.coop.br>
2021-10-28 15:11:17 -0300

    falso positivo bancoob

---
+++ 2021/10/INC0110361.sql
@@ -0,0 +1,84 @@
+begin
+
+/* BACA */
+
+/* Conta 13451421 */
+
+begin 
+
+insert into tbcrd_conta_cartao  (cdcooper,  nrdconta,  nrconta_cartao,  vllimite_global,  cdadmcrd) values (1, 13451421, 7563239818939,  null, 18); 
+
+update crawcrd  set  nrcrcard = 5161620002891009,  insitcrd = 3,  nrcctitg = 7563239818939,  cdlimcrd = 0,  dtrejeit = null,  dtlibera = trunc(sysdate),  dtentreg = null where  cdcooper = 1 and nrdconta = 13451421 and nrctrcrd = 2385044; 
+
+insert into crapcrd  (cdcooper,  nrdconta,  nrcrcard,  nrcpftit,  nmtitcrd,  dddebito,  cdlimcrd,  dtvalida,  nrctrcrd,  cdmotivo,  nrprotoc,  cdadmcrd,  tpcartao,  dtcancel,  flgdebit,  flgprovi) values (1, 13451421, 5161620002891009, 9899557960, 'PATRICIA NASCIMENTO', 11, 0, null, 2385044, 0, 0, 18, 2, null, 1, 0); 
+
+end; 
+
+
+/* Conta 13446924 */
+
+begin 
+
+insert into tbcrd_conta_cartao  (cdcooper,  nrdconta,  nrconta_cartao,  vllimite_global,  cdadmcrd) values (1, 13446924, 7563239818931,  null, 18); 
+
+update crawcrd  set  nrcrcard = 5161620002890613,  insitcrd = 3,  nrcctitg = 7563239818931,  cdlimcrd = 0,  dtrejeit = null,  dtlibera = trunc(sysdate),  dtentreg = null where  cdcooper = 1 and nrdconta = 13446924 and nrctrcrd = 2384979; 
+
+insert into crapcrd  (cdcooper,  nrdconta,  nrcrcard,  nrcpftit,  nmtitcrd,  dddebito,  cdlimcrd,  dtvalida,  nrctrcrd,  cdmotivo,  nrprotoc,  cdadmcrd,  tpcartao,  dtcancel,  flgdebit,  flgprovi) values (1, 13446924, 5161620002890613, 14421705730, 'WELDER  HENRIQUE', 11, 0, null, 2384979, 0, 0, 18, 2, null, 1, 0); 
+
+end; 
+
+
+/* Conta 13448242 */
+
+begin 
+
+insert into tbcrd_conta_cartao  (cdcooper,  nrdconta,  nrconta_cartao,  vllimite_global,  cdadmcrd) values (1, 13448242, 7563239818932,  null, 12); 
+
+update crawcrd  set  nrcrcard = 5127070387422835,  insitcrd = 3,  nrcctitg = 7563239818932,  cdlimcrd = 0,  dtrejeit = null,  dtlibera = trunc(sysdate),  dtentreg = null where  cdcooper = 1 and nrdconta = 13448242 and nrctrcrd = 2383757; 
+
+insert into crapcrd  (cdcooper,  nrdconta,  nrcrcard,  nrcpftit,  nmtitcrd,  dddebito,  cdlimcrd,  dtvalida,  nrctrcrd,  cdmotivo,  nrprotoc,  cdadmcrd,  tpcartao,  dtcancel,  flgdebit,  flgprovi) values (1, 13448242, 5127070387422835, 10411442961, 'RENATA V  SANTOS', 11, 0, null, 2383757, 0, 0, 12, 2, null, 1, 0); 
+
+end; 
+
+
+/* Conta 13449222 */
+
+begin 
+
+insert into tbcrd_conta_cartao  (cdcooper,  nrdconta,  nrconta_cartao,  vllimite_global,  cdadmcrd) values (1, 13449222, 7563239818935,  null, 12); 
+
+update crawcrd  set  nrcrcard = 5127070387423098,  insitcrd = 3,  nrcctitg = 7563239818935,  cdlimcrd = 0,  dtrejeit = null,  dtlibera = trunc(sysdate),  dtentreg = null where  cdcooper = 1 and nrdconta = 13449222 and nrctrcrd = 2383908; 
+
+insert into crapcrd  (cdcooper,  nrdconta,  nrcrcard,  nrcpftit,  nmtitcrd,  dddebito,  cdlimcrd,  dtvalida,  nrctrcrd,  cdmotivo,  nrprotoc,  cdadmcrd,  tpcartao,  dtcancel,  flgdebit,  flgprovi) values (1, 13449222, 5127070387423098, 61250538904, 'FABIANA S COELHO', 19, 0, null, 2383908, 0, 0, 12, 2, null, 1, 0); 
+
+end; 
+
+
+/* Conta 13451570 */
+
+begin 
+
+insert into tbcrd_conta_cartao  (cdcooper,  nrdconta,  nrconta_cartao,  vllimite_global,  cdadmcrd) values (1, 13451570, 7563239818940,  null, 16); 
+
+update crawcrd  set  nrcrcard = 6393500219514230,  insitcrd = 3,  nrcctitg = 7563239818940,  cdlimcrd = 0,  dtrejeit = null,  dtlibera = trunc(sysdate),  dtentreg = null where  cdcooper = 1 and nrdconta = 13451570 and nrctrcrd = 2384353; 
+
+insert into crapcrd  (cdcooper,  nrdconta,  nrcrcard,  nrcpftit,  nmtitcrd,  dddebito,  cdlimcrd,  dtvalida,  nrctrcrd,  cdmotivo,  nrprotoc,  cdadmcrd,  tpcartao,  dtcancel,  flgdebit,  flgprovi) values (1, 13451570, 6393500219514230, 9264777903, 'IOHANA C DE JESUS', 0, 0, null, 2384353, 0, 0, 16, 2, null, 1, 0); 
+
+end; 
+
+
+/* Conta 13452894 */
+
+begin 
+
+insert into tbcrd_conta_cartao  (cdcooper,  nrdconta,  nrconta_cartao,  vllimite_global,  cdadmcrd) values (1, 13452894, 7563239818943,  null, 18); 
+
+update crawcrd  set  nrcrcard = 5161620002891132,  insitcrd = 3,  nrcctitg = 7563239818943,  cdlimcrd = 0,  dtrejeit = null,  dtlibera = trunc(sysdate),  dtentreg = null where  cdcooper = 1 and nrdconta = 13452894 and nrctrcrd = 2385342; 
+
+insert into crapcrd  (cdcooper,  nrdconta,  nrcrcard,  nrcpftit,  nmtitcrd,  dddebito,  cdlimcrd,  dtvalida,  nrctrcrd,  cdmotivo,  nrprotoc,  cdadmcrd,  tpcartao,  dtcancel,  flgdebit,  flgprovi) values (1, 13452894, 5161620002891132, 8916998999, 'ITALINA F SANTOS', 11, 0, null, 2385342, 0, 0, 18, 2, null, 1, 0); 
+
+end; 
+
+commit;
+
+end;
