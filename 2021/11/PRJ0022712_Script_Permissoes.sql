b1a7749d9e293935702569a67efc4e7dce28057f
Evandro Guaranha(Amcom) <evandro.silva@amcom.com.br>
2021-11-03 17:24:44 -0300

    script permissoes carga manual

---
+++ 2021/11/PRJ0022712_Script_Permissoes.sql
@@ -0,0 +1,47 @@
+DECLARE
+
+BEGIN
+
+  FOR rw_crapcop IN (SELECT cdcooper
+                       FROM crapcop c
+                      WHERE c.flgativo = 1) LOOP
+  
+    --Adicionar novo tipo de permissao - importacao manual Limite de credito
+    UPDATE craptel t
+       SET t.cdopptel = t.cdopptel || ',X'
+          ,t.lsopptel = t.lsopptel || ',IMP. MAN LIM'
+     WHERE t.cdcooper = rw_crapcop.cdcooper
+       AND UPPER(t.nmdatela) = 'IMPPRE';
+  
+    -- Replicar permissoes de acesso 
+    INSERT INTO crapace
+      (nmdatela
+      ,cddopcao
+      ,cdoperad
+      ,nmrotina
+      ,cdcooper
+      ,nrmodulo
+      ,idevento
+      ,idambace)
+      SELECT 'IMPPRE'
+            ,'X'
+            ,ope.cdoperad
+            ,' '
+            ,ope.cdcooper
+            ,1
+            ,0
+            ,2
+        FROM crapace ace
+            ,crapope ope
+       WHERE ope.cdcooper = ace.cdcooper
+         AND UPPER(ope.cdoperad) = UPPER(ace.cdoperad)
+         AND UPPER(ace.nmdatela) = 'IMPPRE'
+         AND UPPER(ace.cddopcao) = 'L'
+         AND ope.cdsitope = 1
+         AND ace.cdcooper = rw_crapcop.cdcooper;
+  
+  END LOOP;
+
+  COMMIT;
+
+END;
