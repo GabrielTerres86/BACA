434ad8ea1ec922cc571d213f4d606760fc6a821a
Ramon Martins <ramon.silva@ailos.coop.br>
2021-11-03 15:16:14 -0300

    hotfix/INC0111505/master

---
+++ 2021/10/INC0111505.sql
@@ -0,0 +1,13 @@
+DECLARE
+	UPDATE CRAPASS
+	SET NMPRIMTL = 'THAMIRES DOS SANTOS RICARDO'
+	where cdcooper = 1
+	and nrdconta = 11931396;
+
+	UPDATE CRAPTTL
+	SET nmextttl = 'THAMIRES DOS SANTOS RICARDO'
+	where cdcooper = 1
+	and nrdconta = 11931396;  
+	
+	commit;
+END;
