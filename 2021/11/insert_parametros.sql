bff8237d3dc8c4fdcb414dc3907f959bc96cd7b0
Petrônio Sifuentes Gomes Júnior <mouts-petronio.junior@ailos.coop.br>
2021-11-04 10:07:53 -0300

    inserção de parâmetros de percentual para análise de fraude na admissão presencial

---
+++ 2021/11/insert_parametros.sql
@@ -0,0 +1,8 @@
+declare
+begin
+  
+INSERT INTO cecred.crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) values ('CRED', 0, 'PERC_MEDIO_FRAUDE_ADM_PRES', 'Porcentagem que define risco médio de fraude na admissão presencial', '41');
+INSERT INTO cecred.crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) values ('CRED', 0, 'PERC_ALTO_FRAUDE_ADM_PRES', 'Porcentagem que define risco médio de fraude na admissão presencial', '90');
+commit;
+end;
+/
