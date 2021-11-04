15868cfeca0727299b4b58cf74d80b0989968c31
João Luis Mendes <joao.brito@ailos.coop.br>
2021-11-04 11:38:18 -0300

    Update alterar_conta_do_cartao.sql
      -- Cooperativa de destino do cartão
      vr_cooperativa := 1;
      -- Conta de destino do cartão
      vr_conta := 80096751;
    
    -- Numero do cartão que precisamos ajustar
    --vr_cartao := 6393500065179583; -- AILOS 1
    vr_cartao := 5588190184171591; -- AILOS 2

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -10,11 +10,11 @@ BEGIN
   -- Cooperativa de destino do cartão
   vr_cooperativa := 1;
   -- Conta de destino do cartão
-  vr_conta := 80096719;
+  vr_conta := 80096751;
 
 -- Numero do cartão que precisamos ajustar
 --vr_cartao := 6393500065179583; -- AILOS 1
-vr_cartao := 5756323907327153; -- AILOS 2
+vr_cartao := 5588190184171591; -- AILOS 2
 
 -- vr_cartao := 5127070161674411; -- GISLON
 -- vr_cartao := 5127070320149719; -- GISLON 2
