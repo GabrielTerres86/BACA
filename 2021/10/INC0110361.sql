95655e726d4afd01100b5ba13b26e52ff864e81b
Alessandro Hoffelder <alessandro.hoffelder@cecred.coop.br>
2021-11-04 09:25:31 -0300

    INC0110361 - cartão com nr zerado

--- 2021/10/INC0110361.sql
+++ 2021/10/INC0110361.sql
@@ -1,84 +1,377 @@
+/* -------------------- ATENÇÃO: MUDAR TODOS OS LUGARES ONDE CONSTA O NÚMERO DO INC ----------------------- */
+/* BACA para alimentar a tabela CRAPCRD (proposta de cartão efetivadas) de cartões que já foram liberados ou estão em uso */
+/* Se não for encontrado o arquivo de Rollback no diretório especificado, pode-se resgatar o script retornado no campo DSXMLDAD dessa query:
+
+	select	a.dsxmldad
+	from	crapslr a
+	where	a.dsarqsai	like '%INC0110361%'
+	and	a.dtsolici	=
+		(select	max(x.dtsolici)
+		from	crapslr x
+		where	x.dsarqsai like '%INC0110361%');
+
+*/
+
 begin
 
-/* BACA */
+declare
 
-/* Conta 13451421 */
+ds_exc_erro_v			exception;							/* Para validar o diretório do rollback dinâmico */
+ds_dados_rollback_v		clob			:= null;				/* Para gravar rollback dinâmico */
+ds_texto_rollback_v		varchar2(32600);						/* Para gravar rollback dinâmico */
 
-begin 
+cd_cooperativa_v		crapcop.cdcooper%type	:= 3;
+ds_nome_arquivo_bkp_v		varchar2(100);
+ds_nome_diretorio_v		crapprm.dsvlrprm%type;
+ds_critica_v			crapcri.dscritic%type;
 
-insert into tbcrd_conta_cartao  (cdcooper,  nrdconta,  nrconta_cartao,  vllimite_global,  cdadmcrd) values (1, 13451421, 7563239818939,  null, 18); 
+dt_proposta_inicial_v		date	:= to_date('01/01/2021','dd/mm/yyyy');
+dt_proposta_final_v		date	:= to_date('17/09/2021 23:59:59','dd/mm/yyyy hh24:mi:ss');
+nr_cartao_v			crawcrd.nrcrcard%type;
 
-update crawcrd  set  nrcrcard = 5161620002891009,  insitcrd = 3,  nrcctitg = 7563239818939,  cdlimcrd = 0,  dtrejeit = null,  dtlibera = trunc(sysdate),  dtentreg = null where  cdcooper = 1 and nrdconta = 13451421 and nrctrcrd = 2385044; 
+ds_rowid_crapcrd_v		rowid;
+ds_rowid_tbcrd_conta_cartao_v	rowid;
 
-insert into crapcrd  (cdcooper,  nrdconta,  nrcrcard,  nrcpftit,  nmtitcrd,  dddebito,  cdlimcrd,  dtvalida,  nrctrcrd,  cdmotivo,  nrprotoc,  cdadmcrd,  tpcartao,  dtcancel,  flgdebit,  flgprovi) values (1, 13451421, 5161620002891009, 9899557960, 'PATRICIA NASCIMENTO', 11, 0, null, 2385044, 0, 0, 18, 2, null, 1, 0); 
+cursor	c01 is
+select	distinct
+	a.ds_rowid_crawcrd,									/* Rowid da CRAWCRD */
+	a.ds_dtrejeit,										/* Data de rejeição atual - para rollback */
+	a.ds_dtlibera,										/* Data de liberação atual - para rollback */
+	a.ds_dtentreg,										/* Data de entrega atual - para rollback */
+	a.nrcctitg,										/* Número da conta cartão atual */
+	a.insitcrd,										/* Situação atual da proposta */
+	a.cdcooper,										/* Cooperativa */
+	a.nrdconta,										/* Conta */
+	a.nrcrcard,										/* Número atual do cartão */
+	a.nrcpftit,										/* CPF/CNPJ do titular */
+	a.nmtitcrd,										/* Nome do titular */
+	a.dddebito,										/* Dia do débito em conta corrente */
+	a.cdlimcrd,										/* Código do limite de crédito */
+	a.dtvalida,										/* Data de validade */
+	a.nrctrcrd,										/* Número da proposta */
+	a.cdmotivo,										/* Motivo do cancelamento */
+	a.nrprotoc,										/* Número do protocolo */
+	a.cdadmcrd,										/* Código da administradora */
+	a.tpcartao,										/* Tipo de cartão */
+	a.dtcancel,										/* Data do cancelamento */
+	a.flgdebit,										/* Habilita função débito */
+	a.ie_provisorio,									/* Indica se é um cartão provisório */
+	a.nr_conta_cartao,									/* Número da conta cartão */
+	a.nr_cartao										/* Número do cartão, obtido do arquivo CCR3 */
+from	(
+		select	b.rowid as ds_rowid_crawcrd,
+		decode(b.dtrejeit,null,'null','to_date(' || chr(39) || trim(to_char(b.dtrejeit,'dd/mm/yyyy')) || chr(39) || ',' || chr(39) || 'dd/mm/yyyy' || chr(39) || ')') as ds_dtrejeit,
+		decode(b.dtlibera,null,'null','to_date(' || chr(39) || trim(to_char(b.dtlibera,'dd/mm/yyyy')) || chr(39) || ',' || chr(39) || 'dd/mm/yyyy' || chr(39) || ')') as ds_dtlibera,
+		decode(b.dtentreg,null,'null','to_date(' || chr(39) || trim(to_char(b.dtentreg,'dd/mm/yyyy')) || chr(39) || ',' || chr(39) || 'dd/mm/yyyy' || chr(39) || ')') as ds_dtentreg,
+		b.nrcctitg,
+		b.insitcrd,
+		b.cdcooper,
+		b.nrdconta,
+		b.nrcrcard,
+		b.nrcpftit,
+		b.nmtitcrd,
+		b.dddebito,
+		b.cdlimcrd,
+		b.dtvalida,
+		b.nrctrcrd,
+		b.cdmotivo,
+		b.nrprotoc,
+		b.cdadmcrd,
+		b.tpcartao,
+		b.dtcancel,
+		b.flgdebit,
+		decode(upper(b.nmtitcrd),'CARTAO PROVISORIO',1,0) ie_provisorio,
+		to_number(nvl(trim(h.dsvalor),1000000000000)) as nr_conta_cartao,
+		to_number(nvl(trim(i.dsvalor),0)) as nr_cartao
+	from	tbcrd_linha_arquivo_item j,							/* Log importação arquivo CCR3 - campo código de rejeição */
+		tbcrd_linha_arquivo_item i,							/* Log importação arquivo CCR3 - campo número do cartão */
+		tbcrd_linha_arquivo_item h,							/* Log importação arquivo CCR3 - campo número da conta cartão */
+		tbcrd_linha_arquivo_item g,							/* Log importação arquivo CCR3 - campo tipo de operação */
+		tbcrd_linha_arquivo_item f,							/* Log importação arquivo CCR3 - campo tipo de registro */
+		tbcrd_linha_arquivo_item e,							/* Log importação arquivo CCR3 - campo número da conta */
+		tbcrd_linha_arquivo d,								/* Log importação arquivo CCR3 - linha */
+		tbcrd_arquivo c,								/* Log importação arquivo CCR3 - capa */
+		crawcrd b									/* Propostas de cartão */
+	where	nvl(trim(j.dsvalor),'000')		<> '004'				/* Somente se o código de rejeição for diferente de 004 */
+	and	j.nmcampo				= 'CODREJEI'
+	and	d.idlinha_arquivo			= j.idlinha_arquivo
+	and	i.nmcampo				= 'NRCRCARD'
+	and	d.idlinha_arquivo			= i.idlinha_arquivo
+	and	substr(h.dsvalor,length(h.dsvalor)-5,6)	<> '000000'				/* Somente se a conta cartão não estiver zerada */
+	and	h.nmcampo				= 'NRCONTA_CARTAO'
+	and	d.idlinha_arquivo			= h.idlinha_arquivo
+	and	g.dsvalor				= '01'
+	and	g.nmcampo				= 'TPOPERACAO'
+	and	d.idlinha_arquivo			= g.idlinha_arquivo
+	and	f.dsvalor				= '02'					/* Tipo de registro = dados do cartão */
+	and	f.nmcampo				= 'TPREGISTRO'
+	and	d.idlinha_arquivo			= f.idlinha_arquivo
+	and	e.nmcampo				= 'NRCONTA_DEBITAR'
+	and	ltrim(e.dsvalor,'0')			= b.nrdconta
+	and	d.idlinha_arquivo			= e.idlinha_arquivo
+	and	d.dsinformacao				is null
+	and	c.idarquivo				= d.idarquivo
+	and	c.idarquivo				= 101					/* Restringir arquivo do dia 21/09/2021 */
+	and	(nvl(b.nrcrcard,0) = 0 or b.nrcrcard = 111111)					/* Número do cartão zerado */
+	and	b.nrcctitg				= 0					/* Número da conta cartão zerada */
+	and	b.insitcrd				in (1,2)
+	and	b.dtpropos				between to_date('17/09/2021','dd/mm/yyyy') and to_date('20/09/2021 23:59:59','dd/mm/yyyy hh24:mi:ss')	/* Considerar casos da última execução da nova rotina 672 */
+	) a
+where	substr(a.nr_conta_cartao,length(a.nr_conta_cartao)-5,6) <> '000000'			/* Desconsiderar conta cartão com número zerado */
+/* Somente se não existir registro da conta cartão */
+and	not exists
+	(select	1
+	from	tbcrd_conta_cartao x
+	where	x.nrdconta		= a.nrdconta
+	and	x.cdcooper		= a.cdcooper
+	and	x.nrconta_cartao	= a.nr_conta_cartao)
+/* Somente se não existir registro de proposta efetivada */
+and	not exists
+	(select	1
+	from	crapcrd x
+	where	x.nrcrcard		= a.nr_cartao
+	and	x.nrdconta		= a.nrdconta
+	and	x.cdcooper		= a.cdcooper);
 
-end; 
+/* Valida se o diretório do rollback existe ou se pode ser criado */
+procedure valida_diretorio_p(	ds_nome_diretorio_p	in	varchar2,
+				ds_critica_p		out	crapcri.dscritic%TYPE) is
 
+ds_critica_v	crapcri.dscritic%type;
+ie_tipo_saida_v	varchar2(3);
+ds_saida_v	varchar2(1000);
+   
+begin
 
-/* Conta 13446924 */
+	/* Verificar se o diretório existe */
+	if	(gene0001.fn_exis_diretorio(ds_nome_diretorio_p)) then
 
-begin 
+		ds_critica_p	:= null;
 
-insert into tbcrd_conta_cartao  (cdcooper,  nrdconta,  nrconta_cartao,  vllimite_global,  cdadmcrd) values (1, 13446924, 7563239818931,  null, 18); 
+	else
 
-update crawcrd  set  nrcrcard = 5161620002890613,  insitcrd = 3,  nrcctitg = 7563239818931,  cdlimcrd = 0,  dtrejeit = null,  dtlibera = trunc(sysdate),  dtentreg = null where  cdcooper = 1 and nrdconta = 13446924 and nrctrcrd = 2384979; 
+		/* Se o diretório não foi encontrado, vamos criá-lo */
+		gene0001.pc_OSCommand_Shell(	pr_des_comando	=> 'mkdir ' || ds_nome_diretorio_p || ' 1> /dev/null',
+						pr_typ_saida	=> ie_tipo_saida_v,
+						pr_des_saida	=> ds_saida_v);
 
-insert into crapcrd  (cdcooper,  nrdconta,  nrcrcard,  nrcpftit,  nmtitcrd,  dddebito,  cdlimcrd,  dtvalida,  nrctrcrd,  cdmotivo,  nrprotoc,  cdadmcrd,  tpcartao,  dtcancel,  flgdebit,  flgprovi) values (1, 13446924, 5161620002890613, 14421705730, 'WELDER  HENRIQUE', 11, 0, null, 2384979, 0, 0, 18, 2, null, 1, 0); 
+		/* Tratar um eventual erro na criação do diretório */
+		if	(ie_tipo_saida_v	= 'ERR') then
 
-end; 
+			ds_critica_v	:= 'CRIAR DIRETORIO ARQUIVO --> Nao foi possivel criar o diretorio para gerar os arquivos. ' || ds_saida_v;
+			raise		ds_exc_erro_v;
 
+		end if;
 
-/* Conta 13448242 */
+		/* Conceder permissão total no novo diretório criado, para que o usuário executor da rotina consiga criar o arquivo de rollback dinâmico */
+		gene0001.pc_OSCommand_Shell(	pr_des_comando	=> 'chmod 777 ' || ds_nome_diretorio_p || ' 1> /dev/null',
+						pr_typ_saida	=> ie_tipo_saida_v,
+						pr_des_saida	=> ds_saida_v);
 
-begin 
+		/* Tratar um eventual erro na concessão das permissões */
+		if	(ie_tipo_saida_v	= 'ERR') then
 
-insert into tbcrd_conta_cartao  (cdcooper,  nrdconta,  nrconta_cartao,  vllimite_global,  cdadmcrd) values (1, 13448242, 7563239818932,  null, 12); 
+			ds_critica_v	:= 'PERMISSAO NO DIRETORIO --> Nao foi possivel adicionar permissao no diretorio dos arquivos. ' || ds_saida_v;
+			raise		ds_exc_erro_v;
 
-update crawcrd  set  nrcrcard = 5127070387422835,  insitcrd = 3,  nrcctitg = 7563239818932,  cdlimcrd = 0,  dtrejeit = null,  dtlibera = trunc(sysdate),  dtentreg = null where  cdcooper = 1 and nrdconta = 13448242 and nrctrcrd = 2383757; 
+		end if;
 
-insert into crapcrd  (cdcooper,  nrdconta,  nrcrcard,  nrcpftit,  nmtitcrd,  dddebito,  cdlimcrd,  dtvalida,  nrctrcrd,  cdmotivo,  nrprotoc,  cdadmcrd,  tpcartao,  dtcancel,  flgdebit,  flgprovi) values (1, 13448242, 5127070387422835, 10411442961, 'RENATA V  SANTOS', 11, 0, null, 2383757, 0, 0, 12, 2, null, 1, 0); 
+	end if;
 
-end; 
+exception
+when	ds_exc_erro_v then
 
+        ds_critica_p	:= ds_critica_v;
 
-/* Conta 13449222 */
+end valida_diretorio_p;
 
-begin 
+begin
 
-insert into tbcrd_conta_cartao  (cdcooper,  nrdconta,  nrconta_cartao,  vllimite_global,  cdadmcrd) values (1, 13449222, 7563239818935,  null, 12); 
+/* Diretório de Produção: /micros/cpd/bacas/INC0110361 */
+if	(gene0001.fn_database_name like '%AYLLOSP%') then
 
-update crawcrd  set  nrcrcard = 5127070387423098,  insitcrd = 3,  nrcctitg = 7563239818935,  cdlimcrd = 0,  dtrejeit = null,  dtlibera = trunc(sysdate),  dtentreg = null where  cdcooper = 1 and nrdconta = 13449222 and nrctrcrd = 2383908; 
+	select	max(a.dsvlrprm)
+	into	ds_nome_diretorio_v
+	from	crapprm a
+	where	a.nmsistem	= 'CRED'
+	and	a.cdcooper	= cd_cooperativa_v
+	and	a.cdacesso	= 'ROOT_MICROS';
 
-insert into crapcrd  (cdcooper,  nrdconta,  nrcrcard,  nrcpftit,  nmtitcrd,  dddebito,  cdlimcrd,  dtvalida,  nrctrcrd,  cdmotivo,  nrprotoc,  cdadmcrd,  tpcartao,  dtcancel,  flgdebit,  flgprovi) values (1, 13449222, 5127070387423098, 61250538904, 'FABIANA S COELHO', 19, 0, null, 2383908, 0, 0, 12, 2, null, 1, 0); 
+	if	(ds_nome_diretorio_v	is null) then
 
-end; 
+		select	max(a.dsvlrprm)
+		into	ds_nome_diretorio_v
+		from	crapprm a
+		where	a.nmsistem	= 'CRED'
+		and	a.cdcooper	= 0
+		and	a.cdacesso	= 'ROOT_MICROS';
 
+	end if;
 
-/* Conta 13451570 */
+	ds_nome_diretorio_v	:= ds_nome_diretorio_v || 'cpd/bacas/INC0110361';
 
-begin 
+/* Diretório de Homologação: /progress/f0033552/usr/coop/cecred/INC0110361 */
+else
 
-insert into tbcrd_conta_cartao  (cdcooper,  nrdconta,  nrconta_cartao,  vllimite_global,  cdadmcrd) values (1, 13451570, 7563239818940,  null, 16); 
+	ds_nome_diretorio_v	:= gene0001.fn_diretorio(	pr_tpdireto => 'C',
+								pr_cdcooper => 3);
 
-update crawcrd  set  nrcrcard = 6393500219514230,  insitcrd = 3,  nrcctitg = 7563239818940,  cdlimcrd = 0,  dtrejeit = null,  dtlibera = trunc(sysdate),  dtentreg = null where  cdcooper = 1 and nrdconta = 13451570 and nrctrcrd = 2384353; 
+	ds_nome_diretorio_v	:= ds_nome_diretorio_v || '/INC0110361';
 
-insert into crapcrd  (cdcooper,  nrdconta,  nrcrcard,  nrcpftit,  nmtitcrd,  dddebito,  cdlimcrd,  dtvalida,  nrctrcrd,  cdmotivo,  nrprotoc,  cdadmcrd,  tpcartao,  dtcancel,  flgdebit,  flgprovi) values (1, 13451570, 6393500219514230, 9264777903, 'IOHANA C DE JESUS', 0, 0, null, 2384353, 0, 0, 16, 2, null, 1, 0); 
+end if;
+ 
+/* Validar diretório de rollback e criar pasta do incidente */
+valida_diretorio_p(	ds_nome_diretorio_p	=> ds_nome_diretorio_v,
+			ds_critica_p		=> ds_critica_v);
 
-end; 
+/* Tratar eventual erro no diretório */
+if	(trim(ds_critica_v) is not null) then
 
+	raise ds_exc_erro_v;
 
-/* Conta 13452894 */
+end if;
 
-begin 
+/* Inicializa as variáveis de rollback */
+dbms_lob.createtemporary(ds_dados_rollback_v, true, dbms_lob.CALL);
+dbms_lob.open(ds_dados_rollback_v, dbms_lob.lob_readwrite);
+gene0002.pc_escreve_xml(ds_dados_rollback_v, ds_texto_rollback_v, '-- Programa para rollback das informacoes INC0110361'||chr(13), false);
+gene0002.pc_escreve_xml(ds_dados_rollback_v, ds_texto_rollback_v, 'begin'||chr(13), false);  
+ds_nome_arquivo_bkp_v	:= 'ROLLBACK_INC0110361_'||to_char(sysdate,'hh24miss')||'.sql';
 
-insert into tbcrd_conta_cartao  (cdcooper,  nrdconta,  nrconta_cartao,  vllimite_global,  cdadmcrd) values (1, 13452894, 7563239818943,  null, 18); 
+for	r01 in c01 loop
 
-update crawcrd  set  nrcrcard = 5161620002891132,  insitcrd = 3,  nrcctitg = 7563239818943,  cdlimcrd = 0,  dtrejeit = null,  dtlibera = trunc(sysdate),  dtentreg = null where  cdcooper = 1 and nrdconta = 13452894 and nrctrcrd = 2385342; 
+	begin
 
-insert into crapcrd  (cdcooper,  nrdconta,  nrcrcard,  nrcpftit,  nmtitcrd,  dddebito,  cdlimcrd,  dtvalida,  nrctrcrd,  cdmotivo,  nrprotoc,  cdadmcrd,  tpcartao,  dtcancel,  flgdebit,  flgprovi) values (1, 13452894, 5161620002891132, 8916998999, 'ITALINA F SANTOS', 11, 0, null, 2385342, 0, 0, 18, 2, null, 1, 0); 
+	ds_rowid_crapcrd_v	:= null;
 
-end; 
+	/* Executar os principais comandos do BACA */
+	insert	into tbcrd_conta_cartao
+		(cdcooper,
+		nrdconta,
+		nrconta_cartao,
+		vllimite_global,
+		cdadmcrd)
+	values	(r01.cdcooper,
+		r01.nrdconta,
+		r01.nr_conta_cartao,
+		null,
+		r01.cdadmcrd)
+	returning rowid into ds_rowid_tbcrd_conta_cartao_v;
+
+	update	crawcrd
+	set	nrcrcard	= r01.nr_cartao,
+		insitcrd	= 3,
+		nrcctitg	= r01.nr_conta_cartao,
+		cdlimcrd	= r01.cdlimcrd,
+		dtrejeit	= null,
+		dtlibera	= trunc(sysdate),
+		dtentreg	= null
+	where	rowid		= r01.ds_rowid_crawcrd;
+
+	insert	into crapcrd
+		(cdcooper,
+		nrdconta,
+		nrcrcard,
+		nrcpftit,
+		nmtitcrd,
+		dddebito,
+		cdlimcrd,
+		dtvalida,
+		nrctrcrd,
+		cdmotivo,
+		nrprotoc,
+		cdadmcrd,
+		tpcartao,
+		dtcancel,
+		flgdebit,
+		flgprovi)
+	values	(r01.cdcooper,
+		r01.nrdconta,
+		r01.nr_cartao,
+		r01.nrcpftit,
+		r01.nmtitcrd,
+		r01.dddebito,
+		r01.cdlimcrd,
+		r01.dtvalida,
+		r01.nrctrcrd,
+		r01.cdmotivo,
+		r01.nrprotoc,
+		r01.cdadmcrd,
+		r01.tpcartao,
+		r01.dtcancel,
+		r01.flgdebit,
+		r01.ie_provisorio)
+	returning rowid into ds_rowid_crapcrd_v;
+
+	/* Alimenta o script de rollback */
+	if	(ds_rowid_crapcrd_v		is not null) and
+		(ds_rowid_tbcrd_conta_cartao_v	is not null) and
+		(r01.ds_rowid_crawcrd		is not null) then
+
+		gene0002.pc_escreve_xml(	ds_dados_rollback_v,
+						ds_texto_rollback_v,
+						'delete	from tbcrd_conta_cartao ' || chr(13) ||
+						'where rowid = ' || chr(39) || ds_rowid_tbcrd_conta_cartao_v || chr(39) || '; ' || chr(13) || chr(13) ||
+
+						'update	crawcrd ' || chr(13) ||
+						'set nrcrcard = ' || r01.nrcrcard || ', ' || chr(13) ||
+						'insitcrd = ' || r01.insitcrd || ', ' || chr(13) ||
+						'nrcctitg = ' || r01.nrcctitg || ', ' || chr(13) ||
+						'cdlimcrd = ' || r01.cdlimcrd || ', ' || chr(13) ||
+						'dtrejeit = ' || r01.ds_dtrejeit || ', ' || chr(13) ||
+						'dtlibera = ' || r01.ds_dtlibera || ', ' || chr(13) ||
+						'dtentreg = ' || r01.ds_dtentreg || chr(13) ||
+						'where rowid = ' || chr(39) || r01.ds_rowid_crawcrd || chr(39) || '; ' || chr(13) || chr(13) ||
+
+						'delete	from crapcrd '	|| chr(13) ||
+						'where rowid = ' || chr(39) || ds_rowid_crapcrd_v || chr(39) ||  '; ' || chr(13) || chr(13), false);
+
+	end if;
+
+	commit;
+
+	exception
+	when others then
+		rollback;
+	end;
+
+end loop;
+
+/* Concluindo script de rollback */
+gene0002.pc_escreve_xml(ds_dados_rollback_v, ds_texto_rollback_v, 'commit;'||chr(13), FALSE);
+gene0002.pc_escreve_xml(ds_dados_rollback_v, ds_texto_rollback_v, 'end;'||chr(13), FALSE);
+gene0002.pc_escreve_xml(ds_dados_rollback_v, ds_texto_rollback_v, chr(13), TRUE);
+
+/* Gravar o arquivo de rollback */
+gene0002.pc_solicita_relato_arquivo(	pr_cdcooper	=> cd_cooperativa_v,					/* Cooperativa conectada */
+					pr_cdprogra	=> 'ATENDA',						/* Programa chamador - utilizamos apenas um existente */
+					pr_dtmvtolt	=> trunc(sysdate),					/* Data do movimento atual */
+					pr_dsxml	=> ds_dados_rollback_v,					/* Arquivo XML de dados */
+					pr_dsarqsaid	=> ds_nome_diretorio_v || '/' || ds_nome_arquivo_bkp_v,	/* Path/Nome do arquivo gerado */
+					pr_flg_impri	=> 'N',							/* Chamar a impressão (Imprim.p) */
+					pr_flg_gerar	=> 'S',							/* Gerar o arquivo na hora */
+					pr_flgremarq	=> 'N',							/* Remover arquivo apos geracao */
+					pr_nrcopias	=> 1,							/* Número de cópias para impressão */
+					pr_des_erro	=> ds_critica_v);					/* Retorno de erro */
+
+/* Tratar eventual erro ao criar arquivo de rollback */
+if	(trim(ds_critica_v) is not null) then
+
+	raise	ds_exc_erro_v;
+
+end if;
+
+/* Liberando a memória alocada para o clob */
+dbms_lob.close(ds_dados_rollback_v);
+dbms_lob.freetemporary(ds_dados_rollback_v);
 
 commit;
+
+exception
+when	ds_exc_erro_v then
+
+	raise_application_error(-20111, ds_critica_v);
+
+end;
 
 end;
