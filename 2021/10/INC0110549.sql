bfff1f4e631418cea96e48c0d0bba928ee81ffbf
Francisco Krautchuk Neto <premiersoft-francisco.neto@ailos.coop.br>
2021-11-05 14:20:30 -0300

    INC0110549 - Ajuste de Logs em LGM e LGI

--- 2021/10/INC0110549.sql
+++ 2021/10/INC0110549.sql
@@ -1,6 +1,11 @@
-declare 
-  vr_nrsequenlgi INTEGER;
-  vr_nrsequenlgm INTEGER;
+declare
+  vr_dttransa cecred.craplgm.dttransa%type;
+  vr_hrtransa cecred.craplgm.hrtransa%type;
+  vr_nrdconta cecred.crapass.nrdconta%type;
+  vr_cdcooper cecred.crapcop.cdcooper%type;
+  vr_cdoperad cecred.craplgm.cdoperad%TYPE;
+  vr_dscritic cecred.craplgm.dscritic%TYPE;
+  vr_nrdrowid ROWID;
 
   CURSOR cr_craplcm is
     SELECT DTMVTOLT,
@@ -29,8 +34,10 @@ declare
 
 begin
 
-  vr_nrsequenlgi := 1;
-  vr_nrsequenlgm := 1;
+  vr_dttransa := trunc(sysdate);
+  vr_hrtransa := GENE0002.fn_busca_time;
+  vr_cdcooper := 1;
+  vr_nrdconta := 6212034;
 
   open cr_craplcm;
   fetch cr_craplcm
@@ -42,414 +49,97 @@ begin
      and a.cdhistor = 1545
      and a.nrseqdig = 22648298
      and a.vllanmto = 764.96;
-  INSERT INTO cecred.craplgm
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     dstransa,
-     dsorigem,
-     nmdatela,
-     flgtrans,
-     dscritic,
-     cdoperad,
-     nmendter)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgm,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     'INC0110549 - Lançamento de débito da fatura não sensibilizada.',
-     'AIMARO',
-     '',
-     1,
-     ' ',
-     1,
-     ' ');
 
-  vr_nrsequenlgm := vr_nrsequenlgm + 1;
+  GENE0001.pc_gera_log(pr_cdcooper => vr_cdcooper,
+                       pr_cdoperad => vr_cdoperad,
+                       pr_dscritic => vr_dscritic,
+                       pr_dsorigem => 'AIMARO',
+                       pr_dstransa => 'INC0110549 - Lançamento de débito da fatura não sensibilizada.',
+                       pr_dttransa => to_date('12-07-2021', 'dd-mm-yyyy'),
+                       pr_flgtrans => 1,
+                       pr_hrtransa => 46608,
+                       pr_idseqttl => 0,
+                       pr_nmdatela => NULL,
+                       pr_nrdconta => vr_nrdconta,
+                       pr_nrdrowid => vr_nrdrowid);
 
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.DTMVTOLT',
-     rg_craplcm.DTMVTOLT,
-     null);
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.DTMVTOLT',
+                            pr_dsdadant => rg_craplcm.DTMVTOLT,
+                            pr_dsdadatu => null);
 
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.CDAGENCI',
+                            pr_dsdadant => rg_craplcm.CDAGENCI,
+                            pr_dsdadatu => null);
 
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.CDAGENCI',
-     rg_craplcm.CDAGENCI,
-     null);
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.CDBCCXLT',
+                            pr_dsdadant => rg_craplcm.CDBCCXLT,
+                            pr_dsdadatu => null);
 
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.NRDOLOTE',
+                            pr_dsdadant => rg_craplcm.NRDOLOTE,
+                            pr_dsdadatu => null);
 
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.CDBCCXLT',
-     rg_craplcm.CDBCCXLT,
-     null);
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.NRDCONTA',
+                            pr_dsdadant => rg_craplcm.NRDCONTA,
+                            pr_dsdadatu => null);
 
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.NRDOCMTO',
+                            pr_dsdadant => rg_craplcm.NRDOCMTO,
+                            pr_dsdadatu => null);
 
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.NRDOLOTE',
-     rg_craplcm.NRDOLOTE,
-     null);
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.CDHISTOR',
+                            pr_dsdadant => rg_craplcm.CDHISTOR,
+                            pr_dsdadatu => null);
 
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.NRSEQDIG',
+                            pr_dsdadant => rg_craplcm.NRSEQDIG,
+                            pr_dsdadatu => null);
 
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.NRDCONTA',
-     rg_craplcm.NRDCONTA,
-     null);
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.VLLANMTO',
+                            pr_dsdadant => rg_craplcm.VLLANMTO,
+                            pr_dsdadatu => null);
 
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.NRDCTABB',
+                            pr_dsdadant => rg_craplcm.NRDCTABB,
+                            pr_dsdadatu => null);
 
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.NRDOCMTO',
-     rg_craplcm.NRDOCMTO,
-     null);
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.CDPESQBB',
+                            pr_dsdadant => rg_craplcm.CDPESQBB,
+                            pr_dsdadatu => null);
 
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.DTREFERE',
+                            pr_dsdadant => rg_craplcm.DTREFERE,
+                            pr_dsdadatu => null);
 
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.CDHISTOR',
-     rg_craplcm.CDHISTOR,
-     null);
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.HRTRANSA',
+                            pr_dsdadant => rg_craplcm.HRTRANSA,
+                            pr_dsdadatu => null);
 
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.CDCOOPER',
+                            pr_dsdadant => rg_craplcm.CDCOOPER,
+                            pr_dsdadatu => null);
 
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.NRSEQDIG',
-     rg_craplcm.NRSEQDIG,
-     null);
-
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
-
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.VLLANMTO',
-     rg_craplcm.VLLANMTO,
-     null);
-
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
-
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.NRDCTABB',
-     rg_craplcm.NRDCTABB,
-     null);
-
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
-
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.CDPESQBB',
-     rg_craplcm.CDPESQBB,
-     null);
-
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
-
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.DTREFERE',
-     rg_craplcm.DTREFERE,
-     null);
-
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
-
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.HRTRANSA',
-     rg_craplcm.HRTRANSA,
-     null);
-
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
-
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.CDCOOPER',
-     rg_craplcm.CDCOOPER,
-     null);
-
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
-
-  INSERT INTO cecred.craplgi
-    (cdcooper,
-     nrdconta,
-     idseqttl,
-     nrsequen,
-     dttransa,
-     hrtransa,
-     nrseqcmp,
-     nmdcampo,
-     dsdadant,
-     dsdadatu)
-  VALUES
-    (1,
-     6212034,
-     1,
-     vr_nrsequenlgi,
-     to_date('12-07-2021', 'dd-mm-yyyy'),
-     46608,
-     1,
-     'craplcm.DTTRANS',
-     rg_craplcm.DTTRANS,
-     null);
-
-  vr_nrsequenlgi := vr_nrsequenlgi + 1;
+  GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                            pr_nmdcampo => 'craplcm.DTTRANS',
+                            pr_dsdadant => rg_craplcm.DTTRANS,
+                            pr_dsdadatu => null);
 
   close cr_craplcm;
-      
+
   COMMIT;
 
 EXCEPTION
@@ -457,4 +147,3 @@ EXCEPTION
     ROLLBACK;
     RAISE_APPLICATION_ERROR(-20000, 'Erro ao executar script: ' || SQLERRM);
 END;
-
