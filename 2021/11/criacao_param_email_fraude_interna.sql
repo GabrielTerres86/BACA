e51521dbd3210f81c40c2de8ee809ee92f8c0126
Petrônio Sifuentes Gomes Júnior <mouts-petronio.junior@ailos.coop.br>
2021-11-08 16:08:49 -0300

    Criação de email de monitoramento de fraude interna

--- 2021/11/criacao_param_email_fraude_interna.sql
+++ 2021/11/criacao_param_email_fraude_interna.sql
@@ -1,6 +1,6 @@
 declare
 begin 
-INSERT INTO cecred.crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) values ('CRED', 0, 'MONIT_EMAIL_FRAUDE_INT', 'Email de destino dos monitoramentos.', 'ifraude@ailos.coop.br');
+INSERT INTO cecred.crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) values ('CRED', 0, 'MONIT_EMAIL_FRAUDE_INT', 'Email de destino dos monitoramentos.', 'zuleica.arnese@auditeste.com.br');
 commit;
 end;
 /
