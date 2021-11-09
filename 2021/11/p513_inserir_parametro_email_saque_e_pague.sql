0a843e23d77da2560ae27a4d9b18e41c637699af
Djonata Wehmuth <djonata.wehmuth@ailos.coop.br>
2021-11-09 16:35:00 -0300

    Create p513_inserir_parametro_email_saque_e_pague.sql

---
+++ 2021/11/p513_inserir_parametro_email_saque_e_pague.sql
@@ -0,0 +1,7 @@
+begin
+
+insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 0, 'SEP_EMAIL_ERRO', 'Email das pessoas responsáveis por receber notificação da sonda se ocorrer erro de autenticação ou estiver offline', 'cac_sep@ailos.coop.br');
+
+ commit;
+end;
