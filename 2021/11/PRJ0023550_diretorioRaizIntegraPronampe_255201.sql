b22d255dc52c2d65a7bbe6558b8d2aece7c2058d
Giovane Diego Gibbert <giovane.gibbert@ailos.coop.br>
2021-11-11 08:51:41 -0300

    Ajuste nos script

--- 2021/11/PRJ0023550_diretorioRaizIntegraPronampe_255201.sql
+++ 2021/11/PRJ0023550_diretorioRaizIntegraPronampe_255201.sql
@@ -1,6 +1,6 @@
 BEGIN
 
-  INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+  INSERT INTO cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
   VALUES ('CRED', 0, 'DIR_BB_CONNECTDIRECT', 'Diretório raiz para integração de arquivos com sistemas do Programa Pronampe', '/usr/connect/bb/');
 
   COMMIT;
