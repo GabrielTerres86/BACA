67d6ba49730d6fa0febc432ac58d723005488260
capgemini-rodrigo.erasmo <capgemini-rodrigo.erasmo@ailos.coop.br>
2021-11-16 17:32:30 -0300

    Criação de novo motivo para instruções em boletos via API

---
+++ 2021/11/PRJ0023837_motivo_16_api.sql
@@ -0,0 +1,42 @@
+declare
+  v_erro varchar2(2000);
+begin
+
+  FOR rec IN (select cdcooper From crapcop) LOOP
+    BEGIN
+    
+      INSERT INTO crapmot
+        (CDCOOPER,
+         CDDBANCO,
+         CDOCORRE,
+         TPOCORRE,
+         CDMOTIVO,
+         DSMOTIVO,
+         DSABREVI,
+         CDOPERAD,
+         DTALTERA,
+         HRTRANSA)
+      VALUES
+        (rec.cdcooper,
+         85,
+         9,
+         2,
+         16,
+         'Comanda Cliente Online API',
+         ' ',
+         1,
+         TRUNC(SYSDATE),
+         TO_NUMBER(TO_CHAR(SYSDATE, 'SSSSS')));
+    
+    END;
+  END LOOP;
+
+  commit;
+
+exception
+  when others then
+    v_erro := sqlerrm;
+    rollback;
+    dbms_output.put_line(v_erro);
+    raise_application_error(-20003, v_erro);
+end;
