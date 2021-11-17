1c4af662dbdd391ad45bcf58724d58b04f941e1d
matheus klug <matheus.klug@ailos.coop.br>
2021-11-17 09:00:57 -0300

    Ajuste em registros para continuação dos testes

--- 2021/11/testes_cancelamento_pronampe.sql
+++ 2021/11/testes_cancelamento_pronampe.sql
@@ -1,5 +1,5 @@
 begin
   update credito.tbcred_pronampe_contrato p set p.dtcancelamento = sysdate
-  WHERE p.cdcooper = 1 AND p.dtcancelamento IS NOT NULL and to_date(p.dtcancelamento, 'DD/MM/YYYY') = to_date('12/11/2021', 'DD/MM/YYYY')  
+  WHERE p.cdcooper = 1 AND p.dtcancelamento IS NOT NULL and to_date(p.dtcancelamento, 'DD/MM/YYYY') = to_date('12/11/2021', 'DD/MM/YYYY');  
 commit;
 end;
