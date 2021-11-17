0e631002cba64402f39eecd4400b3d42c85df848
Anderson de Carvalho Almeida <mouts-anderson.almeida@cecred.coop.br>
2021-11-17 15:10:33 -0300

    Insert Ajuste Parcela Teste

---
+++ 2021/11/insert_ajuste_parcela.sql
@@ -0,0 +1,59 @@
+insert into tbgen_evento_soa (IDEVENTO, CDCOOPER, NRDCONTA, NRCTRPRP, TPEVENTO, TPRODUTO_EVENTO, TPOPERACAO, DHOPERACAO, DSPROCESSAMENTO, DSSTATUS, DHEVENTO, DSERRO, NRTENTATIVAS, DSCONTEUDO_REQUISICAO)
+values (5025287, 10, 134090, 16316, 'PAGTO_PAGAR', 'CONSIGNADO', 'INSERT', '16/11/21 16:21:25,288231', null, null, null, null, null, '<?xml version="1.0" encoding="UTF-8"?><Root><convenioCredito>
+                    <cooperativa>
+                      <codigo>10</codigo>
+                    </cooperativa>
+                    <numeroContrato>16316</numeroContrato>
+                  </convenioCredito>
+                  <propostaContratoCredito>
+                    <emitente>
+                      <contaCorrente>
+                        <codigoContaSemDigito>134090</codigoContaSemDigito>
+                      </contaCorrente>
+                    </emitente>
+                  </propostaContratoCredito>
+                  <lote>
+                    <tipoInteracao>
+                      <codigo>INSTALLMENT_SETTLEMENT</codigo> 
+                    </tipoInteracao>
+                  </lote>
+                  <transacaoContaCorrente>
+                    <tipoInteracao>
+                      <codigo>DEBITO</codigo>  
+                    </tipoInteracao>
+                  </transacaoContaCorrente> <parcela>
+                            <dataEfetivacao>2021-10-22T16:21:25</dataEfetivacao>
+                            <dataVencimento>2024-01-10</dataVencimento>
+                            <identificador>829106</identificador> <valor>280.29</valor></parcela><motivoEnvio>REENVIARPAGTO</motivoEnvio><interacaoGrafica>
+                    <dataAcaoUsuario>2021-11-12T16:21:25</dataAcaoUsuario>
+                  </interacaoGrafica></Root>');
+
+insert into tbgen_evento_soa (IDEVENTO, CDCOOPER, NRDCONTA, NRCTRPRP, TPEVENTO, TPRODUTO_EVENTO, TPOPERACAO, DHOPERACAO, DSPROCESSAMENTO, DSSTATUS, DHEVENTO, DSERRO, NRTENTATIVAS, DSCONTEUDO_REQUISICAO)
+values (5025288, 10, 134090, 16316, 'PAGTO_PAGAR', 'CONSIGNADO', 'INSERT', '16/11/21 16:21:25,289489', null, null, null, null, null, '<?xml version="1.0" encoding="UTF-8"?><Root><convenioCredito>
+                    <cooperativa>
+                      <codigo>10</codigo>
+                    </cooperativa>
+                    <numeroContrato>16316</numeroContrato>
+                  </convenioCredito>
+                  <propostaContratoCredito>
+                    <emitente>
+                      <contaCorrente>
+                        <codigoContaSemDigito>134090</codigoContaSemDigito>
+                      </contaCorrente>
+                    </emitente>
+                  </propostaContratoCredito>
+                  <lote>
+                    <tipoInteracao>
+                      <codigo>INSTALLMENT_SETTLEMENT</codigo> 
+                    </tipoInteracao>
+                  </lote>
+                  <transacaoContaCorrente>
+                    <tipoInteracao>
+                      <codigo>DEBITO</codigo>  
+                    </tipoInteracao>
+                  </transacaoContaCorrente> <parcela>
+                            <dataEfetivacao>2021-10-22T16:21:25</dataEfetivacao>
+                            <dataVencimento>2024-02-10</dataVencimento>
+                            <identificador>829107</identificador> <valor>272.91</valor></parcela><motivoEnvio>REENVIARPAGTO</motivoEnvio><interacaoGrafica>
+                    <dataAcaoUsuario>2021-11-12T16:21:25</dataAcaoUsuario>
+                  </interacaoGrafica></Root>');
