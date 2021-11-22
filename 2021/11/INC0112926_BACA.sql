d69faa3dca00a1baa59af50e362bf6ebba83a4c7
Anderson de Carvalho Almeida <mouts-anderson.almeida@cecred.coop.br>
2021-11-22 17:42:47 -0300

    Ajuste do valor do contrato

---
+++ 2021/11/INC0112926_BACA.sql
@@ -0,0 +1,18 @@
+BEGIN
+  UPDATE crapepr pr
+     SET inliquid = 1, vlsdeved = 0
+   WHERE pr.tpemprst = 1
+   AND pr.tpdescto = 2
+   AND pr.inliquid = 0
+   AND NOT EXISTS (SELECT 1 FROM crappep p
+                    WHERE p.cdcooper = pr.cdcooper
+                      AND p.nrdconta = pr.nrdconta
+                      AND p.nrctremp = pr.nrctremp
+                      AND p.inliquid = 0);
+  COMMIT;
+  
+EXCEPTION
+    WHEN OTHERS THEN
+         ROLLBACK;
+		 
+END;
