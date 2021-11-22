5506fd33264710e7951bca4a43c760e70d4b3b5c
Paulo Martins (Mouts) <t0031812@ailos.coop.br>
2021-11-22 19:14:12 +0000

    Commit

--- 2021/11/desabilita_jobs_imobiliario.sql
+++ 2021/11/desabilita_jobs_imobiliario.sql
@@ -3,6 +3,7 @@ dbms_scheduler.disable(name=>'CREDITO.JBEPR_PROCESSA_TED_IMOB');
 dbms_scheduler.disable(name=>'CREDITO.JBEPR_PROCESSA_PAG_IMOB');
 dbms_scheduler.disable(name=>'CREDITO.JBEPR_PROCESSA_RET_IMOB');
 dbms_scheduler.disable(name=>'CREDITO.JBEPR_IMOB_IMP_CONTRATO');
+commit;
 exception
  when others then
   null;
