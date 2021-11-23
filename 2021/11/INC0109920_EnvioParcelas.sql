4c72648a9536a5723cc1aabb40989488d660b568
Anderson de Carvalho Almeida <mouts-anderson.almeida@cecred.coop.br>
2021-11-23 10:04:07 -0300

    Ajuste no arquivo envio de parcelas

--- 2021/11/INC0109920_EnvioParcelas.sql
+++ 2021/11/INC0109920_EnvioParcelas.sql
@@ -86,7 +86,27 @@ BEGIN
                                     ,pr_dsconteudo_requisicao  => vr_dsxmlali.getClobVal()
                                     ,pr_idevento               => vr_idevento
                                     ,pr_dscritic               => vr_dscritic);
-    END LOOP;    
+    END LOOP; 
+
+	UPDATE crappep
+       SET vlpagpar = 320.93,
+           inliquid = 1,
+           vlsdvpar = 0,
+           vlsdvatu = 0
+     WHERE cdcooper = 13
+       AND nrdconta = 172448
+       AND nrctremp = 106536
+       AND inliquid = 0;
+     
+    UPDATE crappep
+       SET vlpagpar = 16.77,
+           inliquid = 1,
+           vlsdvpar = 0,
+           vlsdvatu = 0
+     WHERE cdcooper = 13
+       AND nrdconta = 172448
+       AND nrctremp = 106540
+       AND inliquid = 0;
                                 
     COMMIT;
     
