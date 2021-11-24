f3753edbf713bc020f7726b9bdfe05eb2dab329d
Alcemir Junior <alcemir.junior@mouts.info>
2021-11-24 19:23:04 -0300

    PRJ0023820 - update crapaca valida valor adesao
    update crapaca valida valor adesao

---
+++ 2021/11/PRJ0023820_update_crapaca.sql
@@ -0,0 +1,8 @@
+BEGIN
+  UPDATE crapaca a SET a.lstparam = 'pr_nrdconta,pr_cdprodut,pr_vlcontra,pr_cddchave,pr_vltrnot,pr_vlboont,pr_cdvltrn,pr_cdvlbon'
+   WHERE a.nmdeacao = 'VALIDA_VALOR_ADESAO'
+     AND a.nmpackag = 'CADA0006';
+
+  COMMIT;     
+
+END;
