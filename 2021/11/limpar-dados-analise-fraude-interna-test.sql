3c5495120a8dc5d6296bb2f74eaeaae34d8b3cea
Petrônio Sifuentes Gomes Júnior <mouts-petronio.junior@ailos.coop.br>
2021-11-26 14:08:44 -0300

    Limpeza de dados antigos das tabelas

--- 2021/11/limpar-dados-analise-fraude-interna-test.sql
+++ 2021/11/limpar-dados-analise-fraude-interna-test.sql
@@ -1,7 +1,7 @@
 declare
 begin 
-delete from tbsegcorp_analise_fra_int_reg;
-delete from tbsegcorp_analise_fra_int;
+delete from cecred.tbsegcorp_analise_fra_int_reg;
+delete from cecred.tbsegcorp_analise_fra_int;
 commit;
 end;
 /
