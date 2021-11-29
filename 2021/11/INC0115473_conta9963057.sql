6de1a910953ab7c58317d57f3a7c25c4787ad176
Jaison Fernando Ferreira <jaison.ferreira@ailos.coop.br>
2021-11-29 17:41:06 -0300

    Ajuste de data

--- 2021/11/INC0115473_conta9963057.sql
+++ 2021/11/INC0115473_conta9963057.sql
@@ -6,7 +6,7 @@ BEGIN
   DELETE FROM crapsim sim
    WHERE sim.cdcooper = vr_cdcooper
      AND sim.nrdconta = vr_nrdconta
-     AND sim.dtmvtolt < to_date('01/09/2021','dd/mm/rrrr')
+     AND sim.dtmvtolt < to_date('01/10/2021','dd/mm/rrrr')
      AND NOT EXISTS ( SELECT 1
                         FROM crawepr wpr
                        WHERE wpr.cdcooper = sim.cdcooper
