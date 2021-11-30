09d3d06adad7030defe5bf8d05bfc705ec666e64
Petrônio Sifuentes Gomes Júnior <mouts-petronio.junior@ailos.coop.br>
2021-11-30 17:23:38 -0300

    alteração de e-mail para teste

--- 2021/11/update-crapprm.sql
+++ 2021/11/update-crapprm.sql
@@ -1,8 +1,16 @@
 declare
 begin  
 UPDATE cecred.crapprm
-   SET dsvlrprm = 'zuleica.arnese@auditeste.com.br'
+   SET dsvlrprm = 'prev.fraudes03@ailos.coop.br'
 WHERE cdacesso = 'EMAIL_RISCOFRAUDE_ADM';
+
+UPDATE cecred.crapprm
+   SET dsvlrprm = 'false'
+WHERE cdacesso = 'DATATRUST_ATIVO_ADM_P'; 
+
+update crapprm
+ set dsvlrprm = 'false'
+where cdacesso = 'DATATRUST_ATIVO_ADM_D';
 commit;
 end;
 /
