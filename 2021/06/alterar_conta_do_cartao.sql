6a870b37e139103ce85266e1d9d157d810339fcc
João Luis Mendes <joao.brito@ailos.coop.br>
2021-12-02 17:05:59 -0300

    Update alterar_conta_do_cartao.sql
      -- Cooperativa de destino do cartão
      vr_cooperativa := 1;
      -- Conta de destino do cartão
      vr_conta :=  329;
    
    -- Numero do cartão que precisamos ajustar
     --vr_cartao := 5158940000199642; -- AILOS 1
    -- vr_cartao := 6393500065179583; -- AILOS 1
    -- vr_cartao := 5588190184171591; -- AILOS 2
    -- vr_cartao := 5127070161674411; -- GISLON
    -- vr_cartao := 5127070320149719; -- GISLON 2
    vr_cartao := 5588190184171591; -- Topaz

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -8,17 +8,17 @@ DECLARE
 BEGIN
 
   -- Cooperativa de destino do cartão
-  vr_cooperativa := 9;
+  vr_cooperativa := 1;
   -- Conta de destino do cartão
-  vr_conta :=  14010;
+  vr_conta :=  329;
 
 -- Numero do cartão que precisamos ajustar
- vr_cartao := 5158940000199642; -- AILOS 1
+ --vr_cartao := 5158940000199642; -- AILOS 1
 -- vr_cartao := 6393500065179583; -- AILOS 1
 -- vr_cartao := 5588190184171591; -- AILOS 2
 -- vr_cartao := 5127070161674411; -- GISLON
 -- vr_cartao := 5127070320149719; -- GISLON 2
--- vr_cartao := 5588190184171591; -- Topaz
+vr_cartao := 5588190184171591; -- Topaz
 -- vr_cartao := 5158940000000188; -- Topaz - Lab Demandas
 -- vr_cartao := 5156010019676523; -- Saque & Pague
   
