bdd3107bddd64b5e81a34129bcdf9509744734f9
CECRED\f0033505 <filipe.cardozo@ailos.coop.br>
2021-12-02 14:06:53 -0300

    Atualizar os pagamentos enviados que ficaram com status pendentes Bancoob

---
+++ 2021/12/atualizar_registros_pagfor.sql
@@ -0,0 +1,7 @@
+﻿BEGIN
+  UPDATE tbconv_registro_remessa_pagfor r
+     SET r.cdstatus_processamento = 2,
+         r.dhinclusao_processamento = SYSDATE
+   WHERE r.idsicredi IN (4520983,4521129,4521130,4521132,4521133,4521150,4521151,4521159,4521166,4521214,4521216,4521222,4521228,4521247,4521254,4521257,4521274);
+ COMMIT;     
+END;
