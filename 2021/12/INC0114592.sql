3d5d9a27428b152593d19d0f3344790421d8d0c9
Cristiano Schroeder <cristiano.schroeder@ailos.coop.br>
2021-12-02 11:23:42 -0300

    inclusao contrato 80438792

--- 2021/12/INC0114592.sql
+++ 2021/12/INC0114592.sql
@@ -83,6 +83,49 @@ order by b.cdcooper, b.nrdconta, b.nrctremp, b.nrparepr;
                                   <dataAcaoUsuario>' || dataAcaoUsuario || '</dataAcaoUsuario>
                                 </interacaoGrafica>
                               </Root>';	
+							  
+	v_nrdconta2 tbgen_evento_soa.NRDCONTA%type := 80438792;
+	v_cdcooper2 tbgen_evento_soa.CDCOOPER%type := 1;
+	v_nrctrprp2 tbgen_evento_soa.NRCTRPRP%type := 2956201;
+	v_vlparpag2 VARCHAR2(10) := '893.38';
+  
+  						  
+	conta_80438792_01_21 CLOB := '<?xml version="1.0"?>
+                              <Root>
+                                <convenioCredito>
+                                  <cooperativa>
+                                    <codigo>' ||to_char(v_cdcooper2)|| '</codigo>
+                                  </cooperativa>
+                                  <numeroContrato>' ||to_char(v_nrctrprp2)|| '</numeroContrato>
+                                </convenioCredito>
+                                <propostaContratoCredito>
+                                  <emitente>
+                                    <contaCorrente>
+                                      <codigoContaSemDigito>' ||to_char(v_nrdconta2)|| '</codigoContaSemDigito>
+                                    </contaCorrente>
+                                  </emitente>
+                                </propostaContratoCredito>
+                                <lote>
+                                  <tipoInteracao>
+                                    <codigo>INSTALLMENT_SETTLEMENT</codigo>
+                                  </tipoInteracao>
+                                </lote>
+                                <transacaoContaCorrente>
+                                  <tipoInteracao>
+                                    <codigo>DEBITO</codigo>
+                                  </tipoInteracao>
+                                </transacaoContaCorrente>
+                                <parcela>
+                                  <dataEfetivacao>2021-11-01T07:00:07</dataEfetivacao> 
+                                  <dataVencimento>2021-11-10</dataVencimento>
+                                  <identificador>1068777</identificador>
+                                  <valor>' ||to_char(v_vlparpag2)|| '</valor>
+                                </parcela>
+                                <motivoEnvio>REENVIARPAGTO</motivoEnvio>
+                                <interacaoGrafica>
+                                  <dataAcaoUsuario>' || dataAcaoUsuario || '</dataAcaoUsuario>
+                                </interacaoGrafica>
+                              </Root>';
 	
 BEGIN
   
@@ -120,37 +163,29 @@ BEGIN
     END LOOP;  
 	
 	INSERT INTO tbgen_evento_soa
-	(
-		CDCOOPER,
-		NRDCONTA,
-		NRCTRPRP,
-		TPEVENTO,
-		TPRODUTO_EVENTO,
-		TPOPERACAO,
-		DHOPERACAO,
-		DSPROCESSAMENTO,
-		DSSTATUS,
-		DHEVENTO,
-		DSERRO,
-		NRTENTATIVAS,
-		DSCONTEUDO_REQUISICAO
-	)
+	(CDCOOPER, NRDCONTA, NRCTRPRP, TPEVENTO, TPRODUTO_EVENTO, TPOPERACAO, DHOPERACAO, DSPROCESSAMENTO,
+	 DSSTATUS, DHEVENTO, DSERRO, NRTENTATIVAS, DSCONTEUDO_REQUISICAO)
 	VALUES
-	(
-		v_cdcooper,
-		v_nrdconta,
-		v_nrctrprp,
-		'PAGTO_PAGAR',
-		'CONSIGNADO',
-		'INSERT',
-		SYSDATE,
-		NULL,
-		NULL,
-		NULL,
-		NULL,
-		NULL,
-		conta_525774_09_21
-	);
+	(v_cdcooper, v_nrdconta, v_nrctrprp, 'PAGTO_PAGAR', 'CONSIGNADO', 'INSERT', SYSDATE,
+	 NULL, NULL, NULL, NULL, NULL, conta_525774_09_21);
+	 
+	INSERT INTO tbgen_evento_soa
+	(CDCOOPER, NRDCONTA, NRCTRPRP, TPEVENTO, TPRODUTO_EVENTO, TPOPERACAO, DHOPERACAO, DSPROCESSAMENTO,
+	 DSSTATUS, DHEVENTO, DSERRO, NRTENTATIVAS, DSCONTEUDO_REQUISICAO)
+	VALUES
+	(v_cdcooper2, v_nrdconta2, v_nrctrprp2, 'PAGTO_PAGAR', 'CONSIGNADO', 'INSERT', SYSDATE,
+	 NULL, NULL, NULL, NULL, NULL, conta_80438792_01_21	);
+  
+	UPDATE crappep
+	   SET vlpagpar = 897.98,
+		   inliquid = 1,
+		   vlsdvpar = 0,
+		   vlsdvatu = 0
+	 WHERE cdcooper = v_cdcooper2
+	   AND nrdconta = v_nrdconta2
+	   AND nrctremp = v_nrctrprp2
+	   AND nrparepr = 14
+	   AND inliquid = 0;	 
   
 	UPDATE crappep
 	   SET vlpagpar = 170.60,
