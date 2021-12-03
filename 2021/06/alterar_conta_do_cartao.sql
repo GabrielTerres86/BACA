b8993e66eef249cca7a559451c13118cc096b698
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2021-12-03 11:41:22 -0300

    Remover alteração do Numero do Contrato do Cartão

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -60,14 +60,12 @@ vr_cartao := 5588190184171591; -- Topaz
         ,card.nrcpftit = vr_cpf_titular
         ,card.qtsenerr = 0
         ,card.inacetaa = 1
-        ,card.nrctrcrd = 9
    WHERE card.nrcrcard = vr_cartao;
 
   UPDATE crawcrd card
      SET card.cdcooper = vr_cooperativa
         ,card.nrdconta = vr_conta
         ,card.nrcpftit = vr_cpf_titular
-        ,card.nrctrcrd = 9
    WHERE card.nrcrcard = vr_cartao;
 
   COMMIT;
