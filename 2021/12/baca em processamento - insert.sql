0f50e7730366ece539ee2736274e6cc651c660d7
Guilherme Pinheiro <guilherme.pinheiro@mouts.com.br>
2021-12-07 18:10:59 -0300

    ADD  RAISE_application_error(-20500,SQLERRM);

--- 2021/12/baca em processamento - insert.sql
+++ 2021/12/baca em processamento - insert.sql
@@ -6,9 +6,10 @@ insert into TBGEN_DOMINIO_CAMPO (NMDOMINIO, CDDOMINIO, DSCODIGO)
 values ('INSITIMOVEL', '7', 'Em processamento');
 
 
-commit;
-
-exception
-  when others then
+ COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    RAISE_application_error(-20500,SQLERRM);
     ROLLBACK;
 end;
+/
