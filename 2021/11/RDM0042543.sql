d80f96fef82b23446db068521652399e8a5cddad
Leonardo Henrique Brancalhao <leonardo.brancalhao@ailos.coop.br>
2021-12-08 19:14:54 +0000

    Updated RDM0042543.sql

--- 2021/11/RDM0042543.sql
+++ 2021/11/RDM0042543.sql
@@ -1,3 +1,3 @@
-update parametromobile SET VALOR = '2.36.4' where parametromobileid = 26;
-update parametromobile SET VALOR = '2.36.1' where parametromobileid = 25;
+update parametromobile SET VALOR = '2.37.0' where parametromobileid = 26;
+update parametromobile SET VALOR = '2.37.0' where parametromobileid = 25;
 COMMIT;
