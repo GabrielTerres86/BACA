e7f69c4f7ddcead3c634d67e50263c017aa67b39
Petrônio Sifuentes Gomes Júnior <mouts-petronio.junior@ailos.coop.br>
2021-12-09 11:06:33 -0300

    Ativar envio Datatrust em Test

--- 2021/12/admissao_presencial_digital_testes_TEST_envio_datatrust_ativado.sql
+++ 2021/12/admissao_presencial_digital_testes_TEST_envio_datatrust_ativado.sql
@@ -3,9 +3,9 @@ begin
 UPDATE cecred.crapprm
    SET dsvlrprm = 'true'
 WHERE cdacesso = 'DATATRUST_ATIVO_ADM_P'; 
-update crapprm
- set dsvlrprm = 'true'
-where cdacesso = 'DATATRUST_ATIVO_ADM_D';
+UPDATE cecred.ccrapprm
+ SET dsvlrprm = 'true'
+WHERE cdacesso = 'DATATRUST_ATIVO_ADM_D';
 commit;
 end;
 /
