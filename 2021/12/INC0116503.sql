227148698ab01413efcd4531ece6710a4a868609
Ramon Martins <ramon.silva@ailos.coop.br>
2021-12-09 13:50:57 -0300

    hotfix/INC0116503/master

---
+++ 2021/12/INC0116503.sql
@@ -0,0 +1,14 @@
+DECLARE
+
+UPDATE CRAPEPA
+   SET NMFANSIA = 'HIGH SECURITY'
+     , NATJURID = 2305
+     , CDSETECO = 2
+     , CDRMATIV = 64
+ where cdcooper = 6
+   and nrdconta = 501840;   
+   
+   commit;
+
+END;
+
