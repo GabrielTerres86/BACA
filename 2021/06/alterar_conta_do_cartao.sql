7c6b4d20ab77a5c98e09cd80bc60e37ced085ce2
João Luis Mendes <joao.brito@ailos.coop.br>
2021-12-10 11:39:37 -0300

    Update alterar_conta_do_cartao.sql
      -- Cooperativa de destino do cartão
      vr_cooperativa := 6;
      -- Conta de destino do cartão
      vr_conta := 194646;
    
    -- Numero do cartão que precisamos ajustar
    vr_cartao := 5588190184171591; -- Topaz

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -8,12 +8,12 @@ DECLARE
 BEGIN
 
   -- Cooperativa de destino do cartão
-  vr_cooperativa := 1;
+  vr_cooperativa := 6;
   -- Conta de destino do cartão
-  vr_conta := 6024041;
+  vr_conta := 194646;
 
 -- Numero do cartão que precisamos ajustar
-vr_cartao := 5127070162666663; -- Topaz
+vr_cartao := 5588190184171591; -- Topaz
   
   -- Verificar se a conta possui algum outro cartão para buscar o CPF do Titular
   FOR cartao IN (select distinct a.nrcpftit
