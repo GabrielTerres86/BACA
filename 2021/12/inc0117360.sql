b3689ff8d4ac5f4a18400858ff859679527aba06
Marcus Abreu <premiersoft-marcus.abreu@ailos.coop.br>
2021-12-10 16:44:17 -0300

    inc0117360 - Incluído remoção dos Lançamentos de Bloqueios incorretos, criados pela equipe do Pix ao tentar realizar de forma errada o desbloqueio.

--- 2021/12/inc0117360.sql
+++ 2021/12/inc0117360.sql
@@ -2,6 +2,18 @@ declare
   vr_cdcritic  crapcri.cdcritic%type;
   vr_dscritic  crapcri.dscritic%type;
 begin
+  -- Remove os lançamentos incorretos
+  delete from cecred.craplcm l
+   where 1=1
+   and l.nrdconta =  11371331
+   and l.cdcooper = 1
+   and l.cdagenci = 1
+   and l.cdbccxlt = 100
+   and l.nrdolote = 37000
+   and l.cdhistor = 3728
+   and l.vllanmto = 309.67;    
+  
+  -- Atualiza o Bloqueio com o valor certo, que havia sido bloqueado
   update contacorrente.tbcc_solicitacao_bloqueio t set t.vlbloqueado = 309.67
   where t.idsolblq = 'D1F88837A0DF0230E0530A29357E4CBE';
   
