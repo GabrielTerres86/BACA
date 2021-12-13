ee1c981e8abbb3ffd1d4c459f6a6406d104d92d7
Renier Figueiredo Padilha <renier.padilha@ailos.coop.br>
2021-12-13 18:30:47 +0000

    Updated INC0114189.sql

--- 2021/12/INC0114189.sql
+++ 2021/12/INC0114189.sql
@@ -1,5 +1,5 @@
 DECLARE
- /* RITM0159747 - Inativar convênios BB e seus respetivos contratos */
+ /* [INC0114189] - Erro ao emitir comprovante de pagamento dos boletos */
 BEGIN
 update crappro set dscedent = 'SUREK CIA LTDA - ME'  where nrdconta = 477893 and cdcooper = 11 and dtmvtolt =  '16/11/2021' and nrseqaut = 455;
   COMMIT;
