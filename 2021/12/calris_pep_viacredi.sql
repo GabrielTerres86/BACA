357f827a4716574704830c7a7a9e74dcd77192cc
Jefferson Luiz da Silva <jefferson.luiz@ailos.coop.br>
2021-12-14 11:42:11 -0300

    geracao dados PEP

---
+++ 2021/12/calris_pep_viacredi.sql
@@ -0,0 +1,309 @@
+
+DECLARE--257.04
+  vr_altera      BOOLEAN := TRUE;
+  vr_count       NUMBER;
+  vr_texto_carga CLOB;
+  vr_texto_carga_aux VARCHAR2(32767);
+  vr_dsdireto   VARCHAR2(4000) := gene0001.fn_param_sistema(pr_nmsistem => 'CRED'
+                                                           ,pr_cdcooper => 0
+                                                           ,pr_cdacesso => 'ROOT_DIRCOOP')||'cecred/arq/';
+
+  vr_nmarquiv   VARCHAR2(100)  := 'atualizacao_pep_1.csv';
+  vr_dscritic   VARCHAR2(4000);
+
+  vr_req      utl_http.req;
+  vr_res      utl_http.resp;
+  vr_req_url  VARCHAR2(4000) := 'http://api.advicetech.com.br/apipepadvice/ws/AdviceListaPEP.asmx?op=Consulta';
+  vr_res_body VARCHAR2(4000);
+  vr_req_body VARCHAR2(4000);
+  vr_req_body_m VARCHAR2(4000) := '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Header><ValidationSoapHeader xmlns="http://tempuri.org/"><ChaveSeguranca>ailoslistapep</ChaveSeguranca><ChaveLicenca>HFKR/fN+FGD2TL9BJjl+GZ8cbvD+38Cop88BXFyeCOajlhe73Z+H/zbj7WnkYnobb5eTbWa2deXNtOGzMLnjBrX1AKVTACPNIEZxevUk3Yr3BndhEmvfHK28QObizt+H</ChaveLicenca></ValidationSoapHeader></soap:Header><soap:Body><Consulta xmlns="http://tempuri.org/"><CPF_CNPJ>{nrcpfcgc}</CPF_CNPJ><Nome>{nmextttl}</Nome></Consulta></soap:Body></soap:Envelope>';
+
+  CURSOR cr_crapass IS
+    SELECT crapttl.nrcpfcgc
+          ,crapttl.nmextttl
+      FROM crapass crapass
+          ,crapttl crapttl
+		  ,tbcalris_pessoa calris
+     WHERE crapass.nrdconta = crapttl.nrdconta
+       AND crapass.cdcooper = crapttl.cdcooper
+	   AND calris.tpcalculadora IN (1, 2)
+       AND calris.cdclasrisco_espe_aten > 1
+	   AND calris.nrcpfcgc = crapass.nrcpfcgc
+       AND crapass.dtdemiss IS NULL
+       AND crapass.cdcooper = 1
+     GROUP BY crapttl.nrcpfcgc
+             ,crapttl.nmextttl;
+
+  CURSOR cr_crapttl(pr_nrcpfcgc IN crapttl.nrcpfcgc%TYPE) IS
+    SELECT crapttl.cdcooper
+          ,crapttl.nrdconta
+          ,crapttl.idseqttl
+      FROM crapttl crapttl
+     WHERE crapttl.nrcpfcgc = pr_nrcpfcgc;
+
+  TYPE typ_pep IS RECORD(
+     vr_flgpep       BOOLEAN
+    ,vr_cpfpesquisa  tbcadast_politico_exposto.nrcpf_politico%TYPE
+    ,vr_nomepesquisa tbcadast_politico_exposto.nmpolitico%TYPE
+    ,vr_idpessoa     tbcadast_pessoa.idpessoa%TYPE
+    ,vr_idpessoapep  tbcadast_pessoa.idpessoa%TYPE
+    ,vr_cpfpep       tbcadast_politico_exposto.nrcpf_politico%TYPE
+    ,vr_nomepep      tbcadast_politico_exposto.nmpolitico%TYPE
+    ,vr_tpexposto    tbcadast_politico_exposto.tpexposto%TYPE
+    ,vr_dsocupacao   VARCHAR2(4000)
+    ,vr_ocupacao     gncdocp.dsdocupa%TYPE
+    ,vr_cdocupacao   gncdocp.cdocupa%TYPE --gncdocp
+    ,vr_dsrelacmnto  VARCHAR2(4000) --CONJUGE,1,PAI/MAE,2,FILHO(A),3,COMPANHEIRO(A),4,OUTROS,5,COLABORADOR(A),6,ENTEADO(A),7,ASSESSOR (A),8,PARENTE,9,REPRESENTANTE PEP,10,PROCURADOR PEP,11,SOCIO (A) PEP,12
+    ,vr_relacmnto    VARCHAR2(4000) --craptab WHERE cdacesso = 'VINCULOTTL'
+    ,vr_cdrelacmnto  NUMBER(2)
+    ,vr_dsinicio     VARCHAR2(100)
+    ,vr_dtinicio     tbcadast_politico_exposto.dttermino%TYPE
+    ,vr_dstermino    VARCHAR2(100)
+    ,vr_dttermino    tbcadast_politico_exposto.dttermino%TYPE
+    ,vr_log          VARCHAR(4000));
+  vr_pep typ_pep;
+
+BEGIN
+  dbms_lob.createtemporary(vr_texto_carga, TRUE, dbms_lob.CALL);
+  dbms_lob.open(vr_texto_carga,dbms_lob.lob_readwrite);  
+
+  gene0002.pc_escreve_xml(vr_texto_carga,vr_texto_carga_aux,'CPF Pesquisado;' ||
+                                                            'Nome Pesquisado;' ||
+                                                            'PEP;' ||
+                                                            'CPF PEP;' ||
+                                                            'Nome PEP;' ||
+                                                            'Tipo Exposto;' ||
+                                                            'Ocupacao;' ||
+                                                            'Cod Ocupacao;' ||
+                                                            'Dsc Ocupacao;' ||
+                                                            'Relacionamento;' ||
+                                                            'Cod Relacionamento;' ||
+                                                            'Dsc Relacionamento;' ||
+                                                            'Data Inicio;' ||
+                                                            'Data Termino;' ||
+                                                            'Log Execucao' || CHR(10));
+  FOR rw_crapass IN cr_crapass LOOP
+    vr_req := utl_http.begin_request(vr_req_url, 'POST', ' HTTP/1.1');
+    utl_http.set_header(vr_req, 'Content-Type', 'text/xml');
+    
+    vr_req_body := REPLACE(REPLACE(vr_req_body_m, '{nrcpfcgc}', rw_crapass.nrcpfcgc),'{nmextttl}',rw_crapass.nmextttl);
+    
+    utl_http.set_header(vr_req, 'Content-Length', length(vr_req_body));
+    utl_http.write_text(vr_req, vr_req_body);
+    vr_res := utl_http.get_response(vr_req);
+  
+    BEGIN
+      utl_http.read_line(vr_res, vr_res_body);
+      
+      vr_pep.vr_flgpep       := FALSE;
+      vr_pep.vr_cpfpesquisa  := NULL;
+      vr_pep.vr_nomepesquisa := NULL;
+      vr_pep.vr_idpessoa     := NULL;
+      vr_pep.vr_idpessoapep  := NULL;
+      vr_pep.vr_cpfpep       := NULL;
+      vr_pep.vr_nomepep      := NULL;
+      vr_pep.vr_tpexposto    := NULL;
+      vr_pep.vr_dsocupacao   := NULL;
+      vr_pep.vr_ocupacao     := NULL;
+      vr_pep.vr_cdocupacao   := NULL;
+      vr_pep.vr_dsrelacmnto  := NULL;
+      vr_pep.vr_relacmnto    := NULL;
+      vr_pep.vr_cdrelacmnto  := NULL;
+      vr_pep.vr_dsinicio     := NULL;
+      vr_pep.vr_dtinicio     := NULL;
+      vr_pep.vr_dstermino    := NULL;
+      vr_pep.vr_dttermino    := NULL;
+      vr_pep.vr_log          := NULL;
+      
+      IF instr(vr_res_body, '<DATA>') > 0 THEN
+        vr_pep.vr_flgpep       := TRUE;
+        vr_pep.vr_cpfpesquisa  := to_number(substr(vr_res_body,instr(vr_res_body, '<CPF_CNPJ_STR>') +length('<CPF_CNPJ_STR>'),instr(vr_res_body,'</CPF_CNPJ_STR>') -(instr(vr_res_body,'<CPF_CNPJ_STR>') +length('<CPF_CNPJ_STR>'))));
+        vr_pep.vr_nomepesquisa := substr(vr_res_body,instr(vr_res_body, '<NM_PESSOA>') +length('<NM_PESSOA>'),instr(vr_res_body,'</NM_PESSOA>') -(instr(vr_res_body,'<NM_PESSOA>') +length('<NM_PESSOA>')));
+        vr_pep.vr_cpfpep       := to_number(substr(vr_res_body,instr(vr_res_body, '<CPF_PEP>') +length('<CPF_PEP>'),instr(vr_res_body,'</CPF_PEP>') -(instr(vr_res_body,'<CPF_PEP>') +length('<CPF_PEP>'))));
+        vr_pep.vr_nomepep      := TRIM(upper(substr(vr_res_body,instr(vr_res_body, '<PEP>') +length('<PEP>'),instr(vr_res_body,'</PEP>') -(instr(vr_res_body,'<PEP>') +length('<PEP>')))));
+        vr_pep.vr_tpexposto    := CASE WHEN vr_pep.vr_cpfpesquisa = vr_pep.vr_cpfpep THEN 1 ELSE 2 END;
+        
+        vr_pep.vr_dsocupacao   := TRIM(upper(substr(vr_res_body,instr(vr_res_body, '<DS_CARGO>') +length('<DS_CARGO>'),instr(vr_res_body,'</DS_CARGO>') -(instr(vr_res_body,'<DS_CARGO>') +length('<DS_CARGO>')))));
+        vr_pep.vr_dsrelacmnto  := TRIM(upper(substr(vr_res_body,instr(vr_res_body, '<DS_RELACIONAMENTO>') +length('<DS_RELACIONAMENTO>'),instr(vr_res_body,'</DS_RELACIONAMENTO>') -(instr(vr_res_body,'<DS_RELACIONAMENTO>') +length('<DS_RELACIONAMENTO>')))));
+        
+        vr_pep.vr_dsinicio     := substr(vr_res_body,instr(vr_res_body, '<DT_INICIO>') +length('<DT_INICIO>'),instr(vr_res_body,'</DT_INICIO>') -(instr(vr_res_body,'<DT_INICIO>') +length('<DT_INICIO>')));
+        vr_pep.vr_dsinicio     := substr(vr_pep.vr_dsinicio,0,instr(vr_pep.vr_dsinicio,'T')-1);
+        vr_pep.vr_dstermino    := substr(vr_res_body,instr(vr_res_body, '<DT_FIM>') +length('<DT_FIM>'),instr(vr_res_body,'</DT_FIM>') -(instr(vr_res_body,'<DT_FIM>') +length('<DT_FIM>')));
+        vr_pep.vr_dstermino    := substr(vr_pep.vr_dstermino,0,instr(vr_pep.vr_dstermino,'T')-1);
+          
+        vr_pep.vr_dtinicio     := to_date(vr_pep.vr_dsinicio, 'yyyy-mm-dd');
+        vr_pep.vr_dttermino    := to_date(vr_pep.vr_dstermino, 'yyyy-mm-dd');
+        
+        BEGIN
+          IF instr(vr_pep.vr_dsocupacao, 'CARGO:') > 0 THEN
+            vr_pep.vr_ocupacao := TRIM(substr(vr_pep.vr_dsocupacao,instr(vr_pep.vr_dsocupacao, 'CARGO:') +length('CARGO:'), instr(substr(vr_pep.vr_dsocupacao,instr(vr_pep.vr_dsocupacao, 'CARGO:') +length('CARGO:')),',')-1));
+          ELSIF instr(vr_pep.vr_dsocupacao, '/') > 0 THEN
+            vr_pep.vr_ocupacao := TRIM(substr(vr_pep.vr_dsocupacao,0,instr(vr_pep.vr_dsocupacao, '/')-1));
+          END IF;  
+        
+          CASE vr_pep.vr_ocupacao
+            WHEN 'VICE-PREFEITO' THEN vr_pep.vr_cdocupacao := 312;
+            WHEN 'PREFEITO' THEN vr_pep.vr_cdocupacao := 311;
+            ELSE
+              SELECT cdocupa
+                INTO vr_pep.vr_cdocupacao
+                FROM gncdocp
+               WHERE rownum = 1 AND dsdocupa LIKE '%' || TRIM(vr_pep.vr_ocupacao) || '%';
+           END CASE;
+        EXCEPTION
+          WHEN OTHERS THEN
+            vr_pep.vr_cdocupacao := NULL;
+        END;
+        
+        BEGIN
+          IF instr(vr_pep.vr_dsrelacmnto, ' CONJUGE ') > 0 OR instr(vr_pep.vr_dsrelacmnto, ' CÔNJUGE/') > 0 OR instr(vr_pep.vr_dsrelacmnto, ' EX-CÔNJUGE') > 0 THEN
+            vr_pep.vr_cdrelacmnto := 1;
+            vr_pep.vr_relacmnto   := 'Conjuge';
+          ELSIF instr(vr_pep.vr_dsrelacmnto, 'PAI/MÃE') > 0 OR instr(vr_pep.vr_dsrelacmnto, ' PAI ') > 0 OR instr(vr_pep.vr_dsrelacmnto, ' MÃE ') > 0 OR instr(vr_pep.vr_dsrelacmnto, ' MAE ') > 0 THEN
+            vr_pep.vr_cdrelacmnto := 2;
+            vr_pep.vr_relacmnto   := 'Pai/Mae';
+          ELSIF instr(vr_pep.vr_dsrelacmnto, 'FILHA(O)') > 0 OR instr(vr_pep.vr_dsrelacmnto, 'FILHO(A)') > 0 THEN
+            vr_pep.vr_cdrelacmnto := 3;
+            vr_pep.vr_relacmnto   := 'Filha(o)';
+          ELSIF instr(vr_pep.vr_dsrelacmnto, 'TIO/TIA') > 0 OR instr(vr_pep.vr_dsrelacmnto, 'TIA/TIO') > 0 OR instr(vr_pep.vr_dsrelacmnto, 'TIO(A)') > 0 OR instr(vr_pep.vr_dsrelacmnto, 'TIA(O)') > 0 THEN
+            vr_pep.vr_cdrelacmnto := 9;
+            vr_pep.vr_relacmnto   := 'Tio/tia - Parente';
+          ELSIF instr(vr_pep.vr_dsrelacmnto, 'PRIMO(A)') > 0 OR instr(vr_pep.vr_dsrelacmnto, 'PRIMA(O)') > 0 THEN
+            vr_pep.vr_cdrelacmnto := 9;
+            vr_pep.vr_relacmnto   := 'Primo(a) - Parente';
+          ELSIF instr(vr_pep.vr_dsrelacmnto, 'ENTEADA(O)') > 0 OR instr(vr_pep.vr_dsrelacmnto, 'ENTEADO(A)') > 0 THEN
+            vr_pep.vr_cdrelacmnto := 7;
+            vr_pep.vr_relacmnto   := 'Enteada(o) - Parente';
+          ELSIF instr(vr_pep.vr_dsrelacmnto, ' SÓCIO ') > 0 OR instr(vr_pep.vr_dsrelacmnto, ' SÓCIA ') > 0 THEN
+            vr_pep.vr_cdrelacmnto := 12;
+            vr_pep.vr_relacmnto   := 'Sócio(a)';
+          ELSIF instr(vr_pep.vr_dsrelacmnto, 'IRMÃ(O)') > 0 THEN
+            vr_pep.vr_cdrelacmnto := 9;
+            vr_pep.vr_relacmnto   := 'IRMA(O) - Parente';
+          ELSIF instr(vr_pep.vr_dsrelacmnto, 'SOBRINHO(A)') > 0 THEN
+            vr_pep.vr_cdrelacmnto := 9;
+            vr_pep.vr_relacmnto   := 'SOBRINHO(A) - Parente';
+          END IF;
+        EXCEPTION
+          WHEN OTHERS THEN
+            vr_pep.vr_cdocupacao := NULL;
+        END;
+                                                    
+        IF instr(vr_pep.vr_dsocupacao, 'SUPLENTE') > 0 THEN
+          vr_pep.vr_log := vr_pep.vr_log || 'Suplente, nao altera dados|';
+          CONTINUE;
+        END IF;
+        
+        BEGIN
+          SELECT idpessoa
+            INTO vr_pep.vr_idpessoa
+            FROM tbcadast_pessoa
+           WHERE nrcpfcgc = vr_pep.vr_cpfpesquisa;
+        EXCEPTION
+           WHEN OTHERS THEN
+             vr_pep.vr_idpessoa := NULL;
+        END;
+        
+        BEGIN
+          SELECT idpessoa
+            INTO vr_pep.vr_idpessoapep
+            FROM tbcadast_pessoa
+           WHERE nrcpfcgc = vr_pep.vr_cpfpep;
+        EXCEPTION
+           WHEN OTHERS THEN
+             vr_pep.vr_idpessoapep := NULL;
+             vr_pep.vr_log := vr_pep.vr_log || 'Nao foi encontrado esse cpf do pep na tbcadast_pessoa, nao incluira o idpessoa_politico|';
+        END;
+          
+        IF vr_pep.vr_idpessoa IS NOT NULL THEN
+          SELECT COUNT(1)
+            INTO vr_count
+            FROM tbcadast_pessoa_polexp
+           WHERE idpessoa = vr_pep.vr_idpessoa;
+              
+          IF vr_count = 0 THEN
+            vr_pep.vr_log := vr_pep.vr_log || 'Incluindo tbcadast_pessoa_polexp:' || vr_pep.vr_idpessoa || '|';
+            IF vr_altera THEN
+              INSERT INTO tbcadast_pessoa_polexp
+                (idpessoa,tpexposto,dtinicio,dttermino,cdocupacao,tprelacao_polexp,idpessoa_politico)
+              VALUES
+                (vr_pep.vr_idpessoa,vr_pep.vr_tpexposto,vr_pep.vr_dtinicio,vr_pep.vr_dttermino,vr_pep.vr_cdocupacao,vr_pep.vr_cdrelacmnto,vr_pep.vr_idpessoapep);
+            END IF;
+          ELSE 
+            vr_pep.vr_log := vr_pep.vr_log || 'Ja existe tbcadast_pessoa_polexp, nao altera dados|';
+          END IF;
+        ELSE
+          vr_pep.vr_log := vr_pep.vr_log || 'Nao foi encontrado esse cpf cadastrado na tbcadast_pessoa, nao altera tbcadast_pessoa_polexp|';
+        END IF;
+        
+        FOR rw_crapttl IN cr_crapttl(vr_pep.vr_cpfpesquisa) LOOP
+          vr_pep.vr_log := vr_pep.vr_log || 'Alterando crapttl:' || rw_crapttl.idseqttl || '/' || rw_crapttl.cdcooper || '/' || rw_crapttl.nrdconta || '|';
+          IF vr_altera THEN
+            UPDATE crapttl
+               SET inpolexp = 1
+             WHERE idseqttl = rw_crapttl.idseqttl
+               AND cdcooper = rw_crapttl.cdcooper
+               AND nrdconta = rw_crapttl.nrdconta;
+          END IF;
+            
+          SELECT COUNT(1)
+            INTO vr_count
+            FROM tbcadast_politico_exposto
+           WHERE idseqttl = rw_crapttl.idseqttl
+             AND cdcooper = rw_crapttl.cdcooper
+             AND nrdconta = rw_crapttl.nrdconta;
+            
+          IF vr_count = 0 THEN
+            vr_pep.vr_log := vr_pep.vr_log || 'Incluindo tbcadast_politico_exposto:' || rw_crapttl.idseqttl || '/' || rw_crapttl.cdcooper || '/' || rw_crapttl.nrdconta || '|';
+            IF vr_altera THEN
+              INSERT INTO tbcadast_politico_exposto
+                (cdcooper,nrdconta,idseqttl,tpexposto,dtinicio,dttermino
+                ,nmpolitico,cdocupacao,cdrelacionamento,nrcpf_politico)
+              VALUES
+                (rw_crapttl.cdcooper,rw_crapttl.nrdconta,rw_crapttl.idseqttl,vr_pep.vr_tpexposto,vr_pep.vr_dtinicio,vr_pep.vr_dttermino
+                ,vr_pep.vr_nomepep,vr_pep.vr_cdocupacao,vr_pep.vr_cdrelacmnto,vr_pep.vr_cpfpep);
+            END IF;
+          ELSE 
+            vr_pep.vr_log := vr_pep.vr_log || 'Ja existe tbcadast_politico_exposto, nao altera dados|';
+          END IF;
+        END LOOP;
+        
+        gene0002.pc_escreve_xml(vr_texto_carga,vr_texto_carga_aux,rw_crapass.nrcpfcgc || ';' ||
+                                                                  rw_crapass.nmextttl || ';' ||
+                                                                  CASE WHEN vr_pep.vr_flgpep THEN 'Sim' ELSE 'Nao' END || ';' ||
+                                                                  vr_pep.vr_cpfpep || ';' ||
+                                                                  vr_pep.vr_nomepep || ';' ||
+                                                                  vr_pep.vr_tpexposto || ';' ||
+                                                                  vr_pep.vr_dsocupacao || ';' ||
+                                                                  vr_pep.vr_cdocupacao || ';' ||
+                                                                  vr_pep.vr_ocupacao || ';' ||
+                                                                  vr_pep.vr_dsrelacmnto || ';' ||
+                                                                  vr_pep.vr_cdrelacmnto || ';' ||
+                                                                  vr_pep.vr_relacmnto || ';' ||
+                                                                  vr_pep.vr_dtinicio || ';' ||
+                                                                  vr_pep.vr_dttermino || ';' ||
+                                                                  vr_pep.vr_log || CHR(10));
+      ELSE
+        gene0002.pc_escreve_xml(vr_texto_carga,vr_texto_carga_aux,rw_crapass.nrcpfcgc || ';' || rw_crapass.nmextttl || ';' || CASE WHEN vr_pep.vr_flgpep THEN 'Sim' ELSE 'Nao' END || ';;;;;;;;;;;;;' || CHR(10));
+      END IF;
+    EXCEPTION
+      WHEN utl_http.end_of_body THEN
+        utl_http.end_response(vr_res);
+      WHEN utl_http.too_many_requests THEN
+        utl_http.end_response(vr_res);
+    END;
+    utl_http.end_response(vr_res);
+  END LOOP;
+  gene0002.pc_escreve_xml(vr_texto_carga,vr_texto_carga_aux,'',true);
+
+  -- Gerar o arquivo na pasta converte
+  gene0002.pc_clob_para_arquivo(pr_clob     => vr_texto_carga
+                               ,pr_caminho  => vr_dsdireto
+                               ,pr_arquivo  => vr_nmarquiv
+                               ,pr_des_erro => vr_dscritic);
+
+  -- Liberando a memória alocada pro CLOB
+  dbms_lob.close(vr_texto_carga);
+  dbms_lob.freetemporary(vr_texto_carga);
+  commit;
+END;
