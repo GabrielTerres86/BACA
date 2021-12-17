d1f5b5b9b830cd124e6ac9bdd0618724c1379554
Heron Barreto Peixoto de Vasconcelos <amcom-heron.vasconcelos@ailos.coop.br>
2021-12-17 15:33:12 -0300

    Removendo coluna duplicada.

--- 2021/12/Insert_TBCC_PRODUTO-Produto_de_teste.sql
+++ 2021/12/Insert_TBCC_PRODUTO-Produto_de_teste.sql
@@ -1,26 +1,25 @@
---Produto
-DECLARE
-  n_cdProdMax TBCC_PRODUTO.CDPRODUTO%TYPE;
 BEGIN
-
-  SELECT MAX(P.CDPRODUTO)
-    INTO n_cdProdMax
-    FROM TBCC_PRODUTO P;
-
-  INSERT INTO TBCC_PRODUTO
-    (CDPRODUTO
-    ,DSPRODUTO
-    ,IDFAIXA_VALOR
-    ,FLGITEM_SOA
-    ,IDFAIXA_VALOR)
-    SELECT n_cdProdMax + 1
-          ,'PRODUTO DE TESTE'
-          ,0
-          ,1
-          ,1
-      FROM DUAL
-     WHERE NOT EXISTS (SELECT 1
-              FROM TBCC_PRODUTO
-             WHERE CDPRODUTO = n_cdProdMax + 1);
-  COMMIT;
+  DECLARE
+    n_cdProdMax TBCC_PRODUTO.CDPRODUTO%TYPE;
+  BEGIN
+  
+    SELECT MAX(P.CDPRODUTO)
+      INTO n_cdProdMax
+      FROM TBCC_PRODUTO P;
+  
+    INSERT INTO TBCC_PRODUTO
+      (CDPRODUTO
+      ,DSPRODUTO
+      ,FLGITEM_SOA
+      ,IDFAIXA_VALOR)
+      SELECT n_cdProdMax + 1
+            ,'PRODUTO DE TESTE'
+            ,1
+            ,1
+        FROM DUAL
+       WHERE NOT EXISTS (SELECT 1
+                FROM TBCC_PRODUTO
+               WHERE CDPRODUTO = n_cdProdMax + 1);
+    COMMIT;
+  END;
 END;
