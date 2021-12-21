4398cf82ad446a1a7878dc7e931b8af343f1cee1
Marcus Abreu <premiersoft-marcus.abreu@ailos.coop.br>
2021-12-21 16:17:17 -0300

    INC0119025 - Inclusão da Crítica para validação do Desbloqueio já finalizado, na entidade CRAPCRI.

---
+++ 2021/12/INSERT_CRAPCRI.sql
@@ -0,0 +1,22 @@
+BEGIN
+  INSERT INTO CECRED.CRAPCRI
+    (CDCRITIC,
+    DSCRITIC,
+    PROGRESS_RECID,
+    TPCRITIC,
+    FLGCHAMA)
+  VALUES
+    (10603,
+    '10603 - Bloqueio já finalizado',
+    10603,
+    2,
+    0);
+    
+    
+    COMMIT;    
+    
+EXCEPTION
+  WHEN OTHERS THEN
+    dbms_output.put_line('Erro ao inserir codigo de critica - ' || sqlerrm);
+    rollback;
+END;    
