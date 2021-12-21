0dc5c04383f15faf124afd6154013b999fcf3f62
Lucas Eduardo Ranghetti <lucas.ranghetti@ailos.coop.br>
2021-12-21 10:52:50 -0300

    Remover Menu seguros do App

---
+++ 2021/12/INC0118929delete-menu.sql
@@ -0,0 +1,11 @@
+delete from menumobile where menumobileid = 1026;
+COMMIT;
+
+delete from menumobile where menumobileid = 1027;
+COMMIT;
+
+delete from menumobile where menumobileid = 1028;
+COMMIT;
+
+delete from menumobile where menumobileid = 1034;
+COMMIT;
