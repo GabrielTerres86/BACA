8aaddacf40fe7ce923b0eaf14ee6fb2f61b90d8f
Filipe Ribeiro <mouts-filipe.rego@ailos.coop.br>
2021-12-21 08:37:13 -0300

    Teste de cancelamento dos seguros por idade máxima

--- 2021/12/corretora_update_dtnascimento_para_teste.sql
+++ 2021/12/corretora_update_dtnascimento_para_teste.sql
@@ -7,8 +7,7 @@ BEGIN
   UPDATE tbseg_prestamista c
      SET c.dtnasctl = '01/01/1945'
    WHERE c.cdcooper = 1
-     AND c.nrdconta =  11931205;
-     
+     AND c.nrdconta =  11931205;     
 COMMIT;
 EXCEPTION WHEN OTHERS THEN
   ROLLBACK;
