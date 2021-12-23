cad2c5a5b0c4a3c82a31db63e62680ea5844a145
Petrônio Sifuentes Gomes Júnior <mouts-petronio.junior@ailos.coop.br>
2021-12-23 15:25:25 -0300

    Importação de colaboradores em Test

---
+++ 2021/12/insert_colaboradores.sql
@@ -0,0 +1,605 @@
+DECLARE
+BEGIN
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (5
+    ,'Júlia Miranda'
+    ,48750143093
+    ,1
+    ,'Não tem relacionamento com cliente'
+    ,'FLORIANOPOLIS'
+    ,'SC'
+    ,3500
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (5
+    ,'Isabella Monteiro'
+    ,69581489088
+    ,1
+    ,'Não tem relacionamento com cliente'
+    ,'BRUSQUE'
+    ,'SC'
+    ,7000
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (2
+    ,'Gabriel Cardoso'
+    ,41575875020
+    ,1
+    ,'Não tem relacionamento com cliente'
+    ,'DOURADOS'
+    ,'MS'
+    ,6908
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (2
+    ,'Gael Carvalho'
+    ,97134360083
+    ,1
+    ,'Não tem relacionamento com cliente'
+    ,'URUGUAINA'
+    ,'RS'
+    ,2500
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (3
+    ,'Miguel Almeida'
+    ,90852369085
+    ,1
+    ,'Não tem relacionamento com cliente'
+    ,'BLUMENAU'
+    ,'SC'
+    ,1800
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (3
+    ,'Arthur Alves'
+    ,7060540083
+    ,1
+    ,'Não tem relacionamento com cliente'
+    ,'FOZ DO IGUAÇU'
+    ,'PR'
+    ,2200
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (13
+    ,'Alice Barros'
+    ,38381525063
+    ,1
+    ,'Não tem relacionamento com cliente'
+    ,'ITAJAÍ'
+    ,'SC'
+    ,5600
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (13
+    ,'Theo Batista'
+    ,36752903018
+    ,4
+    ,'Relacionamento direto (com migração de carteira)'
+    ,'CAMBURIU'
+    ,'SC'
+    ,8590
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (7
+    ,'Lucas Marques'
+    ,4874416055
+    ,4
+    ,'Relacionamento direto (com migração de carteira)'
+    ,'GUABIRUBA'
+    ,'SC'
+    ,2500
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (7
+    ,'Manuela Martins'
+    ,60705532097
+    ,4
+    ,'Relacionamento direto (com migração de carteira)'
+    ,'GASPAR'
+    ,'SC'
+    ,25000
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (7
+    ,'Cecília Medeiros'
+    ,96514830080
+    ,4
+    ,'Relacionamento direto (com migração de carteira)'
+    ,'ILHOTA'
+    ,'SC'
+    ,30000
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (8
+    ,'Maria Clara Gonçalves'
+    ,94087318087
+    ,4
+    ,'Relacionamento direto (com migração de carteira)'
+    ,'URUGUAINA'
+    ,'RS'
+    ,8590
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (8
+    ,'Maria Júlia Lima'
+    ,86295661084
+    ,4
+    ,'Relacionamento direto (com migração de carteira)'
+    ,'RODEIO'
+    ,'SC'
+    ,15000
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (10
+    ,'Maria Eduarda Lopes'
+    ,74835841069
+    ,4
+    ,'Relacionamento direto (com migração de carteira)'
+    ,'ITAPEMA'
+    ,'SC'
+    ,16508
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (10
+    ,'Lorena Machado'
+    ,76545983008
+    ,4
+    ,'Relacionamento direto (com migração de carteira)'
+    ,'JOINVILLE'
+    ,'SC'
+    ,6908
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (11
+    ,'Heloísa Fernandes'
+    ,17519882012
+    ,4
+    ,'Relacionamento direto (sem migração de carteira)'
+    ,'CAMBURIU'
+    ,'SC'
+    ,2200
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (11
+    ,'Pedro Ferreira'
+    ,74632603058
+    ,4
+    ,'Relacionamento direto (sem migração de carteira)'
+    ,'CASCAVEL'
+    ,'PR'
+    ,2111
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (12
+    ,'João Miguel Duarte'
+    ,32776624034
+    ,4
+    ,'Relacionamento direto (sem migração de carteira)'
+    ,'RIO DE JANEIRO'
+    ,'RJ'
+    ,4000
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (12
+    ,'Enzo Gabriel Freitas'
+    ,94761837055
+    ,4
+    ,'Relacionamento direto (sem migração de carteira)'
+    ,'ITAJAÍ'
+    ,'SC'
+    ,1800
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (14
+    ,'Bernardo Castro'
+    ,65192036020
+    ,4
+    ,'Relacionamento direto (sem migração de carteira)'
+    ,'BLUMENAU'
+    ,'SC'
+    ,25000
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (14
+    ,'Samuel Costa'
+    ,4619697001
+    ,4
+    ,'Relacionamento direto (sem migração de carteira)'
+    ,'FOZ DO IGUAÇU'
+    ,'PR'
+    ,30000
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (14
+    ,'Valentina Dias'
+    ,51071291025
+    ,4
+    ,'Relacionamento direto (sem migração de carteira)'
+    ,'SÃO PAULO'
+    ,'SP'
+    ,32658
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (17
+    ,'Davi Borges'
+    ,45117570030
+    ,4
+    ,'Relacionamento direto (sem migração de carteira)'
+    ,'CASCAVEL'
+    ,'PR'
+    ,15000
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (17
+    ,'Laura Campos'
+    ,23810547077
+    ,2
+    ,'Relacionamento indireto com o cliente'
+    ,'BARRACÃO'
+    ,'PR'
+    ,16508
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (6
+    ,'Lorenzo Garcia'
+    ,50040745007
+    ,2
+    ,'Relacionamento indireto com o cliente'
+    ,'BARRACÃO'
+    ,'PR'
+    ,5000
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (6
+    ,'Sophia Gomes'
+    ,62769271040
+    ,2
+    ,'Relacionamento indireto com o cliente'
+    ,'DOURADOS'
+    ,'MS'
+    ,5600
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (1
+    ,'Heitor Andrade'
+    ,30000908002
+    ,2
+    ,'Relacionamento indireto com o cliente'
+    ,'SÃO PAULO'
+    ,'SP'
+    ,2111
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (1
+    ,'Helena Barbosa'
+    ,46024474067
+    ,2
+    ,'Relacionamento indireto com o cliente'
+    ,'RIO DE JANEIRO'
+    ,'RJ'
+    ,5000
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (16
+    ,'Maria Cecília Melo'
+    ,35389341007
+    ,2
+    ,'Relacionamento indireto com o cliente'
+    ,'IBIRAMA'
+    ,'SC'
+    ,32658
+    ,'L');
+  INSERT INTO tbcalris_colaboradores
+    (cdcooper
+    ,nmpessoa
+    ,nrcpfcgc
+    ,tprelacionamento
+    ,dsrelacionamento
+    ,dscidade
+    ,dsuf
+    ,vlsalario
+    ,tpcolaborador)
+  VALUES
+    (16
+    ,'Benício Mendes'
+    ,51828378011
+    ,2
+    ,'Relacionamento indireto com o cliente'
+    ,'JARAGUA DO SUL'
+    ,'SC'
+    ,4000
+    ,'L');
+  COMMIT;
+END;
+/
