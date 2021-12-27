cd51150b4199a336cdf4fff3b532a53382ab5dbb
Carlos Henrique Weinhold <carlos.weinhold@ailos.coop.br>
2021-12-27 12:00:48 -0300

    Remover programas crps029, crps083 e crps140 do batch

--- 2021/12/remover_programas_crps029_crps140.sql
+++ 2021/12/remover_programas_crps029_crps140.sql
@@ -1,4 +1,5 @@
---Remover programas do batch
-UPDATE crapprg SET inlibprg = 2, nrsolici = 9029 WHERE cdprogra = 'CRPS029';
-UPDATE crapprg SET inlibprg = 2, nrsolici = 9140 WHERE cdprogra = 'CRPS140';
+--Remover programas do batch (9 + solici anterior + nrcrps)
+UPDATE crapprg SET inlibprg = 2, nrsolici = 92029 WHERE cdprogra = 'CRPS029';
+UPDATE crapprg SET inlibprg = 2, nrsolici = 94083 WHERE cdprogra = 'CRPS083';
+UPDATE crapprg SET inlibprg = 2, nrsolici = 94140 WHERE cdprogra = 'CRPS140';
 COMMIT;
