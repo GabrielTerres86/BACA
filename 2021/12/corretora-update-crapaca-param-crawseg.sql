c281b5ad5d280a8fad7fa4b817df81f8db9cf748
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2021-12-27 15:57:45 -0300

    novo parametro na atualizacao da crawseg

---
+++ 2021/12/corretora-update-crapaca-param-crawseg.sql
@@ -0,0 +1,16 @@
+begin
+
+ update crapaca a 
+    set a.lstparam = 'pr_cdcooper,pr_nrdconta,pr_nrctrato,pr_flggarad,pr_flgassum,pr_tpcustei'
+  where a.nmdeacao = 'ATUALIZA_PROPOSTA_PREST'
+    and a.nmpackag = 'TELA_ATENDA_SEGURO'
+    and a.nmproced = 'pc_atualiza_dados_prest';
+  
+commit;
+
+exception
+
+    WHEN others THEN
+        rollback;
+        raise_application_error(-20501, SQLERRM);
+end;
