0c50e8898f72c978cc4b2e777147a145ca3de28d
Paulo Felipe Kowalsky <t0032603@cecred.coop.br>
2021-12-29 11:28:45 -0300

    delete pagamento indevido

--- 2021/12/delete-craptit-inc0119882.sql
+++ 2021/12/delete-craptit-inc0119882.sql
@@ -3,7 +3,7 @@ begin
 delete craptit t 
 where t.cdcooper = 1 
   and t.nrdconta = 2121131
-  and t.dtmvtolt = '29/12/2021' 
+  and t.dtmvtolt = to_date('29/12/2021','DD/MM/YYYY') 
   and t.vltitulo = 8068.17;
 
 commit;
