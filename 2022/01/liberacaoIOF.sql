954db4969b2a82a471b122c9599ec058a6669de2
Miguel Rodrigues <miguel.rodrigues@ailos.coop.br>
2021-12-29 16:57:42 -0300

    alteracao de aliquota iof

--- 2022/01/liberacaoIOF.sql
+++
@@ -1,9 +0,0 @@
-BEGIN
-  UPDATE tbgen_iof_taxa tit SET tit.vltaxa_iof = 0.00008200 WHERE tit.tpiof = 2;
-  UPDATE tbgen_iof_taxa tit SET tit.vltaxa_iof = 0.00004100 WHERE tit.tpiof = 3;
-  COMMIT;
-EXCEPTION
-  WHEN OTHERS THEN
-    RAISE_application_error(-20500,SQLERRM);
-    ROLLBACK;
-END;
