acb0f17c3f815977d33480a6e0183e67108e8673
Guilherme Pinheiro <guilherme.pinheiro@mouts.com.br>
2021-12-30 11:26:02 -0300

    LOG - relatorio e registra baixa

---
+++ 2021/12/baca-crapaca-baixa-imovel.sql
@@ -0,0 +1,14 @@
+BEGIN
+  UPDATE CRAPACA
+     SET LSTPARAM = 'pr_cdcooper,pr_nrdconta,pr_nrctremp,pr_cdoperad,pr_idseqbem'
+   WHERE NMPACKAG = 'TELA_IMOVEL'
+     AND NMDEACAO = 'BAIXA_IMOVEL'
+     AND NMPROCED = 'pc_gravar_baixa_manual_imovel';
+
+  COMMIT;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    RAISE_application_error(-20500, SQLERRM);
+    ROLLBACK;
+END;
