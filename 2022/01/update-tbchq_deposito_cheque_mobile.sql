5914abfdc2567abafa970cbc9a75e49a4fa7cc64
Paulo Felipe Kowalsky <t0032603@cecred.coop.br>
2022-01-17 11:09:55 -0300

    ajuste script

--- 2022/01/update-tbchq_deposito_cheque_mobile.sql
+++ 2022/01/update-tbchq_deposito_cheque_mobile.sql
@@ -2,7 +2,7 @@ begin
   
 update tbchq_deposito_cheque_mob mob
   set mob.insituacao = 4
-where  mob.dtdeposito < to_date('14/01/2022','DD/MM/YYYY')
+where  mob.dtdeposito < trunc(sysdate)
   and  mob.insituacao = 1;
   
 commit;
