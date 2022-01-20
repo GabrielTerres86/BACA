b84251e23878f92ab85af5d825094561b54aa863
Ilo Douglas Martins <t0034438@cecred.coop.br>
2022-01-20 17:27:02 -0300

    Ajustes nos contratos task-295424

---
+++ 2022/01/task-295424.sql
@@ -0,0 +1,343 @@
+declare
+  vr_cdcritic crapcri.cdcritic%TYPE;
+  vr_dscritic VARCHAR2(10000);
+  vr_exc_saida EXCEPTION;
+  rw_crapdat  BTCH0001.cr_crapdat%ROWTYPE;
+  vr_des_reto varchar(3);
+  vr_tab_erro GENE0001.typ_tab_erro;
+
+  vr_cdcooper crapcop.cdcooper%TYPE := 5;
+  vr_nrdconta crapass.nrdconta%TYPE := 41297;
+  vr_nrctremp craplem.nrctremp%TYPE := 31491;
+  vr_vllanmto craplem.vllanmto%TYPE := 3.93;
+  vr_cdhistor craplem.cdhistor%TYPE := 1705;
+
+  CURSOR cr_crapass(pr_cdcooper IN crapass.cdcooper%TYPE,
+                    pr_nrdconta IN crapass.nrdconta%TYPE) IS
+    SELECT ass.cdagenci
+      FROM crapass ass
+     WHERE ass.cdcooper = pr_cdcooper
+       AND ass.nrdconta = pr_nrdconta;
+  rw_crapass cr_crapass%ROWTYPE;
+
+BEGIN
+
+  OPEN btch0001.cr_crapdat(pr_cdcooper => vr_cdcooper);
+  FETCH btch0001.cr_crapdat
+    INTO rw_crapdat;
+  CLOSE btch0001.cr_crapdat;
+
+  OPEN cr_crapass(pr_cdcooper => vr_cdcooper, pr_nrdconta => vr_nrdconta);
+  FETCH cr_crapass
+    INTO rw_crapass;
+  CLOSE cr_crapass;
+  -- Cria o lancamento de estorno
+  EMPR0001.pc_cria_lancamento_lem(pr_cdcooper => vr_cdcooper,
+                                  pr_dtmvtolt => rw_crapdat.dtmvtolt,
+                                  pr_cdagenci => rw_crapass.cdagenci,
+                                  pr_cdbccxlt => 100,
+                                  pr_cdoperad => 1,
+                                  pr_cdpactra => rw_crapass.cdagenci,
+                                  pr_tplotmov => 5,
+                                  pr_nrdolote => 600031,
+                                  pr_nrdconta => vr_nrdconta,
+                                  pr_cdhistor => vr_cdhistor,
+                                  pr_nrctremp => vr_nrctremp,
+                                  pr_vllanmto => vr_vllanmto,
+                                  pr_dtpagemp => rw_crapdat.dtmvtolt,
+                                  pr_txjurepr => 0,
+                                  pr_vlpreemp => 0,
+                                  pr_nrsequni => 0,
+                                  pr_nrparepr => 11,
+                                  pr_flgincre => FALSE,
+                                  pr_flgcredi => FALSE,
+                                  pr_nrseqava => 0,
+                                  pr_cdorigem => 5,
+                                  pr_cdcritic => vr_cdcritic,
+                                  pr_dscritic => vr_dscritic);
+  IF vr_cdcritic IS NOT NULL OR vr_dscritic IS NOT NULL THEN
+    RAISE vr_exc_saida;
+  END IF;
+  
+  vr_cdcooper  := 10;
+  vr_nrdconta  := 42641;
+  vr_nrctremp  := 15890;
+  vr_vllanmto  := 2.75;
+  vr_cdhistor  := 1705;
+  
+   -- Cria o lancamento de estorno
+  EMPR0001.pc_cria_lancamento_lem(pr_cdcooper => vr_cdcooper,
+                                  pr_dtmvtolt => rw_crapdat.dtmvtolt,
+                                  pr_cdagenci => rw_crapass.cdagenci,
+                                  pr_cdbccxlt => 100,
+                                  pr_cdoperad => 1,
+                                  pr_cdpactra => rw_crapass.cdagenci,
+                                  pr_tplotmov => 5,
+                                  pr_nrdolote => 600031,
+                                  pr_nrdconta => vr_nrdconta,
+                                  pr_cdhistor => vr_cdhistor,
+                                  pr_nrctremp => vr_nrctremp,
+                                  pr_vllanmto => vr_vllanmto,
+                                  pr_dtpagemp => rw_crapdat.dtmvtolt,
+                                  pr_txjurepr => 0,
+                                  pr_vlpreemp => 0,
+                                  pr_nrsequni => 0,
+                                  pr_nrparepr => 14,
+                                  pr_flgincre => FALSE,
+                                  pr_flgcredi => FALSE,
+                                  pr_nrseqava => 0,
+                                  pr_cdorigem => 5,
+                                  pr_cdcritic => vr_cdcritic,
+                                  pr_dscritic => vr_dscritic);
+  IF vr_cdcritic IS NOT NULL OR vr_dscritic IS NOT NULL THEN
+    RAISE vr_exc_saida;
+  END IF;
+  
+  vr_cdcooper  := 10;
+  vr_nrdconta  := 143219;
+  vr_nrctremp  := 23899;
+  vr_vllanmto  := 360.00;
+  vr_cdhistor  := 1044;
+  
+   -- Cria o lancamento de estorno
+  EMPR0001.pc_cria_lancamento_lem(pr_cdcooper => vr_cdcooper,
+                                  pr_dtmvtolt => rw_crapdat.dtmvtolt,
+                                  pr_cdagenci => rw_crapass.cdagenci,
+                                  pr_cdbccxlt => 100,
+                                  pr_cdoperad => 1,
+                                  pr_cdpactra => rw_crapass.cdagenci,
+                                  pr_tplotmov => 5,
+                                  pr_nrdolote => 600031,
+                                  pr_nrdconta => vr_nrdconta,
+                                  pr_cdhistor => vr_cdhistor,
+                                  pr_nrctremp => vr_nrctremp,
+                                  pr_vllanmto => vr_vllanmto,
+                                  pr_dtpagemp => rw_crapdat.dtmvtolt,
+                                  pr_txjurepr => 0,
+                                  pr_vlpreemp => 0,
+                                  pr_nrsequni => 0,
+                                  pr_nrparepr => 8,
+                                  pr_flgincre => FALSE,
+                                  pr_flgcredi => FALSE,
+                                  pr_nrseqava => 0,
+                                  pr_cdorigem => 5,
+                                  pr_cdcritic => vr_cdcritic,
+                                  pr_dscritic => vr_dscritic);
+  IF vr_cdcritic IS NOT NULL OR vr_dscritic IS NOT NULL THEN
+    RAISE vr_exc_saida;
+  END IF;
+  
+  vr_cdcooper  := 10;
+  vr_nrdconta  := 143219;
+  vr_nrctremp  := 23899;
+  vr_vllanmto  := 6.87;
+  vr_cdhistor  := 1048;
+  
+   -- Cria o lancamento de estorno
+  EMPR0001.pc_cria_lancamento_lem(pr_cdcooper => vr_cdcooper,
+                                  pr_dtmvtolt => rw_crapdat.dtmvtolt,
+                                  pr_cdagenci => rw_crapass.cdagenci,
+                                  pr_cdbccxlt => 100,
+                                  pr_cdoperad => 1,
+                                  pr_cdpactra => rw_crapass.cdagenci,
+                                  pr_tplotmov => 5,
+                                  pr_nrdolote => 600031,
+                                  pr_nrdconta => vr_nrdconta,
+                                  pr_cdhistor => vr_cdhistor,
+                                  pr_nrctremp => vr_nrctremp,
+                                  pr_vllanmto => vr_vllanmto,
+                                  pr_dtpagemp => rw_crapdat.dtmvtolt,
+                                  pr_txjurepr => 0,
+                                  pr_vlpreemp => 0,
+                                  pr_nrsequni => 0,
+                                  pr_nrparepr => 8,
+                                  pr_flgincre => FALSE,
+                                  pr_flgcredi => FALSE,
+                                  pr_nrseqava => 0,
+                                  pr_cdorigem => 5,
+                                  pr_cdcritic => vr_cdcritic,
+                                  pr_dscritic => vr_dscritic);
+  IF vr_cdcritic IS NOT NULL OR vr_dscritic IS NOT NULL THEN
+    RAISE vr_exc_saida;
+  END IF;
+  
+  vr_cdcooper  := 10;
+  vr_nrdconta  := 143219;
+  vr_nrctremp  := 23899;
+  vr_vllanmto  := 350.54;
+  vr_cdhistor  := 1044;
+  
+   -- Cria o lancamento de estorno
+  EMPR0001.pc_cria_lancamento_lem(pr_cdcooper => vr_cdcooper,
+                                  pr_dtmvtolt => rw_crapdat.dtmvtolt,
+                                  pr_cdagenci => rw_crapass.cdagenci,
+                                  pr_cdbccxlt => 100,
+                                  pr_cdoperad => 1,
+                                  pr_cdpactra => rw_crapass.cdagenci,
+                                  pr_tplotmov => 5,
+                                  pr_nrdolote => 600031,
+                                  pr_nrdconta => vr_nrdconta,
+                                  pr_cdhistor => vr_cdhistor,
+                                  pr_nrctremp => vr_nrctremp,
+                                  pr_vllanmto => vr_vllanmto,
+                                  pr_dtpagemp => rw_crapdat.dtmvtolt,
+                                  pr_txjurepr => 0,
+                                  pr_vlpreemp => 0,
+                                  pr_nrsequni => 0,
+                                  pr_nrparepr => 9,
+                                  pr_flgincre => FALSE,
+                                  pr_flgcredi => FALSE,
+                                  pr_nrseqava => 0,
+                                  pr_cdorigem => 5,
+                                  pr_cdcritic => vr_cdcritic,
+                                  pr_dscritic => vr_dscritic);
+  IF vr_cdcritic IS NOT NULL OR vr_dscritic IS NOT NULL THEN
+    RAISE vr_exc_saida;
+  END IF;
+  
+  vr_cdcooper  := 10;
+  vr_nrdconta  := 143219;
+  vr_nrctremp  := 23899;
+  vr_vllanmto  := 16.33;
+  vr_cdhistor  := 1048;
+  
+   -- Cria o lancamento de estorno
+  EMPR0001.pc_cria_lancamento_lem(pr_cdcooper => vr_cdcooper,
+                                  pr_dtmvtolt => rw_crapdat.dtmvtolt,
+                                  pr_cdagenci => rw_crapass.cdagenci,
+                                  pr_cdbccxlt => 100,
+                                  pr_cdoperad => 1,
+                                  pr_cdpactra => rw_crapass.cdagenci,
+                                  pr_tplotmov => 5,
+                                  pr_nrdolote => 600031,
+                                  pr_nrdconta => vr_nrdconta,
+                                  pr_cdhistor => vr_cdhistor,
+                                  pr_nrctremp => vr_nrctremp,
+                                  pr_vllanmto => vr_vllanmto,
+                                  pr_dtpagemp => rw_crapdat.dtmvtolt,
+                                  pr_txjurepr => 0,
+                                  pr_vlpreemp => 0,
+                                  pr_nrsequni => 0,
+                                  pr_nrparepr => 9,
+                                  pr_flgincre => FALSE,
+                                  pr_flgcredi => FALSE,
+                                  pr_nrseqava => 0,
+                                  pr_cdorigem => 5,
+                                  pr_cdcritic => vr_cdcritic,
+                                  pr_dscritic => vr_dscritic);
+  IF vr_cdcritic IS NOT NULL OR vr_dscritic IS NOT NULL THEN
+    RAISE vr_exc_saida;
+  END IF;
+  
+  vr_cdcooper  := 10;
+  vr_nrdconta  := 143219;
+  vr_nrctremp  := 23899;
+  vr_vllanmto  := 710.54;
+  vr_cdhistor  := 1040;
+  
+   -- Cria o lancamento de estorno
+  EMPR0001.pc_cria_lancamento_lem(pr_cdcooper => vr_cdcooper,
+                                  pr_dtmvtolt => rw_crapdat.dtmvtolt,
+                                  pr_cdagenci => rw_crapass.cdagenci,
+                                  pr_cdbccxlt => 100,
+                                  pr_cdoperad => 1,
+                                  pr_cdpactra => rw_crapass.cdagenci,
+                                  pr_tplotmov => 5,
+                                  pr_nrdolote => 600031,
+                                  pr_nrdconta => vr_nrdconta,
+                                  pr_cdhistor => vr_cdhistor,
+                                  pr_nrctremp => vr_nrctremp,
+                                  pr_vllanmto => vr_vllanmto,
+                                  pr_dtpagemp => rw_crapdat.dtmvtolt,
+                                  pr_txjurepr => 0,
+                                  pr_vlpreemp => 0,
+                                  pr_nrsequni => 0,
+                                  pr_nrparepr => 0,
+                                  pr_flgincre => FALSE,
+                                  pr_flgcredi => FALSE,
+                                  pr_nrseqava => 0,
+                                  pr_cdorigem => 5,
+                                  pr_cdcritic => vr_cdcritic,
+                                  pr_dscritic => vr_dscritic);
+  IF vr_cdcritic IS NOT NULL OR vr_dscritic IS NOT NULL THEN
+    RAISE vr_exc_saida;
+  END IF;
+  
+  vr_cdcooper  := 10;
+  vr_nrdconta  := 143227;
+  vr_nrctremp  := 21595;
+  vr_vllanmto  := 6.38;
+  vr_cdhistor  := 1705;
+  
+   -- Cria o lancamento de estorno
+  EMPR0001.pc_cria_lancamento_lem(pr_cdcooper => vr_cdcooper,
+                                  pr_dtmvtolt => rw_crapdat.dtmvtolt,
+                                  pr_cdagenci => rw_crapass.cdagenci,
+                                  pr_cdbccxlt => 100,
+                                  pr_cdoperad => 1,
+                                  pr_cdpactra => rw_crapass.cdagenci,
+                                  pr_tplotmov => 5,
+                                  pr_nrdolote => 600031,
+                                  pr_nrdconta => vr_nrdconta,
+                                  pr_cdhistor => vr_cdhistor,
+                                  pr_nrctremp => vr_nrctremp,
+                                  pr_vllanmto => vr_vllanmto,
+                                  pr_dtpagemp => rw_crapdat.dtmvtolt,
+                                  pr_txjurepr => 0,
+                                  pr_vlpreemp => 0,
+                                  pr_nrsequni => 0,
+                                  pr_nrparepr => 0,
+                                  pr_flgincre => FALSE,
+                                  pr_flgcredi => FALSE,
+                                  pr_nrseqava => 0,
+                                  pr_cdorigem => 5,
+                                  pr_cdcritic => vr_cdcritic,
+                                  pr_dscritic => vr_dscritic);
+  IF vr_cdcritic IS NOT NULL OR vr_dscritic IS NOT NULL THEN
+    RAISE vr_exc_saida;
+  END IF;
+
+  vr_cdcooper  := 10;
+  vr_nrdconta  := 143359;
+  vr_nrctremp  := 21600;
+  vr_vllanmto  := 5.11;
+  vr_cdhistor  := 1705;
+  
+   -- Cria o lancamento de estorno
+  EMPR0001.pc_cria_lancamento_lem(pr_cdcooper => vr_cdcooper,
+                                  pr_dtmvtolt => rw_crapdat.dtmvtolt,
+                                  pr_cdagenci => rw_crapass.cdagenci,
+                                  pr_cdbccxlt => 100,
+                                  pr_cdoperad => 1,
+                                  pr_cdpactra => rw_crapass.cdagenci,
+                                  pr_tplotmov => 5,
+                                  pr_nrdolote => 600031,
+                                  pr_nrdconta => vr_nrdconta,
+                                  pr_cdhistor => vr_cdhistor,
+                                  pr_nrctremp => vr_nrctremp,
+                                  pr_vllanmto => vr_vllanmto,
+                                  pr_dtpagemp => rw_crapdat.dtmvtolt,
+                                  pr_txjurepr => 0,
+                                  pr_vlpreemp => 0,
+                                  pr_nrsequni => 0,
+                                  pr_nrparepr => 0,
+                                  pr_flgincre => FALSE,
+                                  pr_flgcredi => FALSE,
+                                  pr_nrseqava => 0,
+                                  pr_cdorigem => 5,
+                                  pr_cdcritic => vr_cdcritic,
+                                  pr_dscritic => vr_dscritic);
+  IF vr_cdcritic IS NOT NULL OR vr_dscritic IS NOT NULL THEN
+    RAISE vr_exc_saida;
+  END IF;
+
+
+  COMMIT;
+EXCEPTION
+  WHEN vr_exc_saida THEN
+    RAISE_application_error(-20500, vr_dscritic);
+    ROLLBACK;
+  WHEN OTHERS THEN
+    RAISE_application_error(-20500, SQLERRM);
+    ROLLBACK;
+end;
