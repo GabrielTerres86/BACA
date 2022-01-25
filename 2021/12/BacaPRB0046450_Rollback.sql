cd38c5be591f6434a9ea5af522e6fee939fb37cb
Olga Linares <premiersoft-olga.linares@ailos.coop.br>
2022-01-25 11:25:11 -0300

    PRB0046450 - Melhoria Baca

---
+++ 2021/12/BacaPRB0046450_Rollback.sql
@@ -0,0 +1,76 @@
+BEGIN
+
+
+INSERT ALL
+  INTO cecred.tbcapt_custodia_arquivo (idarquivo, nmarquivo,idtipo_arquivo,idtipo_operacao,idarquivo_origem,idsituacao,dtcriacao,dtprocesso,dtregistro) VALUES (69547.00, '03021000.210625_DMOVTRANSF_REG_69547.TXT', 1,   'E'  ,'',1,TO_DATE('28/06/2021 17:34:39','dd/mm/yyyy hh24:mi:ss'), TO_DATE('28/06/2021 17:35:03','dd/mm/yyyy hh24:mi:ss'),TO_DATE('25/06/2021','dd/mm/yyyy'))
+  INTO cecred.tbcapt_custodia_arquivo (idarquivo, nmarquivo,idtipo_arquivo,idtipo_operacao,idarquivo_origem,idsituacao,dtcriacao,dtprocesso,dtregistro) VALUES (69548.00, '03021000.210625_DMOVTRANSF_OPE_69548.TXT',  2,   'E' ,'',1,TO_DATE('28/06/2021 17:34:40','dd/mm/yyyy hh24:mi:ss'), TO_DATE('28/06/2021 17:35:04','dd/mm/yyyy hh24:mi:ss'),TO_DATE('25/06/2021','dd/mm/yyyy'))
+  INTO cecred.tbcapt_custodia_arquivo (idarquivo, nmarquivo,idtipo_arquivo,idtipo_operacao,idarquivo_origem,idsituacao,dtcriacao,dtprocesso,dtregistro) VALUES (69549.00, '03021000.210625_DMOVTRANSF_REG_69549.TXT', 1,   'E'  ,'',1,TO_DATE('28/06/2021 17:34:42','dd/mm/yyyy hh24:mi:ss'), TO_DATE('28/06/2021 17:35:05','dd/mm/yyyy hh24:mi:ss'),TO_DATE('25/06/2021','dd/mm/yyyy'))
+  INTO cecred.tbcapt_custodia_arquivo (idarquivo, nmarquivo,idtipo_arquivo,idtipo_operacao,idarquivo_origem,idsituacao,dtcriacao,dtprocesso,dtregistro) VALUES (69550.00, '03021000.210625_DMOVTRANSF_OPE_69550.TXT',  2,   'E' ,'',1,TO_DATE('28/06/2021 17:34:43','dd/mm/yyyy hh24:mi:ss'), TO_DATE('28/06/2021 17:35:06','dd/mm/yyyy hh24:mi:ss'),TO_DATE('25/06/2021','dd/mm/yyyy'))
+  INTO cecred.tbcapt_custodia_arquivo (idarquivo, nmarquivo,idtipo_arquivo,idtipo_operacao,idarquivo_origem,idsituacao,dtcriacao,dtprocesso,dtregistro) VALUES (69551.00, '03021000.210625_DMOVTRANSF_REG_69551.TXT', 1,   'E'  ,'',1,TO_DATE('28/06/2021 17:34:44','dd/mm/yyyy hh24:mi:ss'), TO_DATE('28/06/2021 17:35:08','dd/mm/yyyy hh24:mi:ss'),TO_DATE('25/06/2021','dd/mm/yyyy'))
+  INTO cecred.tbcapt_custodia_arquivo (idarquivo, nmarquivo,idtipo_arquivo,idtipo_operacao,idarquivo_origem,idsituacao,dtcriacao,dtprocesso,dtregistro) VALUES (69552.00, '03021000.210625_DMOVTRANSF_OPE_69552.TXT',  2,   'E' ,'',1,TO_DATE('28/06/2021 17:34:44','dd/mm/yyyy hh24:mi:ss'), TO_DATE('28/06/2021 17:35:09','dd/mm/yyyy hh24:mi:ss'),TO_DATE('25/06/2021','dd/mm/yyyy'))
+  INTO cecred.tbcapt_custodia_arquivo (idarquivo, nmarquivo,idtipo_arquivo,idtipo_operacao,idarquivo_origem,idsituacao,dtcriacao,dtprocesso,dtregistro) VALUES (69553.00, '03021000.210625_DMOVTRANSF_REG_69553.TXT', 1,   'E'  ,'',1,TO_DATE('28/06/2021 17:34:44','dd/mm/yyyy hh24:mi:ss'), TO_DATE('28/06/2021 17:35:10','dd/mm/yyyy hh24:mi:ss'),TO_DATE('25/06/2021','dd/mm/yyyy'))
+  INTO cecred.tbcapt_custodia_arquivo (idarquivo, nmarquivo,idtipo_arquivo,idtipo_operacao,idarquivo_origem,idsituacao,dtcriacao,dtprocesso,dtregistro) VALUES (69554.00, '03021000.210625_DMOVTRANSF_OPE_69554.TXT',  2,   'E' ,'',1,TO_DATE('28/06/2021 17:34:45','dd/mm/yyyy hh24:mi:ss'), TO_DATE('28/06/2021 17:35:11','dd/mm/yyyy hh24:mi:ss'),TO_DATE('25/06/2021','dd/mm/yyyy'))
+  INTO cecred.tbcapt_custodia_arquivo (idarquivo, nmarquivo,idtipo_arquivo,idtipo_operacao,idarquivo_origem,idsituacao,dtcriacao,dtprocesso,dtregistro) VALUES (69555.00, '03021000.210625_DMOVTRANSF_REG_69555.TXT', 1,   'E'  ,'',1,TO_DATE('28/06/2021 17:34:46','dd/mm/yyyy hh24:mi:ss'), TO_DATE('28/06/2021 17:35:12','dd/mm/yyyy hh24:mi:ss'),TO_DATE('25/06/2021','dd/mm/yyyy'))
+  INTO cecred.tbcapt_custodia_arquivo (idarquivo, nmarquivo,idtipo_arquivo,idtipo_operacao,idarquivo_origem,idsituacao,dtcriacao,dtprocesso,dtregistro) VALUES (69556.00, '03021000.210625_DMOVTRANSF_OPE_69556.TXT',  2,   'E' ,'',1,TO_DATE('28/06/2021 17:34:46','dd/mm/yyyy hh24:mi:ss'), TO_DATE('28/06/2021 17:35:13','dd/mm/yyyy hh24:mi:ss'),TO_DATE('25/06/2021','dd/mm/yyyy'))
+  INTO cecred.tbcapt_custodia_arquivo (idarquivo, nmarquivo,idtipo_arquivo,idtipo_operacao,idarquivo_origem,idsituacao,dtcriacao,dtprocesso,dtregistro) VALUES (69557.00, '03021000.210625_DMOVTRANSF_REG_69557.TXT', 1,   'E'  ,'',1,TO_DATE('28/06/2021 17:34:46','dd/mm/yyyy hh24:mi:ss'), TO_DATE('28/06/2021 17:35:14','dd/mm/yyyy hh24:mi:ss'),TO_DATE('25/06/2021','dd/mm/yyyy'))
+SELECT * FROM dual;
+
+update cecred.tbcapt_custodia_lanctos  
+set idsituacao = 0
+where idlancamento = 30395124;
+			  
+
+merge into cecred.tbcapt_custodia_lanctos t1
+using (
+  select lct.*, 
+         count(1) over(partition by lct.cdcooper, lct.idtipo_arquivo, lct.dtregistro) qtregis,
+         row_number() over(partition by lct.cdcooper, lct.idtipo_arquivo, lct.dtregistro order by lct.cdcooper, lct.idtipo_arquivo, lct.dtregistro) nrseqreg
+    from (select rda.cdcooper,
+                 rda.nrdconta,
+                 rda.nraplica,
+                 0 cdprodut,
+                 lct.*
+            from cecred.craprda                   rda,
+                 cecred.tbcapt_custodia_aplicacao apl,
+                 cecred.tbcapt_custodia_lanctos   lct
+           where apl.idaplicacao = lct.idaplicacao
+             and rda.idaplcus = apl.idaplicacao
+             and lct.idsituacao = 0
+             and apl.tpaplicacao in (1, 2)
+             and lct.idtipo_arquivo <> 9
+             and ((lct.idtipo_lancto = 2 and rda.cdcooper in (2,5,6,7,8)) or
+                  (lct.idtipo_lancto = 1 and rda.cdcooper in (2,5,6,7,8,9) and nvl(rda.insaqtot,0) = 0))
+             and trunc(lct.dtregistro) = to_date('25/06/2021','dd/mm/yyyy')
+          union
+          select rac.cdcooper,
+                 rac.nrdconta,
+                 rac.nraplica,
+                 rac.cdprodut,
+                 lct.*
+            from cecred.craprac                   rac,
+                 cecred.tbcapt_custodia_aplicacao apl,
+                 cecred.tbcapt_custodia_lanctos   lct
+           where apl.idaplicacao = lct.idaplicacao
+             and rac.idaplcus = apl.idaplicacao
+             and lct.idsituacao = 0
+             and apl.tpaplicacao in (3, 4)
+             and lct.idtipo_arquivo <> 9
+             and ((lct.idtipo_lancto = 2 and rac.cdcooper in (2,5,6,7,8)) or
+                  (lct.idtipo_lancto = 1 and rac.cdcooper in (2,5,6,7,8,9) and nvl(rac.idsaqtot,0) = 0))
+             and trunc(lct.dtregistro) = to_date('25/06/2021','dd/mm/yyyy')) lct
+         WHERE (lct.idlancto_origem is null or
+                exists (select 1
+                          from cecred.tbcapt_custodia_lanctos lctori
+                         where lctori.idlancamento = lct.idlancto_origem
+                           and lctori.idsituacao = 8
+                           and lctori.cdoperac_cetip is not null))) t2
+ON (t1.idaplicacao = t2.idaplicacao and t1.idlancamento = t2.idlancamento)
+WHEN MATCHED THEN
+UPDATE SET T1.IDSITUACAO = 1;
+ 
+
+	
+	
+COMMIT;	
+
+END;
