831ed0999e85efcbc29dcdd465566e0c3c6971ff
Caroline <premiersoft-caroline.castro@ailos.coop.br>
2022-01-27 14:37:11 -0300

    Baca para retirar os vlaores pagos da tabela de parcela, evitando que pagamentos adiantados gerem saldos negativos.

--- 2022/01/inc0121244_saldonegativo.sql
+++ 2022/01/inc0121244_saldonegativo.sql
@@ -4,6 +4,8 @@ BEGIN
     SET    vlpagpar = 0
           ,vldstcor = 0
           ,vldstrem = 0
+          ,vlsdvatu = vlparepr 
+          ,vlsdvpar = vlparepr
     WHERE  cdcooper = 1
     AND    nrdconta = 2772728
     AND    nrctremp = 2362968
@@ -13,6 +15,8 @@ BEGIN
     SET    vlpagpar = 0
           ,vldstcor = 0
           ,vldstrem = 0
+          ,vlsdvatu = vlparepr 
+          ,vlsdvpar = vlparepr
     WHERE  cdcooper = 1
     AND    nrdconta = 6461328
     AND    nrctremp = 2219322
@@ -22,6 +26,8 @@ BEGIN
     SET    vlpagpar = 0
           ,vldstcor = 0
           ,vldstrem = 0
+          ,vlsdvatu = vlparepr 
+          ,vlsdvpar = vlparepr
     WHERE  cdcooper = 1
     AND    nrdconta = 7780095
     AND    nrctremp = 2332598
