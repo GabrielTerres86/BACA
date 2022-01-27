6d33959bc7ede65251b10bea18569a145e54772f
Alessandro Hoffelder <alessandro.hoffelder@ailos.coop.br>
2022-01-27 18:28:31 +0000

    Updated RITM0177419.sql

--- 2022/01/RITM0177419.sql
+++ 2022/01/RITM0177419.sql
@@ -1,7 +1,7 @@
 begin
 
   insert into craprel (CDRELATO, NRVIADEF, NRVIAMAX, NMRELATO, NRMODULO, NMDESTIN, NMFORMUL, INDAUDIT, CDCOOPER, PERIODIC, TPRELATO, INIMPREL, INGERPDF, DSDEMAIL, CDFILREL, NRSEQPRI)
-  values (1002, 1, 1, 'RELATORIO SENHA TA', 1, 'cartoes\claudio', 'padrao', 0, 3, 'M', 1, 1, 2, ' ', null, null);
+  values (1002, 1, 1, 'RELATORIO SENHA TA', 1, 'cartoes', 'padrao', 0, 3, 'M', 1, 1, 2, ' ', null, null);
 
   commit;
 
