d4c4508a25c1ce4ae7d46496352c8e41b80afcb1
Filipe Ribeiro <mouts-filipe.rego@ailos.coop.br>
2022-02-01 11:14:25 -0300

    SCRIPT - Chave da SEGPRE

---
+++ 2022/02/seguro_pessoa_nao_contributario.sql
@@ -0,0 +1,59 @@
+----------------------------Ribeiro------------------------------------------
+--- Chave SEGPRE
+BEGIN
+  INSERT INTO crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+    VALUES('CRED', 0, 'UTILIZA_REGRAS_SEGPRE', 'Utiliza novas regras prestamista SEGPRE S (SIM) e N (NÃO)', 'N');
+  COMMIT;
+EXCEPTION WHEN OTHERS THEN
+  DBMS_OUTPUT.PUT_LINE('3 ERRO: ' || SQLERRM);
+  ROLLBACK;
+END;
+/
+-- TELA SEGPRE
+BEGIN
+  UPDATE CRAPACA SET LSTPARAM = LSTPARAM ||', PR_TPCUSTEI' where NMPACKAG = 'TELA_SEGPRE' AND NMDEACAO = 'SEGPRE_CONSULTAR';
+  COMMIT;
+EXCEPTION WHEN OTHERS THEN
+  DBMS_OUTPUT.PUT_LINE('4 ERRO: ' || SQLERRM);
+  ROLLBACK;
+END;
+/
+-- TELA SEGPRE
+BEGIN
+  UPDATE crapaca c
+     SET c.lstparam = c.lstparam || ', PR_PAGSEGU, PR_SEQARQU'
+   WHERE c.nmpackag = 'TELA_SEGPRE'
+     AND c.nmproced = 'pc_alterar';
+    COMMIT;
+EXCEPTION WHEN OTHERS THEN
+  DBMS_OUTPUT.PUT_LINE('8 ERRO: ' || SQLERRM);
+  ROLLBACK;
+END;
+/
+------------------RAIZA-------------------
+-- PERMISSAO SEGPRE
+BEGIN
+  INSERT INTO crapace(nmdatela,
+                      cddopcao,
+                      cdoperad,
+                      nmrotina,
+                      cdcooper,
+                      nrmodulo,
+                      idevento,
+                      idambace)
+   (SELECT 'SEGPRE',
+           cddopcao,
+           cdoperad,
+           nmrotina,
+           cdcooper,
+           nrmodulo,
+           idevento,
+           idambace
+      FROM CRAPACE 
+     WHERE NMDATELA = 'TAB049');
+  COMMIT;
+EXCEPTION WHEN OTHERS THEN  
+  DBMS_OUTPUT.PUT_LINE('11 ERRO: ' || SQLERRM);
+  ROLLBACK;
+END;
+/
