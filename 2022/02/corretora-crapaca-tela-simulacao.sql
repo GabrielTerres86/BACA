fabbe36db3c3372f0ef4ec0fed68bb80a2001fc9
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2022-02-04 15:36:30 -0300

    Tela de Simulação de Empréstimos

--- 2022/02/corretora-crapaca-tela-simulacao.sql
+++ 2022/02/corretora-crapaca-tela-simulacao.sql
@@ -1,12 +1,11 @@
 BEGIN
-
   UPDATE crapaca c
      SET c.lstparam = lstparam || ', pr_flgassum, pr_flggarad'
    WHERE c.nmpackag = 'TELA_ATENDA_SIMULACAO'
      AND c.nmdeacao = 'SIMULA_GRAVA_SIMULACAO';
 	 
   INSERT INTO crapaca (NRSEQACA, NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
-	VALUES ((select max(NRSEQACA) + 1 from crapaca), 'CONSULTA_CRAPLCR_TPCUSPR', 'TELA_ATENDA_SEGURO','pc_retorna_tpcuspr','pr_cdcooper,pr_cdlcremp,pr_nrdconta',
+	VALUES ((select max(NRSEQACA) + 1 from crapaca), 'CONSULTA_CRAPLCR_TPCUSPR','TELA_ATENDA_SEGURO','pc_retorna_tpcuspr','pr_cdcooper,pr_cdlcremp,pr_nrdconta,pr_nrctremp',
 	(SELECT nrseqrdr from CRAPRDR WHERE nmprogra = 'TELA_ATENDA_SEGURO')); 
 	
   COMMIT;
