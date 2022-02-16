3f4c3dbb5ce015192c1d582aebe4c51694dfce5d
Petrônio Sifuentes Gomes Júnior <mouts-petronio.junior@ailos.coop.br>
2022-02-16 10:42:15 -0300

    Atualização tanque para teste

--- 2022/02/update-data-teste-pld.sql
+++ 2022/02/update-data-teste-pld.sql
@@ -1,6 +1,6 @@
 declare
 begin  
-update cecred.crapass set dtabtcct = trunc(SYSDATE) - 1 where nrdconta = 434000 and cdcooper = 7;
+update cecred.tbcalris_tanque tanque set tanque.cdstatus = 1 where tanque.nrcpfcgc in (72333938005, 12064245081, 37613080005)
 commit;
 end;
 /
