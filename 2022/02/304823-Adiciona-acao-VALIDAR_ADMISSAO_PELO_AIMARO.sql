bb77602088d987d6da7ff163ed685847fb72828c
Alexandre Oliveira <alexandre.santos@premiersoft.net>
2022-02-17 13:58:05 -0300

    * Adicionando a ação para consultar permissão de criação de contas pelo Aimaro Web.

---
+++ 2022/02/304823-Adiciona-acao-VALIDAR_ADMISSAO_PELO_AIMARO.sql
@@ -0,0 +1,22 @@
+BEGIN
+  --
+  INSERT INTO CECRED.crapaca (
+    nmdeacao
+    , nmpackag
+    , nmproced
+    , lstparam
+    , nrseqrdr
+  ) VALUES (
+    'VALIDAR_ADMISSAO_PELO_AIMARO'
+    , NULL
+    , 'validarAdmissaoPeloAimaro'
+    , NULL
+    , 211 -- matric
+  );
+  --
+  COMMIT;
+  --
+EXCEPTION
+  WHEN OTHERS THEN
+    RAISE_APPLICATION_ERROR(-20000, 'ERRO: ' || SQLERRM);
+END;
