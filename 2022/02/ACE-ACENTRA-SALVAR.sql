d5f977ed122c30859035cb7986024465a2112170
Vinicius Bedin Knapp <mouts-vinicius.knapp@ailos.coop.br>
2022-02-18 11:00:24 -0300

    Acesso inclusão de imóvel Acentra.

---
+++ 2022/02/ACE-ACENTRA-SALVAR.sql
@@ -0,0 +1,30 @@
+DECLARE
+  TYPE tp_varray IS VARRAY(3) OF VARCHAR2(10);
+  vr_user tp_varray := tp_varray('f0050332',
+								 'f0050058',
+								 'f0050040');
+BEGIN
+  DELETE FROM crapace
+   WHERE UPPER(crapace.nmdatela) = 'IMOVEL'
+     AND UPPER(crapace.cddopcao) = 'N'
+     AND UPPER(crapace.nmrotina) = ' '
+     AND crapace.cdcooper = 5
+     AND crapace.idambace = 2;
+  COMMIT;
+  FOR i IN 1 .. vr_user.count LOOP
+    BEGIN
+      INSERT INTO crapace
+        (nmdatela, cddopcao, cdoperad, nmrotina, cdcooper, nrmodulo, idevento, idambace)
+      VALUES
+        ('IMOVEL', 'N', vr_user(i), ' ', 5, 1, 0, 2);
+      COMMIT;
+    EXCEPTION
+      WHEN OTHERS THEN	    
+        ROLLBACK;	
+    END;
+  END LOOP;
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    RAISE_application_error(-20500, SQLERRM);
+END;	   
