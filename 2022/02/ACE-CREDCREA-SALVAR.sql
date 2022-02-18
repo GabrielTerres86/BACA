f3ffaf19c203f87f064e8792dca1a6dbc7c9400f
Vinicius Bedin Knapp <mouts-vinicius.knapp@ailos.coop.br>
2022-02-18 10:55:02 -0300

    Acesso inclusão de imóvel Credcrea.

---
+++ 2022/02/ACE-CREDCREA-SALVAR.sql
@@ -0,0 +1,35 @@
+DECLARE
+  TYPE tp_varray IS VARRAY(8) OF VARCHAR2(10);
+  vr_user tp_varray := tp_varray('f0070618',
+								 'f0070686',
+								 'f0070631',
+								 'f0070451',
+								 'f0070397',
+								 'f0070079',
+								 'f0070681',
+								 'f0070001');
+BEGIN
+  DELETE FROM crapace
+   WHERE UPPER(crapace.nmdatela) = 'IMOVEL'
+     AND UPPER(crapace.cddopcao) = 'N'
+     AND UPPER(crapace.nmrotina) = ' '
+     AND crapace.cdcooper = 7
+     AND crapace.idambace = 2;
+  COMMIT;
+  FOR i IN 1 .. vr_user.count LOOP
+    BEGIN
+      INSERT INTO crapace
+        (nmdatela, cddopcao, cdoperad, nmrotina, cdcooper, nrmodulo, idevento, idambace)
+      VALUES
+        ('IMOVEL', 'N', vr_user(i), ' ', 7, 1, 0, 2);
+      COMMIT;
+    EXCEPTION
+      WHEN OTHERS THEN	    
+        ROLLBACK;	
+    END;
+  END LOOP;
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    RAISE_application_error(-20500, SQLERRM);
+END;	   
