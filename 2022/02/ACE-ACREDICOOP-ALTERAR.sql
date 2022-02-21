206f503e654e2c92179ec94033cb3c713484c80f
Vinicius Bedin Knapp <mouts-vinicius.knapp@ailos.coop.br>
2022-02-21 09:21:54 -0300

    Acesso a opção de Alterar da tela Imóvel.

---
+++ 2022/02/ACE-ACREDICOOP-ALTERAR.sql
@@ -0,0 +1,36 @@
+DECLARE
+  TYPE tp_varray IS VARRAY(9) OF VARCHAR2(10);
+  vr_user tp_varray := tp_varray('f0020460',
+								 'f0020379',
+								 'f0020471',
+								 'f0020380',
+								 'f0020295',
+								 'f0020405',
+								 'f0020661',
+								 'f0020695',
+								 'f0020403');
+BEGIN
+  DELETE FROM crapace
+   WHERE UPPER(crapace.nmdatela) = 'IMOVEL'
+     AND UPPER(crapace.cddopcao) = 'A'
+     AND UPPER(crapace.nmrotina) = ' '
+     AND crapace.cdcooper = 2
+     AND crapace.idambace = 2;
+  COMMIT;
+  FOR i IN 1 .. vr_user.count LOOP
+    BEGIN
+      INSERT INTO crapace
+        (nmdatela, cddopcao, cdoperad, nmrotina, cdcooper, nrmodulo, idevento, idambace)
+      VALUES
+        ('IMOVEL', 'A', vr_user(i), ' ', 2, 1, 0, 2);
+      COMMIT;
+    EXCEPTION
+      WHEN OTHERS THEN	    
+        ROLLBACK;	
+    END;
+  END LOOP;
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    RAISE_application_error(-20500, SQLERRM);
+END;	   
---
+++ 2022/02/ACE-VIACREDI-ALTERAR.sql
@@ -0,0 +1,34 @@
+DECLARE
+  TYPE tp_varray IS VARRAY(7) OF VARCHAR2(10);
+  vr_user tp_varray := tp_varray('f0014246',
+								 'f0015571',
+								 'f0014236',
+								 'f0015085',
+								 'f0014948',
+								 'f0014339',
+								 'f0013438');
+BEGIN
+  DELETE FROM crapace
+   WHERE UPPER(crapace.nmdatela) = 'IMOVEL'
+     AND UPPER(crapace.cddopcao) = 'A'
+     AND UPPER(crapace.nmrotina) = ' '
+     AND crapace.cdcooper = 1
+     AND crapace.idambace = 2;
+  COMMIT;
+  FOR i IN 1 .. vr_user.count LOOP
+    BEGIN
+      INSERT INTO crapace
+        (nmdatela, cddopcao, cdoperad, nmrotina, cdcooper, nrmodulo, idevento, idambace)
+      VALUES
+        ('IMOVEL', 'A', vr_user(i), ' ', 1, 1, 0, 2);
+      COMMIT;
+    EXCEPTION
+      WHEN OTHERS THEN	    
+        ROLLBACK;	
+    END;
+  END LOOP;
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    RAISE_application_error(-20500, SQLERRM);
+END;	   
