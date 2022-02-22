ba3bb75df911be328bce73eeb924b5da9924d52e
Renan Fiedler de Oliveira <renan.fiedler@ailos.coop.br>
2022-02-22 11:54:36 -0300

    [INC0126665] Alterando crapdir - IR

---
+++ 2022/02/INC0126665_update_crapdir.sql
@@ -0,0 +1,18 @@
+begin
+  update crapdir set vlsdapli = 51394.11  where progress_recid = 9516500;
+  update crapdir set vlsdapli = 252171.59 where progress_recid = 9517466;
+  update crapdir set vlsdapli = 22530.97  where progress_recid = 9571933;
+  update crapdir set vlsdapli = 135754.59 where progress_recid = 9518695;
+  update crapdir set vlsdapli = 6215.29   where progress_recid = 9580810;
+  update crapdir set vlsdapli = 7687.76   where progress_recid = 9597576;
+  update crapdir set vlsdapli = 21.09     where progress_recid = 9303831;
+  update crapdir set vlsdapli = 4.89      where progress_recid = 9303898;
+  update crapdir set vlsdapli = 10.85     where progress_recid = 9303925;
+  update crapdir set vlsdapli = 0.23      where progress_recid = 9304255;
+  update crapdir set vlsdapli = 26.97     where progress_recid = 9304294;
+  update crapdir set vlsdapli = 0.02      where progress_recid = 9304352;
+  update crapdir set vlsdapli = 0.02      where progress_recid = 8967798;
+  update crapdir set vlsdapli = 666.87    where progress_recid = 8968582;
+  update crapdir set vlsdapli = 0.08      where progress_recid = 8968913;
+  commit;
+end;
