287172aacff43511035ef4145629fe6e1eb774a8
Filipe Ribeiro <mouts-filipe.rego@ailos.coop.br>
2022-02-23 09:05:30 -0300

    Inclusão da chave data limite segpre

--- 2022/02/seguro_pessoa_insert_crapprm_data_limite_segpre.sql
+++ 2022/02/seguro_pessoa_insert_crapprm_data_limite_segpre.sql
@@ -1,15 +1,18 @@
 BEGIN
-  INSERT INTO crapprm
-    (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
-  VALUES
-    ('CRED',
-     0,
-     'DATA_LIMITE_SEGPRE',
-     'Data limite de geração de rotinas de seguro prestamista não contributário',
-     TO_DATE('31/12/2022', 'DD/MM/RRRR'));
-     COMMIT;
+  FOR rw_cdcooper IN (SELECT c.cdcooper FROM crapcop c WHERE c.flgativo = 1) LOOP
+    INSERT INTO crapprm
+      (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+    VALUES
+      ('CRED',
+       rw_cdcooper.cdcooper,
+       'DATA_LIMITE_SEGPRE',
+       'Data limite de geração de rotinas de seguro prestamista não contributário',
+       TO_DATE('31/12/2022', 'DD/MM/RRRR'));
+  END LOOP;
+  COMMIT;
 EXCEPTION
   WHEN OTHERS THEN
     DBMS_OUTPUT.PUT_LINE('ERRO: ' || SQLERRM);
+    ROLLBACK;
 END;
 /
