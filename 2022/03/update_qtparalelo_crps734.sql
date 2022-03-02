a6a8a79127ff95b78a6d28e6770b67d8d820d28c
Carlos Henrique Weinhold <carlos.weinhold@ailos.coop.br>
2022-03-02 15:13:55 -0300

    Ajuste da quantidade de paralelismo do programa crps734

--- 2022/03/update_qtparalelo_crps734.sql
+++ 2022/03/update_qtparalelo_crps734.sql
@@ -1,3 +1,4 @@
---INC0127905 Reativar o paralelismo do programa crps734, pois o mesmo não está rodando nas cooperativas, apenas na Viacredi
+BEGIN
 UPDATE tbgen_batch_param SET qtparalelo = 10 WHERE cdcooper > 1 AND cdprograma = 'CRPS734';
 COMMIT;
+END;
