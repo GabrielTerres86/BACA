e69ccdaf461c9702a43c7d3dbdd7807328255a49
Carlos Henrique Weinhold <carlos.weinhold@ailos.coop.br>
2022-03-03 09:56:14 -0300

    Inclusão do owner da tabela

--- 2022/03/update_qtparalelo_crps734.sql
+++ 2022/03/update_qtparalelo_crps734.sql
@@ -1,4 +1,4 @@
 BEGIN
-UPDATE tbgen_batch_param SET qtparalelo = 10 WHERE cdcooper > 1 AND cdprograma = 'CRPS734';
+UPDATE cecred.tbgen_batch_param SET qtparalelo = 10 WHERE cdcooper > 1 AND cdprograma = 'CRPS734';
 COMMIT;
 END;
