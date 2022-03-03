71a4f0b5061d24ec1ae9232e087cc618a5168154
Alexandre Oliveira <alexandre.santos@premiersoft.net>
2022-03-03 09:28:28 -0300

    * Ajuste no script

--- 2022/03/prj-onboardingpf-reprocessa-unificado.sql
+++ 2022/03/prj-onboardingpf-reprocessa-unificado.sql
@@ -1,13 +1,13 @@
 BEGIN
   
-  update cadastro.tbcadast_pessoa_atualiza a
+  update cecred.tbcadast_pessoa_atualiza a
     set a.insit_atualiza = 1
   where a.insit_atualiza = 3
-    and trunc(a.dhatualiza) >= trunc(sysdate-4);
+    and trunc(a.dhatualiza) >= trunc(sysdate-10);
   
   COMMIT;
   
 EXCEPTION
   WHEN OTHERS THEN
     RAISE_APPLICATION_ERROR(-20000, 'ERRO: ' || SQLERRM);
-END
+END;
