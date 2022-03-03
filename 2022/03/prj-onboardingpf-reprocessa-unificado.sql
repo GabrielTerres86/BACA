876ee707594b1fa9d15ddf33a574677a1fe73bd9
Alexandre Oliveira <alexandre.santos@premiersoft.net>
2022-03-03 09:30:37 -0300

    * Rodar script para alterar situação de algumas contas a fim de reprocessar a integração do Unificado.

--- 2022/03/prj-onboardingpf-reprocessa-unificado.sql
+++ 2022/03/prj-onboardingpf-reprocessa-unificado.sql
@@ -3,7 +3,7 @@ BEGIN
   update cecred.tbcadast_pessoa_atualiza a
     set a.insit_atualiza = 1
   where a.insit_atualiza = 3
-    and trunc(a.dhatualiza) >= trunc(sysdate-10);
+    and trunc(a.dhatualiza) >= trunc(sysdate-4);
   
   COMMIT;
   
