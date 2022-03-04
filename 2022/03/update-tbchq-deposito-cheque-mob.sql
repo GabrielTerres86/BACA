347a3be3594b9b353fc012ebd516ec553a48182a
Paulo Felipe Kowalsky <t0032603@cecred.coop.br>
2022-03-04 17:38:23 -0300

    ajuste update

--- 2022/03/update-tbchq-deposito-cheque-mob.sql
+++ 2022/03/update-tbchq-deposito-cheque-mob.sql
@@ -3,7 +3,7 @@ begin
 update cecred.tbchq_deposito_cheque_mob mob
   set  mob.insituacao = 4
 where  mob.insituacao = 1
-  and  mob.dtdeposito < to_date('07/03/2022','DD/MM/YYYY');
+  and  mob.dtdeposito <= to_date('07/03/2022','DD/MM/YYYY');
 
 
 commit;
