94b1ec3547aac2ea9b68bfe96f3be220869eb39d
DESKTOP-4FQRPJS\nunes <nunes.luiz1203@gmail.com>
2022-03-08 14:29:45 -0300

    correção script

--- 2022/03/Task290065IntegrarConveniocdcParametro.sql
+++ 2022/03/Task290065IntegrarConveniocdcParametro.sql
@@ -1,13 +1,16 @@
-DECLARE
-  CURSOR c_coop
-  IS
-    SELECT 
-        CDCOOPER
-    FROM 
-        crapcop;
 BEGIN
-  FOR r_coop IN c_coop
-  LOOP
-    insert into CRAPPRM(NMSISTEM, CDCOOPER,CDACESSO,DSTEXPRM,DSVLRPRM) values ('CDC', r_coop.CDCOOPER,'SNINTEGRA_CONVENIO_CDC', 'Ativa/Desativa integração de convênios CDC com esteira de crédito', '0');
-  END LOOP;
+    DECLARE
+    CURSOR c_coop
+    IS
+        SELECT 
+            CDCOOPER
+        FROM 
+            crapcop;
+    BEGIN
+    FOR r_coop IN c_coop
+    LOOP
+        insert into CRAPPRM(NMSISTEM, CDCOOPER,CDACESSO,DSTEXPRM,DSVLRPRM) values ('CDC', r_coop.CDCOOPER,'SNINTEGRA_CONVENIO_CDC', 'Ativa/Desativa integração de convênios CDC com esteira de crédito', '0');
+    END LOOP;
+    END;
 END;
+/
