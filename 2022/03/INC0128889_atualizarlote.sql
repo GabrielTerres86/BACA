f3b9eec0e53c9f74b3b4a9f8cb92241b6d1b32cf
Renier Figueiredo Padilha <renier.padilha@ailos.coop.br>
2022-03-09 15:12:11 -0300

    Hora Atualizada

--- 2022/03/INC0128889_atualizarlote.sql
+++ 2022/03/INC0128889_atualizarlote.sql
@@ -1,8 +1,4 @@
 BEGIN
-
-	update craphpt hpt set dtmvtolt = to_date('13/02/2022')   WHERE hpt.cdcooper = 1  AND hpt.nrdconta = 830968 and nrremret = 254;
+    update craphpt hpt set dtmvtolt = to_date('13/02/2022', 'dd/mm/yyyy')   WHERE hpt.cdcooper = 1  AND hpt.nrdconta = 830968 and nrremret = 254;
     COMMIT;
-EXCEPTION
-    WHEN OTHERS THEN
-      ROLLBACK;
 END; 
