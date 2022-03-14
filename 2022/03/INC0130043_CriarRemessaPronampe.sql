788d6c4ec38409847845debf15e2d84aa0088f56
Rodrigo Jose Heiden <rodrigo.heiden@ailos.coop.br>
2022-03-14 14:45:00 -0300

    INC0130043 - Criar Remessa Pronampe

---
+++ 2022/03/INC0130043_CriarRemessaPronampe.sql
@@ -0,0 +1,18 @@
+BEGIN
+  INSERT INTO credito.tbcred_pronampe_remessa
+    (nrremessa
+    ,dtremessa
+    ,dhgeracao
+    ,cdsituacao)
+  VALUES
+    ((SELECT MAX(rem.nrremessa) + 1 nrremessa
+       FROM credito.tbcred_pronampe_remessa rem)
+    ,trunc(SYSDATE)
+    ,SYSDATE
+    ,1);
+  COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    raise_application_error(-20500, SQLERRM);
+END;
