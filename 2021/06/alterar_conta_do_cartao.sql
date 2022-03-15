b9ae5ad0b2d38cd92f3d3e76642788512c8c4db8
Djonata Wehmuth <djonata.wehmuth@ailos.coop.br>
2022-03-15 15:19:03 -0300

    Update alterar_conta_do_cartao.sql

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -7,9 +7,9 @@ DECLARE
 
 BEGIN
 
-  vr_cooperativa := 2;
-  vr_conta := 14010;
-  vr_cartao := 5158940000000188;
+  vr_cooperativa := 1;
+  vr_conta := 2581639;
+  vr_cartao := 5588190184171591;
 
   FOR cartao IN (select distinct a.nrcpftit
                   from crawcrd a
