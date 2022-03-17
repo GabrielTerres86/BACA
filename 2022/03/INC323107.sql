613538ca0a00a0dc8eb8edc93c9e9b2303fe7c49
Jorge Cleber de Oliveira Patrício <t0034376@cecred.coop.br>
2022-03-17 16:41:03 -0300

    Unificação dos scripts com o objetivo de resolver o mesmo problema

--- 2022/03/INC323107.sql
+++ 2022/03/INC323107.sql
@@ -8,11 +8,26 @@ BEGIN
        
   EXCEPTION 
        WHEN OTHERS THEN
-          raise_application_error(-20001,'Ocorreu um erro ao atualizar o registro da tabela CRAPSLI. Código do erro:'||SQLERRM);   
+          raise_application_error(-20000,'Ocorreu um erro ao atualizar o registro da tabela CRAPSLI. Código do erro:'||SQLERRM);   
   END;
   
-  COMMIT;          
-
+  BEGIN
+      UPDATE crapsli 
+         SET dtrefere = to_date('31/03/2022','dd/mm/yyyy') 
+       WHERE nrdconta = 256021 and cdcooper = 9;    
+      
+  EXCEPTION 
+      WHEN OTHERS THEN
+         RAISE_APPLICATION_ERROR(-20001,'Ocorreu um erro ao atualizar o registro. Código do erro:'||SQLERRM);
+  END;
+  
+  COMMIT;
+  
 END;
+  
+  
+  
 
 
+
+  
