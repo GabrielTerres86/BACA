81065b40b8a9539e0a8b8473a343adbca87b1d65
Alberto J G Neto <mouts-alberto.neto@ailos.coop.br>
2022-03-17 17:14:55 -0300

    Altera registro ocorrência correcao

--- 2022/03/INC0128889-319866-atualiza-ocorrencia-arq.sql
+++ 2022/03/INC0128889-319866-atualiza-ocorrencia-arq.sql
@@ -7,7 +7,9 @@ BEGIN
           and trunc(dtmvtopg) = '10/01/2022' 
           and dscodbar = '00196886100002319900000003280036000001311717'
           and nrremret = 253;
+		  commit;
 END;
+
 
 BEGIN
        update crapdpt 
@@ -17,4 +19,5 @@ BEGIN
           and trunc(dtmvtopg) = '14/01/2022' 
           and dscodbar = '00197886500013050000000001606063000000003117'
           and nrremret = 253;
-ENG;
+		  commit;
+END;
