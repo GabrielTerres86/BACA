801952cf536badb7f883bb3f6ad281e3e07e46bd
Jose Eduardo Gracik ( Mouts ) <jose.gracik@mouts.info>
2022-03-18 16:35:40 +0000

    Ajuste na tela TAB057

---
+++ 2022/03/baca-RFB18.sql
@@ -0,0 +1,6 @@
+﻿begin
+update crapaca set crapaca.lstparam = 'pr_dtiniper,pr_dtfimper,pr_cdempres,pr_status1perna,pr_status2perna,pr_enviadoReproces,pr_tlcooper,pr_nrdconta,pr_tlagenci,pr_nmarquivo,pr_textopesquisa,pr_orderby,pr_nriniseq,pr_nrregist'
+where crapaca.nmpackag like '%TAB057%' and crapaca.nrseqaca = 4956
+commit;
+
+end;
