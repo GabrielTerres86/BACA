89d37c1803fb389bfa4bd8e1eca9e77de699481e
Alberto J G Neto <mouts-alberto.neto@ailos.coop.br>
2022-03-18 17:44:44 -0300

    Alteração do tipo de movto

--- 2022/03/INC0128889-319866-atualiza-ocorrencia-mvt.sql
+++ 2022/03/INC0128889-319866-atualiza-ocorrencia-mvt.sql
@@ -1,13 +1,11 @@
  begin 
        update crapdpt 
-          set cdtipmvt = 7 
-          select nrseqaut from crapdpt              
+          set cdtipmvt = 7                        
         where cdcooper = 1 
           and nrdconta = 830968          
           and trunc(dtmvtopg) = To_Date('10/01/2022','DD/MM/YYYY')
           and dscodbar = '00196886100002319900000003280036000001311717'
-          and nrremret = 253;
-          
+          and nrremret = 253;          
           
        update crapdpt 
           set cdtipmvt = 7                    
