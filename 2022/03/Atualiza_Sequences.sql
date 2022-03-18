6391fa329e18dff00198c3ee3a66edfa2e7b8186
Jose Jair Dill Junior (Mouts) <t0032120@ailos.coop.br>
2022-03-18 16:28:34 -0300

    Ajustes

--- 2022/03/Atualiza_Sequences.sql
+++ 2022/03/Atualiza_Sequences.sql
@@ -1,38 +1,44 @@
-declare
---
-vr_limite_tituloleg number(20);
-vr_limite_opleg number(20);
-vr_number number(20);
---
-begin
---
-select max(to_number("IdTituloLeg")), max(to_number("IdOpLeg")) into vr_limite_tituloleg, vr_limite_opleg
-from tbjdnpcdstleg_jd2lg_optit@jdnpcbisql
-where "CdLeg" = 'LEG'
-and "ISPBAdministrado" = 5463212;
---
-vr_limite_opleg := vr_limite_opleg + 10000;
---
-vr_number := 0;
---
-loop
---
-exit when vr_number > vr_limite_tituloleg;
---
-select seqcob_idtitleg.NEXTVAL into vr_number
-from dual;
---
-end loop;
---
-vr_number := 0;
---
-loop
---
-exit when vr_number > vr_limite_opleg;
---
-select seqcob_idopeleg.NEXTVAL into vr_number
-from dual;
---
-end loop;
---
-end;
+DECLARE
+
+  vr_limite_tituloleg NUMBER(20);
+  vr_limite_opleg     NUMBER(20);
+  vr_number           NUMBER(20);
+
+BEGIN
+
+  SELECT MAX(to_number("IdTituloLeg"))
+         ,MAX(to_number("IdOpLeg"))
+    INTO vr_limite_tituloleg
+        ,vr_limite_opleg
+    FROM tbjdnpcdstleg_jd2lg_optit@jdnpcbisql
+   WHERE "CdLeg" = 'LEG'
+     AND "ISPBAdministrado" = 5463212;
+
+  vr_limite_opleg := vr_limite_opleg + 10000;
+
+  vr_number := 0;
+
+  LOOP
+  
+    EXIT WHEN vr_number > vr_limite_tituloleg;
+  
+    SELECT seqcob_idtitleg.NEXTVAL
+      INTO vr_number
+      FROM dual;
+  
+  END LOOP;
+
+  vr_number := 0;
+
+  LOOP
+  
+    EXIT WHEN vr_number > vr_limite_opleg;
+  
+    SELECT seqcob_idopeleg.NEXTVAL
+      INTO vr_number
+      FROM dual;
+  
+  END LOOP;
+
+  COMMIT;
+END;
