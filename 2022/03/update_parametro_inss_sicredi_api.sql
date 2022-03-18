e386998586cf40af550b8c85ddde77ca8e47268f
CECRED\f0033505 <filipe.cardozo@ailos.coop.br>
2022-03-18 10:32:50 -0300

    Update parâmetro de ligar/desligar chamada api inss sicredi

--- 2022/03/update_parametro_inss_sicredi_api.sql
+++ 2022/03/update_parametro_inss_sicredi_api.sql
@@ -1,4 +1,4 @@
 BEGIN
-    UPDATE crapprm  a SET a.dsvlrprm = 'N' WHERE a.cdacesso = 'FLG_UTL_API_PV_SICREDI';
+    UPDATE crapprm  a SET a.dsvlrprm = 'S' WHERE a.cdacesso = 'FLG_UTL_API_PV_SICREDI';
     COMMIT;
 END;
