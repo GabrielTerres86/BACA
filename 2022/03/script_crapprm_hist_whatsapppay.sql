a22339c043f28783d98bbdd48ae7f143857f1c17
Alcemir Junior <alcemir.junior@mouts.info>
2022-03-18 10:27:31 -0300

    Ajuste
    COMMIT

--- 2022/03/script_crapprm_hist_whatsapppay.sql
+++ 2022/03/script_crapprm_hist_whatsapppay.sql
@@ -2,5 +2,7 @@ BEGIN
 
   insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
   values ('CRED', 0, 'HIST_WHATSAPP_PAY', 'Historicos de operacoes referente ao whatsapp pay', ';3901;3902;3903;3904;');
+  
+  commit;
 
 END;
