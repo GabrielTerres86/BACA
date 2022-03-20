6046d866d9c75c9b2a4113579d80e04d25f0e2c2
Alberto J G Neto <mouts-alberto.neto@ailos.coop.br>
2022-03-19 21:00:19 -0300

    Altera ocorrencia crapdpt

--- 2022/03/INC0128889-319866-atualiza-ocorrencia-mvt.sql
+++ 2022/03/INC0128889-319866-atualiza-ocorrencia-mvt.sql
@@ -5,7 +5,9 @@
           and nrdconta = 830968          
           and trunc(dtmvtopg) = To_Date('10/01/2022','DD/MM/YYYY')
           and dscodbar = '00196886100002319900000003280036000001311717'
-          and nrremret = 253; 		  
+          and nrremret = 253;
+
+ 		  
           
        update crapdpt 
           set cdtipmvt = 7                    
