93985541551b35b8915b1f39980eef6ee3c18e86
Rodrigo Jose Heiden <rodrigo.heiden@ailos.coop.br>
2022-03-22 14:34:03 -0300

    PRJ0023813 Criar Cenário Não Honrado

--- 2022/03/PRJ0023813_CriaCenarioNaoHonrado.sql
+++ 2022/03/PRJ0023813_CriaCenarioNaoHonrado.sql
@@ -1,17 +1,7 @@
 BEGIN
   UPDATE crapris a
      SET a.qtdiaatr = 321
-   WHERE a.cdcooper = 1
-     AND a.nrdconta = 9813047
-     AND a.nrctremp = 2983767
-     AND a.dtrefere = to_date('14/03/2022', 'DD/MM/RRRR');
-
-  UPDATE crapris a
-     SET a.qtdiaatr = 321
-   WHERE a.cdcooper = 14
-     AND a.nrdconta = 206741
-     AND a.nrctremp = 19664
-     AND a.dtrefere = to_date('03/02/2022', 'DD/MM/RRRR');
+   WHERE a.progress_recid IN (2131805806, 2123737044);
 
   COMMIT;
 EXCEPTION
