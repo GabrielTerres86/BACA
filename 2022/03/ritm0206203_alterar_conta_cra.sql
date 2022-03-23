65cacf991d61581949953cef054ccee00d1458a8
Rodrigo Lima <capgemini-rodrigo.erasmo@ailos.coop.br>
2022-03-23 09:03:00 -0300

    Colocando tratamento de exception

--- 2022/03/ritm0206203_alterar_conta_cra.sql
+++ 2022/03/ritm0206203_alterar_conta_cra.sql
@@ -1,11 +1,13 @@
 BEGIN
-
       UPDATE crapprm 
          SET crapprm.dsvlrprm = '237;0099;3027651;INSTITUTO DE PROTESTOS DE TÍTULOS;03.656.766/0003-89;CC;2'
        WHERE crapprm.nmsistem = 'CRED'
          AND crapprm.cdcooper = 3
          AND crapprm.cdacesso = 'CONTA_IEPTB'
          AND crapprm.dsvlrprm = '237;0099;002836;INSTITUTO DE PROTESTOS DE TÍTULOS;03.656.766/0003-89;CC;2';
+  commit;
 
-	commit;
+EXCEPTION  WHEN OTHERS THEN    SISTEMA.excecaoInterna(pr_compleme => 'RITM0206203');
+ROLLBACK;
+
 END;
