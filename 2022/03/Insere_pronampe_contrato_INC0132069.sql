a1e7011dee70aafef2e223a02b6aa9af556793fc
Anderson Valezi Gonçalves <mout-anderson.goncalves@ailos.coop.br>
2022-03-24 10:40:47 -0300

    Inclusão da coluna de data na inserção da remessa

--- 2022/03/Insere_pronampe_contrato_INC0132069.sql
+++ 2022/03/Insere_pronampe_contrato_INC0132069.sql
@@ -3,21 +3,25 @@ BEGIN
     INTO CREDITO.TBCRED_PRONAMPE_CONTRATO(CDCOOPER
                                          ,NRDCONTA
                                          ,NRCONTRATO
-                                         ,IDCONTRATO)
+                                         ,IDCONTRATO
+                                         ,DTVLRCREDIT)
                                    VALUES(1
                                          ,7066600
                                          ,2900346
-                                         ,3075749);
+                                         ,3075749
+                                         ,TO_DATE('03/09/2020','DD/MM/RRRR'));
                                          
   INSERT
     INTO CREDITO.TBCRED_PRONAMPE_CONTRATO(CDCOOPER
                                          ,NRDCONTA
                                          ,NRCONTRATO
-                                         ,IDCONTRATO)
+                                         ,IDCONTRATO
+                                         ,DTVLRCREDIT)
                                    VALUES(1
                                          ,8844933
                                          ,2901576
-                                         ,3077429);   
+                                         ,3077429
+                                         ,TO_DATE('02/09/2020','DD/MM/RRRR'));   
 COMMIT;
 EXCEPTION
   WHEN OTHERS THEN
