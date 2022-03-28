fd0f1c1ad77121c2b522b2523d5710ee748791fb
Giovane Diego Gibbert <giovane.gibbert@ailos.coop.br>
2022-03-28 13:35:08 -0300

    Script para ajustar contas em prejuizo - INC0125737

---
+++ 2022/03/inc0125737.sql
@@ -0,0 +1,291 @@
+DECLARE
+  CURSOR cr_his(pr_cdcooper IN crapcop.cdcooper%TYPE
+               ,pr_cdhistor IN craphis.cdhistor%TYPE) IS
+    SELECT t.cdhistor
+          ,t.dshistor
+          ,t.indebcre
+      FROM craphis t
+     WHERE t.cdcooper = pr_cdcooper
+       AND t.cdhistor = pr_cdhistor;
+  rw_his cr_his%ROWTYPE;
+
+  CURSOR cr_sld_prj(pr_cdcooper IN crapcop.cdcooper%TYPE
+                   ,pr_nrdconta IN crapass.nrdconta%TYPE) IS
+    SELECT t.vlsdprej
+          ,t.idprejuizo
+      FROM tbcc_prejuizo t
+     WHERE t.cdcooper = pr_cdcooper
+       AND t.nrdconta = pr_nrdconta;
+  rw_sld_prj cr_sld_prj%ROWTYPE;
+
+  rw_crapdat btch0001.cr_crapdat%ROWTYPE;
+
+  vr_cdcritic INTEGER := 0;
+  vr_dscritic VARCHAR2(4000);
+  vr_des_erro VARCHAR2(1000);
+  vr_tab_erro GENE0001.typ_tab_erro;
+  vr_exc_erro EXCEPTION;
+  vr_upsld_prej CHAR(1);
+  vr_tipoajus   CHAR(1);
+  vr_indebcre   craphis.indebcre%TYPE;
+  vr_cdagenci   crapass.cdagenci%TYPE;
+  vr_cdcooper   crapepr.cdcooper%TYPE;
+  vr_nrdconta   crapepr.nrdconta%TYPE;
+  vr_nrctremp   crapepr.nrctremp%TYPE;
+  vr_vllanmto   craplem.vllanmto%TYPE;
+  vr_cdhistor   craplem.cdhistor%TYPE;
+  vr_idprejuizo tbcc_prejuizo.idprejuizo%TYPE;
+
+  vr_conta     GENE0002.typ_split;
+  vr_reg_conta GENE0002.typ_split;
+
+  PROCEDURE prc_atlz_prejuizo(prm_cdcooper IN craplcm.cdcooper%TYPE
+                             ,prm_nrdconta IN craplcm.nrdconta%TYPE
+                             ,prm_vllanmto IN craplcm.vllanmto%TYPE
+                             ,prm_cdhistor IN craplcm.cdhistor%TYPE
+                             ,prm_tipoajus IN VARCHAR2) IS
+  
+    vr_idprejuizo tbcc_prejuizo.idprejuizo%TYPE;
+    vr_tipoacao   BOOLEAN;
+    vr_vlsdprej   tbcc_prejuizo.vlsdprej%TYPE;
+    vr_found      BOOLEAN;
+  
+  BEGIN
+  
+    OPEN cr_sld_prj(pr_cdcooper => prm_cdcooper,
+                    pr_nrdconta => prm_nrdconta);
+    FETCH cr_sld_prj
+      INTO rw_sld_prj;
+    vr_found    := cr_sld_prj%FOUND;
+    vr_vlsdprej := rw_sld_prj.vlsdprej;
+    CLOSE cr_sld_prj;
+  
+    IF NOT vr_found THEN
+      vr_dscritic := 'Erro ao buscar Saldo Prejuizo Cop/Cta (' ||
+                     prm_cdcooper || '/' || prm_nrdconta || ')';
+      RAISE vr_exc_erro;
+    END IF;
+    vr_found := NULL;
+  
+    IF prm_tipoajus IS NULL OR prm_tipoajus NOT IN ('E', 'I') THEN
+      vr_dscritic := 'Erro: Tipo de Ajuste invalido! (' || prm_tipoajus || ')';
+      RAISE vr_exc_erro;
+    END IF;
+  
+    OPEN cr_his(pr_cdcooper => prm_cdcooper, pr_cdhistor => prm_cdhistor);
+    FETCH cr_his
+      INTO rw_his;
+    vr_found    := cr_his%FOUND;
+    vr_indebcre := rw_his.indebcre;
+    CLOSE cr_his;
+  
+    IF NOT vr_found THEN
+      vr_dscritic := 'Historico não encontrato Cop/Hist (' || prm_cdcooper || '/' ||
+                     prm_cdhistor || ')';
+      RAISE vr_exc_erro;
+    END IF;
+  
+    IF prm_tipoajus = 'E' THEN
+      IF vr_indebcre = 'C' THEN
+        vr_tipoacao := TRUE;
+      ELSE
+        vr_tipoacao := FALSE;
+      END IF;
+    ELSE
+      IF vr_indebcre = 'C' THEN
+        vr_tipoacao := FALSE;
+      ELSE
+        vr_tipoacao := TRUE;
+      END IF;
+    END IF;
+  
+    IF vr_tipoacao THEN
+      BEGIN
+        UPDATE tbcc_prejuizo a
+           SET a.vlsdprej = Nvl(vlsdprej, 0) - Nvl(prm_vllanmto, 0)
+         WHERE a.cdcooper = prm_cdcooper
+           AND a.nrdconta = prm_nrdconta;
+      EXCEPTION
+        WHEN OTHERS THEN
+          vr_dscritic := 'Erro ao Atualizar Valor Saldo Prejuízo. Erro: ' ||
+                         SubStr(SQLERRM, 1, 255);
+          RAISE vr_exc_erro;
+      END;
+    ELSE
+      BEGIN
+        UPDATE tbcc_prejuizo a
+           SET a.vlsdprej = Nvl(vlsdprej, 0) + Nvl(prm_vllanmto, 0)
+         WHERE a.cdcooper = prm_cdcooper
+           AND a.nrdconta = prm_nrdconta;
+      EXCEPTION
+        WHEN OTHERS THEN
+          vr_dscritic := 'Erro ao Atualizar Valor Saldo Prejuízo. Erro: ' ||
+                         SubStr(SQLERRM, 1, 255);
+          RAISE vr_exc_erro;
+      END;
+    END IF;
+  
+  END prc_atlz_prejuizo;
+
+BEGIN
+  /*cooper
+  conta
+  historico
+  valor
+  atualiza tbcc_prejuizo (Y/N) yes, no
+  tipo de ajuste (E/I) (exclui ou inclui) --se o atualiza for Y, tem que passar o tipo de ajuste i aumenta o valor
+  */
+  vr_conta := GENE0002.fn_quebra_string(pr_string  => '16;433896;2721;4,95;N;I|'||
+                                                      '1;10376836;2721;10,00;N;I|'||
+                                                      '11;441198;2721;2,74;N;I|'||
+                                                      '1;11244020;2721;45,69;N;I|'||
+                                                      '1;11268433;2408;235,60;Y;I|'||
+                                                      '1;10376836;2721;30,00;N;I|'||
+                                                      '1;10376836;2408;20,00;Y;I|'||
+                                                      '1;10134395;2721;6,87;N;I|'||
+                                                      '1;8070601;2721;1,47;N;I|'||
+                                                      '1;8380783;2721;6,54;N;I|'||
+                                                      '1;10370510;2721;2,42;N;I|'||
+                                                      '11;460320;2408;977,81;Y;I|'||
+                                                      '16;297895;2701;13,76;N;I|'||
+                                                      '11;196363;2701;17,50;N;I|'||
+                                                      '13;11152176;2701;500,00;N;I|'
+                                                      ,
+                                        pr_delimit => '|');
+  IF vr_conta.COUNT > 0 THEN
+  
+    FOR vr_idx_lst IN 1 .. vr_conta.COUNT - 1 LOOP
+      vr_reg_conta := GENE0002.fn_quebra_string(pr_string  => vr_conta(vr_idx_lst),
+                                                pr_delimit => ';');
+    
+      vr_cdcooper   := vr_reg_conta(1);
+      vr_nrdconta   := vr_reg_conta(2);
+      vr_cdhistor   := vr_reg_conta(3);
+      vr_vllanmto   := to_number(vr_reg_conta(4));
+      vr_upsld_prej := vr_reg_conta(5);
+      vr_tipoajus   := vr_reg_conta(6);
+    
+      OPEN btch0001.cr_crapdat(pr_cdcooper => vr_cdcooper);
+      FETCH btch0001.cr_crapdat
+        INTO rw_crapdat;
+      CLOSE btch0001.cr_crapdat;
+    
+      IF vr_cdhistor IN (2408, 2721, 2722) THEN
+      
+        BEGIN
+          SELECT a.idprejuizo
+            INTO vr_idprejuizo
+            FROM tbcc_prejuizo a
+           WHERE a.cdcooper = vr_cdcooper
+             AND a.nrdconta = vr_nrdconta;
+        EXCEPTION
+          WHEN OTHERS THEN
+            vr_dscritic := 'Erro ao Buscar chave prejuizo. Erro: ' ||
+                           SubStr(SQLERRM, 1, 255);
+            RAISE vr_exc_erro;
+        END;
+      
+        prej0003.pc_gera_lcto_extrato_prj(pr_cdcooper   => vr_cdcooper,
+                                          pr_nrdconta   => vr_nrdconta,
+                                          pr_dtmvtolt   => rw_crapdat.dtmvtolt,
+                                          pr_cdhistor   => vr_cdhistor,
+                                          pr_idprejuizo => vr_idprejuizo,
+                                          pr_vllanmto   => vr_vllanmto,
+                                          pr_dthrtran   => SYSDATE,
+                                          pr_cdcritic   => vr_cdcritic,
+                                          pr_dscritic   => vr_dscritic);
+        IF Nvl(vr_cdcritic, 0) > 0 OR TRIM(vr_dscritic) IS NOT NULL THEN
+          RAISE vr_exc_erro;
+        END IF;
+      
+        IF vr_upsld_prej = 'Y' THEN
+          prc_atlz_prejuizo(prm_cdcooper => vr_cdcooper,
+                            prm_nrdconta => vr_nrdconta,
+                            prm_vllanmto => vr_vllanmto,
+                            prm_cdhistor => vr_cdhistor,
+                            prm_tipoajus => vr_tipoajus);
+        END IF;
+      
+      ELSIF vr_cdhistor IN (2738) THEN
+      
+        prej0003.pc_gera_cred_cta_prj(pr_cdcooper => vr_cdcooper,
+                                      pr_nrdconta => vr_nrdconta,
+                                      pr_vlrlanc  => vr_vllanmto,
+                                      pr_dtmvtolt => rw_crapdat.dtmvtolt,
+                                      pr_dsoperac => 'Ajuste Contabil - ' ||
+                                                     To_Char(SYSDATE,
+                                                             'dd/mm/yyyy hh24:mi:ss'),
+                                      pr_cdcritic => vr_cdcritic,
+                                      pr_dscritic => vr_dscritic);
+      
+        IF Nvl(vr_cdcritic, 0) > 0 OR TRIM(vr_dscritic) IS NOT NULL THEN
+          RAISE vr_exc_erro;
+        END IF;
+      
+      ELSIF vr_cdhistor IN (2739) THEN
+      
+        prej0003.pc_gera_debt_cta_prj(pr_cdcooper => vr_cdcooper,
+                                      pr_nrdconta => vr_nrdconta,
+                                      pr_vlrlanc  => vr_vllanmto,
+                                      pr_dtmvtolt => rw_crapdat.dtmvtolt,
+                                      pr_dsoperac => 'Ajuste Contabil - ' ||
+                                                     To_Char(SYSDATE,
+                                                             'dd/mm/yyyy hh24:mi:ss'),
+                                      pr_cdcritic => vr_cdcritic,
+                                      pr_dscritic => vr_dscritic);
+      
+        IF Nvl(vr_cdcritic, 0) > 0 OR TRIM(vr_dscritic) IS NOT NULL THEN
+          RAISE vr_exc_erro;
+        END IF;
+      
+      ELSE
+      
+        IF vr_cdhistor IN (2720) THEN
+        
+          EMPR0001.pc_cria_lancamento_cc(pr_cdcooper => vr_cdcooper,
+                                         pr_dtmvtolt => rw_crapdat.dtmvtolt,
+                                         pr_cdagenci => 0,
+                                         pr_cdbccxlt => 100,
+                                         pr_cdoperad => '1',
+                                         pr_cdpactra => 0,
+                                         pr_nrdolote => 650001,
+                                         pr_nrdconta => vr_nrdconta,
+                                         pr_cdhistor => vr_cdhistor,
+                                         pr_vllanmto => vr_vllanmto,
+                                         pr_nrparepr => 0,
+                                         pr_nrctremp => 0,
+                                         pr_des_reto => vr_des_erro,
+                                         pr_tab_erro => vr_tab_erro);
+          IF vr_des_erro <> 'OK' THEN
+            IF vr_tab_erro.COUNT() > 0 THEN
+              vr_cdcritic := vr_tab_erro(vr_tab_erro.FIRST).cdcritic;
+              vr_dscritic := vr_tab_erro(vr_tab_erro.FIRST).dscritic;
+            ELSE
+              vr_cdcritic := 0;
+              vr_dscritic := 'Erro ao criar o lancamento na conta corrente.';
+            END IF;
+          
+            RAISE vr_exc_erro;
+          END IF;
+        
+        END IF;
+      
+      END IF;
+    
+    END LOOP;
+  END IF;
+
+  COMMIT;
+
+  dbms_output.put_line(' ');
+  dbms_output.put_line('Script finalizado com Sucesso em ' ||
+                       To_Char(SYSDATE, 'dd/mm/yyyy hh24:mi:ss'));
+
+EXCEPTION
+  WHEN vr_exc_erro THEN
+    ROLLBACK;
+    raise_application_error(-20111, vr_dscritic);
+  WHEN OTHERS THEN
+    ROLLBACK;
+    raise_application_error(-20111, SQLERRM);
+END;
