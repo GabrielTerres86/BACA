99929b144034b8cc9de55c3943bd7bcadb325359
Ilo <amcom-ilo.martins@ailos.coop.br>
2022-04-05 10:27:15 -0300

    Reenvio de contrato civia

---
+++ 2022/04/task331602.sql
@@ -0,0 +1,146 @@
+declare
+
+conta_661686_191756 clob := '<?xml version="1.0" encoding="UTF-8"?><Root><convenioCredito>
+                        <cooperativa>
+                          <codigo>13</codigo>
+                        </cooperativa>
+                        <numeroContrato>5</numeroContrato>
+                      </convenioCredito>
+                      <configuracaoCredito>
+                        <diasCarencia>39</diasCarencia>
+                        <financiaIOF>true</financiaIOF>
+                        <financiaTarifa>true</financiaTarifa>
+                      </configuracaoCredito>
+                      <propostaContratoCredito>
+                        <CETPercentAoAno>33.75</CETPercentAoAno>
+                        <dataPrimeiraParcela>2022-05-10</dataPrimeiraParcela>
+                        <produto> 
+                          <codigo>161</codigo>
+                        </produto>
+                        <quantidadeParcelas>36</quantidadeParcelas>
+                        <taxaJurosRemuneratorios>2.29</taxaJurosRemuneratorios>
+                        <taxaJurosRemuneratoriosAnual>31.22</taxaJurosRemuneratoriosAnual>
+                        <tipoLiberacao>
+                           <codigo>6</codigo></tipoLiberacao>
+                        <tipoLiquidacao>
+                          <codigo>4</codigo>
+                        </tipoLiquidacao> 
+                        <tributoIOFValor>273.04</tributoIOFValor>
+                        <valor>10448.52</valor><valorBase>10180.00</valorBase><dataProposta>2022-04-05T14:51:16</dataProposta>
+                        <emitente> 
+                          <dataNascOuConstituicao>1983-07-08T00:00:00</dataNascOuConstituicao>
+                          <identificadorReceitaFederal>5077426996</identificadorReceitaFederal>
+                          <razaoSocialOuNome>ANDERSON LUIZ RAUEN</razaoSocialOuNome>
+                          <nacionalidade>
+                            <codigo>42</codigo>
+                          </nacionalidade>
+                          <tipo> 
+                            <codigo>1</codigo>
+                          </tipo>
+                          <contaCorrente>
+                            <agencia>
+                              <codigo>112</codigo>
+                            </agencia>
+                            <banco>
+                              <codigo>85</codigo>
+                            </banco>
+                            <codigoConta>661686</codigoConta>
+                            <cooperativa>
+                              <codigo>13</codigo>
+                            </cooperativa>
+                          </contaCorrente>
+                          <numeroTitularidade>1</numeroTitularidade>
+                          <pessoaContatoEndereco>
+                            <CEP>89295000</CEP>
+                            <cidade>
+                              <descricao>RIO NEGRINHO</descricao>
+                            </cidade>
+                            <nomeBairro>INDUSTRIAL NORTE</nomeBairro>
+                            <numeroLogradouro>344</numeroLogradouro>
+                            <tipoEndereco>
+                              <codigo>13</codigo> 
+                            </tipoEndereco>
+                            <tipoENomeLogradouro>RUA JOAO AUGUSTIN</tipoENomeLogradouro>
+                            <UF>SC</UF>
+                          </pessoaContatoEndereco>
+                        </emitente>
+                        <identificadorProposta>191756</identificadorProposta>
+                        <statusProposta>
+                          <codigo>26</codigo>
+                        </statusProposta>
+                      </propostaContratoCredito>
+                      <pessoaDocumento>
+                        <identificador>4281183</identificador>
+                        <tipo>
+                          <sigla>CI</sigla>
+                        </tipo>
+                      </pessoaDocumento>
+                      <pessoaFisicaOcupacao>
+                        <naturezaOcupacao>
+                          <codigo>1</codigo>
+                        </naturezaOcupacao>
+                      </pessoaFisicaOcupacao>
+                      <pessoaFisicaDetalhamento>
+                        <estadoCivil>
+                          <codigo>4</codigo>  </estadoCivil>
+                        <sexo>
+                          <codigo>1</codigo>
+                        </sexo> 
+                      </pessoaFisicaDetalhamento>
+                      <pessoaFisicaRendimento>
+                        <identificadorRegistroFuncionario>484</identificadorRegistroFuncionario>
+                      </pessoaFisicaRendimento>
+                      <remuneracaoColaborador>
+                        <empregador>
+                          <identificadorReceitaFederal>647670000102</identificadorReceitaFederal>
+                          <razaoSocialOuNome>METALURGICA W3 SAT LTDA</razaoSocialOuNome>
+                        </empregador>
+                      </remuneracaoColaborador>
+                      <beneficio />
+                      <listaPessoasEndereco>
+                        <pessoaEndereco>
+                          <parametroConsignado>
+                            <tipoPessoaEndereco>EMPREGADOR</tipoPessoaEndereco>
+                          </parametroConsignado>
+                          <pessoaContatoEndereco>
+                            <CEP>89295000</CEP>
+                            <cidade>
+                              <descricao>RIO NEGRINHO</descricao>
+                            </cidade>
+                            <nomeBairro>INDL NORTE</nomeBairro>
+                            <numeroLogradouro>102</numeroLogradouro>
+                            <tipoENomeLogradouro>RUA CARLOS SCHREINER</tipoENomeLogradouro>
+                            <UF>SC</UF>
+                          </pessoaContatoEndereco>
+                        </pessoaEndereco>
+                      </listaPessoasEndereco>
+                      <parcela>
+                        <valor>432.37</valor>
+                      </parcela>
+                      <tarifa>
+                        <valor>0.0</valor>
+                      </tarifa>
+                      <inadimplencia>
+                        <despesasCartorarias>0.0</despesasCartorarias>
+                      </inadimplencia><posicao><produtoCategoria> <codigo>32</codigo></produtoCategoria><saldo>674.00</saldo></posicao><usuarioDominioCecred>
+                        <codigo></codigo>
+                      </usuarioDominioCecred>
+                      <parametroConsignado> 
+                        <codigoFisTabelaJuros>1</codigoFisTabelaJuros>
+                        <indicadorContaPrincipal>true</indicadorContaPrincipal> 
+                        <naturalidade>RIO NEGRINHO</naturalidade>
+                         <dataCalculoLegado>2022-04-01T00:00:00</dataCalculoLegado>
+                      </parametroConsignado> </Root>';
+
+begin
+
+insert into tbgen_evento_soa (CDCOOPER, NRDCONTA, NRCTRPRP, TPEVENTO, TPRODUTO_EVENTO, TPOPERACAO, DHOPERACAO, DSPROCESSAMENTO, DSSTATUS, DHEVENTO, DSERRO, NRTENTATIVAS, DSCONTEUDO_REQUISICAO)
+values (13, 661686, 191756, 'EFETIVA_PROPOSTA', 'CONSIGNADO', 'INSERT', sysdate, null, null, null, null, null, conta_661686_191756);
+
+commit;
+
+EXCEPTION
+  WHEN OTHERS THEN
+     raise_application_error(-20500, SQLERRM);
+    ROLLBACK;
+end;
