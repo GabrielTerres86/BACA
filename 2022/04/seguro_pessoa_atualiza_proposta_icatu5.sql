aaecb4558568aa04034606237f438a018b50a95d
Filipe Ribeiro <mouts-filipe.rego@ailos.coop.br>
2022-04-06 16:18:20 -0300

    Atualiza proposta ICATU

---
+++ 2022/04/seguro_pessoa_atualiza_proposta_icatu5.sql
@@ -0,0 +1,24 @@
+BEGIN
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444233);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444241);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444250);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444268);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444276);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444284);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444292);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444306);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444314);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444322);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444330);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444349);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444357);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444365);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444373);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444381);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444390);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444403);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444411);
+INSERT INTO cecred.tbseg_nrproposta (nrproposta) VALUES (770119444420);
+COMMIT;
+END;
+/
