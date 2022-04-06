25bdf739a721b1075a07b27a6fe7b732ee148340
Filipe Ribeiro <mouts-filipe.rego@ailos.coop.br>
2022-04-06 16:38:57 -0300

    Atualiza proposta ICATU

---
+++ 2022/04/seguro_pessoa_atualiza_proposta_icatu6.sql
@@ -0,0 +1,63 @@
+begin
+  update tbseg_prestamista p
+     set p.nrproposta = 770119444136
+   where p.cdcooper = 14
+     and p.nrproposta = 77011944413;
+
+  update crawseg p
+     set p.nrproposta = 770119444136
+   where p.cdcooper = 14
+     and p.nrproposta = 77011944413;
+
+  update tbseg_prestamista p
+     set p.nrproposta = 770119444144
+   where p.cdcooper = 14
+     and p.nrproposta = 77011944414;
+
+  update crawseg p
+     set p.nrproposta = 770119444144
+   where p.cdcooper = 14
+     and p.nrproposta = 77011944414;
+
+  update tbseg_prestamista p
+     set p.nrproposta = 770119444152
+   where p.cdcooper = 14
+     and p.nrproposta = 77011944415;
+
+  update crawseg p
+     set p.nrproposta = 770119444152
+   where p.cdcooper = 14
+     and p.nrproposta = 77011944415;
+
+  update tbseg_prestamista p
+     set p.nrproposta = 770119444160
+   where p.cdcooper = 14
+     and p.nrproposta = 77011944416;
+
+  update crawseg p
+     set p.nrproposta = 770119444160
+   where p.cdcooper = 14
+     and p.nrproposta = 77011944416;
+
+  update tbseg_prestamista p
+     set p.nrproposta = 770119444179
+   where p.cdcooper = 14
+     and p.nrproposta = 77011944417;
+
+  update crawseg p
+     set p.nrproposta = 770119444179
+   where p.cdcooper = 14
+     and p.nrproposta = 77011944417;
+
+  update tbseg_prestamista p
+     set p.nrproposta = 770119444187
+   where p.cdcooper = 14
+     and p.nrproposta = 77011944418;
+
+  update crawseg p
+     set p.nrproposta = 770119444187
+   where p.cdcooper = 14
+     and p.nrproposta = 77011944418;
+commit;
+end;
+/
