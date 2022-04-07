5982e8840d55857d12ae9e92441808b2cd50e91d
Rodrigo Lima <capgemini-rodrigo.erasmo@ailos.coop.br>
2022-04-07 15:38:03 -0300

    Conforme solicitado, script para alterar a conta do CRA

---
+++ 2022/04/ritm0206203_alterar_conta_cra.sql
@@ -0,0 +1,12 @@
+BEGIN
+      UPDATE crapprm 
+         SET crapprm.dsvlrprm = '237;0099;002836;INSTITUTO DE PROTESTOS DE TÍTULOS;03.656.766/0003-89;CC;2'
+       WHERE crapprm.nmsistem = 'CRED'
+         AND crapprm.cdcooper = 3
+         AND crapprm.cdacesso = 'CONTA_IEPTB';
+  commit;
+
+EXCEPTION  WHEN OTHERS THEN    SISTEMA.excecaoInterna(pr_compleme => 'RITM0206203');
+ROLLBACK;
+
+END;
