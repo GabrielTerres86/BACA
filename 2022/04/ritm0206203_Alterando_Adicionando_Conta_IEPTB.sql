d9760e0eeadc768677d0a712c269f8a5d988cd0a
Rodrigo Lima <capgemini-rodrigo.erasmo@ailos.coop.br>
2022-04-08 13:57:15 -0300

    Alterando script para remover parametro e depois inserir os novos

--- 2022/04/ritm0206203_Alterando_Adicionando_Conta_IEPTB.sql
+++ 2022/04/ritm0206203_Alterando_Adicionando_Conta_IEPTB.sql
@@ -1,12 +1,14 @@
 BEGIN
-      UPDATE crapprm 
-         SET crapprm.cdacesso = 'CONTA_IEPTB_TARIFAS'
+      DELETE crapprm 
        WHERE crapprm.nmsistem = 'CRED'
          AND crapprm.cdcooper = 3
          AND crapprm.cdacesso = 'CONTA_IEPTB';
 		 
 	  INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
 	  VALUES ('CRED', 3, 'CONTA_IEPTB_CUSTAS', 'Dados da conta do IEPTB para fazer a TED', '237;0099;002836;INSTITUTO DE PROTESTOS DE TÍTULOS;03.656.766/0003-89;CC;2');
+
+      INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+	  VALUES ('CRED', 3, 'CONTA_IEPTB_TARIFAS', 'Dados da conta do IEPTB para fazer a TED', '237;0099;3027651;INSTITUTO DE PROTESTOS DE TÍTULOS;03.656.766/0003-89;CC;2');
 
     COMMIT;
 
