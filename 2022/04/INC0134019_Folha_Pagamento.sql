3c9332321ba93e658c33ba5cf07b451d8fad70c1
Renier Figueiredo Padilha <renier.padilha@ailos.coop.br>
2022-04-08 15:56:21 -0300

    Ajuste no status dos registros da Folha de Pagamento emp 580

---
+++ 2022/04/INC0134019_Folha_Pagamento.sql
@@ -0,0 +1,16 @@
+ begin 
+update crappfp set flsitcre = 1, 
+       dthorcre = '06/04/2022'
+where rowid = 'AAAS6XAJQAAAqL6ABk';
+
+update craplfp set idsitlct = 'C' 
+ WHERE ROWID IN ('AB+KJiAMsAAH/ngAAf'
+                ,'AB+KJiAMsAAH/ngAAg'
+                ,'AB+KJiAMsAAH/ngAAh'
+                ,'AB+KJiAMsAAH/ngAAi'
+                ,'AB+KJiAMsAAH/ngAAj'
+                ,'AB+KJiAMsAAH/ngAAk'
+                ,'AB+KJiAMsAAH/ngAAl');
+commit;
+end; 
+ 
