15ab6855f726d65f9a7b71b83ae2dbab9fbf628e
Lucas Ian Citadini <premiersoft-lucas.citadini@ailos.coop.br>
2022-04-14 12:12:27 -0300

    Ajuste na rotina para dropar e criar o job novamente

--- 2022/04/inc0134565_ajuste_job_ted.sql
+++
@@ -1,3 +0,0 @@
-BEGIN
-DBMS_SCHEDULER.set_attribute( name => '"CECRED"."JBBLQJ_TRANSFERENCIA"', attribute => 'repeat_interval', value => 'Freq=DAILY;ByDay=MON, TUE, WED, THU, FRI;ByHour=13;ByMinute=10;BySecond=0');
-END;
