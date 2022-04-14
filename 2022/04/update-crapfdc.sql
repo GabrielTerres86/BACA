0d7127c95ef6c71d5f01dbd0ceec7282e95b2040
Paulo Felipe Kowalsky <t0032603@cecred.coop.br>
2022-04-14 11:05:08 -0300

    script cancelamento-depositochequemobile

---
+++ 2022/04/update-crapfdc.sql
@@ -0,0 +1,11 @@
+begin
+  
+update cecred.crapfdc f 
+  set f.incheque = 8, f.nrpedido = null, f.dtretchq = trunc(sysdate), f.dtliqchq = trunc(sysdate), f.dtemschq = trunc(sysdate)
+where  f.cdcooper = 14
+  and  f.nrdconta in (47724,63142)
+  and  f.nrpedido = 123024
+  and  f.nrcheque in (21,22,23,24,25,26,27,28,29,30,91,92,93,94,95,96,97,98,99,100);
+  
+commit;
+end;
