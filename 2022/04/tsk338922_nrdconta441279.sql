3e71814c8f48fe5a29811f8f0bbfb57b754f9ee8
Caroline <premiersoft-caroline.castro@ailos.coop.br>
2022-04-18 11:47:22 -0300

    Ajuste de contrato com saldo zero onde ocorreu erro de integração.

---
+++ 2022/04/tsk338922_nrdconta441279.sql
@@ -0,0 +1,176 @@
+DECLARE
+
+  v_cd_coopr    tbgen_evento_soa.CDCOOPER%TYPE := 7;
+  v_nr_conta    tbgen_evento_soa.NRDCONTA%TYPE := 441279;
+  v_nr_contrato tbgen_evento_soa.NRCTRPRP%TYPE := 77796;
+
+  v_xml_envio_contrato CLOB := '<?xml version="1.0" encoding="UTF-8"?><Root><convenioCredito>
+                        <cooperativa>
+                          <codigo>7</codigo>
+                        </cooperativa>
+                        <numeroContrato>478</numeroContrato>
+                      </convenioCredito>
+                      <configuracaoCredito>
+                        <diasCarencia>34</diasCarencia>
+                        <financiaIOF>true</financiaIOF>
+                        <financiaTarifa>true</financiaTarifa>
+                      </configuracaoCredito>
+                      <propostaContratoCredito>
+                        <CETPercentAoAno>24.50</CETPercentAoAno>
+                        <dataPrimeiraParcela>2022-05-15</dataPrimeiraParcela>
+                        <produto> 
+                          <codigo>161</codigo>
+                        </produto>
+                        <quantidadeParcelas>20</quantidadeParcelas>
+                        <taxaJurosRemuneratorios>1.59</taxaJurosRemuneratorios>
+                        <taxaJurosRemuneratoriosAnual>20.84</taxaJurosRemuneratoriosAnual>
+                        <tipoLiberacao>
+                           <codigo>6</codigo></tipoLiberacao>
+                        <tipoLiquidacao>
+                          <codigo>4</codigo>
+                        </tipoLiquidacao> 
+                        <tributoIOFValor>127.79</tributoIOFValor>
+                        <valor>5127.79</valor><valorBase>5000.00</valorBase><dataProposta>2022-04-19T13:44:34</dataProposta>
+                        <emitente> 
+                          <dataNascOuConstituicao>2002-09-23T00:00:00</dataNascOuConstituicao>
+                          <identificadorReceitaFederal>12909583945</identificadorReceitaFederal>
+                          <razaoSocialOuNome>LARISSA RODRIGUES LOPES DA COS</razaoSocialOuNome>
+                          <nacionalidade>
+                            <codigo>42</codigo>
+                          </nacionalidade>
+                          <tipo> 
+                            <codigo>1</codigo>
+                          </tipo>
+                          <contaCorrente>
+                            <agencia>
+                              <codigo>106</codigo>
+                            </agencia>
+                            <banco>
+                              <codigo>85</codigo>
+                            </banco>
+                            <codigoConta>441279</codigoConta>
+                            <cooperativa>
+                              <codigo>7</codigo>
+                            </cooperativa>
+                          </contaCorrente>
+                          <numeroTitularidade>1</numeroTitularidade>
+                          <pessoaContatoEndereco>
+                            <CEP>88162246</CEP>
+                            <cidade>
+                              <descricao>BIGUACU</descricao>
+                            </cidade>
+                            <nomeBairro>JARDIM JANAINA</nomeBairro>
+                            <numeroLogradouro>188</numeroLogradouro>
+                            <tipoEndereco>
+                              <codigo>13</codigo> 
+                            </tipoEndereco>
+                            <tipoENomeLogradouro>RUA DOMINGOS ANTONIO</tipoENomeLogradouro>
+                            <UF>SC</UF>
+                          </pessoaContatoEndereco>
+                        </emitente>
+                        <identificadorProposta>77796</identificadorProposta>
+                        <statusProposta>
+                          <codigo>26</codigo>
+                        </statusProposta>
+                      </propostaContratoCredito>
+                      <pessoaDocumento>
+                        <identificador>6999374</identificador>
+                        <tipo>
+                          <sigla>CI</sigla>
+                        </tipo>
+                      </pessoaDocumento>
+                      <pessoaFisicaOcupacao>
+                        <naturezaOcupacao>
+                          <codigo>1</codigo>
+                        </naturezaOcupacao>
+                      </pessoaFisicaOcupacao>
+                      <pessoaFisicaDetalhamento>
+                        <estadoCivil>
+                          <codigo>4</codigo>  </estadoCivil>
+                        <sexo>
+                          <codigo>2</codigo>
+                        </sexo> 
+                      </pessoaFisicaDetalhamento>
+                      <pessoaFisicaRendimento>
+                        <identificadorRegistroFuncionario>0</identificadorRegistroFuncionario>
+                      </pessoaFisicaRendimento>
+                      <remuneracaoColaborador>
+                        <empregador>
+                          <identificadorReceitaFederal>82901000000127</identificadorReceitaFederal>
+                          <razaoSocialOuNome>INTELBRAS S A INDUSTRIA DE TELECOMU</razaoSocialOuNome>
+                        </empregador>
+                      </remuneracaoColaborador>
+                      <beneficio />
+                      <listaPessoasEndereco>
+                        <pessoaEndereco>
+                          <parametroConsignado>
+                            <tipoPessoaEndereco>EMPREGADOR</tipoPessoaEndereco>
+                          </parametroConsignado>
+                          <pessoaContatoEndereco>
+                            <CEP>88104800</CEP>
+                            <cidade>
+                              <descricao>SAO JOSE</descricao>
+                            </cidade>
+                            <nomeBairro>DISTRITO INDUST</nomeBairro>
+                            <numeroLogradouro>0</numeroLogradouro>
+                            <tipoENomeLogradouro>RODOVIA BR101</tipoENomeLogradouro>
+                            <UF>SC</UF>
+                          </pessoaContatoEndereco>
+                        </pessoaEndereco>
+                      </listaPessoasEndereco>
+                      <parcela>
+                        <valor>301.96</valor>
+                      </parcela>
+                      <tarifa>
+                        <valor>0.0</valor>
+                      </tarifa>
+                      <inadimplencia>
+                        <despesasCartorarias>0.0</despesasCartorarias>
+                      </inadimplencia><usuarioDominioCecred>
+                        <codigo></codigo>
+                      </usuarioDominioCecred>
+                      <parametroConsignado> 
+                        <codigoFisTabelaJuros>1</codigoFisTabelaJuros>
+                        <indicadorContaPrincipal>true</indicadorContaPrincipal> 
+                        <naturalidade>CAPINZAL</naturalidade>
+                         <dataCalculoLegado>2022-04-11T00:00:00</dataCalculoLegado>
+                      </parametroConsignado> </Root>';
+
+BEGIN
+
+  INSERT INTO tbgen_evento_soa
+    (CDCOOPER,
+     NRDCONTA,
+     NRCTRPRP,
+     TPEVENTO,
+     TPRODUTO_EVENTO,
+     TPOPERACAO,
+     DHOPERACAO,
+     DSPROCESSAMENTO,
+     DSSTATUS,
+     DHEVENTO,
+     DSERRO,
+     NRTENTATIVAS,
+     DSCONTEUDO_REQUISICAO)
+  VALUES
+    (v_cd_coopr,
+     v_nr_conta,
+     v_nr_contrato,
+     'EFETIVA_PROPOSTA',
+     'CONSIGNADO',
+     'INSERT',
+     SYSDATE,
+     NULL,
+     NULL,
+     NULL,
+     NULL,
+     NULL,
+     v_xml_envio_contrato);
+
+  COMMIT;
+
+EXCEPTION
+  WHEN OTHERS THEN 
+    raise_application_error(-20500, SQLERRM);
+    ROLLBACK;  
+END;
