3e09d19bb227b46fd11f8776ef698ed50a38b270
Jorge Cleber de Oliveira Patrício <amcom-jorge.patricio@ailos.coop.br>
2022-04-27 08:51:31 -0300

    Script para ajustar o saldo que caiu na conta de investimento

---
+++ 2022/04/INC342209.sql
@@ -0,0 +1,18 @@
+BEGIN
+  
+  BEGIN
+    UPDATE crapsli
+       SET dtrefere = to_date('30/04/2022','dd/mm/yyyy')
+     WHERE nrdconta = 732001
+       AND cdcooper = 16;
+  
+  EXCEPTION
+     WHEN OTHERS THEN 
+       ROLLBACK;
+       RAISE_APPLICATION_ERROR(-20001,'Ocorreu um problema ao atualizar o registro da tabela crapsli.'||SQLERRM);
+  END;     
+    
+  COMMIT;
+   
+END;
+
