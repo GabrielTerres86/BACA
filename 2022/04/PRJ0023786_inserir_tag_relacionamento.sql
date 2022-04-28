629b4236c5de9e83c5666f84fd96539ead99cb0b
Matheus Santos Sousa e Silva <premiersoft-matheus.silva@ailos.coop.br>
2022-04-28 11:42:16 -0300

    correção owner da tabela

--- 2022/04/PRJ0023786_inserir_tag_relacionamento.sql
+++ 2022/04/PRJ0023786_inserir_tag_relacionamento.sql
@@ -1,5 +1,5 @@
 begin
-insert into tbgen_notif_msg_origem (CDORIGEM_MENSAGEM, DSORIGEM_MENSAGEM, CDTIPO_MENSAGEM, HRINICIO_PUSH, HRFIM_PUSH,  
+insert into cecred.tbgen_notif_msg_origem (CDORIGEM_MENSAGEM, DSORIGEM_MENSAGEM, CDTIPO_MENSAGEM, HRINICIO_PUSH, HRFIM_PUSH,  
 HRTEMPO_VALIDADE_PUSH) values (16, 'Relacionamentos', 3, 0, 86399, 86400);
 commit;
 end;
