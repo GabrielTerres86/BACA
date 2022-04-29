731d3b1c32706eb0d915606f0e64039fefdc06ed
Caroline <premiersoft-caroline.castro@ailos.coop.br>
2022-04-29 13:41:27 -0300

    Ajuste de contas

--- 2022/04/tsk344005.sql
+++ 2022/04/tsk344005.sql
@@ -1,6 +1,6 @@
 begin
   delete craplem
-   where dtmvtolt = to_date('25/04/2022')
+   where dtmvtolt = to_date('25/04/2022','dd/mm/yyyy')
      and (cdcooper, nrdconta, nrctremp) in
          ((13, 386545, 51508), (13, 223913, 57290), (13, 392413, 129492));
    commit;
