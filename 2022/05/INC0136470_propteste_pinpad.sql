70442678bf528626c9fb6431d8487a9264abe3f0
Tatiane <tatiane.hansen@ailos.coop.br>
2022-05-02 14:43:43 -0300

    ajuste proposta cartao

---
+++ 2022/05/INC0136470_propteste_pinpad.sql
@@ -0,0 +1,29 @@
+BEGIN
+  update crawcrd set
+  insitcrd = 4,
+  nrcrcard = 5127070283056646
+  where cdcooper = 1
+  and nrdconta = 930
+  and nrctrcrd = 2010153;  
+  
+  update crapcrd set
+  nrcrcard = 5127070283056646
+  where cdcooper = 1
+  and nrdconta = 930
+  and nrctrcrd = 2010153; 
+  
+    update crawcrd set
+  insitcrd = 4,
+  nrcrcard = 5158940050030200
+  where cdcooper = 1
+  and nrdconta = 930
+  and nrctrcrd = 1279721;  
+  
+  update crapcrd set
+  nrcrcard = 5158940050030200
+  where cdcooper = 1
+  and nrdconta = 930
+  and nrctrcrd = 1279721; 
+
+  COMMIT;
+END;
