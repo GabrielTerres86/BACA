67bafd89769d869098c9d04381eff2c31e512b05
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2022-05-03 16:07:57 -0300

    parametro que define idade que não contnrata garantia adicional

--- 2022/04/seguro-pessoa-crapprm-regra-garantia-adicional.sql
+++ 2022/04/seguro-pessoa-crapprm-regra-garantia-adicional.sql
@@ -1,8 +1,7 @@
 BEGIN
   	
   INSERT INTO crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
-    VALUES('CRED', 0, 'IDADE_COOP_SEM_GARADI', 'Idade do cooperado que não deve contratar garantia adicional no seguro prestamista contributário', '66');
-	
+    VALUES('CRED', 0, 'IDADE_COOP_SEM_GARADI', 'Idade do cooperado que não deve contratar garantia adicional no seguro prestamista contributário', '66');	
   COMMIT;
 EXCEPTION WHEN OTHERS THEN
   ROLLBACK;
