dff7452e3eeb58c79e7cb6568a02fd3072189491
Jefferson Gubetti <jefferson.gubetti@ailos.coop.br>
2022-05-04 11:33:54 -0300

    Ajustado valores dos parâmetros de integração das baixas operacionais com a JDNPC

--- 2022/03/PRJ0023522_Scripts_Parametros_JOBS.sql
+++ 2022/03/PRJ0023522_Scripts_Parametros_JOBS.sql
@@ -1,42 +1,37 @@
---Validados
---Retorno
-insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
-values ('CRED', 0, 'QT_REG_JOBRETORNOBO', 'Quantidade de registros minimo para gerar Jobs de retorno de remessas de baixa operacional em paralelo', '200', NULL);
+BEGIN
 
-insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
-values ('CRED', 0, 'QT_JOBS_RETORNOBO', 'Quantidade máxima de Jobs executados em paralelo no retorno da baixa operacional', '5', NULL);
+  INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+  VALUES ('CRED', 0, 'QT_REG_JOB_RMSBO_AIMARO', 'Quantidade mínima de registros para gerar jobs na geração de remessa de baixa operacional no AIMARO', '1000', NULL);
+  INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+  VALUES ('CRED', 0, 'QT_JOB_PAR_RMSBO_AIMARO', 'Quantidade máxima de jobs executados em paralelo na geração de remessa de baixa operacional no AIMARO', '3', NULL);
+  INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+  VALUES ('CRED', 0, 'QT_COMMIT_RMSBO_AIMARO', 'Quantidade de registros para commitar após a geração da remessa da baixa operacional no AIMARO', '50', NULL);
 
-insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
-values ('CRED', 0, 'QT_COMMIT_RMSRETORNOBO', 'Quantidade de registro para commitar no processo de consultar o retorno remessa da baixa operacional', '25', NULL);
+  INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+  VALUES ('CRED', 0, 'QT_REG_JOB_RMSBO_AIM_CTG', 'Quantidade mínima de registros para gerar jobs na geração de remessa de baixa operacional em contingência no AIMARO', '1000', NULL);
+  INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+  VALUES ('CRED', 0, 'QT_JOB_PAR_RMSBO_AIM_CTG', 'Quantidade máxima de jobs executados em paralelo na geração de remessa de baixa operacional em contingência no AIMARO', '3', NULL);
+  INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+  VALUES ('CRED', 0, 'QT_COMMIT_RMSBO_AIM_CTG', 'Quantidade de registros para commitar após a geração da remessa da baixa operacional em contingência no AIMARO', '50', NULL);
 
---Remessa Aimaro
-insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
-values ('CRED', 0, 'QT_REG_JOB_RMSBO_AIMARO', 'Quantidade de registros minimo para gerar Jobs para geracao de remessas de baixas operacionais - AIMARO', '50', NULL);
+  INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+  VALUES ('CRED', 0, 'QT_REG_JOB_RMSBO_JDNPC', 'Quantidade mínima de registros para gerar jobs na geração de remessa de baixa operacional para a JDNPC', '300', NULL);
+  INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+  VALUES ('CRED', 0, 'QT_JOB_PAR_RMSBO_JDNCP', 'Quantidade máxima de Jobs executados em paralelo na baixa operacional para o processo de envio da remessa para a JDNPC', '5', NULL);
+  INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+  VALUES ('CRED', 0, 'QT_COMMIT_RMSBO_JDNCP', 'Quantidade de registros para commitar após a geração da remessa da baixa operacional para a JDNPC', '25', NULL);
 
-insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
-values ('CRED', 0, 'QT_JOB_PAR_RMSBO_AIMARO', 'Quantidade máxima de Jobs executados em paralelo na geracao de remessas de baixas operacionais - AIMARO', '5', NULL);
+  INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+  VALUES ('CRED', 0, 'QT_REG_JOB_RETORNOBO', 'Quantidade mínima de registros para gerar jobs de para busca do retorno da remessa de baixa operacional', '300', NULL);
+  INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+  VALUES ('CRED', 0, 'QT_JOB_PAR_RETORNOBO', 'Quantidade máxima de Jobs executados em paralelo para busca do retorno da baixa operacional', '5', NULL);
+  INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
+  VALUES ('CRED', 0, 'QT_COMMIT_RETORNOBO', 'Quantidade de registros para commitar no processo de consulta do retorno da remessa de baixa operacional', '25', NULL);
 
-insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
-values ('CRED', 0, 'QT_COMMIT_RMSBO_AIMARO', 'Quantidade de registro para commitar após o envio da remessa da baixa operacional', '5', NULL);
+  COMMIT;
 
---Remessa Aimaro Contingencia
-insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
-values ('CRED', 0, 'QT_REG_JOB_RMSBO_AIM_CTG', 'Quantidade de registros minimo para gerar Jobs para geracao de remessas de baixas operacionais em contingencia - AIMARO (montarJobRemessaBoAimaroCtg)', '50', NULL);
-
-insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
-values ('CRED', 0, 'QT_JOB_PAR_RMSBO_AIM_CTG', 'Quantidade máxima de Jobs executados em paralelo na geracao de remessas de baixas operacionais em contingencia - AIMARO (montarJobRemessaBoAimaroCtg)', '5', NULL);
-
-insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
-values ('CRED', 0, 'QT_COMMIT_RMSBO_AIM_CTG', 'Quantidade de registro para commitar após o envio da remessa da baixa operacional em contingencia - AIMARO (consumirJobRmsBoAimaroCtg)', '5', NULL);
-
---Remessa JDNPC
-insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
-values ('CRED', 0, 'QT_REG_JOB_RMSBO_JDNPC', 'Quantidade de registros minimo para gerar Jobs em paralelo na baixa operacional para a JDNPC', '200', NULL);
-
-insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM, PROGRESS_RECID)
-values ('CRED', 0, 'QT_JOB_PAR_RMSBO_JDNCP', 'Quantidade máxima de Jobs executados em paralelo na baixa operacional para o processo de envio da remessa para a JDNPC', '5', NULL);
-
---Adicionar nomes das procs nos comentarios
-
-COMMIT;
-
+EXCEPTION
+  WHEN OTHERS THEN
+    SISTEMA.excecaoInterna(pr_compleme => 'PRJ0023522');
+    ROLLBACK;
+END;
