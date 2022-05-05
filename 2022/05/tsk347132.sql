3f73bdcc17d011920ebfeb7a5d9b29e8e30497ed
Leonardo Nogueira da Silva <softtek-leonardo.silva@ailos.coop.br>
2022-05-05 11:08:02 -0300

    adição de um espaço no cifrão

--- 2022/05/tsk347132.sql
+++ 2022/05/tsk347132.sql
@@ -1,15 +1,15 @@
 DECLARE
 BEGIN
 UPDATE CECRED.TBGEN_NOTIF_MSG_CADASTRO
-set DSHTML_MENSAGEM = '#NomeCooperado, <br><br>O ajuste de limite Pix que você solicitou foi realizado com sucesso. <br><br>Novos limites: <br><br>DIURNO <br>Período: R$#LimiteDiurno <br>Transação: R$#PorTransacaoLimiteDiurno <br><br>NOTURNO <br>Período: R$#LimiteNoturno <br>Transação: R$#PorTransacaoLimiteNoturno <br><br>Para mais informações sobre Limites Pix, acesse a seção de dúvidas.'
+set DSHTML_MENSAGEM = '#NomeCooperado, <br><br>O ajuste de limite Pix que você solicitou foi realizado com sucesso. <br><br>Novos limites: <br><br>DIURNO <br>Período: R$ #LimiteDiurno <br>Transação: R$ #PorTransacaoLimiteDiurno <br><br>NOTURNO <br>Período: R$ #LimiteNoturno <br>Transação: R$ #PorTransacaoLimiteNoturno <br><br>Para mais informações sobre Limites Pix, acesse a seção de dúvidas.'
 WHERE CDMENSAGEM = 8547 AND CDORIGEM_MENSAGEM = 13;
 
 UPDATE CECRED.TBGEN_NOTIF_MSG_CADASTRO
-set DSHTML_MENSAGEM = '#NomeCooperado, <br><br>O ajuste de limite Pix Saque e Pix Troco que você solicitou foi realizado com sucesso. <br><br>Novos limites: <br><br>DIURNO <br>Período: R$#LimiteDiurno <br>Transação: R$#PorTransacaoLimiteDiurno <br><br>NOTURNO <br>Período: R$#LimiteNoturno <br>Transação: R$#PorTransacaoLimiteNoturno <br><br>Para mais informações sobre Limites Pix, acesse a seção de dúvidas.'
+set DSHTML_MENSAGEM = '#NomeCooperado, <br><br>O ajuste de limite Pix Saque e Pix Troco que você solicitou foi realizado com sucesso. <br><br>Novos limites: <br><br>DIURNO <br>Período: R$ #LimiteDiurno <br>Transação: R$ #PorTransacaoLimiteDiurno <br><br>NOTURNO <br>Período: R$ #LimiteNoturno <br>Transação: R$ #PorTransacaoLimiteNoturno <br><br>Para mais informações sobre Limites Pix, acesse a seção de dúvidas.'
 WHERE CDMENSAGEM = 8548 AND CDORIGEM_MENSAGEM = 13;
 
 UPDATE CECRED.TBGEN_NOTIF_MSG_CADASTRO
-set DSHTML_MENSAGEM = '#NomeCooperado, <br><br>O ajuste de limite Pix que você solicitou foi realizado com sucesso. <br><br>Dados da Conta: <br><br>Nome: #NomeContaCadastrada <br>Banco: #BancoContaCadastrada <br>Agência: #AgenciaContaCadastrada <br>Conta: #NumeroContaCadastrada <br>Novos limites: <br><br>DIURNO <br>Período: R$#LimiteDiurno <br>Transação: R$#PorTransacaoLimiteDiurno <br><br>NOTURNO <br>Período: R$#LimiteNoturno <br>Transação: R$#PorTransacaoLimiteNoturno <br><br>Para mais informações sobre Limites Pix, acesse a seção de dúvidas.'
+set DSHTML_MENSAGEM = '#NomeCooperado, <br><br>O ajuste de limite Pix que você solicitou foi realizado com sucesso. <br><br>Dados da Conta: <br><br>Nome: #NomeContaCadastrada <br>Banco: #BancoContaCadastrada <br>Agência: #AgenciaContaCadastrada <br>Conta: #NumeroContaCadastrada <br>Novos limites: <br><br>DIURNO <br>Período: R$ #LimiteDiurno <br>Transação: R$ #PorTransacaoLimiteDiurno <br><br>NOTURNO <br>Período: R$ #LimiteNoturno <br>Transação: R$ #PorTransacaoLimiteNoturno <br><br>Para mais informações sobre Limites Pix, acesse a seção de dúvidas.'
 WHERE CDMENSAGEM = 8549 AND CDORIGEM_MENSAGEM = 13;
 
 COMMIT;
