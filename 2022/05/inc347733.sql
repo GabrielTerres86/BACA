45133544cb77c02a462184ed6169b2cb448e4b9c
Jorge Cleber de Oliveira Patrício <t0034376@cecred.coop.br>
2022-05-06 15:23:11 -0300

    Adicionado o script para correção na data do registro da tabela crapsli para que seja possível visualizar o valor da conta de investimento

---
+++ 2022/05/inc347733.sql
@@ -0,0 +1,19 @@
+BEGIN
+  
+  UPDATE crapsli 
+     SET dtrefere = to_date('31/05/2022','dd/mm/yyyy') 
+   WHERE nrdconta = 191051 
+     AND cdcooper = 9
+     AND dtrefere = to_date('30/04/2022','dd/mm/yyyy');
+  
+  COMMIT;
+  
+  EXCEPTION 
+      WHEN OTHERS THEN
+         RAISE_APPLICATION_ERROR(-20000,'Ocorreu um erro ao atualizar o registro. Código do erro:'||SQLERRM);
+  
+END;
+
+
+
+
