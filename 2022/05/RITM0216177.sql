5554abdd140f9cbe65d9ef47a0aac482d8b0f85f
Ricardo de Souza (Supero) <t0032449@ailos.coop.br>
2022-05-10 09:57:05 -0300

    Alteração do parâ,metro de envio de e-mail de risco de fraude

--- 2022/05/RITM0216177.sql
+++ 2022/05/RITM0216177.sql
@@ -5,6 +5,13 @@ UPDATE CECRED.crapprm
  WHERE nmsistem = 'CRED'
    AND cdcooper = 0
    AND cdacesso = 'PERC_ALTO_FRAUDE_ADM_P';
+
+UPDATE CECRED.crapprm
+   SET DSVLRPRM = 't0033471@ailos.coop.br'
+ WHERE nmsistem = 'CRED'
+   AND cdcooper = 0
+   AND cdacesso = 'EMAIL_RISCOFRAUDE_ADM';
+
 COMMIT;
 END;
 /
