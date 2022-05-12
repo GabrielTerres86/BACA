75b3acab6041d9a0f6fd026970b20f0d384a36db
Douglas <douglas.kloppel@ailos.coop.br>
2022-05-12 09:05:01 -0300

    alterando o update, pois ele já possui registro nessa data em produção

--- 2022/05/inc0138524_update_crapsli.sql
+++ 2022/05/inc0138524_update_crapsli.sql
@@ -1,10 +1,10 @@
 BEGIN
   
-  UPDATE cecred.crapsli 
-     SET dtrefere = to_date('31/05/2022','dd/mm/yyyy') 
+ UPDATE cecred.crapsli 
+     SET vlsddisp = vlsddisp + 399255.92
    WHERE nrdconta = 191051 
      AND cdcooper = 9
-     AND dtrefere = to_date('30/04/2022','dd/mm/yyyy');
+     AND dtrefere = to_date('31/05/2022','dd/mm/yyyy');
   
   COMMIT;
   
