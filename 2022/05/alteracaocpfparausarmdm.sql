c4a23d185b061e5f014879f70421b1c0be8acd83
Alan Beckhauser <alan.beckhauser@ailos.coop.br>
2022-05-13 15:06:16 -0300

    alterar cpf cnpj na crapcns para uso do mdm

---
+++ 2022/05/alteracaocpfparausarmdm.sql
@@ -0,0 +1,6 @@
+begin
+update cecred.crapcns a set a.nrcpfcgc = 21705564000108 where a.nrcpfcgc = 8628959000188;
+update cecred.crapcns a set a.nrcpfcgc = 64920194900  where a.nrcpfcgc = 4733551835;
+update cecred.crapcns a set a.nrcpfcgc = 28214433000169  where a.nrcpfcgc = 20866517000175;
+commit;
+end;
