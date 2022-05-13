7fc0e15be5f35736214bcfd2ed1a2c6f110e2de0
Felipe Aliprandi Santanna <felipe.santanna@ailos.coop.br>
2022-05-13 11:37:11 -0300

    inclusao emp 580 na craptab

---
+++ 2022/05/PRB0047136IncEmp580CRAPTABTest.sql
@@ -0,0 +1,6 @@
+begin
+      insert into craptab (NMSISTEM, TPTABELA, CDEMPRES, CDACESSO, TPREGIST, DSTEXTAB, CDCOOPER)
+      values ('CRED', 'USUARI', 580, 'EXECDEBEMP', 0, '30/04/2022 16/05/2022 0', 13);
+  commit;
+end;
+/
