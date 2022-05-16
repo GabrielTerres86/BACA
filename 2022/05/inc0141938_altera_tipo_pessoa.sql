667c1f78edceb7300e1451f5c0a79eac9081049a
Rodrigo Lima <capgemini-rodrigo.erasmo@ailos.coop.br>
2022-05-16 14:02:40 -0300

    Script para alterar Tipo de Pessoa do Fornecedor

---
+++ 2022/05/inc0141938_altera_tipo_pessoa.sql
@@ -0,0 +1,17 @@
+BEGIN
+
+  UPDATE crapsab sab
+     SET sab.cdtpinsc = 1 
+   WHERE sab.cdcooper = 7
+     AND sab.cdtpinsc = 2 
+     AND sab.nrdconta = 161187     
+     AND sab.nrinssac = '124385966,00';
+  COMMIT;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    SISTEMA.excecaoInterna(pr_compleme => 'INC0141938');
+    raise_application_error(-20500, SQLERRM);
+  
+END;
