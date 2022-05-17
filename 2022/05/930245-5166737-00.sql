dba17513b426becbe1d421ad1dd786c20e15675b
Guilherme Pinheiro <guilherme.pinheiro@mouts.com.br>
2022-05-17 15:37:05 -0300

    add owner's

--- 2022/05/930245-5166737-00.sql
+++ 2022/05/930245-5166737-00.sql
@@ -2,7 +2,7 @@ DECLARE
   vr_cdcritic crapcri.cdcritic%TYPE;
   vr_dscritic VARCHAR2(10000);
   vr_exc_saida EXCEPTION;
-  rw_crapdat BTCH0001.cr_crapdat%ROWTYPE;
+  rw_crapdat CECRED.BTCH0001.cr_crapdat%ROWTYPE;
 
   vr_cdcooper crapcop.cdcooper%TYPE := 1;
   vr_nrdconta crapass.nrdconta%TYPE := 930245;
@@ -13,74 +13,74 @@ DECLARE
   CURSOR cr_crapass(pr_cdcooper IN crapass.cdcooper%TYPE
                    ,pr_nrdconta IN crapass.nrdconta%TYPE) IS
     SELECT ass.cdagenci
-      FROM crapass ass
+      FROM CECRED.crapass ass
      WHERE ass.cdcooper = pr_cdcooper
        AND ass.nrdconta = pr_nrdconta;
   rw_crapass cr_crapass%ROWTYPE;
 
 BEGIN
 
-  OPEN btch0001.cr_crapdat(pr_cdcooper => vr_cdcooper);
-  FETCH btch0001.cr_crapdat
+  OPEN CECRED.btch0001.cr_crapdat(pr_cdcooper => vr_cdcooper);
+  FETCH CECRED.btch0001.cr_crapdat
     INTO rw_crapdat;
-  CLOSE btch0001.cr_crapdat;
+  CLOSE CECRED.btch0001.cr_crapdat;
 
   OPEN cr_crapass(pr_cdcooper => vr_cdcooper, pr_nrdconta => vr_nrdconta);
   FETCH cr_crapass
     INTO rw_crapass;
   CLOSE cr_crapass;
 
-  EMPR0001.pc_cria_lancamento_lem(pr_cdcooper => vr_cdcooper
-                                 ,pr_dtmvtolt => rw_crapdat.dtmvtolt
-                                 ,pr_cdagenci => rw_crapass.cdagenci
-                                 ,pr_cdbccxlt => 100
-                                 ,pr_cdoperad => 1
-                                 ,pr_cdpactra => rw_crapass.cdagenci
-                                 ,pr_tplotmov => 5
-                                 ,pr_nrdolote => 600031
-                                 ,pr_nrdconta => vr_nrdconta
-                                 ,pr_cdhistor => vr_cdhistor
-                                 ,pr_nrctremp => vr_nrctremp
-                                 ,pr_vllanmto => vr_vllanmto
-                                 ,pr_dtpagemp => rw_crapdat.dtmvtolt
-                                 ,pr_txjurepr => 0
-                                 ,pr_vlpreemp => 0
-                                 ,pr_nrsequni => 0
-                                 ,pr_nrparepr => 0
-                                 ,pr_flgincre => FALSE
-                                 ,pr_flgcredi => FALSE
-                                 ,pr_nrseqava => 0
-                                 ,pr_cdorigem => 5
-                                 ,pr_cdcritic => vr_cdcritic
-                                 ,pr_dscritic => vr_dscritic);
+  CECRED.EMPR0001.pc_cria_lancamento_lem(pr_cdcooper => vr_cdcooper
+                                        ,pr_dtmvtolt => rw_crapdat.dtmvtolt
+                                        ,pr_cdagenci => rw_crapass.cdagenci
+                                        ,pr_cdbccxlt => 100
+                                        ,pr_cdoperad => 1
+                                        ,pr_cdpactra => rw_crapass.cdagenci
+                                        ,pr_tplotmov => 5
+                                        ,pr_nrdolote => 600031
+                                        ,pr_nrdconta => vr_nrdconta
+                                        ,pr_cdhistor => vr_cdhistor
+                                        ,pr_nrctremp => vr_nrctremp
+                                        ,pr_vllanmto => vr_vllanmto
+                                        ,pr_dtpagemp => rw_crapdat.dtmvtolt
+                                        ,pr_txjurepr => 0
+                                        ,pr_vlpreemp => 0
+                                        ,pr_nrsequni => 0
+                                        ,pr_nrparepr => 0
+                                        ,pr_flgincre => FALSE
+                                        ,pr_flgcredi => FALSE
+                                        ,pr_nrseqava => 0
+                                        ,pr_cdorigem => 5
+                                        ,pr_cdcritic => vr_cdcritic
+                                        ,pr_dscritic => vr_dscritic);
 
   IF vr_cdcritic IS NOT NULL OR vr_dscritic IS NOT NULL THEN
     RAISE vr_exc_saida;
   END IF;
 
-  EMPR0001.pc_cria_lancamento_lem(pr_cdcooper => vr_cdcooper
-                                 ,pr_dtmvtolt => rw_crapdat.dtmvtolt
-                                 ,pr_cdagenci => rw_crapass.cdagenci
-                                 ,pr_cdbccxlt => 100
-                                 ,pr_cdoperad => 1
-                                 ,pr_cdpactra => rw_crapass.cdagenci
-                                 ,pr_tplotmov => 5
-                                 ,pr_nrdolote => 600031
-                                 ,pr_nrdconta => vr_nrdconta
-                                 ,pr_cdhistor => 1043
-                                 ,pr_nrctremp => vr_nrctremp
-                                 ,pr_vllanmto => 212.99
-                                 ,pr_dtpagemp => rw_crapdat.dtmvtolt
-                                 ,pr_txjurepr => 0
-                                 ,pr_vlpreemp => 0
-                                 ,pr_nrsequni => 0
-                                 ,pr_nrparepr => 0
-                                 ,pr_flgincre => FALSE
-                                 ,pr_flgcredi => FALSE
-                                 ,pr_nrseqava => 0
-                                 ,pr_cdorigem => 5
-                                 ,pr_cdcritic => vr_cdcritic
-                                 ,pr_dscritic => vr_dscritic);
+  CECRED.EMPR0001.pc_cria_lancamento_lem(pr_cdcooper => vr_cdcooper
+                                        ,pr_dtmvtolt => rw_crapdat.dtmvtolt
+                                        ,pr_cdagenci => rw_crapass.cdagenci
+                                        ,pr_cdbccxlt => 100
+                                        ,pr_cdoperad => 1
+                                        ,pr_cdpactra => rw_crapass.cdagenci
+                                        ,pr_tplotmov => 5
+                                        ,pr_nrdolote => 600031
+                                        ,pr_nrdconta => vr_nrdconta
+                                        ,pr_cdhistor => 1043
+                                        ,pr_nrctremp => vr_nrctremp
+                                        ,pr_vllanmto => 212.99
+                                        ,pr_dtpagemp => rw_crapdat.dtmvtolt
+                                        ,pr_txjurepr => 0
+                                        ,pr_vlpreemp => 0
+                                        ,pr_nrsequni => 0
+                                        ,pr_nrparepr => 0
+                                        ,pr_flgincre => FALSE
+                                        ,pr_flgcredi => FALSE
+                                        ,pr_nrseqava => 0
+                                        ,pr_cdorigem => 5
+                                        ,pr_cdcritic => vr_cdcritic
+                                        ,pr_dscritic => vr_dscritic);
 
   COMMIT;
 
