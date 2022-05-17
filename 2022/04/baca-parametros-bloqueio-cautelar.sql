32e05029120065cbe114a6dea23d619610bc571e
Carlos Vinicius Pinho Moraes <t0034605@cecred.coop.br>
2022-05-17 18:02:56 -0300

    adicao do baca para atualizao da lista de parametro de pc_grava_parametros_bloqueio_cautelar e adicao do parametro de analise da cadfra

--- 2022/04/baca-parametros-bloqueio-cautelar.sql
+++ 2022/04/baca-parametros-bloqueio-cautelar.sql
@@ -1,17 +1,19 @@
 declare
 begin 
-INSERT INTO cecred.crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) values ('CRED', 0, 'VLCORTE_DICT_BLQ_CAUT', 
+INSERT INTO cecred.crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+values ('CRED', 0, 'VLCORTE_DICT_BLQ_CAUT', 
 'Valor de corte das transações para determinar a DICT deve ser consultada ou não no bloqueio cautelar.', '30,00');
 
-INSERT INTO cecred.crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) values ('CRED', 0, 'HRDESAB_DICT_BLQ_CAUT', 
+INSERT INTO cecred.crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) 
+values ('CRED', 0, 'HRDESAB_DICT_BLQ_CAUT', 
 'Quantidade de horas após a ultima atualização que determinará se o registro com informações da DICT estará desatualizado.', '2');
 
-INSERT INTO cecred.crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) values ('CRED', 0, 'ANALISE_DICT_BLQ_CAUT', 'Valor que permite a desativação da análise de PIX no recebimento.', '0');
+INSERT INTO cecred.crapaca (nmdeacao, nmpackag, nmproced, nrseqrdr) 
+values ('CADFRA_BUSCA_PARAM_BLQ_CAUTELAR', 'TELA_CADFRA', 'pc_busca_parametros_bloqueio_cautelar', 704);
 
-INSERT INTO cecred.crapaca (nmdeacao, nmpackag, nmproced, nrseqrdr) values ('CADFRA_BUSCA_PARAM_BLQ_CAUTELAR', 'TELA_CADFRA', 'pc_busca_parametros_bloqueio_cautelar', 704);
-
-INSERT INTO cecred.crapaca (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr) values ('CADFRA_GRAVA_PARAM_BLQ_CAUTELAR', 'TELA_CADFRA', 'pc_grava_parametros_bloqueio_cautelar',
-'pr_vlcortedict,pr_hrdesabilitado, pr_analise', 704);
+INSERT INTO cecred.crapaca (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr) 
+values ('CADFRA_GRAVA_PARAM_BLQ_CAUTELAR', 'TELA_CADFRA', 'pc_grava_parametros_bloqueio_cautelar',
+'pr_vlcortedict,pr_hrdesabilitado', 704);
 
 
 UPDATE cecred.craptel
