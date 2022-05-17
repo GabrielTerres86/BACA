09773e0ea26c9cdf3dda13c9fe6107ff0388fcfc
Caroline <premiersoft-caroline.castro@ailos.coop.br>
2022-05-17 10:30:49 -0300

    Alteração de status.

---
+++ 2022/05/tsk357871.sql
@@ -0,0 +1,13 @@
+begin
+  UPDATE TBEPR_CONSIGNADO_PAGAMENTO
+   SET INSTATUS = 2
+ WHERE IDSEQUENCIA = 1175197
+   AND CDCOOPER = 14
+   AND NRDCONTA = 310190
+   AND NRCTREMP = 40091;
+   
+  COMMIT;
+exception
+  when others then
+    rollback;
+end;
