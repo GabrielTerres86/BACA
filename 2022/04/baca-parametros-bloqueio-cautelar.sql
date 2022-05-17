23f45782aef09e50fc7ae0cd09c7a5ed1cc61732
Carlos Vinicius Pinho Moraes <t0034605@cecred.coop.br>
2022-05-17 17:08:38 -0300

    .

--- 2022/04/baca-parametros-bloqueio-cautelar.sql
+++ 2022/04/baca-parametros-bloqueio-cautelar.sql
@@ -6,21 +6,19 @@ INSERT INTO cecred.crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) va
 INSERT INTO cecred.crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) values ('CRED', 0, 'HRDESAB_DICT_BLQ_CAUT', 
 'Quantidade de horas após a ultima atualização que determinará se o registro com informações da DICT estará desatualizado.', '2');
 
-INSERT INTO cecred.crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) values ('CRED', 0, 'ANALISE_DICT_BLQ_CAUT', 
-'Valor que permite a desativação da análise de PIX no recebimento.', '0');
+INSERT INTO cecred.crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) values ('CRED', 0, 'ANALISE_DICT_BLQ_CAUT', 'Valor que permite a desativação da análise de PIX no recebimento.', '0');
 
 INSERT INTO cecred.crapaca (nmdeacao, nmpackag, nmproced, nrseqrdr) values ('CADFRA_BUSCA_PARAM_BLQ_CAUTELAR', 'TELA_CADFRA', 'pc_busca_parametros_bloqueio_cautelar', 704);
 
 INSERT INTO cecred.crapaca (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr) values ('CADFRA_GRAVA_PARAM_BLQ_CAUTELAR', 'TELA_CADFRA', 'pc_grava_parametros_bloqueio_cautelar',
-'pr_vlcortedict,pr_hrdesabilitado', 704);
+'pr_vlcortedict,pr_hrdesabilitado, pr_analise', 704);
+
 
 UPDATE cecred.craptel
 SET CDOPPTEL = 'C,A,E,B,G', LSOPPTEL = 'CONSULTAR,ALTERAR,EXCLUIR,BLOQUEAR,CAUTELAR'
 WHERE nmdatela = 'CADFRA';
 
-INSERT INTO cecred.crapace (nmdatela, cddopcao, cdoperad, nmrotina, cdcooper, nrmodulo, idevento, idambace) values ('CADFRA', 'G', 'f0031251','', '3', '1', '1','2');
 
-commit;
-end;
+commit;end;
+
 /
-
