681a8e478c7c7396262fc397585e5707298c1cbf
Andrey Leonardo Bauler <t0033422@ailos.coop.br>
2022-05-18 14:50:28 -0300

    Exclusão de menus do contexto de multiplas contas

--- 2022/04/prj686-insere-menus-cobranca2.sql
+++ 2022/04/prj686-insere-menus-cobranca2.sql
@@ -16,28 +16,10 @@ BEGIN
          ,dhcadastro
   ) VALUES (
     (select NVL(max(iditem_menu_multcontas),0) from INTERNETBANKING.tbib_itens_menu_multcontas)+1
-    ,109
-    ,sysdate
-  );
-
-  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
-          iditem_menu_multcontas
-         ,cditem_menu
-         ,dhcadastro
-  ) VALUES (
-    (select NVL(max(iditem_menu_multcontas),0) from INTERNETBANKING.tbib_itens_menu_multcontas)+1
     ,104
     ,sysdate
   );
 
-  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
-          iditem_menu_multcontas
-         ,cditem_menu
-         ,dhcadastro
-  ) VALUES (
-    (select NVL(max(iditem_menu_multcontas),0) from INTERNETBANKING.tbib_itens_menu_multcontas)+1
-    ,105
-    ,sysdate
-  );
+ 
   commit;
 end;
