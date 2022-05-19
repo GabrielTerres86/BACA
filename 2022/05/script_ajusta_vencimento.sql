1541289e97787dc41dc6d004cc2d41fd80ee52cc
Cristiano Schroeder <cristiano.schroeder@ailos.coop.br>
2022-05-19 10:41:05 -0300

    ajuste no vencimento das parcelas

--- 2022/05/script_ajusta_vencimento.sql
+++ 2022/05/script_ajusta_vencimento.sql
@@ -10,6 +10,19 @@ begin
   where pep.cdcooper = 13
     and pep.nrdconta = 478512
     and pep.nrctremp = 185911;
+	
+ update cecred.crappep pep
+    set pep.dtvencto = add_months(pep.dtvencto,1)
+  where pep.cdcooper = 13
+    and pep.nrdconta = 184993
+    and pep.nrctremp = 188624;	
+
+ update cecred.crappep pep
+    set pep.dtvencto = add_months(pep.dtvencto,-1)
+  where pep.cdcooper = 13
+    and pep.nrdconta = 415189
+    and pep.nrctremp = 188905;		
+	
   COMMIT;
 exception
   when others then
