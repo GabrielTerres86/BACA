c80b14f37e355af27b6473794e1a72b8d74f058b
Paulo Felipe Kowalsky <t0032603@cecred.coop.br>
2022-05-23 15:40:38 -0300

    update tabela de envelope

--- 2022/05/update-crapenl-data-movimento.sql
+++ 2022/05/update-crapenl-data-movimento.sql
@@ -1,6 +1,6 @@
 begin
  
-update crapenl crapenl
+update cecred.crapenl crapenl
   set crapenl.dtmvtolt = to_date('23/05/2022','DD/MM/YYYY') 
 where  crapenl.cdcooper = 5
   AND crapenl.dtmvtolt = '25/04/2022' 
@@ -8,6 +8,6 @@ where  crapenl.cdcooper = 5
   AND crapenl.nrterfin = 11 
   and crapenl.vlchqinf > 20000
   and  crapenl.cdsitenv = 0;
-  
+
 commit;
 end;
