272e94e0d4a629f70fe3cf06c86f33a95e9a56fa
Douglas Pagel <douglas.pagel@ailos.coop.br>
2022-05-27 10:39:01 -0300

    aprovação de proposta

--- 2022/05/atualizar_proposta_imob.sql
+++ 2022/05/atualizar_proposta_imob.sql
@@ -1,7 +1,7 @@
  begin
    update credito.tbepr_contrato_imobiliario c
       set c.situacao_analise = 3, c.situacao_aprovacao = 1
-    where c.cdcooper = 1
+    where c.cdcooper = 16
       and c.nrdconta = 2841568
       and c.nrctremp = 469227;
    commit;
