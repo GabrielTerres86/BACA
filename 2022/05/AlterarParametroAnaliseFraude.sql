5b69ee99dee9c7744fea93f0972f8198e8dbf8ab
Ricardo de Souza (Supero) <t0032449@ailos.coop.br>
2022-05-30 14:19:48 -0300

    Zerar paracer da análise para debug

--- 2022/05/AlterarParametroAnaliseFraude.sql
+++ 2022/05/AlterarParametroAnaliseFraude.sql
@@ -3,7 +3,7 @@ BEGIN
 UPDATE tbgen_analise_fraude
 SET cdstatus_analise = 0
 , cdparecer_analise = 0
-WHERE idanalise_fraude = 152524364;
+WHERE idanalise_fraude  in (152524828, 152524694);
  
 COMMIT;
 END;
