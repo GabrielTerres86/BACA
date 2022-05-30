16c2696bf6b7ef7e5d8d35ded005f930629ee0b1
Alana Ribeiro Ramos Sasse <premiersoft-alana.sasse@ailos.coop.br>
2022-05-30 18:04:20 -0300

    Atualizacao

--- 2022/05/Status-Calris-Tanque.sql
+++ 2022/05/Status-Calris-Tanque.sql
@@ -2,15 +2,12 @@ BEGIN
 INSERT INTO tbgen_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
 VALUES
 ('CALRIS_STATUS_TANQUE', 13, 'Aguardando envio de cadidato');
-/
 INSERT INTO tbgen_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
 VALUES
 ('CALRIS_STATUS_TANQUE', 14, 'Envio de cadidato a Gupy com erro');
-/
 INSERT INTO tbgen_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
 VALUES
 ('CALRIS_STATUS_TANQUE', 15, 'Aguardando lista reputacional');
-/
 INSERT INTO tbgen_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
 VALUES
 ('CALRIS_STATUS_TANQUE', 16, 'Lista reputacional com erro');
