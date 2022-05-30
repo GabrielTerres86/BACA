0544fb25954b10ca44c29e2121e105d7b41afbd4
Caroline <premiersoft-caroline.castro@ailos.coop.br>
2022-05-30 10:57:42 -0300

    Ajuste de valor de saldo devedor negativo

---
+++ 2022/05/tsk366145-saldodevedornegativo.sql
@@ -0,0 +1,17 @@
+BEGIN
+     UPDATE crapepr epr
+        SET epr.vlsdeved = (SELECT nvl(sum(vlsdvpar),0)
+                            FROM   crappep pep 
+                            WHERE  pep.cdcooper = epr.cdcooper 
+                            AND    pep.nrdconta = epr.nrdconta 
+                            AND    pep.nrctremp = epr.nrctremp
+                            and    pep.inliquid = 0)
+      WHERE (cdcooper , nrdconta, nrctremp ) in ((16, 647101, 306986),(1, 7928890, 2295730)) ;      
+   
+   COMMIT;
+   
+EXCEPTION
+  WHEN OTHERS THEN
+    RAISE_application_error(-20500, SQLERRM);
+    ROLLBACK;
+END;
