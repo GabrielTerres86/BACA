412f300f70447f80517454fa7e3389e1a177cd5d
Matheus <matheus.leriano@ailos.coop.br>
2022-05-31 10:25:23 -0300

    PR para ajustar parametro de calculo pre aprovado folego

--- 2022/05/RITM0221977-parametro-via-folego.sql
+++ 2022/05/RITM0221977-parametro-via-folego.sql
@@ -1,7 +1,7 @@
 begin
-update crapprm p 
-   set p.dsvlrprm = 6 
- where p.cdacesso in ('COVID_QTDE_PARCELA_PAGAR')
-   and p.cdcooper = 1;
+  update cecred.crapprm p 
+     set p.dsvlrprm = 4 
+   where p.cdacesso in ('COVID_QTDE_PARCELA_PAGAR')
+     and p.cdcooper = 1;
 commit;
 end;   
