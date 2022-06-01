186f5431d24bc02ab5f4888f38d97bcba11df28e
Filipe Ribeiro <mouts-filipe.rego@ailos.coop.br>
2022-06-01 17:20:04 -0300

    Atualização de datas dos seguros de empréstimos renegociados

--- 2022/05/seguro_pessoa_inc0135493.sql
+++ 2022/05/seguro_pessoa_inc0135493.sql
@@ -2,34 +2,34 @@ DECLARE
   vr_ind_arq    utl_file.file_type;
   vr_linha      VARCHAR2(32767);
   vr_dscritic   VARCHAR2(2000);
-  vr_nmdir      VARCHAR2(4000) := gene0001.fn_param_sistema('CRED',3,'ROOT_MICROS')||'cpd/bacas/INC0135493';
+  vr_nmdir      VARCHAR2(4000) := CECRED.gene0001.fn_param_sistema('CRED',3,'ROOT_MICROS')||'cpd/bacas/INC0135493';
   vr_nmarq      VARCHAR2(100)  := 'ROLLBACK_INC0135493.sql';
   vr_exc_saida  EXCEPTION;
   vcount        NUMBER;
 
-  CURSOR cr_crawseg(pr_cdcooper crawseg.cdcooper%TYPE,
-                    pr_nrdconta crawseg.nrdconta%TYPE,
-                    pr_nrctrseg crawseg.nrctrseg%TYPE) IS
+  CURSOR cr_crawseg(pr_cdcooper CECRED.crawseg.cdcooper%TYPE,
+                    pr_nrdconta CECRED.crawseg.nrdconta%TYPE,
+                    pr_nrctrseg CECRED.crawseg.nrctrseg%TYPE) IS
     SELECT w.dtinivig,
            w.dtfimvig
-      FROM crawseg w
+      FROM CECRED.crawseg w
      WHERE w.cdcooper = pr_cdcooper
        AND w.nrdconta = pr_nrdconta
        AND w.nrctrseg = pr_nrctrseg;
        
-  CURSOR cr_crapseg(pr_cdcooper crapseg.cdcooper%TYPE,
-                    pr_nrdconta crapseg.nrdconta%TYPE,
-                    pr_nrctrseg crapseg.nrctrseg%TYPE) IS
+  CURSOR cr_crapseg(pr_cdcooper CECRED.crapseg.cdcooper%TYPE,
+                    pr_nrdconta CECRED.crapseg.nrdconta%TYPE,
+                    pr_nrctrseg CECRED.crapseg.nrctrseg%TYPE) IS
     SELECT p.dtinivig,
            p.dtfimvig
-      FROM crapseg p
+      FROM CECRED.crapseg p
      WHERE p.cdcooper = pr_cdcooper
        AND p.nrdconta = pr_nrdconta
        AND p.tpseguro = 4
        AND p.nrctrseg = pr_nrctrseg;
 
-  CURSOR cr_prestamista(pr_cdcooper tbseg_prestamista.cdcooper%TYPE,
-                        pr_dtmvtolt tbseg_prestamista.dtfimvig%TYPE) IS
+  CURSOR cr_prestamista(pr_cdcooper CECRED.tbseg_prestamista.cdcooper%TYPE,
+                        pr_dtmvtolt CECRED.tbseg_prestamista.dtfimvig%TYPE) IS
     SELECT p.nrdconta,
            p.nrctrseg,
            p.nrctremp,
@@ -37,13 +37,13 @@ DECLARE
            p.dtfimvig,
            e.dtmvtolt,
            (SELECT MAX(pe.dtvencto) 
-              FROM crappep pe
+              FROM CECRED.crappep pe
              WHERE p.cdcooper = pe.cdcooper
                AND p.nrdconta = pe.nrdconta
                AND p.nrctremp = pe.nrctremp) dtvencto
-      FROM crapseg s,
-           crawepr e,
-           tbseg_prestamista p
+      FROM CECRED.crapseg s,
+           CECRED.crawepr e,
+           CECRED.tbseg_prestamista p
      WHERE p.cdcooper = pr_cdcooper
        AND p.cdcooper = e.cdcooper
        AND p.nrdconta = e.nrdconta
@@ -59,15 +59,15 @@ DECLARE
   CURSOR cr_crapcop IS
     SELECT c.cdcooper
           ,d.dtmvtolt
-      FROM crapcop c,
-           crapdat d
+      FROM CECRED.crapcop c,
+           CECRED.crapdat d
      WHERE c.flgativo = 1
        AND c.cdcooper <> 3
        AND c.cdcooper = d.cdcooper;
 
   BEGIN
     BEGIN
-      GENE0001.pc_abre_arquivo(pr_nmdireto => vr_nmdir
+      CECRED.GENE0001.pc_abre_arquivo(pr_nmdireto => vr_nmdir
                               ,pr_nmarquiv => vr_nmarq
                               ,pr_tipabert => 'W'
                               ,pr_utlfileh => vr_ind_arq
@@ -87,16 +87,16 @@ DECLARE
         FOR rw_prestamista IN cr_prestamista(pr_cdcooper => rw_crapcop.cdcooper
                                             ,pr_dtmvtolt => rw_crapcop.dtmvtolt) LOOP
                                             
-          vr_linha :=   'UPDATE tbseg_prestamista '
+          vr_linha :=   'UPDATE CECRED.tbseg_prestamista '
                       ||'   SET dtinivig = ''' || rw_prestamista.dtinivig || ','''
                       ||'       dtfimvig = ''' || rw_prestamista.dtfimvig || ''''
                       ||' WHERE cdcooper = ' || rw_crapcop.cdcooper
                       ||'   AND nrdconta = ' || rw_prestamista.nrdconta
                       ||'   AND nrctrseg = ' || rw_prestamista.nrctrseg ||' ; ';
 
-          GENE0001.pc_escr_linha_arquivo(vr_ind_arq,vr_linha);
+          CECRED.GENE0001.pc_escr_linha_arquivo(vr_ind_arq,vr_linha);
 
-          UPDATE tbseg_prestamista p
+          UPDATE CECRED.tbseg_prestamista p
              SET p.dtinivig = TO_DATE(rw_prestamista.dtmvtolt,'DD/MM/RRRR')
                 ,p.dtfimvig = TO_DATE(rw_prestamista.dtvencto,'DD/MM/RRRR')
            WHERE p.cdcooper = rw_crapcop.cdcooper
@@ -107,16 +107,16 @@ DECLARE
                                       ,pr_nrdconta => rw_prestamista.nrdconta
                                       ,pr_nrctrseg => rw_prestamista.nrctrseg) LOOP
                                       
-            vr_linha :=   'UPDATE crawseg '
+            vr_linha :=   'UPDATE CECRED.crawseg '
                         ||'   SET dtinivig = ''' || rw_crawseg.dtinivig || ','''
                         ||'       dtfimvig = ''' || rw_crawseg.dtfimvig || ''''
                         ||' WHERE cdcooper = ' || rw_crapcop.cdcooper
                         ||'   AND nrdconta = ' || rw_prestamista.nrdconta
                         ||'   AND nrctrseg = ' || rw_prestamista.nrctrseg ||' ; ';
 
-            GENE0001.pc_escr_linha_arquivo(vr_ind_arq,vr_linha);
+            CECRED.GENE0001.pc_escr_linha_arquivo(vr_ind_arq,vr_linha);
 
-            UPDATE crawseg w
+            UPDATE CECRED.crawseg w
                SET w.dtinivig = TO_DATE(rw_prestamista.dtmvtolt,'DD/MM/RRRR')
                   ,w.dtfimvig = TO_DATE(rw_prestamista.dtvencto,'DD/MM/RRRR')
              WHERE w.cdcooper = rw_crapcop.cdcooper
@@ -128,16 +128,16 @@ DECLARE
                                       ,pr_nrdconta => rw_prestamista.nrdconta
                                       ,pr_nrctrseg => rw_prestamista.nrctrseg) LOOP
                                       
-          vr_linha :=   'UPDATE crapseg '
+          vr_linha :=   'UPDATE CECRED.crapseg '
                       ||'   SET dtinivig = ''' || rw_crapseg.dtinivig || ','''
                       ||'       dtfimvig = ''' || rw_crapseg.dtfimvig || ''''
                       ||' WHERE cdcooper = ' || rw_crapcop.cdcooper
                       ||'   AND nrdconta = ' || rw_prestamista.nrdconta
                       ||'   AND nrctrseg = ' || rw_prestamista.nrctrseg ||' ; ';
 
-          GENE0001.pc_escr_linha_arquivo(vr_ind_arq,vr_linha);
+          CECRED.GENE0001.pc_escr_linha_arquivo(vr_ind_arq,vr_linha);
 
-          UPDATE crapseg p
+          UPDATE CECRED.crapseg p
              SET p.dtinivig = TO_DATE(rw_prestamista.dtmvtolt,'DD/MM/RRRR')
                 ,p.dtfimvig = TO_DATE(rw_prestamista.dtvencto,'DD/MM/RRRR')
            WHERE p.cdcooper = rw_crapcop.cdcooper
@@ -154,10 +154,10 @@ DECLARE
         COMMIT;
       END LOOP;
 
-      GENE0001.pc_escr_linha_arquivo(vr_ind_arq,' COMMIT;');
-      GENE0001.pc_escr_linha_arquivo(vr_ind_arq,' END; ');
-      GENE0001.pc_escr_linha_arquivo(vr_ind_arq,'/ ');
-      GENE0001.pc_fecha_arquivo(pr_utlfileh => vr_ind_arq );
+      CECRED.GENE0001.pc_escr_linha_arquivo(vr_ind_arq,' COMMIT;');
+      CECRED.GENE0001.pc_escr_linha_arquivo(vr_ind_arq,' END; ');
+      CECRED.GENE0001.pc_escr_linha_arquivo(vr_ind_arq,'/ ');
+      CECRED.GENE0001.pc_fecha_arquivo(pr_utlfileh => vr_ind_arq );
     EXCEPTION
        WHEN vr_exc_saida THEN
             vr_dscritic := vr_dscritic || SQLERRM || DBMS_UTILITY.FORMAT_ERROR_BACKTRACE;
