d2425fa433ca3a59d910e4faa0d0c68c0f0c7bb0
Leonardo Nogueira da Silva <softtek-leonardo.silva@ailos.coop.br>
2022-06-02 10:15:20 -0300

    ajustes pr

---
+++ 2022/05/ritm219374_nrdconta72249.sql
@@ -0,0 +1,81 @@
+declare
+ vr_exc_saida     EXCEPTION;
+ 
+  vr_cdcooper      crapcop.cdcooper%TYPE := 13;
+  vr_nrdconta      crapass.nrdconta%TYPE := 72249;
+  vr_nrctrempa     craplem.nrctremp%TYPE := 200355;
+  vr_nrctrempd     craplem.nrctremp%TYPE := 137571;
+  
+BEGIN
+   update cecred.crapavl t 
+      set t.nrctravd = vr_nrctrempd
+    where t.cdcooper = vr_cdcooper 
+      and t.nrctaavd = vr_nrdconta 
+      and t.nrctravd = vr_nrctrempa;
+     
+    update cecred.crapavt t 
+       set t.nrctremp = vr_nrctrempd
+     where t.cdcooper = vr_cdcooper 
+       and t.nrdconta = vr_nrdconta 
+       and t.nrctremp = vr_nrctrempa 
+       and tpctrato in (1,4);
+       
+    update cecred.crawcrd t 
+       set t.nrctrcrd = vr_nrctrempd
+     where t.cdcooper = vr_cdcooper 
+       and t.nrdconta = vr_nrdconta 
+       and t.nrctrcrd = vr_nrctrempa;
+       
+    update cecred.crapprp t 
+       set t.nrctrato = vr_nrctrempd
+     where t.cdcooper = vr_cdcooper 
+       and t.nrdconta = vr_nrdconta 
+       and t.nrctrato = vr_nrctrempa 
+       and t.tpctrato = 90;
+       
+    update cecred.crapbpr t 
+       set t.nrctrpro = vr_nrctrempd
+    where t.cdcooper = vr_cdcooper 
+      and t.nrdconta = vr_nrdconta 
+      and t.nrctrpro = vr_nrctrempa 
+      and t.tpctrpro = 90;
+      
+   update cecred.craprpr t 
+      set t.nrctrato = vr_nrctrempd
+    where t.cdcooper = vr_cdcooper 
+      and t.nrdconta = vr_nrdconta 
+      and t.nrctrato = vr_nrctrempa 
+      and t.tpctrato = 90;
+      
+   update cecred.crappep t 
+      set t.nrctremp = vr_nrctrempd
+    where t.cdcooper = vr_cdcooper 
+      and t.nrdconta = vr_nrdconta 
+      and t.nrctremp = vr_nrctrempa;
+      
+   update cecred.tbrating_historicos t 
+      set t.nrctremp = vr_nrctrempd
+    where t.cdcooper = vr_cdcooper 
+      and t.nrdconta = vr_nrdconta 
+      and t.nrctremp = vr_nrctrempa 
+      and t.tpctrato = 90;
+      
+  update cecred.tbrisco_operacoes t 
+     set t.nrctremp = vr_nrctrempd
+   where t.cdcooper = vr_cdcooper 
+     and t.nrdconta = vr_nrdconta 
+     and t.nrctremp = vr_nrctrempa 
+     and t.tpctrato = 90;
+     
+  update cecred.crawepr t 
+    set t.nrctremp = vr_nrctrempd
+  where t.cdcooper = vr_cdcooper 
+    and t.nrdconta = vr_nrdconta 
+    and t.nrctremp = vr_nrctrempa;
+               
+  commit;
+exception 
+  when others then
+    rollback;
+    raise_application_error(-20500, SQLERRM);
+end;
