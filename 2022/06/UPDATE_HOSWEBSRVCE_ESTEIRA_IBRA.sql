fbcb873cce47cf5c5a7bfef0f1e146053edd9525
Rafael Felipe de Oliveira <f0033717@cecred.coop.br>
2022-06-09 16:27:30 -0300

    Alterar esteira de credito destino para wsesteiracredito

--- 2022/06/UPDATE_HOSWEBSRVCE_ESTEIRA_IBRA.sql
+++ 2022/06/UPDATE_HOSWEBSRVCE_ESTEIRA_IBRA.sql
@@ -1,7 +1,7 @@
 BEGIN
 
    UPDATE cecred.crapprm prm
-     SET prm.dsvlrprm = 'https://wsesteiracreditocdc.ailoshml.coop.br'
+     SET prm.dsvlrprm = 'https://wsesteiracredito.ailoshml.coop.br'
    WHERE prm.cdacesso = 'HOSWEBSRVCE_ESTEIRA_IBRA' 
      AND prm.nmsistem = 'CRED';
 
