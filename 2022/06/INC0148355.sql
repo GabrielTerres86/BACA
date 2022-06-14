f348d0675b0c25fff2239ea130dd5fe2bf78a29f
Marcus Abreu <premiersoft-marcus.abreu@ailos.coop.br>
2022-06-14 09:45:00 -0300

    INC0148355 - Inclusão de nova conta, removendo O Lanamento Futuro de Recarga Vivo.

--- 2022/06/INC0148355.sql
+++ 2022/06/INC0148355.sql
@@ -11,10 +11,12 @@ declare
  vr_dttransa          cecred.craplgm.dttransa%type;
  vr_hrtransa          cecred.craplgm.hrtransa%type;
  
- vc_dstransa          CONSTANT VARCHAR2(4000) := 'Baixa das Tarifas pendentes a lancar em Conta encerrada - INC0148355';
+ vc_dstransaTar       CONSTANT VARCHAR2(4000) := 'Baixa das Tarifas pendentes a lancar em Conta encerrada - INC0148355';
+ vc_dstransaRecarga   CONSTANT VARCHAR2(4000) := 'Baixa da Recarga de Celular pendente a lancar em Conta encerrada - INC0148355';
  vc_cdmotest          CONSTANT cecred.craplat.cdmotest%TYPE := 3;
  vc_insitlat          CONSTANT cecred.craplat.insitlat%TYPE := 3;
  vc_cdopeest          CONSTANT cecred.craplat.cdopeest%TYPE := 1;
+ vc_insit_ope_cancelado CONSTANT cecred.craplat.cdopeest%TYPE := 4;
  
  CURSOR cr_tar_fut(pr_cdcooper in cecred.crapcop.cdcooper%type,
                    pr_nrdconta in cecred.crapass.nrdconta%type) IS
@@ -29,6 +31,16 @@ declare
     WHERE craplat.cdcooper = pr_cdcooper  
       AND craplat.nrdconta = pr_nrdconta 
       AND craplat.insitlat = 1;
+      
+ CURSOR cr_recarga_fut(pr_cdcooper in cecred.crapcop.cdcooper%type,
+                       pr_nrdconta in cecred.crapass.nrdconta%type) IS
+   SELECT topr.idoperacao
+         ,topr.insit_operacao
+     FROM tbrecarga_operacao topr
+    WHERE topr.cdcooper = pr_cdcooper
+      AND topr.nrdconta = pr_nrdconta
+      AND topr.insit_operacao = 1;
+      
 
 Begin
   vr_dttransa    := trunc(sysdate);
@@ -53,7 +65,7 @@ Begin
                            pr_cdoperad => vr_cdoperad,
                            pr_dscritic => vr_dscritic,
                            pr_dsorigem => 'AIMARO',
-                           pr_dstransa => vc_dstransa,
+                           pr_dstransa => vc_dstransaTar,
                            pr_dttransa => vr_dttransa,
                            pr_flgtrans => 1,
                            pr_hrtransa => vr_hrtransa,
@@ -107,12 +119,52 @@ Begin
        and lat.cdlantar = rw_tar_fut.cdlantar;
    
   END LOOP;      
+
+  vr_cdcooper    := 16;
+  vr_nrdconta    := 448540;
+
+
+  FOR rw_recarga_fut in cr_recarga_fut(vr_cdcooper, vr_nrdconta) LOOP
+   
+    vr_nrdrowid := null;
+    
+    CECRED.GENE0001.pc_gera_log(pr_cdcooper => vr_cdcooper,
+                           pr_cdoperad => vr_cdoperad,
+                           pr_dscritic => vr_dscritic,
+                           pr_dsorigem => 'AIMARO',
+                           pr_dstransa => vc_dstransaRecarga,
+                           pr_dttransa => vr_dttransa,
+                           pr_flgtrans => 1,
+                           pr_hrtransa => vr_hrtransa,
+                           pr_idseqttl => 0,
+                           pr_nmdatela => NULL,
+                           pr_nrdconta => vr_nrdconta,
+                           pr_nrdrowid => vr_nrdrowid);
+                           
+    CECRED.GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                                     pr_nmdcampo => 'tbrecarga_operacao.IDOPERACAO',
+                                     pr_dsdadant => rw_recarga_fut.idoperacao,
+                                     pr_dsdadatu => rw_recarga_fut.idoperacao);
+                                     
+    CECRED.GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                                     pr_nmdcampo => 'tbrecarga_operacao.INSIT_OPERACAO',
+                                     pr_dsdadant => rw_recarga_fut.insit_operacao,
+                                     pr_dsdadatu => vc_insit_ope_cancelado);
+                                     
+
+    UPDATE cecred.tbrecarga_operacao
+       SET insit_operacao = vc_insit_ope_cancelado
+     WHERE cdcooper = vr_cdcooper
+       AND nrdconta = vr_nrdconta
+       AND idoperacao = rw_recarga_fut.idoperacao;
+   
+  END LOOP;   
   
   COMMIT;
   
 Exception
   when others then
-    RAISE_APPLICATION_ERROR(-20000,'Erro baixar lancamento futuro (craplat): ' || sqlerrm);
+    RAISE_APPLICATION_ERROR(-20000,'Erro baixar lancamento futuro da Cooperativa/Conta: ' || vr_cdcooper || '/' || vr_nrdconta || ' - ' || sqlerrm);
 end; 
  
  
