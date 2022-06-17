684b3f1753157fd045cad75753195c35e59a18f1
Tiago Machado Flor <tiago@ailos.coop.br>
2022-06-17 08:12:16 -0300

    Prefixo namespace tabelas
    Prefixo namespace tabelas

--- 2022/06/baca_descontinuar_prg.sql
+++ 2022/06/baca_descontinuar_prg.sql
@@ -1,17 +1,17 @@
 ﻿DECLARE
 
-  vr_nrsolici crapprg.NRSOLICI%TYPE;
-  vr_nrordprg crapprg.NRORDPRG%TYPE;
-  vr_cdcooper crapcop.cdcooper%TYPE := 0;
+  vr_nrsolici CECRED.crapprg.NRSOLICI%TYPE;
+  vr_nrordprg CECRED.crapprg.NRORDPRG%TYPE;
+  vr_cdcooper CECRED.crapcop.cdcooper%TYPE := 0;
 
   CURSOR cr_crapcop IS
     SELECT cdcooper
-      FROM crapcop c
+      FROM CECRED.crapcop c
      WHERE c.FLGATIVO = 1;
 
   CURSOR cr_crapprg(pr_cdcooper crapcop.cdcooper%TYPE) IS
     SELECT *
-      FROM crapprg p
+      FROM CECRED.crapprg p
      WHERE p.CDPROGRA IN ('CRPS023', 'CRPS188', 'CRPS244',
                           'CRPS602', 'CRPS253', 'CRPS521',
                           'CRPS300', 'CRPS282', 'CRPS457',
@@ -38,7 +38,7 @@ BEGIN
       
       vr_nrsolici := TO_NUMBER( '9' || NVL(vr_nrordprg,0) || LPAD(rw_crapprg.nrsolici,3,'0') );
 
-      UPDATE crapprg p
+      UPDATE CECRED.crapprg p
          SET p.NRSOLICI = vr_nrsolici
            , p.INLIBPRG = 2
        WHERE p.CDCOOPER = rw_crapcop.cdcooper
