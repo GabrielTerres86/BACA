c45259e18614b3f81b04ea9bbf1f934ea35fe56e
Djonata Wehmuth <djonata.wehmuth@ailos.coop.br>
2022-06-21 10:32:21 -0300

    Update alterar_conta_do_cartao.sql

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -7,36 +7,10 @@ DECLARE
 
 BEGIN
 
-  vr_cooperativa := 9;
-  vr_conta := 14010;
-  vr_cartao := 6393500065179583;
-
-  FOR cartao IN (select distinct a.nrcpftit
-                  from crawcrd a
-                 where a.cdcooper = vr_cooperativa
-                   and a.nrdconta = vr_conta
-                   and a.insitcrd = 4
-                 ) LOOP
-    vr_cpf_titular := cartao.nrcpftit;
-
-  END LOOP;
-  
-  IF vr_cpf_titular IS NULL THEN
-    FOR titular IN (select a.nrcpfcgc
-                      from crapttl a
-                     where a.cdcooper = vr_cooperativa
-                       and a.nrdconta = vr_conta
-                       and a.idseqttl = 1
-                   ) LOOP
-      vr_cpf_titular := titular.nrcpfcgc;
-
-    END LOOP;
-
-  END IF;
-
-  IF vr_cpf_titular IS NULL THEN
-    RETURN;
-  END IF;
+  vr_cooperativa := 1;
+  vr_conta := 329;
+  vr_cartao := 5588190184171591;
+  vr_cpf_titular := 03032094968;
 
   UPDATE crapcrd card
      SET card.cdcooper = vr_cooperativa
@@ -44,14 +18,14 @@ BEGIN
         ,card.nrcpftit = vr_cpf_titular
         ,card.qtsenerr = 0
         ,card.inacetaa = 1
-        ,card.NRCTRCRD = 99999999
+        ,card.NRCTRCRD = 99999998
    WHERE card.nrcrcard = vr_cartao;
 
   UPDATE crawcrd card
      SET card.cdcooper = vr_cooperativa
         ,card.nrdconta = vr_conta
         ,card.nrcpftit = vr_cpf_titular
-        ,card.NRCTRCRD = 99999999
+        ,card.NRCTRCRD = 99999998
    WHERE card.nrcrcard = vr_cartao;
 
   COMMIT;
