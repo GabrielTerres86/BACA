9882a367904ab48b37df170388c51cd1bb9aa363
Rodrigo Lima <capgemini-rodrigo.erasmo@ailos.coop.br>
2022-06-21 09:26:58 -0300

    Alteração para ativado = N

--- 2022/05/prj0024441-parametro.sql
+++ 2022/05/prj0024441-parametro.sql
@@ -1,13 +1,12 @@
 BEGIN
 
 	INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm) 
-                 VALUES ('CRED', 0, 'MLC_ATIVO', 'Flag que determina se o fluxo de modernização de liquidação da cobrança (MLC) está ativo', 'S');
+                 VALUES ('CRED', 0, 'MLC_ATIVO', 'Flag que determina se o fluxo de modernização de liquidação da cobrança (MLC) está ativo', 'N');
 
   COMMIT;
 
 EXCEPTION
   WHEN OTHERS THEN
-    ROLLBACK;
     SISTEMA.excecaoInterna(pr_compleme => 'PRJ0024441');
-
+    ROLLBACK;
 END;
