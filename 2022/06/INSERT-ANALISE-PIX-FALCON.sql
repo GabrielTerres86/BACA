d4aacd9cc6559c9140636291f4884746f703ec12
Alana Ribeiro Ramos Sasse <premiersoft-alana.sasse@ailos.coop.br>
2022-06-23 17:05:49 -0300

    Status para analise de pix recebido

---
+++ 2022/06/INSERT-ANALISE-PIX-FALCON.sql
@@ -0,0 +1,27 @@
+INSERT INTO tbgen_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
+VALUES
+('STATUS_ANALISE_PIX_RECEBIDO', 1, 'Aguardando análise');
+/
+INSERT INTO tbgen_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
+VALUES
+('STATUS_ANALISE_PIX_RECEBIDO', 2, 'Recebimento liberado');
+/
+INSERT INTO tbgen_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
+VALUES
+('STATUS_ANALISE_PIX_RECEBIDO', 3, 'Recebimento bloqueado para analise');
+/
+INSERT INTO tbgen_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
+VALUES
+('STATUS_ANALISE_PIX_RECEBIDO', 4, 'Recebimento rejeitado aguardando devolução');
+/
+INSERT INTO tbgen_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
+VALUES
+('STATUS_ANALISE_PIX_RECEBIDO', 5, 'Recebimento rejeitado, transação devolvida a origem');
+/
+INSERT INTO tbgen_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
+VALUES
+('STATUS_ANALISE_PIX_RECEBIDO', 6, 'Liberado por expiração do tempo de análise');
+/
+INSERT INTO tbgen_dominio_campo (NMDOMINIO, CDDOMINIO, DSCODIGO)
+VALUES
+('STATUS_ANALISE_PIX_RECEBIDO', 7, 'Não enviado por falta de informações');
