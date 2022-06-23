c4a45c8574d199d1425bbcdd818bdfa5b979ae64
Douglas <douglas.kloppel@ailos.coop.br>
2022-06-23 08:47:54 -0300

    Mascara data

--- 2022/06/update-saldo-ci.sql
+++ 2022/06/update-saldo-ci.sql
@@ -3,7 +3,7 @@ BEGIN
      SET vlsddisp = 278843.18
    WHERE cdcooper = 1 
      AND nrdconta = 6301282 
-     AND dtrefere = '30/06/2022';
+     AND dtrefere = to_date('30/06/2022','dd/mm/yyyy');
      
    COMMIT;
 END;
