afce9ef2a6a26432a026ab4c1b6d09f443d37391
Douglas <douglas.kloppel@ailos.coop.br>
2022-06-27 11:40:32 -0300

    atualizar saldo para realizar testes

--- 2022/06/update-sdaTest.sql
+++ 2022/06/update-sdaTest.sql
@@ -3,6 +3,6 @@ BEGIN
      set vlsddisp = 2000000
    where cdcooper = 1
      and nrdconta = 834300
-     and dtmvtolt = '30/06/2022';
+     and dtmvtolt = to_date('30/06/2022','dd/mm/yyyy');
   COMMIT;
 END;        
