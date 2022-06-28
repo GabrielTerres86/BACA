43e5f92b7b9cfa4dd654782c02360f585b69d690
Jose Jair Dill Junior (Mouts) <t0032120@ailos.coop.br>
2022-06-28 12:34:04 -0300

    Nova critica de devolucao

---
+++ 2022/06/PRJ00244441_Insert_Critica_Devolucao.sql
@@ -0,0 +1,9 @@
+Declare
+
+Begin
+  Insert into crapcri (CDCRITIC, DSCRITIC, PROGRESS_RECID, TPCRITIC, FLGCHAMA)
+  values (1514, '1514 - Repasse em Duplicidade pela IF Recebedora', 4370, 4, 0);
+
+  commit;
+End;
+  
