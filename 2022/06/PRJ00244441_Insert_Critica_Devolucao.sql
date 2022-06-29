edeb7e6cf958ed77440a80ebea845729225f1054
Jose Jair Dill Junior (Mouts) <t0032120@ailos.coop.br>
2022-06-29 13:37:43 -0300

    Ajustes

--- 2022/06/PRJ00244441_Insert_Critica_Devolucao.sql
+++ 2022/06/PRJ00244441_Insert_Critica_Devolucao.sql
@@ -1,9 +1,18 @@
-Declare
-
-Begin
-  Insert into crapcri (CDCRITIC, DSCRITIC, PROGRESS_RECID, TPCRITIC, FLGCHAMA)
-  values (1514, '1514 - Repasse em Duplicidade pela IF Recebedora', 4370, 4, 0);
-
-  commit;
-End;
+BEGIN
+  INSERT INTO crapcri
+    (CDCRITIC
+    ,DSCRITIC
+    ,TPCRITIC
+    ,FLGCHAMA)
+  VALUES
+    (1514
+    ,'1514 - Repasse em Duplicidade pela IF Recebedora'
+    ,4
+    ,0);
+  COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    SISTEMA.excecaoInterna(pr_compleme => 'PRJ0024441');
+    ROLLBACK;
+END;
   
