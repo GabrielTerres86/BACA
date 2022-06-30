06795ae203b201cf8ebee5c1a24981db10e854c7
Darlei Fernando Zillmer <darlei.zillmer@premiersoft.net>
2022-06-30 15:44:53 -0300

    Adicionada modalidade na ocr

--- 2022/06/data-risco-imobiliario.sql
+++ 2022/06/data-risco-imobiliario.sql
@@ -17,6 +17,7 @@ DECLARE
      WHERE o.cdcooper = pr_cdcooper
        AND o.nrdconta = pr_nrdconta
        AND o.nrctremp = pr_nrctremp
+       and o.cdmodali = 901
        AND o.dtdrisco IS NOT NULL
        AND rownum = 1
      ORDER BY o.dtrefere DESC;
@@ -41,14 +42,14 @@ BEGIN
     FETCH cr_tbrisco_ocr INTO rw_tbrisco_ocr;
     IF cr_tbrisco_ocr%FOUND THEN
       UPDATE crapris SET dtdrisco = rw_tbrisco_ocr.dtdrisco WHERE cdcooper = rw_crapris.cdcooper AND nrdconta = rw_crapris.nrdconta AND nrctremp = rw_crapris.nrctremp AND cdmodali = 901 AND dtdrisco IS NULL;
-      UPDATE tbrisco_central_ocr SET dtdrisco = rw_tbrisco_ocr.dtdrisco WHERE cdcooper = rw_crapris.cdcooper AND nrdconta = rw_crapris.nrdconta AND nrctremp = rw_crapris.nrctremp AND dtdrisco IS NULL;
+      UPDATE tbrisco_central_ocr SET dtdrisco = rw_tbrisco_ocr.dtdrisco WHERE cdcooper = rw_crapris.cdcooper AND nrdconta = rw_crapris.nrdconta AND nrctremp = rw_crapris.nrctremp AND cdmodali = 901 AND dtdrisco IS NULL;
     ELSE
       OPEN cr_crapris_min(pr_cdcooper => rw_crapris.cdcooper
                          ,pr_nrdconta => rw_crapris.nrdconta
                          ,pr_nrctremp => rw_crapris.nrctremp);
       FETCH cr_crapris_min INTO rw_crapris_min;
       UPDATE crapris SET dtdrisco = rw_crapris_min.dtrefere WHERE cdcooper = rw_crapris.cdcooper AND nrdconta = rw_crapris.nrdconta AND nrctremp = rw_crapris.nrctremp AND cdmodali = 901 AND dtdrisco IS NULL;
-      UPDATE tbrisco_central_ocr SET dtdrisco = rw_crapris_min.dtrefere WHERE cdcooper = rw_crapris.cdcooper AND nrdconta = rw_crapris.nrdconta AND nrctremp = rw_crapris.nrctremp AND dtdrisco IS NULL;
+      UPDATE tbrisco_central_ocr SET dtdrisco = rw_crapris_min.dtrefere WHERE cdcooper = rw_crapris.cdcooper AND nrdconta = rw_crapris.nrdconta AND nrctremp = rw_crapris.nrctremp AND cdmodali = 901 AND dtdrisco IS NULL;
       CLOSE cr_crapris_min;
     END IF;
     CLOSE cr_tbrisco_ocr;
