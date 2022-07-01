cdf79d5baae5740c6c4e5da93f23f2af3707bcdc
Guilherme Pinheiro <guilherme.pinheiro@mouts.com.br>
2022-07-01 13:35:05 -0300

    v3

--- 2022/06/inc0149295-baca.sql
+++ 2022/06/inc0149295-baca.sql
@@ -16,7 +16,7 @@ DECLARE
        AND e.inprejuz = 0
        AND w.flgreneg = 1
        AND e.tpemprst = 1
-       AND e.dtmvtolt >= to_date('30/06/2022','dd/mm/yyyy')
+       AND e.dtmvtolt >= to_date('01/07/2022','dd/mm/yyyy')
        AND e.nrctremp = w.nrctrliq##1;
 
 BEGIN
