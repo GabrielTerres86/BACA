7dbf6df55f288b729d08d762a2a39a5c5f5473c9
Guilherme Pinheiro <guilherme.pinheiro@mouts.com.br>
2022-07-01 14:30:12 -0300

    v5

--- 2022/06/inc0149295-baca.sql
+++ 2022/06/inc0149295-baca.sql
@@ -7,22 +7,22 @@ DECLARE
           ,e.dtmvtolt
           ,w.nrctrliq##1
           ,w.nrctrliq##2
-      FROM crapepr e
-      JOIN crawepr w
+      FROM cecred.crapepr e
+      JOIN cecred.crawepr w
         ON w.cdcooper = e.cdcooper
        AND w.nrdconta = e.nrdconta
        AND w.nrctremp = e.nrctremp
      WHERE e.inliquid = 0
        AND e.inprejuz = 0
        AND w.flgreneg = 1
-       AND e.dtmvtolt >= to_date('01/07/2022','dd/mm/yyyy')
+       AND e.dtmvtolt >= to_date('01/07/2022', 'dd/mm/yyyy')
        AND e.nrctremp = w.nrctrliq##1;
 
 BEGIN
 
   FOR rw_reneg IN cr_reneg LOOP
   
-    UPDATE crawepr w
+    UPDATE cecred.crawepr w
        SET w.nrctrliq##1  = 0
           ,w.nrctrliq##2  = 0
           ,w.nrctrliq##3  = 0
