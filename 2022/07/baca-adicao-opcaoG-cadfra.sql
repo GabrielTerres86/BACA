78b3fff649e9a4a07c986c7759f983019f337115
Carlos Vinicius Pinho Moraes <carlos.moraes@compasso.com.br>
2022-07-06 15:07:58 -0300

    baca para adicionar a opção G na tela cadfra

---
+++ 2022/07/baca-adicao-opcaoG-cadfra.sql
@@ -0,0 +1,10 @@
+begin
+
+
+INSERT INTO crapace (nmdatela,cddopcao,cdoperad,nmrotina,cdcooper,nrmodulo,idevento,idambace) 
+VALUES ('CADFRA', 'G', 'F0031251', ' ','3', '1', '1', '2');
+
+commit;
+end;
+
+
