5178a8f64b8e60135b3ceed3938cc38e24d03450
Filipe Ribeiro <mouts-filipe.rego@ailos.coop.br>
2022-07-07 11:08:36 -0300

    Ativação do contributário na credifoz

---
+++ 2022/07/seguro_pessoa_ritm0227782.sql
@@ -0,0 +1,25 @@
+BEGIN
+  UPDATE cecred.crapprm p
+     SET p.dsvlrprm = 0
+   WHERE p.cdcooper = 11
+     AND p.cdacesso = 'TPCUSTEI_PADRAO';
+
+  UPDATE cecred.craplcr SET tpcuspr = 0 WHERE cdcooper = 11;
+
+  COMMIT;
+END;
+/
+
+BEGIN
+  INSERT INTO crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+    VALUES('CRED', 9, 'DIA_ATIVA_CONTRB_SEGPRE', 'Dia da ativação das linhas de credito contributario', '25/04/2022');
+    
+  INSERT INTO crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+    VALUES('CRED', 13, 'DIA_ATIVA_CONTRB_SEGPRE', 'Dia da ativação das linhas de credito contributario', '25/04/2022');
+  
+  INSERT INTO crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+    VALUES('CRED', 11, 'DIA_ATIVA_CONTRB_SEGPRE', 'Dia da ativação das linhas de credito contributario', TO_CHAR(SYSDATE,'DD/MM/YYYY'));
+    
+  COMMIT;
+END;
+/
