939ab76f94ea384ed6d02e949bb8d618ec0a2f9a
Alessandro Hoffelder <alessandro.hoffelder@ailos.coop.br>
2022-07-07 16:19:18 +0000

    Updated seguro_pessoa_ritm0227782.sql

--- 2022/07/seguro_pessoa_ritm0227782.sql
+++ 2022/07/seguro_pessoa_ritm0227782.sql
@@ -11,14 +11,14 @@ END;
 /
 
 BEGIN
-  INSERT INTO crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
-    VALUES('CRED', 9, 'DIA_ATIVA_CONTRB_SEGPRE', 'Dia da ativação das linhas de credito contributario', '25/04/2022');
+  INSERT INTO cecred.crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+    VALUES('CRED', 9, 'DIA_ATIVA_CONTRB_SEGPRE', 'Dia da ativaÃ§Ã£o das linhas de credito contributario', '25/04/2022');
     
-  INSERT INTO crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
-    VALUES('CRED', 13, 'DIA_ATIVA_CONTRB_SEGPRE', 'Dia da ativação das linhas de credito contributario', '25/04/2022');
+  INSERT INTO cecred.crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+    VALUES('CRED', 13, 'DIA_ATIVA_CONTRB_SEGPRE', 'Dia da ativaÃ§Ã£o das linhas de credito contributario', '25/04/2022');
   
-  INSERT INTO crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
-    VALUES('CRED', 11, 'DIA_ATIVA_CONTRB_SEGPRE', 'Dia da ativação das linhas de credito contributario', TO_CHAR(SYSDATE,'DD/MM/YYYY'));
+  INSERT INTO cecred.crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+    VALUES('CRED', 11, 'DIA_ATIVA_CONTRB_SEGPRE', 'Dia da ativaÃ§Ã£o das linhas de credito contributario', TO_CHAR(SYSDATE,'DD/MM/YYYY'));
     
   COMMIT;
 END;
