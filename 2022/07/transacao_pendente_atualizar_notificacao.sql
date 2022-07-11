824626b0d8dec8d0f33d6ae2f009ce81f3078300
Frank Besen <frankbesen@gmail.com>
2022-07-11 09:44:44 -0300

    Alterado o enconding do arquivo

--- 2022/07/transacao_pendente_atualizar_notificacao.sql
+++ 2022/07/transacao_pendente_atualizar_notificacao.sql
@@ -1,17 +1,17 @@
 BEGIN
   update tbgen_notif_msg_cadastro n
    set n.inexibe_botao_acao_mobile = 1,
-       n.dstexto_botao_acao_mobile = 'TransaÃ§Ãµes Pendentes',
+       n.dstexto_botao_acao_mobile = 'Transações Pendentes',
        n.cdmenu_acao_mobile        = 204,
-       n.dslink_acao_mobile        = 'ServiÃ§os > TransaÃ§Ãµes Pendentes',
+       n.dslink_acao_mobile        = 'Serviços > Transações Pendentes',
 	   n.inenviar_push = 1,
        n.dshtml_mensagem = '<p>#nomecompleto</p>
 
-<p>VocÃª possui uma nova transaÃ§Ã£o pendente de aprovaÃ§Ã£o</p>
+<p>Você possui uma nova transação pendente de aprovação</p>
 
 <p>#dsdmensg</p>
 
-<p>Esta transaÃ§Ã£o estarÃ¡ disponÃ­vel para aprovaÃ§Ã£o de todos os representantes atÃ© as 23:00h de #datatransacao, caso nÃ£o seja aprovada serÃ¡ cancelada. Acesse <b>ServiÃ§os > TransaÃ§Ãµes Pendentes</b></p>'
+<p>Esta transação estará disponível para aprovação de todos os representantes até as 23:00h de #datatransacao, caso não seja aprovada será cancelada. Acesse <b>Serviços > Transações Pendentes</b></p>'
  where cdorigem_mensagem = 6
    and cdmensagem = 271;
  COMMIT;
