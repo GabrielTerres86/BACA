8f86f28f08fa0739ea83c4ca7a0f5f045affb5a3
Tiago Machado Flor <tiago@ailos.coop.br>
2022-07-14 11:14:16 -0300

    correcao de branch
    correcao de branch

--- 2022/07/baca_paralelo_crps685.sql
+++ 2022/07/baca_paralelo_crps685.sql
@@ -9,4 +9,4 @@
                               ,1
                               ,'CRPS685'); 
                               
-COMMIT;                              
+COMMIT;                               
