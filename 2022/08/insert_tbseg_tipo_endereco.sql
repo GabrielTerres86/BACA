eeb1f1bdeacffca54afec1331a26ac2ad568fb01
Lucas Eduardo Ranghetti <lucas.ranghetti@ailos.coop.br>
2022-07-15 16:36:34 -0300

    Scripts melhorias seguro residencial

---
+++ 2022/08/insert_tbseg_tipo_endereco.sql
@@ -0,0 +1,53 @@
+begin 
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'AEROPORTO','AE', 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'ALAMEDA','AL', 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'AREA', 'AR' , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'AVENIDA', 'AV' , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'CHACARA', 'CH', 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'CONJUNTO', 'CJ'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'COLONIA', 'CL'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'CAMPO', 'CM'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'CONDOMINIO', 'CO'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'DISTRITO', 'DT'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'ESPLANADA', 'EP'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'ESTRADA', 'ES'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'ESTACAO', 'ET'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'FEIRA', 'FE'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'FAVELA', 'FV'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'FAZENDA', 'FZ'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'JARDIM', 'JD'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'LAGO', 'LA'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'LADEIRA', 'LD'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'LAGOA', 'LO'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'LARGO', 'LG'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'LOTEAMENTO', 'LT'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'MORRO', 'MO'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'NUCLEO', 'NU'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'OUTROS', 'OU'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'PRACA', 'PC'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'PARQUE', 'PQ'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'PASSARELA', 'PS'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'PATIO', 'PT'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'QUADRA', 'QD'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'RUA', 'R.'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'RECANTO', 'RC'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'RODOVIA', 'RD'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'RESIDENCIAL', 'RS'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'SITIO', 'S.'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'SETOR', 'ST'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'TRECHO', 'TC'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'TRAVESSA', 'TV'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'TREVO', 'TO'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'VIA', 'V.'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'VIADUTO', 'VD'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'VILA', 'VI'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'VALE', 'VL'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'VIELA', 'VE'  , 1 );
+insert into seguro.tbseg_tipo_endereco(dstipo_endereco, dstipo_endereco_parceiro, flstatus_tipo_endereco)values(  'VEREDA', 'VR'  , 1 );
+commit;
+exception
+  when others then
+    rollback;
+end;
+
+  
