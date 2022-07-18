001a29671bd99aac7296fb6bc1f925dd3417e6c4
Renato Darosci <premiersoft-renato.darosci@gmail.com>
2022-07-18 11:59:03 -0300

    Bloqueios da atualização cadastral via Aimaro - gerar registros da CRAPACA

---
+++ 2022/07/PRJ0023375_add_CRAPACA.sql
@@ -0,0 +1,29 @@
+DECLARE
+  
+  CURSOR cr_craprdr IS
+    SELECT t.nrseqrdr
+      FROM craprdr t
+     WHERE t.NMPROGRA = 'CONTAS';
+  
+  vr_nrseqrdr craprdr.nrseqrdr%TYPE;
+  
+BEGIN
+  
+  OPEN  cr_craprdr;
+  FETCH cr_craprdr INTO vr_nrseqrdr;
+  
+  IF cr_craprdr%NOTFOUND THEN
+  
+	INSERT INTO craprdr(nmprogra,dtsolici)
+		   VALUES('CONTAS', SYSDATE)
+		   RETURNING craprdr.nrseqrdr INTO vr_nrseqrdr;
+  
+  END IF;
+  
+  CLOSE cr_craprdr;
+
+  INSERT INTO crapaca (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr) VALUES ('PERMITE_ATUALIZACAO_CADASTRAL', NULL, 'validarAtlzCadastralAimaro', 'pr_inpessoa,pr_nrdconta', vr_nrseqrdr);
+  
+  COMMIT;
+  
+END;
