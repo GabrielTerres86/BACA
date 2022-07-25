e32422f699274293013d2f149a1086d9f288b4e3
Filipe Ribeiro <mouts-filipe.rego@ailos.coop.br>
2022-07-25 16:19:15 -0300

    PRJ0022983 Entregável 5

---
+++ 2022/07/seguro_pessoa_crawseg_flgsegma.sql
@@ -0,0 +1,8 @@
+BEGIN
+  UPDATE crapaca c
+     SET c.lstparam = lstparam || ',pr_flgsegma'
+   WHERE c.nmpackag = 'TELA_ATENDA_SEGURO'
+     AND c.nmdeacao = 'ATUALIZA_PROPOSTA_PREST';   
+  COMMIT;
+END;
+/
