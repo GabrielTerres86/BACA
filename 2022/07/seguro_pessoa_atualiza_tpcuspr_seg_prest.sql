92a915122bf1b15b44e7335d006afc01cd95f127
Alessandro Hoffelder <alessandro.hoffelder@ailos.coop.br>
2022-07-25 13:10:15 +0000

    Updated seguro_pessoa_atualiza_tpcuspr_seg_prest.sql

--- 2022/07/seguro_pessoa_atualiza_tpcuspr_seg_prest.sql
+++ 2022/07/seguro_pessoa_atualiza_tpcuspr_seg_prest.sql
@@ -1,8 +1,8 @@
 begin
-	UPDATE craplcr
-	   SET tpcuspr = 1
-	 WHERE flgsegpr = 0 
-	   AND tpcuspr = 0;
+	UPDATE cecred.craplcr a
+	   SET a.tpcuspr = 1
+	 WHERE a.flgsegpr = 0 
+	   AND a.tpcuspr = 0;
   commit;
 end;
 /
