f0ef1419d8748e297c29bf2ff2fdc2dc0d3c6561
Alessandro Hoffelder <alessandro.hoffelder@ailos.coop.br>
2022-07-26 11:55:35 +0000

    Updated seguro_pessoa_proposta_api_icatu.sql

--- 2022/07/seguro_pessoa_proposta_api_icatu.sql
+++ 2022/07/seguro_pessoa_proposta_api_icatu.sql
@@ -1,22 +1,22 @@
 BEGIN
   FOR rw_cdcooper IN (SELECT p.cdcooper
-                        FROM crapcop p
+                        FROM cecred.crapcop p
                        WHERE p.cdcooper <> 3
                          AND p.flgativo = 1) LOOP
-    INSERT INTO crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
-      VALUES('CRED', rw_cdcooper.cdcooper, 'PROPOSTA_API_ICATU', 'Chave para ativar a utilização da API Icatu online', 'N');
+    INSERT INTO cecred.crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+      VALUES('CRED', rw_cdcooper.cdcooper, 'PROPOSTA_API_ICATU', 'Chave para ativar a utilizaÃ§Ã£o da API Icatu online', 'N');
   END LOOP;
   COMMIT;
 END;
 /
 BEGIN
-  INSERT INTO crapcri(cdcritic,dscritic,tpcritic,flgchama)
+  INSERT INTO cecred.crapcri(cdcritic,dscritic,tpcritic,flgchama)
   VALUES (5050,'Aguardando retorno do numero de proposta da seguradora. Aguarde!',4,0);
   COMMIT;
 END;
 /
 BEGIN
-  INSERT INTO crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  INSERT INTO cecred.crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
       VALUES('CRED', 0, 'PROPOSTA_API_ICATU_TEMPO', 'Tempo em minutos de aguardo do retorno da API', '3');
   COMMIT;
 END;
