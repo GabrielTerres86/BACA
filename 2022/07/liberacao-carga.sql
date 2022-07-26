adbbf8ae795fff619e05867ba889f9ce0f9c1e97
Matheus <matheus.leriano@ailos.coop.br>
2022-07-26 09:11:38 -0300

    ajuste dia

--- 2022/07/liberacao-carga.sql
+++ 2022/07/liberacao-carga.sql
@@ -1,6 +1,6 @@
 begin
 update credito.TBCRED_PREAPROV b
-set b.dtliberacao = to_date('03/04/2022','dd/mm/yyyy')
+set b.dtliberacao = to_date('01/07/2022','dd/mm/yyyy')
 where b.idcarga = 182;
 commit;
 end;
