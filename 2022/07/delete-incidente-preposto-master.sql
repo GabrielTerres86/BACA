685236c84b546184c634599d1443f1aa5c0735f5
Andrey Leonardo Bauler <t0033422@ailos.coop.br>
2022-07-27 16:36:02 -0300

    inclusão de owner da tabela

--- 2022/07/delete-incidente-preposto-master.sql
+++ 2022/07/delete-incidente-preposto-master.sql
@@ -1,5 +1,5 @@
 begin
-	delete tbcc_limite_preposto 
+	delete cecred.tbcc_limite_preposto 
 	where (nrdconta = 9945032  and nrcpf = 3952238902 and cdcooper = 1) 
 	   or (nrdconta = 14750732 and nrcpf = 3436697907 and cdcooper = 1) 
 	   or (nrdconta = 14751151 and nrcpf = 3436697907 and cdcooper = 1) 
