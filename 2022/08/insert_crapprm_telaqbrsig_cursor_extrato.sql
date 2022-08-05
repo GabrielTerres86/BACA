2b80b26f427e5eab2a3e6901b953cd7ab2d76604
Daniel Lombardi <t0032628@ailos.coop.br>
2022-08-05 10:43:10 -0300

    Quebra 954 - Parametro de Liga e desliga da função.

---
+++ 2022/08/insert_crapprm_telaqbrsig_cursor_extrato.sql
@@ -0,0 +1,8 @@
+BEGIN
+  INSERT INTO crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+  VALUES ('CRED', 0, 'QBRSIG_EXTRATO', 'Parâmetro para ligar e desligar novo cursor na criação de registros da tabela tbjur_qbr_sig_extrato.', 1);
+
+  COMMIT;
+END;
+
+
