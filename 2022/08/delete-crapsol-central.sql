687d6e1f37e325848ee28b03ba7ede06d43705f6
paulo <paulo.kowalsky@ailos.coop.br>
2022-08-08 16:52:11 -0300

    delete todas solicitacoes

--- 2022/08/delete-crapsol-central.sql
+++ 2022/08/delete-crapsol-central.sql
@@ -1,4 +1,4 @@
 begin
-delete cecred.crapsol s where s.cdcooper = 3 and s.dtrefere = to_date('13/09/2022','DD/MM/YYYY');
+delete cecred.crapsol s where s.dtrefere = to_date('13/09/2022','DD/MM/YYYY');
 commit;
 end;
