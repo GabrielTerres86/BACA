0e5b882d76a4d544c6ae405e6ac99c8a92b688ec
Frank Besen <frankbesen@gmail.com>
2022-08-08 13:41:15 -0300

    remove as notificacoes de transacoes pix criadas indevidas

--- 2022/07/transacao_pendente_atualizar_notificacao.sql
+++ 2022/07/transacao_pendente_atualizar_notificacao.sql
@@ -1,18 +1,10 @@
 BEGIN
-  update tbgen_notif_msg_cadastro n
-   set n.inexibe_botao_acao_mobile = 1,
-       n.dstexto_botao_acao_mobile = 'Transações Pendentes',
-       n.cdmenu_acao_mobile        = 204,
-       n.dslink_acao_mobile        = 'Serviços > Transações Pendentes',
-	   n.inenviar_push = 1,
-       n.dshtml_mensagem = '<p>#nomecompleto</p>
+delete from TBGEN_NOTIF_AUTOMATICA_PRM WHERE CDORIGEM_MENSAGEM = 6 AND CDMOTIVO_MENSAGEM >= 50;
+delete from TBGEN_NOTIF_MSG_CADASTRO WHERE CDORIGEM_MENSAGEM = 6 and (cdmensagem = 8553 or cdmensagem = 8554);
 
-<p>Você possui uma nova transação pendente de aprovação</p>
+COMMIT;
 
-<p>#dsdmensg</p>
-
-<p>Esta transação estará disponível para aprovação de todos os representantes até as 23:00h de #datatransacao, caso não seja aprovada será cancelada. Acesse <b>Serviços > Transações Pendentes</b></p>'
- where cdorigem_mensagem = 6
-   and cdmensagem = 271;
- COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK; 
 END;
