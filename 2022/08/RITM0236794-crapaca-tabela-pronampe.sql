b9c24c07b98c17a734da52843d5bfbaf2473e3a2
Luan Zambinati Manara <amcom-luan.manara@ailos.coop.br>
2022-08-08 18:24:14 -0300

    Nova opção A tela PRONAM - Consultar Receita Federal - Mensageria

---
+++ 2022/08/RITM0236794-crapaca-tabela-pronampe.sql
@@ -0,0 +1,22 @@
+BEGIN
+  INSERT INTO crapaca
+    (NMDEACAO,
+     NMPACKAG,
+     NMPROCED,
+     LSTPARAM,
+     NRSEQRDR)
+  VALUES
+    ('COSULTA_FATURAMENTO_RECEITA',
+     'TELA_PRONAM',
+     'pc_consultar_fat_rec_web',
+     'pr_cdcooper, pr_nrdconta, pr_nriniseq, pr_nrregist',
+     (SELECT NRSEQRDR
+        FROM craprdr
+       WHERE upper(NMPROGRA) = 'TELA_PRONAM'));
+	   
+  COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+	RAISE_APPLICATION_ERROR(-20500, SQLERRM);
+    ROLLBACK;
+END;
