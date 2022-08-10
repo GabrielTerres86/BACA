baebf2182bcb86c4a7151594ebc4c133efe66009
Marcus Abreu <premiersoft-marcus.abreu@ailos.coop.br>
2022-08-10 16:54:27 -0300

    PRJ0024479 - Inclusao do script contendo os dominios:  -> TIPOSCHAVEPIX  -> TIPOCONTA

---
+++ 2022/10/Inserts_TBCC_DOMINIO_CAMPO.sql
@@ -0,0 +1,86 @@
+DECLARE
+  vr_code NUMBER;
+  vr_errm VARCHAR2(64);
+BEGIN
+  
+  INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T
+  (nmdominio
+  ,cddominio
+  ,dscodigo)
+  VALUES
+  ('TIPOSCHAVEPIX'
+  ,'CPF'
+  ,'1');
+  
+  INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T
+    (nmdominio
+    ,cddominio
+    ,dscodigo)
+  VALUES
+    ('TIPOSCHAVEPIX'
+    ,'CNPJ'
+    ,'1');
+    
+  INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T
+    (nmdominio
+    ,cddominio
+    ,dscodigo)
+  VALUES
+    ('TIPOSCHAVEPIX'
+    ,'EMAIL'
+    ,'2'); 
+
+  INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T
+    (nmdominio
+    ,cddominio
+    ,dscodigo)
+  VALUES
+    ('TIPOSCHAVEPIX'
+    ,'PHONE'
+    ,'3');    
+       
+  INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T
+    (nmdominio
+    ,cddominio
+    ,dscodigo)
+  VALUES
+    ('TIPOSCHAVEPIX'
+    ,'EVP'
+    ,'4'); 
+    
+  INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T
+    (nmdominio
+    ,cddominio
+    ,dscodigo)
+  VALUES
+    ('TIPOCONTA'
+    ,'CACC'
+    ,'1'); 
+
+  INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T
+    (nmdominio
+    ,cddominio
+    ,dscodigo)
+  VALUES
+    ('TIPOCONTA'
+    ,'SVGS'
+    ,'3'); 
+
+  INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T
+    (nmdominio
+    ,cddominio
+    ,dscodigo)
+  VALUES
+    ('TIPOCONTA'
+    ,'TRAN'
+    ,'9'); 
+    
+  COMMIT;    
+    
+EXCEPTION
+  WHEN OTHERS THEN
+    vr_code := sqlcode;
+    vr_errm := sqlerrm;
+    ROLLBACK;
+    RAISE_APPLICATION_ERROR(-20000,'Erro ao inserir Tipos de Chave Pix - ' || vr_code || ' / ' || vr_errm);
+END;    
