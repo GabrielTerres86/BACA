103d2c577f76a8f16962efbc799a65da7f09c67b
Carlos Henrique Weinhold <carlos.weinhold@ailos.coop.br>
2022-08-16 11:08:12 -0300

    Opcao de exclusao tela envnot

---
+++ 2022/08/baca_telaenvnot.sql
@@ -0,0 +1,15 @@
+﻿INSERT 
+  INTO CECRED.crapaca (NRSEQACA, NMDEACAO, 
+                       NMPACKAG, NMPROCED, 
+                       LSTPARAM, NRSEQRDR)
+VALUES ((SELECT MAX(NRSEQACA) + 1 FROM CECRED.crapaca), 
+       'EXCLUI_ENVIO_PUSH', 
+       'TELA_ENVNOT', 'pc_exclui_msg_manual', 
+       'pr_cdmensagem', 885);
+
+UPDATE CECRED.craptel t
+   SET t.CDOPPTEL = 'A,C,N,E'
+     , t.LSOPPTEL = 'ALTERACAO,CONSULTA,CADASTRAR/ENVIAR,EXCLUIR'
+ WHERE t.NMDATELA = 'ENVNOT';
+
+COMMIT;
