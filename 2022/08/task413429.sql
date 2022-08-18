fd126400b0598b1d4761802cbc994d81a9a402e9
Douglas <douglas.kloppel@ailos.coop.br>
2022-08-18 13:34:06 -0300

    incluir o owner

--- 2022/08/task413429.sql
+++ 2022/08/task413429.sql
@@ -1,6 +1,6 @@
 BEGIN
   
-  UPDATE craptxi
+  UPDATE cecred.craptxi
      SET dtcadast = to_date('10/08/2022','dd/mm/yyyy')
    WHERE cddindex = 5
      AND dtiniper = to_date('01/07/2022','dd/mm/yyyy');
