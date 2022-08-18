47808ccf4b76573a76b8b63c73172d576a17d1b8
Douglas Pagel <douglas.pagel@ailos.coop.br>
2022-08-18 08:54:06 -0300

    Inclusão de bloco begin end além do já existente,

--- 2022/08/INC0202979_Imob_Motor.sql
+++ 2022/08/INC0202979_Imob_Motor.sql
@@ -1,24 +1,26 @@
-DECLARE
-  CURSOR cr_imobiliario IS
-    SELECT imo.cdcooper
-          ,imo.nrdconta
-          ,imo.nrctremp
-          ,imo.protocolo_motor
-      FROM credito.tbepr_contrato_imobiliario imo
-     WHERE imo.data_envio_motor = to_date('16/08/2022', 'dd/mm/RRRR')
-       AND imo.situacao_analise = 1;
 BEGIN
-  FOR rw_imobiliario IN cr_imobiliario LOOP
-    
-    CECRED.ESTE0009.pc_solicita_retorno_analise(pr_cdcooper => rw_imobiliario.cdcooper
-                                               ,pr_nrdconta => rw_imobiliario.nrdconta
-                                               ,pr_nrctremp => rw_imobiliario.nrctremp
-                                               ,pr_dsprotoc => rw_imobiliario.protocolo_motor);
-                                               
-  END LOOP;
+    DECLARE
+    CURSOR cr_imobiliario IS
+      SELECT imo.cdcooper
+            ,imo.nrdconta
+            ,imo.nrctremp
+            ,imo.protocolo_motor
+        FROM credito.tbepr_contrato_imobiliario imo
+       WHERE imo.data_envio_motor = to_date('16/08/2022', 'dd/mm/RRRR')
+         AND imo.situacao_analise = 1;
+  BEGIN
+    FOR rw_imobiliario IN cr_imobiliario LOOP
+      
+      CECRED.ESTE0009.pc_solicita_retorno_analise(pr_cdcooper => rw_imobiliario.cdcooper
+                                                 ,pr_nrdconta => rw_imobiliario.nrdconta
+                                                 ,pr_nrctremp => rw_imobiliario.nrctremp
+                                                 ,pr_dsprotoc => rw_imobiliario.protocolo_motor);
                                                  
-EXCEPTION
-  WHEN OTHERS THEN
-    ROLLBACK;
-    CECRED.pc_internal_exception(pr_cdcooper => 3);
+    END LOOP;
+                                                   
+  EXCEPTION
+    WHEN OTHERS THEN
+      ROLLBACK;
+      CECRED.pc_internal_exception(pr_cdcooper => 3);
+  END;
 END;
