f2f807f80eb804341b0024975e6e99295ecd213f
Alessandro Hoffelder <alessandro.hoffelder@ailos.coop.br>
2022-08-18 12:25:04 +0000

    Updated INC0198631.sql

--- 2022/08/INC0198631.sql
+++ 2022/08/INC0198631.sql
@@ -78,18 +78,6 @@ BEGIN
      AND p.nrdconta = 654116
      AND p.nrctrseg = 347849
      AND p.tpseguro = 4;
-
-  UPDATE cecred.tbseg_prestamista p
-     SET p.tpregist = 3
-   WHERE p.cdcooper = 16
-     AND p.nrproposta = '770658370626';
-
-  UPDATE cecred.crapseg p
-     SET p.cdmotcan = 20
-   WHERE p.cdcooper = 16
-     AND p.nrdconta = 277819
-     AND p.nrctrseg = 214705
-     AND p.tpseguro = 4;
   
 END;
 
