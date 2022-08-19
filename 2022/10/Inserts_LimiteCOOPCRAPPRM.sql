ac13ce474534615a46e608f72debc5ece6345390
Marcus Abreu <premiersoft-marcus.abreu@ailos.coop.br>
2022-08-19 17:31:18 -0300

    PRJ0024479 - Inserts dos limites de valores devolução via pix (ASVR98) por cooperativa (Temporário R$ 1.000,00, até nova definição).

---
+++ 2022/10/Inserts_LimiteCOOPCRAPPRM.sql
@@ -0,0 +1,36 @@
+DECLARE
+  CURSOR cr_crapcop IS
+    SELECT c.cdcooper
+      FROM cecred.crapcop c
+     WHERE c.flgativo = 1
+       AND c.cdcooper <> 3;
+     
+  vr_code NUMBER;
+  vr_errm VARCHAR2(64);
+BEGIN
+  
+  FOR rg_crapcop IN cr_crapcop LOOP
+    -- Valor limite da transferencia de Valores a Devolver via Pix - ASVR98
+    INSERT INTO CECRED.CRAPPRM P
+    (nmsistem
+    ,cdcooper
+    ,cdacesso
+    ,dstexprm
+    ,dsvlrprm)
+    VALUES
+    ('CRED'
+    ,rg_crapcop.cdcooper
+    ,'LIMITE_VALORDEVPIX'
+    ,'Limite maximo, por cooperativa, do valor a ser devolvido via Pix no ASVR98'
+    ,'1000');
+  END LOOP;
+  
+  COMMIT;    
+    
+EXCEPTION
+  WHEN OTHERS THEN
+    vr_code := sqlcode;
+    vr_errm := sqlerrm;
+    ROLLBACK;
+    RAISE_APPLICATION_ERROR(-20000,'Erro ao inserir as Contas Administrativas - ' || vr_code || ' / ' || vr_errm);
+END;
