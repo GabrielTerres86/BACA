14266cf3634c53a3fadc534c276873a7c9cdc08d
Andrey Leonardo Bauler <t0033422@ailos.coop.br>
2022-08-23 11:29:21 -0300

    Inclusão de owner e filtro

--- 2022/08/INC0202938-deleta-operadores.sql
+++ 2022/08/INC0202938-deleta-operadores.sql
@@ -1,5 +1,5 @@
 begin
-  delete crapopi 
+  delete cecred.crapopi 
     where nrdconta = 14672146 and nrcpfope in 
     (239863011
     ,83454713072
@@ -7,5 +7,6 @@ begin
     ,96894776091
     ,69908982087
     ,160489032);
+    and cdcooper = 1;
   commit;
 end;
