830026de6bef5ea7a345c5203360335432b239ee
Andrey Leonardo Bauler <t0033422@ailos.coop.br>
2022-08-26 16:28:14 -0300

    Inclusão de novos menus no escopo de multiplas contas

--- 2022/06/prj686-insere-menus-fp.sql
+++ 2022/06/prj686-insere-menus-fp.sql
@@ -6,7 +6,7 @@ BEGIN
          ,dhcadastro
   ) VALUES (
     (select NVL(max(iditem_menu_multcontas),0) from INTERNETBANKING.tbib_itens_menu_multcontas)+1
-    ,121
+    ,119
     ,sysdate
   );
 
@@ -16,10 +16,40 @@ BEGIN
          ,dhcadastro
   ) VALUES (
     (select NVL(max(iditem_menu_multcontas),0) from INTERNETBANKING.tbib_itens_menu_multcontas)+1
-    ,122
+    ,120
     ,sysdate
   );
 
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+          iditem_menu_multcontas
+         ,cditem_menu
+         ,dhcadastro
+  ) VALUES (
+    (select NVL(max(iditem_menu_multcontas),0) from INTERNETBANKING.tbib_itens_menu_multcontas)+1
+    ,121
+    ,sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+          iditem_menu_multcontas
+         ,cditem_menu
+         ,dhcadastro
+  ) VALUES (
+    (select NVL(max(iditem_menu_multcontas),0) from INTERNETBANKING.tbib_itens_menu_multcontas)+1
+    ,122
+    ,sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+          iditem_menu_multcontas
+         ,cditem_menu
+         ,dhcadastro
+  ) VALUES (
+    (select NVL(max(iditem_menu_multcontas),0) from INTERNETBANKING.tbib_itens_menu_multcontas)+1
+    ,123
+    ,sysdate
+  );
+  
 
   commit;
 end;
