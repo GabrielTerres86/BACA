65797d33d47d23dfb438661e05a84f0c84d53f47
premiersotf-caroline.castro <premiersotf-caroline.castro@ailos.coop.br>
2022-08-29 10:59:22 -0300

    Ajuste de saldo devedor negativo da conta / contrato :  9633138 / 2792685

---
+++ 2022/08/tsk422219-saldonegativo.sql
@@ -0,0 +1,16 @@
+BEGIN
+     UPDATE crapepr epr
+        SET epr.vlsdeved = (SELECT nvl(sum(vlsdvpar),0)
+                            FROM   crappep pep 
+                            WHERE  pep.cdcooper = epr.cdcooper 
+                            AND    pep.nrdconta = epr.nrdconta 
+                            AND    pep.nrctremp = epr.nrctremp
+                            and    pep.inliquid = 0)
+      WHERE (cdcooper , nrdconta, nrctremp ) in ((1, 9633138, 2792685)) ;      
+   
+   COMMIT;
+   
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+END;
