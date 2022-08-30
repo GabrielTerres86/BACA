af22229cad094d1960c0fb8e6eebdda2fd2e6b99
Lucas Eduardo Ranghetti <lucas.ranghetti@ailos.coop.br>
2022-08-30 08:37:26 -0300

    Scripts Projeto seguro vida digital

---
+++ 2022/09/insert_templates.sql
@@ -0,0 +1,14 @@
+BEGIN
+  insert into SEGURO.tbseg_template_textos( cdtexto_template, dstexto_template, flstatus_texto_template)  values( '4', 'Até R$ 7.000 para serviço funeral ou reembolso.',  1);
+  insert into SEGURO.tbseg_template_textos( cdtexto_template, dstexto_template, flstatus_texto_template)  values( '5', 'Pessoa(s) designada(s) pelo titular do seguro para receberem a indenização em caso de sua ausência.\n\nNa falta de indicação de beneficiários ou se por algum motivo não prevalecer a que for feita, serão considerados beneficiários os herdeiros legais do segurado.\n\nPara beneficiário menor de idade, a indenização será paga diretamente na conta corrente do menor de idade (caso possua) ou de seu represente legal.',  1);
+  insert into SEGURO.tbseg_template_textos( cdtexto_template, dstexto_template, flstatus_texto_template)  values( '7', 'Seguros de Vida administrados por Icatu Seguros S.A, inscrita no CNPJ/MF sob o nº 42.283.770/0001-39, VG Faixa Etária - Processo SUSEP nº 15414.001272/2006-36. \n\nSAC: {telefoneSAC} de segunda a sexta-feira: das 8h às 20h, exceto em feriados nacionais. \n\nTítulos de Capitalização da modalidade incentivo emitido pela Icatu Capitalização  S/A, CNPJ/MF nº 74.267.170/0001-73, VG Faixa Etária - Processo SUSEP nº 15414.001272/2006-36. \n\nApós a comunicação do sorteio, o prêmio estará disponível para pagamento pelo prazo prescricional em vigor, o qual, atualmente é de 5 anos, conforme previsto no Código Civil de 2002. \n\nOuvidoria Icatu Seguros: {telefoneIcatu} de segunda a sexta-feira, das 8h às 18h, exceto feriados.',  1);
+  insert into SEGURO.tbseg_template_textos( cdtexto_template, dstexto_template, flstatus_texto_template)  values( '8', '0800 286 0110',  1);
+  insert into SEGURO.tbseg_template_textos( cdtexto_template, dstexto_template, flstatus_texto_template)  values( '9', '0800 286 0047',  1);
+  insert into SEGURO.tbseg_template_textos( cdtexto_template, dstexto_template, flstatus_texto_template)  values( '10', 'A contratação neste canal só é permitida para pessoas residentes no Brasil. Gentileza entrar em contato com a Ailos Corretora através do WhatsApp pelo número:', 1);
+
+COMMIT;
+exception
+  when others then
+    dbms_output.put_line('Erro: '|| sqlerrm);
+END;
+/
---
+++ 2022/09/insert_termo_adesao.sql
@@ -0,0 +1,75 @@
+DECLARE texto CLOB := '{
+    "paragrafo": [
+		{
+		   "conteudo": "TERMO DO SEGURO VIDA"
+		},
+		{
+		   "conteudo": "1. Ao aceitar as CONDIÇÕES abaixo, o COOPERADO solicita a contratação do Seguro Vida, nos termos e condições apresentadas na simulação realizada por meio do acesso aos Canais Digitais."
+		},
+		{
+		   "conteudo": "2. As Condições Gerais do Seguro poderão ser visualizadas no site da seguradora contratada."
+		},
+		{
+		   "conteudo": "3. O COOPERADO declara e reconhece que:"
+		},
+		{
+		   "conteudo": "3.1. É o primeiro titular da conta corrente indicada na simulação e utilizada para contratação deste seguro;"
+		},
+		{
+		   "conteudo": "3.2. Todos os dados fornecidos na simulação e contratação do seguro são verídicos;"
+		},
+		{
+		   "conteudo": "3.3. A emissão da apólice deste seguro será realizada em seu nome, conforme as opções selecionadas durante a simulação e contratação;"
+		},
+		{
+		   "conteudo": "3.4. É o único e exclusivo responsável pela seleção e preenchimento de todos os dados/informações necessários à efetivação do seguro;"
+		},
+		{
+		   "conteudo": "3.5. O não pagamento da primeira parcela ou, se for o caso, da parcela única do prêmio, implicará no cancelamento automático e sem qualquer aviso da apólice gerada, desde o início da sua vigência;"
+		},
+		{
+		   "conteudo": "3.6. A contratação desse seguro foi realizada por meio de Cana Digital escolhido por ele;"
+		},
+		{
+		   "conteudo": "3.7. O valor da remuneração pela venda desse seguro foi informado durante a contratação do seguro, o qual estava acompanhado dos respectivos valores de prêmio comercial."
+		},
+		{
+		   "conteudo": "4. A COOPERATIVA não será responsabilizada por eventual falha ou erro na seleção e preenchimento dos dados/informações realizadas pelo COOPERADO necessários à efetivação do seguro."
+		 },
+		{
+		   "conteudo": "5. A apólice do seguro será enviada ao COOPERADO através do e-mail informado no momento da solicitação do seguro (dados de contato), após a confirmação da contratação pela Seguradora. A vigência do seguro iniciará às 00hrs do dia do recebimento da confirmação. Para solicitação dos Serviços de Assistência Funeral ou em caso de Sinistro, você poderá contatar a Seguradora através dos números informados na Apólice. "
+		},
+		{
+		   "conteudo": "5.1. O período de carência para acionamento do Seguro contrato deve ser consultado na Proposta apresentada ao COOPERADO durante a contratação."
+		},
+		{
+		   "conteudo": "6. A SUSEP - Superintendência de Seguros Privados - é a Autarquia Federal responsável pela fiscalização, normatização e controle dos mercados de seguro, previdência complementar aberta, capitalização, resseguro e corretagem de seguros."
+		},
+		{
+		   "conteudo": "7. As condições contratuais/regulamentos deste produto protocolizadas por esta Companhia Seguradora junto à SUSEP também poderão ser consultados pelo endereço eletrônico www.susep.gov.br/menu/servicos-ao-cidadao/sistema-de-consulta-publica-de-produtos, por meio do número do processo constante da apólice/proposta."
+		},
+		{
+		   "conteudo": "8. O COOPERADO poderá consultar a situação cadastral de seu corretor de seguros, no site www.susep.gov.br, por meio do número de seu registro na SUSEP, nome completo, CNPJ ou CPF. O telefone GRATUITO da Susep para Atendimento Público é 0800 021 8484."
+		},
+		{
+		   "conteudo": "9. Em atendimento à Lei 12.741/12, incidem as alíquotas de 0,65% de PIS/Pasep e de 4% de COFINS sobre os prêmios de seguros, deduzidos do estabelecido em legislação específica."
+		},
+		{
+		   "conteudo": "10. O COOPERADO está ciente que a COOPERATIVA e o Sistema Ailos realizam o tratamento de seus dados pessoais de acordo com a Declaração de Privacidade disponibilizada no site da COOPERATIVA, assim como autoriza e tem ciência de que: (i) a COOPERATIVA realiza consultas de informações em seu nome, conforme disposto na Cláusulas e Condições Gerais Aplicáveis à Conta Corrente (disponíveis no site da COOPERATIVA); (ii) compartilha seus dados pessoais, bem como dados referentes ao produto, de acordo com as bases legais previstas na LGPD, principalmente com a(s) seguradora(s) contratadas e vinculadas a esta proposta, a fim de permitir sua devida contratação."
+		},
+		{
+		   "conteudo": "11. Nos termos do artigo 29, §5º da Lei 10.931/04 e do §2º da Medida Provisória de nº 2.200-2, o COOPERADO reconhece como válida a assinatura deste documento em formato eletrônico, inclusive através da ferramenta digital que não utilize o certificado digital emitido no padrão ICP-Brasil."
+		},
+		{
+		   "conteudo": "12. Assinado eletronicamente pelo COOPERADO {NOME}, inscrito no CPF de nº {CPF} no dia {DT_CONTRATO}, as {HR_CONTRATO}, através do Aplicativo Ailos ou Conta Online, IP nº {IP}, mediante senha digitada."
+		}
+	]
+}';
+BEGIN
+  INSERT INTO SEGURO.tbseg_template_documento(tpdocumento,dtinicio,dtfim,obtemplate) VALUES (2,TRUNC(SYSDATE),NULL, texto);
+  COMMIT;
+EXCEPTION WHEN OTHERS THEN
+  DBMS_OUTPUT.put_line(SQLERRM);
+  ROLLBACK;
+END;
+/
