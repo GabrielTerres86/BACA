f2cf00c17db2f7c3c89d1570fa44ba45cb79a63d
premiersotf-caroline.castro <premiersotf-caroline.castro@ailos.coop.br>
2022-08-30 10:19:50 -0300

    Ajuste do script

--- 2022/08/tsk422219-saldonegativo.sql
+++ 2022/08/tsk422219-saldonegativo.sql
@@ -1,7 +1,7 @@
 BEGIN
-     UPDATE crapepr epr
+     UPDATE cecred.crapepr epr
         SET epr.vlsdeved = (SELECT nvl(sum(vlsdvpar),0)
-                            FROM   crappep pep 
+                            FROM   cecred.crappep pep 
                             WHERE  pep.cdcooper = epr.cdcooper 
                             AND    pep.nrdconta = epr.nrdconta 
                             AND    pep.nrctremp = epr.nrctremp
