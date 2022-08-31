269d9198243f46103558e8d9ee6d7d9c3fa8a587
Andrey Leonardo Bauler <t0033422@ailos.coop.br>
2022-08-31 14:45:06 -0300

    Adicionado o owner da tabela

--- 2022/06/update_tbgen_notif_automatica_prm_vars2.sql
+++ 2022/06/update_tbgen_notif_automatica_prm_vars2.sql
@@ -1,6 +1,6 @@
 BEGIN
 
-  UPDATE tbgen_notif_automatica_prm SET dsvariaveis_mensagem = null
+  UPDATE CECRED.tbgen_notif_automatica_prm SET dsvariaveis_mensagem = null
     where cdorigem_mensagem = 11 ;
     
   commit;
