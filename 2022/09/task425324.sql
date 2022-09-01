ef3b29cce667f4105afca29ba335fa895a18fab6
Nicolas Rocha da Silva <amcom-nicolas.silva@ailos.coop.br>
2022-09-01 10:51:49 -0300

    ajuste no script de marcar parcelas como liquidadas

--- 2022/09/task425324.sql
+++ 2022/09/task425324.sql
@@ -6,15 +6,17 @@ BEGIN
       epr.vlsdvctr = 0,
       epr.vlsdevat = 0
    where epr.nrdconta = 66605 
-     and epr.nrctremp = 114729;
+     and epr.nrctremp = 114729
+     and epr.cdcooper = 13;
 
-  update cecred.crappep 
-     set vlpagpar = vlparepr,
-         inliquid = 1, 
-         vlsdvatu = 0 
-   where nrdconta = 66605 
-     and nrctremp = 114729 
-     and nrparepr in (28,29,30,31,32,33,34,35,36);
+  update cecred.crappep pep 
+     set pep.vlpagpar = pep.vlparepr,
+         pep.inliquid = 1, 
+         pep.vlsdvatu = 0 
+   where pep.nrdconta = 66605 
+     and pep.nrctremp = 114729 
+     and pep.nrparepr in (28,29,30,31,32,33,34,35,36)
+     and pep.cdcooper = 13;
    
   COMMIT;        
   EXCEPTION
