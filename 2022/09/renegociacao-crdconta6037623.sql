d979a36093751ccbf340a6d31c9641ca4283bfc5
premiersotf-caroline.castro <premiersotf-caroline.castro@ailos.coop.br>
2022-09-14 16:52:40 -0300

    Commit para ajuste de capa de renegociacao

--- 2022/09/renegociacao-crdconta6037623.sql
+++ 2022/09/renegociacao-crdconta6037623.sql
@@ -11,7 +11,7 @@ BEGIN
   VALUES
     (1, 6037623, 6062588, 3637088, 1, 4514.06, 4514.06, 159.24, 1, 0, 96, 15502, to_date('26-09-2022','dd-mm-yyyy'),  
      0, NULL, 0.00,1.01 , 0.00, 4,65 ,15502 ,62 ,  4514.06, 4514.06,1.01 , 0, 34.04, 0, 0, 0, 0);
-  /*****************************************************/   
+ 
   INSERT INTO CECRED.tbepr_renegociacao
     (CDCOOPER, NRDCONTA, NRCTREMP, FLGDOCJE, IDFINIOF, DTDPAGTO, QTPREEMP, VLEMPRST, VLPREEMP, DTLIBERA, IDFINTAR)
   VALUES
