7d36f1068a8b2275f2cd9ecd7f563e71d48e0ab7
paulo <paulo.kowalsky@ailos.coop.br>
2022-09-19 14:10:55 -0300

    ajuste script

--- 2022/09/update-inc022515.sql
+++ 2022/09/update-inc022515.sql
@@ -3,7 +3,7 @@ begin
 update cecred.craplcm m 
   set m.dsidenti = 748806320180129865
 where  m.cdcooper = 9
-   and m.nrdconta in (99747227, 252719)
+   and m.nrdconta = 252719
    and m.dtmvtolt = to_date('11/08/2022','DD/MM/YYYY')
    and m.nrdocmto = 55670016;
    
