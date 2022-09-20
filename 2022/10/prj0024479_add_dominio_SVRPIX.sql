69ea1c7a9be19b0ce5e9655546da3ddd97f51274
Renato Darosci <premiersoft-renato.darosci@ailos.coop.br>
2022-09-20 10:26:55 -0300

    Implementações da tela SVRPIX e inclusão de dominios

--- 2022/10/prj0024479_add_dominio_SVRPIX.sql
+++ 2022/10/prj0024479_add_dominio_SVRPIX.sql
@@ -10,13 +10,17 @@ BEGIN
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
   VALUES ('SITDEVPIX_COMPLETA' ,'4', 'Transferência para Conta Administrativa realizada, aguardando envio Pix');
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
-  VALUES ('SITDEVPIX_COMPLETA' ,'5', 'Processado com sucesso');
+  VALUES ('SITDEVPIX_COMPLETA' ,'5', 'Aguardando confirmação Pix');
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
-  VALUES ('SITDEVPIX_COMPLETA' ,'6', 'Processado com falha');
+  VALUES ('SITDEVPIX_COMPLETA' ,'6', 'Pix não confirmado, necessário validação manual');
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
-  VALUES ('SITDEVPIX_COMPLETA' ,'7', 'Rejeitado');
+  VALUES ('SITDEVPIX_COMPLETA' ,'7', 'Processado com Sucesso');
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
-  VALUES ('SITDEVPIX_COMPLETA' ,'8', 'Resolvido Manualmente');
+  VALUES ('SITDEVPIX_COMPLETA' ,'8', 'Processado com Falha');
+  INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
+  VALUES ('SITDEVPIX_COMPLETA' ,'9', 'Rejeitado');
+  INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
+  VALUES ('SITDEVPIX_COMPLETA' ,'10', 'Resolvido Manualmente');
   
   DELETE CECRED.TBCC_DOMINIO_CAMPO T WHERE t.nmdominio = 'SITDEVPIX_RESUMO';
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
@@ -28,13 +32,17 @@ BEGIN
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
   VALUES ('SITDEVPIX_RESUMO' ,'4', 'Aguardando envio Pix');
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
-  VALUES ('SITDEVPIX_RESUMO' ,'5', 'Processado com sucesso');
+  VALUES ('SITDEVPIX_RESUMO' ,'5', 'Aguardando confirmação Pix');
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
-  VALUES ('SITDEVPIX_RESUMO' ,'6', 'Processado com falha');
+  VALUES ('SITDEVPIX_RESUMO' ,'6', 'Pix não confirmado, validar manualmente');
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
-  VALUES ('SITDEVPIX_RESUMO' ,'7', 'Rejeitado');
+  VALUES ('SITDEVPIX_RESUMO' ,'7', 'Processado com sucesso');
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
-  VALUES ('SITDEVPIX_RESUMO' ,'8', 'Resolvido Manualmente');
+  VALUES ('SITDEVPIX_RESUMO' ,'8', 'Processado com falha');
+  INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
+  VALUES ('SITDEVPIX_RESUMO' ,'9', 'Rejeitado');
+  INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
+  VALUES ('SITDEVPIX_RESUMO' ,'10', 'Resolvido Manualmente');
   
   DELETE CECRED.TBCC_DOMINIO_CAMPO T WHERE t.nmdominio = 'SITDEVPIX_COMPACTO';
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
@@ -46,13 +54,17 @@ BEGIN
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
   VALUES ('SITDEVPIX_COMPACTO' ,'4', 'Aguardando Pix');
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
-  VALUES ('SITDEVPIX_COMPACTO' ,'5', 'Processado com sucesso');
+  VALUES ('SITDEVPIX_COMPACTO' ,'5', 'Aguardando confirmação Pix');
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
-  VALUES ('SITDEVPIX_COMPACTO' ,'6', 'Processado com falha');
+  VALUES ('SITDEVPIX_COMPACTO' ,'6', 'Pix não confirmado');
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
-  VALUES ('SITDEVPIX_COMPACTO' ,'7', 'Rejeitado');
+  VALUES ('SITDEVPIX_COMPACTO' ,'7', 'Processado com sucesso');
   INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
-  VALUES ('SITDEVPIX_COMPACTO' ,'8', 'Resolvido Manualmente');
+  VALUES ('SITDEVPIX_COMPACTO' ,'8', 'Processado com falha');
+  INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
+  VALUES ('SITDEVPIX_COMPACTO' ,'9', 'Rejeitado');
+  INSERT INTO CECRED.TBCC_DOMINIO_CAMPO T (nmdominio, cddominio, dscodigo)
+  VALUES ('SITDEVPIX_COMPACTO' ,'10', 'Resolvido Manualmente');
   
   COMMIT;
   
