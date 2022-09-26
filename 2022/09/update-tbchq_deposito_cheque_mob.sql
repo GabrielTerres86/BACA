6ec05d2c5b5adef3007fa9ae77a0aeafc4b486e4
paulo <paulo.kowalsky@ailos.coop.br>
2022-09-26 09:05:50 -0300

    recusa chequem em analise

--- 2022/09/update-tbchq_deposito_cheque_mob.sql
+++ 2022/09/update-tbchq_deposito_cheque_mob.sql
@@ -1,7 +1,7 @@
 begin
   
 update cecred.tbchq_deposito_cheque_mob mob
-  set mob.insituacao = 4
+   set mob.insituacao = 4
 where  mob.insituacao = 1
   and  mob.dtdeposito <= to_date('25/09/2022','DD/MM/YYYY');
 
