617435a12aaea2da06dc624dc8a79a5d3f99d147
Nicolas Rocha da Silva <amcom-nicolas.silva@ailos.coop.br>
2022-09-27 08:39:39 -0300

    ajuste diferenças contábeis de junho - credifoz

---
+++ 2022/09/task402234.sql
@@ -0,0 +1,182 @@
+DECLARE
+
+  vr_cdcritic cecred.crapcri.cdcritic%TYPE;
+  vr_dscritic VARCHAR2(10000);
+  vr_exc_saida EXCEPTION;
+  rw_crapdat  cecred.BTCH0001.cr_crapdat%ROWTYPE;
+  vr_des_reto varchar(3);
+  vr_tab_erro cecred.GENE0001.typ_tab_erro;
+
+  TYPE dados_typ IS RECORD(
+    vr_cdcooper cecred.crapcop.cdcooper%TYPE,
+    vr_nrdconta cecred.crapass.nrdconta%TYPE,
+    vr_nrctremp cecred.craplem.nrctremp%TYPE,
+    vr_vllanmto cecred.craplem.vllanmto%TYPE,
+    vr_cdhistor cecred.craplem.cdhistor%TYPE);
+
+  TYPE t_dados_tab IS TABLE OF dados_typ;
+  v_dados t_dados_tab := t_dados_tab();
+
+  CURSOR cr_crapass(pr_cdcooper IN cecred.crapass.cdcooper%TYPE
+                   ,pr_nrdconta IN cecred.crapass.nrdconta%TYPE) IS
+    SELECT ass.cdagenci
+      FROM cecred.crapass ass
+     WHERE ass.cdcooper = pr_cdcooper
+       AND ass.nrdconta = pr_nrdconta;
+  rw_crapass cr_crapass%ROWTYPE;
+
+BEGIN
+
+  v_dados.extend();
+  v_dados(v_dados.last()).vr_cdcooper := 11;
+  v_dados(v_dados.last()).vr_nrdconta := 604313;
+  v_dados(v_dados.last()).vr_nrctremp := 122031;
+  v_dados(v_dados.last()).vr_vllanmto := 77.99;
+  v_dados(v_dados.last()).vr_cdhistor := 3919;
+
+  v_dados.extend();
+  v_dados(v_dados.last()).vr_cdcooper := 11;
+  v_dados(v_dados.last()).vr_nrdconta := 660850;
+  v_dados(v_dados.last()).vr_nrctremp := 122382;
+  v_dados(v_dados.last()).vr_vllanmto := 3461.16;
+  v_dados(v_dados.last()).vr_cdhistor := 3918;
+
+  v_dados.extend();
+  v_dados(v_dados.last()).vr_cdcooper := 11;
+  v_dados(v_dados.last()).vr_nrdconta := 454788;
+  v_dados(v_dados.last()).vr_nrctremp := 129483;
+  v_dados(v_dados.last()).vr_vllanmto := 58.86;
+  v_dados(v_dados.last()).vr_cdhistor := 3919;
+
+  v_dados.extend();
+  v_dados(v_dados.last()).vr_cdcooper := 11;
+  v_dados(v_dados.last()).vr_nrdconta := 686530;
+  v_dados(v_dados.last()).vr_nrctremp := 157030;
+  v_dados(v_dados.last()).vr_vllanmto := 21.22;
+  v_dados(v_dados.last()).vr_cdhistor := 3918;
+
+  v_dados.extend();
+  v_dados(v_dados.last()).vr_cdcooper := 11;
+  v_dados(v_dados.last()).vr_nrdconta := 726028;
+  v_dados(v_dados.last()).vr_nrctremp := 158732;
+  v_dados(v_dados.last()).vr_vllanmto := 25.37;
+  v_dados(v_dados.last()).vr_cdhistor := 3883;
+
+  v_dados.extend();
+  v_dados(v_dados.last()).vr_cdcooper := 11;
+  v_dados(v_dados.last()).vr_nrdconta := 293326;
+  v_dados(v_dados.last()).vr_nrctremp := 160248;
+  v_dados(v_dados.last()).vr_vllanmto := 2677.07;
+  v_dados(v_dados.last()).vr_cdhistor := 3919;
+
+  v_dados.extend();
+  v_dados(v_dados.last()).vr_cdcooper := 11;
+  v_dados(v_dados.last()).vr_nrdconta := 610720;
+  v_dados(v_dados.last()).vr_nrctremp := 162126;
+  v_dados(v_dados.last()).vr_vllanmto := 1296.9;
+  v_dados(v_dados.last()).vr_cdhistor := 3918;
+
+  v_dados.extend();
+  v_dados(v_dados.last()).vr_cdcooper := 11;
+  v_dados(v_dados.last()).vr_nrdconta := 606561;
+  v_dados(v_dados.last()).vr_nrctremp := 170419;
+  v_dados(v_dados.last()).vr_vllanmto := 20.44;
+  v_dados(v_dados.last()).vr_cdhistor := 3883;
+
+  v_dados.extend();
+  v_dados(v_dados.last()).vr_cdcooper := 11;
+  v_dados(v_dados.last()).vr_nrdconta := 340790;
+  v_dados(v_dados.last()).vr_nrctremp := 176369;
+  v_dados(v_dados.last()).vr_vllanmto := 168.46;
+  v_dados(v_dados.last()).vr_cdhistor := 3919;
+
+  v_dados.extend();
+  v_dados(v_dados.last()).vr_cdcooper := 11;
+  v_dados(v_dados.last()).vr_nrdconta := 456551;
+  v_dados(v_dados.last()).vr_nrctremp := 180168;
+  v_dados(v_dados.last()).vr_vllanmto := 47.35;
+  v_dados(v_dados.last()).vr_cdhistor := 3883;
+
+  v_dados.extend();
+  v_dados(v_dados.last()).vr_cdcooper := 11;
+  v_dados(v_dados.last()).vr_nrdconta := 691240;
+  v_dados(v_dados.last()).vr_nrctremp := 189595;
+  v_dados(v_dados.last()).vr_vllanmto := 23.18;
+  v_dados(v_dados.last()).vr_cdhistor := 3883;
+
+  v_dados.extend();
+  v_dados(v_dados.last()).vr_cdcooper := 11;
+  v_dados(v_dados.last()).vr_nrdconta := 311537;
+  v_dados(v_dados.last()).vr_nrctremp := 190709;
+  v_dados(v_dados.last()).vr_vllanmto := 3.76;
+  v_dados(v_dados.last()).vr_cdhistor := 3918;
+
+  v_dados.extend();
+  v_dados(v_dados.last()).vr_cdcooper := 11;
+  v_dados(v_dados.last()).vr_nrdconta := 133264;
+  v_dados(v_dados.last()).vr_nrctremp := 197299;
+  v_dados(v_dados.last()).vr_vllanmto := 21.11;
+  v_dados(v_dados.last()).vr_cdhistor := 3883;
+
+  v_dados.extend();
+  v_dados(v_dados.last()).vr_cdcooper := 11;
+  v_dados(v_dados.last()).vr_nrdconta := 424536;
+  v_dados(v_dados.last()).vr_nrctremp := 204085;
+  v_dados(v_dados.last()).vr_vllanmto := 14.12;
+  v_dados(v_dados.last()).vr_cdhistor := 3919;
+
+  FOR x IN NVL(v_dados.first(), 1) .. nvl(v_dados.last(), 0) LOOP
+  
+    OPEN cecred.btch0001.cr_crapdat(pr_cdcooper => v_dados(x).vr_cdcooper);
+    FETCH cecred.btch0001.cr_crapdat
+      INTO rw_crapdat;
+    CLOSE cecred.btch0001.cr_crapdat;
+  
+    OPEN cr_crapass(pr_cdcooper => v_dados(x).vr_cdcooper, pr_nrdconta => v_dados(x).vr_nrdconta);
+    FETCH cr_crapass
+      INTO rw_crapass;
+    CLOSE cr_crapass;
+  
+    cecred.EMPR0001.pc_cria_lancamento_lem(pr_cdcooper => v_dados(x).vr_cdcooper,
+                                           pr_dtmvtolt => rw_crapdat.dtmvtolt,
+                                           pr_cdagenci => rw_crapass.cdagenci,
+                                           pr_cdbccxlt => 100,
+                                           pr_cdoperad => 1,
+                                           pr_cdpactra => rw_crapass.cdagenci,
+                                           pr_tplotmov => 5,
+                                           pr_nrdolote => 600031,
+                                           pr_nrdconta => v_dados(x).vr_nrdconta,
+                                           pr_cdhistor => v_dados(x).vr_cdhistor,
+                                           pr_nrctremp => v_dados(x).vr_nrctremp,
+                                           pr_vllanmto => v_dados(x).vr_vllanmto,
+                                           pr_dtpagemp => rw_crapdat.dtmvtolt,
+                                           pr_txjurepr => 0,
+                                           pr_vlpreemp => 0,
+                                           pr_nrsequni => 0,
+                                           pr_nrparepr => 0,
+                                           pr_flgincre => FALSE,
+                                           pr_flgcredi => FALSE,
+                                           pr_nrseqava => 0,
+                                           pr_cdorigem => 5,
+                                           pr_cdcritic => vr_cdcritic,
+                                           pr_dscritic => vr_dscritic);
+  
+    IF vr_cdcritic IS NOT NULL OR vr_dscritic IS NOT NULL THEN
+      RAISE vr_exc_saida;
+    END IF;
+  
+  END LOOP;
+
+  COMMIT;
+
+EXCEPTION
+
+  WHEN vr_exc_saida THEN
+    RAISE_application_error(-20500, vr_dscritic);
+    ROLLBACK;
+  
+  WHEN OTHERS THEN
+    RAISE_application_error(-20500, SQLERRM);
+    ROLLBACK;
+  
+END;
