556c799e25f09f8a3b4fe7fc80bbd765152ae218
paulo <paulo.kowalsky@ailos.coop.br>
2022-09-27 09:13:49 -0300

    insert craptab - truncagem

---
+++ 2022/10/insert-craptab-truncagem.sql
@@ -0,0 +1,8 @@
+begin
+ 
+insert into cecred.craptab (nmsistem,tptabela,cdempres,cdacesso,tpregist,dstextab,cdcooper)
+  values ('CRED','GENERI',0,'TRUNCAGEM',0,'srvp003kofax01;Fsr#@de3652d7f21',3);
+
+
+commit;
+end;
