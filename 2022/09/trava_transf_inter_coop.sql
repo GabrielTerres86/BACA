109e8421033718683969f774bb3d1a99438aaf9d
Alberto J G Neto <mouts-alberto.neto@ailos.coop.br>
2022-09-29 23:25:59 -0300

    -Trava tranf inter cooper baca

--- 2022/09/trava_transf_inter_coop.sql
+++ 2022/09/trava_transf_inter_coop.sql
@@ -1,8 +1,8 @@
 BEGIN
-	INSERT INTO crapprm 
+	INSERT INTO cecred.crapprm 
 	  (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) 
 	VALUES 
-	  ('CRED', 0, 'TRAVA_TRANSF_INTRA_ATIVO', 'Desabilita transferencia inter cooperativa', '1');
+	  ('CRED', 0, 'TRAVA_TRANSF_INTER_ATIVO', 'Desabilita transferencia inter cooperativa', '1');
 	COMMIT;
 END;	
 /
