92f4d059dd6cf711d5c4e2cce0791a32773c2dcd
Giovane Diego Gibbert <giovane.gibbert@ailos.coop.br>
2022-10-05 08:48:00 -0300

    Parâmetros para geração de novos dados nos arquivos carga e finaneira

---
+++ 2022/10/prj0024136_parametros_arquivoscyber.sql
@@ -0,0 +1,17 @@
+BEGIN
+  BEGIN
+	insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+	values ('CRED', 0, 'CRPS652_CYBER_ENVAILOS+', 'Diretorio de envio do Cyber para Ailos+', '/usr/sistemas/recuperacao/cyber/envia/');
+  EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN
+	  NULL;
+  END;
+  BEGIN
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', 0, 'CRPS652_CYBER_CONTACTR', 'Indica se irá gerar dados da conta e contrato em posições maiores', 'S');
+  EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN
+	  NULL;
+  END;
+  COMMIT;
+END;
