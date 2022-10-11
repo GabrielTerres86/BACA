066bff1e83a68b1bb1cc7b277f633141b82ba2f5
Jose Jair Dill Junior (Mouts) <t0032120@ailos.coop.br>
2022-10-11 11:40:56 -0300

    Ajustar situação do boleto
    Encaminhar atualização do boleto para JD

--- 2022/10/INC0220273_Script.sql
+++ 2022/10/INC0220273_Script.sql
@@ -1,33 +1,69 @@
 DECLARE
-  vr_dscritic VARCHAR2(2000);
-  vr_dserro   VARCHAR2(2000);
-  CURSOR cr_crapcob IS
-    SELECT ROWID
-          ,cdcooper
-          ,nrdconta
-          ,nrcnvcob
-          ,nrdocmto
-      FROM crapcob
-     WHERE (cdcooper, nrdconta, nrcnvcob, nrdocmto) IN ((1, 8058830, 101004, 20054141));
+  vr_tab_remessa_dda DDDA0001.typ_tab_remessa_dda;
+  vr_tab_retorno_dda DDDA0001.typ_tab_retorno_dda;
+
+  vr_cdcritic NUMBER;
+  vr_dscritic VARCHAR2(4000);
+  vr_des_erro   VARCHAR2(100);  
+  vr_excerro EXCEPTION;
+
 BEGIN
-  FOR rw_crapcob IN cr_crapcob LOOP
-    UPDATE crapcob
-       SET insitcrt = 0
-          ,dtsitcrt = NULL
-     WHERE cdcooper = rw_crapcob.cdcooper
-       AND nrdconta = rw_crapcob.nrdconta
-       AND nrcnvcob = rw_crapcob.nrcnvcob
-       AND nrdocmto = rw_crapcob.nrdocmto;
-    paga0001.pc_cria_log_cobranca(pr_idtabcob => rw_crapcob.rowid
+
+  FOR rw_crapcob IN (SELECT cob.*
+                           ,ROWID
+                       FROM crapcob cob
+                      WHERE cob.incobran = 0
+                        AND (cdcooper, nrdconta, nrcnvcob, nrdocmto) IN ((1, 8058830, 101004, 20054141)))
+  LOOP
+  
+    BEGIN
+      UPDATE crapcob
+         SET flgdprot = 0
+            ,qtdiaprt = 0
+            ,insrvprt = 0
+            ,dtbloque = NULL
+            ,insitcrt = 0
+            ,dtsitcrt = NULL
+            ,dtlipgto = CASE
+                          WHEN ADD_MONTHS(dtlipgto, -60) > trunc(SYSDATE) THEN
+                           ADD_MONTHS(dtlipgto, -60)
+                          ELSE
+                           dtlipgto
+                        END
+       WHERE ROWID = rw_crapcob.rowid;
+    END;
+  
+    PAGA0001.pc_cria_log_cobranca(pr_idtabcob => rw_crapcob.rowid
                                  ,pr_cdoperad => '1'
-                                 ,pr_dtmvtolt => trunc(SYSDATE)
-                                 ,pr_dsmensag => 'Protesto cancelado manualmente'
-                                 ,pr_des_erro => vr_dserro
+                                 ,pr_dtmvtolt => SYSDATE
+                                 ,pr_dsmensag => 'Retirar boleto da situacao Remessa em Cartorio - Manual'
+                                 ,pr_des_erro => vr_des_erro
                                  ,pr_dscritic => vr_dscritic);
   
-    COMMIT;
+    DDDA0001.pc_procedimentos_dda_jd(pr_rowid_cob       => rw_crapcob.rowid --ROWID da Cobranca
+                                    ,pr_tpoperad        => 'A' --Tipo Operacao
+                                    ,pr_tpdbaixa        => ' ' --Tipo de Baixa
+                                    ,pr_dtvencto        => rw_crapcob.dtvencto --Data Vencimento
+                                    ,pr_vldescto        => rw_crapcob.vldescto --Valor Desconto
+                                    ,pr_vlabatim        => rw_crapcob.vlabatim --Valor Abatimento
+                                    ,pr_flgdprot        => rw_crapcob.flgdprot --Flag Protesto
+                                    ,pr_tab_remessa_dda => vr_tab_remessa_dda --tabela remessa
+                                    ,pr_tab_retorno_dda => vr_tab_retorno_dda --Tabela memoria retorno DDA
+                                    ,pr_cdcritic        => vr_cdcritic --Codigo Critica
+                                    ,pr_dscritic        => vr_dscritic); --Descricao Critica
+
+    IF NVL(vr_cdcritic, 0) <> 0 OR TRIM(vr_dscritic) IS NOT NULL THEN
+      RAISE vr_excerro;
+    END IF;
   END LOOP;
+
+  COMMIT;
+
 EXCEPTION
+  WHEN vr_excerro THEN
+    ROLLBACK;
+    raise_application_error(-20500, vr_cdcritic || '-' || vr_dscritic);
+  
   WHEN OTHERS THEN
     ROLLBACK;
     SISTEMA.excecaoInterna(pr_compleme => 'INC0220273');
