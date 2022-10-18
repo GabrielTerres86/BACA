7ac70124e45a304b06425a8750da5bc8e300f13c
Alberto J G Neto <mouts-alberto.neto@ailos.coop.br>
2022-10-18 12:08:35 -0300

    -Cria tabela/insere param lista segura - v1

---
+++ 2022/10/insere-param-lista-segura.sql
@@ -0,0 +1,15 @@
+begin
+
+  insert into CECRED.crapaca (
+     nmdeacao,nmpackag,nmproced,lstparam,nrseqrdr)
+  values   
+     ('CADA0006_ATUALIZA_LISTA_SEGURA','CADA0006','pc_atualiza_lista_segura','pr_cdcooper,pr_nrdconta,pr_lstlsdif,pr_inaddexc',1106);
+
+  COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+	RAISE_application_error(-20500, SQLERRM);
+    ROLLBACK;
+END;
+/
+
