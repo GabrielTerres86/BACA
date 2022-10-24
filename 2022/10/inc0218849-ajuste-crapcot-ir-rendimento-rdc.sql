de51e51eae841cdf6b00f71af972cf99e2652192
Eder Rocha <eder.rocha@ailos.coop.br>
2022-10-24 16:46:25 -0300

    INC0218849 - Ajuste devido a embaralhamento do número da conta

--- 2022/10/inc0218849-ajuste-crapcot-ir-rendimento-rdc.sql
+++ 2022/10/inc0218849-ajuste-crapcot-ir-rendimento-rdc.sql
@@ -3,7 +3,7 @@ BEGIN
        SET vlrenrdc##8 = nvl(vlrenrdc##8,0) + 22114.17,
            vlrentot##8 = nvl(vlrentot##8,0) + 22114.17,
            vlirfrdc##8 = nvl(vlirfrdc##8,0) + 4975.69
-     WHERE cdcooper = 13 and nrdconta = 157368; 
+     WHERE cdcooper = 13 and nrdconta = 99842572; 
 
 	COMMIT;
 EXCEPTION
