44d9b317c64969fa77c47710ac5aba9ed7a1ad8b
Carlos Henrique Weinhold <carlos.weinhold@ailos.coop.br>
2022-10-25 11:51:43 -0300

    Parâmetros da paralelização do programa crps001

--- 2022/11/insert_crapprm_crps001.sql
+++ 2022/11/insert_crapprm_crps001.sql
@@ -1,5 +1,5 @@
 BEGIN
-  FOR rw_cooper IN (SELECT cdcooper FROM crapcop WHERE flgativo = 1) LOOP
+  FOR rw_cooper IN (SELECT cdcooper FROM CECRED.crapcop WHERE flgativo = 1) LOOP
     INSERT INTO CECRED.CRAPPRM
       (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
     VALUES
@@ -7,7 +7,7 @@ BEGIN
        rw_cooper.cdcooper,
        'EXEC_IOF_001',
        'Flag de execuçao do IOF/juros cheque especial no CRPS001 no primeiro dia util do mês',
-       '15/09/2022');
+       '25/10/2022');
   END LOOP;
   
   INSERT INTO CECRED.tbgen_batch_param (idparametro, qtparalelo, qtreg_transacao, cdcooper, cdprograma) VALUES(104,20,0,8,'CRPS001');
