f3d1e1f8745da1e9bb3b9856cabb3ccac7efe636
Luis Carlos Gislon <luis.gislon@ailos.coop.br>
2022-10-31 14:41:46 +0000

    Updated alterar_conta_do_cartao.sql

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -5,7 +5,7 @@ DECLARE
   vr_cpf_titular NUMBER(20);
 BEGIN
   vr_cooperativa := 9;  
-  vr_conta := 385453906;
+  vr_conta := 99443163;
   vr_cartao := 5158940000000188; 
   
   FOR cartao IN (select distinct a.nrcpftit
