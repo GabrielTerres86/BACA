ae9c8ac4e8b03ebc884ae520d1a20c147dab9c8e
Heron Barreto Peixoto de Vasconcelos <amcom-heron.vasconcelos@ailos.coop.br>
2022-11-01 13:58:22 -0300

    Script de criação dos parâmetros de sequencial do CSDC do ailos+. #463325 Agora com CDCOOPER = 0.

--- 2022/11/inc453907-params-sequencial-arquivo-csdc-ailos-mais.sql
+++ 2022/11/inc453907-params-sequencial-arquivo-csdc-ailos-mais.sql
@@ -4,14 +4,16 @@ BEGIN
      NMSISTEM,
      CDACESSO,
      DSTEXPRM,
-     DSVLRPRM
+     DSVLRPRM,
+     CDCOOPER
   )
   VALUES
   (
      'CRED',
      'HOST_WEBSRV_CSDC',
      'URL de acesso ao WS Ailos+ para consulta de sequencial de arquivo',
-     'https://cartoesapi.dev.awscloud.ailos.coop.br'
+     'https://cartoesapi.dev.awscloud.ailos.coop.br',
+     0
   );
   
   INSERT INTO CRAPPRM
@@ -19,14 +21,16 @@ BEGIN
      NMSISTEM,
      CDACESSO,
      DSTEXPRM,
-     DSVLRPRM
+     DSVLRPRM,
+     CDCOOPER
   )
   VALUES
   (
      'CRED',
      'URI_WEBSRV_CSDC',
      'URI de acesso ao WS Ailos+ para consulta de sequencial de arquivos',
-     '/v1/arquivos/controle/'
+     '/v1/arquivos/controle/',
+     0
   );
   
   INSERT INTO CRAPPRM
@@ -34,14 +38,16 @@ BEGIN
      NMSISTEM,
      CDACESSO,
      DSTEXPRM,
-     DSVLRPRM
+     DSVLRPRM,
+     CDCOOPER
   )
   VALUES
   (
      'CRED',
      'MET_WEBSRV_CSDC',
      'METHOD de acesso ao WS Ailos+ para consulta de sequencial de arquivos',
-     'GET'
+     'GET',
+     0
   );
   
   INSERT INTO CRAPPRM
@@ -49,14 +55,16 @@ BEGIN
      NMSISTEM,
      CDACESSO,
      DSTEXPRM,
-     DSVLRPRM
+     DSVLRPRM,
+     CDCOOPER
   )
   VALUES
   (
      'CRED',
      'CLIENT_ID_CSDC',
      'Código de CLIENT_ID de acesso ao WS Ailos+ para consulta de sequencial de arquivos',
-     '364094d69e6442e6ab2654048dcf7830'
+     '364094d69e6442e6ab2654048dcf7830',
+     0
   );
   
   INSERT INTO CRAPPRM
@@ -64,14 +72,16 @@ BEGIN
      NMSISTEM,
      CDACESSO,
      DSTEXPRM,
-     DSVLRPRM
+     DSVLRPRM,
+     CDCOOPER
   )
   VALUES
   (
      'CRED',
      'CLIENT_SECRET_CSDC',
      'Código de CLIENT_SECRET de acesso ao WS Ailos+ para consulta de sequencial de arquivos',
-     '2E8ADB69A67b40A78ab35Bc661aBe817'
+     '2E8ADB69A67b40A78ab35Bc661aBe817',
+     0
   );
   
   COMMIT;
