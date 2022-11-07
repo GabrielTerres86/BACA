03e7f48d6437c0428e09e52a3bad7491e925a2a1
Rodrigo Jose Heiden <rodrigo.heiden@ailos.coop.br>
2022-11-07 16:45:44 -0300

    INC0222019 - Ajustar Situação Contrato PEAC

--- 2022/11/INC0222019_AjusteContratoPeac.sql
+++ 2022/11/INC0222019_AjusteContratoPeac.sql
@@ -4,6 +4,7 @@ BEGIN
         ,a.dtsolicitacaohonra = NULL
         ,a.vlsolicitacaohonra = 0
         ,a.vlsaldorecuperar   = 0
+        ,a.flbloqueiohonra    = 1
    WHERE a.nrcontrato IN (2977711, 2987483)
      AND a.cdcooper = 1;
   COMMIT;
