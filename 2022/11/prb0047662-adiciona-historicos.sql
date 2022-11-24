80cbf852a6164203928f51d37d2ba56f8abf6e1b
Renato Darosci <premiersoft-renato.darosci@ailos.coop.br>
2022-11-24 14:15:10 -0300

    Script para adicionar os históricos de bloqueio do pix na composição de saldo de finais de semana e feriados

---
+++ 2022/11/prb0047662-adiciona-historicos.sql
@@ -0,0 +1,17 @@
+BEGIN
+ 
+  BEGIN
+    UPDATE crapprm t 
+       SET t.dsvlrprm = t.dsvlrprm||',3728,3729,3886,3730,3731,3743,3744,3888,3890'
+     WHERE t.nmsistem = 'CRED'
+       AND t.cdcooper = 0
+       AND t.cdacesso = 'HISTOR_SALDO_FIM_SEMANA';
+  EXCEPTION
+    WHEN OTHERS THEN
+      ROLLBACK;
+      raise_application_error(-20001,'Erro ao atualizar parametro: '||SQLERRM);
+  END;
+  
+  COMMIT;
+
+END;
