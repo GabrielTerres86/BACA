f37e77214cb5638e374fb9a34a6f4c0132da035a
Marcus Abreu <premiersoft-marcus.abreu@ailos.coop.br>
2022-11-28 15:52:28 -0300

    INC0234213 - Incluindo retorno de Cotas Capital, retirandoo valor dos Valores a Devolver.

--- 2022/11/INC0234213.sql
+++ 2022/11/INC0234213.sql
@@ -23,17 +23,14 @@ DECLARE
   
   vc_nrdolote_cota                CONSTANT cecred.craplct.nrdolote%type := 600040;
   vc_tpdevCotas                   CONSTANT NUMBER := 3;
-  vc_tpdevDepVista                CONSTANT NUMBER := 4;
-  vc_cdhistDepVistaPF             CONSTANT NUMBER := 2061;
-  vc_cdhistDepVistaPJ             CONSTANT NUMBER := 2062;
-  vc_cdhistCotasPF                CONSTANT NUMBER := 2079;
-  vc_cdhistCotasPJ                CONSTANT NUMBER := 2080;  
+  vc_cdhistCotas                  CONSTANT NUMBER := 61;
   vc_dstransaStatusCC             CONSTANT VARCHAR2(4000) := 'Alteracao da situacao de conta por script - INC0234213';
+  vc_dstransaDevCotas             CONSTANT VARCHAR2(4000) := 'Alteração de Cotas e devolução - INC0234213';
   vc_inpessoaPF                   CONSTANT NUMBER := 1;
   vc_inpessoaPJ                   CONSTANT NUMBER := 2; 
   vc_nrdolote_lanc                CONSTANT NUMBER := 37000;
   vc_cdbccxlt                     CONSTANT NUMBER := 1;
-  vc_cdsitdctLiberada          CONSTANT NUMBER := 1;
+  vc_cdsitdctLiberada             CONSTANT NUMBER := 1;
   vc_bdprod                       CONSTANT VARCHAR2(100) := 'AYLLOSP';
   
   CURSOR cr_crapass(pr_globalname varchar2
@@ -54,7 +51,7 @@ DECLARE
           ,t.cdmotdem as cdmotdem_old
           ,a.cdmotdem as cdmotdem_new
       FROM CECRED.CRAPASS t
-         ,(select 1 as cdcooper, decode(pr_globalname, vc_bdprod, 13463306,9733027) as nrdconta, vc_cdsitdctLiberada as cdsitdct, null as dtdemiss, null as dtelimin, null as dtasitct, null as  cdmotdem from dual
+         ,(select 1 as cdcooper, decode(pr_globalname, vc_bdprod, 13463306,19711280) as nrdconta, vc_cdsitdctLiberada as cdsitdct, null as dtdemiss, null as dtelimin, null as dtasitct, null as  cdmotdem from dual
           ) a
      WHERE 1=1
        AND t.cdcooper = a.cdcooper
@@ -155,6 +152,117 @@ BEGIN
           ,a.dtasitct = nvl(rg_crapass.dtasitct_new, a.dtasitct)
      where a.cdcooper = rg_crapass.cdcooper
        and a.nrdconta = rg_crapass.nrdconta;
+
+    vr_vldcotas_crapcot := 0;
+    
+    BEGIN
+      SELECT nvl((vlcapital-vlpago),0)
+        INTO vr_vldcotas_crapcot
+        FROM CECRED.TBCOTAS_DEVOLUCAO
+       WHERE nrdconta = rg_crapass.nrdconta
+         AND cdcooper = rg_crapass.cdcooper
+         AND TPDEVOLUCAO = vc_tpdevCotas;
+    EXCEPTION
+      WHEN NO_DATA_FOUND THEN
+      
+        dbms_output.put_line(chr(10) ||
+                             'Não encontrado registro em tbcotas_devolucao para conta ' ||
+                             rg_crapass.nrdconta);
+        vr_vldcotas_crapcot := 0;
+      
+    END;
+    
+    IF vr_vldcotas_crapcot > 0 THEN
+      
+      
+      vr_busca := TRIM(to_char(rg_crapass.cdcooper)) || ';' ||
+                          TRIM(to_char(vr_dtmvtolt,'DD/MM/RRRR')) || ';' ||
+                          TRIM(to_char(rg_crapass.cdagenci)) || ';' ||
+                          '100;' || 
+                          vc_nrdolote_cota;
+                          
+      vr_nrdocmto := cecred.fn_sequence('CRAPLCT','NRDOCMTO', vr_busca);
+                                 
+      vr_nrseqdig := cecred.fn_sequence('CRAPLOT','NRSEQDIG',''||rg_crapass.cdcooper||';'||
+                                                                  TRIM(to_char(vr_dtmvtolt, 'DD/MM/RRRR'))||';'||
+                                                                  rg_crapass.cdagenci||
+                                                                  ';100;'|| 
+                                                                  vc_nrdolote_cota);
+    
+
+      vr_cdhistor := vc_cdhistCotas;
+      
+      INSERT INTO CECRED.craplct
+        (cdcooper,
+         cdagenci,
+         cdbccxlt,
+         nrdolote,
+         dtmvtolt,
+         cdhistor,
+         nrctrpla,
+         nrdconta,
+         nrdocmto,
+         nrseqdig,
+         vllanmto,
+         CDOPEORI,
+         DTINSORI)
+      VALUES
+        (rg_crapass.cdcooper,
+         rg_crapass.cdagenci,
+         100,
+         vc_nrdolote_cota,
+         vr_dtmvtolt,
+         vr_cdhistor,
+         0,
+         rg_crapass.nrdconta,
+         vr_nrdocmto,
+         vr_nrseqdig,
+         vr_vldcotas_crapcot,
+         1,
+         sysdate);
+      
+      vr_nrdrowid := null;
+      
+      CECRED.GENE0001.pc_gera_log(pr_cdcooper => rg_crapass.cdcooper,
+                                  pr_cdoperad => vr_cdoperad,
+                                  pr_dscritic => vr_dscritic,
+                                  pr_dsorigem => 'AIMARO',
+                                  pr_dstransa => vc_dstransaDevCotas,
+                                  pr_dttransa => vr_dttransa,
+                                  pr_flgtrans => 1,
+                                  pr_hrtransa => vr_hrtransa,
+                                  pr_idseqttl => 0,
+                                  pr_nmdatela => NULL,
+                                  pr_nrdconta => rg_crapass.nrdconta,
+                                  pr_nrdrowid => vr_nrdrowid);
+    
+      CECRED.GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                                       pr_nmdcampo => 'craplct.dtmvtolt',
+                                       pr_dsdadant => null,
+                                       pr_dsdadatu => vr_dtmvtolt);
+    
+      CECRED.GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                                       pr_nmdcampo => 'craplct.vllanmto',
+                                       pr_dsdadant => null,
+                                       pr_dsdadatu => vr_vldcotas_crapcot);
+    
+      UPDATE CECRED.crapcot
+         SET vldcotas = vr_vldcotas_crapcot
+       WHERE cdcooper = rg_crapass.cdcooper
+         AND nrdconta = rg_crapass.nrdconta;
+    
+      CECRED.GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid,
+                                       pr_nmdcampo => 'crapcot.vldcotas',
+                                       pr_dsdadant => 0,
+                                       pr_dsdadatu => vr_vldcotas_crapcot);
+                                
+       MERGE INTO CECRED.TBCOTAS_DEVOLUCAO a
+       USING (SELECT rg_crapass.cdcooper as cdcooper, rg_crapass.nrdconta as nrdconta, vc_tpdevCotas as tpdevolucao 
+                FROM DUAL) b
+         ON (a.cdcooper = b.cdcooper AND a.nrdconta = b.nrdconta AND a.tpdevolucao = b.tpdevolucao)
+       WHEN MATCHED THEN UPDATE SET a.vlcapital =(a.vlcapital - vr_vldcotas_crapcot);
+
+    END IF;
         
   END LOOP;
   
