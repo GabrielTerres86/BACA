f26a4fecdd03b9e9bd79ab2163cea33d54182672
Renato Darosci <premiersoft-renato.darosci@ailos.coop.br>
2022-11-28 17:43:23 -0300

    Adicionado owner

--- 2022/11/prb0047662-adiciona-historicos.sql
+++ 2022/11/prb0047662-adiciona-historicos.sql
@@ -1,7 +1,7 @@
 BEGIN
  
   BEGIN
-    UPDATE crapprm t 
+    UPDATE CECRED.crapprm t 
        SET t.dsvlrprm = t.dsvlrprm||',3728,3729,3886,3730,3731,3743,3744,3888,3890'
      WHERE t.nmsistem = 'CRED'
        AND t.cdcooper = 0
