be3643d78189abceb7bbcde468f524198fa89c5d
Giovane Diego Gibbert <giovane.gibbert@ailos.coop.br>
2022-11-29 08:11:03 -0300

    Adicionado owner no script

--- 2022/11/PRJ0024136_parametro_arquivoscyber.sql
+++ 2022/11/PRJ0024136_parametro_arquivoscyber.sql
@@ -1,10 +1,10 @@
 BEGIN
   BEGIN
-    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
     values ('CRED', 0, 'CRPS652_CYBER_CONTACTR', 'Indica se irá gerar dados da conta e contrato com mais dígitos', 'S');
   EXCEPTION
     WHEN DUP_VAL_ON_INDEX THEN
-      UPDATE CRAPPRM
+      UPDATE cecred.CRAPPRM
          SET DSVLRPRM = 'S'
        WHERE CDCOOPER = 0
          AND CDACESSO = 'CRPS652_CYBER_CONTACTR'
