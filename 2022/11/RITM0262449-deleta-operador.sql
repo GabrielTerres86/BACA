a7f96d05a673c33a73cdb22e8930353f1f34da4b
Andrey Leonardo Bauler <t0033422@ailos.coop.br>
2022-12-02 11:31:53 -0300

    Retirada de uma das contas

--- 2022/11/RITM0262449-deleta-operador.sql
+++ 2022/11/RITM0262449-deleta-operador.sql
@@ -1,7 +1,7 @@
 BEGIN
   delete cecred.crapopi opi
   where opi.CDCOOPER = 1
-    and opi.NRDCONTA in (13212176,15218961)
+    and opi.NRDCONTA in (13212176)
     and opi.NRCPFOPE in (1857596021);
   
   commit;
