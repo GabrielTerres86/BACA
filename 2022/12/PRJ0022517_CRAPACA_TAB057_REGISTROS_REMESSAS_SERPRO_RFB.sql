5ef8a1147cd0980857d27a8e191015f5fab2b9c0
mouts-mateus.zimmermann <mouts-mateus.zimmermann@ailos.coop.br>
2022-12-26 07:26:10 -0300

    Ajustes RFB - Ajuste nome procedure TAB057

--- 2022/12/PRJ0022517_CRAPACA_TAB057_REGISTROS_REMESSAS_SERPRO_RFB.sql
+++ 2022/12/PRJ0022517_CRAPACA_TAB057_REGISTROS_REMESSAS_SERPRO_RFB.sql
@@ -1,7 +1,7 @@
 BEGIN
 
   INSERT INTO cecred.crapaca (NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
-      VALUES ('TAB057_REGISTROS_REMESSAS_SERPRO_RFB', 'TELA_TAB057', 'pc_busca_registros_remessas_serpro_receita_federal', 'pr_idremessaserpro,pr_dtiniper,pr_dtfimper,pr_cdempres,pr_status1perna,pr_status2perna,pr_enviadoReproces,pr_tlcooper,pr_nrdconta,pr_tlagenci,pr_nmarquivo,pr_textopesquisa,pr_orderby,pr_ocorrenc,pr_nriniseq,pr_nrregist', (SELECT nrseqrdr FROM craprdr WHERE nmprogra = 'TAB057'));
+      VALUES ('TAB057_REGISTROS_REMESSAS_SERPRO', 'TELA_TAB057', 'pc_busca_registros_remessas_serpro', 'pr_idremessaserpro,pr_dtiniper,pr_dtfimper,pr_cdempres,pr_status1perna,pr_status2perna,pr_enviadoReproces,pr_tlcooper,pr_nrdconta,pr_tlagenci,pr_nmarquivo,pr_textopesquisa,pr_orderby,pr_ocorrenc,pr_nriniseq,pr_nrregist', (SELECT nrseqrdr FROM craprdr WHERE nmprogra = 'TAB057'));
     
   COMMIT;
 END;
