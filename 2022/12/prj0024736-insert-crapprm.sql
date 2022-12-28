0f8012023bb9786ee70ee69a4d3f0ffe355822be
Igor Guilherme Vettorello <f0034298@cecred.coop.br>
2022-12-28 10:41:15 -0300

    Criar parâmetro para gerar arquivo

---
+++ 2022/12/prj0024736-insert-crapprm.sql
@@ -0,0 +1,8 @@
+BEGIN
+
+insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+values ('CRED', 3, 'GERAR_ARQ_CONSOR', 'Habilida o programa a gerar o arquivo de envio para Sicredi', '1');
+
+COMMIT;
+
+END;
