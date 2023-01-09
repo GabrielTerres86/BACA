57efd6cd30d59ffc6843f50cc666ae20dd5508f7
Luiz Otávio Olinger Momm <luiz.momm@ailos.coop.br>
2023-01-09 09:36:15 -0300

    Marcar todos os cartões de crédito como individualizado da CREDELESC

--- 2023/01/INC0240043_CREDELESC_AjusteCartaoCredito3040.sql
+++ 2023/01/INC0240043_CREDELESC_AjusteCartaoCredito3040.sql
@@ -2,7 +2,7 @@ BEGIN
   UPDATE crapris ris
      SET ris.flgindiv = 1
    WHERE ris.cdcooper = 8
-     AND ris.dtrefere = to_date('31/12/2022')
+     AND ris.dtrefere = to_date('31/12/2022', 'dd/mm/yyyy')
      AND ris.cdmodali = 1513
      AND ris.cdorigem = 6;
   COMMIT;
