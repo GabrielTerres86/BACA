01c86b838f2346f28e5679ce22a70bce4c0ec28c
Jose Jair Dill Junior (Mouts) <t0032120@ailos.coop.br>
2023-01-12 08:18:00 -0300

    Inclui novos usuarios para baixa forçada
    Inclui novos usuarios para realizar a baixa forçada dos boletos nao registrados na CIP

---
+++ 2023/01/RITM0270411_Insert.sql
@@ -0,0 +1,57 @@
+BEGIN
+  INSERT INTO craptab
+    (NMSISTEM
+    ,TPTABELA
+    ,CDEMPRES
+    ,CDACESSO
+    ,TPREGIST
+    ,DSTEXTAB
+    ,CDCOOPER)
+  VALUES
+    ('CRED'
+    ,'GENERI'
+    ,0
+    ,'USRCOBRBAIXA'
+    ,7
+    ,'f0033708'
+    ,3);
+
+  INSERT INTO craptab
+    (NMSISTEM
+    ,TPTABELA
+    ,CDEMPRES
+    ,CDACESSO
+    ,TPREGIST
+    ,DSTEXTAB
+    ,CDCOOPER)
+  VALUES
+    ('CRED'
+    ,'GENERI'
+    ,0
+    ,'USRCOBRBAIXA'
+    ,8
+    ,'f0032937'
+    ,3);
+
+  INSERT INTO craptab
+    (NMSISTEM
+    ,TPTABELA
+    ,CDEMPRES
+    ,CDACESSO
+    ,TPREGIST
+    ,DSTEXTAB
+    ,CDCOOPER)
+  VALUES
+    ('CRED'
+    ,'GENERI'
+    ,0
+    ,'USRCOBRBAIXA'
+    ,9
+    ,'f0033890'
+    ,3);
+  COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    SISTEMA.excecaoInterna(pr_compleme => 'RITM0270411');
+    ROLLBACK;
+END;
