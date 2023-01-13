105ee00b371e3caf8cc6986dc4433f30b46c19ab
Jose Jair Dill Junior (Mouts) <t0032120@ailos.coop.br>
2023-01-13 16:27:11 -0300

    Inativa convenio de cobranca
    Inativar convenio de cobranca manualmente, devido a conta esta desativada. #495458

---
+++ 2023/01/INC0239630_script.sql
@@ -0,0 +1,12 @@
+BEGIN
+  UPDATE crapceb c
+     SET c.insitceb = 2
+   WHERE c.cdcooper = 7
+     AND c.nrdconta = 99889412
+     AND c.nrconven = 106002;
+  COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    SISTEMA.excecaoInterna(pr_compleme => 'INC0239630');
+    ROLLBACK;
+END;
