6d93272cda9f8ace6c238a372cd88a51aa01d053
Matheus Anklan Braga <f0033714@cecred.coop.br>
2023-01-19 10:44:00 -0300

    baca cadastro parametros ibratan

---
+++ 2023/01/script_parametros_ibratan_URL.sql
@@ -0,0 +1,14 @@
+DECLARE
+	
+BEGIN
+  
+ INSERT INTO crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+ VALUES ('CRED',0, 'SCRIPT_PROTOCOLO_IBRATAN', 'Script curl busca protocolo biro Ibratan','curl -X HEAD -i --noproxy "*" --max-time 30 --location --request POST "[url]" -H "AccessKey: [accesskey]" -H "SecretKey: [secretkey]" -H "Authorization: [authorization]" -H "Content-Type: application/xml" --data-raw "[xml]"');
+ 
+ INSERT INTO crapprm(nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+ VALUES ('CRED',0, 'SCRIPT_RETORNO_IBRATAN', 'Script curl retorno biro Ibratan','curl --noproxy "*" --max-time [timeout] --location --request POST "[url]" -H "AccessKey: [accesskey]" -H "SecretKey: [secretkey]" -H "Content-Type: application/xml" --data-raw "[xml]"');
+ 
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+END;
