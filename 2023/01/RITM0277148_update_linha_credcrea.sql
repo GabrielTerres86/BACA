536d0b5d5458bde1c214a9ba9ec6736d5df61452
Luciana Leite <premiersoft-luciana.leite@ailos.coop.br>
2023-01-25 11:49:31 -0300

    Update na craplcr para as linhas 60202, 60204 ser Financiamento da cooperativa credcrea

---
+++ 2023/01/RITM0277148_update_linha_credcrea.sql
@@ -0,0 +1,13 @@
+BEGIN
+   UPDATE cecred.craplcr 
+      SET dsoperac = 'FINANCIAMENTO' 
+    WHERE cdlcremp IN (60202, 60204) 
+      AND cdcooper =  7;
+        
+  COMMIT;
+  
+  EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+END;
+
