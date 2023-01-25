3d3dde577806ac4bb26ed8ed5d6b26683f0056b1
Luciana Leite <premiersoft-luciana.leite@ailos.coop.br>
2023-01-25 11:43:00 -0300

    Ajuste para colocar um exception

--- 2023/01/RITM0277148_update_linha_credcrea.sql
+++ 2023/01/RITM0277148_update_linha_credcrea.sql
@@ -3,7 +3,11 @@ BEGIN
       SET dsoperac = 'FINANCIAMENTO' 
     WHERE cdlcremp IN (60202, 60204) 
       AND cdcooper =  7;
-  
+        
   COMMIT;
+  
+  EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
 END;
 
