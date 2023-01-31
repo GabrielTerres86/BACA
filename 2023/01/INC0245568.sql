efaabe02c75e8b9d2ef1d0fae2abfadb161caa53
Alessandra Pereira <alessandra.pereira@ailos.coop.br>
2023-01-31 14:51:17 -0300

    INC0245568 - Ajuste script

--- 2023/01/INC0245568.sql
+++ 2023/01/INC0245568.sql
@@ -1,24 +1,24 @@
 BEGIN
   update cecred.crapsnh snh
-  set snh.cdsitsnh = 1, -- liberada
+  set snh.cdsitsnh = 1,
       snh.dtlibera = trunc(sysdate),
       snh.dtaltsit = trunc(sysdate),
-      snh.qtacerro = 0 -- qtde de erros no login
-  where snh.cdcooper = 2 -- cooperativa >>>>>>>>> AJUSTAR AQUI A COOPERATIVA DESEJADA <<<<<<<<<<<<<
-    and snh.nrdconta in (1164929, 581879) -- número da conta  >>>>>>> AJUSTAR AQUI O NÚMERO DE CONTA DESEJADO <<<<<<<
-    and snh.tpdsenha = 1 -- senha de canais (CO e app)
-    and snh.cdsitsnh = 2; -- senha bloqueada
+      snh.qtacerro = 0
+  where snh.cdcooper = 2
+    and snh.nrdconta in (1164929, 581879)
+    and snh.tpdsenha = 1
+    and snh.cdsitsnh = 2;
 
   update cecred.crapopi opi
   set opi.dtlibera = sysdate,
       opi.dtultalt = sysdate,
-      opi.flgsitop = 1, -- liberado
+      opi.flgsitop = 1,
       opi.qtacerro = 0
-  where opi.cdcooper = 2 -- cooperativa    >>>>>>>>> AJUSTAR AQUI A COOPERATIVA DESEJADA <<<<<<<<<<<<<
-    and opi.nrdconta in (1164929, 581879) -- número da conta  >>>>>>> AJUSTAR AQUI O NÚMERO DE CONTA DESEJADO <<<<<<<
-    and opi.flgsitop = 0 -- operador bloqueado
-    and opi.tpoperai = 0 -- operdaor normal
-    and (trunc(sysdate) - trunc(opi.DTULTALT)) <= 5;  -- considerar bloqueio nos últimos 5 dias
+  where opi.cdcooper = 2
+    and opi.nrdconta in (1164929, 581879)
+    and opi.flgsitop = 0
+    and opi.tpoperai = 0
+    and (trunc(sysdate) - trunc(opi.DTULTALT)) <= 5;
     
   commit;  
 END;
