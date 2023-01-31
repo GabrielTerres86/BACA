19206018d605703695302674d1e8facadf275963
Filipe Ribeiro Barbosa Rêgo <mouts-filipe.rego@ailos.coop.br>
2023-01-31 09:46:47 -0300

    Enviar no arquivo de faturamento a proposta

--- 2023/01/seguro_pessoa_inc0244413.sql
+++ 2023/01/seguro_pessoa_inc0244413.sql
@@ -15,7 +15,11 @@ BEGIN
      SET p.cdsitseg = 2
    WHERE p.cdcooper = 1
      AND p.nrdconta = 6177220
-     AND p.nrctrseg = 1134306; 
+     AND p.nrctrseg = 1134306;
+
+  UPDATE tbseg_prestamista p
+     SET p.tpregist = 1
+   WHERE p.nrproposta = '202208458811';
   
   COMMIT;
 END;
