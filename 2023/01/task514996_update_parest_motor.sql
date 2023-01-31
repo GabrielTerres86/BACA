1a11b06406f01bcb8d5ac79c62a51726d16a2769
Luciana Leite <premiersoft-luciana.leite@ailos.coop.br>
2023-01-31 11:59:23 -0300

    Ajustes na crapprm e crapaca para a criacao separado do paramentro do motor para PF e PJ

---
+++ 2023/01/task514996_update_parest_motor.sql
@@ -0,0 +1,38 @@
+DECLARE
+
+CURSOR cr_crapprm IS
+  SELECT nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm, progress_recid, SUBSTR(cdacesso, 7, 1 ) produto 
+   FROM crapprm c 
+  WHERE c.cdacesso LIKE ('MOTOR_%');
+  
+rw_crapprm cr_crapprm%ROWTYPE;
+
+BEGIN
+  
+  FOR rw_crapprm IN cr_crapprm  LOOP
+        
+     insert into crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+     values (rw_crapprm.nmsistem, rw_crapprm.cdcooper, 'MOTOR_PF_'||rw_crapprm.produto, rw_crapprm.dstexprm, '1');
+       
+     insert into crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+     values (rw_crapprm.nmsistem, rw_crapprm.cdcooper, 'MOTOR_PJ_'||rw_crapprm.produto, rw_crapprm.dstexprm, '1');
+        
+  END LOOP;
+    
+  COMMIT;
+   
+  UPDATE cecred.crapaca 
+     SET lstparam = 'pr_tlcooper,pr_flgativo,pr_tpprodut,pr_incomite,pr_contigen,pr_anlautom,pr_nmregmpf,pr_nmregmpj,pr_qtsstime,pr_qtmeschq,pr_qtmeschqal11,pr_qtmeschqal12,pr_qtmesest,pr_qtmesemp,pr_incalris,pr_neurotechpf,pr_neurotechpj,pr_motorpf,pr_motorpj'
+   WHERE nmdeacao = 'PAREST_ALTERA_PARAM';
+        
+  COMMIT;
+  
+  EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+END;
+
+
+
+
+
