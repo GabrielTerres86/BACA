4253787a38500f9256455d3cf11ba84d49574f01
Bruno Henrique de Bastiani <amcom-bruno.bastiani@ailos.coop.br>
2023-02-03 13:10:27 -0300

    chore: remove conflito de comentarios

--- 2023/02/baca_feature_atualiza_notificacoes_desbloqueio.sql
+++ 2023/02/baca_feature_atualiza_notificacoes_desbloqueio.sql
@@ -2,8 +2,6 @@ DECLARE
 
 BEGIN
   
-  -- Cautelar
-  
   UPDATE tbgen_notif_msg_cadastro tnmc
   SET tnmc.dshtml_mensagem = 'Cooperado,<br /><br />Você recebeu um crédito Pix de #valor_pix, porém esta transação está em análise. Este valor ficará com bloqueio cautelar por no máximo 72 horas. Após a conclusão da análise, dentro do prazo citado, você receberá uma nova notificação com o resultado da avaliação.<br /><br />Valor bloqueado: #valor_bloqueado_pix<br />Pagador: #nome_pagador<br />Instituição: #instituicao_pagador<br />Identificação: #identificao_transacao<br />Descrição: <br />Data hora transação: #data_hora_transacao<br /><br />Este é uma procedimento obrigatório de acordo com o Regulamento Pix, emitido pelo Banco Central do Brasil para garantir mais segurança às suas transações.'
   WHERE tnmc.cdmensagem = 10524
@@ -23,8 +21,6 @@ BEGIN
   SET tnap.dsvariaveis_mensagem = '<br />#data_hora_transacao - Data da Transação (Ex.: 17/02/2022 11:06:32)<br />#valor_pix - Valor do Pix (Ex.: 2.000,00)<br />#nome_pagador - Nome do Pagador - ("João da Silva")<br />#instituicao_pagador - Instituição do Pagador ("Viacredi")<br />#identificao_transacao - Identificação da Transação (E18236120202011062016s0644601CBP)<br />#valor_desbloqueado_pix - Valor desbloqueado (Ex.: 2.000,00)<br />#data_hora_bloqueio - Data e hora do bloqueio (Ex.: 18/02/2022 13:12:54)'
   WHERE tnap.cdmensagem = 10525
   AND tnap.cdorigem_mensagem = 13;
-  
-  -- MED
   
   UPDATE tbgen_notif_msg_cadastro tnmc
   SET tnmc.dshtml_mensagem = 'Cooperado, <br><br> Em #data_hora_transacao você recebeu um Pix no valor de #valorpix de #nomepagador e ele foi contestado. O caso foi analisado e identificamos que a contestação é improcedente, então o valor de #valor_bloqueado_pix em #data_hora_bloqueio foi liberado em sua conta. Esta é uma medida de segurança do Pix. <br><br>Agradecemos a sua compreensão.',
