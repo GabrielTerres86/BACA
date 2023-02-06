bb139412d859b3544d67a4ee37b9c00089687339
Renato Darosci <premiersoft-renato.darosci@ailos.coop.br>
2023-02-06 18:08:04 -0300

    Inclusão de novos produtos nas listas de impedimentos para o desligamento da conta.

---
+++ 2023/02/PRB0047805_incluir_cad_produtos.sql
@@ -0,0 +1,87 @@
+DECLARE
+
+   CURSOR cr_crapcop IS
+     SELECT c.cdcooper, t.inpessoa, t.cdtipo_conta
+       FROM tbcc_tipo_conta_coop t
+          , crapcop c
+      WHERE t.cdcooper = c.cdcooper
+        AND c.flgativo = 1
+        AND c.cdcooper <> 3;
+        
+BEGIN
+  
+  INSERT INTO tbcc_produto
+                 (cdproduto
+                 ,dsproduto
+                 ,flgitem_soa
+                 ,flgutiliza_interface_padrao
+                 ,flgenvia_sms
+                 ,flgcobra_tarifa
+                 ,idfaixa_valor
+                 ,flgproduto_api)
+          VALUES (60
+                 ,'POUPANCA'
+                 ,0
+                 ,0
+                 ,0
+                 ,0
+                 ,0
+                 ,0);
+  
+  FOR dados IN cr_crapcop LOOP
+    
+    INSERT INTO tbcc_produtos_coop
+                   (cdcooper
+                   ,tpconta
+                   ,cdproduto
+                   ,nrordem_exibicao
+                   ,tpproduto
+                   ,inpessoa)
+             VALUES(dados.cdcooper
+                   ,dados.cdtipo_conta
+                   ,60
+                   ,40
+                   ,2
+                   ,dados.inpessoa);
+    
+  END LOOP;
+  
+  INSERT INTO tbcc_produto
+                 (cdproduto
+                 ,dsproduto
+                 ,flgitem_soa
+                 ,flgutiliza_interface_padrao
+                 ,flgenvia_sms
+                 ,flgcobra_tarifa
+                 ,idfaixa_valor
+                 ,flgproduto_api)
+          VALUES (61
+                 ,'PACOTE DE SERVICOS'
+                 ,0
+                 ,0
+                 ,0
+                 ,0
+                 ,0
+                 ,0);
+  
+  FOR dados IN cr_crapcop LOOP
+    
+    INSERT INTO tbcc_produtos_coop
+                   (cdcooper
+                   ,tpconta
+                   ,cdproduto
+                   ,nrordem_exibicao
+                   ,tpproduto
+                   ,inpessoa)
+             VALUES(dados.cdcooper
+                   ,dados.cdtipo_conta
+                   ,61
+                   ,41
+                   ,2
+                   ,dados.inpessoa);
+    
+  END LOOP;
+  
+  COMMIT;
+  
+END;
