a2897c7e427b7375a89796e5f75129323d39f1ec
Renato Darosci <premiersoft-renato.darosci@ailos.coop.br>
2023-02-08 16:14:41 -0300

    Inclusão de novos produtos nas listas de impedimentos para o desligamento da conta.

--- 2023/02/PRB0047805_incluir_cad_produtos.sql
+++ 2023/02/PRB0047805_incluir_cad_produtos.sql
@@ -82,6 +82,42 @@ BEGIN
     
   END LOOP;
   
+  INSERT INTO tbcc_produto
+                 (cdproduto
+                 ,dsproduto
+                 ,flgitem_soa
+                 ,flgutiliza_interface_padrao
+                 ,flgenvia_sms
+                 ,flgcobra_tarifa
+                 ,idfaixa_valor
+                 ,flgproduto_api)
+          VALUES (62
+                 ,'CREDITO IMOBILIARIO'
+                 ,0
+                 ,0
+                 ,0
+                 ,0
+                 ,0
+                 ,0);
+  
+  FOR dados IN cr_crapcop LOOP
+    
+    INSERT INTO tbcc_produtos_coop
+                   (cdcooper
+                   ,tpconta
+                   ,cdproduto
+                   ,nrordem_exibicao
+                   ,tpproduto
+                   ,inpessoa)
+             VALUES(dados.cdcooper
+                   ,dados.cdtipo_conta
+                   ,62
+                   ,42
+                   ,2
+                   ,dados.inpessoa);
+    
+  END LOOP;
+  
   COMMIT;
   
 END;
