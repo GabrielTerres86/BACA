51967b59438dfc7b445aa71625181242e86759b3
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2023-02-10 08:05:30 -0300

    Refin pos congelado

--- 2023/01/inc0229532-3.sql
+++ 2023/01/inc0229532-3.sql
@@ -18,14 +18,6 @@ DECLARE
   vr_innivris cecred.crawepr.dsnivori%TYPE;
   vr_progress cecred.crapass.progress_recid%TYPE;
     
-  CURSOR cr_crapass(pr_cdcooper IN cecred.crapass.cdcooper%TYPE
-                   ,pr_progress IN cecred.crapass.progress_recid%TYPE) IS
-    SELECT a.nrdconta 
-      FROM cecred.crapass a
-     WHERE a.cdcooper = pr_cdcooper
-       AND a.progress_recid = pr_progress;
-  rw_crapass cr_crapass%ROWTYPE;
-  
   CURSOR cr_crapcop IS
     SELECT c.cdcooper, c.nmrescop
       FROM cecred.crapcop c
@@ -51,6 +43,7 @@ DECLARE
        AND a.cdsituacao = 1
        AND c.inrisco_acordo <> e.inrisco_refin
        AND c.inrisco_acordo < 10
+       AND e.tpemprst = 2
        AND o.dtrefere = d.dtmvcentral
        AND o.inrisco_melhora IS NULL;
   rw_principal cr_principal%ROWTYPE;
@@ -134,75 +127,6 @@ BEGIN
     dbms_lob.freetemporary(vr_dados_rollback); 
     
   END LOOP;
-  
-  vr_cdcooper := 5;
-  vr_nrdconta := 79820;
-  vr_nrctremp := 96969;
-  vr_innivris := 'C';
-  vr_progress := 449072;
-  
-  OPEN cr_crapass(pr_cdcooper => vr_cdcooper
-                 ,pr_progress => vr_progress);
-  FETCH cr_crapass INTO rw_crapass;
-  IF cr_crapass%FOUND THEN
-    UPDATE cecred.tbrisco_operacoes a 
-       SET a.inrisco_rating = risc0004.fn_traduz_nivel_risco(vr_innivris)
-     WHERE a.cdcooper = vr_cdcooper
-       AND a.nrdconta = rw_crapass.nrdconta
-       AND a.nrctremp = vr_nrctremp
-       AND a.tpctrato = 90;
-             
-    UPDATE cecred.crawepr e
-       SET e.dsnivori = vr_innivris
-     WHERE e.cdcooper = vr_cdcooper
-       AND e.nrdconta = rw_crapass.nrdconta 
-       AND e.nrctremp = vr_nrctremp;
-  END IF;
-  CLOSE cr_crapass;
-  
-  vr_nmarqrbk := 'ROLLBACK_RENEG_'||to_char(sysdate,'ddmmyyyy_hh24miss')||'.sql';
-    
-  vr_dados_rollback := NULL;
-
-  dbms_lob.createtemporary(vr_dados_rollback, TRUE, dbms_lob.CALL);
-  dbms_lob.open(vr_dados_rollback, dbms_lob.lob_readwrite);     
-  
-  gene0002.pc_escreve_xml(vr_dados_rollback
-                         ,vr_texto_rollback
-                         ,'UPDATE cecred.tbrisco_operacoes o ' || chr(13) || 
-                          '   SET o.inrisco_rating = '|| risc0004.fn_traduz_nivel_risco('E') || chr(13) ||
-                          ' WHERE o.cdcooper = ' || vr_cdcooper  || chr(13) ||
-                          '   AND o.nrdconta = ' || vr_nrdconta  || chr(13) ||
-                          '   AND o.nrctremp = ' || vr_nrctremp  || chr(13) ||
-                          '   AND o.tpctrato = 90;' ||chr(13)||chr(13), FALSE);    
-  
-  gene0002.pc_escreve_xml(vr_dados_rollback
-                         ,vr_texto_rollback
-                         ,'UPDATE cecred.crawepr e ' || chr(13) || 
-                          '   SET e.dsnivori = ''E'''|| chr(13) ||
-                          ' WHERE e.cdcooper = ' || vr_cdcooper  || chr(13) ||
-                          '   AND e.nrdconta = ' || vr_nrdconta  || chr(13) ||
-                          '   AND e.nrctremp = ' || vr_nrctremp  || ';' ||chr(13)||chr(13), FALSE);   
-  
-  cecred.gene0002.pc_escreve_xml(vr_dados_rollback, vr_texto_rollback, 'COMMIT;'||chr(13), FALSE);
-  cecred.gene0002.pc_escreve_xml(vr_dados_rollback, vr_texto_rollback, chr(13), TRUE); 
-               
-  cecred.GENE0002.pc_solicita_relato_arquivo(pr_cdcooper  => 3                             
-                                            ,pr_cdprogra  => 'ATENDA'                      
-                                            ,pr_dtmvtolt  => trunc(SYSDATE)                
-                                            ,pr_dsxml     => vr_dados_rollback             
-                                            ,pr_dsarqsaid => vr_nmdireto||'/'||vr_nmarqrbk 
-                                            ,pr_flg_impri => 'N'                           
-                                            ,pr_flg_gerar => 'S'                           
-                                            ,pr_flgremarq => 'N'                           
-                                            ,pr_nrcopias  => 1                             
-                                            ,pr_des_erro  => vr_dscritic);                 
-          
-  IF TRIM(vr_dscritic) IS NOT NULL THEN
-    RAISE vr_exc_erro;
-  END IF;   
-  dbms_lob.close(vr_dados_rollback);
-  dbms_lob.freetemporary(vr_dados_rollback); 
     
   COMMIT;
   
