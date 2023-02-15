252c5065f71bae002618107edfe41c719d1c07c6
Jose Diego Zunino <jose.zunino@ailos.coop.br>
2023-02-15 19:48:15 +0000

    Updated script_criacao_parametro.sql

--- 2023/02/script_criacao_parametro.sql
+++ 2023/02/script_criacao_parametro.sql
@@ -1,10 +1,10 @@
 BEGIN
   -- insere todos os historicos de convenio que validam a duplicidade                                         
-  INSERT INTO crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
+  INSERT INTO cecred.crapprm (nmsistem, cdcooper, cdacesso, dstexprm, dsvlrprm)
   VALUES ('CRED', 0, 'HIST_TRIBUTOS_VAL_DUPLI', 'Lista de tributos(histórico) que valida a duplicidade','2515,3361,3192,3194,3196,3198,3200,3202,3204');
   
   --Aumenta o tempo de validação para 20 segundos
-  UPDATE crapprm
+  UPDATE cecred.crapprm
      SET dsvlrprm = 20
    WHERE cdacesso = 'TEMPO_FATURA_TRIBUTOS';
   
