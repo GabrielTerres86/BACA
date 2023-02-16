b581375d5abfe425ed9050777f72ece985567d72
Jose Diego Zunino <jose.zunino@ailos.coop.br>
2023-02-16 12:39:16 +0000

    Updated script_criacao_parametro.sql

--- 2023/02/script_criacao_parametro.sql
+++ 2023/02/script_criacao_parametro.sql
@@ -3,7 +3,7 @@ BEGIN
   VALUES ('CRED', 0, 'HIST_TRIBUTOS_VAL_DUPLI', 'Lista de tributos(histórico) que valida a duplicidade','2515,3361,3192,3194,3196,3198,3200,3202,3204');
 
   UPDATE cecred.crapprm
-     SET dsvlrprm = 20
+     SET dsvlrprm = 32
    WHERE cdacesso = 'TEMPO_FATURA_TRIBUTOS';
   
   COMMIT;
