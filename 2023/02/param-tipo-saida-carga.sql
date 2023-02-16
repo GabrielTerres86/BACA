2dad51d8aab5f057fa0ef115873b808c6aa79030
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2023-02-16 14:05:49 -0300

    Adicionado parametro ded controle de execucao de saidas

---
+++ 2023/02/param-tipo-saida-carga.sql
@@ -0,0 +1,13 @@
+DECLARE
+  CURSOR cr_crapcop IS
+    SELECT c.cdcooper
+      FROM crapcop c
+     WHERE c.flgativo = 1;
+  rw_crapcop cr_crapcop%ROWTYPE;
+BEGIN
+  FOR rw_crapcop IN cr_crapcop LOOP
+    insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+    values ('CRED', rw_crapcop.cdcooper, 'TPEXEC_SAIDAS_CENTRAL', 'Tipo de execucao das saidas geradas na carga da central (1-saidas diarias / 2-saidas mensais)', '2');
+  END LOOP;
+  COMMIT;
+END;
