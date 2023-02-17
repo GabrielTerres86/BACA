8443bc9e3eabfdbe73c8086c7aed2c676e5a7df7
Renato Darosci <premiersoft-renato.darosci@ailos.coop.br>
2023-02-17 10:25:43 -0300

    Ajustar as datas de demissão, que estavam anterior as datas de admissão.

--- 2023/02/PRB0047805_Atualiza_Dtdemiss.sql
+++ 2023/02/PRB0047805_Atualiza_Dtdemiss.sql
@@ -24,15 +24,15 @@ DECLARE
            select 1 as cdcooper, decode(pr_globalname, vc_bdprod, 8288445,91711495) as nrdconta, to_date('23/02/2021','dd/mm/yyyy') as dtdemiss, 11 as  cdmotdem from dual union all
            select 1 as cdcooper, decode(pr_globalname, vc_bdprod, 6132618,93867328) as nrdconta, to_date('14/05/2021','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
            select 1 as cdcooper, decode(pr_globalname, vc_bdprod, 12354651,87645289) as nrdconta, to_date('14/05/2021','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
-           select 2 as cdcooper, decode(pr_globalname, vc_bdprod, 950165,99049775) as nrdconta, to_date('13/05/2020','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
-           select 2 as cdcooper, decode(pr_globalname, vc_bdprod, 1019600,98980335) as nrdconta, to_date('12/02/2021','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
-           select 2 as cdcooper, decode(pr_globalname, vc_bdprod, 986658,99013282) as nrdconta, to_date('30/07/2020','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
+           select 2 as cdcooper, decode(pr_globalname, vc_bdprod, 950165,99049775) as nrdconta, to_date('23/06/2022','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
+           select 2 as cdcooper, decode(pr_globalname, vc_bdprod, 1019600,98980335) as nrdconta, to_date('30/07/2021','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
+           select 2 as cdcooper, decode(pr_globalname, vc_bdprod, 986658,99013282) as nrdconta, to_date('28/07/2021','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
            select 5 as cdcooper, decode(pr_globalname, vc_bdprod, 303380,99696550) as nrdconta, to_date('08/11/2021','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
            select 9 as cdcooper, decode(pr_globalname, vc_bdprod, 333964,99665972) as nrdconta, to_date('15/06/2020','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
-           select 10 as cdcooper, decode(pr_globalname, vc_bdprod, 180602,99819333) as nrdconta, to_date('15/12/2015','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
+           select 10 as cdcooper, decode(pr_globalname, vc_bdprod, 180602,99819333) as nrdconta, to_date('01/09/2021','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
            select 11 as cdcooper, decode(pr_globalname, vc_bdprod, 628417,99371529) as nrdconta, to_date('02/10/2020','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
-           select 11 as cdcooper, decode(pr_globalname, vc_bdprod, 876194,99123746) as nrdconta, to_date('18/02/2021','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
-           select 11 as cdcooper, decode(pr_globalname, vc_bdprod, 655244,99344696) as nrdconta, to_date('27/05/2016','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual
+           select 11 as cdcooper, decode(pr_globalname, vc_bdprod, 876194,99123746) as nrdconta, to_date('30/01/2021','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual union all
+           select 11 as cdcooper, decode(pr_globalname, vc_bdprod, 655244,99344696) as nrdconta, to_date('06/10/2020','dd/mm/yyyy')  as dtdemiss, 11 as  cdmotdem from dual
            ) a
      WHERE 1=1
        AND t.cdcooper = a.cdcooper
@@ -86,6 +86,7 @@ BEGIN
     update cecred.crapass a
        set a.dtdemiss = nvl(rg_crapass.dtdemiss_new, a.dtdemiss)
           ,a.cdmotdem = nvl(rg_crapass.cdmotdem_new, a.cdmotdem)
+          ,a.dtelimin = nvl(rg_crapass.dtdemiss_new, a.dtdemiss)
      where a.cdcooper = rg_crapass.cdcooper
        and a.nrdconta = rg_crapass.nrdconta;
     
