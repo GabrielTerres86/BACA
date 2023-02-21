c648bdaacadc3f5cf5ffc7963d0478cec06f6c25
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2023-02-21 14:44:04 -0300

    Carga inicial Acordos Ativos

---
+++ 2023/02/carga_inicial_acordos_8_10_12.sql
@@ -0,0 +1,50 @@
+BEGIN
+
+  INSERT INTO GESTAODERISCO.tbrisco_operacao_acordo(cdcooper
+                                                   ,nrdconta
+                                                   ,nrctremp
+                                                   ,nracordo
+                                                   ,cdproduto
+                                                   ,dsnivel_acordo
+                                                   ,tpmanutencao
+                                                   ,dtpagamento_entrada)
+    SELECT cdcooper
+          ,nrdconta
+          ,decode(cdproduto, 4, nrborder, nrctremp) nrctremp
+          ,nracordo
+          ,cdproduto
+          ,RISC0004.fn_traduz_risco(inrisco_acordo) dsnivel_acordo
+          ,tpmanutencao
+          ,dtpagamento_entrada
+      FROM (SELECT a.nracordo
+                  ,a.cdcooper
+                  ,a.nrdconta
+                  ,CASE c.cdorigem
+                     WHEN 1 THEN 10 -- Adp
+                     WHEN 2 THEN 90 -- Emprestimo
+                     WHEN 3 THEN 90 -- Emprestimo
+                     WHEN 4 THEN 3  -- Desconto de titulo
+                     ELSE 0
+                   END cdproduto
+                  ,c.nrctremp
+                  ,c.inrisco_acordo
+                  ,a.cdsituacao
+                  ,1 AS tpmanutencao
+                  ,c.dtpagamento_entrada
+                  ,(SELECT tc.nrborder
+                      FROM tbdsct_titulo_cyber tc
+                     WHERE tc.cdcooper = a.cdcooper
+                       AND tc.nrdconta = a.nrdconta
+                       AND tc.nrctrdsc = c.nrctremp) nrborder
+              FROM cecred.tbrecup_acordo          a
+                  ,cecred.tbrecup_acordo_contrato c
+             WHERE a.nracordo = c.nracordo
+               AND a.cdcooper IN (8, 10, 12)
+               AND c.cdmodelo = 2
+               AND a.cdsituacao = 1);
+  COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    raise_application_error(-20010, 'Erro ao inserir tbrisco_operacao_acordo: '||SQLERRM);
+END;
