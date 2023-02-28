67a8ac492d0ee6cc9f1214b8203147236b2ca6ad
Rafael Jose Spengler <rafael.spengler@ailos.coop.br>
2023-02-28 17:56:54 +0000

    Updated task537151_task537237_altera_notificacao_limites_pix.sql

--- 2023/02/task537151_task537237_altera_notificacao_limites_pix.sql
+++ 2023/02/task537151_task537237_altera_notificacao_limites_pix.sql
@@ -14,7 +14,7 @@ BEGIN
 	UPDATE CECRED.TBGEN_NOTIF_MSG_CADASTRO SET DSHTML_MENSAGEM = '#NomeCooperado, <br><br>O ajuste de limite Pix que você solicitou foi realizado com sucesso. <br><br>Dados da Conta: <br><br>Nome: #NomeContaCadastrada <br>Banco: #BancoContaCadastrada <br>Agência: #AgenciaContaCadastrada <br>Conta: #NumeroContaCadastrada <br>Novos limites: <br><br>DIURNO <br>Período: R$ #LimiteDiurno <br><br>NOTURNO <br>Período: R$ #LimiteNoturno <br><br>Para mais informações sobre Limites Pix, acesse a seção de dúvidas.'
 	WHERE CDORIGEM_MENSAGEM = 13 AND CDMENSAGEM = 9282;
 	
-	UPDATE CECRED.TBGEN_NOTIF_AUTOMATICA_PRM SET DSVARIAVEIS_MENSAGEM = '<br>#nomeCooperado - Nome do cooperado<br>#NomeContaCadastrada - Nome do beneficiario da conta cadastrada<br>#BancoContaCadastrada - Código e descrição do banco da conta cadastrada<br>#AgenciaContaCadastrada - Agencia da conta cadastrada<br>#NumeroContaCadastrada - Número da conta cadastrada<br>#LimiteDiurno - Valor do novo limite do período diurno (Ex.: 1000,00)<br>#LimiteNoturno - Valor do novo limite do período noturno (Ex.: 1000,00)'
+	UPDATE CECRED.TBGEN_NOTIF_AUTOMATICA_PRM SET DSVARIAVEIS_MENSAGEM = '<br>#nomeCooperado - Nome do cooperado<br>#NomeContaCadastrada - Nome do beneficiário da conta cadastrada<br>#BancoContaCadastrada - Código e descrição do banco da conta cadastrada<br>#AgenciaContaCadastrada - Agência da conta cadastrada<br>#NumeroContaCadastrada - Número da conta cadastrada<br>#LimiteDiurno - Valor do novo limite do período diurno (Ex.: 1000,00)<br>#LimiteNoturno - Valor do novo limite do período noturno (Ex.: 1000,00)'
 	WHERE CDORIGEM_MENSAGEM = 13 AND CDMOTIVO_MENSAGEM = 46 AND CDMENSAGEM = 9282;
 COMMIT;
 END;
