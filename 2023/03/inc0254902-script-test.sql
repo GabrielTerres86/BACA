1afc7783f647ad5628e64716013740fc7d8b7210
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2023-03-07 09:33:42 -0300

    Incluir nrpropostas nao contributario

---
+++ 2023/03/inc0254902-script-test.sql
@@ -0,0 +1,14 @@
+begin
+insert into cecred.tbseg_nrproposta values ('202304289955', null,1);
+insert into cecred.tbseg_nrproposta values ('202304289956', null,1);
+insert into cecred.tbseg_nrproposta values ('202304289957', null,1);
+insert into cecred.tbseg_nrproposta values ('202304289958', null,1);
+insert into cecred.tbseg_nrproposta values ('202304289959', null,1);
+insert into cecred.tbseg_nrproposta values ('202304289960', null,1);
+insert into cecred.tbseg_nrproposta values ('202304289961', null,1);
+insert into cecred.tbseg_nrproposta values ('202304289962', null,1);
+insert into cecred.tbseg_nrproposta values ('202304289963', null,1);
+insert into cecred.tbseg_nrproposta values ('202304289964', null,1);
+insert into cecred.tbseg_nrproposta values ('202304289965', null,1);
+commit;
+end;
