285545f92c41ac003d28d24f20c3a35e8c5be66c
Jose Diego Zunino <jose.zunino@ailos.coop.br>
2023-03-13 12:34:00 +0000

    Added atualizao_parametro_RFB.sql to /2023/03

---
+++ 2023/03/atualizao_parametro_RFB.sql
@@ -0,0 +1,8 @@
+UPDATE CECRED.TBCONV_CANALCOOP_LIBERADO CL
+   SET CL.FLGLIBERADO = 1  --ativar
+ WHERE CL.CDCANAL = 2   --apenas caixa
+   AND CL.IDSEQCONVELIB IN (SELECT LIB.IDSEQCONVELIB
+                              FROM CECRED.TBCONV_LIBERACAO LIB
+                             WHERE LIB.CDCOOPER = 5
+                               AND LIB.CDEMPRES = 'D0100'
+                               AND LIB.CDCONVEN = 0);
