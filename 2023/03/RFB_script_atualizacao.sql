a7ccc722ec8a7d560d80d8193dd4064a02c2f843
Jose Diego Zunino <jose.zunino@ailos.coop.br>
2023-03-16 19:16:50 +0000

    Updated RFB_script_atualizacao.sql

--- 2023/03/RFB_script_atualizacao.sql
+++ 2023/03/RFB_script_atualizacao.sql
@@ -1,4 +1,5 @@
 Begin
-
+  insert into crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) 
+       values ('CRED', 0, 'RFB_AGENCIA_ARQ', 'Numero da Agencia enviada no arquivo para a Receita', '0001');
   commit;
 end;
