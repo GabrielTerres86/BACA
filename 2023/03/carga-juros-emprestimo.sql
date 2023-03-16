737218a99285afcc148aa82f7fd6f27e4806a027
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2023-03-16 15:24:12 -0300

    Separação update transmissao rating

--- 2023/03/carga-juros-emprestimo.sql
+++ 2023/03/carga-juros-emprestimo.sql
@@ -42,61 +42,13 @@ DECLARE
        AND c.flgativo = 1;
   rw_principal cr_principal%ROWTYPE;
   
-  CURSOR cr_ope IS
-    SELECT ROWID dsrowid
-      FROM cecred.tbrisco_operacoes
-     WHERE dhalteracao IS NOT NULL
-       AND dhtransmissao IS NULL;
-  TYPE typ_ope IS TABLE OF cr_ope%ROWTYPE INDEX BY PLS_INTEGER;
-  vr_tab_ope typ_ope;
-  
-  CURSOR cr_nrc IS
-    SELECT ROWID dsrowid
-      FROM cecred.crapnrc
-     WHERE dhalteracao IS NOT NULL
-       AND dhtransmissao IS NULL;
-  TYPE typ_nrc IS TABLE OF cr_nrc%ROWTYPE INDEX BY PLS_INTEGER;
-  vr_tab_nrc typ_nrc;
-  
 BEGIN
  
  UPDATE crapprm SET dsvlrprm = '2' WHERE nmsistem = 'CRED' AND cdcooper IN (1, 16) AND cdacesso = 'EXECUTAR_CARGA_CENTRAL';
  UPDATE crapprm SET dsvlrprm = '2' WHERE nmsistem = 'CRED' AND cdacesso = 'TPEXEC_SAIDAS_CENTRAL';
  
  UPDATE gestaoderisco.tbrisco_central_carga c SET c.cdstatus = 7;
- 
- OPEN cr_ope;
- LOOP
-   FETCH cr_ope BULK COLLECT INTO vr_tab_ope LIMIT 5000;
-   EXIT WHEN vr_tab_ope.count = 0;
-   BEGIN
-     FORALL idx IN INDICES OF vr_tab_ope SAVE EXCEPTIONS
-       UPDATE cecred.tbrisco_operacoes
-          SET dhtransmissao = to_date('01/01/2000 08:00:00', 'DD/MM/RRRR HH24:MI:SS')
-        WHERE ROWID = vr_tab_ope(idx).dsrowid;
-     EXCEPTION
-      WHEN OTHERS THEN
-        dbms_output.put_line('Erro ao Atualizar tbrisco_operacoes');
-   END;
- END LOOP;
- CLOSE cr_ope;
-   
- OPEN cr_nrc;
- LOOP
-   FETCH cr_nrc BULK COLLECT INTO vr_tab_nrc LIMIT 5000;
-   EXIT WHEN vr_tab_nrc.count = 0;
-   BEGIN
-     FORALL idx IN INDICES OF vr_tab_nrc SAVE EXCEPTIONS
-       UPDATE cecred.crapnrc
-          SET dhtransmissao = to_date('01/01/2000 08:00:00', 'DD/MM/RRRR HH24:MI:SS')
-        WHERE ROWID = vr_tab_nrc(idx).dsrowid;
-     EXCEPTION
-      WHEN OTHERS THEN
-        dbms_output.put_line('Erro ao Atualizar crapnrc');
-   END;
- END LOOP;
- CLOSE cr_ope;
- 
+  
  DELETE FROM gestaoderisco.tbrisco_juros_emprestimo WHERE cdcooper IN (8, 10, 12);
  
  vr_tab_cr_principal_bulk.DELETE;
