a12f5e8673bb193347c9fe3f1ceff8a9440823c1
Alan Beckhauser <alan.beckhauser@ailos.coop.br>
2023-03-16 08:37:50 -0300

    faltou1

--- 2023/03/Ajuste_INC0255693.sql
+++ 2023/03/Ajuste_INC0255693.sql
@@ -1,6 +1,8 @@
 begin
 update cecred.tbseg_prestamista p set p.NRPROPOSTA = '202304235000' where p.NRPROPOSTA = '770350614753A01';
 update cecred.crawseg p set p.NRPROPOSTA = '202304235000' where p.NRPROPOSTA = '770350614753A01';
+update cecred.tbseg_prestamista p set p.NRPROPOSTA = '202304361869' where p.NRPROPOSTA = '770350561927';
+update cecred.crawseg p set p.NRPROPOSTA = '202304361869' where p.NRPROPOSTA = '770350561927';
 update cecred.tbseg_nrproposta p set p.dhseguro = sysdate where p.NRPROPOSTA = '202304235000';
 Update cecred.craplau l  set l.CDSEQTEL =  '202212139953' where l.IDLANCTO = 76542309;
 Update cecred.craplau l  set l.CDSEQTEL =  '202304400288' where l.IDLANCTO = 77292701;
