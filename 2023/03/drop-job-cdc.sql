81aa74d73c90e206fa663a642d44da850fe9e3b7
Lavínia <lavinia.gesiane@ailos.coop.br>
2023-03-21 11:11:26 -0300

    ajuste

--- 2023/03/drop-job-cdc.sql
+++ 2023/03/drop-job-cdc.sql
@@ -1,6 +1,8 @@
-BEGIN
-  sys.dbms_scheduler.drop_job(job_name => 'CREDITO.JBEPR_BLOQ_PREAPROV_CDC');
+BEGIN  
+  sys.dbms_scheduler.drop_job(job_name => 'CREDITO.JBEPR_BLOQ_PREAPROV_CDC');
 COMMIT;
-  WHEN OTHERS THEN
-    raise_application_error(-20500,SQLERRM);;
+EXCEPTION  
+  WHEN OTHERS THEN    
+	ROLLBACK;    
+    raise_application_error(-20500,SQLERRM);;
 END;
