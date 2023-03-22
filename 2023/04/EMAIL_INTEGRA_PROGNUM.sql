6347894e98502996c6c508ad7baac87efc5cc850
Lucas Eduardo Ranghetti <lucas.ranghetti@ailos.coop.br>
2023-03-22 13:19:04 -0300

    Ajustar e-mail erros arquivo seguros imobiliario

---
+++ 2023/04/EMAIL_INTEGRA_PROGNUM.sql
@@ -0,0 +1,10 @@
+BEGIN
+ update crapprm m 
+    set m.dsvlrprm = 'creditoimobiliario@ailos.coop.br'
+  where m.cdacesso = 'EMAIL_INTEGRA_PROGNUM';
+ commit;
+exception 
+WHEN OTHERS THEN     
+  DBMS_OUTPUT.put_line('Erro ao atualizar EMAIL_INTEGRA_PROGNUM: '|| sqlerrm);
+  ROLLBACK;
+END;
