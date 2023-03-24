b3c5a400e088fd7b37a77f5ea02104006efba6d5
Luciana Leite <premiersoft-luciana.leite@ailos.coop.br>
2023-03-24 16:04:00 -0300

    Ajuste para liquidar parcelas e emprestimo

---
+++ 2023/03/TASK552729-INC0259598.sql
@@ -0,0 +1,25 @@
+BEGIN
+  update cecred.crapepr epr
+  set epr.qtprepag = epr.qtpreemp,
+      epr.vlsdeved = 0,
+      epr.inliquid = 1,
+      epr.vlsdvctr = 0,
+      epr.vlsdevat = 0,
+      epr.dtliquid = sysdate
+   where epr.nrdconta = 1496321 
+     and epr.nrctremp = 40800;
+
+  update cecred.crappep 
+     set vlpagpar = vlparepr,
+         inliquid = 1, 
+         vlsdvatu = 0 
+   where nrdconta = 1496321 
+     and nrctremp = 40800 
+     and nrparepr in (10,11,12);
+   
+  COMMIT;        
+  EXCEPTION
+    WHEN OTHERS THEN
+      ROLLBACK;
+      raise_application_error(-20500,SQLERRM);
+END;
