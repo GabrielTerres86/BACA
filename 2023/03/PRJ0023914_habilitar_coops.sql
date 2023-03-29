a529ed08bd1f0f54816cd137549e5dd40eb10d0a
Odirlei Busana <odirlei.busana@ailos.coop.br>
2023-03-29 13:22:07 -0300

    PRJ0023914 - Habilitar cargas para central de risco ailos+

---
+++ 2023/03/PRJ0023914_habilitar_coops.sql
@@ -0,0 +1,15 @@
+DECLARE
+  
+  
+BEGIN
+ 
+ UPDATE crapprm SET dsvlrprm = '2' WHERE nmsistem = 'CRED' AND cdcooper IN (2,3,5,6,7,9,11,13,14) AND cdacesso = 'EXECUTAR_CARGA_CENTRAL';
+ 
+ UPDATE gestaoderisco.tbrisco_central_carga c 
+    SET c.cdstatus = 7
+  WHERE cdcooper IN (1,2,3,5,6,7,9,11,13,14,16)  
+    AND c.CDSTATUS <> 7;
+
+ COMMIT;
+  
+END;
