31c38a22ec91db20be538ebc5f9d117d473a9f2f
Luciana Leite <premiersoft-luciana.leite@ailos.coop.br>
2023-03-29 10:37:32 -0300

    Ajuste numero da conta

--- 2023/03/TASK552729-INC0259598.sql
+++ 2023/03/TASK552729-INC0259598.sql
@@ -6,14 +6,14 @@ BEGIN
       epr.vlsdvctr = 0,
       epr.vlsdevat = 0,
       epr.dtliquid = sysdate
-   where epr.nrdconta = 1496321 
+   where epr.nrdconta = 14963213 
      and epr.nrctremp = 40800;
 
   update cecred.crappep 
      set vlpagpar = vlparepr,
          inliquid = 1, 
          vlsdvatu = 0 
-   where nrdconta = 1496321 
+   where nrdconta = 14963213
      and nrctremp = 40800 
      and nrparepr in (10,11,12);
    
@@ -23,3 +23,4 @@ BEGIN
       ROLLBACK;
       raise_application_error(-20500,SQLERRM);
 END;
+/
