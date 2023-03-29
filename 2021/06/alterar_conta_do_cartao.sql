6af51af2b8e4c839b6fc0ad1048e2292c06e4537
Djonata <djonata.wehmuth@ailos.coop.br>
2023-03-29 15:32:15 -0300

    Update alterar_conta_do_cartao.sql

--- 2021/06/alterar_conta_do_cartao.sql
+++ 2021/06/alterar_conta_do_cartao.sql
@@ -38,14 +38,12 @@ BEGIN
         ,card.nrcpftit = vr_cpf_titular
         ,card.qtsenerr = 0
         ,card.inacetaa = 1
-        ,card.NRCTRCRD = 199999713
    WHERE card.nrcrcard = vr_cartao;
    
   UPDATE crawcrd card
      SET card.cdcooper = vr_cooperativa
         ,card.nrdconta = vr_conta
         ,card.nrcpftit = vr_cpf_titular
-        ,card.NRCTRCRD = 199999713
    WHERE card.nrcrcard = vr_cartao;
   COMMIT;
 END;
