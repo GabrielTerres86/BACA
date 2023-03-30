01dc6d215e71d6660f7a732201cfb43985f23a28
Adriel Dias <user_vector08@tribanco.com.br>
2023-03-30 09:26:22 -0300

    Retirada da conta do tipo salário.

--- 2023/03/INC026187_inativa_pacotes_contas_inativas.sql
+++ 2023/03/INC026187_inativa_pacotes_contas_inativas.sql
@@ -2,6 +2,7 @@ begin
 	update cecred.Tbtarif_Contas_Pacote tcp1 set tcp1.flgsituacao = 2 where exists (select tcp2.nrdconta from cecred.crapass ca
 	inner join cecred.Tbtarif_Contas_Pacote tcp2 on tcp2.nrdconta = ca.nrdconta and tcp2.cdcooper = ca.cdcooper
 	where ca.dtdemiss is not null
+	and ca.cdtipcta <> 21
 	and tcp2.flgsituacao = 1
 	and tcp1.cdcooper = tcp2.cdcooper
 	and tcp1.nrdconta = tcp2.nrdconta
