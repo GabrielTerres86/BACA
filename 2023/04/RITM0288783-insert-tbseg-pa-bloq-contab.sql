8a3ea2bf95aa39a7dfafd07c795573dd4dbb0a76
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2023-04-04 15:47:47 -0300

    Carga de PAs bloqueados pela contabilidade

---
+++ 2023/04/RITM0288783-insert-tbseg-pa-bloq-contab.sql
@@ -0,0 +1,178 @@
+BEGIN 
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,7);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,27);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,33);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,38);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,41);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,60);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,62);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,66);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,75);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,80);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,84);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,90);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,91);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,94);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,95);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,100);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,121);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,128);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,133);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,135);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,158);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,159);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,161);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,162);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,183);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,195);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,305);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,306);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,307);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,308);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (1,800);
+
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,1);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,2);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,3);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,4);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,5);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,6);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,7);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,11);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,90);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,91);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,114);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,133);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,135);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,800);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (2,999);
+
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,1);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,2);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,4);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,5);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,6);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,7);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,8);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,9);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,10);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,11);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,12);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,13);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,15);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,90);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,108);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,110);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,123);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,127);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,128);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,150);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,164);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,165);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,189);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,800);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,928);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (3,944);
+
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (5,1);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (5,2);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (5,3);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (5,4);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (5,8);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (5,90);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (5,91);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (5,133);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (5,800);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (5,999);
+
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (6,7);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (6,8);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (6,90);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (6,91);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (6,133);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (6,800);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (6,999);
+
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (7,5);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (7,90);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (7,91);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (7,114);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (7,133);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (7,158);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (7,178);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (7,800);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (7,999);
+
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (8,90);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (8,91);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (8,114);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (8,133);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (8,800);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (8,999);
+
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (9,90);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (9,91);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (9,93);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (9,108);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (9,110);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (9,133);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (9,401);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (9,408);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (9,409);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (9,408);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (9,800);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (9,999);
+
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (10,4);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (10,90);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (10,91);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (10,133);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (10,800);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (10,999);
+
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (11,90);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (11,91);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (11,114);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (11,128);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (11,133);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (11,800);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (11,999);
+
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (12,90);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (12,91);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (12,133);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (12,800);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (12,999);
+
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (13,11);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (13,90);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (13,91);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (13,111);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (13,115);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (13,133);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (13,301);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (13,302);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (13,800);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (13,999);
+
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (14,90);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (14,91);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (14,133);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (14,800);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (14,999);
+
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (16,1);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (16,90);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (16,91);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (16,95);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (16,115);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (16,128);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (16,133);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (16,178);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (16,300);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (16,800);
+	insert into seguro.tbseg_pa_bloq_contab(cdcooper, cdagenci) values (16,999);
+
+COMMIT;
+END;
+
