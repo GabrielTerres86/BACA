ec9c93de5a370a215fbafe30690110ef8a19705b
Guilherme Breis <premiersoft-guilherme.pinheiro@ailos.coop.br>
2023-04-11 09:04:32 -0300

    deleta renegociações incorretas.

--- 2023/04/update-viacredi-6793215.sql
+++ 2023/04/update-viacredi-6793215.sql
@@ -6,6 +6,16 @@ BEGIN
          AND nrdconta = 3779815
          AND nrctremp = 6793215;
 
+  DELETE FROM CECRED.tbepr_renegociacao
+   WHERE cdcooper = 1
+         AND nrdconta = 3779815
+         AND nrctremp IN (6793217, 6793219);
+
+  DELETE FROM CECRED.tbepr_renegociacao_contrato
+   WHERE cdcooper = 1
+         AND nrdconta = 3779815
+         AND nrctremp IN (6793217, 6793219);
+
   COMMIT;
 EXCEPTION
   WHEN OTHERS THEN
