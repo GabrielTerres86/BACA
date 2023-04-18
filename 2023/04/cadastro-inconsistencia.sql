42c8e19d31318847a16dec76d25128df59edfd13
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2023-04-18 10:57:22 -0300

    Cadastro de inconsistencias para carga de cartoes da central

---
+++ 2023/04/cadastro-inconsistencia.sql
@@ -0,0 +1,55 @@
+DECLARE
+
+  CURSOR cr_cop IS
+    SELECT cdcooper 
+      FROM cecred.crapcop 
+     WHERE flgativo = 1;
+  rw_cop cr_cop%ROWTYPE;
+  
+  CURSOR cr_email(pr_cdcooper IN cecred.crapcop.cdcooper%TYPE) IS
+    SELECT e.dsendereco_email 
+      FROM cecred.tbgen_inconsist_email_grp e 
+     WHERE e.idinconsist_grp = 5
+       AND e.cdcooper = pr_cdcooper;
+  rw_email cr_email%ROWTYPE;
+  
+  vr_idinconsist cecred.tbgen_inconsist_grp.idinconsist_grp%TYPE;
+BEGIN
+
+  INSERT INTO cecred.tbgen_inconsist_grp
+    (idinconsist_grp,
+     nminconsist_grp,
+     tpconfig_email,
+     dsassunto_email,
+     tpperiodicidade_email)
+  VALUES
+    ((SELECT MAX(idinconsist_grp) + 1 FROM cecred.tbgen_inconsist_grp),
+     'Carga de Cartoes da Central de Risco',
+     2,
+     'Erro na carga de cartoes',
+     1)
+  RETURNING idinconsist_grp INTO vr_idinconsist;
+
+  FOR rw_cop IN cr_cop LOOP
+    INSERT INTO cecred.tbgen_inconsist_acesso_grp
+      (cdcooper, idinconsist_grp, cddepart)
+    VALUES
+      (rw_cop.cdcooper, vr_idinconsist, 20);
+  
+    INSERT INTO cecred.tbgen_inconsist_acesso_grp
+      (cdcooper, idinconsist_grp, cddepart)
+    VALUES
+      (rw_cop.cdcooper, vr_idinconsist, 4);
+    
+    FOR rw_email IN cr_email(pr_cdcooper => rw_cop.cdcooper) LOOP
+      INSERT INTO cecred.tbgen_inconsist_email_grp
+        (cdcooper, idinconsist_grp, dsendereco_email)
+      VALUES
+        (rw_cop.cdcooper, vr_idinconsist, rw_email.dsendereco_email);
+    END LOOP;
+    
+  END LOOP;
+  
+  COMMIT;
+
+END;
