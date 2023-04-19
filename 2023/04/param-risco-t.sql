d41f39f09a625e34d46e0c8287c98d880596b8aa
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2023-04-19 10:13:58 -0300

    Adicionado novo parametro/valor

---
+++ 2023/04/param-risco-t.sql
@@ -0,0 +1,14 @@
+DECLARE
+  CURSOR cr_crapcop IS
+    SELECT cdcooper FROM cecred.crapcop WHERE flgativo = 1;
+  rw_crapcop cr_crapcop%ROWTYPE;
+BEGIN
+  FOR rw_crapcop IN cr_crapcop LOOP
+    UPDATE crapprm
+       SET dsvlrprm = dsvlrprm || ';0'
+     WHERE cdcooper = rw_crapcop.cdcooper
+       AND UPPER(nmsistem) = 'CRED' 
+       AND UPPER(cdacesso) = 'RISCO_CARTAO_BACEN';
+  END LOOP;
+  COMMIT;
+END;
