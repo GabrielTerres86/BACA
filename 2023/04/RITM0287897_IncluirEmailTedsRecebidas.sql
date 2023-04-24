a84ad4ed1df35635ab7d22d30c62414f434f4a95
Jose Dill <jose.junior@premiersoft.net>
2023-04-24 16:55:32 -0300

    Ajustado o campo para incluir emails
    Devido ao tamanho do campo do ieptb, incluimos o email no campo de cobrança. #563529

--- 2023/04/RITM0287897_IncluirEmailTedsRecebidas.sql
+++ 2023/04/RITM0287897_IncluirEmailTedsRecebidas.sql
@@ -1,7 +1,6 @@
 BEGIN
   UPDATE cecred.tbcobran_param_protesto tpp
-     SET tpp.dsemail_cobranca = tpp.dsemail_cobranca || ';' || 'contabilidade01@ailos.coop.br',
-         tpp.dsemail_ieptb    = tpp.dsemail_ieptb || ';' || 'conciliacao@cartoriosdeprotesto.br';
+     SET tpp.dsemail_cobranca = tpp.dsemail_cobranca || ';' || 'contabilidade01@ailos.coop.br;conciliacao@cartoriosdeprotesto.org.br';
   COMMIT;         
 EXCEPTION
   WHEN OTHERS THEN
