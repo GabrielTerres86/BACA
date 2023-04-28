997053d52b1c97b89062e7f2a77b88c868ee0db5
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2023-04-28 13:34:59 -0300

    Atualizar dtmovimento para casos de inadimplentes

---
+++ 2023/04/update-craplau-inadimplentes.sql
@@ -0,0 +1,13 @@
+begin
+
+	update cecred.craplau l
+	   set l.dtmvtolt = to_date('29/10/2022','dd/mm/yyyy')
+	 where cecred.craplau l where l.cdseqtel in ('202300387522','202300387518','202301012871');
+	 
+	update cecred.craplau l
+	   set l.dtmvtolt = to_date('30/10/2022','dd/mm/yyyy')
+	 where craplau l where l.cdseqtel in ('202300387520');
+	 
+	 commit;
+	 
+end;
