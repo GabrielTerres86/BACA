9e2b0c160953239b4af84e48f457c896563874e3
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2023-05-03 13:57:52 -0300

    Adicionados registros de-para de modalidades bacen

---
+++ 2023/05/carga-modalidades-bacen-aimaro.sql
@@ -0,0 +1,74 @@
+BEGIN
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (499, 403);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (902, 902);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (1902, 1902);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (299, 1301);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (901, 901);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (299, 203);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (499, 401);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (499, 499);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (1909, 1909);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (302, 302);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (299, 216);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (101, 101);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (299, 215);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (499, 402);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (301, 301);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (201, 213);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (299, 202);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (201, 201);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (299, 299);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (1513, 1513);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (299, 211);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (903, 903);
+
+    insert into GESTAODERISCO.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+    values (990, 990);
+
+  COMMIT;
+  
+END;
