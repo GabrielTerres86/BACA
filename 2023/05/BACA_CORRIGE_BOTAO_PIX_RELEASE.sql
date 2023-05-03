276530c23e1b204312275e06d12d9dc9555216ec
Bruno Henrique de Bastiani <amcom-bruno.bastiani@ailos.coop.br>
2023-05-03 15:14:33 -0300

    correção baca

--- 2023/05/BACA_CORRIGE_BOTAO_PIX_RELEASE.sql
+++ 2023/05/BACA_CORRIGE_BOTAO_PIX_RELEASE.sql
@@ -1,6 +1,6 @@
 UPDATE cecred.tbgen_notif_msg_cadastro tnmc
-SET tnmc.dshtml_mensagem = '#nomeresumido</br></br>Sua chave Pix #chaveenderecamento recebeu uma solicitação de reivindicação de posse por outro usuário.</br></br>Para aceitar ou cancelar a solicitação você deve acessar o menu Minhas Chaves dentro da opção Pix no seu App Ailos.</br></br><b>Se não houver a confirmação de posse, a chave será cancelada de sua conta na Cooperativa.</b></br></br>Em caso de dúvidas, entre em contato com o SAC.</br></br>'
+SET tnmc.dshtml_mensagem = '#nomeresumido</br></br>Sua chave Pix #chaveenderecamento recebeu uma solicitação de reivindicação de posse por outro usuário.</br></br>Para aceitar ou cancelar a solicitação você deve acessar o menu Minhas Chaves dentro da opção Pix no seu App Ailos.</br></br><b>Se não houver a confirmação de posse, a chave será cancelada de sua conta na Cooperativa.</b></br></br>Em caso de dúvidas, entre em contato com o SAC.</br></br>',
+tnmc.dstexto_botao_acao_mobile = 'Pix',
+tnmc.dsmensagem_acao_mobile = 'Pix'
 WHERE tnmc.cdmensagem = 5460
-AND tnmc.cdorigem_mensagem = 13
-AND tnmc.dstexto_botao_acao_mobile = 'Pix'
-AND tnmc.dsmensagem_acao_mobile = 'Pix';
+AND tnmc.cdorigem_mensagem = 13;
