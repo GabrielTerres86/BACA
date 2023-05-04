738078f5af6f59d5e7af4cf0d92272644226cbd7
Guilherme Henrique Boettcher <guilherme.boettcher@cecred.coop.br>
2023-05-04 10:25:18 -0300

    Central de Risco - Remocao campos crapRIS

---
+++ 2023/05/CentralRisco_Remover_Campos_crapRIS.sql
@@ -0,0 +1,8 @@
+alter table cecred.CRAPRIS DROP COLUMN QTDRICLQ;
+/
+alter table cecred.CRAPRIS DROP COLUMN DTTRFPRJ;
+/
+alter table cecred.CRAPRIS DROP COLUMN FLINDBNDES;
+/
+alter table cecred.CRAPRIS DROP COLUMN VLSLD59D;
+/
