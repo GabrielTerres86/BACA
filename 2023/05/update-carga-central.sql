5f3d19cc7003ea4fb1ac8dcaa725758e7db03f3a
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2023-05-04 08:42:36 -0300

    Corrigida data da carga

--- 2023/05/update-carga-central.sql
+++ 2023/05/update-carga-central.sql
@@ -1,4 +1,4 @@
 BEGIN
-  UPDATE GESTAODERISCO.tbrisco_central_carga c SET cdstatus = 7 WHERE c.DTREFERE = '20/04/2023' AND c.TPPRODUTO = 98;
+  UPDATE GESTAODERISCO.tbrisco_central_carga c SET cdstatus = 7 WHERE c.DTREFERE = '02/05/2023' AND c.TPPRODUTO = 98;
   COMMIT;
 END;
