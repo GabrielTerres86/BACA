e4d23b9a4c05f925d7347833c468da3d0554c557
Guilherme Henrique Boettcher <guilherme.boettcher@cecred.coop.br>
2023-05-04 10:13:46 -0300

    RDM0049969 - Ajuste parametros Motor.2

--- 2023/05/RDM0049969_Ajuste_PRM_eapi_mulesoft.sql
+++ 2023/05/RDM0049969_Ajuste_PRM_eapi_mulesoft.sql
@@ -1,21 +1,21 @@
 BEGIN
-  UPDATE crapprm t
+  UPDATE CECRED.crapprm t
    SET t.dsvlrprm = 'eapi-ailosmais-motorcredito/v1/motorcredito/aimaro/enviar'
   WHERE t.cdacesso = 'NOVOMOTOR_URI_WEBSRV';
 
-  UPDATE crapprm t
+  UPDATE CECRED.crapprm t
    SET t.dsvlrprm = 'eapi-ailosmais-motorcredito/v1/motorcredito/aimaro/webservice'
   WHERE t.cdacesso = 'NOVOMOTOR_URI_WSAYLLOS';
 
-  UPDATE crapprm t
+  UPDATE CECRED.crapprm t
    SET t.dsvlrprm = '/eapi-ailosmais-motorcredito/v1/motorcredito/extrairDocumento'
   WHERE t.cdacesso = 'URL_MULE_DWLD_PDF';
 
-  UPDATE crapprm t
+  UPDATE CECRED.crapprm t
    SET t.dsvlrprm = '1'
   WHERE t.cdacesso = 'NOVOMOTOR_ID_MULE';
 
-  UPDATE crapprm t
+  UPDATE CECRED.crapprm t
    SET t.dsvlrprm = '2'
   WHERE t.cdacesso = 'NOVOMOTOR_SECRET_MULE';
 
