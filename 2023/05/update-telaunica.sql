9444836d3d968ae89067a585c083920c4c6f28de
Diógenes Lazzarini <diogenes.lazzarini@ailos.coop.br>
2023-05-04 11:35:12 -0300

    Create update-telaunica.sql

---
+++ 2023/05/update-telaunica.sql
@@ -0,0 +1,9 @@
+begin
+
+update tbgen_analise_credito set nrversao_analise = 1 where idanalise_contrato = 5778545 and nrdconta = 99998270;
+
+update tbgen_analise_credito set nrversao_analise = 2 where idanalise_contrato = 5778546 and nrdconta = 99998270;
+
+commit;
+
+end;
