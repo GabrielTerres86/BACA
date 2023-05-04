77dc5733ae567b0aa6e452de3cc604170e728e38
Leonardo Nogueira da Silva <softtek-leonardo.silva@ailos.coop.br>
2023-05-04 15:13:03 -0300

    Nova notificacao para limites empresa

---
+++ 2023/05/task575552.sql
@@ -0,0 +1,8 @@
+DECLARE
+    codigoMensagem tbgen_notif_msg_cadastro.cdmensagem%TYPE := 0;
+BEGIN
+SELECT (max(cdmensagem) + 1) into codigoMensagem FROM tbgen_notif_msg_cadastro;
+ INSERT INTO tbgen_notif_msg_cadastro (cdmensagem, cdorigem_mensagem, dstitulo_mensagem, dstexto_mensagem, dshtml_mensagem, cdicone, inexibir_banner, inexibe_botao_acao_mobile, dstexto_botao_acao_mobile, cdmenu_acao_mobile, dsmensagem_acao_mobile, inenviar_push) VALUES
+    (codigoMensagem, 13, 'Alteração de limites Pix para Empresas', 'O ajuste de limite Pix para Empresas, que você solicitou foi realizado com sucesso.', '#NomeCooperado, <br><br>O ajuste de limite Pix para Empresas, que você solicitou foi realizado com sucesso. <br>Para mais informações sobre Limites Pix, acesse a seção de dúvidas.', 9, 0, 1, 'Tirar dúvidas!', 1042, 'Tirar dúvidas!', 1);    
+commit;
+END;
