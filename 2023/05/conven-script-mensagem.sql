36246bf7681c07bd0b88e7ff492854e2275ec7c3
Jose Diego Zunino <jose.zunino@ailos.coop.br>
2023-05-05 18:32:48 +0000

    Updated conven-script-mensagem.sql

--- 2023/05/conven-script-mensagem.sql
+++ 2023/05/conven-script-mensagem.sql
@@ -1,6 +1,6 @@
 begin
 UPDATE crapcri cri
-   SET  SET cri.dscritic =  '001 - Sistema sem data de movimento. 1'
+   SET cri.dscritic =  '001 - Sistema sem data de movimento. 1'
   WHERE cri.cdcritic = 1;
   commit;
   end;
