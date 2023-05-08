e86804ec4c47d4c3c2bedf7e3d355b48f5c94f46
Matheus <matheus.leriano@ailos.coop.br>
2023-05-08 15:45:54 -0300

    git pull

---
+++ 2023/05/AjusteSaldosCooperativa10.sql
@@ -0,0 +1,5 @@
+begin  
+  delete from CECRED.crapsda where cdcooper = 10 and dtmvtolt = '11/05/2023';
+  delete from CECRED.crapsld where cdcooper = 10 and dtmvtolt = '11/05/2023';
+  commit;
+end;
