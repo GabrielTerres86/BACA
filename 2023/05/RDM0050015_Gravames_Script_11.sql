4e58b5ce8d2c904563689447b2b31f5410ccae90
Matheus Anklan Braga <f0033714@cecred.coop.br>
2023-05-08 09:33:42 -0300

    GRAVAMES - Inclusao fila processamento

---
+++ 2023/05/RDM0050015_Gravames_Script_11.sql
@@ -0,0 +1,92 @@
+BEGIN
+
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 5 AND b.nrdconta = 249823 AND b.nrctrpro = 106640 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 5 AND b.nrdconta = 243752 AND b.nrctrpro = 20348 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 5 AND b.nrdconta = 250082 AND b.nrctrpro = 22178 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 5 AND b.nrdconta = 253855 AND b.nrctrpro = 23584 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 5 AND b.nrdconta = 250554 AND b.nrctrpro = 22293 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 5 AND b.nrdconta = 253952 AND b.nrctrpro = 23670 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 5 AND b.nrdconta = 254746 AND b.nrctrpro = 24149 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 5 AND b.nrdconta = 254550 AND b.nrctrpro = 24014 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 5 AND b.nrdconta = 255734 AND b.nrctrpro = 24564 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 5 AND b.nrdconta = 256161 AND b.nrctrpro = 24692 AND b.idseqbem = '1';
+  
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 6 AND b.nrdconta = 16411 AND b.nrctrpro = 253830 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 6 AND b.nrdconta = 112534 AND b.nrctrpro = 227696 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 6 AND b.nrdconta = 48682 AND b.nrctrpro = 253216 AND b.idseqbem = '3';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 6 AND b.nrdconta = 247502 AND b.nrctrpro = 252113 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 6 AND b.nrdconta = 247502 AND b.nrctrpro = 252134 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 6 AND b.nrdconta = 247502 AND b.nrctrpro = 252110 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 6 AND b.nrdconta = 247502 AND b.nrctrpro = 252111 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 6 AND b.nrdconta = 247502 AND b.nrctrpro = 252114 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 6 AND b.nrdconta = 247502 AND b.nrctrpro = 252115 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 6 AND b.nrdconta = 247502 AND b.nrctrpro = 252121 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 6 AND b.nrdconta = 247502 AND b.nrctrpro = 252123 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 6 AND b.nrdconta = 255840 AND b.nrctrpro = 271671 AND b.idseqbem = '1';
+  
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 7 AND b.nrdconta = 85600 AND b.nrctrpro = 102585 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 7 AND b.nrdconta = 294250 AND b.nrctrpro = 103467 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 7 AND b.nrdconta = 287504 AND b.nrctrpro = 33720 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 7 AND b.nrdconta = 345270 AND b.nrctrpro = 54830 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 7 AND b.nrdconta = 389471 AND b.nrctrpro = 103186 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 7 AND b.nrdconta = 15281566 AND b.nrctrpro = 103598 AND b.idseqbem = '1';
+
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 10 AND b.nrdconta = 32697 AND b.nrctrpro = 46517 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 10 AND b.nrdconta = 129119 AND b.nrctrpro = 46598 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 10 AND b.nrdconta = 16032195 AND b.nrctrpro = 46297 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 10 AND b.nrdconta = 16032195 AND b.nrctrpro = 46297 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 10 AND b.nrdconta = 16526546 AND b.nrctrpro = 46308 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 10 AND b.nrdconta = 208221 AND b.nrctrpro = 46059 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 10 AND b.nrdconta = 16541693 AND b.nrctrpro = 46372 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 10 AND b.nrdconta = 16561430 AND b.nrctrpro = 46487 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 10 AND b.nrdconta = 16563530 AND b.nrctrpro = 46474 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 10 AND b.nrdconta = 16565061 AND b.nrctrpro = 46479 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 10 AND b.nrdconta = 16565819 AND b.nrctrpro = 46492 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 10 AND b.nrdconta = 16581997 AND b.nrctrpro = 46546 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 10 AND b.nrdconta = 16583353 AND b.nrctrpro = 46548 AND b.idseqbem = '1';
+  
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 12 AND b.nrdconta = 185973 AND b.nrctrpro = 72884 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 12 AND b.nrdconta = 37117 AND b.nrctrpro = 72963 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 12 AND b.nrdconta = 15820181 AND b.nrctrpro = 73230 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 12 AND b.nrdconta = 15952371 AND b.nrctrpro = 67542 AND b.idseqbem = '12';
+  
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 13 AND b.nrdconta = 115991 AND b.nrctrpro = 74655 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 13 AND b.nrdconta = 261360 AND b.nrctrpro = 276515 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 13 AND b.nrdconta = 151769 AND b.nrctrpro = 162539 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 13 AND b.nrdconta = 667226 AND b.nrctrpro = 164426 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 13 AND b.nrdconta = 668320 AND b.nrctrpro = 163736 AND b.idseqbem = '1';
+  
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 14591 AND b.nrctrpro = 46359 AND b.idseqbem = '5';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 20257 AND b.nrctrpro = 30132 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 36587 AND b.nrctrpro = 23251 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 35203 AND b.nrctrpro = 42172 AND b.idseqbem = '31';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 84506 AND b.nrctrpro = 29993 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 47880 AND b.nrctrpro = 32398 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 108588 AND b.nrctrpro = 29553 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 115282 AND b.nrctrpro = 44605 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 120146 AND b.nrctrpro = 45308 AND b.idseqbem = '3';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 139793 AND b.nrctrpro = 42130 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 121630 AND b.nrctrpro = 44111 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 144690 AND b.nrctrpro = 36529 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 148890 AND b.nrctrpro = 42821 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 149047 AND b.nrctrpro = 46142 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 201111 AND b.nrctrpro = 38606 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 169099 AND b.nrctrpro = 35895 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 205508 AND b.nrctrpro = 39627 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 238180 AND b.nrctrpro = 45160 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 245410 AND b.nrctrpro = 46851 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 255939 AND b.nrctrpro = 45996 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 301124 AND b.nrctrpro = 97048 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 262307 AND b.nrctrpro = 46651 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 325414 AND b.nrctrpro = 46617 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 322970 AND b.nrctrpro = 43122 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 329231 AND b.nrctrpro = 46118 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 341479 AND b.nrctrpro = 47494 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 340928 AND b.nrctrpro = 46866 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 344982 AND b.nrctrpro = 44358 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 344982 AND b.nrctrpro = 44358 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 347280 AND b.nrctrpro = 45055 AND b.idseqbem = '1';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 352250 AND b.nrctrpro = 47265 AND b.idseqbem = '2';
+  UPDATE crapbpr b  SET b.flgregct = 1 , b.flgregim = 0 WHERE b.ccooper = 14 AND b.nrdconta = 356050 AND b.nrctrpro = 47123 AND b.idseqbem = '1';
+  
+END;
