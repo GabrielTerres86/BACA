2396dff75061752606b86faec76efefa9e9bffe4
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2023-05-15 16:23:59 -0300

    Atualizavalor do premio dos seguros contributario financiados

---
+++ 2023/05/INC0270113-atualiza-premio-contrib-abril.sql
@@ -0,0 +1,97 @@
+begin 
+
+update cecred.crawseg w set w.vlpremio = 622.74 where cdcooper = 6 and nrdconta = 197173 and nrctrseg = 103570 and nrproposta = '202213450675';  
+update cecred.crapseg s set s.vlpremio = 622.74 where cdcooper = 6 and nrdconta = 197173 and nrctrseg = 103570;   
+update cecred.tbseg_prestamista t set t.vlprodut = 622.74 where cdcooper = 6 and nrdconta = 197173 and nrctrseg = 103570 and t.nrproposta = '202213450675';
+
+update cecred.crawseg w set w.vlpremio = 1120.75 where cdcooper = 8 and nrdconta = 15946 and nrctrseg = 22741 and nrproposta = '202306298719';  
+update cecred.crapseg s set s.vlpremio = 1120.75 where cdcooper = 8 and nrdconta = 15946 and nrctrseg = 22741;   
+update cecred.tbseg_prestamista t set t.vlprodut = 1120.75 where cdcooper = 8 and nrdconta = 15946 and nrctrseg = 22741 and t.nrproposta = '202306298719';
+
+update cecred.crawseg w set w.vlpremio = 7.92 where cdcooper = 10 and nrdconta = 144347 and nrctrseg = 56479 and nrproposta = '202305958656';  
+update cecred.crapseg s set s.vlpremio = 7.92 where cdcooper = 10 and nrdconta = 144347 and nrctrseg = 56479;   
+update cecred.tbseg_prestamista t set t.vlprodut = 7.92 where cdcooper = 10 and nrdconta = 144347 and nrctrseg = 56479 and t.nrproposta = '202305958656';
+
+update cecred.crawseg w set w.vlpremio = 41.54 where cdcooper = 13 and nrdconta = 75132 and nrctrseg = 459482 and nrproposta = '202305261930';  
+update cecred.crapseg s set s.vlpremio = 41.54 where cdcooper = 13 and nrdconta = 75132 and nrctrseg = 459482;   
+update cecred.tbseg_prestamista t set t.vlprodut = 41.54 where cdcooper = 13 and nrdconta = 75132 and nrctrseg = 459482 and t.nrproposta = '202305261930';
+
+update cecred.crawseg w set w.vlpremio = 219.16 where cdcooper = 14 and nrdconta = 14940 and nrctrseg = 71622 and nrproposta = '202305272482';  
+update cecred.crapseg s set s.vlpremio = 219.16 where cdcooper = 14 and nrdconta = 14940 and nrctrseg = 71622;   
+update cecred.tbseg_prestamista t set t.vlprodut = 219.16 where cdcooper = 14 and nrdconta = 14940 and nrctrseg = 71622 and t.nrproposta = '202305272482';
+
+update cecred.crawseg w set w.vlpremio = 1830.32 where cdcooper = 14 and nrdconta = 168424 and nrctrseg = 70768 and nrproposta = '202305271866';  
+update cecred.crapseg s set s.vlpremio = 1830.32 where cdcooper = 14 and nrdconta = 168424 and nrctrseg = 70768;   
+update cecred.tbseg_prestamista t set t.vlprodut = 1830.32 where cdcooper = 14 and nrdconta = 168424 and nrctrseg = 70768 and t.nrproposta = '202305271866';
+
+update cecred.crawseg w set w.vlpremio = 1547.90 where cdcooper = 14 and nrdconta = 106542 and nrctrseg = 71543 and nrproposta = '202302115228';  
+update cecred.crapseg s set s.vlpremio = 1547.90 where cdcooper = 14 and nrdconta = 106542 and nrctrseg = 71543;   
+update cecred.tbseg_prestamista t set t.vlprodut = 1547.90 where cdcooper = 14 and nrdconta = 106542 and nrctrseg = 71543 and t.nrproposta = '202302115228';
+
+update cecred.crawseg w set w.vlpremio = 198.52 where cdcooper = 14 and nrdconta = 15805506 and nrctrseg = 71614 and nrproposta = '202305272474';  
+update cecred.crapseg s set s.vlpremio = 198.52 where cdcooper = 14 and nrdconta = 15805506 and nrctrseg = 71614;   
+update cecred.tbseg_prestamista t set t.vlprodut = 198.52 where cdcooper = 14 and nrdconta = 15805506 and nrctrseg = 71614 and t.nrproposta = '202305272474';
+
+update cecred.crawseg w set w.vlpremio = 625.30 where cdcooper = 14 and nrdconta = 16625544 and nrctrseg = 71640 and nrproposta = '202305272480';  
+update cecred.crapseg s set s.vlpremio = 625.30 where cdcooper = 14 and nrdconta = 16625544 and nrctrseg = 71640;   
+update cecred.tbseg_prestamista t set t.vlprodut = 625.30 where cdcooper = 14 and nrdconta = 16625544 and nrctrseg = 71640 and t.nrproposta = '202305272480';
+
+update cecred.crawseg w set w.vlpremio = 424.21 where cdcooper = 14 and nrdconta = 19305 and nrctrseg = 71078 and nrproposta = '202305303585';  
+update cecred.crapseg s set s.vlpremio = 424.21 where cdcooper = 14 and nrdconta = 19305 and nrctrseg = 71078;   
+update cecred.tbseg_prestamista t set t.vlprodut = 424.21 where cdcooper = 14 and nrdconta = 19305 and nrctrseg = 71078 and t.nrproposta = '202305303585';
+
+update cecred.crawseg w set w.vlpremio = 516.57 where cdcooper = 14 and nrdconta = 34967 and nrctrseg = 72417 and nrproposta = '202306430788';  
+update cecred.crapseg s set s.vlpremio = 516.57 where cdcooper = 14 and nrdconta = 34967 and nrctrseg = 72417;   
+update cecred.tbseg_prestamista t set t.vlprodut = 516.57 where cdcooper = 14 and nrdconta = 34967 and nrctrseg = 72417 and t.nrproposta = '202306430788';
+
+update cecred.crawseg w set w.vlpremio = 1827.75 where cdcooper = 14 and nrdconta = 141429 and nrctrseg = 71787 and nrproposta = '202305400575';  
+update cecred.crapseg s set s.vlpremio = 1827.75 where cdcooper = 14 and nrdconta = 141429 and nrctrseg = 71787;   
+update cecred.tbseg_prestamista t set t.vlprodut = 1827.75 where cdcooper = 14 and nrdconta = 141429 and nrctrseg = 71787 and t.nrproposta = '202305400575';
+
+update cecred.crawseg w set w.vlpremio = 343.39 where cdcooper = 14 and nrdconta = 170356 and nrctrseg = 70530 and nrproposta = '202302021861';  
+update cecred.crapseg s set s.vlpremio = 343.39 where cdcooper = 14 and nrdconta = 170356 and nrctrseg = 70530;   
+update cecred.tbseg_prestamista t set t.vlprodut = 343.39 where cdcooper = 14 and nrdconta = 170356 and nrctrseg = 70530 and t.nrproposta = '202302021861';
+
+update cecred.crawseg w set w.vlpremio = 141.81 where cdcooper = 14 and nrdconta = 15263754 and nrctrseg = 71562 and nrproposta = '202306172719';  
+update cecred.crapseg s set s.vlpremio = 141.81 where cdcooper = 14 and nrdconta = 15263754 and nrctrseg = 71562;   
+update cecred.tbseg_prestamista t set t.vlprodut = 141.81 where cdcooper = 14 and nrdconta = 15263754 and nrctrseg = 71562 and t.nrproposta = '202306172719';
+
+update cecred.crawseg w set w.vlpremio = 419.50 where cdcooper = 14 and nrdconta = 204676 and nrctrseg = 71139 and nrproposta = '202305270496';  
+update cecred.crapseg s set s.vlpremio = 419.50 where cdcooper = 14 and nrdconta = 204676 and nrctrseg = 71139;   
+update cecred.tbseg_prestamista t set t.vlprodut = 419.50 where cdcooper = 14 and nrdconta = 204676 and nrctrseg = 71139 and t.nrproposta = '202305270496';
+
+update cecred.crawseg w set w.vlpremio = 62.67 where cdcooper = 14 and nrdconta = 204676 and nrctrseg = 71137 and nrproposta = '202305270488';  
+update cecred.crapseg s set s.vlpremio = 62.67 where cdcooper = 14 and nrdconta = 204676 and nrctrseg = 71137;   
+update cecred.tbseg_prestamista t set t.vlprodut = 62.67 where cdcooper = 14 and nrdconta = 204676 and nrctrseg = 71137 and t.nrproposta = '202305270488';
+
+update cecred.crawseg w set w.vlpremio = 74.17 where cdcooper = 14 and nrdconta = 204676 and nrctrseg = 71127 and nrproposta = '202305999694';  
+update cecred.crapseg s set s.vlpremio = 74.17 where cdcooper = 14 and nrdconta = 204676 and nrctrseg = 71127;   
+update cecred.tbseg_prestamista t set t.vlprodut = 74.17 where cdcooper = 14 and nrdconta = 204676 and nrctrseg = 71127 and t.nrproposta = '202305999694';
+
+update cecred.crawseg w set w.vlpremio = 54.82 where cdcooper = 14 and nrdconta = 336521 and nrctrseg = 71352 and nrproposta = '202305270459';  
+update cecred.crapseg s set s.vlpremio = 54.82 where cdcooper = 14 and nrdconta = 336521 and nrctrseg = 71352;   
+update cecred.tbseg_prestamista t set t.vlprodut = 54.82 where cdcooper = 14 and nrdconta = 336521 and nrctrseg = 71352 and t.nrproposta = '202305270459';
+
+update cecred.crawseg w set w.vlpremio = 711.67 where cdcooper = 14 and nrdconta = 235121 and nrctrseg = 71222 and nrproposta = '202305406863';  
+update cecred.crapseg s set s.vlpremio = 711.67 where cdcooper = 14 and nrdconta = 235121 and nrctrseg = 71222;   
+update cecred.tbseg_prestamista t set t.vlprodut = 711.67 where cdcooper = 14 and nrdconta = 235121 and nrctrseg = 71222 and t.nrproposta = '202305406863';
+
+update cecred.crawseg w set w.vlpremio = 420.89 where cdcooper = 14 and nrdconta = 357154 and nrctrseg = 70591 and nrproposta = '202305270376';  
+update cecred.crapseg s set s.vlpremio = 420.89 where cdcooper = 14 and nrdconta = 357154 and nrctrseg = 70591;   
+update cecred.tbseg_prestamista t set t.vlprodut = 420.89 where cdcooper = 14 and nrdconta = 357154 and nrctrseg = 70591 and t.nrproposta = '202305270376';
+
+update cecred.crawseg w set w.vlpremio = 730.47 where cdcooper = 14 and nrdconta = 124281 and nrctrseg = 71483 and nrproposta = '202306050765';  
+update cecred.crapseg s set s.vlpremio = 730.47 where cdcooper = 14 and nrdconta = 124281 and nrctrseg = 71483;   
+update cecred.tbseg_prestamista t set t.vlprodut = 730.47 where cdcooper = 14 and nrdconta = 124281 and nrctrseg = 71483 and t.nrproposta = '202306050765';
+
+update cecred.crawseg w set w.vlpremio = 88.81 where cdcooper = 14 and nrdconta = 7188 and nrctrseg = 72401 and nrproposta = '202305272101';  
+update cecred.crapseg s set s.vlpremio = 88.81 where cdcooper = 14 and nrdconta = 7188 and nrctrseg = 72401;   
+update cecred.tbseg_prestamista t set t.vlprodut = 88.81 where cdcooper = 14 and nrdconta = 7188 and nrctrseg = 72401 and t.nrproposta = '202305272101';
+
+update cecred.crawseg w set w.vlpremio = 224.35 where cdcooper = 14 and nrdconta = 7188 and nrctrseg = 72399 and nrproposta = '202306429986';  
+update cecred.crapseg s set s.vlpremio = 224.35 where cdcooper = 14 and nrdconta = 7188 and nrctrseg = 72399;   
+update cecred.tbseg_prestamista t set t.vlprodut = 224.35 where cdcooper = 14 and nrdconta = 7188 and nrctrseg = 72399 and t.nrproposta = '202306429986';
+
+commit;
+
+end;
