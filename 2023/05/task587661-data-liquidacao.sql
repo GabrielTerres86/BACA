733994d2ca525a6c9f4e79de1c4fc5df70d0e867
Luciana Leite <premiersoft-luciana.leite@ailos.coop.br>
2023-05-15 14:59:17 -0300

    Delete da data de liquidação

---
+++ 2023/05/task587661-data-liquidacao.sql
@@ -0,0 +1,14 @@
+BEGIN
+  UPDATE cecred.crapepr 
+     SET dtliquid = NULL
+   WHERE cdcooper = 16
+     AND nrctremp = 406085
+     AND nrdconta = 14079;
+  
+  COMMIT;
+  
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    raise_application_error(-20000,'ERRO AO EXECUTAR SCRIPT: '||SQLERRM);
+END;
