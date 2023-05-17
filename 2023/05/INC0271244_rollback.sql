457295caa4583486472586943f2b9ca36a1206c5
Alan Beckhauser <alan.beckhauser@ailos.coop.br>
2023-05-17 10:05:41 -0300

    ajustes pontal

--- 2023/05/INC0271244_rollback.sql
+++
@@ -1,5 +0,0 @@
-begin
-update cecred.craplau aa set aa.insitlau = 1, aa.dtdebito = null where   aa.nrdconta = 99952670 and aa.cdcooper=13 and aa.cdhistor = 3651 and aa.cdseqtel = '202304955207';
-update cecred.craplau aa set aa.insitlau = 1, aa.dtdebito = null where   aa.nrdconta = 99883619 and aa.cdcooper=5 and aa.cdhistor = 3651 and aa.cdseqtel = '202302056152';
-commit;
-end;
