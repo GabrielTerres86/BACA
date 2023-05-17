82052a7444535c5a8dc21cbe54f79a9946032ddb
Wesley Pinheiro Rocha <amcom-wesley.rocha@ailos.coop.br>
2023-05-17 16:52:45 -0300

    Script inc0271348 - sysdate

--- 2023/05/INC0271348-update-tbchq_deposito_cheque_mob.sql
+++ 2023/05/INC0271348-update-tbchq_deposito_cheque_mob.sql
@@ -2,7 +2,7 @@ begin
   
 update cecred.tbchq_deposito_cheque_mob mob
    set mob.insituacao = 4
-where mob.dtdeposito <= to_date('16/05/2023','dd/mm/yyyy')
+where mob.dtdeposito <= to_date(sysdate,'dd/mm/yyyy')
       and mob.insituacao = 1;
 
 commit;
