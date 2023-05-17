70d54e9a0665462fbc18743ce5b7302cc9859f55
Jefferson Luiz da Silva <jefferson.luiz@ailos.coop.br>
2023-05-17 10:16:35 -0300

    Ajuste no owner

--- 2023/05/ajuste-valor-corte-topaz-ofsa.sql
+++ 2023/05/ajuste-valor-corte-topaz-ofsa.sql
@@ -1,6 +1,6 @@
 begin
 
-  UPDATE tbgen_analise_fraude_param a
+  UPDATE cecred.tbgen_analise_fraude_param a
      SET a.VLCORTE_ENVIO_OFSAA = 0
        , a.VLCORTE_ENVIO_TOPAZ = 89
    WHERE a.CDOPERACAO = 12;
