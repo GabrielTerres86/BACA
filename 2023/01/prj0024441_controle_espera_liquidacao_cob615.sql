a9b83353dcf7d40cce9977b72bb14569f2442110
Rafael Cechet <rafael.cechet@ailos.coop.br>
2023-05-25 10:56:51 -0300

    Adicionar controle diario para implantacao MLC

--- 2023/01/prj0024441_controle_espera_liquidacao_cob615.sql
+++ 2023/01/prj0024441_controle_espera_liquidacao_cob615.sql
@@ -11,6 +11,16 @@ BEGIN
     ,'CTRL_LQD_COB615'
     ,'Quantidade de tempo em segundos para esperar validação do controle de liquidação do COB615'
     ,'10');
+
+  INSERT INTO COBRANCA.tbcobran_controle_diario_liquidacao
+   (dtreferencia
+   ,tpmarco
+   ,dsobservacao)
+ VALUES
+   (to_date('26/05/2023', 'DD/MM/RRRR')
+   ,1 --1=Liquidação COB615 processada
+   ,'Implantacao MLC');
+
   COMMIT;
 EXCEPTION
   WHEN OTHERS THEN
