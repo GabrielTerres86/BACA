ada75586925eda4b39c825c3d712782c07f982b5
Diógenes Lazzarini <diogenes.lazzarini@ailos.coop.br>
2023-05-26 17:44:51 -0300

    Update baca-apontamento-neurotech.sql

--- 2023/05/baca-apontamento-neurotech.sql
+++ 2023/05/baca-apontamento-neurotech.sql
@@ -1,7 +1,7 @@
 BEGIN
 	
 	UPDATE crapprm t
-	SET t.dsvlrprm = '/hml-eapi-ailosmais-motorcredito/v1/motorcredito/dadostelaunica'
+	SET t.dsvlrprm = '/dev-eapi-ailosmais-motorcredito/v1/motorcredito/dadostelaunica'
 	WHERE t.cdacesso = 'URL_MULE_DWLD_TELAUNICA';
 	
 	COMMIT;
