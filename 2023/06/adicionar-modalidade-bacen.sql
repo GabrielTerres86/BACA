40fd678c99c10855dee5e34eb5934e0f2a9fdeed
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2023-06-01 08:36:25 -0300

    Adicionadas modalidades 1401 e 490

---
+++ 2023/06/adicionar-modalidade-bacen.sql
@@ -0,0 +1,9 @@
+BEGIN
+  INSERT INTO gestaoderisco.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+  VALUES (499, 490);
+
+  INSERT INTO gestaoderisco.tbrisco_modalidade_operacao_bacen (CDMODALI_AIMARO, CDMODALI_BACEN)
+  VALUES (499, 1401);
+
+  COMMIT;
+END;
