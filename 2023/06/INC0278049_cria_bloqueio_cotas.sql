a8b7f9ff5bfc8277fd40e1d9f2d897ef39abb223
Adriano Marchi <f0034475@cecred.coop.br>
2023-06-21 11:23:12 -0300

    Script para criar um bloqueio de cotas.

---
+++ 2023/06/INC0278049_cria_bloqueio_cotas.sql
@@ -0,0 +1,45 @@
+begin 
+
+  -- Inserir o registro do bloqueio
+  INSERT INTO cecred.crapblj(cdcooper
+                            ,nrdconta
+                            ,nrcpfcgc
+                            ,cdmodali
+                            ,cdtipmov
+                            ,flblcrft
+                            ,dtblqini
+                            ,dtblqfim
+                            ,vlbloque
+                            ,cdopdblq
+                            ,cdopddes
+                            ,dsjuizem
+                            ,dsresord
+                            ,dtenvres
+                            ,nroficio
+                            ,nrproces
+                            ,dsinfadc
+                            ,vlresblq
+                            ,hrblqini)
+                     VALUES(1                      -- cdcooper
+                           ,10951300               -- nrdconta
+                           ,14462075000107         -- nrcpfcgc
+                           ,4
+                           ,3                      -- cdtipmov
+                           ,0                      -- flblcrft
+                           ,TRUNC(SYSDATE)         -- dtblqini
+                           ,null                   -- dtblqfim
+                           ,1893.48                -- vlbloque
+                           ,1                      -- cdopdblq /* Operador Bloqueio    */
+                           ,NULL                              -- cdopddes /* Operador Desbloqueio */
+                           ,'Primeira Vara Civel da Comarca de Blumenau'  -- dsjuizem
+                           ,'BLOQUEIO DE CAPITAL ATE R$ 1.893,48'         -- dsresord
+                           ,'19/06/2023'                                  -- dtenvres
+                           ,'310044068089'                                -- nroficio
+                           ,'50000427542014824000'                   -- nrproces
+                           ,''                      -- dsinfadc
+                           ,0                       -- vlresblq
+                           ,To_Number(To_Char(systimestamp,'SSSSS')));
+                             
+   commit;
+                               
+end;                             
