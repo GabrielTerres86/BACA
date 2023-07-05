0b0f55bf1fd50b799e14c1a7db99d7ae810145e8
Rodrigo Jose Heiden <rodrigo.heiden@ailos.coop.br>
2023-07-05 09:54:29 -0300

    INC0275246 - Inclusão Valores Recuperados PEAC

---
+++ 2023/07/INC0275246_2.sql
@@ -0,0 +1,72 @@
+BEGIN
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1376,9999,sysdate,2,'APROVADA',0.21);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1794,9999,sysdate,2,'APROVADA',146.97);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (436,9999,sysdate,2,'APROVADA',245.46);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1857,9999,sysdate,2,'APROVADA',334.89);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (959,9999,sysdate,2,'APROVADA',40000);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (2032,9999,sysdate,2,'APROVADA',14.09);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1654,9999,sysdate,2,'APROVADA',54.41);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1376,9999,sysdate,2,'APROVADA',672.28);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1376,9999,sysdate,2,'APROVADA',1365);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (2032,9999,sysdate,2,'APROVADA',333.95);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1976,9999,sysdate,2,'APROVADA',77.87);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1794,9999,sysdate,2,'APROVADA',35.12);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1976,9999,sysdate,2,'APROVADA',451.73);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1654,9999,sysdate,2,'APROVADA',5876.71);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1376,9999,sysdate,2,'APROVADA',762.39);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (3,9999,sysdate,2,'APROVADA',2685.66);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (338,9999,sysdate,2,'APROVADA',2624.74);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (354,9999,sysdate,2,'APROVADA',3260.02);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (355,9999,sysdate,2,'APROVADA',1950.12);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (410,9999,sysdate,2,'APROVADA',685);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (535,9999,sysdate,2,'APROVADA',7390.83);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (535,9999,sysdate,2,'APROVADA',4000);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (535,9999,sysdate,2,'APROVADA',7390.83);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (797,9999,sysdate,2,'APROVADA',850);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (797,9999,sysdate,2,'APROVADA',1807.56);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (797,9999,sysdate,2,'APROVADA',1807.56);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (797,9999,sysdate,2,'APROVADA',4198.17);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (797,9999,sysdate,2,'APROVADA',3999.78);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (890,9999,sysdate,2,'APROVADA',7011.06);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (890,9999,sysdate,2,'APROVADA',6.02);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (921,9999,sysdate,2,'APROVADA',1478.98);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (921,9999,sysdate,2,'APROVADA',1462.71);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1318,9999,sysdate,2,'APROVADA',420);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1376,9999,sysdate,2,'APROVADA',1000);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1379,9999,sysdate,2,'APROVADA',623.7);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1379,9999,sysdate,2,'APROVADA',333.02);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1435,9999,sysdate,2,'APROVADA',555.52);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1576,9999,sysdate,2,'APROVADA',1365.1);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1619,9999,sysdate,2,'APROVADA',1615.6);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1619,9999,sysdate,2,'APROVADA',1648.85);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1619,9999,sysdate,2,'APROVADA',1682.62);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1794,9999,sysdate,2,'APROVADA',200);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1925,9999,sysdate,2,'APROVADA',1722.91);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1990,9999,sysdate,2,'APROVADA',1067.85);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1990,9999,sysdate,2,'APROVADA',1301.15);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1993,9999,sysdate,2,'APROVADA',6111.71);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1993,9999,sysdate,2,'APROVADA',2769.21);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (2001,9999,sysdate,2,'APROVADA',2726.99);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (2071,9999,sysdate,2,'APROVADA',23341.19);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1779,9999,sysdate,2,'APROVADA',10400.23);
+  insert into credito.tbcred_peac_operacao (idpeac_contrato,idsolicitacao,dhoperacao,tpoperacao,cdstatus,vlrecuperacao) values (1779,9999,sysdate,2,'APROVADA',3536.34);
+  COMMIT;
+
+  UPDATE credito.tbcred_peac_contrato a
+     SET a.vltotalrepassefgi =
+         (SELECT SUM(b.vlrecuperacao)
+            FROM credito.tbcred_peac_operacao b
+           WHERE b.idpeac_contrato = a.idpeac_contrato
+             AND b.tpoperacao = 2
+             AND b.cdstatus = 'APROVADA')
+   WHERE a.idpeac_contrato IN (SELECT c.idpeac_contrato
+                                 FROM credito.tbcred_peac_operacao c
+                                WHERE c.tpoperacao = 2
+                                  AND c.cdstatus = 'APROVADA'
+                                  AND trunc(c.dhoperacao) = trunc(SYSDATE));
+  COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    raise_application_error(-20500, SQLERRM);
+END;
