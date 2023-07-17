d5f373c760e8d784f39a5543df18d3b5e5cc71b8
Gabriel Felipe Cristofolini <gabriel.cristofolini@ailos.coop.br>
2023-07-17 20:11:55 +0000

    Updated UPDATE_TRANSACAO_PRESA.sql

--- 2023/07/UPDATE_TRANSACAO_PRESA.sql
+++ 2023/07/UPDATE_TRANSACAO_PRESA.sql
@@ -1,6 +1,6 @@
 begin
 UPDATE 
-  tbcc_lancamentos_pendentes p
+  CECRED.tbcc_lancamentos_pendentes p
 SET
   p.idsituacao = 'M',
   p.dscritica = 'Ajustado pela RDM0051064 em alinhamento com Juliana Carla'
