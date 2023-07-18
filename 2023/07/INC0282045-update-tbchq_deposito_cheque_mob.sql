162fbd221187068150e15e12672d0759b3c0aa63
Wesley Pinheiro Rocha <amcom-wesley.rocha@ailos.coop.br>
2023-07-18 09:11:00 -0300

    Script INC0282025

---
+++ 2023/07/INC0282045-update-tbchq_deposito_cheque_mob.sql
@@ -0,0 +1,21 @@
+begin
+  
+update cecred.tbchq_deposito_cheque_mob mob
+   set mob.insituacao = 4
+where mob.dtdeposito <= to_date('14/07/2023','dd/mm/yyyy')
+    and mob.insituacao = 1
+    and mob.idseqdeposito >= 156824
+    and mob.idseqdeposito <= 168782;
+
+commit;
+end;
+
+
+
+
+
+
+
+
+
+
