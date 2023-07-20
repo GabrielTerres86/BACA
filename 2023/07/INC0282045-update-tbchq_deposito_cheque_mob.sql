fb593cfdbd0166968aec0e58309dc66c69d72fbb
Wesley Pinheiro Rocha <amcom-wesley.rocha@ailos.coop.br>
2023-07-20 12:28:56 +0000

    dtdeposito atualizada

--- 2023/07/INC0282045-update-tbchq_deposito_cheque_mob.sql
+++ 2023/07/INC0282045-update-tbchq_deposito_cheque_mob.sql
@@ -2,10 +2,10 @@ begin
   
 update cecred.tbchq_deposito_cheque_mob mob
    set mob.insituacao = 4
-where mob.dtdeposito <= to_date('14/07/2023','dd/mm/yyyy')
+where mob.dtdeposito <= trunc(sysdate) - 1
     and mob.insituacao = 1
     and mob.idseqdeposito >= 156824
-    and mob.idseqdeposito <= 168803;
+    and mob.idseqdeposito <= 169739;
 
 commit;
 end;
