183e9fce70fd509805ed548e8d66d6520553d815
Wesley Pinheiro Rocha <amcom-wesley.rocha@ailos.coop.br>
2023-07-26 19:04:15 +0000

    Ajuste condicao dtdeposito

--- 2023/07/PRB0048295-update-tbchq_deposito_cheque_mob.sql
+++ 2023/07/PRB0048295-update-tbchq_deposito_cheque_mob.sql
@@ -2,7 +2,7 @@ begin
   
 update cecred.tbchq_deposito_cheque_mob mob
    set mob.insituacao = 4
-where mob.dtdeposito <= trunc(sysdate) - 1
+where mob.dtdeposito < trunc(sysdate)
     and mob.insituacao = 1;
 commit;
 end;
