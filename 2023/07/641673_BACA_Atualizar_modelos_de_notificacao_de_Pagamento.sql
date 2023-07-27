bf6a9c3069d661a8f8628fbe16781a9b6895bff2
Geder Silva Severino <compasso-geder.severino@ailos.coop.br>
2023-07-27 08:19:29 -0300

    Ajustes textual notificacoes pagamento e recebimento pix.

--- 2023/07/641673_BACA_Atualizar_modelos_de_notificacao_de_Pagamento.sql
+++ 2023/07/641673_BACA_Atualizar_modelos_de_notificacao_de_Pagamento.sql
@@ -1,17 +1,17 @@
 BEGIN
-  UPDATE CECRED.TBGEN_NOTIF_MSG_CADASTRO SET DSHTML_MENSAGEM = 'Cooperado, <br><br>Você enviou um Pix.<br><br>Recebedor: #NomeRecebedor <br>Valor do pagamento: #ValorPix <br>Instituição: #Instituicao <br> Identificação: #Identificacao <br>Data/Hora Transacão : #datahoratransacao <br>Descrição: #Descricao <br><br>Acesse o comprovante no App Ailos ou confira o extrato da sua conta corrente.'
+  UPDATE CECRED.TBGEN_NOTIF_MSG_CADASTRO SET DSHTML_MENSAGEM = 'Cooperado, <br><br>Você enviou um Pix.<br><br>Recebedor: #NomeRecebedor <br>Valor do pagamento: #ValorPix <br>Instituição: #Instituicao <br> Identificação: #Identificacao <br>Data/Hora Transacão: #datahoratransacao <br>Descrição: #Descricao <br><br>Acesse o comprovante no App Ailos ou confira o extrato da sua conta corrente.'
   WHERE CDORIGEM_MENSAGEM = 13 AND CDMENSAGEM = 11399;
   
   UPDATE CECRED.TBGEN_NOTIF_AUTOMATICA_PRM SET DSVARIAVEIS_MENSAGEM = '<br>#NomeRecebedor - Nome do Recebedor<br>#ValorPix - Valor do pagamento (Ex.: 5000,00)<br>#Instituicao - Nome da Instituição<br>#Identificacao - Identificação do Pix<br>#datahoratransacao - Data/Hora Transacão<br>#Descricao - Descrição do Pagamento'
   WHERE CDORIGEM_MENSAGEM = 13 AND CDMOTIVO_MENSAGEM = 53 AND CDMENSAGEM = 11399;
   
-  UPDATE CECRED.TBGEN_NOTIF_MSG_CADASTRO SET DSHTML_MENSAGEM = 'Cooperado, <br><br>Você enviou um Pix.<br><br>Recebedor: #NomeRecebedor <br>Valor do pagamento: #ValorPix <br>Valor da tarifa: #ValorTarifaPix <br>Instituição: #Instituicao <br> Identificação: #Identificacao <br>Data/Hora Transacão : #datahoratransacao <br>Descrição: #Descricao <br><br>Acesse o comprovante no App Ailos ou confira o extrato da sua conta corrente.'
+  UPDATE CECRED.TBGEN_NOTIF_MSG_CADASTRO SET DSHTML_MENSAGEM = 'Cooperado, <br><br>Você enviou um Pix.<br><br>Recebedor: #NomeRecebedor <br>Valor do pagamento: #ValorPix <br>Valor da tarifa: #ValorTarifaPix <br>Instituição: #Instituicao <br> Identificação: #Identificacao <br>Data/Hora Transacão: #datahoratransacao <br>Descrição: #Descricao <br><br>Acesse o comprovante no App Ailos ou confira o extrato da sua conta corrente.'
   WHERE CDORIGEM_MENSAGEM = 13 AND CDMENSAGEM = 11400;
   
   UPDATE CECRED.TBGEN_NOTIF_AUTOMATICA_PRM SET DSVARIAVEIS_MENSAGEM = '<br>#NomeRecebedor - Nome do Recebedor<br>#ValorPix - Valor do pagamento (Ex.: 5000,00)<br>#ValorTarifaPix# - Valor da tarifa (Ex.: 5,00)<br>Instituicao - Nome da Instituição<br>#Identificacao - Identificação do Pix<br>#datahoratransacao - Data/Hora Transacão<br>#Descricao - Descrição do Pagamento'
   WHERE CDORIGEM_MENSAGEM = 13 AND CDMOTIVO_MENSAGEM = 54 AND CDMENSAGEM = 11400;
   
-  UPDATE CECRED.TBGEN_NOTIF_MSG_CADASTRO SET DSHTML_MENSAGEM = 'Cooperado, <br><br>Você enviou um Pix.<br><br>Recebedor: #NomeRecebedor <br>CPF/CNPJ: #DocumentoRecebedor<br>Valor do pagamento: #ValorPix <br>Instituição: #Instituicao <br> Identificação: #Identificacao <br>Data/Hora Transacão : #datahoratransacao <br>Descrição: #Descricao <br><br>Acesse o comprovante no App Ailos ou confira o extrato da sua conta corrente.'
+  UPDATE CECRED.TBGEN_NOTIF_MSG_CADASTRO SET DSHTML_MENSAGEM = 'Cooperado, <br><br>Você enviou um Pix.<br><br>Recebedor: #NomeRecebedor <br>CPF/CNPJ: #DocumentoRecebedor<br>Valor do pagamento: #ValorPix <br>Instituição: #Instituicao <br> Identificação: #Identificacao <br>Data/Hora Transacão: #datahoratransacao <br>Descrição: #Descricao <br><br>Acesse o comprovante no App Ailos ou confira o extrato da sua conta corrente.'
   WHERE CDORIGEM_MENSAGEM = 13 AND CDMENSAGEM = 15249;
   
   UPDATE CECRED.TBGEN_NOTIF_AUTOMATICA_PRM SET DSVARIAVEIS_MENSAGEM = '<br>#NomeRecebedor - Nome do Recebedor<br>#DocumentoRecebedor - CPF/CNPJ<br>#ValorPix - Valor do pagamento (Ex.: 5000,00)<br>#Instituicao - Nome da Instituição<br>#Identificacao - Identificação do Pix<br>#datahoratransacao - Data/Hora Transacão<br>#Descricao - Descrição do Pagamento'
