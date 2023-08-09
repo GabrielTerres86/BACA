6e97031eba4b5ca44cf9d821ef639c47545fd0f3
Jackson Rosá <jackson.rosa@ailos.coop.br>
2023-08-09 08:47:13 -0300

    Elimina comprovantes indevidos da base de dados.

---
+++ 2023/08/ritm0316192-elimina-comprovantes.sql
@@ -0,0 +1,15 @@
+BEGIN
+  DELETE cecred.crappro WHERE progress_recid = 561799527;
+  DELETE cecred.crappro WHERE progress_recid = 562104873;
+  DELETE cecred.crappro WHERE progress_recid = 562197770;
+  DELETE cecred.crappro WHERE progress_recid = 562202351;
+  DELETE cecred.crappro WHERE progress_recid = 574020407;
+  DELETE cecred.crappro WHERE progress_recid = 562144067;
+  DELETE cecred.crappro WHERE progress_recid = 562152132;
+  DELETE cecred.crappro WHERE progress_recid = 562184639;
+  DELETE cecred.crappro WHERE progress_recid = 562189226;
+  DELETE cecred.crappro WHERE progress_recid = 562192353;
+  DELETE cecred.crappro WHERE progress_recid = 562194505;
+
+  COMMIT;
+END;
