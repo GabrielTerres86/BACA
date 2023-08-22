068593658dbaeca39de68faed35f01e8b4fdf06c
eltonfernando <elton@ailos.coop.br>
2023-08-22 14:13:30 -0300

    script atualiza boletos

---
+++ 2023/08/PRB0048336-atualizacao-pagamentos.sql
@@ -0,0 +1,14 @@
+begin
+
+UPDATE craptit b SET b.flgenvio = 0, b.dtdpagto = '08/08/2023' where cdcooper = 10 and dtmvtolt = '08/08/2023' and cdagenci = 90 and b.dscodbar = '00191943600001599840000002964304009942289617';
+UPDATE craptit b SET b.flgenvio = 0, b.dtdpagto = '08/08/2023' where cdcooper = 10 and dtmvtolt = '08/08/2023' and cdagenci = 90 and b.dscodbar = '00191980400000096250000003075800005404477817';
+UPDATE craptit b SET b.flgenvio = 0, b.dtdpagto = '08/08/2023' where cdcooper = 10 and dtmvtolt = '08/08/2023' and cdagenci = 90 and b.dscodbar = '00193943600000694900000002886802100006799817';
+UPDATE craptit b SET b.flgenvio = 0, b.dtdpagto = '08/08/2023' where cdcooper = 10 and dtmvtolt = '08/08/2023' and cdagenci = 90 and b.dscodbar = '00193943800000102000000003075800005435878217';
+UPDATE craptit b SET b.flgenvio = 0, b.dtdpagto = '08/08/2023' where cdcooper = 10 and dtmvtolt = '08/08/2023' and cdagenci = 90 and b.dscodbar = '00198943600000399240000003227730000464882117';
+UPDATE craptit b SET b.flgenvio = 0, b.dtdpagto = '08/08/2023' where cdcooper = 10 and dtmvtolt = '08/08/2023' and cdagenci = 90 and b.dscodbar = '00198943600001250480000003042686000002027417';
+UPDATE craptit b SET b.flgenvio = 0, b.dtdpagto = '08/08/2023' where cdcooper = 10 and dtmvtolt = '08/08/2023' and cdagenci = 90 and b.dscodbar = '00199943600000290000000002863460286346315817';
+UPDATE craptit b SET b.flgenvio = 0, b.dtdpagto = '08/08/2023' where cdcooper = 10 and dtmvtolt = '08/08/2023' and cdagenci = 90 and b.dscodbar = '00199943600001398510623060191534120000767811';
+UPDATE craptit b SET b.flgenvio = 0, b.dtdpagto = '08/08/2023' where cdcooper = 10 and dtmvtolt = '08/08/2023' and cdagenci = 90 and b.dscodbar = '00199943800000000000000003209183002944309817';
+
+commit
+end;
