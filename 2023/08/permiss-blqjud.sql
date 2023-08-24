1714d1c901210211109b7fde9f6c46646f90d227
Vinicius Bedin Knapp <premiersoft-vinicius.knapp@ailos.coop.br>
2023-08-24 15:19:35 -0300

    Inserção das permissoes na BLQJUD.

--- 2023/08/permiss-blqjud.sql
+++ 2023/08/permiss-blqjud.sql
@@ -14,6 +14,44 @@ BEGIN
 	   AND UPPER(t.nmrotina) = 'BLQ JUDICIAL';
                
   END LOOP;
+  
+  INSERT INTO cecred.crapace
+    (NMDATELA
+    ,CDDOPCAO
+    ,CDOPERAD
+    ,NMROTINA
+    ,CDCOOPER
+    ,NRMODULO
+    ,IDEVENTO
+    ,IDAMBACE)
+    (SELECT 'BLQJUD'
+           ,'O'
+           ,ope.cdoperad
+           ,' '
+           ,cop.cdcooper
+           ,1
+           ,0
+           ,2
+       FROM cecred.crapcop cop
+      INNER JOIN (SELECT CASE LEVEL
+                          WHEN 1 THEN
+                           'f0033479'
+                          WHEN 2 THEN
+                           'f0034476'
+                          WHEN 3 THEN
+                           'f0034361'
+                          WHEN 4 THEN
+                           'f0033754'
+                          WHEN 5 THEN
+                           'f0033078'
+                          WHEN 6 THEN
+                           'f0034370'
+                        END AS cdoperad
+                   FROM DUAL
+                 CONNECT BY LEVEL IN (1, 2, 3, 4, 5, 6)) ope
+         ON 1 = 1
+      WHERE cop.flgativo = 1);
+  
 COMMIT;
 EXCEPTION
   WHEN OTHERS THEN
