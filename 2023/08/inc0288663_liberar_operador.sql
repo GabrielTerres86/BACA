e72ea17bb4f7f3857ca84622eb32bbfd46028bd7
Andrey Leonardo Bauler <t0033422@ailos.coop.br>
2023-08-31 14:20:47 -0300

    incluir owner da tabela

--- 2023/08/inc0288663_liberar_operador.sql
+++ 2023/08/inc0288663_liberar_operador.sql
@@ -1,5 +1,5 @@
 DECLARE
 BEGIN
-  UPDATE crapopi opi SET opi.flgsitop = 1 WHERE opi.nrdconta = 14672146 and opi.cdcooper = 1 and opi.nrcpfope = 2070265056;
+  UPDATE CECRED.crapopi opi SET opi.flgsitop = 1 WHERE opi.nrdconta = 14672146 and opi.cdcooper = 1 and opi.nrcpfope = 2070265056;
   commit;
 END;
