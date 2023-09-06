bc13d1bfabf39251d64a76b886f6ffbdac390610
alvori <alvori.feliciano@hotmail.com>
2023-09-06 10:17:23 -0300

    Ajuste Comando
    Ajustado comando de ajuste horario de acordo com orientações dos DBAs

--- 2023/08/Update_horario_RDM0052409.sql
+++ 2023/08/Update_horario_RDM0052409.sql
@@ -1,6 +1,55 @@
-begin
+BEGIN
+  DBMS_SCHEDULER.set_attribute(name      => 'cecred.JBDEB_UNICO_HORA04_COOP01',
+                               attribute => 'repeat_interval',
+                               value     => 'Freq=daily;ByDay=MON, TUE, WED, THU, FRI;ByHour=19;ByMinute=30;BySecond=0');
 
-  update sys.dba_scheduler_jobs set repeat_interval ='Freq=daily;ByDay=MON, TUE, WED, THU, FRI;ByHour=19;ByMinute=30;BySecond=0' where job_name like 'JBDEB_UNICO_HORA04%';
-  commit;
+  DBMS_SCHEDULER.set_attribute(name      => 'cecred.JBDEB_UNICO_HORA04_COOP02',
+                               attribute => 'repeat_interval',
+                               value     => 'Freq=daily;ByDay=MON, TUE, WED, THU, FRI;ByHour=19;ByMinute=30;BySecond=0');
 
-end ;
+  DBMS_SCHEDULER.set_attribute(name      => 'cecred.JBDEB_UNICO_HORA04_COOP05',
+                               attribute => 'repeat_interval',
+                               value     => 'Freq=daily;ByDay=MON, TUE, WED, THU, FRI;ByHour=19;ByMinute=30;BySecond=0');
+
+  DBMS_SCHEDULER.set_attribute(name      => 'cecred.JBDEB_UNICO_HORA04_COOP06',
+                               attribute => 'repeat_interval',
+                               value     => 'Freq=daily;ByDay=MON, TUE, WED, THU, FRI;ByHour=19;ByMinute=30;BySecond=0');
+
+  DBMS_SCHEDULER.set_attribute(name      => 'cecred.JBDEB_UNICO_HORA04_COOP07',
+                               attribute => 'repeat_interval',
+                               value     => 'Freq=daily;ByDay=MON, TUE, WED, THU, FRI;ByHour=19;ByMinute=30;BySecond=0');
+
+  DBMS_SCHEDULER.set_attribute(name      => 'cecred.JBDEB_UNICO_HORA04_COOP08',
+                               attribute => 'repeat_interval',
+                               value     => 'Freq=daily;ByDay=MON, TUE, WED, THU, FRI;ByHour=19;ByMinute=30;BySecond=0');
+
+  DBMS_SCHEDULER.set_attribute(name      => 'cecred.JBDEB_UNICO_HORA04_COOP09',
+                               attribute => 'repeat_interval',
+                               value     => 'Freq=daily;ByDay=MON, TUE, WED, THU, FRI;ByHour=19;ByMinute=30;BySecond=0');
+
+  DBMS_SCHEDULER.set_attribute(name      => 'cecred.JBDEB_UNICO_HORA04_COOP10',
+                               attribute => 'repeat_interval',
+                               value     => 'Freq=daily;ByDay=MON, TUE, WED, THU, FRI;ByHour=19;ByMinute=30;BySecond=0');
+
+  DBMS_SCHEDULER.set_attribute(name      => 'cecred.JBDEB_UNICO_HORA04_COOP11',
+                               attribute => 'repeat_interval',
+                               value     => 'Freq=daily;ByDay=MON, TUE, WED, THU, FRI;ByHour=19;ByMinute=30;BySecond=0');
+
+  DBMS_SCHEDULER.set_attribute(name      => 'cecred.JBDEB_UNICO_HORA04_COOP12',
+                               attribute => 'repeat_interval',
+                               value     => 'Freq=daily;ByDay=MON, TUE, WED, THU, FRI;ByHour=19;ByMinute=30;BySecond=0');
+
+  DBMS_SCHEDULER.set_attribute(name      => 'cecred.JBDEB_UNICO_HORA04_COOP13',
+                               attribute => 'repeat_interval',
+                               value     => 'Freq=daily;ByDay=MON, TUE, WED, THU, FRI;ByHour=19;ByMinute=30;BySecond=0');
+
+  DBMS_SCHEDULER.set_attribute(name      => 'cecred.JBDEB_UNICO_HORA04_COOP14',
+                               attribute => 'repeat_interval',
+                               value     => 'Freq=daily;ByDay=MON, TUE, WED, THU, FRI;ByHour=19;ByMinute=30;BySecond=0');
+
+  DBMS_SCHEDULER.set_attribute(name      => 'cecred.JBDEB_UNICO_HORA04_COOP16',
+                               attribute => 'repeat_interval',
+                               value     => 'Freq=daily;ByDay=MON, TUE, WED, THU, FRI;ByHour=19;ByMinute=30;BySecond=0');
+
+END;
+/
