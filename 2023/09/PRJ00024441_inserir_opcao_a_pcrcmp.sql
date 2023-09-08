8996827c90882796ad1e9a437f8ebb4c33a5988e
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2023-09-08 17:01:44 -0300

    Ajuste na inclusão da opção nova

--- 2023/09/PRJ00024441_inserir_opcao_a_pcrcmp.sql
+++ 2023/09/PRJ00024441_inserir_opcao_a_pcrcmp.sql
@@ -1,15 +1,17 @@
 BEGIN
-  INSERT INTO CECRED.crapaca
+  INSERT INTO crapaca
     (nmdeacao
     ,nmpackag
     ,nmproced
     ,lstparam
+    ,nrseqaca
     ,nrseqrdr)
   VALUES
     ('PCRCMP_MONITORAR_ARQUIVO'
     ,'TELA_PCRCMP'
     ,'pc_listar_monitor_acmp615_640'
     ,'pr_dtmovimento'
+    ,SEQACA_NRSEQACA.NEXTVAL
     ,(SELECT NRSEQRDR
        FROM CECRED.craprdr
       WHERE NMPROGRA = 'TELA_PCRCMP'));
