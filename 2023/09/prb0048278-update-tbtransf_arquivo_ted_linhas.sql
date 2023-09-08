45547e34c2cf723b8a4b96363ef58917920c9901
Darly Cesar Marcelino Agostinho <amcom-darly.agostinho@ailos.coop.br>
2023-09-08 15:09:23 -0300

    update para simular registro com cnab NP e situacao do arquivo ted em processamento

---
+++ 2023/09/prb0048278-update-tbtransf_arquivo_ted_linhas.sql
@@ -0,0 +1,22 @@
+BEGIN
+
+    UPDATE tbtransf_arquivo_ted_linhas tatl
+       SET tatl.dsret_cnab = 'NP'
+     WHERE TRUNC(tatl.dtgeracao) = '08/09/2023'
+       AND tatl.cdsegmento = 'A'
+       AND tatl.nrseq_arq_ted = 37116
+       AND tatl.nrseq_arq_ted_linha IN (369884, 369885);
+    
+    UPDATE tbtransf_arquivo_ted tat
+       SET tat.idsituacao = 3
+     WHERE tat.cdcooper = 8
+       AND tat.nrdconta = 99971488
+       AND tat.nrseq_arquivo = 4
+       AND tat.nrseq_arq_ted = 37116
+       AND TRUNC(tat.dtgeracao) = '08/09/2023';
+
+  COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+END;
