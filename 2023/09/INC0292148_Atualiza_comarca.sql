3681221ca6eb73ac9340543c7038a18db10d4a11
Jose Dill <jose.junior@premiersoft.net>
2023-09-20 15:10:18 -0300

    Ajuste

--- 2023/09/INC0292148_Atualiza_comarca.sql
+++ 2023/09/INC0292148_Atualiza_comarca.sql
@@ -4,6 +4,13 @@ BEGIN
      SET MUN.CDCOMARC = 2708006
    WHERE MUN.CDUFIBGE = 27
      AND MUN.CDCIDADE = 2819;
+
+  UPDATE cecred.crapsqu squ
+     SET squ.nrseqatu = 5
+   WHERE UPPER(squ.nmtabela) = UPPER('CRAPMUN')
+     AND UPPER(squ.nmdcampo) = UPPER('SQARQREM')
+     AND UPPER(squ.dsdchave) = UPPER('085;2708006');
+
   COMMIT;
 EXCEPTION
   WHEN OTHERS THEN
