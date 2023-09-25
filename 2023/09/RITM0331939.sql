e7d464c4d3f8af4e558ff04e5ff81b4618818a3a
Pedro <pedro.morais@ailos.coop.br>
2023-09-25 14:08:45 -0300

    Adição de novo menu mobile para divulgação da página de preciso de ajuda

---
+++ 2023/09/RITM0331939.sql
@@ -0,0 +1,6 @@
+BEGIN
+    INSERT INTO menumobile 
+    (menumobileid, menupaiid, nome, sequencia, habilitado, autorizacao, versaominimaapp) 
+    VALUES 
+    (1052, 40, 'Preciso de ajuda', 3, 1, 0, '2.55.11');
+END;
