a2f5b0d4ad0bc85ef2f5d8eb8dbcfc937accfbcc
Pedro <pedro.morais@ailos.coop.br>
2023-09-25 14:24:21 -0300

    Padronização do PR

--- 2023/09/RITM0331939.sql
+++ 2023/09/RITM0331939.sql
@@ -3,4 +3,5 @@ BEGIN
     (menumobileid, menupaiid, nome, sequencia, habilitado, autorizacao, versaominimaapp) 
     VALUES 
     (1052, 40, 'Preciso de ajuda', 3, 1, 0, '2.55.11');
+    COMMIT;
 END;
