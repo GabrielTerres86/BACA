a2fec40a0ddd96a5953541d3d5400a648ae94d2f
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2023-09-27 10:02:38 -0300

    Adicionado relatorio de compensacao microcredito

--- 2023/10/contabeis_setembro.sql
+++ 2023/10/contabeis_setembro.sql
@@ -6,7 +6,7 @@ DECLARE
   vr_cdcritic         NUMBER;
   
   vr_dtrefere         VARCHAR2(20) := '01/10/2023';
-  vr_dtrefere_ris     VARCHAR2(20) := '30/07/2023';
+  vr_dtrefere_ris     VARCHAR2(20) := '30/09/2023';
   
   vr_cdprogra VARCHAR2(50) := 'RelatoContabParalelo';
   vr_dsplsql  VARCHAR2(4000);
@@ -22,29 +22,6 @@ DECLARE
 BEGIN
 
   FOR rw_crapcop IN cr_crapcop LOOP
-    vr_jobname := 'gera_relato_' || to_char(rw_crapcop.cdcooper) || '_K';
-    vr_dsplsql := 'DECLARE' || chr(13) ||
-                        '  wvr_retfile  VARCHAR2(500);' || chr(13) ||
-                        '  wpr_dscritic VARCHAR2(1500);' || chr(13) ||
-                        'BEGIN' || chr(13) ||
-                        '  CECRED.RISC0001_NOVA_CENTRAL.pc_risco_k(pr_cdcooper => ' || rw_crapcop.cdcooper || chr(13) ||
-                                                                 ',pr_dtrefere => ''' || vr_dtrefere_ris || '''' || chr(13) ||
-                                                                 ',pr_retfile  => wvr_retfile' || chr(13) ||
-                                                                 ',pr_dscritic => wpr_dscritic);' || chr(13) ||
-                  'END;';
-    
-    cecred.gene0001.pc_submit_job(pr_cdcooper => rw_crapcop.cdcooper
-                                 ,pr_cdprogra => vr_cdprogra        
-                                 ,pr_dsplsql  => vr_dsplsql         
-                                 ,pr_dthrexe  => SYSTIMESTAMP       
-                                 ,pr_interva  => NULL               
-                                 ,pr_jobname  => vr_jobname         
-                                 ,pr_des_erro => vr_dscritic);
-    IF vr_dscritic IS NOT null THEN
-      vr_dscritic := vr_dscritic || ' - ' || vr_jobname;
-      RAISE vr_exc_erro;
-    END IF;
-
     vr_jobname := 'gera_relato_' || to_char(rw_crapcop.cdcooper) || '_P';
     
     vr_dsplsql := 'DECLARE' || chr(13) ||
@@ -70,6 +47,18 @@ BEGIN
     END IF;
     
   END LOOP;
+  
+  FOR rw_crapcop IN cr_crapcop LOOP
+    cecred.risc0001_nova_central.gerarRelatorioCompensacaoMicrocredito(pr_cdcooper => rw_crapcop.cdcooper
+                                                                      ,pr_dtrefere => to_date(vr_dtrefere_ris, 'DD/MM/RRRR')
+                                                                      ,pr_dscritic => vr_dscritic);
+    IF vr_dscritic IS NOT null THEN
+      RAISE vr_exc_erro;
+    END IF;
+  END LOOP;
+  
+  COMMIT;
+  
 EXCEPTION 
   WHEN vr_exc_erro THEN
     ROLLBACK;
