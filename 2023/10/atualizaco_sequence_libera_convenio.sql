b087142c6329e483ef974ae3006c6a20847f8577
Jose Diego Zunino <jose.zunino@ailos.coop.br>
2023-10-04 20:51:22 +0000

    Updated atualizaco_sequence_libera_convenio.sql

--- 2023/10/atualizaco_sequence_libera_convenio.sql
+++ 2023/10/atualizaco_sequence_libera_convenio.sql
@@ -1,2 +1,3 @@
--- atualização do valor da sequencia para geracao de liberacao de convenios
-alter SEQUENCE cecred.tbconv_canalcoop_liberado_seq restart start with 20000;
+BEGIN
+  EXECUTE IMMEDIATE 'ALTER SEQUENCE cecred.tbconv_canalcoop_liberado_seq restart start with 20000';
+END;
