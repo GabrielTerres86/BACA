7057cbf683eefbac49749bfafc88015f58e33333
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2023-10-25 17:11:40 -0300

    Adicionado risco k Viacredi com alter session

--- 2023/10/03_RISCO_k_v6.sql
+++ 2023/10/03_RISCO_k_v6.sql
@@ -4,7 +4,6 @@ DECLARE
   vr_retfile          VARCHAR2(500);
   vr_cdcritic         NUMBER;
   
-  vr_dtrefere         VARCHAR2(20) := '02/10/2023';
   vr_dtrefere_ris     VARCHAR2(20) := '30/09/2023';
   
   vr_cdprogra VARCHAR2(50) := 'RelatoContabParalelo';
@@ -15,7 +14,7 @@ DECLARE
     SELECT c.cdcooper
       FROM cecred.crapcop c
      WHERE c.flgativo = 1
-       AND c.cdcooper IN (1,2,3,5,6,7,8,9,10,11,12,13,14,16);
+       AND c.cdcooper IN (1);
   rw_crapcop cr_crapcop%ROWTYPE;
   
 BEGIN
@@ -24,16 +23,15 @@ BEGIN
   EXECUTE IMMEDIATE 'ALTER SESSION SET nls_numeric_characters = ''.,''';
   
   FOR rw_crapcop IN cr_crapcop LOOP
-    vr_jobname := 'gera_relato_' || to_char(rw_crapcop.cdcooper) || '_P';
-    
+    vr_jobname := 'gera_relato_' || to_char(rw_crapcop.cdcooper) || '_K';
     vr_dsplsql := 'DECLARE' || chr(13) ||
-                        '  wvr_cdcritic  VARCHAR2(500);' || chr(13) ||
+                        '  wvr_retfile  VARCHAR2(500);' || chr(13) ||
                         '  wpr_dscritic VARCHAR2(1500);' || chr(13) ||
                         'BEGIN' || chr(13) ||
-                        '  CECRED.gerarRelatoriosContabeisTbriscoRis(pr_cdcooper => ' || rw_crapcop.cdcooper || chr(13) ||
-                                                                   ',pr_dtrefere => to_date(''' || vr_dtrefere || ''', ''DD/MM/RRRR'')' || chr(13) ||
-                                                                   ',pr_cdcritic => wvr_cdcritic' || chr(13) ||
-                                                                   ',pr_dscritic => wpr_dscritic);' || chr(13) ||
+                        '  CECRED.RISC0001_NOVA_CENTRAL.pc_risco_k(pr_cdcooper => ' || rw_crapcop.cdcooper || chr(13) ||
+                                                                 ',pr_dtrefere => ''' || vr_dtrefere_ris || '''' || chr(13) ||
+                                                                 ',pr_retfile  => wvr_retfile' || chr(13) ||
+                                                                 ',pr_dscritic => wpr_dscritic);' || chr(13) ||
                   'END;';
     
     cecred.gene0001.pc_submit_job(pr_cdcooper => rw_crapcop.cdcooper
