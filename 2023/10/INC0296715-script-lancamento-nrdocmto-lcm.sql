618fd747bcf9adb3cb6b93ac748871efbbf3579e
Evandro Guaranha(Amcom) <evandro.silva@amcom.com.br>
2023-10-26 12:20:28 -0300

    script ajuste nrdocmto lcm

---
+++ 2023/10/INC0296715-script-lancamento-nrdocmto-lcm.sql
@@ -0,0 +1,53 @@
+DECLARE
+
+  TYPE typ_reg_lcm IS RECORD(
+       cdcooper cecred.craplcm.cdcooper%TYPE,
+       nrdconta cecred.craplcm.nrdconta%TYPE,
+       cdhistor cecred.craplcm.cdhistor%TYPE,
+       novo_nrdocmto cecred.craplcm.nrdocmto%TYPE);
+  TYPE typ_tab_lcm IS TABLE OF typ_reg_lcm INDEX BY PLS_INTEGER;
+  
+  vr_tab_lcm typ_tab_lcm;
+  vr_qtregistro number(10) := 0;
+             
+BEGIN
+  vr_tab_lcm.delete;
+
+             SELECT cdcooper,
+                    nrdconta,
+                    cdhistor,
+                    novo_nrdocmto
+  BULK COLLECT INTO vr_tab_lcm 
+               FROM(SELECT a.cdcooper,
+                           a.nrdconta,
+                           a.cdhistor,      
+                           SUBSTR(a.cdpesqbb, TRIM(LENGTH('Desconto do Borderô')+1)) AS novo_nrdocmto   
+                      FROM cecred.craplcm a 
+                     WHERE a.cdhistor = 2664 
+                       AND a.nrdocmto IS NULL);
+
+   IF NVL(vr_tab_lcm.count,0) > 0 THEN 
+      FOR i IN vr_tab_lcm.first .. vr_tab_lcm.last
+      LOOP
+        vr_qtregistro := vr_qtregistro + 1;
+        
+        UPDATE cecred.craplcm a
+           SET a.nrdocmto = vr_tab_lcm(i).novo_nrdocmto
+         WHERE a.cdcooper = vr_tab_lcm(i).cdcooper
+           AND a.nrdconta = vr_tab_lcm(i).nrdconta
+           AND a.cdhistor = vr_tab_lcm(i).cdhistor
+           AND a.nrdocmto IS NULL;
+
+        IF MOD(vr_qtregistro,100) = 0 THEN
+          COMMIT;
+        END IF; 
+      END LOOP;  
+   END IF;
+  
+   COMMIT;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    raise_application_error(-20001,'Erro ao atualizar cecred.craplcm.nrdocmto',true);
+END;
