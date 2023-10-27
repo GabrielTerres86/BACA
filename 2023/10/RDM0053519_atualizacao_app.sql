07a48aa814035c107e09a68aa112a0ae0fe18fd9
Mateus Schlingmann Lanser <mateus.lanser@ailos.coop.br>
2023-10-27 21:26:47 +0000

    Added RDM0053519_atualizacao_app.sql

---
+++ 2023/10/RDM0053519_atualizacao_app.sql
@@ -0,0 +1,5 @@
+begin;
+update cecred.parametromobile set valor = '2.55.0' where parametromobileid = 25;
+update cecred.parametromobile set valor = '2.55.0' where parametromobileid = 26;
+commit;
+end;
