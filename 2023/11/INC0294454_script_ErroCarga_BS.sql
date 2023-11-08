050c4d7ed92a5f15ae18b2e2800482c268e1a2fc
Rodrigo Jose Heiden <rodrigo.heiden@ailos.coop.br>
2023-11-08 08:04:29 -0300

    Ajuste formato data

--- 2023/11/INC0294454_script_ErroCarga_BS.sql
+++ 2023/11/INC0294454_script_ErroCarga_BS.sql
@@ -3,7 +3,7 @@ BEGIN
   UPDATE cecred.tbcrd_carga_score a
      SET a.qtregis_fisica = 1368734,
          a.qtregis_juridi = 182293
-   WHERE a.dtbase = '30/09/2023'
+   WHERE a.dtbase = to_date('30/09/2023', 'DD/MM/RRRR')
      AND a.cdmodelo = 3
      AND a.dsmodelo = 'Score Comportamental'
      AND a.cdopcao = 'A';
