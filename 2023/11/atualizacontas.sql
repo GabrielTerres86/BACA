e0489e90d44f47661a61f6de8a1340f3b98a1045
Jackson Rosá <jackson.rosa@ailos.coop.br>
2023-11-17 16:59:05 -0300

    atualiza contas

---
+++ 2023/11/atualizacontas.sql
@@ -0,0 +1,5 @@
+begin
+update crapass set nrdconta = 3560368  where cdcooper = 1 and nrdconta = 96439572;
+update crapttl set nrdconta = 3560368  where cdcooper = 1 and nrdconta = 96439572;
+commit;
+end;
