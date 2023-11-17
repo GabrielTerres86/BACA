9b641615ce43b2dd0ddc57b3171932ccecf1d650
Adriél Inácio Dias <softtek-adriel.dias@ailos.coop.br>
2023-11-17 12:05:50 -0300

    mudança número atual.

--- 2023/11/Atualizacao_sequence_arquivo_serasa.sql
+++ 2023/11/Atualizacao_sequence_arquivo_serasa.sql
@@ -1,5 +1,5 @@
 BEGIN
-   UPDATE CECRED.CRAPSQU S SET S.NRSEQATU = 1898 where UPPER(squ.nmtabela)  = upper('CRAPCOB') and UPPER(squ.nmdcampo)  = upper('NRSEQARQ') and UPPER(squ.dsdchave)  = upper('14');
+   UPDATE CECRED.CRAPSQU S SET S.NRSEQATU = 1899 where UPPER(squ.nmtabela)  = upper('CRAPCOB') and UPPER(squ.nmdcampo)  = upper('NRSEQARQ') and UPPER(squ.dsdchave)  = upper('14');
    UPDATE CECRED.CRAPSQU S SET S.NRSEQATU = 1786 where UPPER(squ.nmtabela)  = upper('CRAPCOB') and UPPER(squ.nmdcampo)  = upper('NRSEQARQ') and UPPER(squ.dsdchave)  = upper('12');
    COMMIT;
 END;
