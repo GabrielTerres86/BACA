db94b72eb4dc8d19d4930c03c8d81a5222c6cdf7
Giovane Diego Gibbert <giovane.gibbert@ailos.coop.br>
2023-11-22 15:59:13 -0300

    script para ajuste de diária - coop 14 - correção da data 2

--- 2023/11/delete_inc0296571.sql
+++ 2023/11/delete_inc0296571.sql
@@ -1,7 +1,7 @@
 BEGIN
   DELETE FROM cecred.tbgen_batch_paralelo
   WHERE cdcooper = 14
-  AND cdprogra IN ('CRPS782', 'CRPS782_13')
+  AND cdprogra IN ('CRPS782', 'CRPS782_15')
   AND dtmvtolt = to_date('19/10/2023','dd/mm/yyyy');
   COMMIT;
 END;
