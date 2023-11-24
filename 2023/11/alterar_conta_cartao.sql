eaaf0ec81e3c2cdbaff5225888a079277c9b10d7
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2023-11-24 10:56:27 -0300

    Cartão Alberto

--- 2023/11/alterar_conta_cartao.sql
+++ 2023/11/alterar_conta_cartao.sql
@@ -1,8 +1,8 @@
 DECLARE
-  vr_cooperativa INTEGER := 8;
-  vr_conta       INTEGER := 99951150;
-  vr_cartao      NUMBER(25) := 6393500012985783;  
-  vr_nrctrcrd    INTEGER := 990999;
+  vr_cooperativa INTEGER := 9;
+  vr_conta       INTEGER := 82322228;
+  vr_cartao      NUMBER(25) := 6393500000038738;  
+  vr_nrctrcrd    INTEGER := 99099;
   vr_cpf_titular NUMBER(20);
 BEGIN
 
