d69deaa0a04e25472fb6fac360227f5e7db3003b
Jackson Rosá <jackson.rosa@ailos.coop.br>
2023-11-27 16:44:06 -0300

    cooperado

---
+++ 2023/11/atualizacao-cooperado.sql
@@ -0,0 +1,14 @@
+begin
+update crapass set nrdconta = 12714429, nrcpfcgc = 1954705913 where cdcooper = 1 and nrdconta = 87285517;
+update crapttl set nrdconta = 12714429, nrcpfcgc = 1954705913 where cdcooper = 1 and nrdconta = 87285517 and IDSEQTTL = 1 ;
+update crapsnh set nrdconta = 12714429, nrcpfcgc = 1954705913 where cdcooper = 1 and nrdconta = 87285517;
+commit;
+update crapass set nrdconta = 17681200, nrcpfcgc = 7276510999 where cdcooper = 1 and nrdconta = 82318735;
+update crapttl set nrdconta = 17681200, nrcpfcgc = 7276510999 where cdcooper = 1 and nrdconta = 82318735 and IDSEQTTL = 1 ;
+update crapsnh set nrdconta = 17681200, nrcpfcgc = 7276510999 where cdcooper = 1 and nrdconta = 82318735;
+commit;
+update crapass set nrdconta = 10653244, nrcpfcgc = 61437794904 where cdcooper = 1 and nrdconta = 89346696;
+update crapttl set nrdconta = 10653244, nrcpfcgc = 61437794904 where cdcooper = 1 and nrdconta = 89346696 and IDSEQTTL = 1 ;
+update crapsnh set nrdconta = 10653244, nrcpfcgc = 61437794904 where cdcooper = 1 and nrdconta = 89346696;
+commit;
+end;
