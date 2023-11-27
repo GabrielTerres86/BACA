1d2ed00ea8682600d202a46333dbedc8372cd62e
James Kuehl <f0034526@cecred.coop.br>
2023-11-27 14:55:35 -0300

    Cria nova apolice acentra v3

--- 2023/11/prj0025037test_cria_apolice_teste_acentra.sql
+++ 2023/11/prj0025037test_cria_apolice_teste_acentra.sql
@@ -1,5 +1,14 @@
 begin
-  insert into cecred.tbseg_parametros_prst (IDSEQPAR, CDCOOPER, TPPESSOA, CDSEGURA, TPDPAGTO, MODALIDA, QTPARCEL, PIEDIAS, PIEPARCE, PIELIMIT, PIETAXA, IFTTDIAS, IFTTPARC, IFTTLIMI, IFTTTAXA, LMPSELEG, TPCUSTEI, VLCOMISS, TPADESAO, LIMITDPS, NRAPOLIC, ENDERFTP, LOGINFTP, SENHAFTP, FLGELEPF, FLGINDEN, IDADEDPS, PAGSEGU, SEQARQU, DTINIVIGENCIA, DTFIMVIGENCIA, LMINSOCI, DTCTRMISTA)
-  values (cecred.tbseg_parametros_prst_seq.nextval, 5, 1, 514, 0, 0, 0, 0, 0, 0.00, 0.00000000, 0, 0, 0.00, 0.00000000, 0.00, 1, 0.00, 1, 199999.99, '77001236', 'filetransfer.icatuseguros.com.br', 'Producao-Ailos', '@Ailos2019@', 0, 0, 0, 0.02088, 0, to_date('05-11-2023', 'dd-mm-yyyy'), to_date('31-12-2053', 'dd-mm-yyyy'), 0.00, null);
-  commit;
+  
+
+
+ 
+  insert into tbseg_param_prst_cap_seg (IDSEQPAR, IDADEMIN, IDADEMAX, CAPITMIN, CAPITMAX)
+  values (1000, 0, 999, 10000.00, 500000.00);
+  
+  insert into tbseg_param_prst_tax_cob (IDSEQPAR, GBIDAMIN, GBIDAMAX, GBSEGMIN, GBSEGMAX)
+  values (1000, 0, 999, 0.01956083, 0.00131917);
+    
+  commit; 
+  
 end; 
