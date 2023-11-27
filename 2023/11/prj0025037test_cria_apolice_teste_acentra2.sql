0b221e93d253e1dc109e56052458a8939a70505b
James Kuehl <f0034526@cecred.coop.br>
2023-11-27 15:19:57 -0300

    Cria nova apolice acentra v5

--- 2023/11/prj0025037test_cria_apolice_teste_acentra2.sql
+++ 2023/11/prj0025037test_cria_apolice_teste_acentra2.sql
@@ -1,8 +1,8 @@
 begin 
-  insert into tbseg_param_prst_cap_seg (IDSEQPAR, IDADEMIN, IDADEMAX, CAPITMIN, CAPITMAX)
+  insert into cecred.tbseg_param_prst_cap_seg (IDSEQPAR, IDADEMIN, IDADEMAX, CAPITMIN, CAPITMAX)
   values (1000, 0, 999, 10000.00, 500000.00);
   
-  insert into tbseg_param_prst_tax_cob (IDSEQPAR, GBIDAMIN, GBIDAMAX, GBSEGMIN, GBSEGMAX)
+  insert into cecred.tbseg_param_prst_tax_cob (IDSEQPAR, GBIDAMIN, GBIDAMAX, GBSEGMIN, GBSEGMAX)
   values (1000, 0, 999, 0.01956083, 0.00131917);
     
   commit; 
