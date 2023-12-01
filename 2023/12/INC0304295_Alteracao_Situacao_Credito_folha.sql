9afeecfdf194ed04906b05bb923d0387fadcd5ee
Joao Carlos Goncalves Pinheiro Junior <joao.pinheiro@ailos.coop.br>
2023-12-01 20:22:39 +0000

    Updated INC0304295_Alteracao_Situacao_Credito_folha.sql

--- 2023/12/INC0304295_Alteracao_Situacao_Credito_folha.sql
+++ 2023/12/INC0304295_Alteracao_Situacao_Credito_folha.sql
@@ -5,7 +5,6 @@ BEGIN
    WHERE cdempres = 441
      AND cdcooper = 12
      AND progress_recid = 1078895;
-  --
   UPDATE cecred.craplfp
      SET IDSITLCT = 'C'
        , dsobslct = 'Lancado manualmente' 
@@ -13,7 +12,6 @@ BEGIN
      AND cdcooper = 12
      AND progress_recid in (10722905, 10722906, 10722907, 10722908, 10722909, 10722910, 10722911, 10722912, 10722913, 10722934, 10722935, 10722936, 10722937);
   COMMIT;
-  --
 EXCEPTION
   WHEN OTHERS THEN
     ROLLBACK;
