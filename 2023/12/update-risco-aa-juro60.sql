ccb487aa1ed0d30f8ea8e90292e9ef865ac50798
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2023-12-01 16:37:08 -0300

    correcao tabela

--- 2023/12/update-risco-aa-juro60.sql
+++ 2023/12/update-risco-aa-juro60.sql
@@ -1,19 +1,19 @@
 BEGIN
-  UPDATE cecred.crapris t
+  UPDATE gestaoderisco.tbrisco_crapris t
      SET t.innivris = 2
    WHERE t.cdcooper = 1
      AND t.nrdconta = 1929518
      AND t.nrctremp = 2562456
      AND t.dtrefere = to_date('30/11/2023');
 
-  UPDATE cecred.crapris t
+  UPDATE gestaoderisco.tbrisco_crapris t
      SET t.innivris = 8
    WHERE t.cdcooper = 1
      AND t.nrdconta = 6375510
      AND t.nrctremp = 5523893
      AND t.dtrefere = to_date('30/11/2023');
 
-  UPDATE cecred.crapris t
+  UPDATE gestaoderisco.tbrisco_crapris t
      SET t.innivris = 6
    WHERE t.cdcooper = 1
      AND t.nrdconta = 8344507
