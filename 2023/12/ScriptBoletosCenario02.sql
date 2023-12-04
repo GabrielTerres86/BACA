99a543bc4ac334af3050a89904a9457be403a7d2
Jose Dill <jose.junior@premiersoft.net>
2023-12-04 16:20:51 -0300

    Testes

---
+++ 2023/12/ScriptBoletosCenario02.sql
@@ -0,0 +1,10 @@
+BEGIN
+  UPDATE cecred.crapcob c
+     SET c.vltitulo = 6
+   WHERE c.nrdconta = 83480102
+     AND c.nrdocmto IN (47, 44);
+  COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    SISTEMA.EXCECAOINTERNA(pr_cdcooper => 3, pr_compleme => 'boletos 47 e 44');
+END;
