72d87905a87c60bbe1b253fe1d5771f00047c714
Jose Dill <jose.junior@premiersoft.net>
2023-12-04 16:28:34 -0300

    Testes

--- 2023/12/ScriptBoletosCenario02.sql
+++ 2023/12/ScriptBoletosCenario02.sql
@@ -3,6 +3,12 @@ BEGIN
      SET c.vltitulo = 6
    WHERE c.nrdconta = 83480102
      AND c.nrdocmto IN (47, 44);
+
+  UPDATE cecred.crapcob c
+     SET c.vltitulo = 2
+   WHERE c.nrdconta = 84606444
+     AND c.nrdocmto IN (13, 14);
+
   COMMIT;
 EXCEPTION
   WHEN OTHERS THEN
