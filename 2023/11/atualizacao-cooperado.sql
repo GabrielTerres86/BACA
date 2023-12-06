7db92aa345cd36249695c10d0690286a5593137a
Jackson Rosá <jackson.rosa@ailos.coop.br>
2023-12-06 14:03:16 -0300

    atualizacao-cooperado.sql

--- 2023/11/atualizacao-cooperado.sql
+++ 2023/11/atualizacao-cooperado.sql
@@ -1,6 +1,4 @@
 BEGIN
-update crapass set nrdconta = 8054916, nrcpfcgc = 31158234953 where cdcooper = 1 and nrdconta = 91945020;
-update crapttl set nrdconta = 8054916, nrcpfcgc = 31158234953 where cdcooper = 1 and nrdconta = 91945020 and IDSEQTTL = 1 ;
-update crapsnh set nrdconta = 8054916, nrcpfcgc = 31158234953 where cdcooper = 1 and nrdconta = 91945020;
+update crapenc set nrdconta = 8054916 where cdcooper = 1 and nrdconta = 90262050 AND idseqttl = 1 AND tpendass = 10;
 COMMIT;
 END;
