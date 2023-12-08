4acb4214ec983e7e1800f464993c503006dfb84e
Carlos Henrique Weinhold <carlos.weinhold@ailos.coop.br>
2023-12-08 14:54:28 -0300

    prb0048432 - atualização dos cadastros dos programas e parâmetros para execução da central de risco em paralelo ao batch.

--- 2023/12/prb0048432_crpsrid_crpsrim.sql
+++ 2023/12/prb0048432_crpsrid_crpsrim.sql
@@ -11,85 +11,85 @@ BEGIN
   UPDATE cecred.crapprg SET inlibprg = 2, nrsolici = 9310 WHERE UPPER(cdprogra) = 'CRPS310' AND cdcooper IN (1,2,3,5,6,7,8,9,10,11,12,13,14,16);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 2, 0, 0, 0, 0, 0, 1, 1, null);
+  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 1, 0, 0, 0, 0, 0, 1, 1, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
   values ('CRED', 'CRPSRIM', 'Cadeia mensal Central de Risco 310e (299p, 627p (628e (516p, 280p)))', '.', '.', '.', 41, 5, 1, 0, 0, 0, 0, 0, 1, 1, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 2, 0, 0, 0, 0, 0, 1, 2, null);
+  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 1, 0, 0, 0, 0, 0, 1, 2, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
   values ('CRED', 'CRPSRIM', 'Cadeia mensal Central de Risco 310e (299p, 627p (628e (516p, 280p)))', '.', '.', '.', 41, 5, 1, 0, 0, 0, 0, 0, 1, 2, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (516e)', ' ', ' ', ' ', 1, 101, 2, 0, 0, 0, 0, 0, 1, 3, null);
+  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (516e)', ' ', ' ', ' ', 1, 101, 1, 0, 0, 0, 0, 0, 1, 3, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
   values ('CRED', 'CRPSRIM', 'Cadeia mensal Central de Risco 310e (299p, 516p, 280p)', '.', '.', '.', 41, 5, 1, 0, 0, 0, 0, 0, 1, 3, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 2, 0, 0, 0, 0, 0, 1, 5, null);
+  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 1, 0, 0, 0, 0, 0, 1, 5, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
   values ('CRED', 'CRPSRIM', 'Cadeia mensal Central de Risco 310e (299p, 627p (628e (516p, 280p)))', '.', '.', '.', 41, 5, 1, 0, 0, 0, 0, 0, 1, 5, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 2, 0, 0, 0, 0, 0, 1, 6, null);
+  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 1, 0, 0, 0, 0, 0, 1, 6, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
   values ('CRED', 'CRPSRIM', 'Cadeia mensal Central de Risco 310e (299p, 627p (628e (516p, 280p)))', '.', '.', '.', 41, 5, 1, 0, 0, 0, 0, 0, 1, 6, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 2, 0, 0, 0, 0, 0, 1, 7, null);
+  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 1, 0, 0, 0, 0, 0, 1, 7, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
   values ('CRED', 'CRPSRIM', 'Cadeia mensal Central de Risco 310e (299p, 627p (628e (516p, 280p)))', '.', '.', '.', 41, 5, 1, 0, 0, 0, 0, 0, 1, 7, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 2, 0, 0, 0, 0, 0, 1, 8, null);
+  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 1, 0, 0, 0, 0, 0, 1, 8, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
   values ('CRED', 'CRPSRIM', 'Cadeia mensal Central de Risco 310e (299p, 627p (628e (516p, 280p)))', '.', '.', '.', 41, 5, 1, 0, 0, 0, 0, 0, 1, 8, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 2, 0, 0, 0, 0, 0, 1, 9, null);
+  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 1, 0, 0, 0, 0, 0, 1, 9, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
   values ('CRED', 'CRPSRIM', 'Cadeia mensal Central de Risco 310e (299p, 627p (628e (516p, 280p)))', '.', '.', '.', 41, 5, 1, 0, 0, 0, 0, 0, 1, 9, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 2, 0, 0, 0, 0, 0, 1, 10, null);
+  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 1, 0, 0, 0, 0, 0, 1, 10, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
   values ('CRED', 'CRPSRIM', 'Cadeia mensal Central de Risco 310e (299p, 627p (628e (516p, 280p)))', '.', '.', '.', 41, 5, 1, 0, 0, 0, 0, 0, 1, 10, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 2, 0, 0, 0, 0, 0, 1, 11, null);
+  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 1, 0, 0, 0, 0, 0, 1, 11, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
   values ('CRED', 'CRPSRIM', 'Cadeia mensal Central de Risco 310e (299p, 627p (628e (516p, 280p)))', '.', '.', '.', 41, 5, 1, 0, 0, 0, 0, 0, 1, 11, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 2, 0, 0, 0, 0, 0, 1, 12, null);
+  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 1, 0, 0, 0, 0, 0, 1, 12, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
   values ('CRED', 'CRPSRIM', 'Cadeia mensal Central de Risco 310e (299p, 627p (628e (516p, 280p)))', '.', '.', '.', 41, 5, 1, 0, 0, 0, 0, 0, 1, 12, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 2, 0, 0, 0, 0, 0, 1, 13, null);
+  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 1, 0, 0, 0, 0, 0, 1, 13, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
   values ('CRED', 'CRPSRIM', 'Cadeia mensal Central de Risco 310e (299p, 627p (628e (516p, 280p)))', '.', '.', '.', 41, 5, 1, 0, 0, 0, 0, 0, 1, 13, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 2, 0, 0, 0, 0, 0, 1, 14, null);
+  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 1, 0, 0, 0, 0, 0, 1, 14, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
   values ('CRED', 'CRPSRIM', 'Cadeia mensal Central de Risco 310e (299p, 627p (628e (516p, 280p)))', '.', '.', '.', 41, 5, 1, 0, 0, 0, 0, 0, 1, 14, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
-  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 2, 0, 0, 0, 0, 0, 1, 16, null);
+  values ('CRED', 'CRPSRID', 'Cadeia diária Central de Risco 515e (634e (635e (516e)))', ' ', ' ', ' ', 1, 101, 1, 0, 0, 0, 0, 0, 1, 16, null);
 
   insert into cecred.crapprg (NMSISTEM, CDPROGRA, DSPROGRA##1, DSPROGRA##2, DSPROGRA##3, DSPROGRA##4, NRSOLICI, NRORDPRG, INCTRPRG, CDRELATO##1, CDRELATO##2, CDRELATO##3, CDRELATO##4, CDRELATO##5, INLIBPRG, CDCOOPER, QTMINMED)
   values ('CRED', 'CRPSRIM', 'Cadeia mensal Central de Risco 310e (299p, 627p (628e (516p, 280p)))', '.', '.', '.', 41, 5, 1, 0, 0, 0, 0, 0, 1, 16, null);
