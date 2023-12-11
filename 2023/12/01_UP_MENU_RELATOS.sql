c39a135aa7acc52e8ea0a606ff18c4dddcf239fe
Gabriel Felipe Cristofolini <gabriel.cristofolini@ailos.coop.br>
2023-12-11 13:55:44 +0000

    Updated 01_UP_MENU_RELATOS.sql

--- 2023/12/01_UP_MENU_RELATOS.sql
+++ 2023/12/01_UP_MENU_RELATOS.sql
@@ -1,4 +1,5 @@
 begin
 insert into menumobile (MENUMOBILEID, MENUPAIID, NOME, SEQUENCIA, HABILITADO, AUTORIZACAO, VERSAOMINIMAAPP, VERSAOMAXIMAAPP)
 values (1039, 1009, 'Autosserviço de relatos de infração', 1, 1, 1, '2.57.0', null);
+commit;
 end;
