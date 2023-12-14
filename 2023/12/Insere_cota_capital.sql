c486fc7d4ab71ffe7077970efc85b4f225ae23e4
Alexandre Oliveira <alexandre.santos@premiersoft.net>
2023-12-14 14:34:28 -0300

    * adicionando contas solicitadas para teste

--- 2023/12/Insere_cota_capital.sql
+++ 2023/12/Insere_cota_capital.sql
@@ -2,7 +2,21 @@ begin
   
   UPDATE crapcot
     SET vldcotas = 1000
-  WHERE nrdconta in ( 17699169 )
+  WHERE nrdconta in (
+      17699169,
+      17699096,
+      17702984,
+      17702992,
+      17703000,
+      17703018,
+      17703026,
+      17703034,
+      17703042,
+      17701511,
+      17700043,
+      17700027,
+      17699550
+    )
     and cdcooper = 1;
     
   commit;
