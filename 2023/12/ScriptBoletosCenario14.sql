0579a17d45ce99d9c0861722198e37f370d1cda6
Jose Dill <jose.junior@premiersoft.net>
2023-12-15 15:42:25 -0300

    Testes

--- 2023/12/ScriptBoletosCenario14.sql
+++ 2023/12/ScriptBoletosCenario14.sql
@@ -4,6 +4,10 @@ BEGIN
      SET c.vltitulo = c.vltitulo - 5
    WHERE (c.nrdconta, c.nrdocmto) in ((84606444,97),(84606444,98),(84606444,99));
 
+  UPDATE cecred.crapcob c
+     SET c.vltitulo = c.vltitulo - 5
+   WHERE (c.nrdconta, c.nrdocmto) in ((83390618,61),(82475920,51));
+
    COMMIT;
 EXCEPTION
   WHEN OTHERS THEN
