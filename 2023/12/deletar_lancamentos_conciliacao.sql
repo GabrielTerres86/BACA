5395b41bb72aad1ba5aea1d2fa673321881de19b
Arthur <atwiiister@gmail.com>
2023-12-15 14:38:04 -0300

    Update deletar_lancamentos_conciliacao.sql

--- 2023/12/deletar_lancamentos_conciliacao.sql
+++ 2023/12/deletar_lancamentos_conciliacao.sql
@@ -1,7 +1,6 @@
 begin
      delete from craplcm a
-   WHERE cdcooper = 3
-   AND nrdconta in (99999862)
-   and a.dtmvtolt > to_date('10/12/2023', 'dd/mm/yyyy');
+   WHERE a.cdcooper = 8
+         a.cdpesqbb = '000092436493;991675;000000009999888;00000013';
    commit;
  end;
