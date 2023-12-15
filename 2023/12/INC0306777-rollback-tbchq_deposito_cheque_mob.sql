74a54d8d8b55e5ec62df053d08d496a4fef06cb9
Wesley Pinheiro Rocha <amcom-wesley.rocha@ailos.coop.br>
2023-12-15 16:53:21 -0300

    Alteracao Script INC0306777

--- 2023/12/INC0306777-rollback-tbchq_deposito_cheque_mob.sql
+++
@@ -1,9 +0,0 @@
-begin
-  
-update cecred.tbchq_deposito_cheque_mob mob
-   set mob.insituacao = 1
-where mob.dtdeposito < trunc(sysdate)
-      and mob.insituacao = 4
-      and mob.idseqdeposito in (193333,194941,195419,195760,195759,196848,197075);
-commit;
-end;
