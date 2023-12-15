05cb414ecff7bf3a110fae0c9f5ddd6b9192eee5
Adriél Inácio Dias <softtek-adriel.dias@ailos.coop.br>
2023-12-15 09:07:17 -0300

    Adição do domínio cecred antes da tabela.

--- 2023/12/INC0306759_Alterar_Parametros_Funcao_Incluir_Desconto_Pix.sql
+++ 2023/12/INC0306759_Alterar_Parametros_Funcao_Incluir_Desconto_Pix.sql
@@ -1,4 +1,4 @@
 BEGIN
-    update crapaca set lstparam = 'pr_cdtarcom,pr_dtinicom,pr_dtfimcom,pr_pedescom,pr_cdtarcob,pr_dtinicob,pr_dtfimcob,pr_pedescob,pr_dsjustif,pr_cdaprova,pr_cdoperad,pr_cdcooper,pr_nrdconta' where nmdeacao = 'INCLUI_DESCONTO_PIX'; 
+    update cecred.crapaca set lstparam = 'pr_cdtarcom,pr_dtinicom,pr_dtfimcom,pr_pedescom,pr_cdtarcob,pr_dtinicob,pr_dtfimcob,pr_pedescob,pr_dsjustif,pr_cdaprova,pr_cdoperad,pr_cdcooper,pr_nrdconta' where nmdeacao = 'INCLUI_DESCONTO_PIX'; 
     COMMIT;
 END;
