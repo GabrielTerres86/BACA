d95fd4cd31efb884ec543d46fc63ed6aff5d6d29
Jose Dill <jose.junior@premiersoft.net>
2023-12-15 15:40:16 -0300

    Testes

---
+++ 2023/12/ScriptBoletosCenario14.sql
@@ -0,0 +1,11 @@
+BEGIN
+
+  UPDATE cecred.crapcob c
+     SET c.vltitulo = c.vltitulo - 5
+   WHERE (c.nrdconta, c.nrdocmto) in ((84606444,97),(84606444,98),(84606444,99));
+
+   COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    SISTEMA.EXCECAOINTERNA(pr_cdcooper => 3, pr_compleme => 'boletos conta yyyyyy');
+END;
