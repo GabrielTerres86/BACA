012cdb3e4cc0dec73d2d27d2bdb1d1ad5425674b
Jefferson Cristian Gubetti <jefferson.gubetti@ailos.coop.br>
2023-12-20 13:03:18 +0000

    Updated devolucao-ailosmais-20231219-01.sql

--- 2023/12/devolucao-ailosmais-20231219-01.sql
+++ 2023/12/devolucao-ailosmais-20231219-01.sql
@@ -1,10 +1,10 @@
 BEGIN
   UPDATE cecred.crapcob c
      SET c.vltitulo = c.vltitulo + c.vlabatim + c.vldescto + 100
-   WHERE c.nrdident IN ('3023110902118124040', '3023121506124355886');
+   WHERE c.nrdident IN ('3023110901118124611', '3023121103123094316');
   COMMIT;
 EXCEPTION
   WHEN OTHERS THEN
-    SISTEMA.EXCECAOINTERNA(pr_cdcooper => 3, pr_compleme => 'boletos 3023110902118124040 e 3023121506124355886');
+    SISTEMA.EXCECAOINTERNA(pr_cdcooper => 3, pr_compleme => 'boletos 3023110901118124611 e 3023121103123094316');
     ROLLBACK;
 END;
