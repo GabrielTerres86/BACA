e50a899bff59c9ef7f6c2c09eab11586be24dd60
Jefferson Cristian Gubetti <jefferson.gubetti@ailos.coop.br>
2023-12-22 16:27:35 +0000

    Updated antifraude_boleto_20231222_01.sql

--- 2023/12/antifraude_boleto_20231222_01.sql
+++ 2023/12/antifraude_boleto_20231222_01.sql
@@ -1,18 +1,12 @@
 BEGIN
   UPDATE PAGAMENTO.tbpagto_analise_antifraude_boleto
-     SET dssessao_usuario               = LOWER(SYS_GUID()) || LOWER(SYS_GUID())
-        ,dsversao_software_usuario      = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Sa'
-        ,dsurl_origem                   = 'https://contaonlinetest.viacredi.coop.br/ib/pagamentos/contas/boletos-diversos'
-        ,vllatitude_localizacao         = -26.905657900
-        ,vllongitude_localizacao        = -49.075626700
-        ,qtmetros_precisao_localizacao  = 22
-        ,nrcarimbo_datahora_localizacao = 20231222
-        ,dssincronizacao_transacao      = ' '
-   WHERE idanalise_fraude = 346122519;
+     SET dssessao_usuario = ' '
+   WHERE idanalise_fraude = 346122523;
 
   UPDATE PAGAMENTO.tbpagto_analise_antifraude_boleto
-     SET dssessao_usuario = ' '
-   WHERE idanalise_fraude = 346122521;
+     SET dssincronizacao_transacao      = ' '
+   WHERE idanalise_fraude = 346122524;
+
   COMMIT;
 EXCEPTION
   WHEN OTHERS THEN
