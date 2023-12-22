a171cf85f6d5e4bd14825ab1802ef12f4e2443ca
Matheus <matheus.leriano@ailos.coop.br>
2023-12-22 11:04:36 -0300

    ajuste saldo reserva

---
+++ 2023/12/ajuste-saldo-reserva.sql
@@ -0,0 +1,12 @@
+BEGIN
+  
+  UPDATE credito.tbepr_reserva_atraso a
+     SET a.vlreserva = 4955,36
+   WHERE a.id_reserva_atraso = 9750;
+  
+  COMMIT;
+EXCEPTION                                         
+  WHEN OTHERS THEN
+    ROLLBACK; 
+    RAISE_application_error(-20500, 'Erro: ' || SQLERRM);
+END;
