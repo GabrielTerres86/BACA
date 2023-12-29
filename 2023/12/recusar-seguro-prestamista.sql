86ca70086ec89ee78a2408b3096ec8f02d0a3539
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2023-12-29 08:59:15 -0300

    Recusar seguro prestamista - teste

--- 2023/12/recusar-seguro-prestamista.sql
+++ 2023/12/recusar-seguro-prestamista.sql
@@ -4,14 +4,14 @@ update cecred.tbseg_prestamista t
    set t.tpregist = 0
  where cdcooper = 11
    and nrdconta = 99198738
-   and nrctrseg = 361648;
+   and nrctrseg = 361651;
    
 update cecred.crapseg s
    set s.cdsitseg = 5
  where cdcooper = 11
    and nrdconta = 99198738
-   and nrctrseg = 361648
-   and tpseguro = 4;   
+   and nrctrseg = 361651
+   and tpseguro = 4;     
    
 commit;
 end;   
