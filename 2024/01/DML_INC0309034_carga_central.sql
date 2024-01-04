cb69e15092faec8d61634abf2dec13f91caccf0b
Odirlei Busana <odirlei.busana@ailos.coop.br>
2024-01-04 16:34:57 +0000

    INC0309034 - Ajuste situação das cargas de cartoes

---
+++ 2024/01/DML_INC0309034_carga_central.sql
@@ -0,0 +1,14 @@
+BEGIN
+  
+  UPDATE squad_riscos.tbrisco_central_carga c
+     SET c.CDSTATUS = 4
+   WHERE c.DTREFERE = to_date('31/12/2023','DD/MM/RRRR')
+     AND c.TPPRODUTO IN (97)
+     AND c.IDCENTRAL_CARGA NOT IN (SELECT MAX(x.IDCENTRAL_CARGA)
+                                     FROM squad_riscos.tbrisco_central_carga x
+                                    WHERE x.DTREFERE = to_date('31/12/2023','DD/MM/RRRR')
+                                      AND x.TPPRODUTO IN (97)
+                                    GROUP BY cdcooper);
+  COMMIT; 
+              
+END;                     
