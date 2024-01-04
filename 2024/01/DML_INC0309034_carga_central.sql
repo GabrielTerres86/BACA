fb47829748f4d1391b655ea124cb4d186f6db700
Odirlei Busana <odirlei.busana@ailos.coop.br>
2024-01-04 16:50:41 +0000

    Updated DML_INC0309034_carga_central.sql

--- 2024/01/DML_INC0309034_carga_central.sql
+++ 2024/01/DML_INC0309034_carga_central.sql
@@ -1,11 +1,11 @@
 BEGIN
   
-  UPDATE squad_riscos.tbrisco_central_carga c
+  UPDATE gestaoderisco.tbrisco_central_carga c
      SET c.CDSTATUS = 4
    WHERE c.DTREFERE = to_date('31/12/2023','DD/MM/RRRR')
      AND c.TPPRODUTO IN (97)
      AND c.IDCENTRAL_CARGA NOT IN (SELECT MAX(x.IDCENTRAL_CARGA)
-                                     FROM squad_riscos.tbrisco_central_carga x
+                                     FROM gestaoderisco.tbrisco_central_carga x
                                     WHERE x.DTREFERE = to_date('31/12/2023','DD/MM/RRRR')
                                       AND x.TPPRODUTO IN (97)
                                     GROUP BY cdcooper);
