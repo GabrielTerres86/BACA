0462bcb0a9021afb3fc7923e1daad2a0ad93690e
Arthur <atwiiister@gmail.com>
2024-01-05 15:48:37 -0300

    Update deletar_lancamentos_conciliacao.sql

--- 2023/12/deletar_lancamentos_conciliacao.sql
+++ 2023/12/deletar_lancamentos_conciliacao.sql
@@ -2,6 +2,6 @@ begin
     update craplcm a set a.dtmvtolt = to_date('03/01/2024', 'dd/mm/yyyy')
     where a.dtmvtolt = to_date('02/01/2024', 'dd/mm/yyyy')
     and a.cdcooper = 8 
-    and a.cdhistor in (2936,2937,2938,3239,3241)
+    and a.cdhistor in (2936,2937,2938,3239,3241);
    commit;
  end;
