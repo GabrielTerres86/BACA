56462cc00e619903b74572f5023bafc3ebb32ac0
Arthur <atwiiister@gmail.com>
2024-01-08 11:52:31 -0300

    Update deletar_lancamentos_conciliacao.sql

--- 2023/12/deletar_lancamentos_conciliacao.sql
+++ 2023/12/deletar_lancamentos_conciliacao.sql
@@ -1,7 +1,8 @@
 begin
-   delete from craplcm a
-   where a.nrdconta =  99999862 
-   and a.dtmvtolt > to_date('01/01/2024', 'dd/mm/yyyy')
-   and a.cdcooper = 3;
+   delete FROM craplcm lcm
+      WHERE lcm.cdcooper = 3
+      AND lcm.nrdconta = 99999862
+      AND lcm.dtmvtolt = to_date('05/01/2024', 'dd/mm/yyyy')
+      AND lcm.cdhistor IN (2939, 2940, 2941, 3240);
    commit;
- end;   
+ end;  
