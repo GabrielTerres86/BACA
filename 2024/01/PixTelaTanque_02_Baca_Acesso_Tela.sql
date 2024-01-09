a543ee2443a4ad38d2fae7b3ad4a683c4abc36c3
Raphael Lorencini <amcom-raphael.lorencini@ailos.coop.br>
2024-01-09 18:03:20 -0300

    Tela Tanque - scripts baca alteração de usuários com permissão

--- 2024/01/PixTelaTanque_02_Baca_Acesso_Tela.sql
+++ 2024/01/PixTelaTanque_02_Baca_Acesso_Tela.sql
@@ -15,6 +15,7 @@ BEGIN
 	'f0033559',
 	'f0033734',
 	'f0034561',
+	'f0033184',
 	't0036027') 
     and crapope.cdsitope = 1
     and crapope.cdcooper = 3; 
@@ -35,6 +36,7 @@ BEGIN
 	'f0033559',
 	'f0033734',
 	'f0034561',
+	'f0033184',
 	't0036027')
     and crapope.cdsitope = 1
     and crapope.cdcooper = 3;     
