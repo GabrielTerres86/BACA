3429774eb34650548bdd7d450a888aa19128921d
Adriano Marchi <adriano.marchi@ailos.coop.br>
2024-01-09 10:43:21 -0300

    INC0309375: script para executar o programa pc_crps691 para a cooperativa Viacredi

--- 2024/01/INC0309375_execucao_pc_crps691.sql
+++ 2024/01/INC0309375_execucao_pc_crps691.sql
@@ -7,7 +7,6 @@ DECLARE
   
 BEGIN
   
-  -- Executar o programa para geração do relatório 5300
   cecred.pc_crps691(pr_cdcooper => 1 -- Viacredi
                    ,pr_flgresta => 0 -- Não se trata de restart
                    ,pr_stprogra => vr_stprogra
@@ -15,7 +14,6 @@ BEGIN
                    ,pr_cdcritic => vr_cdcritic
                    ,pr_dscritic => vr_dscritic);
                    
-  -- Se retornar alguma crítica
   IF NVL(vr_cdcritic,0) > 0 OR vr_dscritic IS NOT NULL THEN
     IF vr_dscritic IS NULL THEN 
       vr_dscritic := gene0001.fn_busca_critica(pr_cdcritic => vr_cdcritic);
@@ -23,10 +21,9 @@ BEGIN
     
     ROLLBACK;
     
-    -- Executar o levante de excessão
     RAISE_APPLICATION_ERROR(-20000,'Erro PC_CRPS691: '||vr_dscritic);
   END IF;
 
-  -- Commit das alterações 
+ 
   COMMIT;
 END;
