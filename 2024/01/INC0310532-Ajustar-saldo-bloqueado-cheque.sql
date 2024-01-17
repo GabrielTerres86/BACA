0756b0a9fd683f0d16b0d00b880cbc81d664db27
Renato Darosci <premiersoft-renato.darosci@ailos.coop.br>
2024-01-17 16:00:48 -0300

    Ajusta as informações de depósito de cheque alterando o valor informado de 45.000.000,00 para 45.000,00.

--- 2024/01/INC0310532-Ajustar-saldo-bloqueado-cheque.sql
+++ 2024/01/INC0310532-Ajustar-saldo-bloqueado-cheque.sql
@@ -1,27 +1,6 @@
 BEGIN
     
   BEGIN 
-    UPDATE cecred.crapsda t
-       SET t.vlsdbloq = ((NVL(t.vlsdbloq,0) - 45000000) + 45000)
-     WHERE t.cdcooper = 7
-       AND t.nrdconta = 163384
-       AND t.dtmvtolt = to_date('17/01/2024','dd/mm/yyyy');
-  EXCEPTION
-    WHEN OTHERS THEN
-      RAISE_APPLICATION_ERROR(-20001,'Erro ao alterar CRAPSDA: '||SQLERRM);
-  END;
-    
-  BEGIN 
-    UPDATE cecred.crapsld t
-       SET t.vlsdbloq = ((NVL(t.vlsdbloq,0) - 45000000) + 45000)
-     WHERE t.cdcooper = 7
-       AND t.nrdconta = 163384;
-  EXCEPTION
-    WHEN OTHERS THEN
-      RAISE_APPLICATION_ERROR(-20002,'Erro ao alterar CRAPSLD: '||SQLERRM);
-  END;
-
-  BEGIN 
     UPDATE cecred.crapdpb t
        SET t.vllanmto = 45000
      WHERE t.cdcooper = 7
