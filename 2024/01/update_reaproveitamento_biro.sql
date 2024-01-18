6f66c662a4c2a86a7984e492ff4e7509b25b12ed
Diogenes Lazzarini <diogenes.lazzarini@ailos.coop.br>
2024-01-18 13:33:09 -0300

    Update update_reaproveitamento_biro.sql

--- 2024/01/update_reaproveitamento_biro.sql
+++ 2024/01/update_reaproveitamento_biro.sql
@@ -1,16 +1,8 @@
 begin
 
-  UPDATE crapmbr
-  SET    nrordimp = 2
-  WHERE cdbircon = 1
-  AND   cdmodbir = 2; 
-  
-  DELETE
-  FROM  crapcbd
-  WHERE crapcbd.inreapro = 0
-    AND crapcbd.inreterr = 0
-    AND crapcbd.nrsdtsoc IS NULL
-    AND trunc(crapcbd.dtconbir) = trunc(SYSDATE);
+  UPDATE crapprm
+  SET DSVLRPRM = 'S'
+  WHERE CDACESSO = 'FL_SALVAR_ARQ_RET_BIRO';
     
   COMMIT;
 
