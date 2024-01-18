3d2f89cf3e6e32aaf04c9df3e2d7f10382d3bb4a
Diogenes Lazzarini <diogenes.lazzarini@ailos.coop.br>
2024-01-18 11:16:54 -0300

    Update update_reaproveitamento_biro.sql

--- 2024/01/update_reaproveitamento_biro.sql
+++ 2024/01/update_reaproveitamento_biro.sql
@@ -1,10 +1,16 @@
 begin
 
   UPDATE craprbi
-  SET qtdiarpv = -1
+  SET qtdiarpv = 30
   WHERE craprbi.cdcooper = 1
-  AND craprbi.inprodut = 7;    
+  AND craprbi.inprodut = 7
+  AND craprbi.inpessoa = 2;       
+  
+  UPDATE crapmbr
+  SET    nrordimp = 3
+  WHERE cdbircon = 1
+  AND   cdmodbir = 2; 
     
-  commit;
+  COMMIT;
 
 end;
