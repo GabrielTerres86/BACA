4edd4c011a7e2675bd60a8e37502b574dfbde0af
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2024-01-22 12:52:38 -0300

    Gerar contabeis solicitados - K, T, Microcredito

---
+++ 2024/01/02_microcred_compensacao_via.sql
@@ -0,0 +1,36 @@
+DECLARE
+  
+  vr_dtrefere_ris     DATE := to_date('31/12/2023', 'DD/MM/RRRR');
+  vr_dscritic         VARCHAR2(4000);  
+  vr_exc_erro         EXCEPTION;  
+  
+  CURSOR cr_crapcop IS
+    SELECT cdcooper
+      FROM cecred.crapcop a
+     WHERE a.flgativo = 1
+       AND a.cdcooper IN (1)
+     ORDER BY a.cdcooper DESC;
+  rw_crapcop cr_crapcop%ROWTYPE;
+  
+BEGIN
+  
+  FOR rw_crapcop IN cr_crapcop LOOP
+    
+    cecred.risc0001_nova_central.gerarRelatorioCompensacaoMicrocredito(pr_cdcooper => rw_crapcop.cdcooper
+                                                                      ,pr_dtrefere => vr_dtrefere_ris
+                                                                      ,pr_dscritic => vr_dscritic);
+    IF TRIM(vr_dscritic) IS NOT NULL THEN
+      RAISE vr_exc_erro;
+    END IF;  
+    
+    COMMIT;
+
+  END LOOP;
+  
+EXCEPTION 
+  WHEN vr_exc_erro THEN
+    ROLLBACK;
+    raise_application_error(-20000, vr_dscritic);
+  WHEN OTHERS THEN
+    raise_application_error(-20000, SQLERRM);
+END;
