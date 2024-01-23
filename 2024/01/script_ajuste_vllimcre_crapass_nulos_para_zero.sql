c5e27e72c534a562d744d3d1678d04eab1d4c12a
Douglas Ferreira Fagundes Gomes <douglas.gomes@ailos.coop.br>
2024-01-23 11:27:08 -0300

    alteração nomeclatura

--- 2024/01/script_ajuste_vllimcre_crapass_nulos_para_zero.sql
+++
@@ -1,8 +0,0 @@
-begin
-
-  update cecred.crapass ass
-     set ass.vllimcre = 0
-   where ass.vllimcre is null 
-     and ass.cdcooper = 2
-
-end;
---
+++ 2024/01/scriptajustevllimcre.sql
@@ -0,0 +1,8 @@
+BEGIN
+
+  UPDATE cecred.crapass ass
+     SET ass.vllimcre = 0
+   WHERE ass.vllimcre IS NULL 
+     AND ass.cdcooper = 2;
+
+END;
