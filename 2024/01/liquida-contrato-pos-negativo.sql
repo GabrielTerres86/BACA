f3888328075976b8dd0095f2c4c358990ee401df
Miguel Rodrigues <miguel.rodrigues@ailos.coop.br>
2024-01-29 15:56:00 -0300

    liquidacao de contratos com valores negativos

--- 2024/01/liquida-contrato-pos-negativo.sql
+++ 2024/01/liquida-contrato-pos-negativo.sql
@@ -1,4 +1,9 @@
+DECLARE
+  rw_crapdat    BTCH0001.cr_crapdat%ROWTYPE;
 BEGIN
+  OPEN  BTCH0001.cr_crapdat(pr_cdcooper => 1);
+  FETCH BTCH0001.cr_crapdat INTO rw_crapdat;
+        
   UPDATE CECRED.crappep
      SET inliquid = 1,        
          vlsdvpar = 0,
@@ -12,7 +17,8 @@ BEGIN
      SET inliquid = 1,
          vlsdeved = 0,
          vlsdevat = 0,
-         qtprepag = qtpreemp
+         qtprepag = qtpreemp,
+         dtliquid = rw_crapdat.dtmvtolt
    WHERE (cdcooper, nrdconta, nrctremp) IN (
           (10, 129305, 31003)
          ,(1, 4063562, 3425190)
