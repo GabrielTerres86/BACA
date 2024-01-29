1f468c7f43030082647a8cc530986058a8380304
Douglas <douglas.kloppel@ailos.coop.br>
2024-01-29 09:44:25 -0300

    incluindo owner

--- 2024/01/2-importacaoparcial.sql
+++ 2024/01/2-importacaoparcial.sql
@@ -8,11 +8,11 @@ declare
   
   cursor cr_crapcar IS
      SELECT crd.rowid, crd.nrconta_cartao, crd.dtmvtolt, crd.cdcooper, crd.nrdconta, 
-           (select car.cdcooper from tbcrd_conta_cartao car where  crd.nrconta_cartao = car.nrconta_cartao) coop_certa,
-           (select car.nrdconta from tbcrd_conta_cartao car where crd.nrconta_cartao = car.nrconta_cartao) conta_certa,
+           (select car.cdcooper from cecred.tbcrd_conta_cartao car where  crd.nrconta_cartao = car.nrconta_cartao) coop_certa,
+           (select car.nrdconta from cecred.tbcrd_conta_cartao car where crd.nrconta_cartao = car.nrconta_cartao) conta_certa,
            crd.qttransa_debito, crd.qttransa_credito, crd.vltransa_debito, crd.vltransa_credito 
-    from tbcrd_utilizacao_cartao crd
-    where not exists(Select 1 from tbcrd_conta_cartao car where crd.cdcooper = car.cdcooper and crd.nrdconta = car.nrdconta and crd.nrconta_cartao = car.nrconta_cartao)
+    from cecred.tbcrd_utilizacao_cartao crd
+    where not exists(Select 1 from cecred.tbcrd_conta_cartao car where crd.cdcooper = car.cdcooper and crd.nrdconta = car.nrdconta and crd.nrconta_cartao = car.nrconta_cartao)
           and crd.dtmvtolt between to_date('01/01/2023','dd/mm/yyyy')  AND to_date('31/12/2023','dd/mm/yyyy');
   rw_crapcar cr_crapcar%ROWTYPE;
 
@@ -41,7 +41,7 @@ BEGIN
                              pr_idprglog   => vr_idprglog);
  
     
-    loga := 'update tbcrd_utilizacao_cartao set nrdconta = '|| rw_crapcar.nrdconta || ', cdcooper = ' || rw_crapcar.cdcooper ||
+    loga := 'update cecred.tbcrd_utilizacao_cartao set nrdconta = '|| rw_crapcar.nrdconta || ', cdcooper = ' || rw_crapcar.cdcooper ||
            ' where tbcrd_utilizacao_cartao.rowid = '''|| rw_crapcar.rowid ||''';'; 
      CECRED.pc_log_programa(pr_dstiplog   => 'O',
                              pr_dsmensagem => loga,
@@ -51,7 +51,7 @@ BEGIN
                              pr_idprglog   => vr_idprglog);   
     
     BEGIN
-      UPDATE tbcrd_utilizacao_cartao
+      UPDATE cecred.tbcrd_utilizacao_cartao
          SET nrdconta      = rw_crapcar.conta_certa,
              cdCooper      = rw_crapcar.coop_certa
        WHERE tbcrd_utilizacao_cartao.rowid = rw_crapcar.rowid;
