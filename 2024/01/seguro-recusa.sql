ac083622ff724a2491d8ef1a006c92578490dce0
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2024-01-29 16:58:17 -0300

    Recusar seguros

--- 2024/01/seguro-recusa.sql
+++ 2024/01/seguro-recusa.sql
@@ -3,14 +3,14 @@ begin
 update cecred.tbseg_prestamista t
    set t.tpregist = 0
  where t.cdcooper = 16
-   and t.nrdconta = 96459727
-   and t.nrctremp = 743962;
+   and t.nrdconta = 99833557
+   and t.nrctremp = 743965;
    
 update cecred.crapseg s
    set s.cdsitseg = 5
   where s.cdcooper = 16
-   and s.nrdconta = 96459727
-   and s.nrctrseg = 441843;
+   and s.nrdconta = 99833557
+   and s.nrctrseg = 441849;
    
 commit;
 end;
