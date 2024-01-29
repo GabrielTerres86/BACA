60225929b95874de091a7f7e01bee25c9b10a485
eltonfernando <elton@ailos.coop.br>
2024-01-29 14:17:17 -0300

    Remoçao consultas desnecessarias

--- 2024/01/script_parametro_form_continuo.sql
+++ 2024/01/script_parametro_form_continuo.sql
@@ -1,5 +1,6 @@
 
-begin
+BEGIN
+  
     insert into cecred.craptab (nmsistem,tptabela,cdempres,cdacesso,tpregist,dstextab,cdcooper) values ('CRED', 'GENERI',0,'FORMCONTINUO',0,'1500',1);
     insert into cecred.craptab (nmsistem,tptabela,cdempres,cdacesso,tpregist,dstextab,cdcooper) values ('CRED', 'GENERI',0,'FORMCONTINUO',0,'1500',2);
     insert into cecred.craptab (nmsistem,tptabela,cdempres,cdacesso,tpregist,dstextab,cdcooper) values ('CRED', 'GENERI',0,'FORMCONTINUO',0,'1500',3);
@@ -14,27 +15,10 @@ begin
     insert into cecred.craptab (nmsistem,tptabela,cdempres,cdacesso,tpregist,dstextab,cdcooper) values ('CRED', 'GENERI',0,'FORMCONTINUO',0,'1500',13);
     insert into cecred.craptab (nmsistem,tptabela,cdempres,cdacesso,tpregist,dstextab,cdcooper) values ('CRED', 'GENERI',0,'FORMCONTINUO',0,'1500',14);
     insert into cecred.craptab (nmsistem,tptabela,cdempres,cdacesso,tpregist,dstextab,cdcooper) values ('CRED', 'GENERI',0,'FORMCONTINUO',0,'1500',16);
-end;
+    
+    COMMIT;
+    
+END;
 
 
-select * from crapcop where flgativo = 1;
 
-select *
-  from craptab
- where /* craptab.cdcooper = 1
-   AND */ craptab.nmsistem = 'CRED'
-   AND craptab.tptabela = 'GENERI'
-   AND craptab.cdempres = 0
-   AND craptab.cdacesso = 'FORMCONTINUO'
-   AND craptab.tpregist = 0;
-
-select *  from craptab where tptabela = 'COMPBB';
-
-select *
-  from craptab
- where craptab.cdcooper = 1
-   AND craptab.nmsistem = 'CRED'
-   AND craptab.tptabela = 'GENERI'
-   AND craptab.cdempres = 0
-   AND craptab.cdacesso = 'VALORESVLB'
-   AND craptab.tpregist = 0;
