de029bc00ed90763fbe9016435cbf493f87e330c
Raphael Lorencini <amcom-raphael.lorencini@ailos.coop.br>
2024-01-30 15:06:39 -0300

    Tela Tanque - adiciona matrículas para acesso

--- 2024/01/PixTelaTanque_02_Baca_Acesso_Tela.sql
+++ 2024/01/PixTelaTanque_02_Baca_Acesso_Tela.sql
@@ -1,26 +1,35 @@
 BEGIN  
- insert into crapace (nmdatela,cddopcao,cdoperad,nmrotina,cdcooper,nrmodulo,idevento,idambace)
+ insert into CECRED.crapace (nmdatela,cddopcao,cdoperad,nmrotina,cdcooper,nrmodulo,idevento,idambace)
   select 'TANQUE','@',crapope.cdoperad,null,3,1,0,2
-  from crapope 
+  from CECRED.crapope 
   where lower(crapope.cdoperad) IN(
 	'f0032543',
 	'f0030606',
 	'f0033550',
 	'f0033197',
 	'f0033184',
+  'f0034735',
+  'f0034744',
+  'f0034308',
+  'f0033589',
+  'f0033656',
 	't0036027') 
     and crapope.cdsitope = 1
     and crapope.cdcooper = 3; 
  
-  insert into crapace (nmdatela,cddopcao,cdoperad,nmrotina,cdcooper,nrmodulo,idevento,idambace)
+  insert into CECRED.crapace (nmdatela,cddopcao,cdoperad,nmrotina,cdcooper,nrmodulo,idevento,idambace)
   select 'TANQUE','C',crapope.cdoperad,null,3,1,0,2
-  from crapope 
+  from CECRED.crapope 
   where lower(crapope.cdoperad) IN(
 	'f0032543',
 	'f0030606',
 	'f0033550',
 	'f0033197',
 	'f0033184',
+  'f0034744',
+  'f0034308',
+  'f0033589',
+  'f0033656',
 	't0036027')
     and crapope.cdsitope = 1
     and crapope.cdcooper = 3;     
