7e890286249fa16bc2d560bf0b01acf439694592
Guilherme Breis <premiersoft-guilherme.pinheiro@ailos.coop.br>
2024-01-30 17:47:21 -0300

    add owner

--- 2024/01/BACA-PERMISS-ADD-USR.sql
+++ 2024/01/BACA-PERMISS-ADD-USR.sql
@@ -1,26 +1,26 @@
 BEGIN
 
   FOR rw_coop IN ((SELECT cdcooper
-                     FROM crapcop c
+                     FROM cecred.crapcop c
                     WHERE c.flgativo = 1)) LOOP
   
-    INSERT INTO crapace
+    INSERT INTO cecred.crapace
       (NMDATELA, CDDOPCAO, CDOPERAD, NMROTINA, CDCOOPER, NRMODULO, IDEVENTO, IDAMBACE)
     VALUES
       ('ATENDA', 'CR', 'f0033078', 'PRESTACOES', rw_coop.cdcooper, 1, 0, 2);    
-    INSERT INTO crapace
+    INSERT INTO cecred.crapace
       (NMDATELA, CDDOPCAO, CDOPERAD, NMROTINA, CDCOOPER, NRMODULO, IDEVENTO, IDAMBACE)
     VALUES
       ('ATENDA', 'CR', 'f0033479', 'PRESTACOES', rw_coop.cdcooper, 1, 0, 2);
-    INSERT INTO crapace
+    INSERT INTO cecred.crapace
       (NMDATELA, CDDOPCAO, CDOPERAD, NMROTINA, CDCOOPER, NRMODULO, IDEVENTO, IDAMBACE)
     VALUES
       ('ATENDA', 'CR', 'f0033754', 'PRESTACOES', rw_coop.cdcooper, 1, 0, 2);
-    INSERT INTO crapace
+    INSERT INTO cecred.crapace
       (NMDATELA, CDDOPCAO, CDOPERAD, NMROTINA, CDCOOPER, NRMODULO, IDEVENTO, IDAMBACE)
     VALUES
       ('ATENDA', 'CR', 'f0034370', 'PRESTACOES', rw_coop.cdcooper, 1, 0, 2);
-    INSERT INTO crapace
+    INSERT INTO cecred.crapace
       (NMDATELA, CDDOPCAO, CDOPERAD, NMROTINA, CDCOOPER, NRMODULO, IDEVENTO, IDAMBACE)
     VALUES
       ('ATENDA', 'CR', 'f0034476', 'PRESTACOES', rw_coop.cdcooper, 1, 0, 2);
