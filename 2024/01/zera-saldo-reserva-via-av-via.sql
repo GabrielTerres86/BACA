3f62e9055b37198ddb049d5dad5dbe1edf420edd
Guilherme Breis <premiersoft-guilherme.pinheiro@ailos.coop.br>
2024-01-31 17:37:13 -0300

    Nova CC - 1 - 14092670

--- 2024/01/zera-saldo-reserva-via-av-via.sql
+++ 2024/01/zera-saldo-reserva-via-av-via.sql
@@ -3,15 +3,20 @@ BEGIN
   UPDATE credito.tbepr_reserva_atraso a
      SET vlreserva = 0
    WHERE a.cdcooperativa = 1
-     AND a.nrconta_corrente = 16281845;
+         AND a.nrconta_corrente = 16281845;
 
   DELETE FROM credito.tbepr_reserva_lancamento a
-   WHERE a.idtbepr_reserva_lancamento IN (218948, 219794);
+   WHERE a.idtbepr_reserva_lancamento IN (218948, 219794, 220400);
 
   UPDATE credito.tbepr_reserva_atraso a
      SET vlreserva = 0
    WHERE a.cdcooperativa = 16
-     AND a.nrconta_corrente = 241687;
+         AND a.nrconta_corrente = 241687;
+
+  UPDATE credito.tbepr_reserva_atraso a
+     SET vlreserva = 0
+   WHERE a.cdcooperativa = 1
+         AND a.nrconta_corrente = 14092670;
 
   COMMIT;
 EXCEPTION
