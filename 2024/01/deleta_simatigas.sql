79bc36f5c3ef61abf51345fc2a124ddc2ff24d75
Vinicius Bedin Knapp <premiersoft-vinicius.knapp@ailos.coop.br>
2024-01-31 16:53:37 -0300

    Code review.

--- 2024/01/deleta_simatigas.sql
+++ 2024/01/deleta_simatigas.sql
@@ -23,7 +23,7 @@ declare
                            WHERE wpr.cdcooper = sim.cdcooper
                              AND wpr.nrdconta = sim.nrdconta
                              AND wpr.nrsimula = sim.nrsimula)
-         AND TO_DATE(sim.dtmvtolt)  < '01/11/2023'; 
+         AND TO_DATE(sim.dtmvtolt)  < TO_DATE('01/11/2023','DD/MM/YYYY'); 
          rw_crapsim cr_crapsim%rowtype;
          
      CURSOR cr_crapass IS
