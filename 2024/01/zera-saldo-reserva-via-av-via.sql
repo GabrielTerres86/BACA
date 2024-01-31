8ab9535c449d172f199c1c5b54d34ca1951d53ae
Guilherme Breis <premiersoft-guilherme.pinheiro@ailos.coop.br>
2024-01-31 17:41:30 -0300

    CODE REVIEW

--- 2024/01/zera-saldo-reserva-via-av-via.sql
+++ 2024/01/zera-saldo-reserva-via-av-via.sql
@@ -1,12 +1,12 @@
 BEGIN
 
+  DELETE FROM credito.tbepr_reserva_lancamento a
+   WHERE a.idtbepr_reserva_lancamento IN (218948, 219794, 220400);
+
   UPDATE credito.tbepr_reserva_atraso a
      SET vlreserva = 0
    WHERE a.cdcooperativa = 1
          AND a.nrconta_corrente = 16281845;
-
-  DELETE FROM credito.tbepr_reserva_lancamento a
-   WHERE a.idtbepr_reserva_lancamento IN (218948, 219794, 220400);
 
   UPDATE credito.tbepr_reserva_atraso a
      SET vlreserva = 0
