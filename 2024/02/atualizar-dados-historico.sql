c6d5299c67dd7c6b26a219ccc07563bde4da1a5f
Renato Darosci <premiersoft-renato.darosci@ailos.coop.br>
2024-02-09 08:43:15 -0300

    Aplica carga de dados do historico

---
+++ 2024/02/atualizar-dados-historico.sql
@@ -0,0 +1,186 @@
+begin
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CASH',0.00,3);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'INTERNET',0.00,3);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'AIMARO',0.00,5);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CAIXA',0.00,5);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CASH',0.00,5);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'INTERNET',0.00,5);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'AIMARO',0.00,6);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CAIXA',0.00,6);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CASH',0.00,6);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'INTERNET',0.00,6);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'AIMARO',0.00,7);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CAIXA',0.00,7);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CASH',0.00,7);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'INTERNET',0.00,7);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'AIMARO',0.00,8);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CAIXA',0.00,8);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CASH',0.00,8);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'INTERNET',0.00,8);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'AIMARO',0.00,9);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CAIXA',0.00,9);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CASH',0.00,9);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'INTERNET',0.00,9);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'AIMARO',0.00,10);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CAIXA',0.00,10);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CASH',0.00,10);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'INTERNET',0.00,10);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'AIMARO',0.00,11);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CAIXA',0.00,11);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CASH',0.00,11);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'INTERNET',0.00,11);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'AIMARO',0.00,12);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CAIXA',0.00,12);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CASH',0.00,12);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'INTERNET',0.00,12);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'AIMARO',0.00,13);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CAIXA',0.00,13);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CASH',0.00,13);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'INTERNET',0.00,13);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'AIMARO',0.00,14);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CAIXA',0.00,14);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CASH',0.00,14);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'INTERNET',0.00,14);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'AIMARO',0.00,16);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CAIXA',0.00,16);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'CASH',0.00,16);
+exception when dup_val_on_index then null; end;
+commit;
+begin
+INSERT INTO crapthi thi (cdhistor,dsorigem,vltarifa,cdcooper) VALUES (4505,'INTERNET',0.00,16);
+exception when dup_val_on_index then null; end;
+commit;
+end;
