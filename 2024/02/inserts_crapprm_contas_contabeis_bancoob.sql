515acd3d4ed35e8a5642f1b31d935dd176a15b30
CECRED\f0033505 <filipe.cardozo@ailos.coop.br>
2024-02-15 10:40:32 -0300

    Script para adicionar parâmetros na crapprm para o lançamento das contas contábeis bancoob

---
+++ 2024/02/inserts_crapprm_contas_contabeis_bancoob.sql
@@ -0,0 +1,6 @@
+BEGIN
+	insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_24420', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+	insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_34420', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+	
+	commit;
+END;
