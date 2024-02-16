96f804c8f7a396628818c8e038911e1e4ae91e4e
CECRED\f0033505 <filipe.cardozo@ailos.coop.br>
2024-02-16 10:14:15 -0300

    Adicionado outros parâmetros para outras cooperativas

--- 2024/02/inserts_crapprm_contas_contabeis_bancoob.sql
+++ 2024/02/inserts_crapprm_contas_contabeis_bancoob.sql
@@ -1,6 +1,29 @@
-BEGIN
-	insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_24420', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
-	insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_34420', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
-	
+﻿BEGIN
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_23265', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_23318', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_24457', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_24420', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_24468', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_24438', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_24416', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_24443', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_24444', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_24449', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_23232', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_24435', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_33265', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_33318', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_34457', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_34420', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_34468', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_34438', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_34416', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_34443', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_34444', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_34449', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_33232', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+  insert into cecred.crapprm (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM) values ('CRED', 0, 'CTA_BO_DOC_34435', 'Configuração dos lançamentos contábeis das operações nas contas cetralizadoras das filiadas', '442.000.000-7;1242');
+
 	commit;
 END;
