59c4f2371fde75d6736147aa9c10090b7f9aff02
Clébio Lima de Freitas <premiersoft-clebio.freitas@ailos.coop.br>
2024-02-20 08:27:04 -0300

    Corrigindo update da mensagem push de notificação de recebimento de pix

--- 2024/02/Update_mensagem_Push_notificacao_Pix.sql
+++ 2024/02/Update_mensagem_Push_notificacao_Pix.sql
@@ -1,6 +1,6 @@
 BEGIN
-  UPDATE DSTEXTO_MENSAGEM = 'Você recebeu de #nomepagador um crédito Pix de #valorpix. O valor já está em sua conta.'
-  FROM CECRED.TBGEN_NOTIF_MSG_CADASTRO 
+  UPDATE CECRED.TBGEN_NOTIF_MSG_CADASTRO
+  SET DSTEXTO_MENSAGEM = 'Você recebeu de #nomepagador um crédito Pix de #valorpix. O valor já está em sua conta.'
   WHERE CDMENSAGEM = 16051 
 	COMMIT;
 EXCEPTION
