6055769a7540bfd888f577137232c56295206d5e
Clébio Lima de Freitas <premiersoft-clebio.freitas@ailos.coop.br>
2024-02-20 16:28:31 -0300

    Incluindo cdmensagem 5469 no update da mensagem push da notificação de cebebimento pix

--- 2024/02/Update_mensagem_Push_notificacao_Pix.sql
+++ 2024/02/Update_mensagem_Push_notificacao_Pix.sql
@@ -1,8 +1,11 @@
 BEGIN
+
   UPDATE CECRED.TBGEN_NOTIF_MSG_CADASTRO
   SET DSTEXTO_MENSAGEM = 'Você recebeu de #nomepagador um crédito Pix de #valorpix. O valor já está em sua conta.'
-  WHERE CDMENSAGEM = 16051;
+  WHERE CDMENSAGEM in (16051, 5469);
+  
 	COMMIT;
+  
 EXCEPTION
   WHEN OTHERS THEN
     ROLLBACK;
