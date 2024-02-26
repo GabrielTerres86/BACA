c6e182f24cc506fd5d552a1a4458f6c09c01800c
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2024-02-26 13:25:23 -0300

    Adicionado rollback

--- 2023/08/drops_grupo_economico.sql
+++ 2023/08/drops_grupo_economico.sql
@@ -4,6 +4,14 @@ DECLARE
   vr_nrordprg crapprg.NRORDPRG%TYPE;
   vr_cdcooper crapcop.cdcooper%TYPE := 0;
   
+  vr_dscritic      VARCHAR2(4000);
+  vr_exc_erro      EXCEPTION;
+  
+  vr_nmarqrbk       VARCHAR2(100);
+  vr_nmdireto       VARCHAR2(400); 
+  vr_dados_rollback CLOB;
+  vr_texto_rollback VARCHAR2(32600);
+  
   CURSOR cr_crapcop IS
     SELECT cdcooper
       FROM cecred.crapcop c
@@ -44,6 +52,16 @@ BEGIN
   
   UPDATE crapprm SET dsvlrprm = 1 WHERE cdacesso = 'EXECUTAR_CARGA_CENTRAL';
   
+  vr_nmdireto := cecred.gene0001.fn_param_sistema('CRED',3,'ROOT_MICROS') || 'cpd/bacas/RISCO';
+  vr_nmarqrbk := 'ROLLBACK_VIRADA_'||to_char(sysdate,'ddmmyyyy_hh24miss')||'.sql';
+    
+  vr_dados_rollback := NULL;
+
+  dbms_lob.createtemporary(vr_dados_rollback, TRUE, dbms_lob.CALL);
+  dbms_lob.open(vr_dados_rollback, dbms_lob.lob_readwrite);     
+
+  cecred.gene0002.pc_escreve_xml(vr_dados_rollback, vr_texto_rollback, 'BEGIN'||chr(13), FALSE);
+    
   FOR rw_crapcop IN cr_crapcop LOOP
     
     vr_nrsolici := 0;
@@ -65,9 +83,45 @@ BEGIN
        WHERE p.CDCOOPER = rw_crapcop.cdcooper
          AND p.CDPROGRA = rw_crapprg.cdprogra;
          
+      gene0002.pc_escreve_xml(vr_dados_rollback
+                             ,vr_texto_rollback
+                             ,'UPDATE cecred.crapprg o ' || chr(13) || 
+                              '   SET o.NRSOLICI = ' || rw_crapprg.nrsolici || chr(13) ||
+                              '      ,o.INLIBPRG = ' || rw_crapprg.inlibprg || chr(13) ||
+                              ' WHERE o.CDCOOPER = ' || rw_crapcop.cdcooper || chr(13) ||
+                              '   AND o.CDPROGRA = ' || rw_crapprg.cdprogra || chr(13) || ';' || chr(13) || chr(13), FALSE);    
+         
     END LOOP;                        
   END LOOP;
-   
+  
+  gene0002.pc_escreve_xml(vr_dados_rollback
+                         ,vr_texto_rollback
+                         ,'UPDATE cecred.craptel SET FLGTELBL = 1 WHERE nmdatela = ''FORMGE'';'                 || chr(13) || 
+                          'UPDATE cecred.crapprm SET dsvlrprm = 1 WHERE cdacesso = ''EXECUTAR_CARGA_CENTRAL'';' || chr(13) ||
+                          'UPDATE cecred.crapprg a SET a.nrsolici = 76, a.inlibprg = 1 WHERE a.nmsistem = ''CRED'' AND a.cdprogra = ''CRPS656'' AND a.cdcooper = 3;' || chr(13) ||
+                           chr(13), FALSE);  
+  
+  cecred.gene0002.pc_escreve_xml(vr_dados_rollback, vr_texto_rollback, 'COMMIT; END;'||chr(13), FALSE);
+  cecred.gene0002.pc_escreve_xml(vr_dados_rollback, vr_texto_rollback, chr(13), TRUE); 
+               
+  cecred.GENE0002.pc_solicita_relato_arquivo(pr_cdcooper  => 3                             
+                                            ,pr_cdprogra  => 'ATENDA'                      
+                                            ,pr_dtmvtolt  => trunc(SYSDATE)                
+                                            ,pr_dsxml     => vr_dados_rollback             
+                                            ,pr_dsarqsaid => vr_nmdireto||'/'||vr_nmarqrbk 
+                                            ,pr_flg_impri => 'N'                           
+                                            ,pr_flg_gerar => 'S'                           
+                                            ,pr_flgremarq => 'N'                           
+                                            ,pr_nrcopias  => 1                             
+                                            ,pr_des_erro  => vr_dscritic);                 
+          
+  IF TRIM(vr_dscritic) IS NOT NULL THEN
+    RAISE vr_exc_erro;
+  END IF;   
+  
+  dbms_lob.close(vr_dados_rollback);
+  dbms_lob.freetemporary(vr_dados_rollback); 
+
   COMMIT; 
   
   BEGIN
