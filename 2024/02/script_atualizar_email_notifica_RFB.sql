9386a64d99a5579b787740b750498ad04b40ef44
Jose Diego Zunino <jose.zunino@ailos.coop.br>
2024-02-27 18:38:07 +0000

    Updated script_atualizar_email_notifica_RFB.sql

--- 2024/02/script_atualizar_email_notifica_RFB.sql
+++ 2024/02/script_atualizar_email_notifica_RFB.sql
@@ -1,8 +1,9 @@
 BEGIN         
-  UPDATE crapprm prm
-     SET prm.dsvlrprm = 'jose.zunino@ailos.coop.br;jeferson.nunes@ailos.coop.br'
-   WHERE prm.nmsistem = 'CRED'
-     AND prm.cdcooper IN(3,0) --> Busca tanto da passada, quanto da geral (se existir)
-     AND prm.cdacesso = 'EMAIL_REM_RFB';
+  UPDATE cecred.crapprm
+     SET dsvlrprm = 'jose.zunino@ailos.coop.br;jeferson.nunes@ailos.coop.br'
+   WHERE nmsistem = 'CRED'
+     AND cdcooper IN(3,0)
+     AND cdacesso = 'EMAIL_REM_RFB';
+
   COMMIT;
 END;    
