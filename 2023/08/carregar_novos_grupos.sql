2a3586407c52f471bbebafa87b66996b5c8fdb53
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2024-02-27 17:30:44 -0300

    Alterada tabela _old grupo economico

--- 2023/08/carregar_novos_grupos.sql
+++ 2023/08/carregar_novos_grupos.sql
@@ -12,18 +12,18 @@ DECLARE
       SELECT g.nrdconta
             ,g.idgrupo
             ,a.nrcpfcgc
-        FROM cecred.tbcc_grupo_economico g
+        FROM cecred.tbcc_grupo_economico_old g
             ,cecred.crapass a
        WHERE g.cdcooper = pr_cdcooper
          AND a.cdcooper = g.cdcooper
          AND a.nrdconta = g.nrdconta
-         AND EXISTS (SELECT 1 FROM cecred.tbcc_grupo_economico_integ i WHERE i.idgrupo = g.idgrupo AND i.dtexclusao IS NULL);
+         AND EXISTS (SELECT 1 FROM cecred.tbcc_grupo_economico_integ_old i WHERE i.idgrupo = g.idgrupo AND i.dtexclusao IS NULL);
     rw_grupo cr_grupo%ROWTYPE;
     
-    CURSOR cr_integ(pr_idgrupo IN cecred.tbcc_grupo_economico.idgrupo%TYPE) IS
+    CURSOR cr_integ(pr_idgrupo IN cecred.tbcc_grupo_economico_old.idgrupo%TYPE) IS
       SELECT i.nrcpfcgc
             ,i.nrdconta
-        FROM cecred.tbcc_grupo_economico_integ i
+        FROM cecred.tbcc_grupo_economico_integ_old i
        WHERE i.idgrupo = pr_idgrupo
          AND i.dtexclusao IS NULL;
     rw_integ cr_integ%ROWTYPE;
