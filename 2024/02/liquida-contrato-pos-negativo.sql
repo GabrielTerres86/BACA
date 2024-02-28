df7d414ff29a4831f848ef81139a14d1e4605441
Miguel Rodrigues <miguel.rodrigues@ailos.coop.br>
2024-02-28 09:09:49 -0300

    Liquida Parcelas Com Saldo Devedor Negativo POS

--- 2024/02/liquida-contrato-pos-negativo.sql
+++ 2024/02/liquida-contrato-pos-negativo.sql
@@ -4,12 +4,11 @@ BEGIN
      SET inliquid = 1,        
          vlsdvpar = 0,
          vlsdvatu = 0
-   WHERE (cdcooper, nrdconta, nrctremp) IN (
-         (16,731935,545521)
-         ,(1,10814264,2978569)
-         ,(16,811149,528002)
-         ,(16,609307,453824)
-
+   WHERE (cdcooper, nrdconta, nrctremp, nrparepr) IN (
+         (16,731935,545521,6)
+         ,(1,10814264,2978569,38)
+         ,(16,811149,528002,24)
+         ,(16,609307,453824,33)
        );
 
   COMMIT;
