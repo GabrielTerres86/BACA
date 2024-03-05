6bf9d0068d7df0228369104197b3132de3cf22b8
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2024-03-05 10:14:26 -0300

    TAA 111

--- 2023/12/incluir_saldo_taa.sql
+++ 2023/12/incluir_saldo_taa.sql
@@ -13,5 +13,19 @@ BEGIN
 	  ,1000.00
 	  ,9);
 
+	INSERT INTO crapstf
+	  (DTMVTOLT
+	  ,NRTERFIN
+	  ,VLDSDINI
+	  ,VLDSDFIN
+	  ,CDCOOPER)
+	VALUES
+	  (to_date('05-03-2024', 'dd-mm-yyyy')
+	  ,111
+	  ,1000.00
+	  ,1000.00
+	  ,9);
+
+
 	COMMIT; 
 END;
