2c9df97501eaf1d8d0e9a87a750da3cf21c32142
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2024-03-05 10:36:39 -0300

    Saldo - TAA 111, 120 e 127 - 06.03

--- 2023/12/incluir_saldo_taa.sql
+++ 2023/12/incluir_saldo_taa.sql
@@ -1,18 +1,19 @@
 BEGIN   
 
 	INSERT INTO crapstf
-	  (DTMVTOLT
-	  ,NRTERFIN
-	  ,VLDSDINI
-	  ,VLDSDFIN
-	  ,CDCOOPER)
+	  (DTMVTOLT,NRTERFIN,VLDSDINI,VLDSDFIN,CDCOOPER)
 	VALUES
-	  (to_date('05-03-2024', 'dd-mm-yyyy')
-	  ,111
-	  ,1000.00
-	  ,1000.00
-	  ,9);
+	  (to_date('06-03-2024', 'dd-mm-yyyy'),111,1000.00,1000.00,9);
 
+	INSERT INTO crapstf
+	  (DTMVTOLT,NRTERFIN,VLDSDINI,VLDSDFIN,CDCOOPER)
+	VALUES
+	  (to_date('06-03-2024', 'dd-mm-yyyy'),120,1000.00,1000.00,9);
+
+	INSERT INTO crapstf
+	  (DTMVTOLT,NRTERFIN,VLDSDINI,VLDSDFIN,CDCOOPER)
+	VALUES
+	  (to_date('06-03-2024', 'dd-mm-yyyy'),127,1000.00,1000.00,9);
 
 	COMMIT; 
 END;
