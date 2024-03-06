fdc55a7e3495a997e8cfdd276d737753195c0d75
Odirlei Busana <odirlei.busana@ailos.coop.br>
2024-03-06 12:08:21 +0000

    Updated Atualiza_sit_carga_05032024.sql

--- 2024/03/Atualiza_sit_carga_05032024.sql
+++ 2024/03/Atualiza_sit_carga_05032024.sql
@@ -10,14 +10,14 @@ UPDATE gestaoderisco.tbrisco_central_carga x
    SET x.CDSTATUS = 2
  WHERE x.DTREFERE = to_date('05/03/2024','DD/MM/RRRR')
    AND t.TPPRODUTO IN (95,96)
-   AND t.CDCOOPER IN (  1, -- VIACREDI
-                        6, -- UNILOS
-                        8, -- CREDELESC
-                       10, -- CREDICOMIN
-                       11, -- CREDIFOZ
-                       12, -- CREVISC
-                       13, -- CIVIA
-                       14  -- EVOLUA
+   AND t.CDCOOPER IN (  1, 
+                        6, 
+                        8, 
+                       10, 
+                       11, 
+                       12, 
+                       13, 
+                       14  
 
 );
 
