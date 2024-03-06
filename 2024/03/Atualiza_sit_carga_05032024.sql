0946fe0a611772023b67868468ec62d5e1000e05
Odirlei Busana <odirlei.busana@ailos.coop.br>
2024-03-06 12:43:20 +0000

    Updated Atualiza_sit_carga_05032024.sql

--- 2024/03/Atualiza_sit_carga_05032024.sql
+++ 2024/03/Atualiza_sit_carga_05032024.sql
@@ -9,8 +9,8 @@ UPDATE gestaoderisco.tbrisco_central_carga x
 UPDATE gestaoderisco.tbrisco_central_carga x
    SET x.CDSTATUS = 2
  WHERE x.DTREFERE = to_date('05/03/2024','DD/MM/RRRR')
-   AND t.TPPRODUTO IN (95,96)
-   AND t.CDCOOPER IN (  1, 
+   AND x.TPPRODUTO IN (95,96)
+   AND x.CDCOOPER IN (  1, 
                         6, 
                         8, 
                        10, 
