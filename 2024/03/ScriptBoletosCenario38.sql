417bbc1fdb896d8472b7714b51ad7dee7f950900
Jose Dill <jose.junior@premiersoft.net>
2024-03-06 16:22:34 -0300

    Ajuste

--- 2024/03/ScriptBoletosCenario38.sql
+++ 2024/03/ScriptBoletosCenario38.sql
@@ -1,7 +1,7 @@
 BEGIN
 
   UPDATE cecred.crapcob c
-     SET c.vltitulo = c.vltitulo - 50
+     SET c.vltitulo = c.vltitulo - 5
    WHERE (idtitleg, nrdconta, nrdocmto) in ((118078094, 82702764, 15), (118078063, 82702764, 4));
 
    COMMIT;
