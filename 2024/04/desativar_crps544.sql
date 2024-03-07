31e02d121f8d1b9b2fa08ace37a662bdc6d10535
alvori <alvori.feliciano@hotmail.com>
2024-03-07 10:44:12 -0300

    Ajuste Update
    Ajustado update para execução da desativação do mesmo

--- 2024/04/desativar_crps544.sql
+++ 2024/04/desativar_crps544.sql
@@ -1,4 +1,4 @@
 BEGIN
-  UPDATE CECRED.crapprg SET nrsolici = 1, inlibprg = 1 WHERE cdprogra = 'CRPS544';
+  UPDATE CECRED.crapprg SET nrsolici =99993, inlibprg = 2 WHERE cdprogra = 'CRPS544';
   COMMIT;
 END;
