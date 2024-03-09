e264c6213beab7785b33aaf6d3a92ab62ad232f5
Odirlei Busana <odirlei.busana@ailos.coop.br>
2024-03-09 00:20:25 +0000

    INC0319199 - Limpar dados de cartao cabal para reprocessamento

---
+++ 2024/03/INC0319199_limpar_dados_cartoes.sql.sql
@@ -0,0 +1,50 @@
+create index GESTAODERISCO.TBRISCO_OPERACAO_EMPRESTIMO_IDX1 on GESTAODERISCO.TBRISCO_OPERACAO_EMPRESTIMO(IDCARGA_OPERACAO) parallel 48;
+BEGIN
+
+  DELETE FROM crapvri x
+  WHERE x.rowid IN(SELECT DISTINCT r.rowid
+                     FROM tbcrd_risco t, crapvri r
+                    WHERE t.IDARQUIVO IN ( SELECT t.idarquivo
+                                             FROM tbcrd_arq_risco t
+                                            WHERE t.dtrefere   = to_date('29/02/2024','dd/mm/RRRR')
+                                              AND t.idbandeira = 1)
+                      AND r.dtrefere = t.DTREFERE
+                      AND r.cdcooper = t.cdcooper
+                      AND r.nrdconta = t.nrdconta
+                      AND r.nrctremp = t.NRCONTRATO
+                      AND r.cdmodali = t.NRMODALIDADE
+                      );
+
+  DELETE FROM crapris x
+  WHERE x.rowid IN(SELECT DISTINCT r.rowid
+                     FROM tbcrd_risco t, crapris r
+                    WHERE t.IDARQUIVO IN ( SELECT t.idarquivo
+                                             FROM tbcrd_arq_risco t
+                                            WHERE t.dtrefere   = to_date('29/02/2024','dd/mm/RRRR')
+                                              AND t.idbandeira = 1)
+                      AND r.dtrefere = t.DTREFERE
+                      AND r.cdcooper = t.cdcooper
+                      AND r.nrdconta = t.nrdconta
+                      AND r.nrctremp = t.NRCONTRATO
+                      AND r.cdmodali = t.NRMODALIDADE
+                      AND r.INDDOCTO = 4
+                      );
+
+  DELETE FROM tbcrd_risco x
+   WHERE x.idarquivo IN (SELECT t.idarquivo
+                           FROM tbcrd_arq_risco t
+                          WHERE t.dtrefere   = to_date('29/02/2024','dd/mm/RRRR')
+                            AND t.idbandeira = 1);
+
+  DELETE FROM tbcrd_arq_risco t
+   WHERE t.DTREFERE = to_date('29/02/2024','dd/mm/RRRR')
+     AND t.IDBANDEIRA = 1;
+
+  UPDATE crapprm t
+     SET t.DSVLRPRM = '06/02/2024;0;0;1'
+   WHERE t.CDACESSO = 'RISCO_CARTAO_BACEN'
+     AND t.cdcooper = 8;
+
+  COMMIT;
+  
+END;
