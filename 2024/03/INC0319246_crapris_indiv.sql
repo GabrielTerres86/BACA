c92a420e0522654422e63ab134c5272817874c83
Odirlei Busana <odirlei.busana@ailos.coop.br>
2024-03-09 17:21:22 +0000

    limpar indicador de individualizado para regerar 3040

---
+++ 2024/03/INC0319246_crapris_indiv.sql
@@ -0,0 +1,21 @@
+DECLARE 
+
+  CURSOR cr_crapcop IS
+    SELECT c.cdcooper
+      FROM crapcop c
+     WHERE c.flgativo = 1;
+
+BEGIN
+
+  FOR rw_crapcop IN cr_crapcop LOOP
+    
+    UPDATE crapris ris
+       SET ris.flgindiv = 0
+     WHERE ris.cdcooper = rw_crapcop.cdcooper
+       AND ris.dtrefere = to_date('29/02/2024', 'DD/MM/RRRR');
+
+    COMMIT;
+    
+  END LOOP;
+  
+END;
