d801e7b77aa42c1ddd95b76e24d8a35c30e05b85
Elton Giusti <t0031664@ailos.coop.br>
2024-03-15 15:50:06 -0300

    Ajuste

--- 2024/03/ajuste_debitar.sql
+++ 2024/03/ajuste_debitar.sql
@@ -3,8 +3,8 @@ BEGIN
 DELETE CREDITO.TBEPR_TRANSF_ARQUIVO_IMOB imo
             WHERE imo.cdcooper = 0
               AND imo.nrdconta = 0
-              AND imo.idtipoarq = 2 -- Retorno
-              AND imo.idtiporem = 'P' -- Pagamento
+              AND imo.idtipoarq = 2 
+              AND imo.idtiporem = 'P' 
               AND imo.dtgeracao = to_date('15/03/2024', 'dd-mm-yyyy');
   
 insert into craplcm (DTMVTOLT, CDAGENCI, CDBCCXLT, NRDOLOTE, NRDCONTA, NRDOCMTO, CDHISTOR, NRSEQDIG, VLLANMTO, NRDCTABB, CDPESQBB, VLDOIPMF, NRAUTDOC, NRSEQUNI, CDBANCHQ, CDCMPCHQ, CDAGECHQ, NRCTACHQ, NRLOTCHQ, SQLOTCHQ, DTREFERE, HRTRANSA, CDOPERAD, DSIDENTI, CDCOOPER, NRDCTITG, DSCEDENT, CDCOPTFN, CDAGETFN, NRTERFIN, NRPAREPR,  NRSEQAVA, NRAPLICA, CDORIGEM, IDLAUTOM, DTTRANS)
