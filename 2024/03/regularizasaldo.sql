2fc242df6ee97539df0e8a120d6875e6ff9b4606
Jaison Fernando Ferreira <jaison.ferreira@ailos.coop.br>
2024-03-15 10:27:07 -0300

    ajuste

--- 2024/03/regularizasaldo.sql
+++ 2024/03/regularizasaldo.sql
@@ -1,7 +1,7 @@
 begin
-	DELETE
-	  from crapsda a 
+	UPDATE crapsld a 
+	  SET a.dtrefere = TO_DATE('14/03/2024','DD/MM/YYYY')
 	  where a.cdcooper = 13
-	  and a.dtmvtolt >= to_date('08/03/2024','dd/mm/yyyy');
+	  and a.dtrefere >= TO_DATE('04/03/2024','DD/MM/YYYY');
 	commit;
 end;
