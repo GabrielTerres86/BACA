95fe8e6409a761fd718e944d31f4f4978aac9f4a
Andrey Leonardo Bauler <t0033422@ailos.coop.br>
2024-03-18 17:09:36 -0300

    Inserir menus do pix no escopo de multiplas contas

---
+++ 2024/03/insere_menus_pix_mc.sql
@@ -0,0 +1,247 @@
+BEGIN
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    82,
+    sysdate
+  );
+  
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    83,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    84,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    90,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    136,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    87,
+    sysdate
+  );
+  
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    141,
+    sysdate
+  );
+  
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    142,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    143,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    144,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    145,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    146,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    147,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    148,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    149,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    150,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    151,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    152,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    153,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    154,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    155,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    156,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    158,
+    sysdate
+  );
+  
+  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
+    IDITEM_MENU_MULTCONTAS,
+    CDITEM_MENU,
+    DHCADASTRO
+  ) VALUES (
+    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
+    159,
+    sysdate
+  );
+  COMMIT;
+
+END;
