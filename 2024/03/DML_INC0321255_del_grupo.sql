94ca8e0f1a5fcd0b40647d44bab56fff4308f690
Odirlei Busana <odirlei.busana@ailos.coop.br>
2024-03-19 12:51:27 +0000

    Updated DML_INC0321255_del_grupo.sql

--- 2024/03/DML_INC0321255_del_grupo.sql
+++ 2024/03/DML_INC0321255_del_grupo.sql
@@ -1,7 +1,7 @@
 BEGIN
-  DELETE cecred.tbrisco_grupo_economico_integrante g
+  DELETE gestaoderisco.tbrisco_grupo_economico_integrante g
    WHERE g.cdcooper = 1
      AND g.NRDCONTA = 10736174
-     AND g.cdgrupo_economico = 312398;
+     AND g.cdgrupo_economico = 131188;
   COMMIT;   
 END;
