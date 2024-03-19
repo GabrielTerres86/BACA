1f3b5d884a91859ee92066c4162f97f83c7008ff
Darly Cesar Marcelino Agostinho <amcom-darly.agostinho@ailos.coop.br>
2024-03-19 08:29:46 -0300

    Deletar logs tabelas transacionais

--- 2024/03/RITM0375608_deletar_logs_tabelas_2.sql
+++ 2024/03/RITM0375608_deletar_logs_tabelas_2.sql
@@ -3,10 +3,10 @@ BEGIN
    WHERE lgm.cdoperad = 't0035324';
 
   DELETE FROM craplgi lgi
-   WHERE lgi.dttransa = TRUNC(SYSDATE - 1);
+   WHERE lgi.dttransa = TRUNC(SYSDATE);
 
   DELETE FROM cecred.tbgen_erro_sistema s
-   WHERE TRUNC(s.dherro) = TRUNC(SYSDATE - 1);
+   WHERE TRUNC(s.dherro) = TRUNC(SYSDATE);
  COMMIT;
 EXCEPTION 
   WHEN OTHERS THEN 
