ee384c471ac2c12cc1b02c3bfbe2049a7e1c2b6d
alvori <alvori.feliciano@hotmail.com>
2024-03-19 18:59:27 -0300

    desabilitacao Programas PLD
    Desativado os programas CRPS598,599 e 608 conforme solicitacao da Squad responsavel

---
+++ 2024/04/desativar_crps598_599_608.sql
@@ -0,0 +1,6 @@
+BEGIN
+  UPDATE CECRED.crapprg SET nrsolici = 9980, inlibprg = 2 WHERE cdprogra = 'CRPS598';
+  UPDATE CECRED.crapprg SET nrsolici = 9979, inlibprg = 2 WHERE cdprogra = 'CRPS599';
+  UPDATE CECRED.crapprg SET nrsolici = 9978, inlibprg = 1 WHERE cdprogra = 'CRPS608';
+  COMMIT;
+END;
