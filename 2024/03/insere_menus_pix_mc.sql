691bba2f1ee0885b505b273bf3a5e07ca0564269
Andrey Leonardo Bauler <t0033422@ailos.coop.br>
2024-03-19 14:18:20 -0300

    ajustes

--- 2024/03/insere_menus_pix_mc.sql
+++ 2024/03/insere_menus_pix_mc.sql
@@ -148,46 +148,6 @@ BEGIN
     DHCADASTRO
   ) VALUES (
     (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
-    146,
-    sysdate
-  );
-  
-  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
-    IDITEM_MENU_MULTCONTAS,
-    CDITEM_MENU,
-    DHCADASTRO
-  ) VALUES (
-    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
-    147,
-    sysdate
-  );
-  
-  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
-    IDITEM_MENU_MULTCONTAS,
-    CDITEM_MENU,
-    DHCADASTRO
-  ) VALUES (
-    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
-    148,
-    sysdate
-  );
-  
-  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
-    IDITEM_MENU_MULTCONTAS,
-    CDITEM_MENU,
-    DHCADASTRO
-  ) VALUES (
-    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
-    149,
-    sysdate
-  );
-  
-  INSERT INTO INTERNETBANKING.tbib_itens_menu_multcontas (
-    IDITEM_MENU_MULTCONTAS,
-    CDITEM_MENU,
-    DHCADASTRO
-  ) VALUES (
-    (SELECT max(IDITEM_MENU_MULTCONTAS) + 1 FROM tbib_itens_menu_multcontas),
     150,
     sysdate
   );
