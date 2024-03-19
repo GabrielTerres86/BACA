9cb1f7350f250c1db2b87b31e9287d668ed66f8d
Douglas <douglas.kloppel@ailos.coop.br>
2024-03-19 18:09:13 -0300

    incluindo o owner

--- 2024/03/Script_Altera_Tabela_IRPF.sql
+++ 2024/03/Script_Altera_Tabela_IRPF.sql
@@ -1,27 +1,27 @@
 BEGIN
 
-  UPDATE tbcotas_faixas_irrf i
+  UPDATE cecred.tbcotas_faixas_irrf i
      SET i.vlfaixa_final = 2259.20
    WHERE i.inpessoa = 1
      AND i.cdfaixa = 1;
 
-  UPDATE tbcotas_faixas_irrf i
+  UPDATE cecred.tbcotas_faixas_irrf i
      SET i.vlfaixa_inicial = 2259.21
         ,i.vldeducao       = 169.44
    WHERE i.inpessoa = 1
      AND i.cdfaixa = 2;
 
-  UPDATE tbcotas_faixas_irrf i
+  UPDATE cecred.tbcotas_faixas_irrf i
      SET i.vldeducao = 381.44
    WHERE i.inpessoa = 1
      AND i.cdfaixa = 3;
 
-  UPDATE tbcotas_faixas_irrf i
+  UPDATE cecred.tbcotas_faixas_irrf i
      SET i.vldeducao = 662.77
    WHERE i.inpessoa = 1
      AND i.cdfaixa = 4;
 
-  UPDATE tbcotas_faixas_irrf i
+  UPDATE cecred.tbcotas_faixas_irrf i
      SET i.vldeducao = 896
    WHERE i.inpessoa = 1
      AND i.cdfaixa = 5;
