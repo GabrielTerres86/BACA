f2e9b689fb65034b23ce439b5328b0cfbeb33cc7
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2024-03-20 10:13:56 -0300

    Ajuste script

--- 2024/03/ajuste-endiv-ge.sql
+++ 2024/03/ajuste-endiv-ge.sql
@@ -1,3 +1,9 @@
 BEGIN
+  UPDATE cecred.tbcadast_cooperativa a 
+     SET a.vlsomatorio_leco = (SELECT SUM(l.vlendividamento) 
+                                 FROM credito.tbcred_somatorioleco l
+                                WHERE l.cdcooper = a.cdcooper
+                                  AND l.dtrefere = to_date('15/03/2024', 'DD/MM/RRRR'))
+   WHERE a.cdcooper NOT IN (1);
   COMMIT;
 END;
