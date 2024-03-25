fa7f2afa4f219f86b01c7277b1ad46d0a75a45b2
Evandro Guaranha(Amcom) <evandro.silva@amcom.com.br>
2024-03-25 14:54:58 -0300

    Ajuste removendo contas ja corrigidas em prod

--- 2024/03/script_INC0318922_ajusta_diff_contabil.sql
+++ 2024/03/script_INC0318922_ajusta_diff_contabil.sql
@@ -1,31 +1,15 @@
 DECLARE
-  rw_crapdat     datasCooperativa;
-  rw_crapdat_2   datasCooperativa;
-    
+  rw_crapdat datasCooperativa;   
 BEGIN
-   rw_crapdat := datasCooperativa(pr_cdcooper => 14);
-       
-   update gestaoderisco.tbcc_historico_juros_adp his
-      set his.vlcorte = 2018.98
-    where his.cdcooper = 14
-      and his.nrdconta = 14995921
-      and his.dtmvtolt = rw_crapdat.dtmvtoan;           
-
-   update gestaoderisco.tbcc_historico_juros_adp his
-      set his.vlcorte = 697.66
-    where his.cdcooper = 14
-      and his.nrdconta = 16047109
-      and his.dtmvtolt = rw_crapdat.dtmvtoan;
-  
-  rw_crapdat_2 := datasCooperativa(pr_cdcooper => 5);
+   rw_crapdat := datasCooperativa(pr_cdcooper => 5);
   
    update gestaoderisco.tbcc_historico_juros_adp his
-      set his.vlcorte = 11260.20
+      set his.vlcorte = 191.10
     where his.cdcooper = 5
       and his.nrdconta = 147265
-      and his.dtmvtolt = rw_crapdat_2.dtmvtoan;
+      and his.dtmvtolt = rw_crapdat.dtmvtoan;
       
-  COMMIT;
+   COMMIT;
 
 EXCEPTION
   WHEN OTHERS THEN
