f8cbf5d12c25d2811480bc0db68541e8c7b039e6
Guilherme Breis <premiersoft-guilherme.pinheiro@ailos.coop.br>
2024-03-25 17:03:26 -0300

    aprovar contrato

--- 2024/03/baca-aprova-ctr.sql
+++ 2024/03/baca-aprova-ctr.sql
@@ -6,4 +6,10 @@ BEGIN
          AND nrdconta = 99455960
          AND nrctremp = 322730;
 
+  UPDATE crawepr
+     SET insitest = 3, insitapr = 1
+   WHERE cdcooper = 13
+         AND nrdconta = 99993473
+         AND nrctremp = 322731;
+
 END;
