0fbc763bb2695b349f6dfd74f76d6c9b3b854f30
Odirlei Busana <odirlei.busana@ailos.coop.br>
2024-03-29 18:34:15 +0000

    Added 4 files to /2024/03

---
+++ 2024/03/DML_RISCO_K_CREDIFOZ.sql
@@ -0,0 +1,23 @@
+DECLARE
+ pr_cdcooper NUMBER := 11;
+ pr_dtrefere DATE   := to_date('31/03/2024','DD/MM/RRRR');
+ pr_retfile  VARCHAR2(4000) := NULL;
+ pr_dscritic VARCHAR2(4000) := NULL;
+ 
+  
+BEGIN 
+  CECRED.RISC0001_3103.pc_risco_k(pr_cdcooper => pr_cdcooper,
+                           pr_dtrefere => pr_dtrefere,
+                           pr_retfile  => pr_retfile,
+                           pr_dscritic => pr_dscritic);
+                           
+  IF pr_dscritic IS NOT NULL THEN
+    raise_application_error(-20500,'ERRO: '||pr_dscritic);
+    
+  END IF;
+  
+  raise_application_error(-20500,'GERADO COM SUCESSO: '||pr_retfile);
+    
+  
+                             
+end;
