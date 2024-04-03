55cb4ef5768e6df09edee1611e4023c62b5f9ab3
Derick Antonio Vareschi <amcom-derick.vareschi@ailos.coop.br>
2024-04-03 10:02:11 -0300

    Ajuste Flag de erro para permitir apovação manual de emprestimo de portabilidade

--- 2024/04/bt_erro_ajusteRitm.sql
+++ 2024/04/bt_erro_ajusteRitm.sql
@@ -6,6 +6,12 @@ BEGIN
      and TP.NRDCONTA = 16904974
      and TP.NRCTREMP = 338282;
 
+  UPDATE CECRED.crawepr A
+     SET A.dtlibera = to_date('03/04/2024', 'DD/MM/YYYY')
+   WHERE A.CDCOOPER = 13
+     and A.NRDCONTA = 16904974
+     and A.NRCTREMP = 338282;
+
   COMMIT;
 
 EXCEPTION
