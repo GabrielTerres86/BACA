a692d78d5a20f59bb8e3bbf859b4c9907bfd842f
James Kuehl <f0034526@cecred.coop.br>
2024-04-08 00:31:54 -0300

    SFTP testes

--- 2024/04/inc0315899_sftp_alteracao_pwd.sql
+++ 2024/04/inc0315899_sftp_alteracao_pwd.sql
@@ -1,4 +1,4 @@
 begin
-  update cecred.tbseg_parametros_prst pp set pp.senhaftp = '@Ailos2019@X';       
+  update cecred.tbseg_parametros_prst p set p.enderftp = 'producao.icatuseguros.com.br', p.loginftp = 'Producao-AilosTST', p.senhaftp = 'TsA!xvnj@109';       
   commit;
 end;  
