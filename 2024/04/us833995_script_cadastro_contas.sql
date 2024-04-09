67e52ad33d98db18506fa6bb53b970e0075ca1a2
Evandro Guaranha(Amcom) <evandro.silva@amcom.com.br>
2024-04-09 11:33:08 -0300

    script cadastro cadpar contas

---
+++ 2024/04/us833995_script_cadastro_contas.sql
@@ -0,0 +1,59 @@
+DECLARE
+
+  CURSOR cr_crapcop IS
+  SELECT cop.cdcooper,
+         'CONTA_FUNDING_CONTRATO' nome_param,
+         CASE WHEN cop.cdcooper = 1 THEN 671
+              WHEN cop.cdcooper = 2 THEN 892
+              WHEN cop.cdcooper = 7 THEN 914
+              WHEN cop.cdcooper = 11 THEN 932
+              WHEN cop.cdcooper = 16 THEN 680 
+              ELSE 0 END conta_param     
+    FROM crapcop cop
+   WHERE cop.flgativo = 1
+     AND cop.cdcooper IN(1,2,7,11,16)
+   UNION
+  SELECT cop.cdcooper,
+         'CONTA_PRO_COTISTA_CTR' nome_param,
+         CASE WHEN cop.cdcooper = 1 THEN 710
+              WHEN cop.cdcooper = 2 THEN 906
+              WHEN cop.cdcooper = 7 THEN 922
+              WHEN cop.cdcooper = 11 THEN 949
+              WHEN cop.cdcooper = 16 THEN 728 
+              ELSE 0 END conta_param    
+    FROM crapcop cop
+   WHERE cop.flgativo = 1
+     AND cop.cdcooper IN(1,2,7,11,16)
+ORDER BY nome_param,cdcooper;
+ 
+BEGIN
+  
+  FOR rw_crapcop IN cr_crapcop LOOP
+      INSERT INTO crappat
+        (CDPARTAR
+        ,NMPARTAR
+        ,TPDEDADO
+        ,CDPRODUT)
+      SELECT (SELECT MAX(cdpartar) + 1 FROM crappat)
+             ,rw_crapcop.nome_param
+             ,2
+             ,0
+        FROM DUAL
+       WHERE NOT EXISTS(SELECT 1 FROM crappat b WHERE b.nmpartar = rw_crapcop.nome_param);
+       
+      INSERT INTO crappco
+         (CDPARTAR
+         ,CDCOOPER
+         ,DSCONTEU)
+      VALUES
+         ((SELECT a.cdpartar FROM crappat a WHERE a.nmpartar = rw_crapcop.nome_param)
+         ,rw_crapcop.cdcooper
+         ,rw_crapcop.conta_param);
+  
+      COMMIT;
+  END LOOP;
+  
+EXCEPTION 
+  WHEN OTHERS THEN
+    ROLLBACK;
+END;
