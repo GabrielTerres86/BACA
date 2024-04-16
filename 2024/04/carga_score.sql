d8f327f4bbe04b8f4535e436aca8000f512aae51
Giovane Diego Gibbert <giovane.gibbert@ailos.coop.br>
2024-04-16 16:26:53 -0300

    Adicionado tratamento de erro

--- 2024/04/carga_score.sql
+++ 2024/04/carga_score.sql
@@ -2,31 +2,31 @@ BEGIN
 
 insert into CECRED.TBCRD_CARGA_SCORE (CDMODELO, DTBASE, DSMODELO, DTINICIO, DTTERMINO, CDOPCAO, CDOPERAD, QTREGIS_FISICA, QTREGIS_JURIDI, DSREJEICAO)
 values (4, to_date(SYSDATE, 'dd-mm-yyyy'), 'COLLECTION', to_date(SYSDATE, 'dd-mm-yyyy hh24:mi:ss'), to_date(SYSDATE, 'dd-mm-yyyy hh24:mi:ss'), 'A', 'BI', null, null, null);
-
+COMMIT;
 insert into CECRED.tbcrd_score (CDCOOPER, CDMODELO, DTBASE, TPPESSOA, NRCPFCNPJBASE, NRSCORE_ALINHADO, DSCLASSE_SCORE, DSEXCLUSAO_PRINCIPAL, FLVIGENTE, CDSCORE, OBDETALHAMENTO, CDSEGMENTO_MODELO)
 values (0, 4, to_date(SYSDATE, 'dd-mm-yyyy'), 1, 50248668, 486, ' ', null, 1, 4, '[{"cdscore1":"","cdscore2":"","cdscore3":616,"cdscore4":486,"cdscore5":"1","cdscore6":"","cdscore7":"","cdscore8":"","cdscore9":"","cdscore10":74}]', 4);
-
+COMMIT;
 insert into CECRED.tbcrd_score (CDCOOPER, CDMODELO, DTBASE, TPPESSOA, NRCPFCNPJBASE, NRSCORE_ALINHADO, DSCLASSE_SCORE, DSEXCLUSAO_PRINCIPAL, FLVIGENTE, CDSCORE, OBDETALHAMENTO, CDSEGMENTO_MODELO)
 values (0, 4, to_date(SYSDATE, 'dd-mm-yyyy'), 1, 45017331, 658, ' ', null, 1, 3, '[{"cdscore1":"","cdscore2":"","cdscore3":616,"cdscore4":486,"cdscore5":"2","cdscore6":"","cdscore7":"","cdscore8":"","cdscore9":"","cdscore10":74}]', 3);
-
+COMMIT;
 insert into CECRED.tbcrd_score (CDCOOPER, CDMODELO, DTBASE, TPPESSOA, NRCPFCNPJBASE, NRSCORE_ALINHADO, DSCLASSE_SCORE, DSEXCLUSAO_PRINCIPAL, FLVIGENTE, CDSCORE, OBDETALHAMENTO, CDSEGMENTO_MODELO)
 values (0, 4, to_date(SYSDATE, 'dd-mm-yyyy'), 1, 27787265, 458, ' ', null, 1, 4, '[{"cdscore1":"","cdscore2":"","cdscore3":616,"cdscore4":486,"cdscore5":"3","cdscore6":"","cdscore7":"","cdscore8":"","cdscore9":"","cdscore10":74}]', 4);
-
+COMMIT;
 insert into CECRED.tbcrd_score (CDCOOPER, CDMODELO, DTBASE, TPPESSOA, NRCPFCNPJBASE, NRSCORE_ALINHADO, DSCLASSE_SCORE, DSEXCLUSAO_PRINCIPAL, FLVIGENTE, CDSCORE, OBDETALHAMENTO, CDSEGMENTO_MODELO)
 values (0, 4, to_date(SYSDATE, 'dd-mm-yyyy'), 1, 36766523, 383, ' ', null, 1, 4, '[{"cdscore1":"","cdscore2":"","cdscore3":616,"cdscore4":486,"cdscore5":"4","cdscore6":"","cdscore7":"","cdscore8":"","cdscore9":"","cdscore10":74}]', 4);
-
+COMMIT;
 insert into CECRED.tbcrd_score (CDCOOPER, CDMODELO, DTBASE, TPPESSOA, NRCPFCNPJBASE, NRSCORE_ALINHADO, DSCLASSE_SCORE, DSEXCLUSAO_PRINCIPAL, FLVIGENTE, CDSCORE, OBDETALHAMENTO, CDSEGMENTO_MODELO)
 values (0, 4, to_date(SYSDATE, 'dd-mm-yyyy'), 1, 23870809, 577, ' ', null, 1, 4, '[{"cdscore1":"","cdscore2":"","cdscore3":616,"cdscore4":486,"cdscore5":"6","cdscore6":"","cdscore7":"","cdscore8":"","cdscore9":"","cdscore10":74}]', 4);
-
+COMMIT;
 insert into CECRED.tbcrd_score (CDCOOPER, CDMODELO, DTBASE, TPPESSOA, NRCPFCNPJBASE, NRSCORE_ALINHADO, DSCLASSE_SCORE, DSEXCLUSAO_PRINCIPAL, FLVIGENTE, CDSCORE, OBDETALHAMENTO, CDSEGMENTO_MODELO)
 values (0, 4, to_date(SYSDATE, 'dd-mm-yyyy'), 1, 27383664, 480, ' ', null, 1, 4, '[{"cdscore1":"","cdscore2":"","cdscore3":616,"cdscore4":486,"cdscore5":"7","cdscore6":"","cdscore7":"","cdscore8":"","cdscore9":"","cdscore10":74}]', 4);
-
+COMMIT;
 insert into CECRED.tbcrd_score (CDCOOPER, CDMODELO, DTBASE, TPPESSOA, NRCPFCNPJBASE, NRSCORE_ALINHADO, DSCLASSE_SCORE, DSEXCLUSAO_PRINCIPAL, FLVIGENTE, CDSCORE, OBDETALHAMENTO, CDSEGMENTO_MODELO)
 values (0, 4, to_date(SYSDATE, 'dd-mm-yyyy'), 1, 09249638, 417, ' ', null, 1, 4, '[{"cdscore1":"","cdscore2":"","cdscore3":616,"cdscore4":486,"cdscore5":"8","cdscore6":"","cdscore7":"","cdscore8":"","cdscore9":"","cdscore10":74}]', 4);
-
+COMMIT;
 insert into CECRED.tbcrd_score (CDCOOPER, CDMODELO, DTBASE, TPPESSOA, NRCPFCNPJBASE, NRSCORE_ALINHADO, DSCLASSE_SCORE, DSEXCLUSAO_PRINCIPAL, FLVIGENTE, CDSCORE, OBDETALHAMENTO, CDSEGMENTO_MODELO)
 values (0, 4, to_date(SYSDATE, 'dd-mm-yyyy'), 1, 01064738, 441, ' ', null, 1, 4, '[{"cdscore1":"","cdscore2":"","cdscore3":616,"cdscore4":486,"cdscore5":"9","cdscore6":"","cdscore7":"","cdscore8":"","cdscore9":"","cdscore10":74}]', 4);
-
+COMMIT;
 insert into CECRED.tbcrd_score (CDCOOPER, CDMODELO, DTBASE, TPPESSOA, NRCPFCNPJBASE, NRSCORE_ALINHADO, DSCLASSE_SCORE, DSEXCLUSAO_PRINCIPAL, FLVIGENTE, CDSCORE, OBDETALHAMENTO, CDSEGMENTO_MODELO)
 values (0, 4, to_date(SYSDATE, 'dd-mm-yyyy'), 1, 01668530, 638, ' ', null, 1, 4, '[{"cdscore1":"","cdscore2":"","cdscore3":616,"cdscore4":486,"cdscore5":"999","cdscore6":"","cdscore7":"","cdscore8":"","cdscore9":"","cdscore10":74}]', 4);
 
@@ -34,4 +34,5 @@ values (0, 4, to_date(SYSDATE, 'dd-mm-yyyy'), 1, 01668530, 638, ' ', null, 1, 4,
 EXCEPTION
 WHEN OTHERS THEN
   ROLLBACK;
+  RAISE_APPLICATION_ERROR(-20000, 'ERRO: ' || SQLERRM);
 END;
