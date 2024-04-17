984a0acd3a52c442053501affa40fdb52a4bd0f6
Jackson Rosá <jackson.rosa@ailos.coop.br>
2024-04-17 11:32:55 +0000

    Cria fase 115 para concluir TED.

---
+++ 2024/04/cria-fase-115-spb.sql
@@ -0,0 +1,22 @@
+BEGIN
+  INSERT INTO cecred.tbspb_msg_recebida_fase
+    (nrseq_mensagem,
+     cdfase,
+     nmmensagem,
+     dhmensagem,
+     insituacao,
+     nrseq_mensagem_xml,
+     dhdthr_bc,
+     flintegra)
+  VALUES
+    (240238011543,
+     115,
+     'STR0004R2',
+     to_date('14-02-2024 17:14:30', 'dd-mm-yyyy hh24:mi:ss'),
+     'OK',
+     24020074920235,
+     to_date('14-02-2024 17:14:30', 'dd-mm-yyyy hh24:mi:ss'),
+     1);
+     
+  COMMIT;
+END;
