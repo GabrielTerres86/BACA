e79fc704e6bc6f2e1eeac9f6405d28f31d723279
Odirlei Busana <odirlei.busana@ailos.coop.br>
2024-04-19 12:05:46 +0000

    Updated DML_INC0327905_risco_carga.sql

--- 2024/04/DML_INC0327905_risco_carga.sql
+++ 2024/04/DML_INC0327905_risco_carga.sql
@@ -5,5 +5,11 @@ BEGIN
  WHERE t.CDSTATUS = 6;
  
  COMMIT;
+
+   UPDATE gestaoderisco.tbrisco_Central_Carga t
+   SET t.CDSTATUS = 4
+ WHERE t.CDSTATUS = 2
+   and t.cdcooper = 3;
  
+ COMMIT; 
 END; 
