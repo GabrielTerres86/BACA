01f046b95414d00187d114f1be09e7b025e220dc
Pedro <pedro.morais@ailos.coop.br>
2024-04-22 10:30:57 -0300

    Ajustado owner

--- 2024/04/PRJ0025156_habilita_menu.sql
+++ 2024/04/PRJ0025156_habilita_menu.sql
@@ -1,7 +1,7 @@
 BEGIN
-    update menumobile set flavaliacao = 1 where menumobileid = 200 and menupaiid = 30;
-    update menumobile set flavaliacao = 1 where menumobileid = 700 and menupaiid = 701 and versaominimaapp = '2.0.0.0';
-    update menumobile set flavaliacao = 1 where menumobileid = 804 and menupaiid = 30;
-    update menumobile set flavaliacao = 1 where menumobileid = 1011 and menupaiid = 1009;
+    update cecred.menumobile set flavaliacao = 1 where menumobileid = 200 and menupaiid = 30;
+    update cecred.menumobile set flavaliacao = 1 where menumobileid = 700 and menupaiid = 701 and versaominimaapp = '2.0.0.0';
+    update cecred.menumobile set flavaliacao = 1 where menumobileid = 804 and menupaiid = 30;
+    update cecred.menumobile set flavaliacao = 1 where menumobileid = 1011 and menupaiid = 1009;
     COMMIT;
 END;
