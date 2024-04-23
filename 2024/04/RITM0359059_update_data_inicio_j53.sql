a9b4c35ac8ae6cb4cefe50cef47dc47df9321491
Darly Cesar Marcelino Agostinho <amcom-darly.agostinho@ailos.coop.br>
2024-04-23 10:20:22 -0300

    Atualizar data de inicio j53 2

--- 2024/04/RITM0359059_update_data_inicio_j53.sql
+++ 2024/04/RITM0359059_update_data_inicio_j53.sql
@@ -1,16 +1,10 @@
 BEGIN 
-  INSERT INTO CECRED.crapprm
-    (nmsistem
-    ,cdcooper
-    ,cdacesso
-    ,dstexprm
-    ,dsvlrprm)
-  VALUES 
-    ('CRED'
-    ,0
-    ,'DT_LIM_CNAB240_SEM_J53'
-    ,'Data limite para aceite de arquivos CNAB240 sem o J53.'
-    , TO_DATE('30/06/2024','DD/MM/RRRR'));
+  UPDATE CECRED.crapprm prm
+     SET prm.dstexprm = 'Data limite para aceite de arquivos CNAB240 sem o J53.'
+        ,prm.dsvlrprm = TO_DATE('30/06/2024', 'DD/MM/RRRR')
+   WHERE prm.nmsistem = 'CRED'
+     AND prm.cdcooper = 0
+     AND prm.cdacesso = 'DT_LIM_CNAB240_SEM_J53';
   COMMIT;
 EXCEPTION 
   WHEN OTHERS THEN 
