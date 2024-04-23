5bdac0de53587d8552b88ebfe43021d53a270dd2
Rafael Felipe de Oliveira <rafael.oliveira@ailos.coop.br>
2024-04-23 18:51:44 -0300

    Inclusao de novas mensagens de retorno - B3 Gravames

--- 2024/04/INC0324611-inclusao-retornos-b3.sql
+++ 2024/04/INC0324611-inclusao-retornos-b3.sql
@@ -1,13 +1,341 @@
 BEGIN
 
-  UPDATE cecred.crapcob c
-     SET c.incobran = 5
-   WHERE progress_recid in (131238846, 133954547, 139450642);
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 301, 'Endereço do credor não informado.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 302, 'Número do endereço do credor não informado');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 303, 'Bairro do endereço do credor não informado.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 305, 'Código do município do imóvel do credor não consta na tabela.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 306, 'UF do endereço do credor não informado');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 307, 'UF do endereço do credor inexistente.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 308, 'CEP do imóvel do credor não informado');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 309, 'DDD do telefone do credor inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 310, 'Número do telefone do credor inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 311, 'Endereço do financiado não informado.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 312, 'Número do endereço do financiado não informado.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 313, 'Bairro do endereço do financiado não informado.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 314, 'Código do município do imóvel do financiado não consta na tabela.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 315, 'Código do município do imóvel do financiado não consta na tabela.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 316, 'UF do endereço do financiado não informado.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 317, 'UF do imóvel do financiado inválida.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 318, 'CEP do imóvel do financiado não informado.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 319, 'DDD do telefone do financiado inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 320, 'Número do telefone do financiado inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 321, 'Multa não informada.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 322, 'Juros de mora não informado.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 324, 'Penalidade não informada ou inválida.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 326, 'Comissão não informada.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 330, 'CPF/CNPJ do principal recebedor do pagamento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 331, 'CPF/CNPJ do vendedor do bem inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 332, 'Complemento do endereço do credor com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 333, 'Complemento do endereço do financiado com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 334, 'Participante com pendência de registro de contrato. Contatar a UF de licenciamento');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 335, 'Financiado com pendência financeira. Contatar a UF de licenciamento');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 336, 'Cidade liberação da operação inválida');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 337, 'CEP do imóvel do financiado inválido');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 338, 'Número do imóvel do financiado não corresponde ao CEP informado');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 401, 'RENAVAM com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 402, 'Placa não informada.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 403, 'Placa deve ter 7 caracteres.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 404, 'UF da placa não informada.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 408, 'CNPJ do financiado deve ter 14 caracteres.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 409, 'CPF do financiado deve ter 11 caracteres.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 411, 'Nome do financiado com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 412, 'Nome do financiado tem mais de um espaço em branco entre as palavras.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 416, 'Ano de fabricação não pode ser maior que ano atual.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 419, 'Ano do modelo deve ser maior ou igual ao ano de fabricação.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 420, 'Ano do modelo não deve ser superior a 1 ano da fabricação');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 421, 'Quantidade de meses com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 423, 'Número do contrato com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 426, 'Data do contrato inválida');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 427, 'Data do contrato não pode ser maior que 2093');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 428, 'Grupo consórcio com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 441, 'Data vencimento da primeira parcela não pode ser maior que 2093');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 442, 'Data vencimento da última parcela não deve ser anterior à data atual.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 444, 'Data vencimento da última parcela não pode ser maior que 2093');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 452, 'Endereço do credor com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 453, 'Endereço do credor tem mais de um espaço em branco entre as palavras.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 455, 'Número do endereço do credor com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 457, 'Bairro do endereço do credor tem mais de um espaço em branco entre as palavras');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 458, 'Bairro do endereço do credor com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 468, 'Endereço do financiado com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 469, 'Endereço do financiado tem mais de um espaço em branco entre as palavras.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 471, 'Número do endereço do financiado com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 473, 'Bairro do endereço do financiado tem mais de um espaço em branco entre as palavras.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 474, 'Bairro do endereço do financiado com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 483, 'Multa com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 485, 'Juros de mora com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 488 , 'Descrição da penalidade tem mais de um espaço em branco entre as palavras.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 490, 'Comissão com preenchimento inválido');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 492, 'Número do documento (CPF) do recebedor deve ter 11 caracteres');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 493, 'Número do documento (CNPJ) do recebedor deve ter 14 caracteres');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 496, 'Complemento do endereço do credor tem mais de um espaço em branco entre as palavras.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 498, 'Complemento do endereço do financiado tem mais de um espaço em branco entre as palavras.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 508, 'CPF/CNPJ do credor com preenchimento inválido.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 514, 'Observação com preenchimento inválido. Existem caracteres inválidos.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 515, 'Observação tem mais de um espaço em branco entre as palavras.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 520, 'Nome do credor não informado');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'I', 4, 999, 'Erro de sintaxe no contrato - Consulte B3/CETIP');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'C', 3, 55, 'CPF/CNPJ do financiado divergente da inclusão da restrição financeira.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'C', 3, 999, 'Erro de sintaxe no contrato - Consulte B3/CETIP');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'Q', 3, 85, 'Não foi possível realizar a baixa, veículo com restrição.');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'Q', 3, 999, 'Erro de sintaxe no contrato - Consulte B3/CETIP');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'O', 1, 35, 'Não existe resultado para consulta realizada');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'O', 1, 40, 'Chassi inválido');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'O', 1, 41, 'Placa inválida');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'O', 1, 42, 'Chassi e placa não informados');
+  
+  INSERT INTO CECRED.CRAPRTO
+  (CDPRODUT, CDOPERAC, NRTABELA, CDRETORN, DSRETORN)
+  VALUES(1, 'O', 1, 999, 'Erro de sintaxe no contrato - Consulte B3/CETIP');
 
   COMMIT;
 	
-EXCEPTION
-  WHEN OTHERS THEN
-    ROLLBACK;
-   	dbms_output.put_line('Erro ao executar baca de atualizacao incobran na tabela crapcob: ' || SQLERRM);
+  EXCEPTION
+    WHEN OTHERS THEN
+      ROLLBACK;
+   	  dbms_output.put_line('Erro ao inserir mensagens B3 Gravam: ' || SQLERRM);
 END;
