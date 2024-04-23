da4a74ba63728df5aa6be449e7162a99a2ac99ab
Rafael Felipe de Oliveira <rafael.oliveira@ailos.coop.br>
2024-04-23 11:08:26 -0300

    Atualiza gravam para lote

--- 2024/04/update-gravam-cancel-lote.sql
+++ 2024/04/update-gravam-cancel-lote.sql
@@ -1,10 +1,13 @@
 BEGIN
 
 UPDATE	CECRED.crapbpr
-SET	tpcancel = 'A'
+SET	flcancel = 1,
+	tpcancel = 'A',
+	flgbaixa = 0,
+	tpdbaixa = null
 WHERE	CDCOOPER = 9
 AND	NRDCONTA = 82938261
-AND	NRCTRPRO = 95611;
+AND	NRCTRPRO = 95610;
 
 COMMIT;
 
