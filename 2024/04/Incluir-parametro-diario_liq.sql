14805a254d136b3c04559daf4a83c8583fdabd1a
Renato Darosci <premiersoft-renato.darosci@ailos.coop.br>
2024-04-30 07:46:41 -0300

    Incluir controle diário liquidacao

--- 2024/04/Incluir-parametro-diario_liq.sql
+++ 2024/04/Incluir-parametro-diario_liq.sql
@@ -2,7 +2,7 @@ BEGIN
   INSERT INTO COBRANCA.tbcobran_controle_diario_liquidacao(DTREFERENCIA
                                                           ,TPMARCO
                                                           ,DSOBSERVACAO)
-                                                   VALUES (to_date('17/04/2024','dd/mm/yyyy')
+                                                   VALUES (to_date('29/04/2024','dd/mm/yyyy')
                                                           ,1
                                                           ,'TESTES MLC');
   COMMIT;
