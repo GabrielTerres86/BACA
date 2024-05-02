0fc50c9f57e13814ac89aab385c66d729cc96b10
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2024-05-02 16:59:13 -0300

    Projeto PJ - Atualização Ambiente TEST

--- 2024/05/CRIA_SEGURO_QUESTIONARIO_RISCO_PJ.sql
+++
@@ -1,8 +0,0 @@
-BEGIN 
-
-UPDATE CECRED.crapaca a
-   SET a.lstparam = 'pr_cdcooper,pr_nrdconta,pr_nrctremp,pr_nmrepresentante,pr_dscargo,pr_nrcpfcgcrep,pr_nmsocio,pr_nrcpfcgcsoc,pr_pesocio,pr_dtadmsoc,pr_dtnascsoc'
- WHERE a.nmdeacao = 'CRIA_SEGURO_PRESTAMISTA_PJ';
- 
-COMMIT;
-END;
