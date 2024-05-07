98540cc765d7e8e3f484a8c67592d857e7452259
Odirlei Busana <odirlei.busana@ailos.coop.br>
2024-05-07 16:48:47 +0000

    Updated DML_INC0330225_limpar_scr.sql

--- 2024/04/DML_INC0330225_limpar_scr.sql
+++ 2024/04/DML_INC0330225_limpar_scr.sql
@@ -2,10 +2,10 @@ BEGIN
 
   DELETE cecred.tbbi_opf_header x
    WHERE x.DTBASE = to_date('31/03/2024', 'DD/MM/RRRR');
-
+  commit;
   DELETE cecred.crapopf x
    WHERE x.DTREFERE = to_date('31/03/2024', 'DD/MM/RRRR');
-
+  commit;
   DELETE cecred.crapvop x
    WHERE x.DTREFERE = to_date('31/03/2024', 'DD/MM/RRRR');
 
