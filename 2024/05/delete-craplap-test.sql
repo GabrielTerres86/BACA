d0ec8ad3312334914d72c759c6e3ae01cfee8be2
Douglas <douglas.kloppel@ailos.coop.br>
2024-05-13 15:31:39 -0300

    corrigindo a data de exclusão dos registros

--- 2024/05/delete-craplap-test.sql
+++ 2024/05/delete-craplap-test.sql
@@ -3,7 +3,7 @@ BEGIN
  where cdcooper = 1
    and ((nrdconta = 97933740 AND nraplica =  167087) OR
         (nrdconta = 97976750 AND nraplica in (244856, 188035)))
-   and dtmvtolt = '24/04/2024';
+   and dtmvtolt = to_date('24/04/2024','dd/mm/yyyy';
    
   COMMIT;
 END;     
