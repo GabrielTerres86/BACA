52a0e6c3660c5ff48d009a272f2a6bd0a1c00c10
Renato Darosci <premiersoft-renato.darosci@ailos.coop.br>
2024-05-14 07:49:41 -0300

    Incluir controle diário liquidacao

--- 2024/05/incluir_controle_liquidacao_mlc.sql
+++ 2024/05/incluir_controle_liquidacao_mlc.sql
@@ -1,6 +1,6 @@
 BEGIN
   INSERT INTO tbcobran_controle_diario_liquidacao(dtreferencia,tpmarco,dsobservacao)
-                       VALUES(to_date('10/05/2024','dd/mm/yyyy')
+                       VALUES(to_date('13/05/2024','dd/mm/yyyy')
                              ,1
                              ,'TESTES MLC');
   COMMIT;
