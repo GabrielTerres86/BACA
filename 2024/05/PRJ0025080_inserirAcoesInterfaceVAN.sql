4d5d41b1d6239cd13bd228d29165deb9a58b72b2
“[Rafael <[rafael.cechet@ailos.coop.br]>
2024-05-20 11:41:01 -0300

    Ajusta insert na crapaca usando max + 1

--- 2024/05/PRJ0025080_inserirAcoesInterfaceVAN.sql
+++ 2024/05/PRJ0025080_inserirAcoesInterfaceVAN.sql
@@ -1,5 +1,6 @@
 declare
   vr_nrseqrdr cecred.crapaca.nrseqrdr%type;
+  vr_nrseqaca cecred.crapaca.nrseqaca%TYPE;
 begin
   select max(cecred.craprdr.nrseqrdr) + 1
     into vr_nrseqrdr 
@@ -10,32 +11,47 @@ begin
   values
     (vr_nrseqrdr, 'VAN', sysdate);
 
+  select max(cecred.crapaca.nrseqaca) + 1
+    into vr_nrseqaca 
+    from cecred.crapaca;
+
   insert into cecred.crapaca
-    (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
+    ( nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr, nrseqaca)
   values
     ('INSERIRADESAOVANXML',
      null,
      'pagamento.inserirAdesaoVanXml',
      'pr_cdcooperativa,pr_nrconta_corrente,pr_cdproduto',
-     vr_nrseqrdr);
+     vr_nrseqrdr,
+     vr_nrseqaca);
+
+  select max(cecred.crapaca.nrseqaca) + 1
+    into vr_nrseqaca 
+    from cecred.crapaca;
 
   insert into cecred.crapaca
-    (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
+    (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr, nrseqaca)
   values
     ('CANCELARADESAOVANXML',
      null,
      'pagamento.cancelarAdesaoVanXml',
      'pr_cdcooperativa,pr_nrconta_corrente,pr_cdproduto',
-     vr_nrseqrdr);
+     vr_nrseqrdr,
+     vr_nrseqaca);
+
+  select max(cecred.crapaca.nrseqaca) + 1
+    into vr_nrseqaca 
+    from cecred.crapaca;
      
   insert into cecred.crapaca
-    (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr)
+    (nmdeacao, nmpackag, nmproced, lstparam, nrseqrdr, nrseqaca)
   values
     ('VALIDARADESAOVANXML',
      null,
      'pagamento.validarAdesaoVanXml',
      'pr_cdcooperativa,pr_nrconta_corrente,pr_cdproduto',
-     vr_nrseqrdr);     
+     vr_nrseqrdr,
+     vr_nrseqaca);
 
   commit;
 exception
