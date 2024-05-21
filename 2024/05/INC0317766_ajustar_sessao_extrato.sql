7ec906e04bcc5e098ac2995ba4a2541c4798b3d0
Renato Darosci <premiersoft-renato.darosci@ailos.coop.br>
2024-05-21 10:52:34 -0300

    Ajustar script para incluir owner

--- 2024/05/INC0317766_ajustar_sessao_extrato.sql
+++ 2024/05/INC0317766_ajustar_sessao_extrato.sql
@@ -24,30 +24,30 @@ BEGIN
         raise_application_error(-20001,'Erro ao atualizar conta ('||reg.dsdrowid||'): '||SQLERRM);
     END;
     
-    INSERT INTO craplgm(cdcooper
-                       ,cdoperad
-                       ,dscritic
-                       ,dsorigem
-                       ,dstransa
-                       ,dttransa
-                       ,flgtrans
-                       ,hrtransa
-                       ,idseqttl
-                       ,nmdatela
-                       ,nrdconta)
-                 VALUES(reg.cdcooper
-                       ,'1'
-                       ,NULL
-                       ,'AIMARO'
-                       ,'Ajuste Secao para Extrato - Destino Extrato'
-                       ,TRUNC(SYSDATE)
-                       ,1
-                       ,CECRED.GENE0002.fn_busca_time
-                       ,0
-                       ,NULL
-                       ,reg.nrdconta)
-              RETURNING ROWID INTO vr_nrdrowid;
-              
+    INSERT INTO CECRED.craplgm(cdcooper
+                             ,cdoperad
+                             ,dscritic
+                             ,dsorigem
+                             ,dstransa
+                             ,dttransa
+                             ,flgtrans
+                             ,hrtransa
+                             ,idseqttl
+                             ,nmdatela
+                             ,nrdconta)
+                       VALUES(reg.cdcooper
+                             ,'1'
+                             ,NULL
+                             ,'AIMARO'
+                             ,'Ajuste Secao para Extrato - Destino Extrato'
+                             ,TRUNC(SYSDATE)
+                             ,1
+                             ,CECRED.GENE0002.fn_busca_time
+                             ,0
+                             ,NULL
+                             ,reg.nrdconta)
+                    RETURNING ROWID INTO vr_nrdrowid;
+                      
     CECRED.GENE0001.pc_gera_log_item(pr_nrdrowid => vr_nrdrowid
                                     ,pr_nmdcampo => 'crapass.cdsecext'
                                     ,pr_dsdadant => reg.cdsecext
