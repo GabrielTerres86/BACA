9dca94d3ff928d01e012c89a5945505006d6d16e
Pedro <pedro.morais@ailos.coop.br>
2024-05-23 14:59:52 -0300

    Ajuste da versão minima do APP

---
+++ 2024/05/INC0334326_versao_minima_app.sql
@@ -0,0 +1,5 @@
+BEGIN
+    UPDATE cecred.parametromobile SET valor = '2.61.1' WHERE parametromobileid = 25 AND nome = 'VersaoMinimaiOS';
+    UPDATE cecred.parametromobile SET valor = '2.61.1' WHERE parametromobileid = 26 AND nome = 'VersaoMinimaAndroid';
+    COMMIT;
+END;
