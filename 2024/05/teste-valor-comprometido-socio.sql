cba1abb408a4e05d3e26c070ff0f864ba5752eb1
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2024-05-23 15:03:19 -0300

    Teste valor comprometido

--- 2024/05/teste-valor-comprometido-socio.sql
+++ 2024/05/teste-valor-comprometido-socio.sql
@@ -13,9 +13,9 @@ and nrdconta = 99966140
 and nrctrseg = 147249;
 
 insert into crapseg(nrdconta,nrctrseg,dtinivig,dtfimvig,dtmvtolt,cdsitseg,tpseguro,cdcooper)
-values (99966140,147245,to_date('24/04/2024','dd/mm/yyyy'),to_date('10/05/2024','dd/mm/yyyy'),to_date('24/04/2024','dd/mm/yyyy'),1,4,5);
+values (99966140,147245,to_date('25/04/2024','dd/mm/yyyy'),to_date('25/05/2024','dd/mm/yyyy'),to_date('25/04/2024','dd/mm/yyyy'),1,4,5);
 insert into crapseg(nrdconta,nrctrseg,dtinivig,dtfimvig,dtmvtolt,cdsitseg,tpseguro,cdcooper)
-values (99966140,147249,to_date('24/04/2024','dd/mm/yyyy'),to_date('10/05/2024','dd/mm/yyyy'),to_date('24/04/2024','dd/mm/yyyy'),1,4,5);
+values (99966140,147249,to_date('26/04/2024','dd/mm/yyyy'),to_date('26/05/2024','dd/mm/yyyy'),to_date('26/04/2024','dd/mm/yyyy'),1,4,5);
 
 commit;
 end;
