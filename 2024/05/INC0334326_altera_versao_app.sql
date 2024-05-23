e85c29a12d72bfba3d16d35f83ac9810a3990991
Pedro <pedro.morais@ailos.coop.br>
2024-05-23 13:51:57 -0300

    Ajustado versão atual

--- 2024/05/INC0334326_altera_versao_app.sql
+++ 2024/05/INC0334326_altera_versao_app.sql
@@ -1,6 +1,4 @@
 BEGIN
     UPDATE cecred.parametromobile SET valor = '2.61.1' WHERE parametromobileid = 39 AND nome = 'VersaoAtualiOS';
-    UPDATE cecred.parametromobile SET valor = '2.61.1' WHERE parametromobileid = 25 AND nome = 'VersaoMinimaiOS';
-    UPDATE cecred.parametromobile SET valor = '2.61.1' WHERE parametromobileid = 26 AND nome = 'VersaoMinimaAndroid';
     COMMIT;
 END;
