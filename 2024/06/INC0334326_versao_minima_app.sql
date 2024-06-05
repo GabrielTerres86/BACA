f8ad7e434a9140a2ad29fb599b2a18101b885507
Mauricio Pfleger <mauricio.pfleger@ailos.coop.br>
2024-06-05 08:48:59 -0300

    iOS

--- 2024/06/INC0334326_versao_minima_app.sql
+++ 2024/06/INC0334326_versao_minima_app.sql
@@ -1,5 +1,5 @@
-BEGIN
+BEGIN    
+	UPDATE cecred.parametromobile SET valor = '2.61.3' WHERE parametromobileid = 39 AND nome = 'VersaoAtualiOS';
     UPDATE cecred.parametromobile SET valor = '2.61.3' WHERE parametromobileid = 25 AND nome = 'VersaoMinimaiOS';
-    UPDATE cecred.parametromobile SET valor = '2.61.3' WHERE parametromobileid = 26 AND nome = 'VersaoMinimaAndroid';
     COMMIT;
 END;
