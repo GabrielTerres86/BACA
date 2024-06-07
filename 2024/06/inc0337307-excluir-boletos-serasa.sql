8403a2bed885c07c06e2f82a3a723ffabdb1ccfe
Rodrigo Schaefer Erasmo de Lima <amcom-rodrigo.lima@ailos.coop.br>
2024-06-07 14:05:15 -0300

    Alteração no script para incluir rotinas

--- 2024/06/inc0337307-excluir-boletos-serasa.sql
+++ 2024/06/inc0337307-excluir-boletos-serasa.sql
@@ -1,6 +1,7 @@
 DECLARE
 
-  PROCEDURE atualizarExclusaoBoleto(pr_rowid IN ROWID) IS
+  PROCEDURE atualizarExclusaoBoleto(pr_rowid    IN ROWID
+                                   ,pr_dtmvtolt IN crapdat.dtmvtolt%TYPE) IS
   
     CURSOR cr_crapcob IS
       SELECT a.inserasa
@@ -26,14 +27,71 @@ DECLARE
          AND b.nrconven = a.nrcnvcob;
     rw_crapcob cr_crapcob%ROWTYPE;
   
-    vr_intipret PLS_INTEGER;
-    vr_inserasa crapcob.inserasa%TYPE;
-    vr_dtretser DATE; 
-    vr_dslog    VARCHAR2(500);
-    vr_cdcritic NUMBER := 0;
-    vr_dscritic VARCHAR2(4000);
-    vr_des_erro VARCHAR2(10);
-    vr_tab_lcm  PAGA0001.typ_tab_lcm_consolidada;
+    CURSOR cr_dtlipgto(pr_rowid    ROWID
+                      ,pr_dtmvtolt crapdat.dtmvtolt%TYPE) IS
+      SELECT cob.dtvencto
+            ,cob.vldescto
+            ,cob.vlabatim
+            ,cob.flgdprot
+        FROM crapcob cob
+       WHERE cob.dtlipgto < pr_dtmvtolt
+         AND cob.incobran = 0
+         AND cob.rowid = pr_rowid;
+    rw_dtlipgto cr_dtlipgto%ROWTYPE;
+  
+    CURSOR cr_ultimaSituacaoSerasa(pr_cdcooper crapcob.cdcooper%TYPE
+                                  ,pr_nrdconta crapcob.nrdconta%TYPE
+                                  ,pr_cdbandoc crapcob.cdbandoc%TYPE
+                                  ,pr_nrdctabb crapcob.nrdctabb%TYPE
+                                  ,pr_nrcnvcob crapcob.nrcnvcob%TYPE
+                                  ,pr_nrdocmto crapcob.nrdocmto%TYPE) IS
+      WITH UltimoSequencial AS
+       (SELECT NVL(MAX(nrsequencia), 0) nrsequencia
+          FROM CECRED.tbcobran_his_neg_serasa
+         WHERE cdcooper = pr_cdcooper
+           AND cdbandoc = pr_cdbandoc
+           AND nrdctabb = pr_nrdctabb
+           AND nrcnvcob = pr_nrcnvcob
+           AND nrdconta = pr_nrdconta
+           AND nrdocmto = pr_nrdocmto),
+      UltimaSituacao AS
+       (SELECT inserasa
+          FROM CECRED.tbcobran_his_neg_serasa
+         INNER JOIN UltimoSequencial
+            ON tbcobran_his_neg_serasa.nrsequencia = UltimoSequencial.nrsequencia
+         WHERE tbcobran_his_neg_serasa.cdcooper = pr_cdcooper
+           AND tbcobran_his_neg_serasa.cdbandoc = pr_cdbandoc
+           AND tbcobran_his_neg_serasa.nrdctabb = pr_nrdctabb
+           AND tbcobran_his_neg_serasa.nrcnvcob = pr_nrcnvcob
+           AND tbcobran_his_neg_serasa.nrdconta = pr_nrdconta
+           AND tbcobran_his_neg_serasa.nrdocmto = pr_nrdocmto)
+      SELECT inserasa FROM UltimaSituacao;
+    rw_ultimaSituacaoSerasa cr_ultimaSituacaoSerasa%ROWTYPE;
+  
+    CURSOR cr_dtvencto(pr_rowid ROWID) IS
+      SELECT cob.dtvencto
+            ,cob.vldescto
+            ,cob.vlabatim
+            ,cob.flgdprot
+        FROM crapcob cob
+       WHERE cob.rowid = pr_rowid
+         AND cob.incobran = 0;
+    rw_dtvencto cr_dtvencto%ROWTYPE;
+  
+    vr_intipret            PLS_INTEGER;
+    vr_inserasa            crapcob.inserasa%TYPE;
+    vr_dtretser            DATE;
+    vr_dslog               VARCHAR2(500);
+    vr_cdcritic            NUMBER := 0;
+    vr_dscritic            VARCHAR2(4000);
+    vr_des_erro            VARCHAR2(10);
+    vr_tab_lcm             PAGA0001.typ_tab_lcm_consolidada;
+    vr_tab_lat_consolidada PAGA0001.typ_tab_lat_consolidada;
+  
+    vr_tab_remessa_dda DDDA0001.typ_tab_remessa_dda;
+    vr_tab_retorno_dda DDDA0001.typ_tab_retorno_dda;
+  
+    vr_exc_saida EXCEPTION;
   
   BEGIN
   
@@ -55,6 +113,128 @@ DECLARE
          SET flserasa = 0
             ,qtdianeg = 0
        WHERE ROWID = pr_rowid;
+    END IF;
+  
+    IF (NOT (vr_intipret = 1)) OR vr_inserasa = 6 THEN
+      BEGIN
+        UPDATE crapcob
+           SET inserasa = vr_inserasa
+              ,dtretser = vr_dtretser
+         WHERE ROWID = pr_rowid;
+      
+        OPEN cr_ultimaSituacaoSerasa(pr_cdcooper => rw_crapcob.cdcooper
+                                    ,pr_nrdconta => rw_crapcob.nrdconta
+                                    ,pr_cdbandoc => rw_crapcob.cdbandoc
+                                    ,pr_nrdctabb => rw_crapcob.nrdctabb
+                                    ,pr_nrcnvcob => rw_crapcob.nrcnvcob
+                                    ,pr_nrdocmto => rw_crapcob.nrdocmto);
+        FETCH cr_ultimaSituacaoSerasa
+          INTO rw_ultimaSituacaoSerasa;
+        CLOSE cr_ultimaSituacaoSerasa;
+      
+        IF vr_inserasa IN (0, 6) AND NVL(rw_ultimaSituacaoSerasa.inserasa, -1) NOT IN (0, 6) THEN
+          BEGIN
+          
+            UPDATE crapcob SET dtlipgto = ADD_MONTHS(dtlipgto, -60) WHERE ROWID = pr_rowid;
+          
+            OPEN cr_dtlipgto(pr_dtmvtolt => pr_dtmvtolt, pr_rowid => pr_rowid);
+            FETCH cr_dtlipgto
+              INTO rw_dtlipgto;
+          
+            IF cr_dtlipgto%FOUND THEN
+              COBR0007.pc_inst_pedido_baixa_decurso(pr_cdcooper            => rw_crapcob.cdcooper
+                                                   ,pr_nrdconta            => rw_crapcob.nrdconta
+                                                   ,pr_nrcnvcob            => rw_crapcob.nrcnvcob
+                                                   ,pr_nrdocmto            => rw_crapcob.nrdocmto
+                                                   ,pr_cdocorre            => 2
+                                                   ,pr_dtmvtolt            => pr_dtmvtolt
+                                                   ,pr_cdoperad            => '1'
+                                                   ,pr_nrremass            => 0
+                                                   ,pr_tab_lat_consolidada => vr_tab_lat_consolidada
+                                                   ,pr_cdcritic            => vr_cdcritic
+                                                   ,pr_dscritic            => vr_dscritic);
+              vr_cdcritic := NULL;
+              vr_dscritic := NULL;
+              PAGA0001.pc_efetua_lancto_tarifas_lat(pr_cdcooper            => rw_crapcob.cdcooper
+                                                   ,pr_dtmvtolt            => pr_dtmvtolt
+                                                   ,pr_tab_lat_consolidada => vr_tab_lat_consolidada
+                                                   ,pr_cdcritic            => vr_cdcritic
+                                                   ,pr_dscritic            => vr_dscritic);
+              vr_cdcritic := NULL;
+              vr_dscritic := NULL;
+            
+            ELSE
+              OPEN cr_dtvencto(pr_rowid => pr_rowid);
+              FETCH cr_dtvencto
+                INTO rw_dtvencto;
+              IF cr_dtvencto%FOUND THEN
+                DDDA0001.pc_procedimentos_dda_jd(pr_rowid_cob       => pr_rowid
+                                                ,pr_tpoperad        => 'A'
+                                                ,pr_tpdbaixa        => ' '
+                                                ,pr_dtvencto        => rw_dtvencto.dtvencto
+                                                ,pr_vldescto        => rw_dtvencto.vldescto
+                                                ,pr_vlabatim        => rw_dtvencto.vlabatim
+                                                ,pr_flgdprot        => rw_dtvencto.flgdprot
+                                                ,pr_tab_remessa_dda => vr_tab_remessa_dda
+                                                ,pr_tab_retorno_dda => vr_tab_retorno_dda
+                                                ,pr_cdcritic        => vr_cdcritic
+                                                ,pr_dscritic        => vr_dscritic);
+                vr_cdcritic := NULL;
+                vr_dscritic := NULL;
+              END IF;
+              CLOSE cr_dtvencto;
+            END IF;
+            CLOSE cr_dtlipgto;
+          
+          EXCEPTION
+            WHEN OTHERS THEN
+              pc_internal_exception(PR_COMPLEME => '2 - instrucao dtlipgto serasa - crapcob.rowid = ' ||
+                                                   pr_rowid);
+          END;
+        END IF;
+      EXCEPTION
+        WHEN OTHERS THEN
+          vr_dscritic := 'Erro ao alterar CRAPCOB: ' || SQLERRM;
+          RAISE vr_exc_saida;
+      END;
+    
+      BEGIN
+        INSERT INTO tbcobran_his_neg_serasa
+          (cdcooper
+          ,cdbandoc
+          ,nrdctabb
+          ,nrcnvcob
+          ,nrdconta
+          ,nrdocmto
+          ,nrsequencia
+          ,dhhistorico
+          ,inserasa
+          ,dtretser
+          ,cdoperad)
+        VALUES
+          (rw_crapcob.cdcooper
+          ,rw_crapcob.cdbandoc
+          ,rw_crapcob.nrdctabb
+          ,rw_crapcob.nrcnvcob
+          ,rw_crapcob.nrdconta
+          ,rw_crapcob.nrdocmto
+          ,(SELECT nvl(MAX(nrsequencia), 0) + 1
+             FROM tbcobran_his_neg_serasa
+            WHERE cdcooper = rw_crapcob.cdcooper
+              AND cdbandoc = rw_crapcob.cdbandoc
+              AND nrdctabb = rw_crapcob.nrdctabb
+              AND nrcnvcob = rw_crapcob.nrcnvcob
+              AND nrdconta = rw_crapcob.nrdconta
+              AND nrdocmto = rw_crapcob.nrdocmto)
+          ,SYSDATE
+          ,vr_inserasa
+          ,vr_dtretser
+          ,'1');
+      EXCEPTION
+        WHEN OTHERS THEN
+          vr_dscritic := 'Erro ao inserir na tbcobran_his_neg_serasa: ' || SQLERRM;
+          RAISE vr_exc_saida;
+      END;
     END IF;
   
     paga0001.pc_cria_log_cobranca(pr_idtabcob => pr_rowid
@@ -134,8 +314,14 @@ DECLARE
 BEGIN
 
   DECLARE
+    rw_crapdat             btch0001.cr_crapdat%ROWTYPE;
     vr_qtboleto_processado NUMBER := 0;
   BEGIN
+  
+    OPEN btch0001.cr_crapdat(3);
+    FETCH btch0001.cr_crapdat
+      INTO rw_crapdat;
+    CLOSE btch0001.cr_crapdat;
   
     FOR rw_boleto IN (SELECT cob.rowid idrowid_crapcob
                         FROM cecred.crapcob cob
@@ -146,8 +332,9 @@ BEGIN
                              ,(1, 85, 101002, 101002, 11564377, 197)
                              ,(1, 85, 101002, 101002, 11564377, 211)
                              ,(1, 85, 101002, 101002, 11564377, 213))) LOOP
-
-      atualizarExclusaoBoleto(pr_rowid => rw_boleto.idrowid_crapcob);
+    
+      atualizarExclusaoBoleto(pr_rowid    => rw_boleto.idrowid_crapcob
+                             ,pr_dtmvtolt => rw_crapdat.dtmvtolt);
     
       vr_qtboleto_processado := vr_qtboleto_processado + 1;
     
