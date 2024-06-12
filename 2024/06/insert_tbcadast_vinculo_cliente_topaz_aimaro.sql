f8747c75a7af794631e0ed8a85141a7a1461d8b0
Carlos Henrique Weinhold <carlos.weinhold@ailos.coop.br>
2024-06-12 17:14:31 -0300

    Inserindo registro para teste de extração de dados aimaro.

---
+++ 2024/06/insert_tbcadast_vinculo_cliente_topaz_aimaro.sql
@@ -0,0 +1,7 @@
+BEGIN
+  
+  INSERT INTO cadastro.TBCADAST_VINCULO_CLIENTE_TOPAZ_AIMARO (idcooperativa, nrconta_corrente, nrclientetopaz, dhregistro)
+  VALUES (1,99996790,1,SYSDATE);
+  
+  COMMIT;
+END;
