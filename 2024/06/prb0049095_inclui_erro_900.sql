c763a425211f4ccd2183081b831d12ad4f0322da
Ademir Jose Fink <ademir.fink@ailos.coop.br>
2024-06-14 14:01:21 -0300

    Prepara cenario de testes do reprocessamento do serasa no prb0049095

---
+++ 2024/06/prb0049095_inclui_erro_900.sql
@@ -0,0 +1,18 @@
+BEGIN
+  INSERT INTO cecred.tbcobran_erros_serasa
+    (cderro_serasa
+    ,dserro_serasa
+    ,cdocorre
+    ,cdmotivo)
+  VALUES
+    (900
+    ,'CALAMIDADE PUBLICA'
+    ,92
+    ,'16');
+  COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    sistema.excecaointerna(pr_cdcooper => 3, pr_compleme => 'PRB0049095 - US889190');
+    RAISE;
+END;
