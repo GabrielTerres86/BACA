edb67318d7a773224586ae4ef870a1bb5d6f98c9
Jefferson Luiz da Silva <jefferson.luiz@ailos.coop.br>
2024-06-17 13:00:34 +0000

    Updated calris_alterar_data.sql

--- 2024/06/calris_alterar_data.sql
+++ 2024/06/calris_alterar_data.sql
@@ -3,7 +3,7 @@ BEGIN
 UPDATE cecred.tbcalris_colaboradores a
    SET a.dhalteracao = sysdate -1
     , a.dhrecebimento = sysdate - 1
- WHERE trunc(a.dhrecebimento) = to_date('12/06/2024', 'dd/mm/yyyy');
+ WHERE trunc(a.dhrecebimento) = to_date('13/06/2024', 'dd/mm/yyyy');
 
 COMMIT;
 
