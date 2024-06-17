3cd0f6184ee4c5cf18c18b3779cb0db66b4218aa
Leandro Muniz <leandro.muniz@ailos.coop.br>
2024-06-17 10:51:40 -0300

    Ajuste Script

--- 2024/06/ajusta-inc0339180.sql
+++ 2024/06/ajusta-inc0339180.sql
@@ -3,7 +3,7 @@ BEGIN
 update cecred.tb_consumo_barramento set inem_processamento = null
 where nrseq_consumo = '240600134813'
 and nrcontrole_if = '1240617010000152866A'
-and inem_processamento = 'P'
+and inem_processamento = 'P';
 
 COMMIT;
 END;
