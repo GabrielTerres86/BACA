eed717cc58c8628e37493e1c2d2554176198457d
Paulo Felipe Kowalsky <t0035805@cecred.coop.br>
2024-06-18 14:40:47 -0300

    update craplau coop 2

--- 2024/06/update-craplau-coop-2.sql
+++ 2024/06/update-craplau-coop-2.sql
@@ -2,7 +2,7 @@ BEGIN
   
 update cecred.craplau u
   set u.dtmvtopg = to_date('18/06/2024','DD/MM/YYYY')
-where  u.cdcooper = 2
+where  u.cdcooper in (5,9)
   and  u.cdhistor = 3826
   and  u.insitlau = 1
   and  u.dtmvtopg > sysdate;
