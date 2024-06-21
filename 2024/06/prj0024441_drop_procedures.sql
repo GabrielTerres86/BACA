736025d99f7f14117ad50af64d8415d96d3ec2c8
Rodrigo Schaefer Erasmo de Lima <amcom-rodrigo.lima@ailos.coop.br>
2024-06-21 11:07:00 -0300

    Adicionando os sinonimos

--- 2024/06/prj0024441_drop_procedures.sql
+++ 2024/06/prj0024441_drop_procedures.sql
@@ -5,7 +5,14 @@ BEGIN
   EXECUTE IMMEDIATE 'DROP PROCEDURE cecred.pc_crps538_faba3';
   EXECUTE IMMEDIATE 'DROP PROCEDURE cecred.pc_crps538_tiago';
   EXECUTE IMMEDIATE 'DROP PROCEDURE cecred.pc_crps538_versao';
-  commit;
+  drop public synonym PC_CRPS538_TIAGO;
+  drop public synonym PC_CRPS538_FABA2;
+  drop public synonym PC_CRPS538_CARLOS;
+  drop public synonym PC_CRPS538_FABA;
+  drop public synonym PC_CRPS538_VERSAO;
+  drop public synonym PC_CRPS538_FABA3;
+
+  COMMIT;
 EXCEPTION
   WHEN OTHERS THEN
     SISTEMA.EXCECAOINTERNA(pr_cdcooper => 3, pr_compleme => 'Drop procedures pc_crps538_*');
