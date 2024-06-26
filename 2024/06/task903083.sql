ff1169a7f5798f34d16fb93b84582aec6e13bef5
Douglas <douglas.kloppel@ailos.coop.br>
2024-06-26 09:42:53 -0300

    Atualizando pessoas ligadas da cooperativa Credifoz

---
+++ 2024/06/task903083.sql
@@ -0,0 +1,8 @@
+BEGIN
+  update crapprm 
+     set dsvlrprm = ',27261441015,95333843953,05633307917,05196722912,'
+   where cdacesso = 'PESSOA_LIGADA_CNSLH_FSCL'
+     and cdcooper = 11;
+     
+  COMMIT;
+END;
