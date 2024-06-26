953e2a67a241c0e081b1405739e7634f11c5cb8c
Douglas <douglas.kloppel@ailos.coop.br>
2024-06-26 09:49:15 -0300

    incluindo orwner

--- 2024/06/task903083.sql
+++ 2024/06/task903083.sql
@@ -1,5 +1,5 @@
 BEGIN
-  update crapprm 
+  update cecred.crapprm 
      set dsvlrprm = ',27261441015,95333843953,05633307917,05196722912,'
    where cdacesso = 'PESSOA_LIGADA_CNSLH_FSCL'
      and cdcooper = 11;
