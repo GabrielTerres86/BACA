d9f89efbdc0432dab183f1529d9b1cd143125c77
NicolasRochaS <nicolas.silva@guarida.com.br>
2024-06-27 14:04:51 -0300

    update para situacao conta corrente 4 ifrs9

---
+++ 2024/06/atualiza-conta-preju-ifrs9.sql
@@ -0,0 +1,12 @@
+BEGIN
+  UPDATE crapass a
+     SET a.cdsitdct = 4
+   WHERE a.cdcooper = 8
+     AND a.nrdconta = 81526733;
+     
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    RAISE_application_error(-20500, SQLERRM);    
+
+END;
