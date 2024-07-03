dee1e986f8f4616d95b9b34f6773d670d6f9740a
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2024-07-03 15:10:34 -0300

    Ajuste mensagem inclusão manual sócio pj

--- 2024/07/UPDATE_CALCULAR_SEGURO_PRESTAMISTA_PJ_FLGSEGMA.sql
+++ 2024/07/UPDATE_CALCULAR_SEGURO_PRESTAMISTA_PJ_FLGSEGMA.sql
@@ -1,7 +1,7 @@
 BEGIN 
 
 UPDATE CECRED.crapaca a
-   SET a.lstparam = 'pr_cdcooper,pr_nrdconta,pr_nrctremp,pr_nmsocio,pr_nrcpfcgcsoc,pr_dtnascsoc,pr_dtadmsoc,pr_pesocio,pr_flgsegma'
+   SET a.lstparam = 'pr_cdcooper,pr_nrdconta,pr_nrctremp,pr_nmsocio,pr_nrcpfcgcsoc,pr_dtnascsoc,pr_dtadmsoc,pr_pesocio'
  WHERE a.nmdeacao = 'CALCULAR_SEGURO_PRESTAMISTA_PJ';
  
 COMMIT;
