e81c6b059ba2c4f0e87b076fa6f984287a19b5f7
Renato Darosci <premiersoft-renato.darosci@ailos.coop.br>
2024-07-04 09:34:03 -0300

    Aplicar script para incluir controle de liquidação

--- 2024/05/incluir_controle_liquidacao_mlc.sql
+++ 2024/05/incluir_controle_liquidacao_mlc.sql
@@ -2,7 +2,7 @@ BEGIN
   INSERT INTO tbcobran_controle_diario_liquidacao(dtreferencia,
                                                   tpmarco,
                                                   dsobservacao)
-                                          VALUES (to_date('22/05/2024','dd/mm/yyyy')
+                                          VALUES (to_date('04/07/2024','dd/mm/yyyy')
                                                  ,1
                                                  ,'TESTES MLC');
                                                  
