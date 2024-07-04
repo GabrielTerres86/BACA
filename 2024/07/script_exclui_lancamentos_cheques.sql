5f3e1f12306a02a909140e2a1147f3586e7f8cf0
eltonfernando <elton@ailos.coop.br>
2024-07-04 17:40:19 -0300

    Exclui lcm

--- 2024/07/script_exclui_lancamentos_cheques.sql
+++ 2024/07/script_exclui_lancamentos_cheques.sql
@@ -1,6 +1,12 @@
 begin 
   
-delete cecred.craplcm where cdcooper = 12 and dtmvtolt = '24/06/2024' and nrdconta = 82921288 and cdhistor = 524 and progress_recid >= 2323338386
+delete cecred.craplcm
+ where cdcooper = 12
+   and dtmvtolt = to_date('24/06/2024','dd/mm/yyyy')
+   and nrdconta = 82921288
+   and cdhistor = 524
+   and progress_recid >= 2323338386
 
 commit;
+
 end;
