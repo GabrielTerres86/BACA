7d649d043c1c2d0ec1435f72fed19976fe591281
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2024-07-16 14:02:29 -0300

    Saldo a Liberar

--- 2023/12/valor_saldo_a_liberar.sql
+++ 2023/12/valor_saldo_a_liberar.sql
@@ -2,9 +2,9 @@ BEGIN
 
   UPDATE crapsda sda
      SET sda.vlsdbloq = 500000
-   WHERE sda.cdcooper = 9
-     AND sda.dtmvtolt = to_date('11/07/2024', 'dd/mm/yyyy')
-     AND sda.nrdconta IN (81613970, 99999595);
+   WHERE sda.cdcooper = 10
+     AND sda.dtmvtolt >= to_date('14/07/2024', 'dd/mm/yyyy')
+     AND sda.nrdconta = 81417799;
 
   COMMIT;
 
