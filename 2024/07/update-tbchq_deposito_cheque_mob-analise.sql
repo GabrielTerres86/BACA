5d7170d4fac66b8fff9eee193654febd7f6bea69
Paulo Felipe Kowalsky <t0035805@cecred.coop.br>
2024-07-17 14:41:41 -0300

    update cheques em analise

--- 2024/07/update-tbchq_deposito_cheque_mob-analise.sql
+++ 2024/07/update-tbchq_deposito_cheque_mob-analise.sql
@@ -2,7 +2,7 @@ begin
   
 update cecred.tbchq_deposito_cheque_mob m
   set  m.insituacao = 4
-where  m.dtdeposito < sysdate
+where  m.dtdeposito < trunc(sysdate)
   and  m.insituacao = 1;
 
 commit;
