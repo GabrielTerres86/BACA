e0fba41103f289211ed3188ceb23080ee6829dd0
Jose Diego Zunino <jose.zunino@ailos.coop.br>
2024-07-19 11:42:25 +0000

    Updated script_atualizacao_pagamentos_RFB.sql

--- 2024/07/script_atualizacao_pagamentos_RFB.sql
+++ 2024/07/script_atualizacao_pagamentos_RFB.sql
@@ -3,7 +3,7 @@ BEGIN
           SET reg.nmarquivo_inclusao_serpro       = 'K3244.K05438BA.B0850316.D240717.H000703',
               reg.dhinclusao_processamento_serpro = to_date('16/07/2024 23:00:01', 'DD/MM/YYYY HH245:mi:ss'), 
               reg.idremessa_serpro                = 316 
-        WHERE TRUNC(reg.dhinclusao_processamento)           = ('16/07/2024') 
+        WHERE TRUNC(reg.dhinclusao_processamento)           = to_Date('16/07/2024', 'DD/MM/YYYY')
    AND reg.cdempresa_documento IN ('G0270', 'D0432', 'D0328', 'D0385', 'D0153', 'D0100', 'D0064');
         
   UPDATE tbconv_remessa_pagfor rem
@@ -11,7 +11,7 @@ BEGIN
               rem.dhenvio_remessa_serpro   = to_date('16/07/2024 23:00:01', 'DD/MM/YYYY HH245:mi:ss'),
               rem.cdstatus_remessa_serpro  = 1,
               rem.idremessa_serpro         = 316
-        WHERE TRUNC(rem.DHENVIO_REMESSA)           = ('16/07/2024')
+        WHERE TRUNC(rem.DHENVIO_REMESSA)           = to_Date('16/07/2024', 'DD/MM/YYYY')
    AND rem.CDCONVENIO_PAGFOR = 'RFB.%';         
   COMMIT;
 END;
