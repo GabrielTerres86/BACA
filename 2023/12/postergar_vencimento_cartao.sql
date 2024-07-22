94610537c3c111c1cfad34df0cfc634ffc6f162f
Douglas Quisinski <douglas.quisinski@ailos.coop.br>
2024-07-22 14:20:50 -0300

    vencimento cartão

--- 2023/12/postergar_vencimento_cartao.sql
+++ 2023/12/postergar_vencimento_cartao.sql
@@ -9,8 +9,8 @@ BEGIN
     ,dtinclusao)
   VALUES
     (3
-    ,to_date('01-04-2024', 'dd-mm-yyyy')
-    ,to_date('31-01-2026', 'dd-mm-yyyy')
+    ,to_date('01-06-2016', 'dd-mm-yyyy')
+    ,to_date('31-01-2030', 'dd-mm-yyyy')
     ,to_date('02-01-2021', 'dd-mm-yyyy')
     ,'1'
     ,to_date('08-06-2021 16:24:37', 'dd-mm-yyyy hh24:mi:ss'));
