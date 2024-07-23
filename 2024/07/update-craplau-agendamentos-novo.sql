3e23762b96dca68684097ea0b4ced137edb50264
Paulo Felipe Kowalsky <t0035805@cecred.coop.br>
2024-07-23 14:37:06 -0300

    update agendamentos

---
+++ 2024/07/update-craplau-agendamentos-novo.sql
@@ -0,0 +1,15 @@
+begin
+  
+update cecred.craplau lau
+  set  lau.dtmvtopg = to_date('24/07/2024','dd/mm/yyyy')
+WHERE  lau.cdcooper = 11 
+   AND lau.dsorigem IN ('INTERNET', 'TAA')
+   AND lau.insitlau = 1 
+   AND lau.dtmvtopg = to_date('23/07/2024','dd/mm/yyyy')
+   AND lau.cdtiptra <> 4
+   and lau.cdhistor = 508
+   and substr(dscodbar,1,1) = '8'
+   and lau.nrdconta = 81333820;
+
+commit;
+end;
