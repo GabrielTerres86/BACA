31ca55ef552a73c8108d7ea1fe983131fde5bd11
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2024-07-29 09:37:16 -0300

    Remover proposta já cancelada em produção

--- 2024/07/Recusas_Mensal_Contributario.sql
+++ 2024/07/Recusas_Mensal_Contributario.sql
@@ -6,7 +6,7 @@ vr_dscritic VARCHAR(500);
 CURSOR cr_propostas IS
   SELECT *
     FROM cecred.tbseg_prestamista t
-   WHERE nrproposta IN ('202408692692','202408707257','202408580895','202314944167','202408583217','202403506139','202312952044','202408857589','202408874620','202308765289','202408538935','202408606486','202319741058','202408617533','202406497553','202406457660','202403874848','202407941461','202409015899','202408685291','202408853661','202408539271','202408246786','202408685364','202408434029','202408682970','202406803951','202408846874','202408748417','202408751119','202408461050','202408563434','202213671638','202408280362','202408558805','202406783120','202406652238','202408368870','202409023656','202305432498','202319744089','202401204033','202401505675','202408709355','202408438854','202408530546','202409016854','202408282028','202407894586','202408999656','202408618843','202406801100','202408845751','202408599849','202406225762','202307986359','202317679318','202408857143','202408616169','202408556812','202408729522','202315532365','202408748307','202408840457','202408750683','202408697138','202405587857','202407389336','202408605016','202408531168','202306705733','202408602966','202409121918','202409121719','202409025255');
+   WHERE nrproposta IN ('202408692692','202408707257','202408580895','202314944167','202408583217','202403506139','202312952044','202408857589','202408874620','202308765289','202408538935','202408606486','202319741058','202408617533','202406497553','202406457660','202403874848','202407941461','202409015899','202408685291','202408539271','202408246786','202408685364','202408434029','202408682970','202406803951','202408846874','202408748417','202408751119','202408461050','202408563434','202213671638','202408280362','202408558805','202406783120','202406652238','202408368870','202409023656','202305432498','202319744089','202401204033','202401505675','202408709355','202408438854','202408530546','202409016854','202408282028','202407894586','202408999656','202408618843','202406801100','202408845751','202408599849','202406225762','202307986359','202317679318','202408857143','202408616169','202408556812','202408729522','202315532365','202408748307','202408840457','202408697138','202405587857','202407389336','202408605016','202408531168','202306705733','202408602966','202409121918','202409121719','202409025255');
 
 BEGIN
 
@@ -46,7 +46,6 @@ UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tp
 UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tprecusa = 8 ,p.cdmotrec = 193 ,p.tpregist = 0 WHERE p.nrproposta = '202407941461';
 UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tprecusa = 8 ,p.cdmotrec = 193 ,p.tpregist = 0 WHERE p.nrproposta = '202409015899';
 UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tprecusa = 8 ,p.cdmotrec = 193 ,p.tpregist = 0 WHERE p.nrproposta = '202408685291';
-UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tprecusa = 8 ,p.cdmotrec = 75  ,p.tpregist = 0 WHERE p.nrproposta = '202408853661';
 UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tprecusa = 8 ,p.cdmotrec = 193 ,p.tpregist = 0 WHERE p.nrproposta = '202408539271';
 UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tprecusa = 8 ,p.cdmotrec = 193 ,p.tpregist = 0 WHERE p.nrproposta = '202408246786';
 UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tprecusa = 8 ,p.cdmotrec = 193 ,p.tpregist = 0 WHERE p.nrproposta = '202408685364';
@@ -90,7 +89,6 @@ UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tp
 UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tprecusa = 8 ,p.cdmotrec = 193 ,p.tpregist = 0 WHERE p.nrproposta = '202315532365';
 UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tprecusa = 8 ,p.cdmotrec = 193 ,p.tpregist = 0 WHERE p.nrproposta = '202408748307';
 UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tprecusa = 8 ,p.cdmotrec = 193 ,p.tpregist = 0 WHERE p.nrproposta = '202408840457';
-UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tprecusa = 8 ,p.cdmotrec = 75  ,p.tpregist = 0 WHERE p.nrproposta = '202408750683';
 UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tprecusa = 8 ,p.cdmotrec = 193 ,p.tpregist = 0 WHERE p.nrproposta = '202408697138';
 UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tprecusa = 8 ,p.cdmotrec = 193 ,p.tpregist = 0 WHERE p.nrproposta = '202405587857';
 UPDATE cecred.tbseg_prestamista p SET p.situacao = 0, p.dtrecusa = SYSDATE ,p.tprecusa = 8 ,p.cdmotrec = 193 ,p.tpregist = 0 WHERE p.nrproposta = '202407389336';
