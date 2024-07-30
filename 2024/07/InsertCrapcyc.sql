759d8318541bc71f0ad4c5a96e6c6d96db8652db
Rodrigo Jose Heiden <rodrigo.heiden@ailos.coop.br>
2024-07-30 13:24:20 -0300

    InsertCrapcyc

--- 2024/07/InsertCrapcyc.sql
+++ 2024/07/InsertCrapcyc.sql
@@ -1,39 +1,9 @@
 BEGIN
-  INSERT INTO cecred.crapcyc
-    (CDCOOPER
-    ,CDORIGEM
-    ,NRDCONTA
-    ,NRCTREMP
-    ,FLGJUDIC
-    ,FLEXTJUD
-    ,FLGEHVIP
-    ,CDOPERAD
-    ,DTENVCBR
-    ,DTINCLUS
-    ,CDOPEINC
-    ,DTALTERA
-    ,CDASSESS
-    ,CDMOTCIN
-    ,FLVIPANT
-    ,CDMOTANT
-    ,CDPROLEG)
-  VALUES
-    (14
-    ,1
-    ,99662957
-    ,336980
-    ,1
-    ,0
-    ,0
-    ,'1'
-    ,to_date('09-06-2022', 'dd-mm-yyyy')
-    ,to_date('23-05-2022', 'dd-mm-yyyy')
-    ,'cyber'
-    ,to_date('30-04-2024', 'dd-mm-yyyy')
-    ,2
-    ,0
-    ,0
-    ,0
-    ,888333);
+  UPDATE cecred.crapcyb a
+     SET a.nrdconta = 99662957
+   WHERE a.cdcooper = 14
+     AND a.nrdconta = 99662957
+     AND a.cdorigem = 1
+     AND a.nrctremp = 336980;
   COMMIT;
 END;
