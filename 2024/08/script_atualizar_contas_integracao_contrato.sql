5cfa5f315b064c3cb3ea5a6d8ac54d26c985ca05
CECRED\f0033505 <filipe.cardozo@ailos.coop.br>
2024-08-01 10:41:19 -0300

    Script para atualizar as contas correntes do ambiente de test

---
+++ 2024/08/script_atualizar_contas_integracao_contrato.sql
@@ -0,0 +1,12 @@
+﻿BEGIN
+  UPDATE credito.tbcred_integracao_contrato ctt
+     SET ctt.nrconta_corrente =
+         (SELECT nrdconta
+            FROM cecred.crapepr
+           WHERE nrctremp = ctt.nrcontrato
+             AND cdcooper = ctt.cdcooperativa
+             AND ROWNUM <= 1)
+   WHERE ctt.cdcooperativa = 1
+     AND ctt.idtipo_integracao = 7;
+  COMMIT;
+END;
