7903c628df7b2964fef6711964b2891804d5648b
CECRED\f0033505 <filipe.cardozo@ailos.coop.br>
2024-08-01 10:43:44 -0300

    ajuste para não vir resultado nulo

--- 2024/08/script_atualizar_contas_integracao_contrato.sql
+++ 2024/08/script_atualizar_contas_integracao_contrato.sql
@@ -1,11 +1,11 @@
 ﻿BEGIN
   UPDATE credito.tbcred_integracao_contrato ctt
      SET ctt.nrconta_corrente =
-         (SELECT nrdconta
+         NVL((SELECT nrdconta
             FROM cecred.crapepr
            WHERE nrctremp = ctt.nrcontrato
              AND cdcooper = ctt.cdcooperativa
-             AND ROWNUM <= 1)
+             AND ROWNUM <= 1),ctt.nrconta_corrente)
    WHERE ctt.cdcooperativa = 1
      AND ctt.idtipo_integracao = 7;
   COMMIT;
