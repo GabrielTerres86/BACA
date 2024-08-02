54cef59e7611019709a39d6a7c170d3fc6d7c243
eltonfernando <elton@ailos.coop.br>
2024-08-02 11:24:13 -0300

    incluido cecred

--- 2024/08/script_insere_craptab_devolu.sql
+++ 2024/08/script_insere_craptab_devolu.sql
@@ -1,5 +1,5 @@
 BEGIN
-  INSERT INTO craptab
+  INSERT INTO cecred.craptab
     (cdcooper, nmsistem, tptabela, cdempres, cdacesso, tpregist)
   values
     (12, 'CRED', 'GENERI', 0, 'BUSCADEVOLU', 17078652);
