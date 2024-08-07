4b75f477b36ed5047f773f85f38889a44dff47a5
Bruno Henrique Jacobsen de Andrade <t0036649@cecred.coop.br>
2024-08-07 14:46:37 -0300

    Cancelamento Seguro PJ

---
+++ 2024/08/US855446-opcao-cancelar-seguro-pj.sql
@@ -0,0 +1,9 @@
+begin
+  INSERT INTO CECRED.CRAPACA (NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
+  VALUES ('CANCELA_SEGURO_PRST_PJ', 'SEGU0004', 'pc_exclui_lcto_canc_seguro', 'pr_cdcooper,pr_nrdconta,pr_nrctrseg,pr_cdmotcan', 2564);
+  
+  COMMIT;
+end;                                                                           
+                                                                               
+                                                                               
+                                                                               
