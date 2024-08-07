2622ce81255dc94136961415129070fc32dea88b
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2024-08-07 14:17:50 -0300

    Ajuste carga Score

---
+++ 2024/08/RISCO_Carga_Score_agosto.sql
@@ -0,0 +1,16 @@
+DECLARE
+  vr_dscritic VARCHAR2(4000);
+  
+begin
+  
+
+  gestaoderisco.gerarCargaScore(pr_idscore => 1021,
+                                pr_dscritic => vr_dscritic);
+
+  IF TRIM(vr_dscritic) IS NOT NULL THEN
+    dbms_output.put_line('ERRO: ' || vr_dscritic);
+    ROLLBACK;
+  END IF;
+
+  COMMIT;
+end;
