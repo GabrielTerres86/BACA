b9d0834c4ca12a141d885b3ec66611ed66be9dab
Filipe Ribeiro Barbosa Rêgo <premiersoft-filipe.rego@ailos.coop.br>
2024-08-08 14:44:31 -0300

    Ajuste no script

--- 2024/08/seguro_prb049154.sql
+++ 2024/08/seguro_prb049154.sql
@@ -9,12 +9,6 @@ VALUES ('CRED',
        'AUTBUR_SCRIPT_SFTP_ICATU',
        'Script para envio e recebimento dos arquivos ICATU via SFTP',
        '/usr/local/bin/exec_comando_oracle.sh perl_remoto /usr/local/cecred/bin/sftp_envia_recebe_icatu.pl');
-
-
-  UPDATE cecred.tbseg_parametros_prst p
-     SET p.enderftp = 'producao.icatuseguros.com.br', 
-         p.loginftp = 'Producao-AilosTST',
-         p.senhaftp = 'TsA!xvnj@109'; 
   COMMIT;
 END;
 /
