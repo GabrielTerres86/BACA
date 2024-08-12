095eb59ffed31c7e5c100a86637c49d3ffe39a8a
Bruno Henrique Jacobsen de Andrade <t0036649@cecred.coop.br>
2024-08-12 15:30:06 -0300

    US855449-termo de cancelamento pj ajuste

--- 2024/08/insert_de_para_termo_cancelamento_pj.sql
+++ 2024/08/insert_de_para_termo_cancelamento_pj.sql
@@ -1,9 +1,15 @@
-begin
+DECLARE
+  vr_seq_hist cecred.tbseg_historico_relatorio.idhistorico_relatorio%TYPE;
+BEGIN
   INSERT INTO CECRED.CRAPACA (NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
   VALUES ('IMPRIME_TERMO_CANCELA_SEGURO_PRST_PJ', 'SEGU0004', 'pc_imp_termo_cancel_seg_pres', 'pr_cdcooper,pr_nrdconta,pr_nrctrseg', 2564);
   
-  INSERT INTO CECRED.TBSEG_HISTORICO_RELATORIO (CDACESSO, DSTEXPRM, DSVLRPRM, DTINICIO, FLTPCUSTEIO, TPPESSOA)
-  VALUES ('TERMO_CANCELAMENTO_PJ', 'Relatório Prestamista referente ao termo de cancelamento do seguro prestamista', 'termo_cancelamento_pj.jasper', to_date('01/08/2024','dd/mm/yyyy'), 0, 2);
+  SELECT nvl(MAX(t.idhistorico_relatorio),0)+1
+    INTO vr_seq_hist
+    FROM cecred.tbseg_historico_relatorio t;
+  
+  INSERT INTO CECRED.TBSEG_HISTORICO_RELATORIO (IDHISTORICO_RELATORIO, CDACESSO, DSTEXPRM, DSVLRPRM, DTINICIO, FLTPCUSTEIO, TPPESSOA)
+  VALUES (vr_seq_hist, 'TERMO_CANCELAMENTO_PJ', 'Relatório Prestamista referente ao termo de cancelamento do seguro prestamista', 'termo_cancelamento_pj.jasper', '01/08/2024', 0, 2);
   
   COMMIT;
-end;
+END;
