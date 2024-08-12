5f574094844b843dec63a80f3709d5490d03000e
Vinicius Bedin Knapp <premiersoft-vinicius.knapp@ailos.coop.br>
2024-08-12 15:59:58 -0300

    Ajuste de conveniada.

--- 2024/08/aca-atu-concilia.sql
+++ 2024/08/aca-atu-concilia.sql
@@ -2,7 +2,7 @@ BEGIN
 	UPDATE tbcadast_empresa_consig A
 	  SET A.cdlocal_conciliacao = 2
 	WHERE A.cdcooper = 14
-	  AND A.cdempres = 865;
+	  AND A.cdempres = 242;
 	COMMIT;
 EXCEPTION
 	WHEN OTHERS THEN
