cc110f09170858a3f28e259ed111b04cf874ddf8
Bruno Henrique Jacobsen de Andrade <t0036649@cecred.coop.br>
2024-08-12 15:09:06 -0300

    US855449-termo de cancelamento pj ajuste

--- 2024/08/insert_de_para_termo_cancelamento_pj.sql
+++ 2024/08/insert_de_para_termo_cancelamento_pj.sql
@@ -3,7 +3,7 @@ begin
   VALUES ('IMPRIME_TERMO_CANCELA_SEGURO_PRST_PJ', 'SEGU0004', 'pc_imp_termo_cancel_seg_pres', 'pr_cdcooper,pr_nrdconta,pr_nrctrseg', 2564);
   
   INSERT INTO CECRED.TBSEG_HISTORICO_RELATORIO (CDACESSO, DSTEXPRM, DSVLRPRM, DTINICIO, FLTPCUSTEIO, TPPESSOA)
-  VALUES ('TERMO_CANCELAMENTO_PJ', 'Relatório Prestamista referente ao termo de cancelamento do seguro prestamista', 'termo_cancelamento_pj.jasper', '01/08/2024', 0, 2);
+  VALUES ('TERMO_CANCELAMENTO_PJ', 'Relatório Prestamista referente ao termo de cancelamento do seguro prestamista', 'termo_cancelamento_pj.jasper', to_date('01/08/2024','dd/mm/yyyy'), 0, 2);
   
   COMMIT;
 end;
