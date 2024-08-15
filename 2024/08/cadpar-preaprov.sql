b5fe1f655664636c7e97196bb2ee104ac10f73fc
Sabrinne de Souza Cunha Lopes <sabrinne.lopes@ailos.coop.br>
2024-08-15 17:02:16 +0000

    Updated cadpar-preaprov.sql

--- 2024/08/cadpar-preaprov.sql
+++ 2024/08/cadpar-preaprov.sql
@@ -2,33 +2,33 @@ BEGIN
   
   insert into cecred.crappat (CDPARTAR, NMPARTAR, TPDEDADO, CDPRODUT)
   values (323, 'Pre-Aprovado fluxo IFRS9', 1, 0);  
-  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  insert into cecred.crappco (CDPARTAR, CDCOOPER, DSCONTEU)
   values (323, 7, '0');
-  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  insert into cecred.crappco (CDPARTAR, CDCOOPER, DSCONTEU)
   values (323, 16, '0');
-  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  insert into cecred.crappco (CDPARTAR, CDCOOPER, DSCONTEU)
   values (323, 12, '0');
-  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  insert into cecred.crappco (CDPARTAR, CDCOOPER, DSCONTEU)
   values (323, 6, '0');
-  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  insert into cecred.crappco (CDPARTAR, CDCOOPER, DSCONTEU)
   values (323, 9, '0');
-  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  insert into cecred.crappco (CDPARTAR, CDCOOPER, DSCONTEU)
   values (323, 1, '0');
-  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  insert into cecred.crappco (CDPARTAR, CDCOOPER, DSCONTEU)
   values (323, 2, '0');
-  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  insert into cecred.crappco (CDPARTAR, CDCOOPER, DSCONTEU)
   values (323, 13, '0');
-  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  insert into cecred.crappco (CDPARTAR, CDCOOPER, DSCONTEU)
   values (323, 3, '1');
-  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  insert into cecred.crappco (CDPARTAR, CDCOOPER, DSCONTEU)
   values (323, 10, '0');
-  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  insert into cecred.crappco (CDPARTAR, CDCOOPER, DSCONTEU)
   values (323, 14, '0');
-  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  insert into cecred.crappco (CDPARTAR, CDCOOPER, DSCONTEU)
   values (323, 5, '0');
-  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  insert into cecred.crappco (CDPARTAR, CDCOOPER, DSCONTEU)
   values (323, 8, '0');
-  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  insert into cecred.crappco (CDPARTAR, CDCOOPER, DSCONTEU)
   values (323, 11, '0');
   COMMIT;
 EXCEPTION
