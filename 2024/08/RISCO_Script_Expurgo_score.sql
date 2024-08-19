071dfd3f5740ae0f41e9106a8f37c4de22524cd7
Guilherme Henrique Boettcher <guilherme.boettcher@cecred.coop.br>
2024-08-19 09:13:29 -0300

    RISCO - Ajuste pasta estrutura

--- 2024/08/RISCO_Script_Expurgo_score.sql
+++ 2024/08/RISCO_Script_Expurgo_score.sql
@@ -2,23 +2,23 @@ DECLARE
 
   vr_dscritic         VARCHAR2(4000);
   vr_exc_erro         EXCEPTION;
- 
-  vr_cdprogra VARCHAR2(50) := 'exp_tbcrd_score_';
+
+  vr_cdprogra VARCHAR2(50) := 'exp_tbcrd_score';
   vr_dsplsql  VARCHAR2(4000);
   vr_jobname  VARCHAR2(4000);
-  
+
   CURSOR cr_crapcop IS
     SELECT c.cdcooper
       FROM cecred.crapcop c
      WHERE c.flgativo = 1
      ORDER BY c.cdcooper DESC;
   rw_crapcop cr_crapcop%ROWTYPE;
-  
+
 BEGIN
-  
+
   EXECUTE IMMEDIATE 'ALTER SESSION SET nls_date_format = ''DD/MM/RRRR''';
   EXECUTE IMMEDIATE 'ALTER SESSION SET nls_numeric_characters = '',.''';
-  
+
   FOR rw_crapcop IN cr_crapcop LOOP
     vr_jobname := 'expurgoScore_' || to_char(rw_crapcop.cdcooper) || '_';
     vr_dsplsql := 'DECLARE'                                                     || chr(13) ||
@@ -30,13 +30,13 @@ BEGIN
                   '   WHERE CDCOOPER =  '|| rw_crapcop.cdcooper                 || chr(13) ||
                   '     AND CDMODELO IN (3,4)'                                  || chr(13) ||
                   '     AND FLVIGENTE = 0;'                                     || chr(13) ||
-                  '  '                                                          || chr(13) ||
+                  ' '                                                           || chr(13) ||
                   '  DELETE FROM CECRED.TBCRD_SCORE'                            || chr(13) ||
                   '   WHERE CDCOOPER = '|| rw_crapcop.cdcooper                  || chr(13) ||
                   '     AND CDMODELO IN (3,4)'                                  || chr(13) ||
                   '     AND FLVIGENTE = 0'                                      || chr(13) ||
                   '     AND DTBASE < vr_dtlimite_expurgo;'                      || chr(13) ||
-                  '  '                                                          || chr(13) ||
+                  ' '                                                           || chr(13) ||
                   '  COMMIT;'                                                   || chr(13) ||
                   'END;';
     cecred.gene0001.pc_submit_job(pr_cdcooper => rw_crapcop.cdcooper
@@ -50,11 +50,11 @@ BEGIN
       vr_dscritic := vr_dscritic || ' - ' || vr_jobname;
       RAISE vr_exc_erro;
     END IF;
-  
+
   END LOOP;
-  
+
   COMMIT;
-  
+
 EXCEPTION 
   WHEN vr_exc_erro THEN
     ROLLBACK;
