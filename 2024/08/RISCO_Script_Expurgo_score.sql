1eb5d217a9e432d97b1c6f0a804286d838364986
Guilherme Henrique Boettcher <guilherme.boettcher@cecred.coop.br>
2024-08-19 09:54:52 -0300

    RISCOS - Inclusao cooperativa 0

--- 2024/08/RISCO_Script_Expurgo_score.sql
+++ 2024/08/RISCO_Script_Expurgo_score.sql
@@ -10,7 +10,6 @@ DECLARE
   CURSOR cr_crapcop IS
     SELECT c.cdcooper
       FROM cecred.crapcop c
-     WHERE c.flgativo = 1
      ORDER BY c.cdcooper DESC;
   rw_crapcop cr_crapcop%ROWTYPE;
 
@@ -53,9 +52,41 @@ BEGIN
 
   END LOOP;
 
+  -- Coop Zero - 0
+  vr_jobname := 'expurgoScore_0_';
+  vr_dsplsql := 'DECLARE'                                                     || chr(13) ||
+                '  vr_dtlimite_expurgo DATE;'                                 || chr(13) ||
+                'BEGIN'                                                       || chr(13) ||
+                '  SELECT (MAX(DTBASE) - INTERVAL ''12'' MONTH) AS vr_dtlimite_expurgo' || chr(13) ||
+                '    INTO vr_dtlimite_expurgo'                                || chr(13) ||
+                '    FROM TBCRD_SCORE'                                        || chr(13) ||
+                '   WHERE CDCOOPER =  0'                                      || chr(13) ||
+                '     AND CDMODELO IN (3,4)'                                  || chr(13) ||
+                '     AND FLVIGENTE = 0;'                                     || chr(13) ||
+                ' '                                                           || chr(13) ||
+                '  DELETE FROM CECRED.TBCRD_SCORE'                            || chr(13) ||
+                '   WHERE CDCOOPER =  0'                                      || chr(13) ||
+                '     AND CDMODELO IN (3,4)'                                  || chr(13) ||
+                '     AND FLVIGENTE = 0'                                      || chr(13) ||
+                '     AND DTBASE < vr_dtlimite_expurgo;'                      || chr(13) ||
+                ' '                                                           || chr(13) ||
+                '  COMMIT;'                                                   || chr(13) ||
+                'END;';
+  cecred.gene0001.pc_submit_job(pr_cdcooper => 3
+                               ,pr_cdprogra => vr_cdprogra        
+                               ,pr_dsplsql  => vr_dsplsql         
+                               ,pr_dthrexe  => SYSTIMESTAMP       
+                               ,pr_interva  => NULL               
+                               ,pr_jobname  => vr_jobname         
+                               ,pr_des_erro => vr_dscritic);
+  IF vr_dscritic IS NOT null THEN
+    vr_dscritic := vr_dscritic || ' - ' || vr_jobname;
+    RAISE vr_exc_erro;
+  END IF;
+
   COMMIT;
 
-EXCEPTION 
+EXCEPTION
   WHEN vr_exc_erro THEN
     ROLLBACK;
     raise_application_error(-20000, vr_dscritic);
