1e36069d16e0c4fa15aab59a4f5cea0735515ebb
Jackson Rosá <jackson.rosa@ailos.coop.br>
2024-08-23 11:40:52 +0000

    Inclusao de dados de conta corrente

--- 2024/08/update-tbchq_deposito_cheque_mob-22-08.sql
+++ 2024/08/update-tbchq_deposito_cheque_mob-22-08.sql
@@ -3,7 +3,7 @@ begin
 update  cecred.tbchq_deposito_cheque_mob m
    set m.insituacao = 4
 where  m.insituacao = 1
-  and  m.dtdeposito < trunc(sysdate);
+  and  m.dtdeposito < trunc(sysdate)
   and (m.cdcooper, m.nrdconta, m.idseqdeposito) IN
        ((16, 943746, 243396),
         (9, 432610, 243393),
