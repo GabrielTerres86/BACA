27a22d01022bc57acc71b814eb9fd8085acd050b
Joao Carlos Goncalves Pinheiro Junior <joao.pinheiro@ailos.coop.br>
2024-08-27 12:43:11 +0000

    Updated INC0354017_Execucao_pc_gerar_seg_arquivo_retorno.sql

--- 2024/08/INC0354017_Execucao_pc_gerar_seg_arquivo_retorno.sql
+++ 2024/08/INC0354017_Execucao_pc_gerar_seg_arquivo_retorno.sql
@@ -1,10 +1,10 @@
 DECLARE
   vr_cdcritic number;
   vr_dscritic varchar2(10000);  
-  vr_dtretorn date := to_date('23/08/2024', 'dd/mm/yyyy');
+  vr_dtretorn date;
   
 BEGIN
-  
+  vr_dtretorn := to_date('23/08/2024', 'dd/mm/yyyy');
   cecred.pgta0001.pc_gerar_seg_arquivo_retorno(pr_dtretorno => vr_dtretorn
                                               ,pr_cdcritic  => vr_cdcritic
                                               ,pr_dscritic  => vr_dscritic); 
@@ -12,6 +12,7 @@ BEGIN
     RAISE_application_error(-20500,'Erro rotina pc_gerar_seg_arquivo_retorno: '||vr_dscritic);
   end if;
   
+  COMMIT;
 EXCEPTION
   WHEN OTHERS THEn
     raise_application_error(-20000, SQLERRM || ' - ' || dbms_utility.format_error_backtrace);
