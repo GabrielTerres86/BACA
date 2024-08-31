03c173cb7bd3da8a4931dfad0f9683fb9efceb63
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2024-08-31 10:36:50 -0300

    Limpeza de cheques duplicados na central

---
+++ 2024/08/limpar_cheques_duplicados_central.sql
@@ -0,0 +1,15 @@
+BEGIN
+  DELETE FROM cecred.crapris WHERE cdcooper = 1 AND dtrefere = '31/08/2024' AND progress_recid = 3580076950;
+  
+  DELETE FROM cecred.crapris WHERE cdcooper = 1 AND dtrefere = '31/08/2024' AND progress_recid = 3580301255;
+  DELETE FROM cecred.crapris WHERE cdcooper = 1 AND dtrefere = '31/08/2024' AND progress_recid = 3580301259;
+  DELETE FROM cecred.crapris WHERE cdcooper = 1 AND dtrefere = '31/08/2024' AND progress_recid = 3580301263;
+  DELETE FROM cecred.crapris WHERE cdcooper = 1 AND dtrefere = '31/08/2024' AND progress_recid = 3580301278;
+  DELETE FROM cecred.crapris WHERE cdcooper = 1 AND dtrefere = '31/08/2024' AND progress_recid = 3580301281;
+  
+  DELETE FROM cecred.crapris WHERE cdcooper = 1 AND dtrefere = '31/08/2024' AND progress_recid = 3579667285;
+  
+  DELETE FROM cecred.crapris WHERE cdcooper = 11 AND dtrefere = '31/08/2024' AND progress_recid = 3579141999;
+  
+  COMMIT;
+END;
