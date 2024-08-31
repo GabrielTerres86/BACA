3349055b1b13b6316f1c4d9525b9cf956aaeef54
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2024-08-31 15:08:37 -0300

    Ajuste limpeza

--- 2024/08/limpeza_transpo_3108.sql
+++ 2024/08/limpeza_transpo_3108.sql
@@ -32,7 +32,21 @@ BEGIN
     
     END LOOP;
       
-    COMMIT; 
+    COMMIT;
+
+    LOOP 
+      
+      DELETE /*+ parallel(20) */ gestaoderisco.htrisco_central_retorno x WHERE x.cdcooper = pr_cdcooper 
+         AND x.dtreferencia = pr_dtrefere AND rownum < 10000;
+      vr_registros := SQL%ROWCOUNT;
+      
+      COMMIT;
+      
+      EXIT WHEN vr_registros = 0;
+    
+    END LOOP;
+      
+    COMMIT;     
     
     update cecred.crapdat set dtmvcentral = to_date('29/08/2024', 'DD/MM/RRRR') where cdcooper = 9;
     commit;
