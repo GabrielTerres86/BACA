1e6a773872c1a26f8cb49b03cd25169408e199d6
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2024-09-03 16:38:54 -0300

    Teste lançamento debito

---
+++ 2024/09/delete-lancamento-debito-parcela.sql
@@ -0,0 +1,7 @@
+begin
+
+delete from cecred.craplau where cdcooper = 11 and nrdconta = 83728627 and cdhistor = 4129 and nrctremp = 440091 and nrdocmto = 99597225;
+delete from cecred.craplcm where cdcooper = 11 and nrdconta = 83728627 and cdhistor = 4129 and nrdocmto = 99597225;
+
+commit;
+end;
