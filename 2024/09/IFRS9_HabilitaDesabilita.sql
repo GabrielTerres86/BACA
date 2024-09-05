6eb780316144626f0fc16f62d6e28362ecfe264b
Andre Ricardo Maass <andre.maass@ailos.coop.br>
2024-09-05 19:24:46 +0000

    Added IFRS9_HabilitaDesabilita.sql to /2024/09

---
+++ 2024/09/IFRS9_HabilitaDesabilita.sql
@@ -0,0 +1,43 @@
+BEGIN
+
+  SELECT MAX(cdpartar) INTO aux_cdpartar_add FROM crappat;
+
+  aux_cdpartar_add :=  aux_cdpartar_add + 1;
+
+  -- Habilitado = 1 e Desabilitado = 0
+  insert into crappat (CDPARTAR, NMPARTAR, TPDEDADO, CDPRODUT)
+  values (aux_cdpartar_add, 'Habilita|Desabilita IFRS9', 2, 13);
+
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 1,  '1');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 2,  '1');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 3,  '1');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)  
+  values (aux_cdpartar_add, 5,  '1');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 6,  '1');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 7,  '1');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 8,  '1');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 9,  '1');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 10, '1');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 11, '1');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 12, '1');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 13, '1');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 14, '1');
+  insert into crappco (CDPARTAR, CDCOOPER, DSCONTEU)
+  values (aux_cdpartar_add, 16, '1');
+  
+  COMMIT;
+ 
+END;
+/
