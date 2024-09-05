5c2a2d925cd43b5cc1c13e431f0b4466513ddd39
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2024-09-05 13:55:07 -0300

    Ajuste no script para comportar a carga do score

--- 2024/09/task_947775_crapprm.sql
+++ 2024/09/task_947775_crapprm.sql
@@ -1,7 +1,21 @@
+DECLARE
+  vr_dscritic VARCHAR2(4000);
 BEGIN
-   insert into CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
-   values ('CRED', 0, 'LINHA_PEAC_3040_SOLIDARI', 'Parametro linha LINHA_PEAC_3040_SOLIDARIO', '4627,4626');
+
+  insert into CRAPPRM (NMSISTEM, CDCOOPER, CDACESSO, DSTEXPRM, DSVLRPRM)
+  values ('CRED', 0, 'LINHA_PEAC_3040_SOLIDARI', 'Parametro linha LINHA_PEAC_3040_SOLIDARIO', '4627,4626');
+
   COMMIT;
+  
+  gestaoderisco.gerarCargaScore(pr_idscore => 1021,
+                                pr_dscritic => vr_dscritic);
+
+  IF TRIM(vr_dscritic) IS NOT NULL THEN
+    dbms_output.put_line('ERRO: ' || vr_dscritic);
+    ROLLBACK;
+  END IF;
+  commit;
+  
 EXCEPTION
   WHEN OTHERS THEN
     DBMS_OUTPUT.put_line('Erro no script de inser CRAPPRM: ' || SQLERRM);
