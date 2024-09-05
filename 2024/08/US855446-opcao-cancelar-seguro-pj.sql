b6b01525cfe6b7c7645032dc3018c6892c11cfb4
Bruno Henrique Jacobsen de Andrade <t0036649@cecred.coop.br>
2024-09-05 13:50:38 -0300

    Ajuste registros de informacoes prestamista

--- 2024/08/US855446-opcao-cancelar-seguro-pj.sql
+++ 2024/08/US855446-opcao-cancelar-seguro-pj.sql
@@ -1,9 +1,6 @@
 begin
   INSERT INTO CECRED.CRAPACA (NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
-  VALUES ('CANCELA_SEGURO_PRST_PJ', 'SEGU0004', 'pc_exclui_lcto_canc_seguro', 'pr_cdcooper,pr_nrdconta,pr_nrctrseg,pr_cdmotcan', 2564);
+  VALUES ('CANCELA_SEGURO_PRST_PJ', 'SEGU0004', 'pc_exclui_lcto_canc_seguro', 'pr_cdcooper,pr_nrdconta,pr_nrctrseg,pr_cdmotcan', (SELECT NRSEQRDR FROM CECRED.CRAPRDR WHERE NMPROGRA = 'SEGU0004'));
   
   COMMIT;
-end;                                                                           
-                                                                               
-                                                                               
-                                                                               
+end;
--- 2024/08/insert_de_para_termo_cancelamento_pj.sql
+++ 2024/08/insert_de_para_termo_cancelamento_pj.sql
@@ -2,7 +2,7 @@ DECLARE
   vr_seq_hist cecred.tbseg_historico_relatorio.idhistorico_relatorio%TYPE;
 BEGIN
   INSERT INTO CECRED.CRAPACA (NMDEACAO, NMPACKAG, NMPROCED, LSTPARAM, NRSEQRDR)
-  VALUES ('IMPRIME_TERMO_CANCELA_SEGURO_PRST_PJ', 'SEGU0004', 'pc_imp_termo_cancel_seg_pres', 'pr_cdcooper,pr_nrdconta,pr_nrctrseg', 2564);
+  VALUES ('IMPRIME_TERMO_CANCELA_SEGURO_PRST_PJ', 'SEGU0004', 'pc_imp_termo_cancel_seg_pres', 'pr_cdcooper,pr_nrdconta,pr_nrctrseg', (SELECT NRSEQRDR FROM CECRED.CRAPRDR WHERE NMPROGRA = 'SEGU0004'));
   
   SELECT nvl(MAX(t.idhistorico_relatorio),0)+1
     INTO vr_seq_hist
