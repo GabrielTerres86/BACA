7f948394a2d81d464f4409affac79d629dadd957
Douglas <douglas.kloppel@ailos.coop.br>
2024-09-12 17:53:25 -0300

    Correção saldo aplicações RDCA60

---
+++ 2024/09/scriptinc2022907.sql
@@ -0,0 +1,23 @@
+DECLARE
+  vr_idprglog  tbgen_prglog.idprglog%TYPE := 0;
+BEGIN
+  UPDATE craprda
+     SET vlslfmes = 0,
+         insaqtot = 1,
+         vlsdrdca = 0
+   WHERE cdcooper = 1
+     AND ((nrdconta = 2023180 and nraplica in (163391,148888))or
+          (nrdconta = 2022907 and nraplica in (188092)));       
+  
+  COMMIT;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    CECRED.pc_log_programa(pr_dstiplog   => 'O',
+                           pr_dsmensagem => SQLERRM,
+                           pr_cdmensagem => 333,
+                           pr_cdprograma => 'INC0344661',
+                           pr_cdcooper   => 3,
+                           pr_idprglog   => vr_idprglog); 
+      ROLLBACK;  
+END;      
