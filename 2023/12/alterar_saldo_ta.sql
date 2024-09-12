69bda6f503270befb03bbc08fe5f6f4581eb3fea
Arthur <atwiiister@gmail.com>
2024-09-12 11:59:10 -0300

    Update alterar_saldo_ta.sql

--- 2023/12/alterar_saldo_ta.sql
+++ 2023/12/alterar_saldo_ta.sql
@@ -1,8 +1,8 @@
 BEGIN   
-    	update crapstf set VLDSDINI = 1000.00, VLDSDFIN = 1000.00
+       update crapstf set VLDSDINI = 1000.00, VLDSDFIN = 1000.00
 	   where NRTERFIN = 201 and
 	   CDCOOPER = 9 and
-	   DTMVTOLT = to_date('12-09-2024', 'dd-mm-yyyy')
+	   DTMVTOLT = to_date('12-09-2024', 'dd-mm-yyyy');
 
     COMMIT; 
     
