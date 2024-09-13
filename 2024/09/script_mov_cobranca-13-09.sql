fde70d152954aee30550310f106150160966ecc0
Evandro Guaranha(Amcom) <evandro.silva@amcom.com.br>
2024-09-13 08:29:13 -0300

    Ajustes tabelas diaria

--- 2024/09/script_mov_cobranca-13-09.sql
+++ 2024/09/script_mov_cobranca-13-09.sql
@@ -5,9 +5,6 @@ BEGIN
     INSERT INTO COBRANCA.tbcobran_controle_diario_liquidacao (DTREFERENCIA, TPMARCO)
     VALUES (to_date('13-09-2024', 'dd-mm-yyyy'), 1);
     
-    INSERT INTO COBRANCA.tbcobran_controle_diario_liquidacao (DTREFERENCIA, TPMARCO)
-    VALUES (to_date('16-09-2024', 'dd-mm-yyyy'), 1);
-    
     DELETE tbgen_batch_paralelo a 
      WHERE a.cdcooper = 7
        AND a.dtmvtolt >= to_date('09/09/2024', 'dd/mm/yyyy');
