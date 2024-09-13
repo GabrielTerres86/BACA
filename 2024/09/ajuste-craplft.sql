711892a1db4fc2a1ba6d63af296afce68e34dafb
Jackson Rosá <jackson.rosa@ailos.coop.br>
2024-09-13 19:00:45 +0000

    Added ajuste-craplft.sql

---
+++ 2024/09/ajuste-craplft.sql
@@ -0,0 +1,14 @@
+BEGIN
+  UPDATE cecred.craplft
+     SET flintegra  = NULL,
+         cdsituacao = NULL,
+         dsintegra  = NULL,
+         insitfat   = 1,
+         dtmvtolt   = to_date('13/09/2024', 'DD/MM/YYYY'),
+         dtvencto   = to_date('13/09/2024', 'DD/MM/YYYY'),
+         nrseqdig   = nrseqdig  + 9000000
+   WHERE progress_recid IN (77853192,77853167,77853090,77852792,77852164,
+                            77851333,77850567,77848571,77848449,77847002,
+                            77846858,77845659,77844819,77839141);
+  COMMIT;
+END;
