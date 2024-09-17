1871d789b0c8231b6036b576caee8d53f43a8f4a
Jackson Rosá <jackson.rosa@ailos.coop.br>
2024-09-17 11:36:11 +0000

    Resetar status e datas de registros de faturas para possibilitar a integração ao Ailos+

---
+++ 2024/09/ajuste-faturas-170924.sql
@@ -0,0 +1,15 @@
+BEGIN
+  UPDATE cecred.craplft
+     SET flintegra  = NULL,
+         cdsituacao = NULL,
+         dsintegra  = NULL,
+         insitfat   = 1,
+         dtmvtolt   = to_date('17/09/2024', 'DD/MM/YYYY'),
+         dtvencto   = to_date('17/09/2024', 'DD/MM/YYYY'),
+         nrseqdig   = nrseqdig + 9000000
+   WHERE progress_recid IN (77901012,77901028,77900577,77899957,77900234,77899708,77899552,77900927,77899762,
+                            77899400,77899461,77900358,77900729,77900334,77900357,77899716,77899438,77900915,
+                            77900914);
+
+  COMMIT;
+END;
