d9d2cc1e9f020726c9e9082798b2ce9928a7abbd
Geder Silva Severino <compasso-geder.severino@ailos.coop.br>
2024-09-18 14:12:49 -0300

    [#970974] [#970976] Carga de fases de antifraude dispositivo.

---
+++ 2024/09/970138_02_CARGA_PIX_TBPIX_TIPO_FASE.sql
@@ -0,0 +1,5 @@
+BEGIN
+  INSERT INTO PIX.TBPIX_TIPO_FASE (IDFASE, NMFASE, IDSITUACAO) VALUES (24, 'Antifraude - Consulta Dispositivo Cadastrado.', 'A');
+  INSERT INTO PIX.TBPIX_TIPO_FASE (IDFASE, NMFASE, IDSITUACAO) VALUES (25, 'Antifraude - Consulta Marcação de Fraude.', 'A');
+COMMIT;
+END;
