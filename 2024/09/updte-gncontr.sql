bb874d32edc2da4fd17f59f40754096fecac8e24
Paulo Felipe Kowalsky <t0035805@cecred.coop.br>
2024-09-23 13:40:54 -0300

    ajustes gncontr coop7

--- 2024/09/updte-gncontr.sql
+++ 2024/09/updte-gncontr.sql
@@ -1,8 +1,9 @@
 begin
 update cecred.gncontr 
-   set dtmvtolt = to_date('20/09/2024','dd/mm/yyyy')
+   set dtmvtolt = to_date('20/09/2024','dd/mm/yyyy'),
+       tpdcontr = 4
    where cdconven in (172,173) 
-   and tpdcontr in (1,4)
+   and tpdcontr = 1
    and dtmvtolt = to_date('19/04/2024','dd/mm/yyyy');
 commit; 
 end;
