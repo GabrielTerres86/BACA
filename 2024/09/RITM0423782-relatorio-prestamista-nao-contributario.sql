070397de2a14d011ce11885bdbaf0a6220af3dbe
Raiza Maus <premiersoft-raiza.maus@ailos.coop.br>
2024-09-26 08:12:54 -0300

    Novo relatório não contributário sem assinatura

--- 2024/09/RITM0423782-relatorio-prestamista-nao-contributario.sql
+++ 2024/09/RITM0423782-relatorio-prestamista-nao-contributario.sql
@@ -5,7 +5,7 @@ UPDATE cecred.tbseg_historico_relatorio r
  WHERE r.cdacesso = 'NOVA_PROPOSTA_PREST_4';
  
 INSERT INTO cecred.tbseg_historico_relatorio(idhistorico_relatorio, cdacesso, dstexprm, dsvlrprm, dtinicio, dtfim, fltpcusteio, tppessoa)
-VALUES (10,'NOVA_PROPOSTA_PREST_5','Relatório Prestamista depois da modificação da RITM0423782','proposta_prestamista_r0423782.jasper',TO_DATE('25/09/2024','DD/MM/YYYY'),NULL,1,1);
+VALUES (11,'NOVA_PROPOSTA_PREST_5','Relatório Prestamista depois da modificação da RITM0423782','proposta_prestamista_r0423782.jasper',TO_DATE('25/09/2024','DD/MM/YYYY'),NULL,1,1);
 
 COMMIT;
 END;
