0de57e18db08eb8f98e265b98a47e86cc150aa0f
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2024-10-03 13:29:47 -0300

    Ajuste

--- 2024/09/ajuste-scr-agosto.sql
+++ 2024/09/ajuste-scr-agosto.sql
@@ -1,6 +1,4 @@
 DECLARE
-  vr_dscritic VARCHAR2(4000);
-  
   CURSOR cr_principal IS
     SELECT a.nrcpfcgc nrcpfcgc_destino
           ,o.nrcpfcgc nrcpfcgc_origem
@@ -75,15 +73,8 @@ BEGIN
     
     COMMIT;
   END LOOP;
-  
-  gestaoderisco.gerarCargaScore(pr_idscore => 1901,
-                                pr_dscritic => vr_dscritic);
-  IF TRIM(vr_dscritic) IS NOT NULL THEN
-    dbms_output.put_line('ERRO: ' || vr_dscritic);
-    ROLLBACK;
-  END IF;
-  
-  COMMIT;
+
+  commit;
 
 EXCEPTION
   WHEN OTHERS THEN
