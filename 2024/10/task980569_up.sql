05f4e2b27c316ac739ded9561c2f1c7979525d56
Cristiano Schroeder <premiersoft-cristiano.schroeder@ailos.coop.br>
2024-10-03 13:57:42 -0300

    Pull Request 168444: Task 980569 alteracao Qualificação da Operação. Credcrea Conta 15684393 e Con...

--- 2024/10/task980569_up.sql
+++ 2024/10/task980569_up.sql
@@ -1,20 +1,20 @@
 BEGIN
-  UPDATE CECRED.CRAPEPR EPR
-     SET EPR.IDQUAPRC = 3
-   WHERE EPR.CDCOOPER = 7
-     AND EPR.NRDCONTA = 99731509 
-     AND EPR.NRCTREMP = 95129;
+  UPDATE cecred.crapepr epr
+     SET epr.idquaprc = 3
+   WHERE epr.cdcooper = 7
+     AND epr.nrdconta = 99731509 
+     AND epr.nrctremp = 95129;
   
-  UPDATE CECRED.CRAWEPR EPR
-     SET EPR.IDQUAPRO = 3
-   WHERE EPR.CDCOOPER = 7
-     AND EPR.NRDCONTA = 99731509 
-     AND EPR.NRCTREMP = 95129;
+  UPDATE cecred.crawepr epr
+     SET epr.idquapro = 3
+   WHERE epr.cdcooper = 7
+     AND epr.nrdconta = 99731509 
+     AND epr.nrctremp = 95129;
      
   COMMIT;
      
   EXCEPTION
     WHEN OTHERS THEN
       ROLLBACK;
-      RAISE_application_error(-20500, SQLERRM);      
+      raise_application_error(-20500, SQLERRM);      
 END;
