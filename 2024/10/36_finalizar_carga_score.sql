5113c817a485d93472b7098dfb3087431dca55f5
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2024-10-09 10:29:27 -0300

    Ajuste update

--- 2024/10/36_finalizar_carga_score.sql
+++ 2024/10/36_finalizar_carga_score.sql
@@ -7,14 +7,16 @@ begin
     UPDATE cecred.tbcrd_score c
        SET c.tppessoa = nvl((SELECT MAX(ass.inpessoa) FROM crapass ass WHERE ass.nrcpfcnpj_base = c.nrcpfcnpjbase), c.tppessoa)
      WHERE c.dtbase = to_date('01/09/2024', 'DD/MM/RRRR')
-       AND c.cdmodelo = 3;
+       AND c.cdmodelo = 3
+       AND c.nrcpfcnpjbase NOT IN (27396908,50760050,5405017,27845990,39477932);
 
     commit;
     
     update cecred.tbcrd_score_exclusao e
        SET e.tppessoa = nvl((SELECT MAX(ass.inpessoa) FROM crapass ass WHERE ass.nrcpfcnpj_base = e.nrcpfcnpjbase), e.tppessoa)
      WHERE e.dtbase = to_date('01/09/2024', 'DD/MM/RRRR')
-       AND e.cdmodelo = 3;
+       AND e.cdmodelo = 3
+       AND e.nrcpfcnpjbase NOT IN (27396908,50760050,5405017,27845990,39477932);
     
     commit;
     
