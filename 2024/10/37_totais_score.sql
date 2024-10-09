5f05e7cac3ae6e983122789283aa5a79707197de
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2024-10-09 13:50:12 -0300

    ajuste totais

--- 2024/10/37_totais_score.sql
+++ 2024/10/37_totais_score.sql
@@ -1,7 +1,7 @@
 BEGIN
   UPDATE cecred.tbcrd_carga_score x
      SET x.qtregis_fisica = 1480220
-        ,x.qtregis_juridi = 196839
+        ,x.qtregis_juridi = 196870
    WHERE x.dtbase = to_date('01/09/2024', 'DD/MM/RRRR')
      AND x.cdmodelo = 3;
   COMMIT;
