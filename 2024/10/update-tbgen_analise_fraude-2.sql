a5edc92ce3693e60f59e2e4639bafdc5b58d9435
Paulo Felipe Kowalsky <t0035805@cecred.coop.br>
2024-10-09 17:39:21 -0300

    update antifraude

---
+++ 2024/10/update-tbgen_analise_fraude-2.sql
@@ -0,0 +1,10 @@
+begin
+  
+update cecred.tbgen_analise_fraude fra
+  set fra.cdparecer_analise = 1
+WHERE fra.idanalise_fraude in (400601413,400601416,400601414,400601415,400601417)
+  and fra.cdparecer_analise = 0;
+
+
+commit;
+end;
