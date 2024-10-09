48b03c9d5603cffaf9c973b0e43b1497d880e022
Darlei Fernando Zillmer (Supero) <t0032717@ailos.coop.br>
2024-10-09 11:41:45 -0300

    Ajuste chaves

--- 2024/10/36_finalizar_carga_score.sql
+++ 2024/10/36_finalizar_carga_score.sql
@@ -1,35 +1,86 @@
 DECLARE
   vr_dscritic VARCHAR2(4000);
-  
-begin
-  
-  begin
-    UPDATE cecred.tbcrd_score c
-       SET c.tppessoa = nvl((SELECT MAX(ass.inpessoa) FROM crapass ass WHERE ass.nrcpfcnpj_base = c.nrcpfcnpjbase), c.tppessoa)
-     WHERE c.dtbase = to_date('01/09/2024', 'DD/MM/RRRR')
-       AND c.cdmodelo = 3
-       AND c.nrcpfcnpjbase NOT IN (27396908,50760050,5405017,27845990,39477932);
 
-    commit;
-    
-    update cecred.tbcrd_score_exclusao e
-       SET e.tppessoa = nvl((SELECT MAX(ass.inpessoa) FROM crapass ass WHERE ass.nrcpfcnpj_base = e.nrcpfcnpjbase), e.tppessoa)
-     WHERE e.dtbase = to_date('01/09/2024', 'DD/MM/RRRR')
-       AND e.cdmodelo = 3
-       AND e.nrcpfcnpjbase NOT IN (27396908,50760050,5405017,27845990,39477932);
-    
-    commit;
-    
-  exception
-    when others then
-      rollback;
-      raise_application_error(-20000, sqlerrm);
-  end;
+  CURSOR cr_loop IS
+    SELECT *
+      FROM (SELECT c.*,
+                   (SELECT MAX(ass.inpessoa)
+                      FROM crapass ass
+                     WHERE ass.nrcpfcnpj_base = c.nrcpfcnpjbase) tppessoa_novo
+              FROM tbcrd_score_exclusao c
+             WHERE c.dtbase = to_date('01/09/2024', 'DD/MM/RRRR')
+               AND c.cdmodelo = 3
+               AND c.nrcpfcnpjbase NOT IN
+                   (27396908, 50760050, 5405017, 27845990, 39477932))
+     WHERE tppessoa_novo <> tppessoa;
+  rw_loop cr_loop%ROWTYPE;
+
+  CURSOR cr_loop2 IS
+    SELECT *
+      FROM (SELECT c.*,
+                   (SELECT MAX(ass.inpessoa)
+                      FROM crapass ass
+                     WHERE ass.nrcpfcnpj_base = c.nrcpfcnpjbase) tppessoa_novo
+              FROM tbcrd_score c
+             WHERE c.dtbase = to_date('01/09/2024', 'DD/MM/RRRR')
+               AND c.cdmodelo = 3
+               AND c.nrcpfcnpjbase NOT IN
+                   (27396908, 50760050, 5405017, 27845990, 39477932))
+     WHERE tppessoa_novo <> tppessoa;
+  rw_loop2 cr_loop2%ROWTYPE;
+
+BEGIN
+
+  FOR rw_loop IN cr_loop LOOP
   
-  commit;
+    INSERT INTO tbcrd_score
+      SELECT y.cdcooper,
+             y.cdmodelo,
+             y.dtbase,
+             rw_loop.tppessoa_novo,
+             y.nrcpfcnpjbase,
+             y.nrscore_alinhado,
+             y.dsclasse_score,
+             y.dsexclusao_principal,
+             y.flvigente,
+             y.cdscore,
+             y.obdetalhamento,
+             y.cdsegmento_modelo
+        FROM tbcrd_score y
+       WHERE y.cdcooper = rw_loop.cdcooper
+         AND y.dtbase = rw_loop.dtbase
+         AND y.nrcpfcnpjbase = rw_loop.nrcpfcnpjbase;
   
-exception
-  when others then
-    rollback;
-    raise_application_error(-20000, sqlerrm);
-end;
+    UPDATE tbcrd_score_exclusao x
+       SET x.tppessoa = rw_loop.tppessoa_novo
+     WHERE x.cdcooper = rw_loop.cdcooper
+       AND x.dtbase = rw_loop.dtbase
+       AND x.nrcpfcnpjbase = rw_loop.nrcpfcnpjbase;
+  
+    DELETE tbcrd_score y
+     WHERE y.cdcooper = rw_loop.cdcooper
+       AND y.dtbase = rw_loop.dtbase
+       AND y.nrcpfcnpjbase = rw_loop.nrcpfcnpjbase
+       AND y.tppessoa = rw_loop.tppessoa;
+  
+    COMMIT;
+  END LOOP;
+
+  COMMIT;
+
+  FOR rw_loop2 IN cr_loop2 LOOP
+    UPDATE tbcrd_score x
+       SET x.tppessoa = rw_loop2.tppessoa_novo
+     WHERE x.cdcooper = rw_loop2.cdcooper
+       AND x.dtbase = rw_loop2.dtbase
+       AND x.nrcpfcnpjbase = rw_loop2.nrcpfcnpjbase;
+    COMMIT;
+  END LOOP;
+
+  COMMIT;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    raise_application_error(-20000, SQLERRM);
+END;
