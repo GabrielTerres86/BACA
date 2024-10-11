bf20308386079edfda2d2d4f497df476c8957265
Guilherme Breis <premiersoft-guilherme.pinheiro@ailos.coop.br>
2024-10-11 14:38:23 -0300

    Espaçamento

--- 2024/10/update-consignado-179261.sql
+++ 2024/10/update-consignado-179261.sql
@@ -3,9 +3,9 @@ BEGIN
   UPDATE cecred.tbepr_consignado_pagamento t
      SET t.instatus = 5
    WHERE t.cdcooper = 13
-         AND t.nrdconta = 416606
-         AND t.nrctremp = 179261
-         AND t.nrparepr = 22;
+     AND t.nrdconta = 416606
+     AND t.nrctremp = 179261
+     AND t.nrparepr = 22;
 
   COMMIT;
 EXCEPTION
