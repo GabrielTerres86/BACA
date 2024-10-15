5bca650646b4e9bea33b83b7f34b3603ddc78f83
Evandro Almeida Oliveira <premiersoft-evandro.oliveira@ailos.coop.br>
2024-10-15 10:31:31 -0300

    Atualiza o campo cdempresinclusao e remove arquivo temporário

--- 2024/10/atualiza-cd-empresInclusao.sql
+++ 2024/10/atualiza-cd-empresInclusao.sql
@@ -1,4 +1,4 @@
-﻿BEGIN
+BEGIN
 
   FOR x IN (SELECT ep.cdcooper
                   ,ep.nrdconta
@@ -8,20 +8,20 @@
              WHERE ep.cdempres > 0
                AND ep.cdempres_inclusao = 0) LOOP
   
-BEGIN 
-   UPDATE cecred.crapepr ep
-       SET ep.cdempres_inclusao = x.cdempres
-     WHERE ep.cdcooper = x.cdcooper
-       AND ep.nrdconta = x.nrdconta
-       AND ep.nrctremp = x.nrctremp;
+    BEGIN
+      UPDATE cecred.crapepr ep
+         SET ep.cdempres_inclusao = x.cdempres
+       WHERE ep.cdcooper = x.cdcooper
+         AND ep.nrdconta = x.nrdconta
+         AND ep.nrctremp = x.nrctremp;
+    
+    EXCEPTION
+      WHEN OTHERS THEN
+        ROLLBACK;
+    END;
   
-  EXCEPTION
-    WHEN OTHERS THEN     
-  ROLLBACK;
-END;
-
-COMMIT;
-
-END LOOP;
+    COMMIT;
+  
+  END LOOP;
 
 END;
