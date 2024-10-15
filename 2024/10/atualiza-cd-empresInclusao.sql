d1e10807346c09a8b9d20fc2c71516b40c54a100
Evandro Almeida Oliveira <premiersoft-evandro.oliveira@ailos.coop.br>
2024-10-15 09:36:14 -0300

    Atualiza o campo cdempresinclusao

---
+++ 2024/10/atualiza-cd-empresInclusao.sql
@@ -0,0 +1,26 @@
+﻿BEGIN
+
+  FOR x IN (SELECT ep.cdcooper
+                  ,ep.nrdconta
+                  ,ep.nrctremp
+                  ,ep.cdempres
+              FROM cecred.crapepr ep
+             WHERE ep.cdempres > 0
+               AND ep.cdempres_inclusao = 0) loop
+  
+BEGIN 
+   UPDATE cecred.crapepr ep
+       SET ep.cdempres_inclusao = x.cdempres
+     WHERE ep.cdcooper = x.cdcooper
+       AND ep.nrdconta = x.nrdconta
+       AND ep.nrctremp = x.nrctremp;
+  
+  EXCEPTION
+    WHEN OTHERS THEN     
+  ROLLBACK;
+END;
+
+
+END LOOP;
+
+END;
