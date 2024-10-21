6ae39e6091af28436ee8ad65026b2c90f6291616
James Kuehl <f0034526@cecred.coop.br>
2024-10-21 15:50:26 -0300

    Exclusao proposta api

--- 2024/10/prj0025037_api_exclusao_proposta.sql
+++ 2024/10/prj0025037_api_exclusao_proposta.sql
@@ -1,4 +1,5 @@
 begin
-   delete cecred.tbseg_req_nrproposta_pj pj where pj.cdcooper = 11 and pj.cdstatus = 1;
+   delete seguro.tbseg_req_nrproposta_pj pj where pj.cdcooper = 11 and pj.cdstatus = 1;
    commit;
 end;   
+
