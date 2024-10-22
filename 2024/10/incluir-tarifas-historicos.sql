75e5195229b240d15c52bd57a37e9efd6c89078c
Renato Darosci <premiersoft-renato.darosci@ailos.coop.br>
2024-10-22 13:57:17 -0300

    Incluir registros de históricos

---
+++ 2024/10/incluir-tarifas-historicos.sql
@@ -0,0 +1,29 @@
+DECLARE 
+
+  CURSOR cr_dados IS
+    SELECT t.cdcooper
+         , t.cdhistor
+      FROM craphis t 
+     WHERE t.cdcooper IN (8,16)
+       AND t.cdhistor >= 4590;
+       
+BEGIN
+  
+  FOR reg IN cr_dados LOOP
+    BEGIN
+      INSERT INTO crapthi(cdhistor
+                         ,dsorigem
+                         ,vltarifa
+                         ,cdcooper)
+                   VALUES(reg.cdhistor
+                         ,'AIMARO'
+                         ,0
+                         ,reg.cdcooper);
+    EXCEPTION
+      WHEN dup_val_on_index THEN
+        NULL;
+    END;
+  END LOOP;
+  
+  COMMIT;
+END;
