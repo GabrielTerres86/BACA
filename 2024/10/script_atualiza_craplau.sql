5b291096a7df00ad6c395334636b864ecaf17cbd
eltonfernando <elton@ailos.coop.br>
2024-10-22 12:04:01 -0300

    atualiza craplau

---
+++ 2024/10/script_atualiza_craplau.sql
@@ -0,0 +1,12 @@
+
+begin 
+
+  update craplau l
+     set l.dtmvtopg = to_date('22/10/2024','dd/mm/yyyy')
+   where l.cdcooper = 2
+     and l.nrdconta = 99020432
+     and l.vllanaut = 4 ;
+  commit;                        
+ 
+end;                        
+
