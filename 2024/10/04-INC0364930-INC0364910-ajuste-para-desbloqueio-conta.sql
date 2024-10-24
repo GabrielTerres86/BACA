008ec952726d7cb94f04a7321129d5019caab4fa
Daniel Filipe Santos Costa <daniel.costa@ailos.coop.br>
2024-10-24 20:39:37 -0300

    [INC0364930] [INC0364910] Ajuste em bloqueios para serem finalizados

---
+++ 2024/10/04-INC0364930-INC0364910-ajuste-para-desbloqueio-conta.sql
@@ -0,0 +1,17 @@
+DECLARE
+	  pr_idsolblqanalitico CONSTANT VARCHAR2(100) := '213981F530320428E0630A29357434D1'; -- Valor do ID do Bloqueio
+	  pr_cdcritic crapcri.cdcritic%TYPE;
+	  pr_dscritic VARCHAR2(4000);
+	BEGIN
+	  CONTACORRENTE.devolucaoParcialBloqueioPix(
+		pr_idsolblqanalitico => pr_idsolblqanalitico,
+		pr_cdcritic => pr_cdcritic,
+		pr_dscritic => pr_dscritic
+	  );
+	  -- Exibir os valores dos parâmetros de saída
+	  DBMS_OUTPUT.PUT_LINE('Crítica: ' || pr_cdcritic);
+	  DBMS_OUTPUT.PUT_LINE('Descrição da Crítica: ' || pr_dscritic);
+	EXCEPTION
+	  WHEN OTHERS THEN
+		DBMS_OUTPUT.PUT_LINE('Ocorreu um erro: ' || SQLERRM);
+	END;
