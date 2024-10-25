c5c4e644e09d77b2c5d0d1e4fa8748c4d7f8fc4d
Mauricio Pfleger <mauricio.pfleger@ailos.coop.br>
2024-10-25 10:35:20 -0300

    Revert "Commit e4145d99: Inclusão de parâmetro para ligar/desligar logs na estatisticamobile"
    This reverts commit d952fe5dd50381029e102bb3f85a0700299c5a7f.

--- 2024/10/insert_parametromobile.sql
+++
@@ -1,4 +0,0 @@
-BEGIN
-  INSERT INTO CECRED.PARAMETROMOBILE (NOME, DESCRICAO, VALOR, SISTEMA) VALUES ('GravarLogEstatisticaMobile', 'Flag que determina se deve ocorrer a gravação de log da estatisticamobile', 1, 1);
-  COMMIT;   
-END;
